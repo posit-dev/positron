8d6bb48dd3b078aaddbe126763cacbe85b3470f9
Don Jayamanne <don.jayamanne@yahoo.com>
2018-03-08 16:55:03 -0600

    Unit testing of experimental debugger on CI server
    Fixes microsoft/vscode-python#742

--- extensions/positron-python/src/test/debugger/portAndHost.test.ts
+++ extensions/positron-python/src/test/debugger/portAndHost.test.ts
@@ -7,6 +7,7 @@ import * as getFreePort from 'get-port';
 import * as net from 'net';
 import * as path from 'path';
 import { DebugClient } from 'vscode-debugadapter-testsupport';
+import { noop } from '../../client/common/core.utils';
 import { LaunchRequestArguments } from '../../client/debugger/Common/Contracts';
 import { IS_MULTI_ROOT_TEST, TEST_DEBUGGER } from '../initialize';
 
@@ -28,10 +29,6 @@ const EXPERIMENTAL_DEBUG_ADAPTER = path.join(__dirname, '..', '..', 'client', 'd
                 // tslint:disable-next-line:no-invalid-this
                 this.skip();
             }
-            if (debuggerType !== 'python') {
-                // tslint:disable-next-line:no-invalid-this
-                return this.skip();
-            }
             await new Promise(resolve => setTimeout(resolve, 1000));
             debugClient = new DebugClient('node', testAdapterFilePath, debuggerType);
             await debugClient.start();
@@ -40,14 +37,12 @@ const EXPERIMENTAL_DEBUG_ADAPTER = path.join(__dirname, '..', '..', 'client', 'd
             // Wait for a second before starting another test (sometimes, sockets take a while to get closed).
             await new Promise(resolve => setTimeout(resolve, 1000));
             try {
-                // tslint:disable-next-line:no-empty
-                debugClient.stop().catch(() => { });
+                debugClient.stop().catch(noop);
                 // tslint:disable-next-line:no-empty
             } catch (ex) { }
         });
 
         function buildLauncArgs(pythonFile: string, stopOnEntry: boolean = false, port?: number, host?: string): LaunchRequestArguments {
-            // pythonPath: '/Users/donjayamanne/anaconda3/envs/py36/bin/python',
             return {
                 program: path.join(debugFilesPath, pythonFile),
                 cwd: debugFilesPath,
@@ -103,8 +98,7 @@ const EXPERIMENTAL_DEBUG_ADAPTER = path.join(__dirname, '..', '..', 'client', 'd
             expect(exception!.message).contains('ENOTFOUND', 'Debugging failed for some other reason');
         });
         test('Confirm debuggig fails when provided port is in use', async () => {
-            // tslint:disable-next-line:no-empty
-            const server = net.createServer((s) => { });
+            const server = net.createServer(noop);
             const port = await new Promise<number>((resolve, reject) => server.listen({ host: 'localhost', port: 0 }, () => resolve(server.address().port)));
             let exception: Error | undefined;
             try {
