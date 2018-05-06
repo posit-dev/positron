c81f4b79ab01ab7101791c6ef29c738c9ef24b8c
Don Jayamanne <don.jayamanne@yahoo.com>
2018-05-05 23:11:16 -0500

    Fix typescript compilation error (microsoft/vscode-python#1624)
    * Fix compiler error
    * Fixes microsoft/vscode-python#1623

--- extensions/positron-python/src/client/common/application/applicationShell.ts
+++ extensions/positron-python/src/client/common/application/applicationShell.ts
@@ -6,50 +6,49 @@
 const opn = require('opn');
 
 import { injectable } from 'inversify';
-import * as vscode from 'vscode';
-import { Disposable, StatusBarAlignment, StatusBarItem, WorkspaceFolder, WorkspaceFolderPickOptions } from 'vscode';
+import { CancellationToken, Disposable, InputBoxOptions, MessageItem, MessageOptions, OpenDialogOptions, QuickPickItem, QuickPickOptions, SaveDialogOptions, StatusBarAlignment, StatusBarItem, Uri, window, WorkspaceFolder, WorkspaceFolderPickOptions } from 'vscode';
 import { IApplicationShell } from './types';
 
 @injectable()
 export class ApplicationShell implements IApplicationShell {
     public showInformationMessage(message: string, ...items: string[]): Thenable<string>;
-    public showInformationMessage(message: string, options: vscode.MessageOptions, ...items: string[]): Thenable<string>;
-    public showInformationMessage<T extends vscode.MessageItem>(message: string, ...items: T[]): Thenable<T>;
-    public showInformationMessage<T extends vscode.MessageItem>(message: string, options: vscode.MessageOptions, ...items: T[]): Thenable<T>;
+    public showInformationMessage(message: string, options: MessageOptions, ...items: string[]): Thenable<string>;
+    public showInformationMessage<T extends MessageItem>(message: string, ...items: T[]): Thenable<T>;
+    public showInformationMessage<T extends MessageItem>(message: string, options: MessageOptions, ...items: T[]): Thenable<T>;
     public showInformationMessage(message: string, options?: any, ...items: any[]): Thenable<any> {
-        return vscode.window.showInformationMessage(message, options, ...items);
+        return window.showInformationMessage(message, options, ...items);
     }
 
     public showWarningMessage(message: string, ...items: string[]): Thenable<string>;
-    public showWarningMessage(message: string, options: vscode.MessageOptions, ...items: string[]): Thenable<string>;
-    public showWarningMessage<T extends vscode.MessageItem>(message: string, ...items: T[]): Thenable<T>;
-    public showWarningMessage<T extends vscode.MessageItem>(message: string, options: vscode.MessageOptions, ...items: T[]): Thenable<T>;
+    public showWarningMessage(message: string, options: MessageOptions, ...items: string[]): Thenable<string>;
+    public showWarningMessage<T extends MessageItem>(message: string, ...items: T[]): Thenable<T>;
+    public showWarningMessage<T extends MessageItem>(message: string, options: MessageOptions, ...items: T[]): Thenable<T>;
     public showWarningMessage(message: any, options?: any, ...items: any[]) {
-        return vscode.window.showWarningMessage(message, options, ...items);
+        return window.showWarningMessage(message, options, ...items);
     }
 
     public showErrorMessage(message: string, ...items: string[]): Thenable<string>;
-    public showErrorMessage(message: string, options: vscode.MessageOptions, ...items: string[]): Thenable<string>;
-    public showErrorMessage<T extends vscode.MessageItem>(message: string, ...items: T[]): Thenable<T>;
-    public showErrorMessage<T extends vscode.MessageItem>(message: string, options: vscode.MessageOptions, ...items: T[]): Thenable<T>;
+    public showErrorMessage(message: string, options: MessageOptions, ...items: string[]): Thenable<string>;
+    public showErrorMessage<T extends MessageItem>(message: string, ...items: T[]): Thenable<T>;
+    public showErrorMessage<T extends MessageItem>(message: string, options: MessageOptions, ...items: T[]): Thenable<T>;
     public showErrorMessage(message: any, options?: any, ...items: any[]) {
-        return vscode.window.showErrorMessage(message, options, ...items);
+        return window.showErrorMessage(message, options, ...items);
     }
 
-    public showQuickPick(items: string[] | Thenable<string[]>, options?: vscode.QuickPickOptions, token?: vscode.CancellationToken): Thenable<string>;
-    public showQuickPick<T extends vscode.QuickPickItem>(items: T[] | Thenable<T[]>, options?: vscode.QuickPickOptions, token?: vscode.CancellationToken): Thenable<T>;
-    public showQuickPick(items: any, options?: any, token?: any) {
-        return vscode.window.showQuickPick(items, options, token);
+    public showQuickPick(items: string[] | Thenable<string[]>, options?: QuickPickOptions, token?: CancellationToken): Thenable<string>;
+    public showQuickPick<T extends QuickPickItem>(items: T[] | Thenable<T[]>, options?: QuickPickOptions, token?: CancellationToken): Thenable<T>;
+    public showQuickPick(items: any, options?: any, token?: any): Thenable<any> {
+        return window.showQuickPick(items, options, token);
     }
 
-    public showOpenDialog(options: vscode.OpenDialogOptions): Thenable<vscode.Uri[] | undefined> {
-        return vscode.window.showOpenDialog(options);
+    public showOpenDialog(options: OpenDialogOptions): Thenable<Uri[] | undefined> {
+        return window.showOpenDialog(options);
     }
-    public showSaveDialog(options: vscode.SaveDialogOptions): Thenable<vscode.Uri | undefined> {
-        return vscode.window.showSaveDialog(options);
+    public showSaveDialog(options: SaveDialogOptions): Thenable<Uri | undefined> {
+        return window.showSaveDialog(options);
     }
-    public showInputBox(options?: vscode.InputBoxOptions, token?: vscode.CancellationToken): Thenable<string | undefined> {
-        return vscode.window.showInputBox(options, token);
+    public showInputBox(options?: InputBoxOptions, token?: CancellationToken): Thenable<string | undefined> {
+        return window.showInputBox(options, token);
     }
     public openUrl(url: string): void {
         opn(url);
@@ -59,14 +58,14 @@ export class ApplicationShell implements IApplicationShell {
     public setStatusBarMessage(text: string, hideWhenDone: Thenable<any>): Disposable;
     public setStatusBarMessage(text: string): Disposable;
     public setStatusBarMessage(text: string, arg?: any): Disposable {
-        return vscode.window.setStatusBarMessage(text, arg);
+        return window.setStatusBarMessage(text, arg);
     }
 
     public createStatusBarItem(alignment?: StatusBarAlignment, priority?: number): StatusBarItem {
-        return vscode.window.createStatusBarItem(alignment, priority);
+        return window.createStatusBarItem(alignment, priority);
     }
     public showWorkspaceFolderPick(options?: WorkspaceFolderPickOptions): Thenable<WorkspaceFolder | undefined> {
-        return vscode.window.showWorkspaceFolderPick(options);
+        return window.showWorkspaceFolderPick(options);
     }
 
 }
