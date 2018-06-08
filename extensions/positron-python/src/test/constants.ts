af72c987f379cc0ff6b58e78e1c6c9040720de82
Derek Keeler <d3r3kk@users.noreply.github.com>
2018-06-07 21:56:40 -0500

    Enable JUnit report output during testRunner processing. (microsoft/vscode-python#1901)
    Reporting support for VSTS primarily.
    - Add reporter & reporterOptions to testRunner
    - Add support for JUnit output file

--- extensions/positron-python/src/test/constants.ts
+++ extensions/positron-python/src/test/constants.ts
@@ -8,6 +8,12 @@ export const IS_APPVEYOR = process.env.APPVEYOR === 'true';
 export const IS_TRAVIS = process.env.TRAVIS === 'true';
 export const IS_VSTS = process.env.TF_BUILD !== undefined;
 export const IS_CI_SERVER = IS_TRAVIS || IS_APPVEYOR || IS_VSTS;
+
+// allow the CI server to specify JUnit output...
+export const MOCHA_REPORTER_JUNIT: boolean = IS_CI_SERVER && process.env.MOCHA_REPORTER_JUNIT !== undefined;
+export const MOCHA_CI_REPORTFILE: string = MOCHA_REPORTER_JUNIT && process.env.MOCHA_CI_REPORTFILE !== undefined ? process.env.MOCHA_CI_REPORTFILE.toString() : './junit-out.xml';
+export const MOCHA_CI_PROPERTIES: string = MOCHA_REPORTER_JUNIT && process.env.MOCHA_CI_PROPERTIES !== undefined ? process.env.MOCHA_CI_PROPERTIES.toString() : '';
+
 export const TEST_TIMEOUT = 25000;
 export const IS_MULTI_ROOT_TEST = isMultitrootTest();
 export const IS_CI_SERVER_TEST_DEBUGGER = process.env.IS_CI_SERVER_TEST_DEBUGGER === '1';
