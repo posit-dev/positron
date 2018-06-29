2b87e8fd53a13c77f809824283e16fb26cfcbaec
Mikhail Arkhipov <mikhaila@microsoft.com>
2018-06-29 11:46:30 -0500

    LS settings for typeshed and diagnostics (microsoft/vscode-python#2065)
    * LS symbol providers
    
    * Typeshed paths
    
    * Typeshed submodule
    
    * Add submodule
    
    * New settings
    
    * Add diagnostics control settings
    
    * Add typeshed paths change check
    
    * Exclude some typeshed files from packages

--- extensions/positron-python/src/client/common/types.ts
+++ extensions/positron-python/src/client/common/types.ts
@@ -133,6 +133,7 @@ export interface IPythonSettings {
     readonly envFile: string;
     readonly disableInstallationChecks: boolean;
     readonly globalModuleInstallation: boolean;
+    readonly analysis: IAnalysisSettings;
 }
 export interface ISortImportSettings {
     readonly path: string;
@@ -236,13 +237,16 @@ export interface ITerminalSettings {
     readonly launchArgs: string[];
     readonly activateEnvironment: boolean;
 }
-export interface IPythonAnalysisEngineSettings {
-    readonly showAdvancedMembers: boolean;
+export interface IAnalysisSettings {
+    readonly openFilesOnly: boolean;
     readonly typeshedPaths: string[];
+    readonly errors: string[];
+    readonly warnings: string[];
+    readonly information: string[];
+    readonly disabled: string[];
 }
 
 export const IConfigurationService = Symbol('IConfigurationService');
-
 export interface IConfigurationService {
     getSettings(resource?: Uri): IPythonSettings;
     isTestExecution(): boolean;
