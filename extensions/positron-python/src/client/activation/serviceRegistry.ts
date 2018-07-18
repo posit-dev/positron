8eb8b08101481bb449d54006accad9cde1d2866c
Don Jayamanne <don.jayamanne@yahoo.com>
2018-07-18 18:41:06 -0500

    Move service registrations from debug adapter host to extension host (microsoft/vscode-python#2193)
    Fixes microsoft/vscode-python#2191

--- extensions/positron-python/src/client/activation/serviceRegistry.ts
+++ extensions/positron-python/src/client/activation/serviceRegistry.ts
@@ -3,7 +3,10 @@
 
 'use strict';
 
+import { BANNER_NAME_LS_SURVEY, BANNER_NAME_PROPOSE_LS, IPythonExtensionBanner } from '../common/types';
 import { IServiceManager } from '../ioc/types';
+import { LanguageServerSurveyBanner } from '../languageServices/languageServerSurveyBanner';
+import { ProposeLanguageServerBanner } from '../languageServices/proposeLanguageServerBanner';
 import { ExtensionActivationService } from './activationService';
 import { JediExtensionActivator } from './jedi';
 import { LanguageServerExtensionActivator } from './languageServer';
@@ -13,4 +16,6 @@ export function registerTypes(serviceManager: IServiceManager) {
     serviceManager.addSingleton<IExtensionActivationService>(IExtensionActivationService, ExtensionActivationService);
     serviceManager.add<IExtensionActivator>(IExtensionActivator, JediExtensionActivator, ExtensionActivators.Jedi);
     serviceManager.add<IExtensionActivator>(IExtensionActivator, LanguageServerExtensionActivator, ExtensionActivators.DotNet);
+    serviceManager.addSingleton<IPythonExtensionBanner>(IPythonExtensionBanner, LanguageServerSurveyBanner, BANNER_NAME_LS_SURVEY);
+    serviceManager.addSingleton<IPythonExtensionBanner>(IPythonExtensionBanner, ProposeLanguageServerBanner, BANNER_NAME_PROPOSE_LS);
 }
