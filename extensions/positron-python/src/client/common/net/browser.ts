0e1bbd254f0305ac44173f349d8fda7ead89199c
Derek Keeler <d3r3kk@users.noreply.github.com>
2018-08-09 11:31:26 -0500

    Ensure default browser is launched on Windows for survey/information popups. [ALTERNATE] (microsoft/vscode-python#2359)
    - Use opn to launch browsers for surveys/information.

--- extensions/positron-python/src/client/common/net/browser.ts
+++ extensions/positron-python/src/client/common/net/browser.ts
@@ -1,30 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import * as child_process from 'child_process';
+'use strict';
+
+// tslint:disable:no-require-imports no-var-requires
+const opn = require('opn');
+
 import { injectable } from 'inversify';
-import * as os from 'os';
 import { IBrowserService } from '../types';
 
 export function launch(url: string) {
-    let openCommand: string | undefined;
-    if (os.platform() === 'win32') {
-        openCommand = 'explorer';
-    } else if (os.platform() === 'darwin') {
-        openCommand = '/usr/bin/open';
-    } else {
-        openCommand = '/usr/bin/xdg-open';
-    }
-    if (!openCommand) {
-        console.error(`Unable to determine platform to launch the browser in the Python extension on platform '${os.platform()}'.`);
-        console.error(`Link is: ${url}`);
-    }
-    child_process.spawn(openCommand, [url]);
+    opn(url);
 }
 
 @injectable()
 export class BrowserService implements IBrowserService {
-    public launch(url: string): void{
+    public launch(url: string): void {
         launch(url);
     }
 }
