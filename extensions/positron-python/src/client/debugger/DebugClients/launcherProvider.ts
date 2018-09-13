319400fd97fc3688a26013e88f6a7e339fda6591
Don Jayamanne <don.jayamanne@yahoo.com>
2018-09-13 18:44:30 -0500

    Use a python script to launch the debugger (microsoft/vscode-python#2554)
    * Use a python script to launch the debugger

--- extensions/positron-python/src/client/debugger/DebugClients/launcherProvider.ts
+++ extensions/positron-python/src/client/debugger/DebugClients/launcherProvider.ts
@@ -6,16 +6,17 @@
 // tslint:disable:max-classes-per-file
 
 import * as path from 'path';
+import { EXTENSION_ROOT_DIR } from '../../common/constants';
 import { IDebugLauncherScriptProvider } from '../types';
 
 export class NoDebugLauncherScriptProvider implements IDebugLauncherScriptProvider {
     public getLauncherFilePath(): string {
-        return path.join(path.dirname(__dirname), '..', '..', '..', 'pythonFiles', 'PythonTools', 'visualstudio_py_launcher_nodebug.py');
+        return path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'experimental', 'ptvsd_launcher.py');
     }
 }
 
 export class DebuggerLauncherScriptProvider implements IDebugLauncherScriptProvider {
     public getLauncherFilePath(): string {
-        return path.join(path.dirname(__dirname), '..', '..', '..', 'pythonFiles', 'PythonTools', 'visualstudio_py_launcher.py');
+        return path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'experimental', 'ptvsd_launcher.py');
     }
 }
