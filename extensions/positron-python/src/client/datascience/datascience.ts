95f95ddba780195a03f713881bfbaf25a195bcd5
Ian Huff <ian.huff@gmail.com>
2018-11-08 14:34:19 -0600

    remove IsTestExecution guard from around data science banner calls (microsoft/vscode-python#3258)
    * remove misuse of IsTestExecution around data science banner

--- extensions/positron-python/src/client/datascience/datascience.ts
+++ extensions/positron-python/src/client/datascience/datascience.ts
@@ -7,8 +7,9 @@ import { inject, injectable } from 'inversify';
 import * as vscode from 'vscode';
 import { ICommandManager } from '../common/application/types';
 import { PythonSettings } from '../common/configSettings';
-import { isTestExecution, PYTHON } from '../common/constants';
+import { PYTHON } from '../common/constants';
 import { ContextKey } from '../common/contextKey';
+import '../common/extensions';
 import { BANNER_NAME_DS_SURVEY, IConfigurationService, IDisposableRegistry, IExtensionContext, IPythonExtensionBanner } from '../common/types';
 import { IServiceContainer } from '../ioc/types';
 import { Commands, EditorContexts } from './constants';
@@ -57,9 +58,8 @@ export class DataScience implements IDataScience {
     }
 
     public async runAllCells(codeWatcher: ICodeWatcher): Promise<void> {
-        if (!isTestExecution()) {
-            this.dataScienceSurveyBanner.showBanner().ignoreErrors();
-        }
+        this.dataScienceSurveyBanner.showBanner().ignoreErrors();
+
         let activeCodeWatcher: ICodeWatcher | undefined = codeWatcher;
         if (!activeCodeWatcher) {
             activeCodeWatcher = this.getCurrentCodeWatcher();
@@ -72,9 +72,8 @@ export class DataScience implements IDataScience {
     }
 
     public async runCell(codeWatcher: ICodeWatcher, range: vscode.Range): Promise<void> {
-        if (!isTestExecution()) {
-            this.dataScienceSurveyBanner.showBanner().ignoreErrors();
-        }
+        this.dataScienceSurveyBanner.showBanner().ignoreErrors();
+
         if (codeWatcher) {
             return codeWatcher.runCell(range);
         } else {
@@ -83,9 +82,8 @@ export class DataScience implements IDataScience {
     }
 
     public async runCurrentCell(): Promise<void> {
-        if (!isTestExecution()) {
-            this.dataScienceSurveyBanner.showBanner().ignoreErrors();
-        }
+        this.dataScienceSurveyBanner.showBanner().ignoreErrors();
+
         const activeCodeWatcher = this.getCurrentCodeWatcher();
         if (activeCodeWatcher) {
             return activeCodeWatcher.runCurrentCell();
@@ -95,9 +93,8 @@ export class DataScience implements IDataScience {
     }
 
     public async runCurrentCellAndAdvance(): Promise<void> {
-        if (!isTestExecution()) {
-            this.dataScienceSurveyBanner.showBanner().ignoreErrors();
-        }
+        this.dataScienceSurveyBanner.showBanner().ignoreErrors();
+
         const activeCodeWatcher = this.getCurrentCodeWatcher();
         if (activeCodeWatcher) {
             return activeCodeWatcher.runCurrentCellAndAdvance();
