adf929b199110216ae61d1c5db434f9dc4cc5e8a
Ian Huff <ian.huff@gmail.com>
2019-01-18 16:48:04 -0600

    When input code is hidden hide all cells that don't have output (microsoft/vscode-python#4050)

--- extensions/positron-python/src/test/datascience/mockJupyterManager.ts
+++ extensions/positron-python/src/test/datascience/mockJupyterManager.ts
@@ -181,7 +181,11 @@ export class MockJupyterManager implements IJupyterSessionManager {
             if (c.data.cell_type === 'code') {
                 const massagedResult = this.massageCellResult(result, mimeType);
                 const data: nbformat.ICodeCell = c.data as nbformat.ICodeCell;
-                data.outputs = [...data.outputs, massagedResult];
+                if (result) {
+                    data.outputs = [...data.outputs, massagedResult];
+                } else {
+                    data.outputs = [...data.outputs];
+                }
                 c.data = data;
             }
 
