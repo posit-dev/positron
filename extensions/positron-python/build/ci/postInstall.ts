1a25af893df6098860115c95500e5fbc015da748
Don Jayamanne <don.jayamanne@yahoo.com>
2019-01-18 13:05:32 -0600

    Ensure type definition files in node_modules/@jupyterlab compile in strict mode (microsoft/vscode-python#4046)
    * Ensure jupyter lab files compiles in strict mode

---
+++ extensions/positron-python/build/ci/postInstall.ts
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import * as fs from 'fs';
+import * as path from 'path';
+import { ExtensionRootDir } from '../constants';
+
+/**
+ * In order to compile the extension in strict mode, one of the dependencies (@jupyterlab) has some files that
+ * just won't compile in strict mode.
+ * Unfortunately we cannot fix it by overriding their type definitions
+ * Note: that has been done for a few of the JupyterLabl files (see typings/index.d.ts).
+ * The solution is to modify the type definition file after `npm install`.
+ */
+function fixJupyterLabDTSFiles() {
+    const filePath = path.join(
+        ExtensionRootDir,
+        'node_modules',
+        '@jupyterlab',
+        'coreutils',
+        'lib',
+        'settingregistry.d.ts'
+    );
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`Type Definition file from JupyterLab not found '${filePath}' (pvsc post install script)`);
+    }
+
+    const fileContents = fs.readFileSync(filePath, { encoding: 'utf8' });
+    const replacedText = fileContents.replace('[key: string]: ISchema;', '[key: string]: ISchema | undefined;');
+    if (fileContents === replacedText) {
+        throw new Error('Fix for JupyterLabl file \'settingregistry.d.ts\' failed (pvsc post install script)');
+    }
+    fs.writeFileSync(filePath, replacedText);
+}
+
+fixJupyterLabDTSFiles();
