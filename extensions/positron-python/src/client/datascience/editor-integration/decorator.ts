3fdd4b271f59e5a0f14ace6ad47fff5565857d3f
Rich Chiodo <rchiodo@users.noreply.github.com>
2019-04-12 11:58:53 -0500

    Add current cell highlight (microsoft/vscode-python#5219)
    For microsoft/vscode-python#3542
    
    <!--
      If an item below does not apply to you, then go ahead and check it off as "done" and strikethrough the text, e.g.:
        - [x] ~Has unit tests & system/integration tests~
    -->
    - [x] Pull request represents a single change (i.e. not fixing disparate/unrelated things in a single PR)
    - [x] Title summarizes what is changing
    - [x] Has a [news entry](https://github.com/Microsoft/vscode-python/tree/master/news) file (remember to thank yourself!)
    - [ ] Has sufficient logging.
    - [ ] Has telemetry for enhancements.
    - [ ] Unit tests & system/integration tests are added/updated
    - [ ] [Test plan](https://github.com/Microsoft/vscode-python/blob/master/.github/test_plan.md) is updated as appropriate
    - [ ] [`package-lock.json`](https://github.com/Microsoft/vscode-python/blob/master/package-lock.json) has been regenerated by running `npm install` (if dependencies have changed)
    - [ ] The wiki is updated with any design decisions/details.

---
+++ extensions/positron-python/src/client/datascience/editor-integration/decorator.ts
@@ -0,0 +1,89 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+'use strict';
+import { inject, injectable } from 'inversify';
+import * as vscode from 'vscode';
+
+import { IExtensionActivationService } from '../../activation/types';
+import { IDocumentManager } from '../../common/application/types';
+import { PYTHON_LANGUAGE } from '../../common/constants';
+import { IConfigurationService, IDisposable, IDisposableRegistry, Resource } from '../../common/types';
+import { generateCellRanges } from '../cellFactory';
+
+@injectable()
+export class Decorator implements IExtensionActivationService, IDisposable {
+
+    private activeCellType: vscode.TextEditorDecorationType;
+    private timer: NodeJS.Timer | undefined;
+
+    constructor(@inject(IDocumentManager) private documentManager: IDocumentManager,
+                @inject(IDisposableRegistry) disposables: IDisposableRegistry,
+                @inject(IConfigurationService) private configuration: IConfigurationService)
+    {
+        this.activeCellType = this.documentManager.createTextEditorDecorationType({
+            backgroundColor: new vscode.ThemeColor('sideBarSectionHeader.background'),
+            isWholeLine: true
+        });
+        disposables.push(this);
+        disposables.push(this.configuration.getSettings().onDidChange(this.settingsChanged, this));
+        disposables.push(this.documentManager.onDidChangeActiveTextEditor(this.changedEditor, this));
+        disposables.push(this.documentManager.onDidChangeTextEditorSelection(this.changedSelection, this));
+        disposables.push(this.documentManager.onDidChangeTextDocument(this.changedDocument, this));
+        this.settingsChanged();
+    }
+
+    public activate(_resource: Resource) : Promise<void> {
+        // We don't need to do anything here as we already did all of our work in the
+        // constructor.
+        return Promise.resolve();
+    }
+
+    public dispose() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+    }
+
+    private settingsChanged() {
+        if (this.documentManager.activeTextEditor) {
+            this.triggerUpdate(this.documentManager.activeTextEditor);
+        }
+    }
+
+    private changedEditor(editor: vscode.TextEditor | undefined) {
+        this.triggerUpdate(editor);
+    }
+
+    private changedDocument(e: vscode.TextDocumentChangeEvent) {
+        if (this.documentManager.activeTextEditor && e.document === this.documentManager.activeTextEditor.document) {
+            this.triggerUpdate(this.documentManager.activeTextEditor);
+        }
+    }
+
+    private changedSelection(e: vscode.TextEditorSelectionChangeEvent) {
+        if (e.textEditor && e.textEditor.selection.anchor) {
+            this.triggerUpdate(e.textEditor);
+        }
+    }
+
+    private triggerUpdate(editor: vscode.TextEditor | undefined) {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+        this.timer = setTimeout(() => this.update(editor), 100);
+    }
+
+    private update(editor: vscode.TextEditor | undefined) {
+        if (editor && editor.document && editor.document.languageId === PYTHON_LANGUAGE) {
+            const settings = this.configuration.getSettings().datascience;
+            if (settings.decorateCells && settings.enabled) {
+                // Find all of the cells
+                const cells = generateCellRanges(editor.document, this.configuration.getSettings().datascience);
+                const cellRanges = cells.map(c => c.range).filter(r => r.contains(editor.selection.anchor));
+                editor.setDecorations(this.activeCellType, cellRanges);
+            } else {
+                editor.setDecorations(this.activeCellType, []);
+            }
+        }
+    }
+}
