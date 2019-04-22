33e7a030b0952f0e11c75c0f7d1cf7447877d852
Don Jayamanne <don.jayamanne@yahoo.com>
2019-04-22 17:50:49 -0500

    Reliably end test tasks in Azure Pipelines (microsoft/vscode-python#5410)
    For microsoft/vscode-python#5129

--- extensions/positron-python/src/test/common/exitCIAfterTestReporter.ts
+++ extensions/positron-python/src/test/common/exitCIAfterTestReporter.ts
@@ -8,45 +8,65 @@
 // The hack is to force it to die when tests are done, if this doesn't work we've got a bigger problem on our hands.
 
 // tslint:disable:no-var-requires no-require-imports no-any no-console no-unnecessary-class no-default-export
-const log = require('why-is-node-running');
+import * as fs from 'fs-extra';
+import * as net from 'net';
+import * as path from 'path';
+import { EXTENSION_ROOT_DIR } from '../../client/constants';
+import { noop } from '../core';
+
+let client: net.Socket | undefined;
 const mochaTests: any = require('mocha');
 const { EVENT_RUN_BEGIN, EVENT_RUN_END } = mochaTests.Runner.constants;
+
+async function connectToServer() {
+    const portFile = path.join(EXTENSION_ROOT_DIR, 'port.txt');
+    if (!(await fs.pathExists(portFile))) {
+        return;
+    }
+    const port = parseInt(await fs.readFile(portFile, 'utf-8'), 10);
+    console.log(`Need to connect to port ${port}`);
+    return new Promise(resolve => {
+        try {
+            client = new net.Socket();
+            client.connect({ port }, () => {
+                console.log(`Connected to port ${port}`);
+                resolve();
+            });
+        } catch {
+            console.error('Failed to connect to socket server to notify completion of tests');
+            resolve();
+        }
+    });
+}
+function notifyCompleted(hasFailures: boolean) {
+    if (!client || client.destroyed || !client.writable) {
+        console.error('No client to write from');
+        return;
+    }
+    try {
+        const exitCode = hasFailures ? 1 : 0;
+        console.log(`Notify server of test completion with code ${exitCode}`);
+        // If there are failures, send a code of 1 else 0.
+        client.write(exitCode.toString());
+        client.end();
+        console.log('Notified server of test completion');
+    } catch (ex) {
+        console.error('Socket client error', ex);
+    }
+}
+
 class ExitReporter {
     constructor(runner: any) {
         console.log('Initialize Exit Reporter for Mocha (PVSC).');
+        connectToServer().catch(noop);
         const stats = runner.stats;
         runner
             .once(EVENT_RUN_BEGIN, () => {
                 console.info('Start Exit Reporter for Mocha.');
             })
-            .once(EVENT_RUN_END, () => {
-                process.stdout.cork();
+            .once(EVENT_RUN_END, async () => {
+                notifyCompleted(stats.failures > 0);
                 console.info('End Exit Reporter for Mocha.');
-                process.stdout.write('If process does not die in 30s, then log and kill.');
-                process.stdout.uncork();
-                // NodeJs generally waits for pending timeouts, however the process running Mocha
-                // No idea why it times, out. Once again, this is a hack.
-                // Solution (i.e. hack), lets add a timeout with a delay of 30 seconds,
-                // & if this process doesn't die, lets kill it.
-                function die() {
-                    setTimeout(() => {
-                        console.info('Exiting from custom PVSC Mocha Reporter.');
-                        try {
-                            log();
-                        } catch (ex) {
-                            // Do nothing.
-                        }
-                        process.exit(stats.failures === 0 ? 0 : 1);
-                        try {
-                            // Lets just close VSC, hopefully that'll be sufficient (more graceful).
-                            const vscode = require('vscode');
-                            vscode.commands.executeCommand('workbench.action.closeWindow');
-                        } catch (ex) {
-                            // Do nothing.
-                        }
-                    }, 30000);
-                }
-                die();
             });
     }
 }
