02db24a1d1c5d9a865974da86b398811d13286d7
Ian Huff <ianhu@microsoft.com>
2019-05-21 14:34:40 -0500

    Fix variable explorer per issues (microsoft/vscode-python#5708)

--- extensions/positron-python/src/test/datascience/variableexplorer.functional.test.tsx
+++ extensions/positron-python/src/test/datascience/variableexplorer.functional.test.tsx
@@ -77,7 +77,8 @@ value = 'hello world'`;
         // We should show a string and show an int, the modules should be hidden
         let targetVariables: IJupyterVariable[] = [
             {name: 'a', value: '1', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'value', value: 'hello world', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
+            // tslint:disable-next-line:quotemark
+            {name: 'value', value: "'hello world'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
         ];
         verifyVariables(wrapper, targetVariables);
 
@@ -120,7 +121,8 @@ value = 'hello world'`;
 
         targetVariables = [
             {name: 'a', value: '1', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'value', value: 'hello world', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
+            // tslint:disable-next-line:quotemark
+            {name: 'value', value: "'hello world'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
         ];
         verifyVariables(wrapper, targetVariables);
 
@@ -130,8 +132,10 @@ value = 'hello world'`;
 
         targetVariables = [
             {name: 'a', value: '1', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'value', value: 'hello world', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'value2', value: 'hello world 2', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
+            // tslint:disable-next-line:quotemark
+            {name: 'value', value: "'hello world'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable-next-line:quotemark
+            {name: 'value2', value: "'hello world 2'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
         ];
         verifyVariables(wrapper, targetVariables);
     }, () => { return ioc; });
@@ -159,7 +163,8 @@ value = 'hello world'`;
 
         targetVariables = [
             {name: 'a', value: '1', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'value', value: 'hello world', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
+            // tslint:disable-next-line:quotemark
+            {name: 'value', value: "'hello world'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false}
         ];
         verifyVariables(wrapper, targetVariables);
     }, () => { return ioc; });
@@ -214,11 +219,75 @@ myTuple = 1,2,3,4,5,6,7,8,9
         const targetVariables: IJupyterVariable[] = [
             {name: 'a', value: '1', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
             {name: 'myComplex', value: '(1+1j)', supportsDataExplorer: false, type: 'complex', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'myDataframe', value: '                  0\n0           0.00000\n1           2.00004\n2           4.00008\n3           6.00012\n4           8.00016\n5          10.00020\n6          12.00024\n7          14.00028\n8          16.00032\n', supportsDataExplorer: true, type: 'DataFrame', size: 54, shape: '', count: 0, truncated: false},
+            {name: 'myDataframe', value: `                  0
+0           0.00000
+1           2.00004
+2           4.00008
+3           6.00012
+4           8.00016
+5          10.00020
+6          12.00024
+7          14.00028
+8          16.00032
+9          18.00036
+10         20.00040
+11         22.00044
+12         24.00048
+13         26.00052
+14         28.00056
+15         30.00060
+16         32.00064
+17         34.00068
+18         36.00072
+19         38.00076
+20         40.00080
+21         42.00084
+22         44.00088
+23         46.00092
+24         48.00096
+25         50.00100
+26         52.00104
+27         54.00108
+28         56.00112
+29         58.00116
+...             ...
+49970   99941.99884
+49971   99943.99888
+49972   99945.99892
+49973   99947.99896
+49974   99949.99900
+49975   99951.99904
+49976   99953.99908
+49977   99955.99912
+49978   99957.99916
+49979   99959.99920
+49980   99961.99924
+49981   99963.99928
+49982   99965.99932
+49983   99967.99936
+49984   99969.99940
+49985   99971.99944
+49986   99973.99948
+49987   99975.99952
+49988   99977.99956
+49989   99979.99960
+49990   99981.99964
+49991   99983.99968
+49992   99985.99972
+49993   99987.99976
+49994   99989.99980
+49995   99991.99984
+49996   99993.99988
+49997   99995.99992
+49998   99997.99996
+49999  100000.00000
+
+[50000 rows x 1 columns]`, supportsDataExplorer: true, type: 'DataFrame', size: 54, shape: '', count: 0, truncated: false},
             {name: 'myFloat', value: '9999.9999', supportsDataExplorer: false, type: 'float', size: 58, shape: '', count: 0, truncated: false},
             {name: 'myInt', value: '99999999', supportsDataExplorer: false, type: 'int', size: 56, shape: '', count: 0, truncated: false},
-            {name: 'mynpArray', value: `[0.00000000e+00 2.00004000e+00 4.00008000e+00 ... 9.99959999e+04
- 9.99980000e+04 1.00000000e+05]`, supportsDataExplorer: true, type: 'ndarray', size: 54, shape: '', count: 0, truncated: false},
+            {name: 'mynpArray', value: `array([0.00000000e+00, 2.00004000e+00, 4.00008000e+00, ...,
+       9.99959999e+04, 9.99980000e+04, 1.00000000e+05])`, supportsDataExplorer: true, type: 'ndarray', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable:no-trailing-whitespace
             {name: 'mySeries', value: `0             0.00000
 1             2.00004
 2             4.00008
@@ -228,7 +297,59 @@ myTuple = 1,2,3,4,5,6,7,8,9
 6            12.00024
 7            14.00028
 8            16.00032
-9 `, supportsDataExplorer: true, type: 'Series', size: 54, shape: '', count: 0, truncated: false},
+9            18.00036
+10           20.00040
+11           22.00044
+12           24.00048
+13           26.00052
+14           28.00056
+15           30.00060
+16           32.00064
+17           34.00068
+18           36.00072
+19           38.00076
+20           40.00080
+21           42.00084
+22           44.00088
+23           46.00092
+24           48.00096
+25           50.00100
+26           52.00104
+27           54.00108
+28           56.00112
+29           58.00116
+             ...     
+49970     99941.99884
+49971     99943.99888
+49972     99945.99892
+49973     99947.99896
+49974     99949.99900
+49975     99951.99904
+49976     99953.99908
+49977     99955.99912
+49978     99957.99916
+49979     99959.99920
+49980     99961.99924
+49981     99963.99928
+49982     99965.99932
+49983     99967.99936
+49984     99969.99940
+49985     99971.99944
+49986     99973.99948
+49987     99975.99952
+49988     99977.99956
+49989     99979.99960
+49990     99981.99964
+49991     99983.99968
+49992     99985.99972
+49993     99987.99976
+49994     99989.99980
+49995     99991.99984
+49996     99993.99988
+49997     99995.99992
+49998     99997.99996
+49999    100000.00000
+Name: 0, Length: 50000, dtype: float64`, supportsDataExplorer: true, type: 'Series', size: 54, shape: '', count: 0, truncated: false},
             {name: 'myTuple', value: '(1, 2, 3, 4, 5, 6, 7, 8, 9)', supportsDataExplorer: false, type: 'tuple', size: 54, shape: '', count: 0, truncated: false}
         ];
         verifyVariables(wrapper, targetVariables);
@@ -252,21 +373,27 @@ strc = 'c'`;
             {name: 'a', value: '1', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
             {name: 'b', value: '2', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
             {name: 'c', value: '3', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'stra', value: 'a', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'strb', value: 'b', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'strc', value: 'c', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable-next-line:quotemark
+            {name: 'stra', value: "'a'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable-next-line:quotemark
+            {name: 'strb', value: "'b'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable-next-line:quotemark
+            {name: 'strc', value: "'c'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
         ];
         verifyVariables(wrapper, targetVariables);
 
         sortVariableExplorer(wrapper, 'value', 'DESC');
 
         targetVariables = [
-            {name: 'strc', value: 'c', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'strb', value: 'b', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
-            {name: 'stra', value: 'a', supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
             {name: 'c', value: '3', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
             {name: 'b', value: '2', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
             {name: 'a', value: '1', supportsDataExplorer: false, type: 'int', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable-next-line:quotemark
+            {name: 'strc', value: "'c'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable-next-line:quotemark
+            {name: 'strb', value: "'b'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
+            // tslint:disable-next-line:quotemark
+            {name: 'stra', value: "'a'", supportsDataExplorer: false, type: 'str', size: 54, shape: '', count: 0, truncated: false},
         ];
         verifyVariables(wrapper, targetVariables);
     }, () => { return ioc; });
