ac477a15eb7f57293da4abf7cb8bf52f68b0667f
Rich Chiodo <rchiodo@users.noreply.github.com>
2019-06-13 14:58:31 -0500

    Fix shift enter banner telemetry (microsoft/vscode-python#6014)
    * Fix shift enter banner telemetry
    
    * Make sure to clear telemetry reporter before finishing test

--- extensions/positron-python/src/test/datascience/shiftEnterBanner.unit.test.ts
+++ extensions/positron-python/src/test/datascience/shiftEnterBanner.unit.test.ts
@@ -1,60 +1,113 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 'use strict';
-
-// tslint:disable:no-any max-func-body-length
+//tslint:disable:max-func-body-length match-default-export-name no-any no-multiline-string no-trailing-whitespace
+import { expect } from 'chai';
+import rewiremock from 'rewiremock';
 import * as typemoq from 'typemoq';
+
 import { IApplicationShell } from '../../client/common/application/types';
-import { IConfigurationService, IDataScienceSettings, IPersistentState, IPersistentStateFactory, IPythonSettings } from '../../client/common/types';
+import {
+    IConfigurationService,
+    IDataScienceSettings,
+    IPersistentState,
+    IPersistentStateFactory,
+    IPythonSettings
+} from '../../client/common/types';
+import { Telemetry } from '../../client/datascience/constants';
 import { InteractiveShiftEnterBanner, InteractiveShiftEnterStateKeys } from '../../client/datascience/shiftEnterBanner';
 import { IJupyterExecution } from '../../client/datascience/types';
+import { clearTelemetryReporter } from '../../client/telemetry';
 
 suite('Interactive Shift Enter Banner', () => {
+    const oldValueOfVSC_PYTHON_UNIT_TEST = process.env.VSC_PYTHON_UNIT_TEST;
+    const oldValueOfVSC_PYTHON_CI_TEST = process.env.VSC_PYTHON_CI_TEST;
     let appShell: typemoq.IMock<IApplicationShell>;
     let jupyterExecution: typemoq.IMock<IJupyterExecution>;
     let config: typemoq.IMock<IConfigurationService>;
 
+    class Reporter {
+        public static eventNames: string[] = [];
+        public static properties: Record<string, string>[] = [];
+        public static measures: {}[] = [];
+        public sendTelemetryEvent(eventName: string, properties?: {}, measures?: {}) {
+            Reporter.eventNames.push(eventName);
+            Reporter.properties.push(properties!);
+            Reporter.measures.push(measures!);
+        }
+    }
+
     setup(() => {
+        process.env.VSC_PYTHON_UNIT_TEST = undefined;
+        process.env.VSC_PYTHON_CI_TEST = undefined;
         appShell = typemoq.Mock.ofType<IApplicationShell>();
         jupyterExecution = typemoq.Mock.ofType<IJupyterExecution>();
         config = typemoq.Mock.ofType<IConfigurationService>();
+        rewiremock.enable();
+        rewiremock('vscode-extension-telemetry').with({ default: Reporter });
+    });
+
+    teardown(() => {
+        process.env.VSC_PYTHON_UNIT_TEST = oldValueOfVSC_PYTHON_UNIT_TEST;
+        process.env.VSC_PYTHON_CI_TEST = oldValueOfVSC_PYTHON_CI_TEST;
+        Reporter.properties = [];
+        Reporter.eventNames = [];
+        Reporter.measures = [];
+        rewiremock.disable();
+        clearTelemetryReporter();
     });
 
     test('Shift Enter Banner with Jupyter available', async() => {
-        const shiftBanner = loadBanner(appShell, jupyterExecution, config, true, true, true, true, false);
+        const shiftBanner = loadBanner(appShell, jupyterExecution, config, true, true, true, true, true, 'Yes');
         await shiftBanner.showBanner();
 
         appShell.verifyAll();
         jupyterExecution.verifyAll();
         config.verifyAll();
+
+        expect(Reporter.eventNames).to.deep.equal([Telemetry.ShiftEnterBannerShown, Telemetry.EnableInteractiveShiftEnter]);
     });
 
     test('Shift Enter Banner without Jupyter available', async() => {
-        const shiftBanner = loadBanner(appShell, jupyterExecution, config, true, false, false, true, false);
+        const shiftBanner = loadBanner(appShell, jupyterExecution, config, true, false, false, true, false, 'Yes');
         await shiftBanner.showBanner();
 
         appShell.verifyAll();
         jupyterExecution.verifyAll();
         config.verifyAll();
+
+        expect(Reporter.eventNames).to.deep.equal([]);
     });
 
     test('Shift Enter Banner don\'t check Jupyter when disabled', async() => {
-        const shiftBanner = loadBanner(appShell, jupyterExecution, config, false, false, false, false, false);
+        const shiftBanner = loadBanner(appShell, jupyterExecution, config, false, false, false, false, false, 'Yes');
         await shiftBanner.showBanner();
 
         appShell.verifyAll();
         jupyterExecution.verifyAll();
         config.verifyAll();
+
+        expect(Reporter.eventNames).to.deep.equal([]);
     });
 
     test('Shift Enter Banner changes setting', async() => {
-        const shiftBanner = loadBanner(appShell, jupyterExecution, config, false, false, false, false, true);
+        const shiftBanner = loadBanner(appShell, jupyterExecution, config, false, false, false, false, true, 'Yes');
         await shiftBanner.enableInteractiveShiftEnter();
 
         appShell.verifyAll();
         jupyterExecution.verifyAll();
         config.verifyAll();
+    });
+
+    test('Shift Enter Banner say no', async() => {
+        const shiftBanner = loadBanner(appShell, jupyterExecution, config, true, true, true, true, true, 'No');
+        await shiftBanner.showBanner();
+
+        appShell.verifyAll();
+        jupyterExecution.verifyAll();
+        config.verifyAll();
+
+        expect(Reporter.eventNames).to.deep.equal([Telemetry.ShiftEnterBannerShown, Telemetry.DisableInteractiveShiftEnter]);
     });
 });
 
@@ -67,7 +120,8 @@ function loadBanner(
     jupyterFound: boolean,
     bannerShown: boolean,
     executionCalled: boolean,
-    configCalled: boolean
+    configCalled: boolean,
+    questionResponse: string
 ): InteractiveShiftEnterBanner {
     // Config persist state
     const persistService: typemoq.IMock<IPersistentStateFactory> = typemoq.Mock.ofType<IPersistentStateFactory>();
@@ -102,10 +156,11 @@ function loadBanner(
     appShell.setup(a => a.showInformationMessage(typemoq.It.isAny(),
         typemoq.It.isValue(yes),
         typemoq.It.isValue(no)))
+        .returns(() => Promise.resolve(questionResponse))
         .verifiable(bannerShown ? typemoq.Times.once() : typemoq.Times.never());
 
     // Config settings
-    config.setup(c => c.updateSetting(typemoq.It.isValue('dataScience.sendSelectionToInteractiveWindow'), typemoq.It.isValue(true), typemoq.It.isAny(), typemoq.It.isAny()))
+    config.setup(c => c.updateSetting(typemoq.It.isValue('dataScience.sendSelectionToInteractiveWindow'), typemoq.It.isAny(), typemoq.It.isAny(), typemoq.It.isAny()))
         .returns(() => Promise.resolve())
         .verifiable(configCalled ? typemoq.Times.once() : typemoq.Times.never());
 
