6b0c8c6afc046d1a752d4c2eb2699c7837883632
Don Jayamanne <don.jayamanne@yahoo.com>
2019-06-13 12:20:08 -0500

    Disable prompts when updating settings related to tests (microsoft/vscode-python#5985)
    * Disable prompts when updating settings
    * Address code review comments
    * Fix code review comment
    * Fix paths

---
+++ extensions/positron-python/src/client/testing/common/updateTestSettings.ts
@@ -0,0 +1,72 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { inject, injectable } from 'inversify';
+import * as path from 'path';
+import { IExtensionActivationService } from '../../activation/types';
+import { IApplicationEnvironment, IWorkspaceService } from '../../common/application/types';
+import '../../common/extensions';
+import { traceDecorators, traceError } from '../../common/logger';
+import { IFileSystem } from '../../common/platform/types';
+import { Resource } from '../../common/types';
+import { swallowExceptions } from '../../common/utils/decorators';
+
+@injectable()
+export class UpdateTestSettingService implements IExtensionActivationService {
+    constructor(@inject(IFileSystem) private readonly fs: IFileSystem,
+        @inject(IApplicationEnvironment) private readonly application: IApplicationEnvironment,
+        @inject(IWorkspaceService) private readonly workspace: IWorkspaceService) {
+    }
+    public async activate(resource: Resource): Promise<void> {
+        this.updateTestSettings(resource).ignoreErrors();
+    }
+    @traceDecorators.error('Failed to update test settings')
+    public async updateTestSettings(resource: Resource): Promise<void> {
+        const filesToBeFixed = await this.getFilesToBeFixed(resource);
+        await Promise.all(filesToBeFixed.map(file => this.fixSettingInFile(file)));
+    }
+    public getSettingsFiles(resource: Resource) {
+        const settingsFiles: string[] = [];
+        if (this.application.userSettingsFile) {
+            settingsFiles.push(this.application.userSettingsFile);
+        }
+        const workspaceFolder = this.workspace.getWorkspaceFolder(resource);
+        if (workspaceFolder) {
+            settingsFiles.push(path.join(workspaceFolder.uri.fsPath, '.vscode', 'settings.json'));
+        }
+        return settingsFiles;
+    }
+    public async getFilesToBeFixed(resource: Resource) {
+        const files = this.getSettingsFiles(resource);
+        const result = await Promise.all(files.map(async file => {
+            const needsFixing = await this.doesFileNeedToBeFixed(file);
+            return { file, needsFixing };
+        }));
+        return result.filter(item => item.needsFixing).map(item => item.file);
+    }
+    @swallowExceptions('Failed to update settings.json')
+    public async fixSettingInFile(filePath: string) {
+        let fileContents = await this.fs.readFile(filePath);
+        const setting = new RegExp('"python.unitTest', 'g');
+        const setting_pytest_enabled = new RegExp('.pyTestEnabled"', 'g');
+        const setting_pytest_args = new RegExp('.pyTestArgs"', 'g');
+        const setting_pytest_path = new RegExp('.pyTestPath"', 'g');
+
+        fileContents = fileContents.replace(setting, '"python.testing');
+        fileContents = fileContents.replace(setting_pytest_enabled, '.pytestEnabled"');
+        fileContents = fileContents.replace(setting_pytest_args, '.pytestArgs"');
+        fileContents = fileContents.replace(setting_pytest_path, '.pytestPath"');
+        await this.fs.writeFile(filePath, fileContents);
+    }
+    public async doesFileNeedToBeFixed(filePath: string) {
+        try {
+            const contents = await this.fs.readFile(filePath);
+            return contents.indexOf('python.unitTest.') > 0 || contents.indexOf('.pyTest') > 0;
+        } catch (ex) {
+            traceError('Failed to check if file needs to be fixed', ex);
+            return false;
+        }
+    }
+}
---
+++ extensions/positron-python/src/test/application/diagnostics/serviceRegistry.unit.test.ts
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { instance, mock, verify } from 'ts-mockito';
+import { ApplicationDiagnostics } from '../../../client/application/diagnostics/applicationDiagnostics';
+import { EnvironmentPathVariableDiagnosticsService, EnvironmentPathVariableDiagnosticsServiceId } from '../../../client/application/diagnostics/checks/envPathVariable';
+import { InvalidLaunchJsonDebuggerService, InvalidLaunchJsonDebuggerServiceId } from '../../../client/application/diagnostics/checks/invalidLaunchJsonDebugger';
+import { InvalidPythonPathInDebuggerService, InvalidPythonPathInDebuggerServiceId } from '../../../client/application/diagnostics/checks/invalidPythonPathInDebugger';
+import { LSNotSupportedDiagnosticService, LSNotSupportedDiagnosticServiceId } from '../../../client/application/diagnostics/checks/lsNotSupported';
+import { InvalidMacPythonInterpreterService, InvalidMacPythonInterpreterServiceId } from '../../../client/application/diagnostics/checks/macPythonInterpreter';
+import { PowerShellActivationHackDiagnosticsService, PowerShellActivationHackDiagnosticsServiceId } from '../../../client/application/diagnostics/checks/powerShellActivation';
+import { InvalidPythonInterpreterService, InvalidPythonInterpreterServiceId } from '../../../client/application/diagnostics/checks/pythonInterpreter';
+import { DiagnosticsCommandFactory } from '../../../client/application/diagnostics/commands/factory';
+import { IDiagnosticsCommandFactory } from '../../../client/application/diagnostics/commands/types';
+import { DiagnosticFilterService } from '../../../client/application/diagnostics/filter';
+import { DiagnosticCommandPromptHandlerService, DiagnosticCommandPromptHandlerServiceId, MessageCommandPrompt } from '../../../client/application/diagnostics/promptHandler';
+import { registerTypes } from '../../../client/application/diagnostics/serviceRegistry';
+import { IDiagnosticFilterService, IDiagnosticHandlerService, IDiagnosticsService } from '../../../client/application/diagnostics/types';
+import { IApplicationDiagnostics } from '../../../client/application/types';
+import { ServiceManager } from '../../../client/ioc/serviceManager';
+import { IServiceManager } from '../../../client/ioc/types';
+
+suite('Application Diagnostics - Register classes in IOC Container', () => {
+    let serviceManager: IServiceManager;
+    setup(() => {
+        serviceManager = mock(ServiceManager);
+    });
+    test('Register Classes', () => {
+        registerTypes(instance(serviceManager));
+
+        verify(serviceManager.addSingleton<IDiagnosticFilterService>(IDiagnosticFilterService, DiagnosticFilterService));
+        verify(serviceManager.addSingleton<IDiagnosticHandlerService<MessageCommandPrompt>>(IDiagnosticHandlerService, DiagnosticCommandPromptHandlerService, DiagnosticCommandPromptHandlerServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsService>(IDiagnosticsService, EnvironmentPathVariableDiagnosticsService, EnvironmentPathVariableDiagnosticsServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsService>(IDiagnosticsService, InvalidLaunchJsonDebuggerService, InvalidLaunchJsonDebuggerServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsService>(IDiagnosticsService, InvalidPythonInterpreterService, InvalidPythonInterpreterServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsService>(IDiagnosticsService, InvalidPythonPathInDebuggerService, InvalidPythonPathInDebuggerServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsService>(IDiagnosticsService, LSNotSupportedDiagnosticService, LSNotSupportedDiagnosticServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsService>(IDiagnosticsService, PowerShellActivationHackDiagnosticsService, PowerShellActivationHackDiagnosticsServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsService>(IDiagnosticsService, InvalidMacPythonInterpreterService, InvalidMacPythonInterpreterServiceId));
+        verify(serviceManager.addSingleton<IDiagnosticsCommandFactory>(IDiagnosticsCommandFactory, DiagnosticsCommandFactory));
+        verify(serviceManager.addSingleton<IApplicationDiagnostics>(IApplicationDiagnostics, ApplicationDiagnostics));
+    })
+});
