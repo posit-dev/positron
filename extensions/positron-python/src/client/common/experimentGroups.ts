a187aed2a6fceb89a6608eb8290eac94f16e215b
Don Jayamanne <don.jayamanne@yahoo.com>
2019-06-19 13:28:32 -0500

    Experiment to always displaying test explorer (microsoft/vscode-python#6227)
    * Experiment for always displaying test explorer
    
    * News entry
    
    * Oops

--- extensions/positron-python/src/client/common/experimentGroups.ts
+++ extensions/positron-python/src/client/common/experimentGroups.ts
@@ -1,2 +1,8 @@
 export const LSControl = 'LS - control';
 export const LSEnabled = 'LS - enabled';
+
+// Experiment to check whether to always display the test explorer.
+export enum AlwaysDisplayTestExplorerGroups {
+    control = 'AlwaysDisplayTestExplorer - control',
+    enabled = 'AlwaysDisplayTestExplorer - enabled'
+}
---
+++ extensions/positron-python/src/test/testing/main.unit.test.ts
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import * as sinon from 'sinon';
+import { anything, instance, mock, verify, when } from 'ts-mockito';
+import { Disposable } from 'vscode';
+import { CommandManager } from '../../client/common/application/commandManager';
+import { ICommandManager } from '../../client/common/application/types';
+import { AlwaysDisplayTestExplorerGroups } from '../../client/common/experimentGroups';
+import { ExperimentsManager } from '../../client/common/experiments';
+import { IDisposableRegistry, IExperimentsManager } from '../../client/common/types';
+import { ServiceContainer } from '../../client/ioc/container';
+import { IServiceContainer } from '../../client/ioc/types';
+import { JediSymbolProvider } from '../../client/providers/symbolProvider';
+import { UnitTestManagementService } from '../../client/testing/main';
+
+suite('Unit Tests - ManagementService', () => {
+    suite('Experiments', () => {
+        let serviceContainer: IServiceContainer;
+        let sandbox: sinon.SinonSandbox;
+        let experiment: IExperimentsManager;
+        let commandManager: ICommandManager;
+        let testManagementService: UnitTestManagementService;
+        setup(() => {
+            serviceContainer = mock(ServiceContainer);
+            sandbox = sinon.createSandbox();
+
+            sandbox.stub(UnitTestManagementService.prototype, 'registerSymbolProvider');
+            sandbox.stub(UnitTestManagementService.prototype, 'registerCommands');
+            sandbox.stub(UnitTestManagementService.prototype, 'registerHandlers');
+            sandbox.stub(UnitTestManagementService.prototype, 'autoDiscoverTests').callsFake(() => Promise.resolve());
+
+            experiment = mock(ExperimentsManager);
+            commandManager = mock(CommandManager);
+
+            when(serviceContainer.get<Disposable[]>(IDisposableRegistry)).thenReturn([]);
+            when(serviceContainer.get<IExperimentsManager>(IExperimentsManager)).thenReturn(instance(experiment));
+            when(serviceContainer.get<ICommandManager>(ICommandManager)).thenReturn(instance(commandManager));
+            when(commandManager.executeCommand(anything(), anything(), anything())).thenResolve();
+
+            testManagementService = new UnitTestManagementService(instance(serviceContainer));
+        });
+        teardown(() => {
+            sandbox.restore();
+        });
+
+        test('Execute command if in experiment', async () => {
+            when(experiment.inExperiment(AlwaysDisplayTestExplorerGroups.enabled)).thenReturn(true);
+
+            await testManagementService.activate(instance(mock(JediSymbolProvider)));
+
+            verify(commandManager.executeCommand('setContext', 'testsDiscovered', true)).once();
+            verify(experiment.inExperiment(AlwaysDisplayTestExplorerGroups.enabled)).once();
+            verify(experiment.inExperiment(AlwaysDisplayTestExplorerGroups.control)).never();
+            verify(experiment.sendTelemetryIfInExperiment(anything())).never();
+        });
+        test('If not in experiment, check and send Telemetry for control group and do not execute command', async () => {
+            when(experiment.inExperiment(AlwaysDisplayTestExplorerGroups.enabled)).thenReturn(false);
+
+            await testManagementService.activate(instance(mock(JediSymbolProvider)));
+
+            verify(commandManager.executeCommand('setContext', 'testsDiscovered', anything())).never();
+            verify(experiment.inExperiment(AlwaysDisplayTestExplorerGroups.enabled)).once();
+            verify(experiment.inExperiment(AlwaysDisplayTestExplorerGroups.control)).never();
+            verify(experiment.sendTelemetryIfInExperiment(AlwaysDisplayTestExplorerGroups.control)).once();
+        });
+    });
+});
