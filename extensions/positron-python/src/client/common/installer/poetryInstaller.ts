647ee1509c4edc482605b6dfb1d4c5aff4932f3a
Alexander Pushkov <iamale@firemail.cc>
2019-06-20 10:48:51 -0500

    Append  --allow-prereleases to black installation command when Poetry is used (microsoft/vscode-python#5967)
    * if black is being installed, append --allow-prereleases, fixes microsoft/vscode-python#5756
    * update news
    * add unit test for installing black with poetry

--- extensions/positron-python/src/client/common/installer/poetryInstaller.ts
+++ extensions/positron-python/src/client/common/installer/poetryInstaller.ts
@@ -60,8 +60,12 @@ export class PoetryInstaller extends ModuleInstaller implements IModuleInstaller
     }
     protected async getExecutionInfo(moduleName: string, resource?: Uri): Promise<ExecutionInfo> {
         const execPath = this.configurationService.getSettings(resource).poetryPath;
+        const args = ['add', '--dev', moduleName];
+        if (moduleName === 'black') {
+            args.push('--allow-prereleases');
+        }
         return {
-            args: ['add', '--dev', moduleName],
+            args,
             execPath
         };
     }
--- extensions/positron-python/src/test/common/installer/poetryInstaller.unit.test.ts
+++ extensions/positron-python/src/test/common/installer/poetryInstaller.unit.test.ts
@@ -122,4 +122,15 @@ suite('Module Installer - Poetry', () => {
 
         assert.deepEqual(info, { args: ['add', '--dev', 'something'], execPath: 'poetry path' });
     });
+    test('Get executable info when installing black', async () => {
+        const uri = Uri.file(__dirname);
+        const settings = mock(PythonSettings);
+
+        when(configurationService.getSettings(uri)).thenReturn(instance(settings));
+        when(settings.poetryPath).thenReturn('poetry path');
+
+        const info = await poetryInstaller.getExecutionInfo('black', uri);
+
+        assert.deepEqual(info, { args: ['add', '--dev', 'black', '--allow-prereleases'], execPath: 'poetry path' });
+    });
 });
