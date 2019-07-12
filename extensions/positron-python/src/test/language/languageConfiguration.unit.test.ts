44a8526c11c743044e7fd1f397bd89fe0e21f6fa
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2019-07-12 11:16:46 -0500

    Add regex to dedent `else` and friends (microsoft/vscode-python#6497)
    * Dedent on elif|else
    * Add unit tests (will conflict with microsoft/vscode-python#4241)
    * More descriptive comment (same as f5daf78)
    * Move tests from extension to language config file
    * Add news file
    * Use regex we just introduced
    * Space is free
    * Update regexes and existing tests (so they pass)
    * Add support for open tuples and dictionaries
    * Shuffle and add tests
    * Typo + more descriptive key name
    * Updated news file after new changes
    * Apply suggestions from code review (first batch)
    Co-Authored-By: Eric Snow <ericsnowcurrently@gmail.com>
    * Update src/client/language/languageConfiguration.ts
    Co-Authored-By: Eric Snow <ericsnowcurrently@gmail.com>
    * Third batch of review fixes
    * Add some comments
    * Separate unit tests
    * More test simplification
    * Simplify return regex
    * Remove unnecessary tslint disable rule
    * pipeline is stuck

--- extensions/positron-python/src/test/language/languageConfiguration.unit.test.ts
+++ extensions/positron-python/src/test/language/languageConfiguration.unit.test.ts
@@ -5,19 +5,84 @@
 
 import { expect } from 'chai';
 
-import { MULTILINE_SEPARATOR_INDENT_REGEX } from '../../client/language/languageConfiguration';
+import { DECREASE_INDENT_REGEX, INCREASE_INDENT_REGEX, MULTILINE_SEPARATOR_INDENT_REGEX, OUTDENT_ONENTER_REGEX } from '../../client/language/languageConfiguration';
 
 suite('Language configuration regexes', () => {
     test('Multiline separator indent regex should not pick up strings with no multiline separator', async () => {
         const result = MULTILINE_SEPARATOR_INDENT_REGEX.test('a = "test"');
-        expect (result).to.be.equal(false, 'Multiline separator indent regex for regular strings should not have matches');
+        expect(result).to.be.equal(false, 'Multiline separator indent regex for regular strings should not have matches');
     });
+
     test('Multiline separator indent regex should not pick up strings with escaped characters', async () => {
         const result = MULTILINE_SEPARATOR_INDENT_REGEX.test('a = \'hello \\n\'');
-        expect (result).to.be.equal(false, 'Multiline separator indent regex for strings with escaped characters should not have matches');
+        expect(result).to.be.equal(false, 'Multiline separator indent regex for strings with escaped characters should not have matches');
     });
+
     test('Multiline separator indent regex should pick up strings ending with a multiline separator', async () => {
         const result = MULTILINE_SEPARATOR_INDENT_REGEX.test('a = \'multiline \\');
-        expect (result).to.be.equal(true, 'Multiline separator indent regex for strings with newline separator should have matches');
+        expect(result).to.be.equal(true, 'Multiline separator indent regex for strings with newline separator should have matches');
+    });
+
+    [
+        'async def test(self):',
+        'class TestClass:',
+        'def foo(self, node, namespace=""):',
+        'for item in items:',
+        'if foo is None:',
+        'try:',
+        'while \'::\' in macaddress:',
+        'with self.test:'
+    ].forEach(example => {
+        const keyword = example.split(' ')[0];
+
+        test(`Increase indent regex should pick up lines containing the ${keyword} keyword`, async () => {
+            const result = INCREASE_INDENT_REGEX.test(example);
+            expect(result).to.be.equal(true, `Increase indent regex should pick up lines containing the ${keyword} keyword`);
+        });
+
+        test(`Decrease indent regex should not pick up lines containing the ${keyword} keyword`, async () => {
+            const result = DECREASE_INDENT_REGEX.test(example);
+            expect(result).to.be.equal(false, `Decrease indent regex should not pick up lines containing the ${keyword} keyword`);
+        });
+    });
+
+    ['elif x < 5:', 'else:', 'except TestError:', 'finally:'].forEach(example => {
+        const keyword = example.split(' ')[0];
+
+        test(`Increase indent regex should pick up lines containing the ${keyword} keyword`, async () => {
+            const result = INCREASE_INDENT_REGEX.test(example);
+            expect(result).to.be.equal(true, `Increase indent regex should pick up lines containing the ${keyword} keyword`);
+        });
+
+        test(`Decrease indent regex should pick up lines containing the ${keyword} keyword`, async () => {
+            const result = DECREASE_INDENT_REGEX.test(example);
+            expect(result).to.be.equal(true, `Decrease indent regex should pick up lines containing the ${keyword} keyword`);
+        });
+    });
+
+    test('Increase indent regex should not pick up lines without keywords', async () => {
+        const result = INCREASE_INDENT_REGEX.test('a = \'hello \\n \'');
+        expect(result).to.be.equal(false, 'Increase indent regex should not pick up lines without keywords');
+    });
+
+    test('Decrease indent regex should not pick up lines without keywords', async () => {
+        const result = DECREASE_INDENT_REGEX.test('a = \'hello \\n \'');
+        expect(result).to.be.equal(false, 'Decrease indent regex should not pick up lines without keywords');
+    });
+
+    ['    break', '\t\t continue', ' pass', 'raise Exception(\'Unknown Exception\'', '    return [ True, False, False ]'].forEach(example => {
+        const keyword = example.trim().split(' ')[0];
+
+        const testWithoutComments = `Outdent regex for on enter rule should pick up lines containing the ${keyword} keyword`;
+        test(testWithoutComments, () => {
+            const result = OUTDENT_ONENTER_REGEX.test(example);
+            expect(result).to.be.equal(true, testWithoutComments);
+        });
+
+        const testWithComments = `Outdent regex on enter should pick up lines containing the ${keyword} keyword and ending with comments`;
+        test(testWithComments, () => {
+            const result = OUTDENT_ONENTER_REGEX.test(`${example} # test comment`);
+            expect(result).to.be.equal(true, testWithComments);
+        });
     });
 });
