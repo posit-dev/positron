6aeadeec7cb065fffa1590421e3a72336eaa1746
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2019-07-12 11:16:46 -0500

    Add regex to dedent `else` and friends (microsoft/vscode-python#6497)
    * Dedent on elif|else
    * Add unit tests (will conflict with microsoft/vscode-python#4241)
    * More descriptive comment (same as f5daf78)
    * Move tests from extension to language config file
    * Add news file
    * Use regex we just introduced
    * Space is free
    * Update regexes and existing tests (so they pass)
    * Add support for open tuples and dictionaries
    * Shuffle and add tests
    * Typo + more descriptive key name
    * Updated news file after new changes
    * Apply suggestions from code review (first batch)
    Co-Authored-By: Eric Snow <ericsnowcurrently@gmail.com>
    * Update src/client/language/languageConfiguration.ts
    Co-Authored-By: Eric Snow <ericsnowcurrently@gmail.com>
    * Third batch of review fixes
    * Add some comments
    * Separate unit tests
    * More test simplification
    * Simplify return regex
    * Remove unnecessary tslint disable rule
    * pipeline is stuck

--- extensions/positron-python/src/client/language/languageConfiguration.ts
+++ extensions/positron-python/src/client/language/languageConfiguration.ts
@@ -2,19 +2,24 @@
 // Licensed under the MIT License.
 'use strict';
 
-import { IndentAction, languages } from 'vscode';
-import { PYTHON_LANGUAGE } from '../common/constants';
+import { IndentAction } from 'vscode';
 
 export const MULTILINE_SEPARATOR_INDENT_REGEX = /^(?!\s+\\)[^#\n]+\\$/;
+/**
+ * This does not handle all cases. However, it does handle nearly all usage.
+ * Here's what it does not cover:
+ * - the statement is split over multiple lines (and hence the ":" is on a different line)
+ * - the code block is inlined (after the ":")
+ * - there are multiple statements on the line (separated by semicolons)
+ * Also note that `lambda` is purposefully excluded.
+ */
+export const INCREASE_INDENT_REGEX = /^\s*(?:(?:async|class|def|elif|except|for|if|while|with)\b.*|(else|finally|try))\s*:\s*(#.*)?$/;
+export const DECREASE_INDENT_REGEX = /^\s*(?:else|finally|(?:elif|except)\b.*)\s*:\s*(#.*)?$/;
+export const OUTDENT_ONENTER_REGEX = /^\s*(?:break|continue|pass|(?:raise|return)\b.*)\s*(#.*)?$/;
 
-export function setLanguageConfiguration() {
-    // Enable indentAction
-    languages.setLanguageConfiguration(PYTHON_LANGUAGE, {
+export function getLanguageConfiguration() {
+    return {
         onEnterRules: [
-            {
-                beforeText: /^\s*(?:def|class|for|if|elif|else|while|try|with|finally|except|async)\b.*:\s*/,
-                action: { indentAction: IndentAction.Indent }
-            },
             {
                 beforeText: MULTILINE_SEPARATOR_INDENT_REGEX,
                 action: { indentAction: IndentAction.Indent }
@@ -25,10 +30,13 @@ export function setLanguageConfiguration() {
                 action: { indentAction: IndentAction.None, appendText: '# ' }
             },
             {
-                beforeText: /^\s+(continue|break|return)\b.*/,
-                afterText: /\s+$/,
+                beforeText: OUTDENT_ONENTER_REGEX,
                 action: { indentAction: IndentAction.Outdent }
             }
-        ]
-    });
+        ],
+        indentationRules: {
+            increaseIndentPattern: INCREASE_INDENT_REGEX,
+            decreaseIndentPattern: DECREASE_INDENT_REGEX
+        }
+    };
 }
