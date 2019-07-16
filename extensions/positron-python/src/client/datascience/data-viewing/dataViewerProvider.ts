7ec5f1f26b8af849052fa5bae9f50d768ec6d096
Rich Chiodo <rchiodo@users.noreply.github.com>
2019-07-15 19:04:40 -0500

    Fix pandas version parsing (microsoft/vscode-python#6600)

--- extensions/positron-python/src/client/datascience/data-viewing/dataViewerProvider.ts
+++ extensions/positron-python/src/client/datascience/data-viewing/dataViewerProvider.ts
@@ -19,11 +19,11 @@ export class DataViewerProvider implements IDataViewerProvider, IAsyncDisposable
     private activeExplorers: IDataViewer[] = [];
     constructor(
         @inject(IServiceContainer) private serviceContainer: IServiceContainer,
-        @inject(IAsyncDisposableRegistry) asyncRegistry : IAsyncDisposableRegistry,
+        @inject(IAsyncDisposableRegistry) asyncRegistry: IAsyncDisposableRegistry,
         @inject(IJupyterVariables) private variables: IJupyterVariables,
-        @inject(IPythonExecutionFactory) private pythonFactory : IPythonExecutionFactory,
+        @inject(IPythonExecutionFactory) private pythonFactory: IPythonExecutionFactory,
         @inject(IInterpreterService) private interpreterService: IInterpreterService
-        ) {
+    ) {
         asyncRegistry.push(this);
     }
 
@@ -31,7 +31,7 @@ export class DataViewerProvider implements IDataViewerProvider, IAsyncDisposable
         await Promise.all(this.activeExplorers.map(d => d.dispose()));
     }
 
-    public async create(variable: string) : Promise<IDataViewer>{
+    public async create(variable: string): Promise<IDataViewer> {
         // Make sure this is a valid variable
         const variables = await this.variables.getVariables();
         const index = variables.findIndex(v => v && v.name === variable);
@@ -45,17 +45,17 @@ export class DataViewerProvider implements IDataViewerProvider, IAsyncDisposable
         throw new Error(localize.DataScience.dataExplorerInvalidVariableFormat().format(variable));
     }
 
-    public async getPandasVersion() : Promise<{major: number; minor: number; build: number} | undefined> {
+    public async getPandasVersion(): Promise<{ major: number; minor: number; build: number } | undefined> {
         const interpreter = await this.interpreterService.getActiveInterpreter();
         const launcher = await this.pythonFactory.createActivatedEnvironment({ resource: undefined, interpreter, allowEnvironmentFetchExceptions: true });
         try {
-            const result = await launcher.exec(['-c', 'import pandas;print(pandas.__version__)'], {throwOnStdErr: true});
-            const versionMatch = /^\s*(\d+)\.(\d+)\.(\d+)\s*$/.exec(result.stdout);
+            const result = await launcher.exec(['-c', 'import pandas;print(pandas.__version__)'], { throwOnStdErr: true });
+            const versionMatch = /^\s*(\d+)\.(\d+)\.(.+)\s*$/.exec(result.stdout);
             if (versionMatch && versionMatch.length > 2) {
                 const major = parseInt(versionMatch[1], 10);
                 const minor = parseInt(versionMatch[2], 10);
                 const build = parseInt(versionMatch[3], 10);
-                return {major, minor, build};
+                return { major, minor, build };
             }
         } catch {
             noop();
