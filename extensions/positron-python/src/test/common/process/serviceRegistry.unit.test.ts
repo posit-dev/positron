0d9cf16ad1a4fbacf049d67abd1b40f782f3564c
Kartik Raj <karraj@microsoft.com>
2019-08-06 01:19:28 -0500

    Added all serviceRegistry tests (microsoft/vscode-python#6860)
    * Added tests
    
    * Update src/test/common/installer/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/dotnet/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/platform/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/terminals/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/process/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/linters/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/variables/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/providers/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>

---
+++ extensions/positron-python/src/test/common/process/serviceRegistry.unit.test.ts
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { instance, mock, verify } from 'ts-mockito';
+import { BufferDecoder } from '../../../client/common/process/decoder';
+import { ProcessServiceFactory } from '../../../client/common/process/processFactory';
+import { PythonExecutionFactory } from '../../../client/common/process/pythonExecutionFactory';
+import { PythonToolExecutionService } from '../../../client/common/process/pythonToolService';
+import { registerTypes } from '../../../client/common/process/serviceRegistry';
+import { IBufferDecoder, IProcessServiceFactory, IPythonExecutionFactory, IPythonToolExecutionService } from '../../../client/common/process/types';
+import { ServiceManager } from '../../../client/ioc/serviceManager';
+import { IServiceManager } from '../../../client/ioc/types';
+
+suite('Common Process Service Registry', () => {
+    let serviceManager: IServiceManager;
+
+    setup(() => {
+        serviceManager = mock(ServiceManager);
+    });
+
+    test('Ensure services are registered', async () => {
+        registerTypes(instance(serviceManager));
+        verify(serviceManager.addSingleton<IBufferDecoder>(IBufferDecoder, BufferDecoder)).once();
+        verify(serviceManager.addSingleton<IProcessServiceFactory>(IProcessServiceFactory, ProcessServiceFactory)).once();
+        verify(serviceManager.addSingleton<IPythonExecutionFactory>(IPythonExecutionFactory, PythonExecutionFactory)).once();
+        verify(serviceManager.addSingleton<IPythonToolExecutionService>(IPythonToolExecutionService, PythonToolExecutionService)).once();
+    });
+});
---
+++ extensions/positron-python/src/test/providers/serviceRegistry.unit.test.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { instance, mock, verify } from 'ts-mockito';
+import { ServiceManager } from '../../client/ioc/serviceManager';
+import { IServiceManager } from '../../client/ioc/types';
+import { SortImportsEditingProvider } from '../../client/providers/importSortProvider';
+import { registerTypes } from '../../client/providers/serviceRegistry';
+import { ISortImportsEditingProvider } from '../../client/providers/types';
+
+suite('Common Providers Service Registry', () => {
+    let serviceManager: IServiceManager;
+
+    setup(() => {
+        serviceManager = mock(ServiceManager);
+    });
+
+    test('Ensure services are registered', async () => {
+        registerTypes(instance(serviceManager));
+        verify(serviceManager.addSingleton<ISortImportsEditingProvider>(ISortImportsEditingProvider, SortImportsEditingProvider)).once();
+    });
+});
