fea8a8e279f5ccfcefa363d751614898bc7588fa
Kartik Raj <karraj@microsoft.com>
2019-08-06 01:19:28 -0500

    Added all serviceRegistry tests (microsoft/vscode-python#6860)
    * Added tests
    
    * Update src/test/common/installer/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/dotnet/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/platform/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/terminals/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/process/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/linters/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/common/variables/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>
    
    * Update src/test/providers/serviceRegistry.unit.test.ts
    
    Co-Authored-By: Don Jayamanne <don.jayamanne@yahoo.com>

---
+++ extensions/positron-python/src/test/common/installer/serviceRegistry.unit.test.ts
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { instance, mock, verify } from 'ts-mockito';
+import { IWebPanelProvider } from '../../../client/common/application/types';
+import { WebPanelProvider } from '../../../client/common/application/webPanelProvider';
+import { InstallationChannelManager } from '../../../client/common/installer/channelManager';
+import { CondaInstaller } from '../../../client/common/installer/condaInstaller';
+import { InsidersBuildInstaller, StableBuildInstaller } from '../../../client/common/installer/extensionBuildInstaller';
+import { PipEnvInstaller } from '../../../client/common/installer/pipEnvInstaller';
+import { PipInstaller } from '../../../client/common/installer/pipInstaller';
+import { PoetryInstaller } from '../../../client/common/installer/poetryInstaller';
+import { CTagsProductPathService, DataScienceProductPathService, FormatterProductPathService, LinterProductPathService, RefactoringLibraryProductPathService, TestFrameworkProductPathService } from '../../../client/common/installer/productPath';
+import { ProductService } from '../../../client/common/installer/productService';
+import { registerTypes } from '../../../client/common/installer/serviceRegistry';
+import { IExtensionBuildInstaller, IInstallationChannelManager, IModuleInstaller, INSIDERS_INSTALLER, IProductPathService, IProductService, STABLE_INSTALLER } from '../../../client/common/installer/types';
+import { ProductType } from '../../../client/common/types';
+import { ServiceManager } from '../../../client/ioc/serviceManager';
+import { IServiceManager } from '../../../client/ioc/types';
+
+suite('Common installer Service Registry', () => {
+    let serviceManager: IServiceManager;
+
+    setup(() => {
+        serviceManager = mock(ServiceManager);
+    });
+
+    test('Ensure services are registered', async () => {
+        registerTypes(instance(serviceManager));
+        verify(serviceManager.addSingleton<IModuleInstaller>(IModuleInstaller, CondaInstaller)).once();
+        verify(serviceManager.addSingleton<IModuleInstaller>(IModuleInstaller, PipInstaller)).once();
+        verify(serviceManager.addSingleton<IModuleInstaller>(IModuleInstaller, PipEnvInstaller)).once();
+        verify(serviceManager.addSingleton<IModuleInstaller>(IModuleInstaller, PoetryInstaller)).once();
+        verify(serviceManager.addSingleton<IInstallationChannelManager>(IInstallationChannelManager, InstallationChannelManager)).once();
+        verify(serviceManager.addSingleton<IExtensionBuildInstaller>(IExtensionBuildInstaller, StableBuildInstaller, STABLE_INSTALLER)).once();
+        verify(serviceManager.addSingleton<IExtensionBuildInstaller>(IExtensionBuildInstaller, InsidersBuildInstaller, INSIDERS_INSTALLER)).once();
+
+        verify(serviceManager.addSingleton<IProductService>(IProductService, ProductService)).once();
+        verify(serviceManager.addSingleton<IProductPathService>(IProductPathService, CTagsProductPathService, ProductType.WorkspaceSymbols)).once();
+        verify(serviceManager.addSingleton<IProductPathService>(IProductPathService, FormatterProductPathService, ProductType.Formatter)).once();
+        verify(serviceManager.addSingleton<IProductPathService>(IProductPathService, LinterProductPathService, ProductType.Linter)).once();
+        verify(serviceManager.addSingleton<IProductPathService>(IProductPathService, TestFrameworkProductPathService, ProductType.TestFramework)).once();
+        verify(serviceManager.addSingleton<IProductPathService>(IProductPathService, RefactoringLibraryProductPathService, ProductType.RefactoringLibrary)).once();
+        verify(serviceManager.addSingleton<IProductPathService>(IProductPathService, DataScienceProductPathService, ProductType.DataScience)).once();
+        verify(serviceManager.addSingleton<IWebPanelProvider>(IWebPanelProvider, WebPanelProvider)).once();
+    });
+});
---
+++ extensions/positron-python/src/test/linters/serviceRegistry.unit.test.ts
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import { instance, mock, verify } from 'ts-mockito';
+import { ServiceManager } from '../../client/ioc/serviceManager';
+import { IServiceManager } from '../../client/ioc/types';
+import { AvailableLinterActivator } from '../../client/linters/linterAvailability';
+import { LinterManager } from '../../client/linters/linterManager';
+import { LintingEngine } from '../../client/linters/lintingEngine';
+import { registerTypes } from '../../client/linters/serviceRegistry';
+import { IAvailableLinterActivator, ILinterManager, ILintingEngine } from '../../client/linters/types';
+
+suite('Linters Service Registry', () => {
+    let serviceManager: IServiceManager;
+
+    setup(() => {
+        serviceManager = mock(ServiceManager);
+    });
+
+    test('Ensure services are registered', async () => {
+        registerTypes(instance(serviceManager));
+        verify(serviceManager.addSingleton<ILintingEngine>(ILintingEngine, LintingEngine)).once();
+        verify(serviceManager.addSingleton<ILinterManager>(ILinterManager, LinterManager)).once();
+        verify(serviceManager.add<IAvailableLinterActivator>(IAvailableLinterActivator, AvailableLinterActivator)).once();
+    });
+});
