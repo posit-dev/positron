4d1582e984493a99704f4e360fcc2f9f733a537c
Don Jayamanne <don.jayamanne@yahoo.com>
2019-08-12 12:10:32 -0500

    Fix smoke tests (microsoft/vscode-python#6936)

--- extensions/positron-python/src/test/standardTest.ts
+++ extensions/positron-python/src/test/standardTest.ts
@@ -7,12 +7,13 @@ import { EXTENSION_ROOT_DIR_FOR_TESTS } from './constants';
 process.env.IS_CI_SERVER_TEST_DEBUGGER = '';
 process.env.VSC_PYTHON_CI_TEST = '1';
 const workspacePath = process.env.CODE_TESTS_WORKSPACE ? process.env.CODE_TESTS_WORKSPACE : path.join(__dirname, '..', '..', 'src', 'test');
+const extensionDevelopmentPath = process.env.CODE_EXTENSIONS_PATH ? process.env.CODE_EXTENSIONS_PATH : EXTENSION_ROOT_DIR_FOR_TESTS;
 
 function start() {
     console.log('*'.repeat(100));
     console.log('Start Standard tests');
     runTests({
-        extensionDevelopmentPath: EXTENSION_ROOT_DIR_FOR_TESTS,
+        extensionDevelopmentPath: extensionDevelopmentPath,
         extensionTestsPath: path.join(EXTENSION_ROOT_DIR_FOR_TESTS, 'out', 'test', 'index'),
         launchArgs: [workspacePath],
         version: 'stable'
