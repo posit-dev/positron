341e9e74e36c0bdd4a375a575e44b158b6e3a2d7
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2019-09-19 17:04:28 -0500

    Guarantee that master branch has "-dev" tag in version (microsoft/vscode-python#7477)
    * Test this
    * Skip tests when running locally
    * Prep for microsoft/vscode-python#5908 (release* branches)
    * News file
    * Use require()
    * Forgot to update something lol
    * Add comment + undo microsoft/vscode-python#5908 change
    * Use application.ts
    * Review changes

--- extensions/positron-python/src/test/extension.unit.test.ts
+++ extensions/positron-python/src/test/extension.unit.test.ts
@@ -7,6 +7,8 @@
 
 import { expect } from 'chai';
 import { buildApi } from '../client/api';
+import { ApplicationEnvironment } from '../client/common/application/applicationEnvironment';
+import { IApplicationEnvironment } from '../client/common/application/types';
 import { EXTENSION_ROOT_DIR } from '../client/common/constants';
 
 const expectedPath = `${EXTENSION_ROOT_DIR.fileToCommandArgument()}/pythonFiles/ptvsd_launcher.py`;
@@ -21,5 +23,42 @@ suite('Extension API Debugger', () => {
         const args = await buildApi(Promise.resolve()).debug.getRemoteLauncherCommand('something', 1234, true);
         const expectedArgs = [expectedPath, '--default', '--host', 'something', '--port', '1234', '--wait'];
         expect(args).to.be.deep.equal(expectedArgs);
+    });
+});
+
+suite('Extension version tests', () => {
+    let version: string;
+    let applicationEnvironment: IApplicationEnvironment;
+    const branchName = process.env.CI_BRANCH_NAME;
+
+    suiteSetup(async function() {
+        // Skip the entire suite if running locally
+        if (!branchName) {
+            // tslint:disable-next-line: no-invalid-this
+            return this.skip();
+        }
+    });
+
+    setup(() => {
+        applicationEnvironment = new ApplicationEnvironment(undefined as any, undefined as any, undefined as any);
+        version = applicationEnvironment.packageJson.version;
+    });
+
+    test('If we are running a pipeline in the master branch, the extension version in `package.json` should have the "-dev" suffix', async function() {
+        if (branchName !== 'master') {
+            // tslint:disable-next-line: no-invalid-this
+            return this.skip();
+        }
+
+        return expect(version.endsWith('-dev'), 'When running a pipeline in the master branch, the extension version in package.json should have the -dev suffix').to.be.true;
+    });
+
+    test('If we are running a pipeline in the release branch, the extension version in `package.json` should not have the "-dev" suffix', async function() {
+        if (!branchName!.startsWith('release')) {
+            // tslint:disable-next-line: no-invalid-this
+            return this.skip();
+        }
+
+        return expect(version.endsWith('-dev'), 'When running a pipeline in the release branch, the extension version in package.json should not have the -dev suffix').to.be.false;
     });
 });
