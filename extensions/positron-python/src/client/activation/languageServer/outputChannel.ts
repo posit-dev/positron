d03a80bb18fa057a0b1c940c76fca4ba252cf73f
Don Jayamanne <don.jayamanne@yahoo.com>
2019-09-18 23:26:30 -0500

    Add command to display language server output panel (microsoft/vscode-python#7461)
    * Add command to display ls output panel
    
    * Code review changes
    
    * Add tests

--- extensions/positron-python/src/client/activation/languageServer/outputChannel.ts
+++ extensions/positron-python/src/client/activation/languageServer/outputChannel.ts
@@ -4,7 +4,8 @@
 'use strict';
 
 import { inject, injectable } from 'inversify';
-import { IApplicationShell } from '../../common/application/types';
+import { IApplicationShell, ICommandManager } from '../../common/application/types';
+import '../../common/extensions';
 import { IOutputChannel } from '../../common/types';
 import { OutputChannelNames } from '../../common/utils/localize';
 import { ILanguageServerOutputChannel } from '../types';
@@ -12,14 +13,27 @@ import { ILanguageServerOutputChannel } from '../types';
 @injectable()
 export class LanguageServerOutputChannel implements ILanguageServerOutputChannel {
     public output: IOutputChannel | undefined;
+    private registered = false;
     constructor(
-        @inject(IApplicationShell) private readonly appShell: IApplicationShell
+        @inject(IApplicationShell) private readonly appShell: IApplicationShell,
+        @inject(ICommandManager) private readonly commandManager: ICommandManager
     ) { }
 
     public get channel() {
         if (!this.output) {
             this.output = this.appShell.createOutputChannel(OutputChannelNames.languageServer());
+            this.registerCommand().ignoreErrors();
         }
         return this.output;
+    }
+    private async registerCommand() {
+        if (this.registered) {
+            return;
+        }
+        this.registered = true;
+        // This controls the visibility of the command used to display the LS Output panel.
+        // We don't want to display it when Jedi is used instead of LS.
+        await this.commandManager.executeCommand('setContext', 'python.hasLanguageServerOutputChannel', true);
+        this.commandManager.registerCommand('python.viewLanguageServerOutput', () => this.output!.show(true));
     }
 }
