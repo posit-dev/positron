7751bfb5a363d38f8897d3314c226423b246286a
Karthik Nadig <kanadig@microsoft.com>
2019-10-31 15:24:00 -0500

    Adds logging support for python debug adapter (microsoft/vscode-python#8272)
    * Add debug session logging via tracker
    * Add log messages
    * Ensure we only log as needed.
    * Change log file name
    * Add news item
    * Adding unit test for debug session logger
    * Improve coverage.
    * Address review comments.

--- extensions/positron-python/src/client/debugger/extension/serviceRegistry.ts
+++ extensions/positron-python/src/client/debugger/extension/serviceRegistry.ts
@@ -8,6 +8,7 @@ import { IServiceManager } from '../../ioc/types';
 import { AttachRequestArguments, LaunchRequestArguments } from '../types';
 import { DebugAdapterActivator } from './adapter/activator';
 import { DebugAdapterDescriptorFactory } from './adapter/factory';
+import { DebugSessionLoggingFactory } from './adapter/logging';
 import { DebuggerBanner } from './banner';
 import { PythonDebugConfigurationService } from './configuration/debugConfigurationService';
 import { LaunchJsonCompletionProvider } from './configuration/launch.json/completionProvider';
@@ -26,7 +27,7 @@ import { IDebugConfigurationProviderFactory, IDebugConfigurationResolver } from 
 import { ChildProcessAttachEventHandler } from './hooks/childProcessAttachHandler';
 import { ChildProcessAttachService } from './hooks/childProcessAttachService';
 import { IChildProcessAttachService, IDebugSessionEventHandlers } from './hooks/types';
-import { DebugConfigurationType, IDebugAdapterDescriptorFactory, IDebugConfigurationProvider, IDebugConfigurationService, IDebuggerBanner } from './types';
+import { DebugConfigurationType, IDebugAdapterDescriptorFactory, IDebugConfigurationProvider, IDebugConfigurationService, IDebuggerBanner, IDebugSessionLoggingFactory } from './types';
 
 export function registerTypes(serviceManager: IServiceManager) {
     serviceManager.addSingleton<IExtensionSingleActivationService>(IExtensionSingleActivationService, LaunchJsonCompletionProvider);
@@ -47,4 +48,5 @@ export function registerTypes(serviceManager: IServiceManager) {
     serviceManager.addSingleton<IDebugEnvironmentVariablesService>(IDebugEnvironmentVariablesService, DebugEnvironmentVariablesHelper);
     serviceManager.addSingleton<IExtensionSingleActivationService>(IExtensionSingleActivationService, DebugAdapterActivator);
     serviceManager.addSingleton<IDebugAdapterDescriptorFactory>(IDebugAdapterDescriptorFactory, DebugAdapterDescriptorFactory);
+    serviceManager.addSingleton<IDebugSessionLoggingFactory>(IDebugSessionLoggingFactory, DebugSessionLoggingFactory);
 }
