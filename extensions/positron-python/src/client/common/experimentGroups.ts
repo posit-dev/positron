3c86c4e8dcdb152e17d1372b6d13b7fe82ab4245
Luciana Abud <45497113+luabud@users.noreply.github.com>
2019-10-31 15:23:09 -0500

    Changing NewDebugAdapter experiment to be on for 50% of insiders users (microsoft/vscode-python#8267)
    * Changing NewDebugAdapter experiment to be on for 50% of insiders users
    
    * Fix wrong rename of DebugAdapterDescriptorFactory enum
    
    * Revert changes for Debug Adapter tests

--- extensions/positron-python/src/client/common/experimentGroups.ts
+++ extensions/positron-python/src/client/common/experimentGroups.ts
@@ -15,8 +15,8 @@ export enum ShowExtensionSurveyPrompt {
 
 // Experiment to check whether the extension should use the new VS Code debug adapter API.
 export enum DebugAdapterDescriptorFactory {
-    control = 'DebugAdapterFactory - control',
-    experiment = 'DebugAdapterFactory - experiment'
+    control = 'DebugAdapterFactoryInsiders - control',
+    experiment = 'DebugAdapterFactoryInsiders - experiment'
 }
 
 // Experiment to check whether the ptvsd launcher should use pre-installed ptvsd wheels for debugging.
