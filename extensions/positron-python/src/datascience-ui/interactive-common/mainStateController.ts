9aecc274dd31dabfa71d53bc71f2030e13d5cd3b
David Kutugata <dakutuga@microsoft.com>
2019-10-31 18:02:43 -0500

    clear execution count of all cells when user presses 'Clear All Output' (microsoft/vscode-python#8318)
    * clear execution count of all cells when user
    presses 'Clear All Output'
    
    * remove execution count with immutable

--- extensions/positron-python/src/datascience-ui/interactive-common/mainStateController.ts
+++ extensions/positron-python/src/datascience-ui/interactive-common/mainStateController.ts
@@ -399,7 +399,8 @@ export class MainStateController implements IMessageHandler {
 
     public clearAllOutputs = () => {
         const newList = this.pendingState.cellVMs.map(cellVM => {
-            return immutable.updateIn(cellVM, ['cell', 'data', 'outputs'], () => []);
+            const updatedVm = immutable.updateIn(cellVM, ['cell', 'data', 'outputs'], () => []);
+            return immutable.removeIn(updatedVm, ['cell', 'data', 'execution_count']);
         });
         this.setState({
             cellVMs: newList
