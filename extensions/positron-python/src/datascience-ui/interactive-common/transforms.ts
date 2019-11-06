085b7812b5051d4e6035cdbc462eecbc3378056a
Rich Chiodo <rchiodo@users.noreply.github.com>
2019-11-06 17:39:01 -0600

    Add support for vega (microsoft/vscode-python#8409)
    * Add support for vegas
    
    * Add news entry
    
    * Update dependencies

--- extensions/positron-python/src/datascience-ui/interactive-common/transforms.ts
+++ extensions/positron-python/src/datascience-ui/interactive-common/transforms.ts
@@ -1,11 +1,6 @@
 /* tslint:disable */
 'use strict';
 
-// This code is from @nteract/transforms-full except without the Vega transforms:
-// https://github.com/nteract/nteract/blob/v0.12.2/packages/transforms-full/src/index.js .
-// Vega transforms mess up our npm pkg install because they rely on the npm canvas module that needs
-// to be built on each system.
-
 import PlotlyTransform, {
     PlotlyNullTransform
 } from "@nteract/transform-plotly";
@@ -15,7 +10,7 @@ import ModelDebug from "@nteract/transform-model-debug";
 
 import DataResourceTransform from "@nteract/transform-dataresource";
 
-// import { VegaLite1, VegaLite2, Vega2, Vega3 } from "@nteract/transform-vega";
+import { VegaLite1, VegaLite2, VegaLite3, Vega, Vega2, Vega3 } from "@nteract/transform-vega";
 
 import {
     standardTransforms,
@@ -30,6 +25,12 @@ const additionalTransforms = [
     PlotlyNullTransform,
     PlotlyTransform,
     GeoJSONTransform,
+    VegaLite1,
+    VegaLite2,
+    VegaLite3,
+    Vega,
+    Vega2,
+    Vega3
 ];
 
 const { transforms, displayOrder } = additionalTransforms.reduce(
