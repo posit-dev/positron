526f767e29d4c9885de10f308ec5752dc6051c76
Don Jayamanne <don.jayamanne@yahoo.com>
2019-11-15 16:10:04 -0600

    Add new folder into `src` for `ipywidgets` (microsoft/vscode-python#8609)
    * Initial commit
    * Ignore ipywidgets folder

---
+++ extensions/positron-python/src/ipywidgets/scripts/copyBuild.js
@@ -0,0 +1,6 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+var fs = require('fs');
+// TODO: To be fixed when integrating ipywidgets with extension.
+// fs.copyFileSync('built/ipywidgets.js', '/Users/donjayamanne/.vscode-insiders/extensions/pythonVSCode/out/datascience-ui/native-editor/ipywidgets.js');
