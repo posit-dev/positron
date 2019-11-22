1e9134fb42a6f3ddee659daa5703cb2380108166
Don Jayamanne <don.jayamanne@yahoo.com>
2019-11-22 14:17:27 -0600

    Pass resource when accessing VS Code settings (microsoft/vscode-python#8641)
    * Pass in resource when accessing settings
    
    * News entry

--- extensions/positron-python/src/client/datascience/interactive-ipynb/autoSaveService.ts
+++ extensions/positron-python/src/client/datascience/interactive-ipynb/autoSaveService.ts
@@ -92,7 +92,7 @@ export class AutoSaveService implements IInteractiveWindowListener {
         };
     }
     private onSettingsChanded(e: ConfigurationChangeEvent) {
-        if (e.affectsConfiguration('files.autoSave') || e.affectsConfiguration('files.autoSaveDelay')) {
+        if (e.affectsConfiguration('files.autoSave', this.notebookUri) || e.affectsConfiguration('files.autoSaveDelay', this.notebookUri)) {
             // Reset the timer, as we may have increased it, turned it off or other.
             this.clearTimeout();
             this.setTimer();
