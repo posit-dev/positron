ef7612383cee274cc20639103be05a5029756aae
Karthik Nadig <kanadig@microsoft.com>
2019-11-28 01:30:42 -0600

    Fix the experiment check while getting remote debugger arguments. (microsoft/vscode-python#8808)
    * Fix extension api experiment check and update test
    
    * Update package-lock.json

--- extensions/positron-python/src/test/extension.unit.test.ts
+++ extensions/positron-python/src/test/extension.unit.test.ts
@@ -11,89 +11,100 @@ import { anyString, anything, instance, mock, when } from 'ts-mockito';
 import { buildApi } from '../client/api';
 import { ApplicationEnvironment } from '../client/common/application/applicationEnvironment';
 import { IApplicationEnvironment } from '../client/common/application/types';
-import { ConfigurationService } from '../client/common/configuration/service';
 import { EXTENSION_ROOT_DIR } from '../client/common/constants';
 import { ExperimentsManager } from '../client/common/experiments';
-import { IConfigurationService, IExperimentsManager, IPythonSettings } from '../client/common/types';
+import { IExperimentsManager } from '../client/common/types';
 import { DebugAdapterDescriptorFactory } from '../client/debugger/extension/adapter/factory';
 import { IDebugAdapterDescriptorFactory } from '../client/debugger/extension/types';
 
+// tslint:disable-next-line: max-func-body-length
 suite('Extension API Debugger', () => {
     const expectedLauncherPath = `${EXTENSION_ROOT_DIR.fileToCommandArgument()}/pythonFiles/ptvsd_launcher.py`;
     const ptvsdPath = path.join('path', 'to', 'ptvsd');
+    const ptvsdHost = 'somehost';
+    const ptvsdPort = 12345;
 
     let experimentsManager: IExperimentsManager;
     let debugAdapterFactory: IDebugAdapterDescriptorFactory;
-    let configurationService: IConfigurationService;
 
     setup(() => {
         experimentsManager = mock(ExperimentsManager);
         debugAdapterFactory = mock(DebugAdapterDescriptorFactory);
-        configurationService = mock(ConfigurationService);
     });
 
-    function mockInExperiment() {
+    function mockInExperiment(host: string, port: number, wait: boolean) {
         when(experimentsManager.inExperiment(anyString())).thenReturn(true);
         when(debugAdapterFactory.useNewPtvsd(anyString())).thenResolve(true);
         when(debugAdapterFactory.getPtvsdPath()).thenReturn(ptvsdPath);
-        when(configurationService.getSettings(undefined)).thenReturn(({ pythonPath: 'python' } as any) as IPythonSettings);
+        if (wait) {
+            when(debugAdapterFactory.getRemotePtvsdArgs(anything())).thenReturn(['--host', host, '--port', port.toString(), '--wait']);
+        } else {
+            when(debugAdapterFactory.getRemotePtvsdArgs(anything())).thenReturn(['--host', host, '--port', port.toString()]);
+        }
     }
 
-    function mockNotInExperiment() {
+    function mockNotInExperiment(host: string, port: number, wait: boolean) {
         when(experimentsManager.inExperiment(anyString())).thenReturn(false);
         when(debugAdapterFactory.useNewPtvsd(anyString())).thenResolve(false);
+        if (wait) {
+            when(debugAdapterFactory.getRemotePtvsdArgs(anything())).thenReturn(['--default', '--host', host, '--port', port.toString(), '--wait']);
+        } else {
+            when(debugAdapterFactory.getRemotePtvsdArgs(anything())).thenReturn(['--default', '--host', host, '--port', port.toString()]);
+        }
     }
 
     test('Test debug launcher args (no-wait and not in experiment)', async () => {
-        mockNotInExperiment();
+        const waitForAttach = false;
+        mockNotInExperiment(ptvsdHost, ptvsdPort, waitForAttach);
 
-        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory), instance(configurationService)).debug.getRemoteLauncherCommand(
-            'something',
-            1234,
-            false
+        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory)).debug.getRemoteLauncherCommand(
+            ptvsdHost,
+            ptvsdPort,
+            waitForAttach
         );
-        const expectedArgs = [expectedLauncherPath, '--default', '--host', 'something', '--port', '1234'];
+        const expectedArgs = [expectedLauncherPath, '--default', '--host', ptvsdHost, '--port', ptvsdPort.toString()];
 
         expect(args).to.be.deep.equal(expectedArgs);
     });
 
     test('Test debug launcher args (no-wait and in experiment)', async () => {
-        mockInExperiment();
-        when(debugAdapterFactory.getRemotePtvsdArgs(anything())).thenReturn(['--host', 'something', '--port', '1234']);
+        const waitForAttach = false;
+        mockInExperiment(ptvsdHost, ptvsdPort, waitForAttach);
 
-        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory), instance(configurationService)).debug.getRemoteLauncherCommand(
-            'something',
-            1234,
-            false
+        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory)).debug.getRemoteLauncherCommand(
+            ptvsdHost,
+            ptvsdPort,
+            waitForAttach
         );
-        const expectedArgs = [ptvsdPath, '--host', 'something', '--port', '1234'];
+        const expectedArgs = [ptvsdPath, '--host', ptvsdHost, '--port', ptvsdPort.toString()];
 
         expect(args).to.be.deep.equal(expectedArgs);
     });
 
     test('Test debug launcher args (wait and not in experiment)', async () => {
-        mockNotInExperiment();
+        const waitForAttach = true;
+        mockNotInExperiment(ptvsdHost, ptvsdPort, waitForAttach);
 
-        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory), instance(configurationService)).debug.getRemoteLauncherCommand(
-            'something',
-            1234,
-            true
+        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory)).debug.getRemoteLauncherCommand(
+            ptvsdHost,
+            ptvsdPort,
+            waitForAttach
         );
-        const expectedArgs = [expectedLauncherPath, '--default', '--host', 'something', '--port', '1234', '--wait'];
+        const expectedArgs = [expectedLauncherPath, '--default', '--host', ptvsdHost, '--port', ptvsdPort.toString(), '--wait'];
 
         expect(args).to.be.deep.equal(expectedArgs);
     });
 
     test('Test debug launcher args (wait and in experiment)', async () => {
-        mockInExperiment();
-        when(debugAdapterFactory.getRemotePtvsdArgs(anything())).thenReturn(['--host', 'something', '--port', '1234', '--wait']);
+        const waitForAttach = true;
+        mockInExperiment(ptvsdHost, ptvsdPort, waitForAttach);
 
-        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory), instance(configurationService)).debug.getRemoteLauncherCommand(
-            'something',
-            1234,
-            true
+        const args = await buildApi(Promise.resolve(), instance(experimentsManager), instance(debugAdapterFactory)).debug.getRemoteLauncherCommand(
+            ptvsdHost,
+            ptvsdPort,
+            waitForAttach
         );
-        const expectedArgs = [ptvsdPath, '--host', 'something', '--port', '1234', '--wait'];
+        const expectedArgs = [ptvsdPath, '--host', ptvsdHost, '--port', ptvsdPort.toString(), '--wait'];
 
         expect(args).to.be.deep.equal(expectedArgs);
     });
