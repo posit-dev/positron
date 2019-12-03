2444327eb464e3bca7fada2e080f308f17a25fea
Ian Huff <ianhu@microsoft.com>
2019-12-03 15:14:37 -0600

    SVG Display issue (microsoft/vscode-python#8881)

--- extensions/positron-python/src/datascience-ui/interactive-common/cellOutput.tsx
+++ extensions/positron-python/src/datascience-ui/interactive-common/cellOutput.tsx
@@ -246,12 +246,18 @@ export class CellOutput extends React.Component<ICellOutputProps> {
         try {
             const mimeBundle = copy.data as nbformat.IMimeBundle;
             let data: nbformat.MultilineString | JSONObject = mimeBundle[mimeType];
+            // For un-executed output we might get text or svg output as multiline string arrays
+            // we want to concat those so we don't display a bunch of weird commas as we expect
+            // Single strings in our output
+            if (Array.isArray(data)) {
+                data = concatMultilineStringOutput(data as nbformat.MultilineString);
+            }
 
             // Text based mimeTypes don't get a white background
             if (/^text\//.test(mimeType)) {
                 return {
                     mimeType,
-                    data: concatMultilineStringOutput(data as nbformat.MultilineString),
+                    data,
                     isText,
                     isError,
                     renderWithScrollbars: true,
