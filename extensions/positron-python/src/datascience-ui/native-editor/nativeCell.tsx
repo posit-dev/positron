a8a1a602901b7dbd383f218fddf315d7980fd1dc
Rich Chiodo <rchiodo@users.noreply.github.com>
2019-12-09 18:14:16 -0600

    Fix scrolling in large cells. (microsoft/vscode-python#8971)

--- extensions/positron-python/src/datascience-ui/native-editor/nativeCell.tsx
+++ extensions/positron-python/src/datascience-ui/native-editor/nativeCell.tsx
@@ -219,13 +219,13 @@ export class NativeCell extends React.Component<INativeCellProps> {
         switch (e.code) {
             case 'ArrowUp':
             case 'k':
-                if ((isFocusedWhenNotSuggesting && e.editorInfo!.isFirstLine) || !this.isFocused()) {
+                if ((isFocusedWhenNotSuggesting && e.editorInfo!.isFirstLine && !e.shiftKey) || !this.isFocused()) {
                     this.arrowUpFromCell(e);
                 }
                 break;
             case 'ArrowDown':
             case 'j':
-                if ((isFocusedWhenNotSuggesting && e.editorInfo!.isLastLine) || !this.isFocused()) {
+                if ((isFocusedWhenNotSuggesting && e.editorInfo!.isLastLine && !e.shiftKey) || !this.isFocused()) {
                     this.arrowDownFromCell(e);
                 }
                 break;
