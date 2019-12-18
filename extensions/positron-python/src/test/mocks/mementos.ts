c86d1739b0f98fefc9368fcf1ccab4e110e6951f
Rich Chiodo <rchiodo@users.noreply.github.com>
2019-12-18 14:54:37 -0600

    Eliminate global storage for notebooks. (microsoft/vscode-python#9192)
    * Eliminate global storage for notebooks.
    
    * Change case
    
    * Code review feedback
    
    * More comments

--- extensions/positron-python/src/test/mocks/mementos.ts
+++ extensions/positron-python/src/test/mocks/mementos.ts
@@ -3,21 +3,24 @@ import { Memento } from 'vscode';
 
 @injectable()
 export class MockMemento implements Memento {
-    private map: Map<string, {}> = new Map<string, {}>();
+    // Note: This has to be called _value so that it matches
+    // what VS code has for a memento. We use this to eliminate a bad bug
+    // with writing too much data to global storage. See bug https://github.com/microsoft/vscode-python/issues/9159
+    private _value: Record<string, {}> = {};
     // @ts-ignore
     // tslint:disable-next-line:no-any
     public get(key: any, defaultValue?: any);
     public get<T>(key: string, defaultValue?: T): T {
-        const exists = this.map.has(key);
+        const exists = this._value.hasOwnProperty(key);
         // tslint:disable-next-line:no-any
-        return exists ? this.map.get(key) : defaultValue! as any;
+        return exists ? this._value[key] : defaultValue! as any;
     }
     // tslint:disable-next-line:no-any
     public update(key: string, value: any): Thenable<void> {
-        this.map.set(key, value);
+        this._value[key] = value;
         return Promise.resolve();
     }
     public clear() {
-        this.map.clear();
+        this._value = {};
     }
 }
