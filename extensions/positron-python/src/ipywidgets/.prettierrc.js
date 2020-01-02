cc992d1f3410b3ff1d6dfa8485f8c7e1563ea570
Don Jayamanne <don.jayamanne@yahoo.com>
2020-01-02 11:14:22 -0600

    Remove ipywidgets (microsoft/vscode-python#9253)

--- extensions/positron-python/src/ipywidgets/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    semi: true,
-    singleQuote: true,
-    printWidth: 180,
-    tabWidth: 4
-};
--- extensions/positron-python/src/ipywidgets/scripts/copyBuild.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-
-fs.copyFileSync(path.resolve(__dirname, '..', 'dist', 'ipywidgets', 'ipywidgets.js'), path.resolve(__dirname, '..', '..', '..', 'out', 'datascience-ui', 'native-editor', 'ipywidgets.js'));
