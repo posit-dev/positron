b434663ebbaab3a662b59c9a90e266d31c077ee4
Don Jayamanne <don.jayamanne@yahoo.com>
2020-01-02 11:14:22 -0600

    Remove ipywidgets (microsoft/vscode-python#9253)

--- extensions/positron-python/src/ipywidgets/scripts/copyfiles.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-'use strict';
-
-var fs = require('fs');
-fs.copyFileSync('src/widgets.css', 'out/widgets.css');
