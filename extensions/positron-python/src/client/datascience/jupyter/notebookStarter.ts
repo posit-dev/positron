d08c63f535ac578cad381de789d2af8ef74ecaf3
Don Jayamanne <don.jayamanne@yahoo.com>
2020-01-08 16:01:12 -0600

    Bypass conda service when creating daemons (microsoft/vscode-python#9454)
    * Bypass conda run for DS code

--- extensions/positron-python/src/client/datascience/jupyter/notebookStarter.ts
+++ extensions/positron-python/src/client/datascience/jupyter/notebookStarter.ts
@@ -241,7 +241,7 @@ export class NotebookStarter implements Disposable {
         const isActiveInterpreter = activeInterpreter ? activeInterpreter.path === interpreter.path : false;
         const daemon = await (isActiveInterpreter
             ? this.executionFactory.createDaemon({ daemonModule: PythonDaemonModule, pythonPath: interpreter.path })
-            : this.executionFactory.createActivatedEnvironment({ allowEnvironmentFetchExceptions: true, interpreter }));
+            : this.executionFactory.createActivatedEnvironment({ allowEnvironmentFetchExceptions: true, interpreter, bypassCondaExecution: true }));
         // We have a small python file here that we will execute to get the server info from all running Jupyter instances
         const newOptions: SpawnOptions = { mergeStdOutErr: true, token: cancelToken };
         const file = path.join(EXTENSION_ROOT_DIR, 'pythonFiles', 'datascience', 'getServerInfo.py');
