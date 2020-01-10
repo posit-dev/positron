2753005f94e5e4aec2ec3c1fc79b16552c01de28
David Kutugata <dakutuga@microsoft.com>
2020-01-09 18:46:13 -0600

    changed some pixel and % measurements to em (microsoft/vscode-python#9451)
    * changed some pixel and % measurements to em
    
    * use linear regression to get a
    dynamic width for the kernel toolbar
    
    * fixed shrinking buttons on the
    native editor toolbar

--- extensions/positron-python/src/datascience-ui/interactive-common/kernelSelection.tsx
+++ extensions/positron-python/src/datascience-ui/interactive-common/kernelSelection.tsx
@@ -28,23 +28,32 @@ export class KernelSelection extends React.Component<IKernelSelectionProps> {
     }
 
     public render() {
+        const serverTextSize = getLocString('DataScience.jupyterServer', 'Jupyter Server').length + this.props.kernel.localizedUri.length + 4; // plus 4 for the icon
+        const displayNameTextSize = this.props.kernel.displayName.length + this.props.kernel.jupyterServerStatus.length - 1;
         const dynamicFont: React.CSSProperties = {
             fontSize: this.props.font.size > 2 ? this.props.font.size - 2 : this.props.font.size,
-            fontFamily: this.props.font.family
+            fontFamily: this.props.font.family,
+            maxWidth: this.getMaxWidth(serverTextSize + displayNameTextSize + 5) // plus 5 for the line and margins
+        };
+        const serverTextWidth: React.CSSProperties = {
+            maxWidth: this.getMaxWidth(serverTextSize)
+        };
+        const displayNameTextWidth: React.CSSProperties = {
+            maxWidth: this.getMaxWidth(displayNameTextSize)
         };
         const kernelSelectionClass = this.isKernelSelectionAllowed
             ? 'kernel-status-section kernel-status-section-hoverable kernel-status-status'
             : 'kernel-status-section kernel-status-status';
         return (
             <div className="kernel-status" style={dynamicFont}>
-                <div className="kernel-status-section kernel-status-server" role="button">
+                <div className="kernel-status-section kernel-status-server" style={serverTextWidth} role="button">
                     <div className="kernel-status-text" title={this.props.kernel.localizedUri}>
                         {getLocString('DataScience.jupyterServer', 'Jupyter Server')}: {this.props.kernel.localizedUri}
                     </div>
                     <Image baseTheme={this.props.baseTheme} class="image-button-image kernel-status-icon" image={this.getIcon()} />
                 </div>
                 <div className="kernel-status-divider" />
-                <div className={kernelSelectionClass} onClick={this.selectKernel} role="button">
+                <div className={kernelSelectionClass} style={displayNameTextWidth} onClick={this.selectKernel} role="button">
                     {this.props.kernel.displayName}: {this.props.kernel.jupyterServerStatus}
                 </div>
             </div>
@@ -57,5 +66,12 @@ export class KernelSelection extends React.Component<IKernelSelectionProps> {
     }
     private getIcon(): ImageName {
         return this.props.kernel.localizedUri === getLocString('DataScience.noKernel', 'No Kernel') ? ImageName.JupyterServerDisconnected : ImageName.JupyterServerConnected;
+    }
+
+    private getMaxWidth(charLenght: number): string {
+        // This comes from a linear regression
+        const width = 0.57674 * charLenght + 1.70473;
+        const unit = 'em';
+        return Math.round(width).toString() + unit;
     }
 }
