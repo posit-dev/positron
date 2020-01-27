b23d5c9bcaf9c5c3852d465c57a5c1ea3176ff09
David Kutugata <dakutuga@microsoft.com>
2020-01-27 17:53:31 -0600

    Test performance pipeline (microsoft/vscode-python#9729)
    * Start of a performance test pipeline.
    Still missing:
    - use grep on the pipeline to only test a specific test
    - add a way to fail or pass the test
    
    * changed ts files to js to avoid compiling.
    added a opts file to only run some test suites
    
    * - removed unnecessary files
    - made the jobs run in parallel
    - made them to execute on saturdays at 00:00
    
    * made the jobs run sequentially

---
+++ extensions/positron-python/build/ci/performance/savePerformanceResults.js
@@ -0,0 +1,100 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+'use strict';
+const fastXmlParser = require('fast-xml-parser');
+const fs = require('fs');
+const path = require('path');
+const constants = require('../../constants');
+
+const xmlFile = path.join(constants.ExtensionRootDir, 'test-results.xml');
+const jsonFile = path.join(constants.ExtensionRootDir, 'build', 'ci', 'performance', 'performance-results.json');
+let performanceData = [];
+
+fs.readFile(xmlFile, 'utf8', (xmlReadError, xmlData) => {
+    if (xmlReadError) {
+        throw xmlReadError;
+    }
+
+    if (fastXmlParser.validate(xmlData)) {
+        const defaultOptions = {
+            attributeNamePrefix: '',
+            ignoreAttributes: false
+        };
+        const jsonObj = fastXmlParser.parse(xmlData, defaultOptions);
+
+        fs.readFile(jsonFile, 'utf8', (jsonReadError, data) => {
+            if (jsonReadError) {
+                // File doesn't exist, so we create it
+                jsonObj.testsuites.testsuite.forEach(suite => {
+                    if (parseInt(suite.tests, 10) > 0) {
+                        if (Array.isArray(suite.testcase)) {
+                            suite.testcase.forEach(testcase => {
+                                const test = {
+                                    name: testcase.name,
+                                    times: [parseFloat(testcase.time)]
+                                };
+                                performanceData.push(test);
+                            });
+                        } else {
+                            const test = {
+                                name: suite.testcase.name,
+                                times: [parseFloat(suite.testcase.time)]
+                            };
+                            performanceData.push(test);
+                        }
+                    }
+                });
+            } else {
+                performanceData = JSON.parse(data);
+
+                jsonObj.testsuites.testsuite.forEach(suite => {
+                    if (parseInt(suite.tests, 10) > 0) {
+                        if (Array.isArray(suite.testcase)) {
+                            suite.testcase.forEach(testcase => {
+                                let test = performanceData.find(x => x.name === testcase.name);
+                                if (test) {
+                                    // if the test name is already there, we add the new time
+                                    test.times.push(parseFloat(testcase.time));
+                                } else {
+                                    // if its not there, we add the whole thing
+                                    const test = {
+                                        name: testcase.name,
+                                        times: [parseFloat(testcase.time)]
+                                    };
+
+                                    performanceData.push(test);
+                                }
+                            });
+                        } else {
+                            let test = performanceData.find(x => x.name === suite.testcase.name);
+                            if (test) {
+                                // if the test name is already there, we add the new time
+                                test.times.push(parseFloat(suite.testcase.time));
+                            } else {
+                                // if its not there, we add the whole thing
+                                test = {
+                                    name: suite.testcase.name,
+                                    times: [parseFloat(suite.testcase.time)]
+                                };
+
+                                performanceData.push(test);
+                            }
+                        }
+                    }
+                });
+            }
+
+            fs.writeFile(
+                path.join(constants.ExtensionRootDir, 'build', 'ci', 'performance', 'performance-results.json'),
+                JSON.stringify(performanceData, null, 2),
+                writeResultsError => {
+                    if (writeResultsError) {
+                        throw writeResultsError;
+                    }
+                    // tslint:disable-next-line: no-console
+                    console.log('performance-results.json was saved!');
+                }
+            );
+        });
+    }
+});
