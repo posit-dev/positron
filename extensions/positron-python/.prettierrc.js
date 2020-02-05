0233edd7c7f0248ff36ce104c19a4706d4991c54
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2020-02-05 00:02:37 -0600

    Start running CI using a GitHub Action: build, compile, hygiene, check deps (microsoft/vscode-python#9843)
    * Try caching repo in CI workflow
    * Cache all the things
    * Compare checkout vs cache
    * Remove repo caching job
    * Enable compile + check deps jobs
    * Changing the tab width for yaml files to 2
    * Quotes
    * Add hygiene + build VSIX jobs
    * The woes of not renaming all occurrences
    * Rename artifact + package pythonFiles too
    * Clean directory before packaging the vsix
    * Fix pythonFiles/ retrieval
    * Run CI on push on master
    * Be consistent with my # use
    * Remove tool check
    * Move cache pip files one step up
    * Try different reqs file pattern matching
    * Change branch config for testing
    * Add --prefer-offline flag when running npm ci
    * Slightly more specific python cache keys
    * quotes
    * Quotes, again
    * Quotes, third time's the charm
    * One big hygiene job instead of multiple small ones
    * curl ptvsd versions (pip cache key uniqueness)
    * Save curl hash in output
    * Undo hash output + copy curl

--- extensions/positron-python/.prettierrc.js
+++ extensions/positron-python/.prettierrc.js
@@ -1,5 +1,13 @@
 module.exports = {
     singleQuote: true,
     printWidth: 180,
-    tabWidth: 4
+    tabWidth: 4,
+    overrides: [
+        {
+            files: ['*.yml', '*.yaml'],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
 };
