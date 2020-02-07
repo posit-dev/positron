89ca41ac78be00d82a3dd0265aff166ce73fd3f6
David Kutugata <dakutuga@microsoft.com>
2020-02-07 17:36:52 -0600

    Fix latex manipulation case with multiple formulas inside a single '$$' block (microsoft/vscode-python#9971)
    * Changed the latex manipulation to only fix
    single formulas.
    
    * added news file
    
    * Fixed an issue with multiple latex formulas in the
    same '$$' block.
    
    * reverted changes on old tests

--- extensions/positron-python/src/test/datascience/latexManipulation.unit.test.ts
+++ extensions/positron-python/src/test/datascience/latexManipulation.unit.test.ts
@@ -89,9 +89,9 @@ $$
 $$
 \\begin{equation*}
 \\mathbf{V}_1 \\times \\mathbf{V}_2 = \\begin{vmatrix}
-\\mathbf{i} & \\mathbf{j} & \\mathbf{k} \\
-\\frac{\partial X}{\\partial u} & \\frac{\\partial Y}{\\partial u} & 0 \\\\
-\\frac{\partial X}{\\partial v} & \\frac{\\partial Y}{\\partial v} & 0
+\\mathbf{i} & \\mathbf{j} & \\mathbf{k} \\\\
+\\frac{\\partial X}{\\partial u} & \\frac{\\partial Y}{\\partial u} & 0 \\\\
+\\frac{\\partial X}{\\partial v} & \\frac{\\partial Y}{\\partial v} & 0
 \\end{vmatrix}
 \\end{equation*}
 $$
@@ -115,6 +115,42 @@ $$
 
 This expression $\\sqrt{3x-1}+(1+x)^2$ is an example of a TeX inline equation in a [Markdown-formatted](https://daringfireball.net/projects/markdown/) sentence.
 `;
+
+    const markdown6 = `$$
+\\begin{aligned}
+\\frac{\\partial}{\\partial\\omega_j}C(\\omega) &= \\frac1m\\sum_{i=1}^m\\varphi_j\\left(x^i\\right)\\left(\\varphi^T\\left(x^i\\right)\\omega-t^i\\right)
+= 0
+\\end{aligned}
+$$
+$$
+\\begin{pmatrix}
+\\varphi_j\\left(x^1\\right) & \\dots & \\varphi_j\\left(x^m\\right)
+\\end{pmatrix}
+\\begin{pmatrix}
+\\varphi_1\\left(x^1\\right) & \\dots & \\varphi_n\\left(x^1\\right)\\\\
+\\vdots & \\ddots & \\vdots\\\\
+\\varphi_1\\left(x^m\\right) & \\dots & \\varphi_n\\left(x^m\\right)
+\\end{pmatrix}
+\\begin{pmatrix}
+\\omega_1\\\\
+\\vdots\\\\
+\\omega_n
+\\end{pmatrix}
+=
+\\begin{pmatrix}
+\\varphi_j\\left(x^1\\right) & \\dots & \\varphi_j\\left(x^m\\right)
+\\end{pmatrix}
+\\begin{pmatrix}
+t^1\\\\
+\\vdots\\\\
+t^m
+\\end{pmatrix}
+$$
+
+Assuming that $T = (t^1, t^2, ..., t^m)^T$，$X = \\left(\\varphi(x^1), \\varphi(x^2), ..., \\varphi(x^m)\\right)^T$, then
+$$
+X^TX\\omega = X^TT
+$$`;
 
     test("Latex - Equations don't have $$", () => {
         const result = fixLatexEquations(markdown1);
@@ -152,5 +188,10 @@ This expression $\\sqrt{3x-1}+(1+x)^2$ is an example of a TeX inline equation in
     test('Latex - Multiple types', () => {
         const result = fixLatexEquations(markdown5);
         expect(result).to.be.equal(output5, 'Result is incorrect');
+    });
+
+    test('Latex - Multiple /begins inside $$', () => {
+        const result = fixLatexEquations(markdown6);
+        expect(result).to.be.equal(markdown6, 'Result should not have changed');
     });
 });
