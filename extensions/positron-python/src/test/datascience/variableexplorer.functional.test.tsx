2fc1dcde9216e08121eb9ddc68d673eda3950699
Rich Chiodo <rchiodo@users.noreply.github.com>
2020-02-12 16:07:13 -0600

    Fix ndarray types to be viewable again (microsoft/vscode-python#10093)
    * Eliminate variable value when computing data frame info
    
    * Fix ndarrays to work again
    Add test to make sure we don't regress this again

--- extensions/positron-python/src/test/datascience/variableexplorer.functional.test.tsx
+++ extensions/positron-python/src/test/datascience/variableexplorer.functional.test.tsx
@@ -539,21 +539,36 @@ function verifyRow(rowWrapper: ReactWrapper<any, Readonly<{}>, React.Component>,
     if (targetVariable.value) {
         verifyCell(rowCells.at(3), targetVariable.value, targetVariable.name);
     }
+
+    verifyCell(rowCells.at(4), targetVariable.supportsDataExplorer, targetVariable.name);
 }
 
 // Verify a single cell value against a specific target value
-function verifyCell(cellWrapper: ReactWrapper<any, Readonly<{}>, React.Component>, value: string, targetName: string) {
+function verifyCell(
+    cellWrapper: ReactWrapper<any, Readonly<{}>, React.Component>,
+    value: string | boolean,
+    targetName: string
+) {
     const cellHTML = parse(cellWrapper.html()) as any;
-    // tslint:disable-next-line:no-string-literal
-    const match = /value="([\s\S]+?)"\s+/.exec(cellHTML.innerHTML);
-    expect(match).to.not.be.equal(null, `${targetName} does not have a value attribute`);
+    const innerHTML = cellHTML.innerHTML;
+    if (typeof value === 'string') {
+        // tslint:disable-next-line:no-string-literal
+        const match = /value="([\s\S]+?)"\s+/.exec(innerHTML);
+        expect(match).to.not.be.equal(null, `${targetName} does not have a value attribute`);
 
-    // Eliminate whitespace differences
-    const actualValueNormalized = match![1].replace(/^\s*|\s(?=\s)|\s*$/g, '').replace(/\r\n/g, '\n');
-    const expectedValueNormalized = value.replace(/^\s*|\s(?=\s)|\s*$/g, '').replace(/\r\n/g, '\n');
+        // Eliminate whitespace differences
+        const actualValueNormalized = match![1].replace(/^\s*|\s(?=\s)|\s*$/g, '').replace(/\r\n/g, '\n');
+        const expectedValueNormalized = value.replace(/^\s*|\s(?=\s)|\s*$/g, '').replace(/\r\n/g, '\n');
 
-    expect(actualValueNormalized).to.be.equal(
-        expectedValueNormalized,
-        `${targetName} has an unexpected value ${cellHTML.innerHTML} in variable explorer cell`
-    );
+        expect(actualValueNormalized).to.be.equal(
+            expectedValueNormalized,
+            `${targetName} has an unexpected value ${innerHTML} in variable explorer cell`
+        );
+    } else {
+        if (value) {
+            expect(innerHTML).to.include('image-button-image', `Image class not found in ${targetName}`);
+        } else {
+            expect(innerHTML).to.not.include('image-button-image', `Image class was found ${targetName}`);
+        }
+    }
 }
