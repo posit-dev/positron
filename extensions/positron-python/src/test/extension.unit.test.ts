2deb0e3ee4bdfdbde1c595c9a66ec0a03d73ec71
Don Jayamanne <don.jayamanne@yahoo.com>
2020-02-12 13:58:31 -0600

    Add tests to ensure localization files can be loaded (microsoft/vscode-python#10077)
    For microsoft/vscode-python#10073

--- extensions/positron-python/src/test/extension.unit.test.ts
+++ extensions/positron-python/src/test/extension.unit.test.ts
@@ -6,6 +6,8 @@
 // tslint:disable:no-any
 
 import { expect } from 'chai';
+import * as fs from 'fs';
+import * as glob from 'glob';
 import * as path from 'path';
 import { anyString, anything, instance, mock, when } from 'ts-mockito';
 import { buildApi } from '../client/api';
@@ -182,5 +184,20 @@ suite('Extension version tests', () => {
             version.endsWith('-dev'),
             'When running a pipeline in the release branch, the extension version in package.json should not have the -dev suffix'
         ).to.be.false;
+    });
+});
+
+suite('Extension localization files', () => {
+    test('Load localization file', () => {
+        const filesFailed: string[] = [];
+        glob.sync('package.nls.*.json', { sync: true, cwd: EXTENSION_ROOT_DIR }).forEach(localizationFile => {
+            try {
+                JSON.parse(fs.readFileSync(path.join(EXTENSION_ROOT_DIR, localizationFile)).toString());
+            } catch {
+                filesFailed.push(localizationFile);
+            }
+        });
+
+        expect(filesFailed).to.be.lengthOf(0, `Failed to load JSON for ${filesFailed.join(', ')}`);
     });
 });
