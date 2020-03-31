af5f06208ff6197ff3bae775fedf654ccecfcbd2
Rich Chiodo <rchiodo@users.noreply.github.com>
2020-03-31 15:11:48 -0500

    Make switching a kernel dirty a notebook (microsoft/vscode-python#10882)
    * Update notebook metadata on kernel change
    
    * Add news entry

--- extensions/positron-python/src/client/datascience/interactive-common/interactiveBase.ts
+++ extensions/positron-python/src/client/datascience/interactive-common/interactiveBase.ts
@@ -1395,6 +1395,16 @@ export abstract class InteractiveBase extends WebViewHost<IInteractiveWindowMapp
         } else {
             await this.addSysInfo(SysInfoReason.New);
         }
+
+        // Force an update of the kernel metadata
+        const kernelSpec: IJupyterKernelSpec = {
+            path: kernel.path ?? '',
+            name: kernel.name,
+            language: kernel.language ?? 'python',
+            display_name: kernel.display_name ?? kernel.name,
+            argv: kernel.argv ?? []
+        };
+        return this.updateNotebookOptions(kernelSpec, this._notebook?.getMatchingInterpreter());
     }
 
     private openSettings(setting: string | undefined) {
