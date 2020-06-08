027e14514ad08b5f492d0c60d8da50d316e2d251
Don Jayamanne <don.jayamanne@yahoo.com>
2020-06-08 14:20:17 -0500

    Disable sort imports in notebook cells (microsoft/vscode-python#12194)
    For microsoft/vscode-python#12193

--- extensions/positron-python/src/client/providers/codeActionProvider/pythonCodeActionProvider.ts
+++ extensions/positron-python/src/client/providers/codeActionProvider/pythonCodeActionProvider.ts
@@ -4,14 +4,18 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { isNotebookCell } from '../../common/utils/misc';
 
 export class PythonCodeActionProvider implements vscode.CodeActionProvider {
     public provideCodeActions(
-        _document: vscode.TextDocument,
+        document: vscode.TextDocument,
         _range: vscode.Range,
         _context: vscode.CodeActionContext,
         _token: vscode.CancellationToken
     ): vscode.ProviderResult<vscode.CodeAction[]> {
+        if (isNotebookCell(document)) {
+            return [];
+        }
         const sortImports = new vscode.CodeAction('Sort imports', vscode.CodeActionKind.SourceOrganizeImports);
         sortImports.command = {
             title: 'Sort imports',
