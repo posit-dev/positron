6f2c0f8eaaef19e163a06f54e6efe85576ea32c4
Hugues Valois <huvalo@microsoft.com>
2020-06-17 17:30:39 -0500

    Do not trigger unnecessary work in language server when formatting a document (microsoft/vscode-python#12097)
    * Change getTempFileWithDocumentContents to not use the document's original file extension.
    Instead, use the .tmp extension which is not associated with Python language. This prevents a file add/delete event from reaching language server, invalidating some of its cache and causing unnecessary work.
    
    * Fix build.
    
    Co-authored-by: Don Jayamanne <don.jayamanne@yahoo.com>

--- extensions/positron-python/src/client/common/editor.ts
+++ extensions/positron-python/src/client/common/editor.ts
@@ -248,20 +248,21 @@ function getTextEditsInternal(before: string, diffs: [number, string][], startLi
 }
 
 export async function getTempFileWithDocumentContents(document: TextDocument, fs: IFileSystem): Promise<string> {
-    // Hardcode extension to `.py` for notebook cells (do not use `.ipyn` doesn't work).
-    const ext = isNotebookCell(document.uri) ? '.py' : path.extname(document.uri.fsPath);
     // Don't create file in temp folder since external utilities
     // look into configuration files in the workspace and are not
     // to find custom rules if file is saved in a random disk location.
     // This means temp file has to be created in the same folder
     // as the original one and then removed.
+    // Use a .tmp file extension (instead of the original extension)
+    // because the language server is watching the file system for Python
+    // file add/delete/change and we don't want this temp file to trigger it.
 
-    let fileName = `${document.uri.fsPath}.${md5(document.uri.fsPath)}${ext}`;
-
+    // tslint:disable-next-line:no-require-imports
+    let fileName = `${document.uri.fsPath}.${md5(document.uri.fsPath)}.tmp`;
     try {
         // When dealing with untitled notebooks, there's no original physical file, hence create a temp file.
         if (isNotebookCell(document.uri) && !(await fs.fileExists(document.uri.fsPath))) {
-            fileName = (await fs.createTemporaryFile(`${path.basename(document.uri.fsPath)}${ext}`)).filePath;
+            fileName = (await fs.createTemporaryFile(`${path.basename(document.uri.fsPath)}.tmp`)).filePath;
         }
         await fs.writeFile(fileName, document.getText());
     } catch (ex) {
