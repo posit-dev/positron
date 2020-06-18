a88504560d47356111e21798b818c1be7f9a1116
Don Jayamanne <don.jayamanne@yahoo.com>
2020-06-17 19:12:42 -0500

    Disable use of proposedApi in stable VS Code (microsoft/vscode-python#12411)
    * Fixes to using Native NB with stable VS Code
    
    * Fixes

--- extensions/positron-python/src/client/datascience/notebook/integration.ts
+++ extensions/positron-python/src/client/datascience/notebook/integration.ts
@@ -33,25 +33,29 @@ export class NotebookIntegration implements IExtensionSingleActivationService {
         @inject(NotebookKernel) private readonly notebookKernel: NotebookKernel,
         @inject(NotebookOutputRenderer) private readonly renderer: NotebookOutputRenderer
     ) {}
+    public get isEnabled() {
+        const packageJsonFile = path.join(this.context.extensionPath, 'package.json');
+        const content = JSON.parse(this.fs.readFileSync(packageJsonFile));
+
+        // This code is temporary.
+        return (
+            content.enableProposedApi &&
+            Array.isArray(content.contributes.notebookOutputRenderer) &&
+            (content.contributes.notebookOutputRenderer as []).length > 0 &&
+            Array.isArray(content.contributes.notebookProvider) &&
+            (content.contributes.notebookProvider as []).length > 0
+        );
+    }
+    public async enableSideBySideUsage() {
+        await this.enableNotebooks(false);
+    }
     public async activate(): Promise<void> {
         // This condition is temporary.
         // If user belongs to the experiment, then make the necessary changes to package.json.
         // Once the API is final, we won't need to modify the package.json.
         if (this.experiment.inExperiment(NotebookEditorSupport.nativeNotebookExperiment)) {
-            const packageJsonFile = path.join(this.context.extensionPath, 'package.json');
-            const content = JSON.parse(this.fs.readFileSync(packageJsonFile));
-
-            // This code is temporary.
-            if (content.contributes.notebookProvider[0].priority !== 'default') {
-                content.contributes.notebookProvider[0].priority = 'default';
-
-                await this.fs.writeFile(packageJsonFile, JSON.stringify(content, undefined, 4));
-                await this.commandManager
-                    .executeCommand('python.reloadVSCode', DataScience.reloadVSCodeNotebookEditor())
-                    .then(noop, noop);
-            }
+            await this.enableNotebooks(true);
         }
-
         this.disposables.push(
             this.vscNotebook.registerNotebookContentProvider(JupyterNotebookView, this.notebookContentProvider)
         );
@@ -85,5 +89,59 @@ export class NotebookIntegration implements IExtensionSingleActivationService {
                 this.renderer
             )
         );
+    }
+    private async enableNotebooks(useVSCodeNotebookAsDefaultEditor: boolean) {
+        const packageJsonFile = path.join(this.context.extensionPath, 'package.json');
+        const content = JSON.parse(this.fs.readFileSync(packageJsonFile));
+
+        // This code is temporary.
+        if (
+            !content.enableProposedApi ||
+            !Array.isArray(content.contributes.notebookOutputRenderer) ||
+            !Array.isArray(content.contributes.notebookProvider)
+        ) {
+            content.enableProposedApi = true;
+            content.contributes.notebookOutputRenderer = [
+                {
+                    viewType: 'jupyter-notebook-renderer',
+                    displayName: 'Jupyter Notebook Renderer',
+                    mimeTypes: [
+                        'application/geo+json',
+                        'application/vdom.v1+json',
+                        'application/vnd.dataresource+json',
+                        'application/vnd.plotly.v1+json',
+                        'application/vnd.vega.v2+json',
+                        'application/vnd.vega.v3+json',
+                        'application/vnd.vega.v4+json',
+                        'application/vnd.vega.v5+json',
+                        'application/vnd.vegalite.v1+json',
+                        'application/vnd.vegalite.v2+json',
+                        'application/vnd.vegalite.v3+json',
+                        'application/vnd.vegalite.v4+json',
+                        'application/x-nteract-model-debug+json',
+                        'image/gif',
+                        'text/latex',
+                        'text/vnd.plotly.v1+html'
+                    ]
+                }
+            ];
+            content.contributes.notebookProvider = [
+                {
+                    viewType: JupyterNotebookView,
+                    displayName: 'Jupyter Notebook (preview)',
+                    selector: [
+                        {
+                            filenamePattern: '*.ipynb'
+                        }
+                    ],
+                    priority: useVSCodeNotebookAsDefaultEditor ? 'default' : 'option'
+                }
+            ];
+
+            await this.fs.writeFile(packageJsonFile, JSON.stringify(content, undefined, 4));
+            await this.commandManager
+                .executeCommand('python.reloadVSCode', DataScience.reloadVSCodeNotebookEditor())
+                .then(noop, noop);
+        }
     }
 }
