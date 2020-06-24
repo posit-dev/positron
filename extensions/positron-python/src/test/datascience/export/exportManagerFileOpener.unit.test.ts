35502f3b39213421be0b658bbcc656a7f22e2550
Timothy Ruscica <35348871+techwithtim@users.noreply.github.com>
2020-06-24 17:03:15 -0500

    added tests for pdf export (microsoft/vscode-python#12548)

--- extensions/positron-python/src/test/datascience/export/exportManagerFileOpener.unit.test.ts
+++ extensions/positron-python/src/test/datascience/export/exportManagerFileOpener.unit.test.ts
@@ -80,4 +80,32 @@ suite('Data Science - Export File Opener', () => {
 
         verify(browserService.launch(anything())).never();
     });
+    test('Exporting to PDF displays message if operation fails', async () => {
+        when(exporter.export(anything(), anything())).thenThrow(new Error('Export failed...'));
+        when(applicationShell.showErrorMessage(anything())).thenResolve();
+        await fileOpener.export(ExportFormat.pdf, model);
+        verify(applicationShell.showErrorMessage(anything())).once();
+    });
+    test('PDF File opened if yes button pressed', async () => {
+        const uri = Uri.file('test.pdf');
+        when(exporter.export(anything(), anything())).thenResolve(uri);
+        when(applicationShell.showInformationMessage(anything(), anything(), anything())).thenReturn(
+            Promise.resolve(getLocString('DataScience.openExportFileYes', 'Yes'))
+        );
+
+        await fileOpener.export(ExportFormat.pdf, model);
+
+        verify(browserService.launch(anything())).once();
+    });
+    test('PDF File not opened if no button button pressed', async () => {
+        const uri = Uri.file('test.pdf');
+        when(exporter.export(anything(), anything())).thenResolve(uri);
+        when(applicationShell.showInformationMessage(anything(), anything(), anything())).thenReturn(
+            Promise.resolve(getLocString('DataScience.openExportFileNo', 'No'))
+        );
+
+        await fileOpener.export(ExportFormat.pdf, model);
+
+        verify(browserService.launch(anything())).never();
+    });
 });
