6ee4bdb12826263cb768919dcc2e81d06ecfc01b
Eric Snow <eric.snow@microsoft.com>
2020-08-24 17:20:54 -0500

    Add a helper for chaining async iterators. (microsoft/vscode-python#13528)
    This will be used in upcoming locator-related changes.

--- extensions/positron-python/src/client/common/utils/async.ts
+++ extensions/positron-python/src/client/common/utils/async.ts
@@ -110,3 +110,86 @@ export function createDeferredFromPromise<T>(promise: Promise<T>): Deferred<T> {
     promise.then(deferred.resolve.bind(deferred)).catch(deferred.reject.bind(deferred));
     return deferred;
 }
+
+//================================
+// iterators
+
+/**
+ * An iterator that yields nothing.
+ */
+export function iterEmpty<T, R = void>(): AsyncIterator<T, R> {
+    // tslint:disable-next-line:no-empty
+    return ((async function* () {})() as unknown) as AsyncIterator<T, R>;
+}
+
+type NextResult<T, R = void> = { index: number } & (
+    | { result: IteratorResult<T, R>; err: null }
+    | { result: null; err: Error }
+);
+async function getNext<T, R = void>(it: AsyncIterator<T, R>, indexMaybe?: number): Promise<NextResult<T, R>> {
+    const index = indexMaybe === undefined ? -1 : indexMaybe;
+    try {
+        const result = await it.next();
+        return { index, result, err: null };
+    } catch (err) {
+        return { index, err, result: null };
+    }
+}
+
+// tslint:disable-next-line:promise-must-complete no-empty
+const NEVER: Promise<unknown> = new Promise(() => {});
+
+/**
+ * Yield everything produced by the given iterators as soon as each is ready.
+ *
+ * When one of the iterators has something to yield then it gets yielded
+ * right away, regardless of where the iterator is located in the array
+ * of iterators.
+ *
+ * @param iterators - the async iterators from which to yield items
+ * @param onError - called/awaited once for each iterator that fails
+ */
+export async function* chain<T, R = void>(
+    iterators: AsyncIterator<T | void, R>[],
+    onError?: (err: Error, index: number) => Promise<void>
+    // Ultimately we may also want to support cancellation.
+): AsyncIterator<T | R | void, void> {
+    const promises = iterators.map(getNext);
+    let numRunning = iterators.length;
+    while (numRunning > 0) {
+        const { index, result, err } = await Promise.race(promises);
+        if (err !== null) {
+            promises[index] = NEVER as Promise<NextResult<T, R>>;
+            numRunning -= 1;
+            if (onError !== undefined) {
+                await onError(err, index);
+            }
+            // XXX Log the error.
+        } else if (result!.done) {
+            promises[index] = NEVER as Promise<NextResult<T, R>>;
+            numRunning -= 1;
+            // If R is void then result.value will be undefined.
+            if (result!.value !== undefined) {
+                yield result!.value;
+            }
+        } else {
+            promises[index] = getNext(iterators[index], index);
+            yield result!.value;
+        }
+    }
+}
+
+/**
+ * Get everything yielded by the iterator.
+ */
+export async function flattenIterator<T>(iterator: AsyncIterator<T, void>): Promise<T[]> {
+    const results: T[] = [];
+    // We are dealing with an iterator, not an iterable, so we have
+    // to iterate manually rather than with a for-await loop.
+    let result = await iterator.next();
+    while (!result.done) {
+        results.push(result.value);
+        result = await iterator.next();
+    }
+    return results;
+}
