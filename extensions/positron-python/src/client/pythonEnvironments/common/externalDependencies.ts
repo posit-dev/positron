03eb17e100886ce5a6da9cfe496d00060f98f02c
Karthik Nadig <kanadig@microsoft.com>
2020-09-10 13:28:24 -0500

    Venv identifier (microsoft/vscode-python#13844)
    * Venv identifier
    
    * Add unit tests
    
    * Test tweaks
    
    * More tests
    
    * Fix linting errors
    
    * Address comments..

--- extensions/positron-python/src/client/pythonEnvironments/common/externalDependencies.ts
+++ extensions/positron-python/src/client/pythonEnvironments/common/externalDependencies.ts
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import * as fsapi from 'fs-extra';
 import { ExecutionResult, IProcessServiceFactory } from '../../common/process/types';
+import { createDeferred } from '../../common/utils/async';
 import { IServiceContainer } from '../../ioc/types';
 
 let internalServiceContainer: IServiceContainer;
@@ -16,4 +18,12 @@ function getProcessFactory(): IProcessServiceFactory {
 export async function shellExecute(command: string, timeout: number): Promise<ExecutionResult<string>> {
     const proc = await getProcessFactory().create();
     return proc.shellExec(command, { timeout });
+}
+
+export function pathExists(absPath: string): Promise<boolean> {
+    const deferred = createDeferred<boolean>();
+    fsapi.exists(absPath, (result) => {
+        deferred.resolve(result);
+    });
+    return deferred.promise;
 }
