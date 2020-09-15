244b4830c415d722181de30ff43caca59f881cbb
Kartik Raj <karraj@microsoft.com>
2020-09-15 15:12:31 -0500

    Add API to identify pipenv (microsoft/vscode-python#13762)
    * Add helpers for pipenv
    
    * Add error message
    
    * oOpS
    
    * Reverting regex approach

--- extensions/positron-python/src/client/pythonEnvironments/common/externalDependencies.ts
+++ extensions/positron-python/src/client/pythonEnvironments/common/externalDependencies.ts
@@ -2,8 +2,9 @@
 // Licensed under the MIT License.
 
 import * as fsapi from 'fs-extra';
+import * as path from 'path';
 import { ExecutionResult, IProcessServiceFactory } from '../../common/process/types';
-import { createDeferred } from '../../common/utils/async';
+import { getOSType, OSType } from '../../common/utils/platform';
 import { IServiceContainer } from '../../ioc/types';
 
 let internalServiceContainer: IServiceContainer;
@@ -21,9 +22,18 @@ export async function shellExecute(command: string, timeout: number): Promise<Ex
 }
 
 export function pathExists(absPath: string): Promise<boolean> {
-    const deferred = createDeferred<boolean>();
-    fsapi.exists(absPath, (result) => {
-        deferred.resolve(result);
-    });
-    return deferred.promise;
+    return fsapi.pathExists(absPath);
+}
+
+export function readFile(filePath: string): Promise<string> {
+    return fsapi.readFile(filePath, 'utf-8');
+}
+
+export function arePathsSame(path1: string, path2: string): boolean {
+    path1 = path.normalize(path1);
+    path2 = path.normalize(path2);
+    if (getOSType() === OSType.Windows) {
+        return path1.toUpperCase() === path2.toUpperCase();
+    }
+    return path1 === path2;
 }
