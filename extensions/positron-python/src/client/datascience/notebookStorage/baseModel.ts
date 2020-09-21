7e30e5b9f551e7cfec166f5f0e97f587a613907b
Don Jayamanne <don.jayamanne@yahoo.com>
2020-09-21 11:50:25 -0500

    Find interpreter based on hash in kernelspec of nb metadata (microsoft/vscode-python#13856)

--- extensions/positron-python/src/client/datascience/notebookStorage/baseModel.ts
+++ extensions/positron-python/src/client/datascience/notebookStorage/baseModel.ts
@@ -23,6 +23,19 @@ type KernelIdListEntry = {
     kernelId: string | undefined;
 };
 
+export function getInterpreterInfoStoredInMetadata(
+    metadata?: nbformat.INotebookMetadata
+): { displayName: string; hash: string } | undefined {
+    if (!metadata || !metadata.kernelspec || !metadata.kernelspec.name) {
+        return;
+    }
+    // See `updateNotebookMetadata` to determine how & where exactly interpreter hash is stored.
+    // tslint:disable-next-line: no-any
+    const kernelSpecMetadata: undefined | any = metadata.kernelspec.metadata as any;
+    const interpreterHash = kernelSpecMetadata?.interpreter?.hash;
+    return interpreterHash ? { displayName: metadata.kernelspec.name, hash: interpreterHash } : undefined;
+}
+
 // tslint:disable-next-line: cyclomatic-complexity
 export function updateNotebookMetadata(
     metadata?: nbformat.INotebookMetadata,
@@ -80,7 +93,7 @@ export function updateNotebookMetadata(
         }
     } else if (kernelConnection?.kind === 'startUsingPythonInterpreter') {
         // Store interpreter name, we expect the kernel finder will find the corresponding interpreter based on this name.
-        const name = kernelConnection.interpreter.displayName || kernelConnection.interpreter.path;
+        const name = kernelConnection.interpreter.displayName || '';
         if (metadata.kernelspec?.name !== name || metadata.kernelspec?.display_name !== name) {
             changed = true;
             metadata.kernelspec = {
