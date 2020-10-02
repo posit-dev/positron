56254f44a1ba44b4ba2573edcb42d7b5b9773a3d
Ian Huff <ianhu@microsoft.com>
2020-10-02 13:07:39 -0500

    Fix export from interactive (microsoft/vscode-python#14224)

--- extensions/positron-python/src/client/datascience/interactive-window/interactiveWindow.ts
+++ extensions/positron-python/src/client/datascience/interactive-window/interactiveWindow.ts
@@ -256,7 +256,7 @@ export class InteractiveWindow extends InteractiveBase implements IInteractiveWi
                 break;
 
             case InteractiveWindowMessages.ExportNotebookAs:
-                this.handleMessage(message, payload, this.exportAs.bind);
+                this.handleMessage(message, payload, this.exportAs);
                 break;
 
             case InteractiveWindowMessages.HasCellResponse:
