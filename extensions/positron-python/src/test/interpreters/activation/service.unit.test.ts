221dc19c880ae148702a24a91abf43f778ced0b3
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2020-10-08 14:45:46 -0500

    Fix unit tests failing because of spaces in the Insiders workflow (microsoft/vscode-python#14306)
    * Add .fileToCommandArgument()
    * Add custom workflow to test with spaces
    * Use toCommandArgument instead of fileToCommandArgument
    * Fix upload coverage step
    * $$$$$
    * Delete test workflow
    * Update upload coverage step

--- extensions/positron-python/src/test/interpreters/activation/service.unit.test.ts
+++ extensions/positron-python/src/test/interpreters/activation/service.unit.test.ts
@@ -146,7 +146,7 @@ suite('Interpreters Activation - Python Environment Variables', () => {
                             const expectedCommand = [
                                 ...cmd,
                                 `echo '${getEnvironmentPrefix}'`,
-                                `python ${isolated} ${printEnvPyFile.fileToCommandArgument()}`
+                                `python ${isolated.toCommandArgument()} ${printEnvPyFile.fileToCommandArgument()}`
                             ].join(' && ');
 
                             expect(shellCmd).to.equal(expectedCommand);
