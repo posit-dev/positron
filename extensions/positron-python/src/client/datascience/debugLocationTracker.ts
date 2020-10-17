d33eeac622fbdde30b156f2493d54e52fb0c37c9
Joyce Er <joyceerhuiling@gmail.com>
2020-10-17 02:08:42 -0500

    Ensure debug adapter request/response sequence numbers tally (microsoft/vscode-python#14336)

--- extensions/positron-python/src/client/datascience/debugLocationTracker.ts
+++ extensions/positron-python/src/client/datascience/debugLocationTracker.ts
@@ -1,19 +1,23 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 'use strict';
+import { injectable } from 'inversify';
 import { DebugAdapterTracker, Event, EventEmitter } from 'vscode';
 import { DebugProtocol } from 'vscode-debugprotocol';
 
 import { IDebugLocation } from './types';
 
 // When a python debugging session is active keep track of the current debug location
+@injectable()
 export class DebugLocationTracker implements DebugAdapterTracker {
-    private waitingForStackTrace: boolean = false;
+    protected topMostFrameId = 0;
+    protected sequenceNumbersOfRequestsPendingResponses = new Set<number>();
+    private waitingForStackTrace = false;
     private _debugLocation: IDebugLocation | undefined;
     private debugLocationUpdatedEvent: EventEmitter<void> = new EventEmitter<void>();
     private sessionEndedEmitter: EventEmitter<DebugLocationTracker> = new EventEmitter<DebugLocationTracker>();
 
-    constructor(private _sessionId: string) {
+    constructor(private _sessionId: string | undefined) {
         this.DebugLocation = undefined;
     }
 
@@ -33,8 +37,22 @@ export class DebugLocationTracker implements DebugAdapterTracker {
         return this._debugLocation;
     }
 
-    // tslint:disable-next-line:no-any
-    public onDidSendMessage(message: DebugProtocol.ProtocolMessage) {
+    public onDidSendMessage(message: DebugProtocol.Response) {
+        if (this.isResponseForRequestToFetchAllFrames(message)) {
+            // This should be the top frame. We need to use this to compute the value of a variable
+            const topMostFrame = message.body.stackFrames[0];
+            this.topMostFrameId = topMostFrame?.id;
+            this.sequenceNumbersOfRequestsPendingResponses.delete(message.request_seq);
+            // If we are waiting for a stack trace, check our messages for one
+            if (this.waitingForStackTrace) {
+                this.DebugLocation = {
+                    lineNumber: topMostFrame?.line,
+                    fileName: this.normalizeFilePath(topMostFrame?.source?.path),
+                    column: topMostFrame.column
+                };
+                this.waitingForStackTrace = false;
+            }
+        }
         if (this.isStopEvent(message)) {
             // Some type of stop, wait to see our next stack trace to find our location
             this.waitingForStackTrace = true;
@@ -45,19 +63,21 @@ export class DebugLocationTracker implements DebugAdapterTracker {
             this.DebugLocation = undefined;
             this.waitingForStackTrace = false;
         }
-
-        if (this.waitingForStackTrace) {
-            // If we are waiting for a stack track, check our messages for one
-            const debugLoc = this.getStackTrace(message);
-            if (debugLoc) {
-                this.DebugLocation = debugLoc;
-                this.waitingForStackTrace = false;
-            }
-        }
     }
 
     public onWillStopSession() {
         this.sessionEndedEmitter.fire(this);
+    }
+
+    public onWillReceiveMessage(message: DebugProtocol.Request) {
+        if (this.isRequestToFetchAllFrames(message)) {
+            // VSCode sometimes sends multiple stackTrace requests. The true topmost frame is determined
+            // based on the response to a stackTrace request where the startFrame is 0 or undefined (i.e.
+            // this request retrieves all frames). Here, remember the sequence number of the outgoing
+            // request whose startFrame === 0 or undefined, and update this.topMostFrameId only when we
+            // receive the response with a matching sequence number.
+            this.sequenceNumbersOfRequestsPendingResponses.add(message.seq);
+        }
     }
 
     // Set our new location and fire our debug event
@@ -70,7 +90,15 @@ export class DebugLocationTracker implements DebugAdapterTracker {
         }
     }
 
-    // tslint:disable-next-line:no-any
+    private normalizeFilePath(path: string): string {
+        // Make the path match the os. Debugger seems to return
+        // invalid path chars on linux/darwin
+        if (process.platform !== 'win32') {
+            return path.replace(/\\/g, '/');
+        }
+        return path;
+    }
+
     private isStopEvent(message: DebugProtocol.ProtocolMessage) {
         if (message.type === 'event') {
             const eventMessage = message as DebugProtocol.Event;
@@ -82,34 +110,6 @@ export class DebugLocationTracker implements DebugAdapterTracker {
         return false;
     }
 
-    // tslint:disable-next-line:no-any
-    private getStackTrace(message: DebugProtocol.ProtocolMessage): IDebugLocation | undefined {
-        if (message.type === 'response') {
-            const responseMessage = message as DebugProtocol.Response;
-            if (responseMessage.command === 'stackTrace') {
-                const messageBody = responseMessage.body;
-                if (messageBody.stackFrames.length > 0) {
-                    const lineNumber = messageBody.stackFrames[0].line;
-                    const fileName = this.normalizeFilePath(messageBody.stackFrames[0].source.path);
-                    const column = messageBody.stackFrames[0].column;
-                    return { lineNumber, fileName, column };
-                }
-            }
-        }
-
-        return undefined;
-    }
-
-    private normalizeFilePath(path: string): string {
-        // Make the path match the os. Debugger seems to return
-        // invalid path chars on linux/darwin
-        if (process.platform !== 'win32') {
-            return path.replace(/\\/g, '/');
-        }
-        return path;
-    }
-
-    // tslint:disable-next-line:no-any
     private isContinueEvent(message: DebugProtocol.ProtocolMessage): boolean {
         if (message.type === 'event') {
             const eventMessage = message as DebugProtocol.Event;
@@ -124,5 +124,22 @@ export class DebugLocationTracker implements DebugAdapterTracker {
         }
 
         return false;
+    }
+
+    private isResponseForRequestToFetchAllFrames(message: DebugProtocol.Response) {
+        return (
+            message.type === 'response' &&
+            message.command === 'stackTrace' &&
+            message.body.stackFrames[0] &&
+            this.sequenceNumbersOfRequestsPendingResponses.has(message.request_seq)
+        );
+    }
+
+    private isRequestToFetchAllFrames(message: DebugProtocol.Request) {
+        return (
+            message.type === 'request' &&
+            message.command === 'stackTrace' &&
+            (message.arguments.startFrame === 0 || message.arguments.startFrame === undefined)
+        );
     }
 }
