93431d1da3305bae04e163e120dd01916aa3971c
Kartik Raj <karraj@microsoft.com>
2020-12-10 17:12:04 -0600

    Do not call resolveEnv in component adapter where it's too expensive. (microsoft/vscode-python#14923)
    * Do not call resolveEnv in component adapter where it's not needed
    
    * Fix imports
    
    * Added the OS check
    
    * Apply suggestions from code review
    
    Co-authored-by: Eric Snow <ericsnowcurrently@gmail.com>
    
    * Code reviews
    
    Co-authored-by: Eric Snow <ericsnowcurrently@gmail.com>

---
+++ extensions/positron-python/src/client/pythonEnvironments/base/locators/lowLevel/macDefaultLocator.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { getOSType, OSType } from '../../../../common/utils/platform';
+
+// tslint:disable-next-line:no-suspicious-comment
+// TODO: Add tests for 'isMacDefaultPythonPath' when working on the locator
+
+/**
+ * Decide if the given Python executable looks like the MacOS default Python.
+ */
+export function isMacDefaultPythonPath(pythonPath: string): boolean {
+    if (getOSType() !== OSType.OSX) {
+        return false;
+    }
+    return pythonPath === 'python' || pythonPath === '/usr/bin/python';
+}
