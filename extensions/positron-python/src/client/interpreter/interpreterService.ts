1b0497276180fd3141d5e19801bc7ff4d447d7e1
Kartik Raj <karraj@microsoft.com>
2020-12-10 16:39:59 -0600

    Set display name for the component if it's not set (microsoft/vscode-python#14919)
    * Set displayname for the component if it's not set
    
    * Code reviews

--- extensions/positron-python/src/client/interpreter/interpreterService.ts
+++ extensions/positron-python/src/client/interpreter/interpreterService.ts
@@ -178,6 +178,10 @@ export class InterpreterService implements Disposable, IInterpreterService {
     public async getInterpreterDetails(pythonPath: string, resource?: Uri): Promise<PythonEnvironment | undefined> {
         const info = await this.pyenvs.getInterpreterDetails(pythonPath);
         if (info !== undefined) {
+            if (!info.displayName) {
+                // Set display name for the environment returned by component if it's not set (this should eventually go away)
+                info.displayName = await this.getDisplayName(info, resource);
+            }
             return info;
         }
 
