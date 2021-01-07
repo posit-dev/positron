48291074a0457faf15d56a8f6724cc3f2a6a91b3
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-01-07 17:39:18 -0600

    Use col offset in bandit output to tag the correct node in the AST (microsoft/vscode-python#15003)
    * Use col offset in bandit output
    
    * Add news item
    
    * Format linter input to keep the linter happy
    
    * Update test assertion for bandit call

--- extensions/positron-python/src/client/linters/bandit.ts
+++ extensions/positron-python/src/client/linters/bandit.ts
@@ -24,7 +24,15 @@ export class Bandit extends BaseLinter {
     protected async runLinter(document: TextDocument, cancellation: CancellationToken): Promise<ILintMessage[]> {
         // View all errors in bandit <= 1.5.1 (https://github.com/PyCQA/bandit/issues/371)
         const messages = await this.run(
-            ['-f', 'custom', '--msg-template', '{line},0,{severity},{test_id}:{msg}', '-n', '-1', document.uri.fsPath],
+            [
+                '-f',
+                'custom',
+                '--msg-template',
+                '{line},{col},{severity},{test_id}:{msg}',
+                '-n',
+                '-1',
+                document.uri.fsPath
+            ],
             document,
             cancellation,
         );
