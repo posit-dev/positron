1253069b71976a4f37023c9126646e2901071099
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2021-01-11 16:00:17 -0600

    Partial ESLint fixes in src/test/mocks/ (microsoft/vscode-python#15101)

--- extensions/positron-python/src/test/mocks/moduleInstaller.ts
+++ extensions/positron-python/src/test/mocks/moduleInstaller.ts
@@ -7,16 +7,22 @@ export class MockModuleInstaller extends EventEmitter implements IModuleInstalle
         super();
     }
 
+    // eslint-disable-next-line class-methods-use-this
     public get name(): string {
         return 'mock';
     }
 
+    // eslint-disable-next-line class-methods-use-this
     public get priority(): number {
         return 0;
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public async installModule(name: string, _resource?: Uri): Promise<void> {
         this.emit('installModule', name);
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     public async isSupported(_resource?: Uri): Promise<boolean> {
         return this.supported;
     }
--- extensions/positron-python/src/test/mocks/process.ts
+++ extensions/positron-python/src/test/mocks/process.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 'use strict';
 
 import { injectable } from 'inversify';
@@ -10,19 +11,28 @@ import { EnvironmentVariables } from '../../client/common/variables/types';
 @injectable()
 export class MockProcess implements ICurrentProcess {
     constructor(public env: EnvironmentVariables = { ...process.env }) {}
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/ban-types
     public on(_event: string | symbol, _listener: Function): this {
         return this;
     }
+
+    // eslint-disable-next-line class-methods-use-this
     public get argv(): string[] {
         return [];
     }
+
+    // eslint-disable-next-line class-methods-use-this
     public get stdout(): NodeJS.WriteStream {
         return TypeMoq.Mock.ofType<NodeJS.WriteStream>().object;
     }
+
+    // eslint-disable-next-line class-methods-use-this
     public get stdin(): NodeJS.ReadStream {
         return TypeMoq.Mock.ofType<NodeJS.ReadStream>().object;
     }
 
+    // eslint-disable-next-line class-methods-use-this
     public get execPath(): string {
         return '';
     }
