334bab620637f217108c75a9d9af2989e2cb3a28
Brett Cannon <brett@python.org>
2021-02-01 16:48:45 -0600

    Count preexisting reviews towards the requested reviewer count (microsoft/vscode-python#15270)

--- extensions/positron-python/.github/assign-reviewers/__main__.py
+++ extensions/positron-python/.github/assign-reviewers/__main__.py
@@ -30,25 +30,55 @@ class ConfigData(typing.TypedDict):
     reviewers: list[str]
 
 
+async def already_reviewed(gh: gidgethub.abc.GitHubAPI) -> FrozenSet[str]:
+    """Get the list of people who have already left a review."""
+    event = gidgethub.actions.event()
+    reviews = gh.getiter(
+        "/repos/{owner}/{repo}/pulls/{pull_number}/reviews",
+        url_vars={
+            "owner": event["repository"]["owner"]["login"],
+            "repo": event["repository"]["name"],
+            "pull_number": event["pull_request"]["number"],
+        },
+    )
+    reviewers = set()
+    # GitHub provides the complete history of reviews for a PR in
+    # chronological order.
+    async for review in reviews:
+        reviewer = review["user"]["login"]
+        # A comment is not a review.
+        if review["state"] == "COMMENTED":
+            continue
+        else:
+            reviewers.add(reviewer)
+
+    return frozenset(reviewers)
+
+
 def select_reviewers(
     *,
     author: str,
     available_reviewers: AbstractSet[str],
     assigned_reviewers: AbstractSet[str],
+    already_reviewers: AbstractSet[str],
     count: int,
 ) -> Tuple[FrozenSet[str], FrozenSet[str]]:
     """Select people to review the PR.
 
     If the author is a potential reviewer, remove them from contention. Also
     deduct the number of reviewers necessary based on any that have already
-    been asked to review who are also eligible to review.
+    been asked to review who are also eligible to review or have already
+    reviewed.
 
     """
-    already_reviewing = frozenset(available_reviewers & assigned_reviewers)
+    already_reviewing = frozenset(
+        (available_reviewers & assigned_reviewers)
+        | (available_reviewers & already_reviewers)
+    )
     potential_reviewers = set(available_reviewers)  # Mutable copy.
-    potential_reviewers -= assigned_reviewers
+    potential_reviewers -= already_reviewing
     potential_reviewers.discard(author)
-    count -= len(assigned_reviewers)
+    count -= len(already_reviewing)
     selected_reviewers = []
     while count > 0 and potential_reviewers:
         selected = random.choice(list(potential_reviewers))
@@ -103,18 +133,22 @@ async def main(token: str):
     with config_file.open(encoding="utf-8") as file:
         config: ConfigData = yaml.safe_load(file)
     event = gidgethub.actions.event()
-    team_reviewers, reviewers_to_add = select_reviewers(
-        author=event["pull_request"]["user"]["login"],
-        available_reviewers=frozenset(config["reviewers"]),
-        assigned_reviewers={
-            reviewer["login"]
-            for reviewer in event["pull_request"]["requested_reviewers"]
-        },
-        count=int(config["numberOfReviewers"]),
-    )
+    author = event["pull_request"]["user"]["login"]
+    available_reviewers = frozenset(config["reviewers"])
+    assigned_reviewers = {
+        reviewer["login"] for reviewer in event["pull_request"]["requested_reviewers"]
+    }
     async with httpx.AsyncClient(timeout=None) as client:
         gh = gidgethub.httpx.GitHubAPI(
             client, event["repository"]["full_name"], oauth_token=token
+        )
+        already_reviewers = await already_reviewed(gh)
+        team_reviewers, reviewers_to_add = select_reviewers(
+            author=author,
+            available_reviewers=available_reviewers,
+            assigned_reviewers=assigned_reviewers,
+            already_reviewers=already_reviewers,
+            count=int(config["numberOfReviewers"]),
         )
         async with trio.open_nursery() as nursery:
             if not event["pull_request"]["assignee"]:
