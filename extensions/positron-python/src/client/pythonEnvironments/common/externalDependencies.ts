e48e7696d8e7d8b975c0022e4ad0957b7267c4db
Kartik Raj <karraj@microsoft.com>
2021-02-24 12:38:39 -0600

    Ensure Posix Path locator doesn't return relative binary paths (microsoft/vscode-python#15495)
    * Ensure Posix Path locator doesn't return relative binary paths
    
    * News entries

--- extensions/positron-python/src/client/pythonEnvironments/common/externalDependencies.ts
+++ extensions/positron-python/src/client/pythonEnvironments/common/externalDependencies.ts
@@ -105,7 +105,12 @@ export async function resolveSymbolicLink(absPath: string): Promise<string> {
     const stats = await fsapi.lstat(absPath);
     if (stats.isSymbolicLink()) {
         const link = await fsapi.readlink(absPath);
-        return resolveSymbolicLink(link);
+        // Result from readlink is not guaranteed to be an absolute path. For eg. on Mac it resolves
+        // /usr/local/bin/python3.9 -> ../../../Library/Frameworks/Python.framework/Versions/3.9/bin/python3.9
+        //
+        // The resultant path is reported relative to the symlink directory we resolve. Convert that to absolute path.
+        const absLinkPath = path.isAbsolute(link) ? link : path.resolve(path.dirname(absPath), link);
+        return resolveSymbolicLink(absLinkPath);
     }
     return absPath;
 }
