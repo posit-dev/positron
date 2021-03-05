f789003c02f9801607bca1209d900e92597a70bd
Antti Pessa <33652286+anttipessa@users.noreply.github.com>
2021-03-05 12:29:18 -0600

    Add column offset to fix mypy linter pointing to wrong column number (off by one) (microsoft/vscode-python#15505)
    * Fix mypy column offset off by one
    
    * Add news item
    
    * Fix unit test
    
    * Update the expected output

--- extensions/positron-python/src/client/linters/mypy.ts
+++ extensions/positron-python/src/client/linters/mypy.ts
@@ -6,10 +6,11 @@ import { BaseLinter } from './baseLinter';
 import { ILintMessage } from './types';
 
 export const REGEX = '(?<file>[^:]+):(?<line>\\d+)(:(?<column>\\d+))?: (?<type>\\w+): (?<message>.*)\\r?(\\n|$)';
+const COLUMN_OFF_SET = 1;
 
 export class MyPy extends BaseLinter {
     constructor(outputChannel: OutputChannel, serviceContainer: IServiceContainer) {
-        super(Product.mypy, outputChannel, serviceContainer);
+        super(Product.mypy, outputChannel, serviceContainer, COLUMN_OFF_SET);
     }
 
     protected async runLinter(document: TextDocument, cancellation: CancellationToken): Promise<ILintMessage[]> {
