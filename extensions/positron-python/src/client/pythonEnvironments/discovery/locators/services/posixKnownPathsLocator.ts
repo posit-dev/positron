3d6e9034b4120266c0eae3ec8c1a7fe6e6fb8cf9
Karthik Nadig <kanadig@microsoft.com>
2021-03-08 18:34:42 -0600

    Use shorter paths when possible. (microsoft/vscode-python#15581)
    * Use shorter paths when possible.
    
    * Ensure all stubbed methods are restored.

--- extensions/positron-python/src/client/pythonEnvironments/discovery/locators/services/posixKnownPathsLocator.ts
+++ extensions/positron-python/src/client/pythonEnvironments/discovery/locators/services/posixKnownPathsLocator.ts
@@ -1,45 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import * as fs from 'fs';
 import * as path from 'path';
-import { traceError, traceInfo } from '../../../../common/logger';
+import { traceError } from '../../../../common/logger';
 import { Architecture } from '../../../../common/utils/platform';
 import { PythonEnvInfo, PythonEnvKind, PythonEnvSource, PythonReleaseLevel, PythonVersion } from '../../../base/info';
 import { buildEnvInfo } from '../../../base/info/env';
 import { parseVersion } from '../../../base/info/pythonVersion';
 import { IPythonEnvsIterator, Locator } from '../../../base/locator';
-import { getFileInfo, resolveSymbolicLink } from '../../../common/externalDependencies';
-import { commonPosixBinPaths, matchPythonBinFilename } from '../../../common/posixUtils';
+import { getFileInfo } from '../../../common/externalDependencies';
+import { commonPosixBinPaths, getPythonBinFromPosixPaths } from '../../../common/posixUtils';
 import { isPyenvShimDir } from './pyenvLocator';
-
-async function getPythonBinFromKnownPaths(): Promise<string[]> {
-    // Filter out pyenv shims. They are not actual python binaries, they are used to launch
-    // the binaries specified in .python-version file in the cwd. We should not be reporting
-    // those binaries as environments.
-    const knownDirs = (await commonPosixBinPaths()).filter((dirname) => !isPyenvShimDir(dirname));
-    const pythonBins: Set<string> = new Set();
-    for (const dirname of knownDirs) {
-        const paths = (await fs.promises.readdir(dirname, { withFileTypes: true }))
-            .filter((dirent: fs.Dirent) => !dirent.isDirectory())
-            .map((dirent: fs.Dirent) => path.join(dirname, dirent.name))
-            .filter(matchPythonBinFilename);
-
-        for (const filepath of paths) {
-            // Ensure that we have a collection of unique global binaries by
-            // resolving all symlinks to the target binaries.
-            try {
-                const resolvedBin = await resolveSymbolicLink(filepath);
-                pythonBins.add(resolvedBin);
-                traceInfo(`Found: ${filepath} --> ${resolvedBin}`);
-            } catch (ex) {
-                traceError('Failed to resolve symbolic link: ', ex);
-            }
-        }
-    }
-
-    return Array.from(pythonBins);
-}
 
 export class PosixKnownPathsLocator extends Locator {
     private kind: PythonEnvKind = PythonEnvKind.OtherGlobal;
@@ -47,7 +18,11 @@ export class PosixKnownPathsLocator extends Locator {
     public iterEnvs(): IPythonEnvsIterator {
         const buildPathEnvInfo = (bin: string) => this.buildPathEnvInfo(bin);
         const iterator = async function* () {
-            const exes = await getPythonBinFromKnownPaths();
+            // Filter out pyenv shims. They are not actual python binaries, they are used to launch
+            // the binaries specified in .python-version file in the cwd. We should not be reporting
+            // those binaries as environments.
+            const knownDirs = (await commonPosixBinPaths()).filter((dirname) => !isPyenvShimDir(dirname));
+            const exes = await getPythonBinFromPosixPaths(knownDirs);
             yield* exes.map(buildPathEnvInfo);
         };
         return iterator();
