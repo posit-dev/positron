bf503fec722e210fbe40246d00bafe7b8bd03c66
Karthik Nadig <kanadig@microsoft.com>
2021-03-09 12:35:48 -0600

    Ensure that jedi language server uses jedi shipped with the extension (microsoft/vscode-python#15603)

--- extensions/positron-python/pythonFiles/runJediLanguageServer.py
+++ extensions/positron-python/pythonFiles/runJediLanguageServer.py
@@ -3,7 +3,7 @@ import os
 
 # Add the lib path to our sys path so jedi_language_server can find its references
 EXTENSION_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(EXTENSION_ROOT, "pythonFiles", "lib", "python"))
+sys.path.insert(0, os.path.join(EXTENSION_ROOT, "pythonFiles", "lib", "python"))
 
 import pygls.protocol
 
