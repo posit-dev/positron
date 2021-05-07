40f4ef0f42db4e5b0e07f0df833df190d7b51728
paulacamargo25 <paulitacv25@gmail.com>
2021-05-06 19:24:52 -0500

    Automatically fill data in report issue (microsoft/vscode-python#16085)
    * Add python version and language server
    
    * Add tests for env data
    
    * Clean  up
    
    * Remove test values and use ResourceServer
    
    * Fix virtualenv value
    
    * Fix default python path value
    
    Co-authored-by: Karthik Nadig <kanadig@microsoft.com>

--- extensions/positron-python/src/test/common/application/commands/reportIssueCommand.unit.test.ts
+++ extensions/positron-python/src/test/common/application/commands/reportIssueCommand.unit.test.ts
@@ -3,22 +3,85 @@
 
 'use strict';
 
-import { anything, instance, mock, verify, when } from 'ts-mockito';
+import * as sinon from 'sinon';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { anyString, anything, capture, instance, mock, verify, when } from 'ts-mockito';
+import { expect } from 'chai';
+import { LanguageServerType } from '../../../../client/activation/types';
 import { CommandManager } from '../../../../client/common/application/commandManager';
 import { ReportIssueCommandHandler } from '../../../../client/common/application/commands/reportIssueCommand';
-import { ICommandManager } from '../../../../client/common/application/types';
+import { ICommandManager, IWorkspaceService } from '../../../../client/common/application/types';
+import { WorkspaceService } from '../../../../client/common/application/workspace';
+import { IInterpreterService, IInterpreterVersionService } from '../../../../client/interpreter/contracts';
+import { InterpreterVersionService } from '../../../../client/interpreter/interpreterVersion';
+import { PythonEnvKind } from '../../../../client/pythonEnvironments/base/info';
+import * as EnvIdentifier from '../../../../client/pythonEnvironments/common/environmentIdentifier';
+import { MockWorkspaceConfiguration } from '../../../startPage/mockWorkspaceConfig';
+import { EXTENSION_ROOT_DIR_FOR_TESTS } from '../../../constants';
+import { InterpreterService } from '../../../../client/interpreter/interpreterService';
 
 suite('Report Issue Command', () => {
     let reportIssueCommandHandler: ReportIssueCommandHandler;
     let cmdManager: ICommandManager;
+    let workspaceService: IWorkspaceService;
+    let interpreterVersionService: IInterpreterVersionService;
+    let interpreterService: IInterpreterService;
+    let identifyEnvironmentStub: sinon.SinonStub;
+
     setup(async () => {
+        interpreterVersionService = mock(InterpreterVersionService);
+        workspaceService = mock(WorkspaceService);
         cmdManager = mock(CommandManager);
-        reportIssueCommandHandler = new ReportIssueCommandHandler(instance(cmdManager));
-        when(cmdManager.executeCommand(anything())).thenResolve();
+        interpreterService = mock(InterpreterService);
+
+        when(interpreterVersionService.getVersion(anything(), anything())).thenResolve('3.9.0');
+        when(workspaceService.getConfiguration('python')).thenReturn(
+            new MockWorkspaceConfiguration({
+                languageServer: LanguageServerType.Node,
+            }),
+        );
+        when(interpreterService.getActiveInterpreter(anything())).thenResolve(undefined);
+        identifyEnvironmentStub = sinon.stub(EnvIdentifier, 'identifyEnvironment');
+        identifyEnvironmentStub.resolves(PythonEnvKind.Venv);
+
+        cmdManager = mock(CommandManager);
+        reportIssueCommandHandler = new ReportIssueCommandHandler(
+            instance(cmdManager),
+            instance(workspaceService),
+            instance(interpreterService),
+            instance(interpreterVersionService),
+        );
+
+        when(cmdManager.executeCommand(anyString(), anything())).thenResolve();
         await reportIssueCommandHandler.activate();
     });
 
-    test('Confirm command handler is added', async () => {
+    teardown(() => {
+        identifyEnvironmentStub.restore();
+    });
+
+    test('Test if issue body is filled', async () => {
+        await reportIssueCommandHandler.openReportIssue();
+
+        const templatePath = path.join(
+            EXTENSION_ROOT_DIR_FOR_TESTS,
+            'src',
+            'test',
+            'common',
+            'application',
+            'commands',
+            'issueTemplateVenv1.md',
+        );
+        const expectedIssueBody = fs.readFileSync(templatePath, 'utf8');
+
+        const args: [string, { extensionId: string; issueBody: string }] = capture<
+            string,
+            { extensionId: string; issueBody: string }
+        >(cmdManager.executeCommand).last();
+
         verify(cmdManager.registerCommand('python.reportIssue', anything(), anything())).once();
+        verify(cmdManager.executeCommand('workbench.action.openIssueReporter', anything())).once();
+        expect(args[1].issueBody).to.be.equal(expectedIssueBody);
     });
 });
