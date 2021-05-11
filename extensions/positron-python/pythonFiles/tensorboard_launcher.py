18ce969f45d2e56dd18911cedac76cb45064bbbe
Joyce Er <joyceerhuiling@gmail.com>
2021-05-11 17:05:46 -0500

    Allow TensorBoard to load in VS Code despite existing MIME type misconfig (microsoft/vscode-python#16203)

--- extensions/positron-python/pythonFiles/tensorboard_launcher.py
+++ extensions/positron-python/pythonFiles/tensorboard_launcher.py
@@ -1,17 +1,25 @@
 import time
 import sys
 import os
-from tensorboard import default
+import mimetypes
 from tensorboard import program
 
 
 def main(logdir):
+    # Environment variable for PyTorch profiler TensorBoard plugin
+    # to detect when it's running inside VS Code
     os.environ["VSCODE_TENSORBOARD_LAUNCH"] = "1"
+
+    # Work around incorrectly configured MIME types on Windows
+    mimetypes.add_type("application/javascript", ".js")
+
+    # Start TensorBoard using their Python API
     tb = program.TensorBoard()
     tb.configure(bind_all=False, logdir=logdir)
     url = tb.launch()
     sys.stdout.write("TensorBoard started at %s\n" % (url))
     sys.stdout.flush()
+
     while True:
         try:
             time.sleep(60)
