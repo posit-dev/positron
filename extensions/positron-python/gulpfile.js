ae99410be2728d0f4887b7083542dbad9ac1f457
Karthik Nadig <kanadig@microsoft.com>
2021-08-25 18:12:18 -0500

    Revert "Remove jupyter from the extension pack. (microsoft/vscode-python#17080)" (microsoft/vscode-python#17129)
    This reverts commit 84f4fcc48397395be9bb0c39b95cc5d99fa60a39.

--- extensions/positron-python/gulpfile.js
+++ extensions/positron-python/gulpfile.js
@@ -80,7 +80,7 @@ async function addExtensionPackDependencies() {
     // extension dependencies need not be installed during development
     const packageJsonContents = await fsExtra.readFile('package.json', 'utf-8');
     const packageJson = JSON.parse(packageJsonContents);
-    packageJson.extensionPack = ['ms-python.vscode-pylance'].concat(
+    packageJson.extensionPack = ['ms-toolsai.jupyter', 'ms-python.vscode-pylance'].concat(
         packageJson.extensionPack ? packageJson.extensionPack : [],
     );
     await fsExtra.writeFile('package.json', JSON.stringify(packageJson, null, 4), 'utf-8');
