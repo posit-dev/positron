3f05e9259a9fa73eab1d75f363cf80d3459d53e7
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2021-09-02 16:12:44 -0500

    Clean up src/test/mocks/vsc/* (microsoft/vscode-python#17245)
    * Clean up src/test/mocks/vsc/*
    
    * Use old extHostedTypes
    
    * Fix some stuff
    
    * Fix compilation erorrs in hosted types
    
    * Fix some stuff
    
    * Fix more stuff and Object.setPrototypeOf
    
    * Fix linting issue
    
    * Last problem

--- extensions/positron-python/src/test/mocks/vsc/strings.ts
+++ extensions/positron-python/src/test/mocks/vsc/strings.ts
@@ -1,38 +1,35 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 'use strict';
 
-export namespace vscMockStrings {
-    /**
-     * Determines if haystack starts with needle.
-     */
-    export function startsWith(haystack: string, needle: string): boolean {
-        if (haystack.length < needle.length) {
-            return false;
-        }
-
-        for (let i = 0; i < needle.length; i++) {
-            if (haystack[i] !== needle[i]) {
-                return false;
-            }
-        }
-
-        return true;
+/**
+ * Determines if haystack starts with needle.
+ */
+export function startsWith(haystack: string, needle: string): boolean {
+    if (haystack.length < needle.length) {
+        return false;
     }
 
-    /**
-     * Determines if haystack ends with needle.
-     */
-    export function endsWith(haystack: string, needle: string): boolean {
-        let diff = haystack.length - needle.length;
-        if (diff > 0) {
-            return haystack.indexOf(needle, diff) === diff;
-        } else if (diff === 0) {
-            return haystack === needle;
-        } else {
+    for (let i = 0; i < needle.length; i += 1) {
+        if (haystack[i] !== needle[i]) {
             return false;
         }
     }
+
+    return true;
+}
+
+/**
+ * Determines if haystack ends with needle.
+ */
+export function endsWith(haystack: string, needle: string): boolean {
+    const diff = haystack.length - needle.length;
+    if (diff > 0) {
+        return haystack.indexOf(needle, diff) === diff;
+    }
+    if (diff === 0) {
+        return haystack === needle;
+    }
+    return false;
 }
