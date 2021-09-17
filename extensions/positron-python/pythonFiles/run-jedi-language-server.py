65241157d4134b07bf52237740a25dc20ebdf210
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2021-09-17 18:10:49 -0500

    Jedi behind LSP reqs in main reqs files (microsoft/vscode-python#17446)
    * Jedi behind LSP reqs in main reqs files
    
    * Update requirements to be Python 3 only
    
    * Update workflows to install reqs only for Python 3
    
    * Update gulpfile
    
    * Delete jedils_requirements files
    
    * Re-add dataclasses but for python 3.6 only
    
    * Update file path in JediLanguageServerManager
    
    * jedi lsp now lives under pythonFiles/lib directly
    
    * lol

--- extensions/positron-python/pythonFiles/run-jedi-language-server.py
+++ extensions/positron-python/pythonFiles/run-jedi-language-server.py
@@ -3,7 +3,7 @@ import os
 
 # Add the lib path to our sys path so jedi_language_server can find its references
 EXTENSION_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(EXTENSION_ROOT, "pythonFiles", "lib", "jedilsp"))
+sys.path.insert(0, os.path.join(EXTENSION_ROOT, "pythonFiles", "lib", "python"))
 
 
 from jedi_language_server.cli import cli
