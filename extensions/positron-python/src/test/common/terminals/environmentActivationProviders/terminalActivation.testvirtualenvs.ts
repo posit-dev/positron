e3c4cd1bfc8b68af543c0aa3142f47556d662677
Karthik Nadig <kanadig@microsoft.com>
2021-10-15 16:18:25 -0500

    Skip tests due to microsoft/vscode-python#17666 (microsoft/vscode-python#17757)

--- extensions/positron-python/src/test/common/terminals/environmentActivationProviders/terminalActivation.testvirtualenvs.ts
+++ extensions/positron-python/src/test/common/terminals/environmentActivationProviders/terminalActivation.testvirtualenvs.ts
@@ -182,12 +182,18 @@ suite('Activation of Environments in Terminal', () => {
         if (process.env.CI_PYTHON_VERSION && process.env.CI_PYTHON_VERSION.startsWith('2.')) {
             this.skip();
         }
+        // https://github.com/microsoft/vscode-python/issues/17666
+        this.skip();
         await testActivation(envPaths.venvPath);
     });
-    test('Should activate with pipenv', async () => {
+    test('Should activate with pipenv', async function () {
+        // https://github.com/microsoft/vscode-python/issues/17666
+        this.skip();
         await testActivation(envPaths.pipenvPath);
     });
-    test('Should activate with virtualenv', async () => {
+    test('Should activate with virtualenv', async function () {
+        // https://github.com/microsoft/vscode-python/issues/17666
+        this.skip();
         await testActivation(envPaths.virtualEnvPath);
     });
     test('Should activate with conda', async function () {
