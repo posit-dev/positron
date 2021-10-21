4ad3f50c21c03ea3bf6c926fde49662678981047
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2021-10-21 11:58:50 -0500

    Remove some unused packages (microsoft/vscode-python#17657)
    * Remove some unused packages
    
    * Re-add packages (failing tests)
    
    * Revert "Re-add packages (failing tests)"
    
    This reverts commit c4ee1ea708d06b06e88fc22d21a85b128add3988.

--- extensions/positron-python/src/test/common/asyncDump.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//tslint:disable:no-require-imports no-var-requires
-const log = require('why-is-node-running');
-
-// Call this function to debug async hangs. It should print out stack traces of still running promises.
-export function asyncDump() {
-    log();
-}
