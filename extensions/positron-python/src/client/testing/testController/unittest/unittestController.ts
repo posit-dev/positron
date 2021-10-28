c5d0c894239470427cc5896c1de1f74529ffeb6d
Karthik Nadig <kanadig@microsoft.com>
2021-10-28 00:39:04 -0500

    Make updateTestItemFromRawData async (microsoft/vscode-python#17864)
    * make updateTestItemFromRawData async
    
    * Add news

--- extensions/positron-python/src/client/testing/testController/unittest/unittestController.ts
+++ extensions/positron-python/src/client/testing/testController/unittest/unittestController.ts
@@ -70,7 +70,7 @@ export class UnittestController implements ITestFrameworkController {
                     }
 
                     if (rawTestData.tests.length > 0) {
-                        updateTestItemFromRawData(
+                        await updateTestItemFromRawData(
                             item,
                             testController,
                             this.idToRawData,
@@ -85,7 +85,7 @@ export class UnittestController implements ITestFrameworkController {
                 } else {
                     const workspaceNode = getWorkspaceNode(item, this.idToRawData);
                     if (workspaceNode) {
-                        updateTestItemFromRawData(
+                        await updateTestItemFromRawData(
                             item,
                             testController,
                             this.idToRawData,
