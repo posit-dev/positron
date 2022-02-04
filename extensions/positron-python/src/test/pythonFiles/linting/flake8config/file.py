45d8f1a0384b7559bbcda1dadc0fd120e6b7e2ff
Kim-Adeline Miguel <51720070+kimadeline@users.noreply.github.com>
2022-02-04 12:36:50 -0600

    CI failures: Pin prospector to 1.6.0, pytest to < 7.0.0, and fix linting issues with flake8 3.9.2 (microsoft/vscode-python#18446)
    * See if pinning pip works
    
    * Pin in pr check
    
    * Pin pip everywhere else
    
    * angery
    
    * Undo pip changes
    
    * Pin pytest < 7.0.0
    
    * Fix linting?

--- extensions/positron-python/src/test/pythonFiles/linting/flake8config/file.py
+++ extensions/positron-python/src/test/pythonFiles/linting/flake8config/file.py
@@ -2,6 +2,7 @@
 
 __revision__ = None
 
+
 class Foo(object):
     """block-disable test"""
 
@@ -10,78 +11,76 @@ class Foo(object):
 
     def meth1(self, arg):
         """this issues a message"""
-        print self
+        print(self)
 
     def meth2(self, arg):
         """and this one not"""
         # pylint: disable=unused-argument
-        print self\
-              + "foo"
+        print(self + "foo")
 
     def meth3(self):
         """test one line disabling"""
         # no error
-        print self.bla # pylint: disable=no-member
+        print(self.bla)  # pylint: disable=no-member
         # error
-        print self.blop
+        print(self.blop)
 
     def meth4(self):
         """test re-enabling"""
         # pylint: disable=no-member
         # no error
-        print self.bla
-        print self.blop
+        print(self.bla)
+        print(self.blop)
         # pylint: enable=no-member
         # error
-        print self.blip
+        print(self.blip)
 
     def meth5(self):
         """test IF sub-block re-enabling"""
         # pylint: disable=no-member
         # no error
-        print self.bla
+        print(self.bla)
         if self.blop:
             # pylint: enable=no-member
             # error
-            print self.blip
+            print(self.blip)
         else:
             # no error
-            print self.blip
+            print(self.blip)
         # no error
-        print self.blip
+        print(self.blip)
 
     def meth6(self):
         """test TRY/EXCEPT sub-block re-enabling"""
         # pylint: disable=no-member
         # no error
-        print self.bla
+        print(self.bla)
         try:
             # pylint: enable=no-member
             # error
-            print self.blip
-        except UndefinedName: # pylint: disable=undefined-variable
+            print(self.blip)
+        except UndefinedName:  # pylint: disable=undefined-variable
             # no error
-            print self.blip
+            print(self.blip)
         # no error
-        print self.blip
+        print(self.blip)
 
     def meth7(self):
         """test one line block opening disabling"""
-        if self.blop: # pylint: disable=no-member
+        if self.blop:  # pylint: disable=no-member
             # error
-            print self.blip
+            print(self.blip)
         else:
             # error
-            print self.blip
+            print(self.blip)
         # error
-        print self.blip
-
+        print(self.blip)
 
     def meth8(self):
         """test late disabling"""
         # error
-        print self.blip
+        print(self.blip)
         # pylint: disable=no-member
         # no error
-        print self.bla
-        print self.blop
+        print(self.bla)
+        print(self.blop)
