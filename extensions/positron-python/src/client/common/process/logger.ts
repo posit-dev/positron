6316cf9f4de4c150c1da4e28a18a124c7a33fc9b
sunyinqi0508 <sunyinqi0508@126.com>
2022-04-11 13:29:39 -0500

    Fixed problem where match string contain special characters (microsoft/vscode-python#18878)
    * Fixed problem where match string contain special characters
    
    * Fixes issue where () and | are escaped for windows
    
    * Fixed a problem when some characters are not escaped.
    Added Credits.
    
    * Update src/client/common/process/logger.ts
    
    Simplify code.
    
    Co-authored-by: Kartik Raj <karraj@microsoft.com>
    
    * Only replace '\\' with '(\\|/)' on Windows to save a scan on other OSs.
    
    * Update src/client/common/process/logger.ts
    
    Co-authored-by: Kartik Raj <karraj@microsoft.com>

--- extensions/positron-python/src/client/common/process/logger.ts
+++ extensions/positron-python/src/client/common/process/logger.ts
@@ -10,6 +10,7 @@ import { isCI, isTestExecution } from '../constants';
 import { Logging } from '../utils/localize';
 import { getOSType, getUserHomeDir, OSType } from '../utils/platform';
 import { IProcessLogger, SpawnOptions } from './types';
+import { escapeRegExp } from 'lodash';
 
 @injectable()
 export class ProcessLogger implements IProcessLogger {
@@ -50,10 +51,13 @@ export class ProcessLogger implements IProcessLogger {
  * Finds case insensitive matches in the original string and replaces it with character provided.
  */
 function replaceMatchesWithCharacter(original: string, match: string, character: string): string {
-    // Backslashes have special meaning in regexes, we need an extra backlash so
-    // it's not considered special. Also match both forward and backward slash
-    // versions of 'match' for Windows.
-    const pattern = match.replaceAll('\\', getOSType() === OSType.Windows ? '(\\\\|/)' : '\\\\');
+    // Backslashes, plus signs, brackets and other characters have special meaning in regexes,
+    // we need to escape using an extra backlash so it's not considered special.
+    let pattern = escapeRegExp(match);
+    if (getOSType() === OSType.Windows) {
+        // Match both forward and backward slash versions of 'match' for Windows.
+        pattern = pattern.replaceAll('\\\\', '(\\\\|/)');
+    }
     let regex = new RegExp(pattern, 'ig');
     return original.replace(regex, character);
 }
