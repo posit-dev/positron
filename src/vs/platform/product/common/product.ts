c8165f96dbe53c2fb6c21e303a528a6d73acec3c
Jonathan McPherson <jonathan@rstudio.com>
2022-05-24 17:49:34 -0500

    extremely basic rebranding

--- src/vs/platform/product/common/product.ts
+++ src/vs/platform/product/common/product.ts
@@ -59,14 +59,14 @@ else {
 	if (Object.keys(product).length === 0) {
 		Object.assign(product, {
 			version: '1.67.0-dev',
-			nameShort: 'Code - OSS Dev',
-			nameLong: 'Code - OSS Dev',
-			applicationName: 'code-oss',
-			dataFolderName: '.vscode-oss',
-			urlProtocol: 'code-oss',
-			reportIssueUrl: 'https://github.com/microsoft/vscode/issues/new',
+			nameShort: 'Myriac',
+			nameLong: 'Myriac',
+			applicationName: 'myriac',
+			dataFolderName: '.myriac',
+			urlProtocol: 'myriac',
+			reportIssueUrl: 'https://github.com/rstudio/myriac/issues/new',
 			licenseName: 'MIT',
-			licenseUrl: 'https://github.com/microsoft/vscode/blob/main/LICENSE.txt'
+			licenseUrl: 'https://github.com/rstudio/myriac/blob/main/LICENSE.txt'
 		});
 	}
 }
