ccdea7a80c24a19a50198affab87115f934489d9
Jonathan McPherson <jonathan@rstudio.com>
2022-05-24 17:49:34 -0500

    extremely basic rebranding

--- src/vs/workbench/contrib/welcomeGettingStarted/browser/gettingStarted.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/browser/gettingStarted.ts
@@ -745,7 +745,7 @@ export class GettingStartedPage extends EditorPane {
 
 		const header = $('.header', {},
 			$('h1.product-name.caption', {}, this.productService.nameLong),
-			$('p.subtitle.description', {}, localize({ key: 'gettingStarted.editingEvolved', comment: ['Shown as subtitle on the Welcome page.'] }, "Editing evolved"))
+			$('p.subtitle.description', {}, localize({ key: 'gettingStarted.editingEvolved', comment: ['Shown as subtitle on the Welcome page.'] }, "An IDE for data science"))
 		);
 
 
