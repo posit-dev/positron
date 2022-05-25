fbb61beff58c628dfc74329526717bb359d6c7ac
Jonathan McPherson <jonathan@rstudio.com>
2022-05-25 16:07:44 -0500

    fix apt usage; install tinytex

--- .devcontainer/cache/post-create.sh
+++ .devcontainer/cache/post-create.sh
@@ -6,12 +6,16 @@
 #
 # This wraps the restore-diff script that is the only post-create command
 # formerly executed by the VS Code dev container.
+#
+# After changing this script, use the "Remote Container - Rebuild Dev Container
+# Without Cache" command in VS Code to test your changes.
 
 echo "Setting up Myriac dev environment...."
+sudo apt update
 
 # Install Python infrastructure; the dev container already contains Python, but not pip.
 echo "*** Installing pip package manager"
-sudo apt install python3-pip
+sudo apt --assume-yes install python3-pip
 
 # Install key Python packages
 echo "*** Installing key Python packages"
@@ -19,19 +23,23 @@ pip3 install ipython ipykernel pandas numpy matplotlib
 
 # Install R base system
 echo "*** Installing R base system"
-sudo apt install r-base
+sudo apt --assume-yes install r-base
 
 # Install key R packages; the dev container doesn't have a writeable system
 # library, so create a user library first and write everything there.
 echo "*** Installing key R packages"
 RPKGLIB=$(Rscript -e 'cat(path.expand(Sys.getenv("R_LIBS_USER")))')
 mkdir -p "$RPKGLIB"
-Rscript -e "install.packages(c('rmarkdown', 'renv', 'shiny', 'testthat', 'tidyverse'), repos='https://cran.rstudio.com/')"
+Rscript -e "install.packages(c('rmarkdown', 'renv', 'shiny', 'testthat', 'tidyverse', 'tinytex'), repos='https://cran.rstudio.com/')"
+Rscript -e "tinytex::install_tinytex()"
 
 # Install Myriac extensions from OpenVSX. Note that this list of extensions
 # differs from the one in `devcontainer.json`; that one controls the extensions
 # installed in the VS Code host, whereas this one installs extensions into the
 # development/working copy of Myriac.
+#
+# TODO: This may not succeed since the `code` binary isn't available in the
+# image at the post-create step.
 echo "*** Installing extensions from OpenVSX..."
 code --install-extension ms-python.python
 code --install-extension ms-toolsai.jupyter
