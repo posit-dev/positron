52299a262af420406b000fce3b3e53e03be6801d
paulacamargo25 <paulitacv25@gmail.com>
2022-06-07 14:39:47 -0500

    Fix regex expression in env file (microsoft/vscode-python#19244)
    * Fix regex expression
    
    * Fix tests
    
    * Fix reg exp
    
    * Fix test

--- extensions/positron-python/src/test/common/variables/envVarsService.unit.test.ts
+++ extensions/positron-python/src/test/common/variables/envVarsService.unit.test.ts
@@ -395,7 +395,6 @@ Path=/usr/x:/usr/y
 SPAM=1234
 ham=5678
 Eggs=9012
-_bogus1=...
 1bogus2=...
 bogus 3=...
 bogus.4=...
@@ -403,15 +402,17 @@ bogus-5=...
 bogus~6=...
 VAR1=3456
 VAR_2=7890
+_VAR_3=1234
             `);
 
             expect(vars).to.not.equal(undefined, 'Variables is undefiend');
-            expect(Object.keys(vars!)).lengthOf(5, 'Incorrect number of variables');
+            expect(Object.keys(vars!)).lengthOf(6, 'Incorrect number of variables');
             expect(vars).to.have.property('SPAM', '1234', 'value is invalid');
             expect(vars).to.have.property('ham', '5678', 'value is invalid');
             expect(vars).to.have.property('Eggs', '9012', 'value is invalid');
             expect(vars).to.have.property('VAR1', '3456', 'value is invalid');
             expect(vars).to.have.property('VAR_2', '7890', 'value is invalid');
+            expect(vars).to.have.property('_VAR_3', '1234', 'value is invalid');
         });
 
         test('Empty values become empty string', () => {
