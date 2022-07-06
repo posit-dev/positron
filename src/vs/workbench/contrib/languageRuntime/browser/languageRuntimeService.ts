2e1a9ac3312f9dc2b8f5aac3d3725897acac4815
Jonathan McPherson <jonathan@rstudio.com>
2022-07-06 11:06:25 -0500

    language runtime contribution registration

---
+++ src/vs/workbench/contrib/languageRuntime/browser/languageRuntimeService.ts
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) RStudio, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ILanguageRuntimeService } from 'vs/workbench/contrib/languageRuntime/common/languageRuntimeService';
+import { INotebookKernel } from 'vs/workbench/contrib/notebook/common/notebookKernelService';
+
+/**
+ * The implementation of ILanguageRuntimeService
+ */
+export class LanguageRuntimeService implements ILanguageRuntimeService {
+	private readonly _runtimes: Map<String, INotebookKernel> = new Map();
+	private _activeLanguage: string = '';
+
+	registerRuntime(language: string, kernel: INotebookKernel): void {
+		this._runtimes.set(language, kernel);
+
+		// pick up the active language if we haven't set one yet
+		if (this._activeLanguage === '') {
+			this._activeLanguage = language;
+		}
+	}
+
+	getActiveRuntime(language: string | null): INotebookKernel | undefined {
+		if (typeof language === 'string') {
+			return this._runtimes.get(language);
+		}
+		return this._runtimes.get(this._activeLanguage);
+	}
+
+	setActiveRuntime(language: string): void {
+		this._activeLanguage = language;
+	}
+}
---
+++ src/vs/workbench/contrib/languageRuntime/common/languageRuntimeService.ts
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) RStudio, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+import { INotebookKernel } from 'vs/workbench/contrib/notebook/common/notebookKernelService';
+
+export const ILanguageRuntimeService = createDecorator<ILanguageRuntimeService>('ILanguageRuntimeService');
+
+export interface ILanguageRuntimeService {
+
+	/**
+	 * @param language The language being registered
+	 * @param kernel The NotebookKernel for the language
+	 */
+	registerRuntime(language: string, kernel: INotebookKernel): void;
+
+	/**
+	 *
+	 * @param language The specific language runtime to retrieve, or `null` to
+	 *   retrieve the default
+	 */
+	getActiveRuntime(language: string | null): INotebookKernel | undefined;
+
+	/**
+	 * Selects the active language runtime
+	 *
+	 * @param language The language to select
+	 */
+	setActiveRuntime(language: string): void;
+}
