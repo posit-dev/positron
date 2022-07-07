4e9bd2f4d60420b0f3835e5e4c998de4cfb74f65
Jonathan McPherson <jonathan@rstudio.com>
2022-07-07 10:42:11 -0400

    stub command for switching active langauge runtime

--- src/vs/workbench/contrib/languageRuntime/browser/languageRuntime.contribution.ts
+++ src/vs/workbench/contrib/languageRuntime/browser/languageRuntime.contribution.ts
@@ -5,6 +5,10 @@
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { LanguageRuntimeService } from 'vs/workbench/contrib/languageRuntime/browser/languageRuntimeService';
 import { ILanguageRuntimeService } from 'vs/workbench/contrib/languageRuntime/common/languageRuntimeService';
+import { registerLanguageRuntimeActions } from 'vs/workbench/contrib/languageRuntime/browser/languageRuntimeActions';
 
-// Register REPL service singleton with platform
+// Register language runtime singleton
 registerSingleton(ILanguageRuntimeService, LanguageRuntimeService, true);
+
+// Register commands
+registerLanguageRuntimeActions();
---
+++ src/vs/workbench/contrib/languageRuntime/common/languageRuntime.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) RStudio, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+export const REPL_VIEW_ID = 'repl';
+import * as nls from 'vs/nls';
+
+export const enum LanguageRuntimeCommandId {
+	Select = 'workbench.action.languageRuntime.select',
+}
+
+export const LANGUAGE_RUNTIME_ACTION_CATEGORY = nls.localize('languageRuntimeCategory', "Language Runtime");
