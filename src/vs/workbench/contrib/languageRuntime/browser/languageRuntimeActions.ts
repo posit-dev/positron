047888693c427c7184d33e82d0efc9ff91c7ff45
Jonathan McPherson <jonathan@rstudio.com>
2022-07-07 10:21:40 -0500

    prompt user to select language runtime

--- src/vs/workbench/contrib/languageRuntime/browser/languageRuntimeActions.ts
+++ src/vs/workbench/contrib/languageRuntime/browser/languageRuntimeActions.ts
@@ -4,10 +4,14 @@
 
 import { Action2, registerAction2 } from 'vs/platform/actions/common/actions';
 import { localize } from 'vs/nls';
+import * as nls from 'vs/nls';
+import { URI } from 'vs/base/common/uri';
 import { LanguageRuntimeCommandId, LANGUAGE_RUNTIME_ACTION_CATEGORY } from 'vs/workbench/contrib/languageRuntime/common/languageRuntime';
 import { ILocalizedString } from 'vs/platform/action/common/action';
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
 import { Codicon } from 'vs/base/common/codicons';
+import { INotebookKernelService } from 'vs/workbench/contrib/notebook/common/notebookKernelService';
+import { IQuickInputService, IQuickPickItem } from 'vs/platform/quickinput/common/quickInput';
 
 export function registerLanguageRuntimeActions() {
 	const category: ILocalizedString = { value: LANGUAGE_RUNTIME_ACTION_CATEGORY, original: 'Language Runtime' };
@@ -34,12 +38,35 @@ export function registerLanguageRuntimeActions() {
 		}
 
 		/**
-		 * Runs the select.language.runtime command to select a language runtime
+		 * Prompts the user to select a language runtime
 		 *
 		 * @param accessor The service accessor.
 		 */
 		async run(accessor: ServicesAccessor) {
-			throw new Error('Sorry, no idea how to do that.');
+			// Get all registered kernels. (TODO: This will eventually use the
+			// language runtime service instead of relying on registered
+			// notebook kernels)
+			const kernelService = accessor.get(INotebookKernelService);
+			const allKernels = kernelService.getMatchingKernel({
+				uri: URI.parse('repl:any'),
+				viewType: 'interactive'
+			}).all;
+
+			// Map to quick-pick items for user selection
+			const selections = allKernels.map((k) => (<IQuickPickItem>{
+				id: k.id,
+				label: k.label,
+				description: k.description
+			}));
+
+			// Prompt the user to select a kernel/runtime
+			const pickService = accessor.get(IQuickInputService);
+			const selection = await pickService.pick(selections, {
+				canPickMany: false,
+				placeHolder: nls.localize('language runtime placeholder', 'Select Language Runtime')
+			});
+
+			throw new Error('Seems like you wanted this one: ' + selection?.id);
 		}
 	});
 }
