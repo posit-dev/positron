9262917e7acb51fd8082085ebcbdb8877ef11113
Karthik Nadig <kanadig@microsoft.com>
2022-07-13 15:22:16 -0500

    Update app insights key. (microsoft/vscode-python#19463)

--- extensions/positron-python/src/client/common/constants.ts
+++ extensions/positron-python/src/client/common/constants.ts
@@ -23,7 +23,7 @@ export const PYTHON_NOTEBOOKS = [
 export const PVSC_EXTENSION_ID = 'ms-python.python';
 export const PYLANCE_EXTENSION_ID = 'ms-python.vscode-pylance';
 export const JUPYTER_EXTENSION_ID = 'ms-toolsai.jupyter';
-export const AppinsightsKey = 'AIF-d9b70cd4-b9f9-4d70-929b-a071c400b217';
+export const AppinsightsKey = '0c6ae279ed8443289764825290e4f9e2-1a736e7c-1324-4338-be46-fc2a58ae4d14-7255';
 
 export type Channel = 'stable' | 'insiders';
 
