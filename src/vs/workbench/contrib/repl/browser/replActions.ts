5860eb4c85a2c18e0b1be4178b15c6cb336cf860
Jonathan McPherson <jonathan@rstudio.com>
2022-07-18 14:54:48 -0500

    basic execution-by-selection command impl

--- src/vs/workbench/contrib/repl/browser/replActions.ts
+++ src/vs/workbench/contrib/repl/browser/replActions.ts
@@ -9,6 +9,10 @@ import { ILocalizedString } from 'vs/platform/action/common/action';
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
 import { Codicon } from 'vs/base/common/codicons';
 import { ICreateReplOptions, IReplService } from 'vs/workbench/contrib/repl/browser/repl';
+import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
+import { ILogService } from 'vs/platform/log/common/log';
+import { IEditor } from 'vs/editor/common/editorCommon';
+import { ITextModel } from 'vs/editor/common/model';
 
 export function registerReplActions() {
 	const category: ILocalizedString = { value: REPL_ACTION_CATEGORY, original: 'REPL' };
@@ -64,14 +68,72 @@ export function registerReplActions() {
 		}
 
 		/**
-		 * Runs the repl.new command to create a new REPL instance.
+		 * Runs the repl.clear command to clear the REPL instance.
+		 *
+		 * @param accessor The service accessor.
+		 */
+		async run(accessor: ServicesAccessor) {
+			const replService = accessor.get(IReplService);
+			await replService.clearActiveRepl();
+		}
+	});
+
+	registerAction2(class extends Action2 {
+		constructor() {
+			super({
+				id: ReplCommandId.Clear,
+				title: { value: localize('workbench.action.repl.send', "Send to REPL"), original: 'Send to REPL' },
+				f1: true,
+				category,
+				icon: Codicon.plus,
+				description: {
+					description: 'workbench.action.repl.send',
+					args: []
+				}
+			});
+		}
+
+		/**
+		 * Runs the repl.send command to send the current selection, line, or
+		 * statement to the REPL
 		 *
 		 * @param accessor The service accessor.
 		 * @param options The options for the new REPL instance.
 		 */
 		async run(accessor: ServicesAccessor) {
+			const editorService = accessor.get(IEditorService);
+			const logService = accessor.get(ILogService);
+
+			// Ensure an editor is open to read from
+			const editor = editorService.activeTextEditorControl as IEditor;
+			if (!editor) {
+				logService.warn('Attempt to run selection without an open editor');
+				return;
+			}
+
+			// Ensure we have a target language
+			const language = editorService.activeTextEditorLanguageId;
+			if (!language) {
+				logService.warn('Attempt to run selection without a discernable input language');
+				return;
+			}
+
+			let code = '';
+			const selection = editor.getSelection();
+			const model = editor.getModel() as ITextModel;
+			if (selection) {
+				code = model.getValueInRange(selection);
+			} else {
+				const position = editor.getPosition();
+				if (position) {
+					code = model.getLineContent(position.lineNumber);
+				} else {
+					logService.warn('Cannot determine location of cursor for running current line');
+				}
+			}
+
 			const replService = accessor.get(IReplService);
-			await replService.clearActiveRepl();
+			replService.executeCode(language, code);
 		}
 	});
 }
