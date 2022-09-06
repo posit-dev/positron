857b6e3ed0c25e62afea5712b842bc1162df8391
Jonathan McPherson <jonathan@rstudio.com>
2022-09-06 17:37:52 -0500

    mirror/wrap external API types internally

--- src/vs/workbench/contrib/languageRuntime/common/languageRuntimeService.ts
+++ src/vs/workbench/contrib/languageRuntime/common/languageRuntimeService.ts
@@ -4,9 +4,80 @@
 
 import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
 import { INotebookKernel } from 'vs/workbench/contrib/notebook/common/notebookKernelService';
-import { Event } from 'vs/base/common/event';
+import { Emitter, Event } from 'vs/base/common/event';
 
 export const ILanguageRuntimeService = createDecorator<ILanguageRuntimeService>('ILanguageRuntimeService');
+
+/**
+ * LanguageRuntimeMessage is an interface that defines an event occurring in a
+ * language runtime, such as outputting text or plots.
+ */
+export interface ILanguageRuntimeMessage {
+	/** The event ID */
+	id: string;
+
+	/** The ID of this event's parent (the event that caused it), if applicable */
+	parent_id: string;
+}
+
+/** LanguageRuntimeOutput is a LanguageRuntimeMessage representing output (text, plots, etc.) */
+export interface ILanguageRuntimeOutput extends ILanguageRuntimeMessage {
+	/** A map of data MIME types to the associated data, e.g. `text/plain` => `'hello world'` */
+	data: Map<string, string>;
+}
+
+export enum RuntimeOnlineState {
+	/** The runtime is starting up */
+	Starting = 'starting',
+
+	/** The runtime is currently processing an instruction or code fragment */
+	Busy = 'busy',
+
+	/** The runtime is idle */
+	Idle = 'idle',
+}
+
+export interface ILanguageRuntimeState extends ILanguageRuntimeMessage {
+	/** The new state */
+	state: RuntimeOnlineState;
+}
+
+export interface ILanguageRuntimeError extends ILanguageRuntimeMessage {
+	/** The error name */
+	name: string;
+
+	/** The error message */
+	message: string;
+
+	/** The error stack trace */
+	traceback: Array<string>;
+}
+
+export interface ILanguageRuntime {
+	/** The language identifier for this runtime. */
+	language: string;
+
+	/** The name of the runtime. */
+	name: string;
+
+	/** The version of the runtime. */
+	version: string;
+
+	/** An object that emits language runtime events */
+	messages: Emitter<ILanguageRuntimeMessage>;
+
+	/** Execute code in the runtime; returns the ID of the code execution. */
+	execute(code: string): Thenable<string>;
+
+	/** Interrupt the runtime */
+	interrupt(): void;
+
+	/** Restart the runtime */
+	restart(): void;
+
+	/** Shut down the runtime */
+	shutdown(): void;
+}
 
 export interface ILanguageRuntimeService {
 	readonly _serviceBrand: undefined;
