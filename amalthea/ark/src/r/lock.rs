877d493d3734e73da8d06ad2dc032257c9ce9869
Kevin Ushey <kevinushey@gmail.com>
2022-09-16 13:17:21 -0700

    add some debug logging around locks

--- amalthea/ark/src/r/lock.rs
+++ amalthea/ark/src/r/lock.rs
@@ -16,8 +16,6 @@
 use parking_lot::ReentrantMutex;
 use parking_lot::ReentrantMutexGuard;
 
-use crate::lsp::logger::dlog;
-
 extern "C" {
     pub static mut R_PolledEvents: Option<unsafe extern "C" fn()>;
 }
@@ -41,45 +39,63 @@ pub static mut R_RUNTIME_LOCK_GUARD: Option<ReentrantMutexGuard<()>> = None;
 // yield control to the child thread requesting access.
 pub static mut R_RUNTIME_TASKS_PENDING: bool = false;
 
+pub fn with_r_lock<T, F: FnMut() -> T>(mut callback: F) -> T {
+
+    // Let the logger know we're taking the lock.
+    let id = std::thread::current().id();
+    dlog!("Thread {:?} is requesting R runtime lock.", id);
+
+    // Record how long it takes the acquire the lock.
+    let now = std::time::SystemTime::now();
+
+    // Let the main thread know tasks are pending.
+    unsafe { R_RUNTIME_TASKS_PENDING = true };
+
+    // Wait until we can get the runtime lock.
+    unsafe { R_RUNTIME_LOCK_GUARD = Some(R_RUNTIME_LOCK.lock()) };
+
+    // Log how long we were stuck waiting.
+    let elapsed = now.elapsed().unwrap().as_millis();
+    dlog!("Thread {:?} obtained lock after waiting for {} milliseconds.", id, elapsed);
+
+    // Disable polled events in this scope.
+    let polled_events = unsafe { R_PolledEvents };
+    unsafe { R_PolledEvents = Some(r_polled_events_disabled) };
+
+    // Execute the callback.
+    let now = std::time::SystemTime::now();
+    let result = callback();
+
+    // Restore the polled events handler.
+    unsafe { R_PolledEvents = polled_events };
+
+    // Release the runtime lock.
+    unsafe { R_RUNTIME_LOCK_GUARD = None };
+
+    // Tasks are no longer pending.
+    unsafe { R_RUNTIME_TASKS_PENDING = false };
+
+    // Let the logger know we've released the lock..
+    let elapsed = now.elapsed().unwrap().as_millis();
+    dlog!("Thread {:?} has released the R runtime lock after {} milliseconds.", id, elapsed);
+
+    // Return the resulting expression.
+    result
+
+
+}
+
 macro_rules! r_lock {
 
     ($($expr:tt)*) => {{
 
-        // Let the logger know we're taking the lock.
-        dlog!("Thread {:?} is requesting R runtime lock.", std::thread::current().id());
-        let now = std::time::SystemTime::now();
-
-        // Let the main thread know tasks are pending.
-        unsafe { $crate::r::lock::R_RUNTIME_TASKS_PENDING = true };
-
-        // Wait until we can get the runtime lock.
-        unsafe { $crate::r::lock::R_RUNTIME_LOCK_GUARD = Some($crate::r::lock::R_RUNTIME_LOCK.lock()) };
-
-        dlog!("Thread {:?} obtained lock after waiting for {} milliseconds.", std::thread::current().id(), now.elapsed().unwrap().as_millis());
-
-        // Disable polled events in this scope.
-        let polled_events = unsafe { $crate::r::lock::R_PolledEvents };
-        unsafe { $crate::r::lock::R_PolledEvents = Some($crate::r::lock::r_polled_events_disabled) };
-
-        // Execute the wrapped code.
-        let result = unsafe { $($expr)* };
-
-        // Restore the polled events handler.
-        unsafe { $crate::r::lock::R_PolledEvents = polled_events };
-
-        // Release the runtime lock.
-        unsafe { $crate::r::lock::R_RUNTIME_LOCK_GUARD = None };
-
-        // Tasks are no longer pending.
-        unsafe { $crate::r::lock::R_RUNTIME_TASKS_PENDING = false };
-
-        // Let the logger know we've released the lock..
-        dlog!("Thread {:?} has released the R runtime lock.", std::thread::current().id());
-
-        // Return the resulting expression.
-        result
+        $crate::r::lock::with_r_lock(|| {
+            unsafe { $($expr)* }
+        })
 
     }}
 
 }
 pub(crate) use r_lock;
+
+use crate::lsp::logger::dlog;
