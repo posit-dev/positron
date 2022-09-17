5bdd08ebd8250d0a0b7ea5383f19fe5bcc86f9c9
Brian Lambert <brianlambert@gmail.com>
2022-09-17 15:21:26 -0500

    Work on the modal dialog service

---
+++ src/vs/platform/modalDialogs/browser/modalDialogs.ts
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) RStudio, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import { IModalDialogsService } from 'vs/platform/modalDialogs/common/modalDialogs';
+import { ModalDialog } from 'vs/base/browser/ui/modalDialog/modalDialog';
+import { ILayoutService } from 'vs/platform/layout/browser/layoutService';
+
+/**
+ * ModalDialogs class.
+ */
+export class ModalDialogs implements IModalDialogsService {
+
+	declare readonly _serviceBrand: undefined;
+
+	/**
+	 * Initializes a new instance of the ModalDialogs class.
+	 * @param layoutService The layout service.
+	 */
+	constructor(
+		@ILayoutService private readonly layoutService: ILayoutService,
+	) {
+	}
+
+	/**
+	 * Shows the time modal dialog.
+	 */
+	async showTimeModalDialog(): Promise<void> {
+
+		console.log('We are here');
+		const ddd = new ModalDialog(this.layoutService.container);
+		await ddd.show();
+	}
+}
