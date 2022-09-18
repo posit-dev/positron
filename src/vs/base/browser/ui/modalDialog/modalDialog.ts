96f8e5981c37f56e4c74545b728af45de1ad91ab
Brian Lambert <brianlambert@gmail.com>
2022-09-17 21:04:47 -0500

    Work on modal dialogs

--- src/vs/base/browser/ui/modalDialog/modalDialog.ts
+++ src/vs/base/browser/ui/modalDialog/modalDialog.ts
@@ -7,14 +7,27 @@ import * as DOM from 'vs/base/browser/dom';
 import { Disposable } from 'vs/base/common/lifecycle';
 
 /**
+ * IModalDialogOptions interface.
+ */
+export interface IModalDialogOptions {
+	readonly title: string;
+}
+
+/**
  * ModalDialog class.
  */
 export class ModalDialog extends Disposable {
 	//#region Private Member Variables
 
-	private readonly element: HTMLElement;
-	private readonly shadowElement: HTMLElement;
-	private modalElement: HTMLElement | undefined;
+	private modalBlockElement: HTMLElement | undefined;
+	private readonly shadowContainerElement: HTMLElement;
+	private readonly dialogBoxElement: HTMLElement;
+	private readonly dialogBoxBodyElement: HTMLElement;
+	private readonly titleBarElement: HTMLElement;
+	private readonly titleElement: HTMLElement;
+	private readonly actionsBarElement: HTMLElement;
+
+	private readonly okButtonElement: HTMLElement | undefined;
 
 	//#endregion Private Member Variables
 
@@ -24,26 +37,69 @@ export class ModalDialog extends Disposable {
 	 * Initializes a new instance of the ModalDialog class.
 	 * @param container The HTMLElement that contains the ModalDialog.
 	 */
-	constructor(private container: HTMLElement) {
+	constructor(private container: HTMLElement, modalDialogOptions: IModalDialogOptions) {
+		// Initialize.
 		super();
 
-		this.modalElement = this.container.appendChild(DOM.$(`.monaco-modal-dialog-modal-block.dimmed`));
-		this.shadowElement = this.modalElement.appendChild(DOM.$('.modal-dialog-shadow'));
+		// Create the basic dialog box.
+		this.modalBlockElement = this.container.appendChild(DOM.$(`.monaco-modal-dialog-modal-block.dimmed`));
+		this.shadowContainerElement = this.modalBlockElement.appendChild(DOM.$('.modal-dialog-shadow-container'));
+		this.dialogBoxElement = this.shadowContainerElement.appendChild(DOM.$('.monaco-modal-dialog-box'));
+		this.dialogBoxElement.setAttribute('role', 'dialog');
+		this.dialogBoxElement.tabIndex = -1;
 
-		this.element = this.shadowElement.appendChild(DOM.$('.monaco-modal-dialog-box'));
-		this.element.setAttribute('role', 'dialog');
-		this.element.tabIndex = -1;
+		// Create the dialog box body.
+		this.dialogBoxBodyElement = this.dialogBoxElement.appendChild(DOM.$('.monaco-modal-dialog-box-body'));
 
-		const content = this.element.appendChild(DOM.$('.monaco-modal-dialog-box-content'));
-		content.innerText = 'This is a modal dialog box.';
+		// Create the title bar and title.
+		this.titleBarElement = this.dialogBoxBodyElement.appendChild(DOM.$('.title-bar'));
+		this.titleElement = this.titleBarElement.appendChild(DOM.$('.title-bar-title'));
+		this.titleElement.innerText = modalDialogOptions.title;
 
-		DOM.hide(this.element);
+		// Create the actions bar and actions.
+		this.actionsBarElement = this.dialogBoxBodyElement.appendChild(DOM.$('.actions-bar.top'));
+
+		this.okButtonElement = DOM.$('a');
+		this.okButtonElement = document.createElement('a');
+		this.okButtonElement.classList.add('push-button');
+		this.okButtonElement.tabIndex = 0;
+		this.okButtonElement.setAttribute('role', 'button');
+		this.okButtonElement.innerText = 'OK';
+		this.okButtonElement.onclick = () => {
+			console.log('YAYA!');
+		};
+		this.actionsBarElement.appendChild(this.okButtonElement);
+
+		// Placeholder.
+		const divie = this.dialogBoxBodyElement.appendChild(DOM.$('.foo'));
+		divie.innerText = 'This is a modal dialog box.';
+
+		// Hide the dialog box element until it's shown.
+		DOM.hide(this.dialogBoxElement);
 	}
 
+	/**
+	 * Disposes the modal dialog box.
+	 */
+	override dispose(): void {
+		super.dispose();
+
+		if (this.modalBlockElement) {
+			this.modalBlockElement.remove();
+			this.modalBlockElement = undefined;
+		}
+
+		// Return focus to focusToReturn...
+	}
+
+	/**
+	 * Shows the modal dialog box.
+	 */
 	async show(): Promise<void> {
 		//const focusToReturn = document.activeElement as HTMLElement;
 
-		DOM.show(this.element);
+		DOM.show(this.dialogBoxElement);
+		this.dialogBoxElement.focus();
 	}
 
 	//#endregion Class Initialization
