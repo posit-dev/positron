a7fa0cd8530f0af11f2659df8dc606f5e890efb9
Brian Lambert <brianlambert@gmail.com>
2022-09-19 19:44:27 -0400

    Updates

---
+++ src/vs/base/browser/ui/components/header.tsx
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) RStudio, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as React from 'react';
+
+// Header component.
+const Header = () => {
+	// Render.
+	return (
+		<>
+			<div className='ss'>
+				This is SPARTA!
+			</div>
+		</>
+	);
+};
+
+// Export the Header component.
+export default Header;
