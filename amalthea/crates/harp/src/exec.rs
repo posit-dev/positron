d1ad7693a0298ca9d264df7e7cff0d33b84990f0
Kevin Ushey <kevinushey@gmail.com>
2022-09-23 22:05:10 -0700

    tweaks to locking

--- amalthea/crates/harp/src/exec.rs
+++ amalthea/crates/harp/src/exec.rs
@@ -142,15 +142,16 @@ pub unsafe fn geterrmessage() -> String {
 #[cfg(test)]
 mod tests {
 
+    use std::io::Write;
+
     use crate::r_lock;
-    use crate::test::start_r;
+    use crate::r_test;
+    use crate::r_test_unlocked;
 
     use super::*;
 
     #[test]
-    fn test_basic_function() { unsafe {
-
-        start_r();
+    fn test_basic_function() { r_test! {
 
         // try adding some numbers
         let result = RFunction::new("", "+")
@@ -163,12 +164,10 @@ mod tests {
         assert!(Rf_isInteger(*result) != 0);
         assert!(Rf_asInteger(*result) == 4);
 
-    } }
+    }}
 
     #[test]
-    fn test_utf8_strings() { unsafe {
-
-        start_r();
+    fn test_utf8_strings() { r_test! {
 
         // try sending some UTF-8 strings to and from R
         let result = RFunction::new("base", "paste")
@@ -188,9 +187,7 @@ mod tests {
     }}
 
     #[test]
-    fn test_named_arguments() { unsafe {
-
-        start_r();
+    fn test_named_arguments() { r_test! {
 
         let result = RFunction::new("stats", "rnorm")
             .add(1.0)
@@ -205,21 +202,28 @@ mod tests {
     }}
 
     #[test]
-    fn test_threads() {
-
-        const N : i32 = 1000000;
-        start_r();
+    fn test_threads() { r_test_unlocked! {
 
         // Spawn a bunch of threads that try to interact with R.
+        const N : i32 = 1000;
         let mut handles : Vec<_> = Vec::new();
-        for _i in 1..100 {
+        for _i in 1..20 {
             let handle = std::thread::spawn(move || {
-                for _j in 1..10 {
+                let id = std::thread::current().id();
+                for _j in 1..20 {
                     r_lock! {
+                        println!("Thread {:?} acquiring R lock.", id);
+                        std::io::stdout().flush().unwrap();
                         let mut protect = RProtect::new();
                         let code = protect.add(Rf_lang2(r_symbol!("rnorm"), Rf_ScalarInteger(N)));
-                        let result = Rf_eval(code, R_GlobalEnv);
+                        println!("Thread {:?} about to evaluate R code.", id);
+                        std::io::stdout().flush().unwrap();
+                        let result = protect.add(Rf_eval(code, R_GlobalEnv));
+                        println!("Thread {:?} finished evaluating R code.", id);
+                        std::io::stdout().flush().unwrap();
                         assert!(Rf_length(result) == N);
+                        println!("Thread {:?} releasing R lock.", std::thread::current().id());
+                        std::io::stdout().flush().unwrap();
                     };
                 }
             });
@@ -230,7 +234,7 @@ mod tests {
             handle.join().unwrap();
         }
 
-    }
+    }}
 
 }
 
