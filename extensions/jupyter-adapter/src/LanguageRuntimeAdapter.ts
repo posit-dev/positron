b10b1ca1439384dd9852d6e67f4b0f9eb3d7bd7d
Jonathan McPherson <jonathan@rstudio.com>
2022-09-27 09:48:57 -0700

    display_data marshal

--- extensions/jupyter-adapter/src/LanguageRuntimeAdapter.ts
+++ extensions/jupyter-adapter/src/LanguageRuntimeAdapter.ts
@@ -5,6 +5,8 @@
 import * as vscode from 'vscode';
 import { JupyterKernel } from './JupyterKernel';
 import { JupyterKernelSpec } from './JupyterKernelSpec';
+import { JupyterMessagePacket } from './JupyterMessagePacket';
+import { JupyterDisplayData } from './JupyterDisplayData';
 
 /**
  * LangaugeRuntimeAdapter wraps a JupyterKernel in a LanguageRuntime compatible interface.
@@ -18,10 +20,16 @@ export class LanguageRuntimeAdapter
 		this._kernel = new JupyterKernel(this._spec);
 		this.language = this._spec.language;
 		this.name = this._spec.display_name;
+
 		// TODO
 		this.version = '';
 		this.id = '';
+
+		// Create emitter for LanguageRuntime messages
 		this.messages = new vscode.EventEmitter<vscode.LanguageRuntimeMessage>();
+
+		// Bind to message stream from kernel
+		this._kernel.addListener('message', this.onMessage);
 	}
 
 	id: string;
@@ -41,6 +49,31 @@ export class LanguageRuntimeAdapter
 	}
 	shutdown(): void {
 		this._kernel.shutdown(false);
+	}
+
+	onMessage(msg: JupyterMessagePacket) {
+		const message = msg.message;
+		switch (msg.msgType) {
+			case 'display_data':
+				this.onDisplayData(msg, message as JupyterDisplayData);
+				break;
+		}
+	}
+
+	/**
+	 * Converts a Jupyter display_data message to a LanguageRuntimeMessage and
+	 * emits it.
+	 *
+	 * @param message The message packet
+	 * @param data The display_data message
+	 */
+	onDisplayData(message: JupyterMessagePacket, data: JupyterDisplayData) {
+		this.messages.fire({
+			id: message.msgId,
+			parent_id: message.originId,
+			type: vscode.LanguageRuntimeMessageType.Output,
+			data: data.data as any
+		} as vscode.LanguageRuntimeOutput);
 	}
 
 	dispose() {
