ca9b715b280fe23d45aac121fc907b6f8f986557
Jonathan McPherson <jonathan@rstudio.com>
2022-09-29 16:36:39 -0700

    read kernel path from setting (WIP)

--- amalthea/crates/ark/extension/src/extension.ts
+++ amalthea/crates/ark/extension/src/extension.ts
@@ -29,6 +29,23 @@ export function activate(context: vscode.ExtensionContext) {
     let ext = vscode.extensions.getExtension("posit.jupyter-adapter");
     if (ext) {
         // We're in Positron, so need to create a language runtime.
+
+        // Read the ark.kernel.path setting to determine the path to the
+        // R kernel executable.
+        let kernelPath = vscode.workspace.getConfiguration("ark").get("kernel.path");
+        if (kernelPath) {
+            // We have a kernel path; use the VS Code file system API to see if it exists on disk.
+            let fs = require('fs');
+            if (fs.existsSync(kernelPath)) {
+                return startArkKernel(ext, context, kernelPath);
+            } else {
+                vscode.window.showErrorMessage("ARK kernel path specified in 'ark.kernel.path' setting does not exist: " + kernelPath);
+                return;
+            }
+        } else {
+            // No kernel path specified; show an error message.
+            vscode.window.showErrorMessage("No path to the ARK kernel set. Please set the ark.kernel.path setting.");
+        }
         // TODO: This needs to pass a JupyterKernelSpec describing the location
         // of the R kernel.
         return ext.exports.adaptKernel();
@@ -128,4 +145,28 @@ export function deactivate(): Thenable<void> | undefined {
 		return undefined;
 	}
 	return client.stop();
+}
+
+export function startArkKernel(ext: vscode.Extension<any>,
+    context: vscode.ExtensionContext,
+    kernelPath: string): vscode.Disposable {
+
+    let kernelSpec = {
+        "argv": [ kernelPath, "--connection_file", "{connection_file}" ],
+        "display_name": "Amalthea R Kernel (ARK)", // eslint-disable-line
+        "language": "R",
+        "env": {
+            "RUST_LOG": "trace", // eslint-disable-line
+            "R_HOME": "/Library/Frameworks/R.framework/Resources" // eslint-disable-line
+        }
+    };
+
+    // Create an adapter for the kernel to fulfill the LanguageRuntime interface.
+    let kernel = ext.exports.adaptKernel(
+        kernelSpec,
+        true // has embedded LSP
+    );
+
+    // Register a language runtime provider for the ARK kernel.
+    return vscode.myriac.registerLanguageRuntime(kernel);
 }
