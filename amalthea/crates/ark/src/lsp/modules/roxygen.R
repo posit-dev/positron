c6c3dbf2c31d1956b43d6fbb70b8cd04cc9df4a9
Kevin Ushey <kevinushey@gmail.com>
2022-10-03 14:11:35 -0700

    remove unused stub code

--- amalthea/crates/ark/src/lsp/modules/roxygen.R
+++ amalthea/crates/ark/src/lsp/modules/roxygen.R
@@ -4,13 +4,3 @@
 # Copyright (C) 2022 by RStudio, PBC
 #
 #
-
-.rs.roxygen.tagList <- function() {
-
-    # check for roxygen2 tags to be used
-    tagList <- system.file("roxygen2-tags.yml", package = "roxygen2")
-    if (file.exists(tagList)) {
-
-    }
-    c("@param")
-}
