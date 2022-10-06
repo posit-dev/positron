a469c6fffd27454804f5cbf6b2d716a7cf9133f7
Jonathan McPherson <jonathan@rstudio.com>
2022-10-06 16:35:00 -0700

    default to heartbeat every 1s, not no heartbeat

--- extensions/jupyter-adapter/src/JupyterKernel.ts
+++ extensions/jupyter-adapter/src/JupyterKernel.ts
@@ -185,16 +185,12 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 				this._channel.appendLine('Receieved initial heartbeat: ' + msg);
 				this.setStatus(vscode.RuntimeState.Ready);
 
-				const seconds = vscode.workspace.getConfiguration('myriac').get('heartbeat') as number;
-				if (seconds > 0) {
-					this._channel.appendLine(`Starting heartbeat check at ${seconds} second intervals...`);
-					this.heartbeat();
-					this._heartbeat?.socket().on('message', (msg: string) => {
-						this.onHeartbeat(msg);
-					});
-				} else {
-					this._channel.appendLine(`Heartbeat check disabled via configuration.`);
-				}
+				const seconds = vscode.workspace.getConfiguration('positron').get('heartbeat', 1) as number;
+				this._channel.appendLine(`Starting heartbeat check at ${seconds} second intervals...`);
+				this.heartbeat();
+				this._heartbeat?.socket().on('message', (msg: string) => {
+					this.onHeartbeat(msg);
+				});
 			});
 			this._heartbeat?.socket().send(['hello']);
 		});
@@ -282,7 +278,7 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 		this.shutdown(true);
 
 		// Start the kernel again once the process finishes shutting down
-		this._process?.on('exit', () => {
+		this._process?.once('exit', () => {
 			this._channel.appendLine(`Waiting for '${this._spec.display_name}' to restart...`);
 			// Start the kernel again, rebinding to the LSP client if we have
 			// one
@@ -499,7 +495,7 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 	 * Emits a heartbeat message and waits for the kernel to respond.
 	 */
 	private heartbeat() {
-		const seconds = vscode.workspace.getConfiguration('myriac').get('heartbeat') as number;
+		const seconds = vscode.workspace.getConfiguration('positron').get('heartbeat', 1) as number;
 		this._lastHeartbeat = new Date().getUTCMilliseconds();
 		this._channel.appendLine(`SEND heartbeat`);
 		this._heartbeat?.socket().send(['hello']);
@@ -529,7 +525,7 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 		}
 
 		// Schedule the next heartbeat at the configured interval
-		const seconds = vscode.workspace.getConfiguration('myriac').get('heartbeat') as number;
+		const seconds = vscode.workspace.getConfiguration('positron').get('heartbeat', 1) as number;
 		setTimeout(() => {
 			this.heartbeat();
 		}, seconds * 1000);
