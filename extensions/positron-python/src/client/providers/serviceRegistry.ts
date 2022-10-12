6b3d2b528cf6cd9ae0e09c399e6a5a5ad1063d97
Karthik Nadig <kanadig@microsoft.com>
2022-10-12 17:50:24 -0500

    Use isort extension instead of isort. (microsoft/vscode-python#19997)

--- extensions/positron-python/src/client/providers/serviceRegistry.ts
+++ extensions/positron-python/src/client/providers/serviceRegistry.ts
@@ -6,11 +6,8 @@
 import { IExtensionSingleActivationService } from '../activation/types';
 import { IServiceManager } from '../ioc/types';
 import { CodeActionProviderService } from './codeActionProvider/main';
-import { SortImportsEditingProvider } from './importSortProvider';
-import { ISortImportsEditingProvider } from './types';
 
 export function registerTypes(serviceManager: IServiceManager): void {
-    serviceManager.addSingleton<ISortImportsEditingProvider>(ISortImportsEditingProvider, SortImportsEditingProvider);
     serviceManager.addSingleton<IExtensionSingleActivationService>(
         IExtensionSingleActivationService,
         CodeActionProviderService,
