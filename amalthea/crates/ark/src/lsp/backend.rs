da85f8715ec9e652b7292dc01429d6cc5ea50a70
Kevin Ushey <kevinushey@gmail.com>
2022-10-13 18:56:14 -0500

    skeleton for goto_definition implementation

--- amalthea/crates/ark/src/lsp/backend.rs
+++ amalthea/crates/ark/src/lsp/backend.rs
@@ -7,9 +7,9 @@
 
 use std::collections::HashSet;
 use std::path::Path;
+use std::sync::mpsc::SyncSender;
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::sync::mpsc::SyncSender;
 
 use dashmap::DashMap;
 use harp::r_lock;
@@ -20,12 +20,15 @@ use stdext::*;
 use tokio::net::TcpStream;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
+use tower_lsp::lsp_types::request::GotoImplementationParams;
+use tower_lsp::lsp_types::request::GotoImplementationResponse;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+use crate::lsp::completions::append_document_completions;
 use crate::lsp::completions::append_session_completions;
 use crate::lsp::completions::can_provide_completions;
-use crate::lsp::completions::append_document_completions;
 use crate::lsp::completions::completion_context;
+use crate::lsp::definitions::goto_definition_context;
 use crate::lsp::document::Document;
 use crate::lsp::modules;
 use crate::request::Request;
@@ -45,11 +48,11 @@ pub(crate) struct Workspace {
 }
 
 impl Default for Workspace {
-
     fn default() -> Self {
-        Self { folders: Default::default() }
+        Self {
+            folders: Default::default(),
+        }
     }
-
 }
 
 #[derive(Debug)]
@@ -62,22 +65,22 @@ pub(crate) struct Backend {
 }
 
 impl Backend {
-
-    pub(crate) fn with_document<T, F>(&self, path: &Path, mut callback: F) -> std::result::Result<T, ()>
+    pub(crate) fn with_document<T, F>(
+        &self,
+        path: &Path,
+        mut callback: F,
+    ) -> std::result::Result<T, ()>
     where
-        F: FnMut(&Document) -> std::result::Result<T, ()>
+        F: FnMut(&Document) -> std::result::Result<T, ()>,
     {
         let mut fallback = || {
-
             let contents = unwrap!(std::fs::read_to_string(path), {
                 info!("reading from {:?} failed", path);
                 return Err(());
             });
 
-
             let document = Document::new(contents.as_str());
             return callback(&document);
-
         };
 
         // If we have a cached copy of the document (because we're monitoring it)
@@ -88,28 +91,23 @@ impl Backend {
             return fallback();
         });
 
-
         let document = unwrap!(self.documents.get(&uri), {
             info!("no document for uri {:?}; using fallback", uri);
             return fallback();
         });
 
         return callback(document.value());
-
     }
-
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, params: InitializeParams) -> Result<InitializeResult> {
-
         backend_trace!(self, "initialize({:#?})", params);
 
         // initialize the set of known workspaces
         let mut folders: Vec<String> = Vec::new();
         if let Ok(mut workspace) = self.workspace.lock() {
-
             // initialize the workspace folders
             if let Some(workspace_folders) = params.workspace_folders {
                 for folder in workspace_folders.iter() {
@@ -121,7 +119,6 @@ impl LanguageServer for Backend {
                     }
                 }
             }
-
         }
 
         // initialize our support functions
@@ -150,9 +147,9 @@ impl LanguageServer for Backend {
                     ..Default::default()
                 }),
                 signature_help_provider: None,
-                definition_provider: None,
+                definition_provider: Some(OneOf::Left(true)),
                 type_definition_provider: None,
-                implementation_provider: None,
+                implementation_provider: Some(ImplementationProviderCapability::Simple(true)),
                 references_provider: Some(OneOf::Left(true)),
                 execute_command_provider: Some(ExecuteCommandOptions {
                     commands: vec!["dummy.do_something".to_string()],
@@ -210,7 +207,6 @@ impl LanguageServer for Backend {
             params.text_document.uri,
             Document::new(params.text_document.text.as_str()),
         );
-
     }
 
     async fn did_change(&self, params: DidChangeTextDocumentParams) {
@@ -229,7 +225,6 @@ impl LanguageServer for Backend {
                 backend_trace!(self, "doc.update(): unexpected error {}", error);
             }
         }
-
     }
 
     async fn did_save(&self, params: DidSaveTextDocumentParams) {
@@ -279,7 +274,7 @@ impl LanguageServer for Backend {
         info!("Completion token at point {:?}: '{}'", context.point, context.token);
 
         // start building completions
-        let mut completions : Vec<CompletionItem> = vec!();
+        let mut completions: Vec<CompletionItem> = vec![];
 
         // add session completions
         let result = append_session_completions(&context, &mut completions);
@@ -295,7 +290,7 @@ impl LanguageServer for Backend {
 
         // remove duplicates
         let mut uniques = HashSet::new();
-        completions.retain(|x| { uniques.insert(x.label.clone()) });
+        completions.retain(|x| uniques.insert(x.label.clone()));
 
         // sort completions by providing custom 'sort' text to be used when
         // ordering completion results. we use some placeholders at the front
@@ -318,7 +313,6 @@ impl LanguageServer for Backend {
         } else {
             Ok(None)
         }
-
     }
 
     // TODO: Use completion_resolve() to provide extra information about a completion.
@@ -338,12 +332,56 @@ impl LanguageServer for Backend {
         }))
     }
 
+    async fn goto_definition(&self, params: GotoDefinitionParams) -> Result<Option<GotoDefinitionResponse>> {
+        backend_trace!(self, "goto_definition({:?})", params);
+
+        // get reference to document
+        let uri = &params.text_document_position_params.text_document.uri;
+        let mut document = unwrap!(self.documents.get_mut(uri), {
+            backend_trace!(self, "completion(): No document associated with URI {}", uri);
+            return Ok(None);
+        });
+
+        // build goto definition context
+        let context = unwrap!(goto_definition_context(&mut document, params), error {
+            error!("{}", error);
+            return Ok(None);
+        });
+
+        // TODO: We should see if we can find the referenced item in:
+        //
+        // 1. The document's current AST,
+        // 2. The public functions from other documents in the project,
+        // 3. A definition in the R session (which we could open in a virtual document)
+        //
+        // If we can't find a definition, then we can return the referenced item itself,
+        // which will tell Positron to instead try to look for references for that symbol.
+        let link = LocationLink {
+            origin_selection_range: Some(context.range),
+            target_uri: context.params.text_document_position_params.text_document.uri,
+            target_range: context.range,
+            target_selection_range: context.range,
+        };
+
+        let response = GotoDefinitionResponse::Link(vec![link]);
+        Ok(Some(response))
+    }
+
+    async fn goto_implementation(&self, params: GotoImplementationParams) -> Result<Option<GotoImplementationResponse>> {
+        backend_trace!(self, "goto_implementation({:?})", params);
+        let _ = params;
+        error!("Got a textDocument/implementation request, but it is not implemented");
+        return Ok(None);
+    }
+
     async fn references(&self, params: ReferenceParams) -> Result<Option<Vec<Location>>> {
         backend_trace!(self, "references({:?})", params);
 
         let locations = match self.find_references(params) {
             Ok(locations) => locations,
-            Err(_error) => { return Ok(None); }
+            Err(_error) => {
+                return Ok(None);
+            }
         };
 
         if locations.is_empty() {
@@ -356,7 +394,6 @@ impl LanguageServer for Backend {
 
 #[tokio::main]
 pub async fn start_lsp(address: String, channel: SyncSender<Request>) {
-
     #[cfg(feature = "runtime-agnostic")]
     use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
 
