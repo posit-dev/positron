7aa63c026fb4efb73ac46bd6e33363babaac1014
Brian Lambert <brianlambert@gmail.com>
2022-10-14 14:03:25 -0500

    Refactoring

--- src/vs/workbench/contrib/positronModalDialogs/browser/positronModalDialogs.contribution.ts
+++ src/vs/workbench/contrib/positronModalDialogs/browser/positronModalDialogs.contribution.ts
@@ -3,7 +3,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { InstantiationType, registerSingleton } from 'vs/platform/instantiation/common/extensions';
-import { PositronModalDialogs } from 'vs/platform/positronModalDialogs/browser/positronModalDialogs';
-import { IPositronModalDialogsService } from 'vs/platform/positronModalDialogs/common/positronModalDialogs';
+import { PositronModalDialogs } from 'vs/workbench/contrib/positronModalDialogs/browser/positronModalDialogs';
+import { IPositronModalDialogsService } from 'vs/workbench/services/positronModalDialogs/browser/positronModalDialogs';
 
 registerSingleton(IPositronModalDialogsService, PositronModalDialogs, InstantiationType.Delayed);
---
+++ src/vs/workbench/contrib/positronModalDialogs/common/positronModalDialogs.ts
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Posit, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+// import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+
+// /**
+//  * Export the service identifier.
+//  */
+// export const IPositronModalDialogsService = createDecorator<IPositronModalDialogsService>('positronModalDialogsService');
+
+// /**
+//  * A service that displays modal dialogs.
+//  */
+// export interface IPositronModalDialogsService {
+
+// 	readonly _serviceBrand: undefined;
+
+// 	/**
+// 	 * Shows example modal dialog 1.
+// 	 */
+// 	showExampleModalDialog1(title: string): Promise<void>;
+
+// 	/**
+// 	 * Shows example modal dialog 2.
+// 	 */
+// 	showExampleModalDialog2(title: string): Promise<boolean>;
+// }
