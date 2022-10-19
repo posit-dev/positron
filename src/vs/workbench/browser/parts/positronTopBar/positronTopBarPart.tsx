338b9028534158c5764a110517639764ece71ccd
Brian Lambert <brianlambert@gmail.com>
2022-10-19 13:23:04 -0400

    Top bar button work

--- src/vs/workbench/browser/parts/positronTopBar/positronTopBarPart.tsx
+++ src/vs/workbench/browser/parts/positronTopBar/positronTopBarPart.tsx
@@ -4,35 +4,20 @@
 
 import 'vs/css!./media/css/positronTopBarPart';
 const React = require('react');
-// import { localize } from 'vs/nls';
-// import * as DOM from 'vs/base/browser/dom';
 import { Emitter } from 'vs/base/common/event';
 import { Part } from 'vs/workbench/browser/part';
 import { KeyCode } from 'vs/base/common/keyCodes';
-// import { IAction } from 'vs/base/common/actions';
-import { POSITRON_TOP_BAR_BACKGROUND } from 'vs/workbench/common/theme';
 import { TopBarFocused } from 'vs/workbench/common/contextkeys';
 import { IStorageService } from 'vs/platform/storage/common/storage';
 import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
 import { PositronReactRenderer } from 'vs/base/browser/positronReactRenderer';
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
-// import { BaseActionViewItem } from 'vs/base/browser/ui/actionbar/actionViewItems';
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
-// import { ActionBar, ActionsOrientation } from 'vs/base/browser/ui/actionbar/actionbar';
-import { IThemeService, registerThemingParticipant } from 'vs/platform/theme/common/themeService';
+import { IThemeService } from 'vs/platform/theme/common/themeService';
 import { IWorkbenchLayoutService, Parts } from 'vs/workbench/services/layout/browser/layoutService';
 import { KeybindingsRegistry, KeybindingWeight } from 'vs/platform/keybinding/common/keybindingsRegistry';
 import { IPositronTopBarService } from 'vs/workbench/services/positronTopBar/browser/positronTopBarService';
 import { PositronTopBarComponent } from 'vs/workbench/browser/parts/positronTopBar/positronTopBarComponent';
-
-// Theme support
-
-registerThemingParticipant((theme, collector) => {
-	const backgroundColor = theme.getColor(POSITRON_TOP_BAR_BACKGROUND);
-	if (backgroundColor) {
-		collector.addRule(`.monaco-workbench .part.positron-top-bar { background-color: ${backgroundColor}; }`);
-	}
-});
 
 /**
  * PositronTopBarPart class.
