ddbb402c28bfe2b48d60aaf484a396c5f2f8ebe3
Brian Lambert <brianlambert@gmail.com>
2022-10-19 14:58:03 -0500

    Updates

--- src/vs/workbench/browser/parts/positronTopBar/components/buttonComponent/buttonComponent.tsx
+++ src/vs/workbench/browser/parts/positronTopBar/components/buttonComponent/buttonComponent.tsx
@@ -2,6 +2,7 @@
  *  Copyright (c) Posit, PBC.
  *--------------------------------------------------------------------------------------------*/
 
+import { useState } from 'react';
 import 'vs/css!./css/buttonComponent';
 const React = require('react');
 
@@ -19,16 +20,31 @@ interface ButtonComponentProps {
  * @returns The component.
  */
 export const ButtonComponent = (props: ButtonComponentProps) => {
+	// Hooks.
+	const [hover, setHover] = useState(false);
+
 	const clickHandler = () => {
 		console.log('Button was clicked');
 	};
 
+	const mouseEnterHandler = () => {
+		console.log('Mouse is inside button!');
+		setHover(true);
+	};
+
+	const mouseLeaveHandler = () => {
+		console.log('Mouse is outside button!');
+		setHover(false);
+	};
+
+
 	// Render.
 	return (
-		<div className='button-component' onClick={clickHandler}>
+		<div className='button-component' onClick={clickHandler} onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
 			<div className='button-face'>
 				<div className={`button-background ${props.classNameBackground}`} />
 				{props.dropDown && <div className='button-drop-down-arrow' />}
+				{hover && <div className='toolie'>Hello</div>}
 			</div>
 		</div>
 	);
