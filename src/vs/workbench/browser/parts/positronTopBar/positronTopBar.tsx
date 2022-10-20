fc8974816a339fe117a7422543f6f57b4db404ad
JJ Allaire <jj.allaire@gmail.com>
2022-10-20 10:39:27 -0500

    initial work on command center

--- src/vs/workbench/browser/parts/positronTopBar/positronTopBar.tsx
+++ src/vs/workbench/browser/parts/positronTopBar/positronTopBar.tsx
@@ -5,6 +5,8 @@
 import 'vs/css!./media/css/positronTopBar';
 const React = require('react');
 import { TopBarButton } from 'vs/workbench/browser/parts/positronTopBar/components/topBarButton/topBarButton';
+import { TopBarCommandCenter } from 'vs/workbench/browser/parts/positronTopBar/components/topBarCommandCenter/topBarCommandCenter';
+import { TopBarRegion } from 'vs/workbench/browser/parts/positronTopBar/components/topBarRegion/topBarRegion';
 import { TopBarSeparator } from 'vs/workbench/browser/parts/positronTopBar/components/topBarSeparator/topBarSeparator';
 
 /**
@@ -22,16 +24,26 @@ export const PositronTopBar = (props: PositronTopBarProps) => {
 	// Render.
 	return (
 		<div className='positron-top-bar'>
-			<TopBarButton iconClassName='new-file-icon' dropDown={true} />
-			<TopBarSeparator />
-			<TopBarButton iconClassName='new-project-icon' />
-			<TopBarSeparator />
-			<TopBarButton iconClassName='open-file-icon' dropDown={true} />
-			<TopBarSeparator />
-			<TopBarButton iconClassName='save-icon' />
-			<TopBarButton iconClassName='save-all-icon' />
-			<TopBarSeparator />
-			<TopBarButton iconClassName='print-icon' />
+			<TopBarRegion align='left'>
+				<TopBarButton iconClassName='new-file-icon' dropDown={true} />
+				<TopBarSeparator />
+				<TopBarButton iconClassName='new-project-icon' />
+				<TopBarSeparator />
+				<TopBarButton iconClassName='open-file-icon' dropDown={true} />
+				<TopBarSeparator />
+				<TopBarButton iconClassName='save-icon' />
+				<TopBarButton iconClassName='save-all-icon' />
+			</TopBarRegion>
+
+			<TopBarRegion align='center'>
+				<TopBarButton iconClassName='back-icon' />
+				<TopBarButton iconClassName='forward-icon' />
+				<TopBarCommandCenter />
+			</TopBarRegion>
+
+			<TopBarRegion align='right'>
+				<TopBarButton iconClassName='print-icon' />
+			</TopBarRegion>
 		</div>
 	);
 };
