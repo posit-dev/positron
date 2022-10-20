43c6c29c26446ede5ad8bd23b924b4be437c53d8
Brian Lambert <brianlambert@gmail.com>
2022-10-20 11:02:18 -0400

    Updates

--- src/vs/workbench/browser/parts/positronTopBar/positronTopBar.tsx
+++ src/vs/workbench/browser/parts/positronTopBar/positronTopBar.tsx
@@ -2,10 +2,12 @@
  *  Copyright (c) Posit, PBC.
  *--------------------------------------------------------------------------------------------*/
 
+import { useState } from 'react';
 import 'vs/css!./media/css/positronTopBar';
 const React = require('react');
 import { TopBarButton } from 'vs/workbench/browser/parts/positronTopBar/components/topBarButton/topBarButton';
 import { TopBarSeparator } from 'vs/workbench/browser/parts/positronTopBar/components/topBarSeparator/topBarSeparator';
+import { TooltipManager } from 'vs/workbench/browser/parts/positronTopBar/tooltipManager';
 
 /**
  * PositronTopBarProps interface.
@@ -19,19 +21,22 @@ interface PositronTopBarProps {
  * @returns The component.
  */
 export const PositronTopBar = (props: PositronTopBarProps) => {
+	// Hooks.
+	const [hoverManager] = useState(new TooltipManager());
+
 	// Render.
 	return (
 		<div className='positron-top-bar'>
-			<TopBarButton iconClassName='new-file-icon' dropDown={true} />
+			<TopBarButton tooltipManager={hoverManager} iconClassName='new-file-icon' dropDown={true} tooltip='New File' />
 			<TopBarSeparator />
-			<TopBarButton iconClassName='new-project-icon' />
+			<TopBarButton tooltipManager={hoverManager} iconClassName='new-project-icon' tooltip='New Project' />
 			<TopBarSeparator />
-			<TopBarButton iconClassName='open-file-icon' dropDown={true} />
+			<TopBarButton tooltipManager={hoverManager} iconClassName='open-file-icon' dropDown={true} tooltip='Open File' />
 			<TopBarSeparator />
-			<TopBarButton iconClassName='save-icon' />
-			<TopBarButton iconClassName='save-all-icon' />
+			<TopBarButton tooltipManager={hoverManager} iconClassName='save-icon' tooltip='Save' />
+			<TopBarButton tooltipManager={hoverManager} iconClassName='save-all-icon' tooltip='Save All' />
 			<TopBarSeparator />
-			<TopBarButton iconClassName='print-icon' />
+			<TopBarButton tooltipManager={hoverManager} iconClassName='print-icon' tooltip='Print' />
 		</div>
 	);
 };
