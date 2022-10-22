19484f65645147ec35607e5f1b96bc0e22604c6f
J.J. Allaire <jj.allaire@gmail.com>
2022-10-22 09:53:25 -0400

    remove top bar separators

--- src/vs/workbench/browser/parts/positronTopBar/positronTopBar.tsx
+++ src/vs/workbench/browser/parts/positronTopBar/positronTopBar.tsx
@@ -67,9 +67,7 @@ export const PositronTopBar = (props: PositronTopBarProps) => {
 			<div className='positron-top-bar'>
 				<TopBarRegion align='left'>
 					<TopBarNewMenu />
-					<TopBarSeparator />
 					<TopBarOpenMenu />
-					<TopBarSeparator />
 					<TopBarCommandButton id={kFileSave} iconClassName='save-icon' />
 					<TopBarCommandButton id={kFileSaveAll} iconClassName='save-all-icon' />
 				</TopBarRegion>
