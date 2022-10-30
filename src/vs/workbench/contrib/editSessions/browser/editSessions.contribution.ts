d9ef40281cdc14b7f74594435bc0a6a512c59af8
Brian Lambert <brianlambert@gmail.com>
2022-10-30 12:31:54 -0500

    Merge remote-tracking branch 'upstream/main'
    Lead-authored-by: Brian Lambert <brianlambert@gmail.com>
    Co-authored-by: positron-bot[bot] <173392469+positron-bot[bot]@users.noreply.github.com>

--- src/vs/workbench/contrib/editSessions/browser/editSessions.contribution.ts
+++ src/vs/workbench/contrib/editSessions/browser/editSessions.contribution.ts
@@ -20,7 +20,7 @@ import { encodeBase64 } from 'vs/base/common/buffer';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { IProgressService, ProgressLocation } from 'vs/platform/progress/common/progress';
 import { EditSessionsWorkbenchService } from 'vs/workbench/contrib/editSessions/browser/editSessionsStorageService';
-import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
+import { InstantiationType, registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { UserDataSyncErrorCode, UserDataSyncStoreError } from 'vs/platform/userDataSync/common/userDataSync';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { INotificationService, Severity } from 'vs/platform/notification/common/notification';
@@ -57,8 +57,8 @@ import { sha1Hex } from 'vs/base/browser/hash';
 import { IStorageService, StorageScope, StorageTarget } from 'vs/platform/storage/common/storage';
 import { IActivityService, NumberBadge } from 'vs/workbench/services/activity/common/activity';
 
-registerSingleton(IEditSessionsLogService, EditSessionsLogService, false);
-registerSingleton(IEditSessionsStorageService, EditSessionsWorkbenchService, false);
+registerSingleton(IEditSessionsLogService, EditSessionsLogService, InstantiationType.Delayed);
+registerSingleton(IEditSessionsStorageService, EditSessionsWorkbenchService, InstantiationType.Delayed);
 
 const continueWorkingOnCommand: IAction2Options = {
 	id: '_workbench.editSessions.actions.continueEditSession',
@@ -292,13 +292,20 @@ export class EditSessionsContribution extends Disposable implements IWorkbenchCo
 
 				const shouldStoreEditSession = await that.shouldContinueOnWithEditSession();
 
-				let uri = workspaceUri ?? await that.pickContinueEditSessionDestination();
-				if (uri === undefined) { return; }
-
 				// Run the store action to get back a ref
 				let ref: string | undefined;
 				if (shouldStoreEditSession) {
 					ref = await that.storeEditSession(false);
+				}
+
+				let uri = workspaceUri ?? await that.pickContinueEditSessionDestination();
+				if (uri === undefined) {
+					// If the user didn't end up picking a Continue On destination
+					// and we stored an edit session, clean up the stored edit session
+					if (ref !== undefined) {
+						void that.editSessionsStorageService.delete(ref);
+					}
+					return;
 				}
 
 				// Append the ref to the URI
