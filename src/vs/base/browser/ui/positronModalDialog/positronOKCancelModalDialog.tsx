c5ae723b20c97966b23c0859d16f1b44a51daa49
Brian Lambert <brianlambert@gmail.com>
2022-11-03 21:20:17 -0400

    Work in progress

--- src/vs/base/browser/ui/positronModalDialog/positronOKCancelModalDialog.tsx
+++ src/vs/base/browser/ui/positronModalDialog/positronOKCancelModalDialog.tsx
@@ -5,7 +5,6 @@
 import 'vs/css!./positronModalDialog';
 const React = require('react');
 import { PropsWithChildren } from 'react';
-import { SimpleTitleBar } from 'vs/base/browser/ui/positronModalDialog/components/simpleTitleBar';
 import { ContentArea } from 'vs/base/browser/ui/positronModalDialog/components/contentArea';
 import { OKCancelActionBar } from 'vs/base/browser/ui/positronModalDialog/components/okCancelActionBar';
 import { PositronModalDialog, PositronModalDialogProps } from 'vs/base/browser/ui/positronModalDialog/positronModalDialog';
@@ -27,9 +26,9 @@ export interface OKCancelModalDialogProps extends PositronModalDialogProps {
  * @returns The component.
  */
 export const OKCancelModalDialog = (props: PropsWithChildren<OKCancelModalDialogProps>) => {
+	// Render.
 	return (
 		<PositronModalDialog {...props}>
-			<SimpleTitleBar {...props} />
 			<ContentArea>
 				{props.children}
 			</ContentArea>
