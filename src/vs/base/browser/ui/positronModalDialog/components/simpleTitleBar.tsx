58a8b0ba0676042d603c752cb2bad82d773e4353
Brian Lambert <brianlambert@gmail.com>
2022-11-03 20:20:17 -0500

    Work in progress

--- src/vs/base/browser/ui/positronModalDialog/components/simpleTitleBar.tsx
+++ src/vs/base/browser/ui/positronModalDialog/components/simpleTitleBar.tsx
@@ -4,22 +4,120 @@
 
 import 'vs/css!./simpleTitleBar';
 import * as React from 'react';
+import { MouseEvent } from 'react'; // eslint-disable-line no-duplicate-imports
 
 /**
  * SimpleTitleBarProps interface.
  */
 interface SimpleTitleBarProps {
 	title: string;
+	move?: (x: number, y: number) => void;
 }
+
+// interface Point {
+// 	clientX: number;
+// 	clientY: number;
+// }
+
+/**
+ * Events.
+ */
+type DocumentMouseEvent = globalThis.MouseEvent;
+
 
 /**
  * SimpleTitleBar component.
  * @param props A SimpleTitleBarProps that contains the properties for the component.
  */
 export const SimpleTitleBar = (props: SimpleTitleBarProps) => {
+	// Hooks.
+	// const [moveStartPoint, setMoveStartPoint] = useState<Point | undefined>(undefined);
+
+	// // Memoize the mouse event handler.
+	// const xxxx = useCallback((e: globalThis.MouseEvent) => {
+	// 	console.log(`XXXX!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ${moveStartPoint?.clientX},${moveStartPoint?.clientY}`);
+	// 	if (props.move && moveStartPoint) {
+	// 		console.log('DOING IT');
+	// 		e.preventDefault();
+	// 		e.stopPropagation();
+	// 		//e.preventDefault();
+	// 		props.move(moveStartPoint.clientX - e.clientX, moveStartPoint.clientY - e.clientY);
+	// 		setMoveStartPoint({
+	// 			clientX: e.clientX,
+	// 			clientY: e.clientY
+	// 		});
+	// 	}
+	// }, []);
+
+	// Mouse down handler.
+	const mouseDownHandler = (e: MouseEvent) => {
+		if (!props.move) {
+			return;
+		}
+
+		e.preventDefault();
+
+		let clientX = e.clientX;
+		let clientY = e.clientY;
+
+		const movie1 = (e: DocumentMouseEvent) => {
+			if (!props.move) {
+				return;
+			}
+
+			e.preventDefault();
+			console.log(`MOUSE MOVE HANDLER ${e.clientX},${e.clientY}`);
+
+			props.move(clientX - e.clientX, clientY - e.clientY);
+			clientX = e.clientX;
+			clientY = e.clientY;
+		};
+
+		const movie2 = (e: DocumentMouseEvent) => {
+			console.log(`MOUSE UP HANDLER ${e.clientX},${e.clientY}`);
+			document.removeEventListener('mousemove', movie1);
+			document.removeEventListener('mouseup', movie2);
+		};
+
+		document.addEventListener('mousemove', movie1, false);
+		document.addEventListener('mouseup', movie2, false);
+
+		// if (props.move) {
+
+		// 	document.addEventListener('mousemove', xxxx, false);
+
+		// 	console.log(`Starting move operation from ${e.clientX},${e.clientX}`);
+		// }
+	};
+
+	// // Mouse move handler.
+	// const mouseMoveHandler = (e: MouseEvent) => {
+	// 	console.log(`MOUSE MOVE!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
+	// 	if (props.move && moveStartPoint) {
+	// 		e.preventDefault();
+	// 		e.stopPropagation();
+	// 		//e.preventDefault();
+	// 		props.move(moveStartPoint.clientX - e.clientX, moveStartPoint.clientY - e.clientY);
+	// 		setMoveStartPoint({
+	// 			clientX: e.clientX,
+	// 			clientY: e.clientY
+	// 		});
+	// 	}
+	// };
+
+	// // Mouse up handler.
+	// const mouseUpHandler = (e: MouseEvent) => {
+	// 	if (props.move && moveStartPoint) {
+	// 		e.preventDefault();
+	// 		e.stopPropagation();
+	// 		props.move(moveStartPoint.clientX - e.clientX, moveStartPoint.clientY - e.clientY);
+	// 		setMoveStartPoint(undefined);
+	// 	}
+	// };
+
 	// Render.
 	return (
-		<div className='simple-title-bar' >
+		<div className='simple-title-bar' onMouseDown={mouseDownHandler} /*onMouseMove={mouseMoveHandler} onMouseUp={mouseUpHandler}*/>
 			<div className='simple-title-bar-title'>
 				{props.title}
 			</div>
--- src/vs/base/browser/ui/positronModalDialog/positronModalDialog.tsx
+++ src/vs/base/browser/ui/positronModalDialog/positronModalDialog.tsx
@@ -4,7 +4,8 @@
 
 import 'vs/css!./positronModalDialog';
 import * as React from 'react';
-import { PropsWithChildren, useCallback, useEffect } from 'react'; // eslint-disable-line no-duplicate-imports
+import { PropsWithChildren, useCallback, useEffect, useRef } from 'react'; // eslint-disable-line no-duplicate-imports
+import { SimpleTitleBar } from 'vs/base/browser/ui/positronModalDialog/components/simpleTitleBar';
 
 /**
  * Events.
@@ -15,6 +16,7 @@ type DocumentKeyboardEvent = globalThis.KeyboardEvent;
  * PositronModalDialogProps interface.
  */
 export interface PositronModalDialogProps {
+	title: string;
 	width: number;
 	height: number;
 	accept?: () => void;
@@ -26,6 +28,9 @@ export interface PositronModalDialogProps {
  * @param props A PositronModalDialogProps that contains the modal dialog component properties.
  */
 export const PositronModalDialog = (props: PropsWithChildren<PositronModalDialogProps>) => {
+	// Hooks.
+	const ref = useRef<HTMLDivElement>(undefined!);
+
 	// Memoize the keydown event handler.
 	const keydownHandler = useCallback((event: DocumentKeyboardEvent) => {
 		// Fully suppresses an event.
@@ -56,11 +61,23 @@ export const PositronModalDialog = (props: PropsWithChildren<PositronModalDialog
 		};
 	}, []);
 
+	const moveHandler = (x: number, y: number) => {
+		console.log(`Move was called ${x},${y}`);
+
+		ref.current.style.setProperty('left', `${ref.current.offsetLeft - x}px`);
+		ref.current.style.setProperty('top', `${ref.current.offsetTop - y}px`);
+
+		// set the element's new position:
+		// elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		// elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	};
+
 	// Render.
 	return (
 		<div className='positron-modal-dialog-shadow-container'>
 			<div className='positron-modal-dialog-container' role='dialog' tabIndex={-1}>
-				<div className='positron-modal-dialog-box' style={{ width: props.width, height: props.height }}>
+				<div ref={ref} className='positron-modal-dialog-box' style={{ width: props.width, height: props.height }}>
+					{props.title && <SimpleTitleBar {...props} move={moveHandler} />}
 					{props.children}
 				</div>
 			</div>
