fdafe19da7b3e4d52534017e4944b49e5c17d8c3
Brian Lambert <brianlambert@gmail.com>
2022-11-08 15:04:38 -0600

    Working on new contrib for help

---
+++ src/vs/workbench/contrib/positronHelp/browser/positronHelpService.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Posit, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Disposable } from 'vs/base/common/lifecycle';
+import { IPositronHelpService } from 'vs/workbench/services/positronHelp/common/positronHelp';
+
+export class PositronHelpService extends Disposable implements IPositronHelpService {
+
+	declare readonly _serviceBrand: undefined;
+
+}
