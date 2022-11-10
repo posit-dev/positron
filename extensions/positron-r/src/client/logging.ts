f4a2190960a24fe305dc66d419542ca1ad7cba2a
Jonathan McPherson <jonathan@rstudio.com>
2022-11-09 19:12:51 -0600

    clean up another round of eslint warnings

--- extensions/positron-r/src/client/logging.ts
+++ extensions/positron-r/src/client/logging.ts
@@ -2,24 +2,24 @@
  *  Copyright (c) Posit, PBC.
  *--------------------------------------------------------------------------------------------*/
 
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 let _traceOutputChannel: vscode.OutputChannel | undefined;
 
 export function initializeLogging(context: vscode.ExtensionContext) {
 
-    const config = vscode.workspace.getConfiguration('positron.r');
-    const tracingEnabled = config.get<boolean>('trace.server');
-    if (tracingEnabled) {
-        _traceOutputChannel = vscode.window.createOutputChannel("Positron R Language Server (Trace)");
-    }
+	const config = vscode.workspace.getConfiguration('positron.r');
+	const tracingEnabled = config.get<boolean>('trace.server');
+	if (tracingEnabled) {
+		_traceOutputChannel = vscode.window.createOutputChannel('Positron R Language Server (Trace)');
+	}
 
 }
 
 export function trace(message: string) {
-    _traceOutputChannel?.appendLine(message);
+	_traceOutputChannel?.appendLine(message);
 }
 
 export function traceOutputChannel() {
-    return _traceOutputChannel;
+	return _traceOutputChannel;
 }
--- extensions/positron-r/src/client/util.ts
+++ extensions/positron-r/src/client/util.ts
@@ -6,10 +6,10 @@ import * as vscode from 'vscode';
 
 export function withActiveExtension(ext: vscode.Extension<any>, callback: () => void) {
 
-    if (ext.isActive) {
-        callback();
-    } else {
-        ext.activate().then(callback);
-    }
+	if (ext.isActive) {
+		callback();
+	} else {
+		ext.activate().then(callback);
+	}
 
 }
