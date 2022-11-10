fe5e03159e5595c8fe3148be8ed7b03d14eb72b4
Kevin Ushey <kevinushey@gmail.com>
2022-11-10 10:34:14 -0800

    all, any macros

---
+++ amalthea/crates/stdext/src/all.rs
@@ -0,0 +1,29 @@
+//
+// all.rs
+//
+// Copyright (C) 2022 by Posit, PBC
+//
+//
+
+#[macro_export]
+macro_rules! all {
+    ($($expr:expr)*) => {{
+        let result = true;
+        $(let result = result && $expr;)*
+        result
+    }}
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_all() {
+        assert!(all!() == true);
+        assert!(all!(false false) == false);
+        assert!(all!(true false) == false);
+        assert!(all!(true true) == true);
+    }
+
+}
+
