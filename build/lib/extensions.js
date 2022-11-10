fd9f26f9ace8e51bba1e9eea7894a0e3d32ffdc6
Jonathan McPherson <jonathan@rstudio.com>
2022-11-09 18:02:50 -0600

    copy zeromq to Jupyter adapter extension bundle

--- build/lib/extensions.js
+++ build/lib/extensions.js
@@ -133,10 +133,12 @@ function fromLocalWebpack(extensionPath, webpackConfigFileName) {
                 // source map handling:
                 // * rewrite sourceMappingURL
                 // * save to disk so that upload-task picks this up
-                const contents = data.contents.toString('utf8');
-                data.contents = Buffer.from(contents.replace(/\n\/\/# sourceMappingURL=(.*)$/gm, function (_m, g1) {
-                    return `\n//# sourceMappingURL=${sourceMappingURLBase}/extensions/${path.basename(extensionPath)}/${relativeOutputPath}/${g1}`;
-                }), 'utf8');
+                if (!data.path.endsWith('.node')) {
+                    const contents = data.contents.toString('utf8');
+                    data.contents = Buffer.from(contents.replace(/\n\/\/# sourceMappingURL=(.*)$/gm, function (_m, g1) {
+                        return `\n//# sourceMappingURL=${sourceMappingURLBase}/extensions/${path.basename(extensionPath)}/${relativeOutputPath}/${g1}`;
+                    }), 'utf8');
+                }
                 this.emit('data', data);
             }));
         });
