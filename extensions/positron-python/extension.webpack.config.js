8202f47e73af8cd5984961fbad6b492f4e2e5384
Jonathan McPherson <jonathan@rstudio.com>
2022-11-10 12:26:38 -0600

    target webpack config for client/

--- extensions/positron-python/extension.webpack.config.js
+++ extensions/positron-python/extension.webpack.config.js
@@ -4,11 +4,25 @@
 
 'use strict';
 
-const config = require('./build/webpack/webpack.extension.config');
+const path = require('path');
+
+// Load the shared webpack config for all built-in extensions
 const withDefaults = require('../shared.webpack.config');
 
+// Load the webpack config for the Python extension
+const config = require('./build/webpack/webpack.extension.config');
+
+// Merge them with settings for this environment
 module.exports = withDefaults({
     ...config.default,
+    entry: {
+        extension: './src/client/extension.ts',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist', 'client'),
+        libraryTarget: 'commonjs',
+    },
     context: __dirname
 });
 
