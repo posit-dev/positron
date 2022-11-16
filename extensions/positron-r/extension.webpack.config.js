55035f6fa05c2c0ca29e92e32701d1d30fa72fad
Jonathan McPherson <jonathan@rstudio.com>
2022-11-16 11:33:08 -0800

    Revert "try to make ark kernel executable"
    This reverts commit d3f30c2c528910975472fa6098ddeed5f667cd50.

--- extensions/positron-r/extension.webpack.config.js
+++ extensions/positron-r/extension.webpack.config.js
@@ -10,7 +10,6 @@
 const path = require('path');
 const withDefaults = require('../shared.webpack.config');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const PermissionsOutputPlugin = require('webpack-permissions-plugin');
 
 module.exports = withDefaults({
 	context: __dirname,
@@ -19,24 +18,11 @@ module.exports = withDefaults({
 	},
 	plugins: [
 		...withDefaults.nodePlugins(__dirname),
-
-		// CopyWebpackPlugin loses the executable bit, so we need to set it back using
-		// webpack-permissions-plugin.
-		//
-		// See: https://github.com/webpack-contrib/copy-webpack-plugin/issues/35
-		new PermissionsOutputPlugin({
-			buildFolders: [
-				path.resolve(__dirname, 'dist', 'bin')
-			]
-		}),
-
-		// Copy the ark executable to the output folder so it will get included in the packaging
-		// step.
 		new CopyWebpackPlugin({
 			patterns: [
 				{
 					from: './amalthea/target/debug/ark',
-					to: path.resolve(__dirname, 'dist', 'bin')
+					to: './bin'
 				},
 			],
 		}),
