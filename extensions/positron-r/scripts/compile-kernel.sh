07ae1c4834b848c3218a1efce9024e5f0b6f67df
Jonathan McPherson <jonathan@rstudio.com>
2022-11-17 13:02:55 -0600

    allow cross-compilation for rust-zmq

--- extensions/positron-r/scripts/compile-kernel.sh
+++ extensions/positron-r/scripts/compile-kernel.sh
@@ -15,11 +15,28 @@ pushd "${SCRIPTDIR}/../amalthea"
 # cross-compile an x86_64 version of the kernel on an ARM64 (Apple Silicon)
 # machine.
 if [ -n "${RUST_TARGET}" ]; then
+	# Allow cross-compilation in pkg-config. Needed to compile native code in rust-zmq.
+	export PKG_CONFIG_ALLOW_CROSS=1
+
+	# Set the target triple. Note that this requires the machine doing the compilation
+	# to have the appropriate cross-compilation toolchain installed.
+	#
+	# For example, to add both x86_64 and aarch64 targets, run:
+	#
+	#   $ rustup target add aarch64-apple-darwin x86_64-apple-darwin
+	#
 	CARGO_TARGET="--target ${RUST_TARGET}"
 fi
 
-# Build the kernel
+# Build the kernel!
 cargo build ${CARGO_TARGET}
+
+# If we built a cross-compiled version of the ark kernel, copy it to the right
+# place; it is always expected in target/debug.
+if [ -n "${RUST_TARGET}" ]; then
+	mkdir -p "target/debug"
+	cp "target/${RUST_TARGET}/debug/ark" "target/debug/ark"
+fi
 
 popd
 
