887724e0be2db369ee950f1ef1f2336d2092d092
Jonathan McPherson <jonathan@rstudio.com>
2022-11-17 11:09:07 -0600

    Merge branch 'feature/ark-lab-build'
    * fix paths; don't try to copy in webpack any more
    
    * use separate gulp task
    
    * use positron-specific metadata to handle file copies
    
    * Revert "try to make ark kernel executable"
    
    This reverts commit d3f30c2c528910975472fa6098ddeed5f667cd50.
    
    * avoid linking to specific R
    
    * try to make ark kernel executable
    
    Authored-by: Jonathan McPherson <jonathan@rstudio.com>

--- extensions/positron-r/extension.webpack.config.js
+++ extensions/positron-r/extension.webpack.config.js
@@ -9,22 +9,10 @@
 
 const path = require('path');
 const withDefaults = require('../shared.webpack.config');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = withDefaults({
 	context: __dirname,
 	entry: {
 		extension: './src/extension.ts',
 	},
-	plugins: [
-		...withDefaults.nodePlugins(__dirname),
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: './amalthea/target/debug/ark',
-					to: './bin'
-				},
-			],
-		}),
-	],
 });
