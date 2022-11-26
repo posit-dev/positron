05012bb7b964206c8834e19593f4f0e33fb0c6af
Brian Lambert <brianlambert@gmail.com>
2022-11-26 17:19:54 -0600

    Lots closer

--- src/positron-help.js
+++ src/positron-help.js
@@ -2,36 +2,26 @@
  *  Copyright (c) Posit, PBC.
  *--------------------------------------------------------------------------------------------*/
 
-console.log('POSITRON HELP SCRIPT LOADED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-
-// The find text.
 let findText = undefined;
-let results = false;
-
-window.addEventListener('load', function () {
-	if (window.find) {
-		console.log('The window has a find method.');
-	}
-});
+let findResult = false;
 
 window.addEventListener('message', (event) => {
 	if (window.find) {
 		if (event.data.command === 'find') {
 			findText = event.data.findText;
-			results = findText && window.find(findText, false, false, true, false, true);
-
-			if (results) {
+			findResult = findText && window.find(findText, false, false, true, false, true);
+			if (findResult) {
 				window.focus();
 			} else {
 				window.getSelection().removeAllRanges();
 			}
 		} else if (event.data.command === 'find-previous') {
-			if (results) {
+			if (findResult) {
 				window.find(findText, false, true, false, false, true);
 				window.focus();
 			}
 		} else if (event.data.command === 'find-next') {
-			if (results) {
+			if (findResult) {
 				window.find(findText, false, false, false, false, true);
 				window.focus();
 			}
