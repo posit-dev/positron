8ccaa26f651feb53d1a0f233295d1915b5dca6e9
Jonathan McPherson <jonathan@rstudio.com>
2022-11-28 10:27:56 -0800

    pass log file argument to kernel

--- extensions/jupyter-adapter/src/JupyterKernel.ts
+++ extensions/jupyter-adapter/src/JupyterKernel.ts
@@ -34,7 +34,10 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 	private _process: ChildProcess | null;
 
 	/** The kernel connection file (path to JSON file) */
-	private _file: string | null;
+	private _connectionFile: string | null;
+
+	/** The log file (path to a text file) */
+	private _logFile: string | null;
 
 	/** The kernel's current state */
 	private _status: positron.RuntimeState;
@@ -63,7 +66,8 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 		super();
 		this._spec = spec;
 		this._process = null;
-		this._file = null;
+		this._connectionFile = null;
+		this._logFile = null;
 
 		this._control = null;
 		this._shell = null;
@@ -134,14 +138,23 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 		// Write connection definition to a file
 		const tempdir = os.tmpdir();
 		const sep = path.sep;
-		this._file = path.join(fs.mkdtempSync(`${tempdir}${sep}kernel-`), 'connection.json');
-		fs.writeFileSync(this._file, JSON.stringify(conn));
+		const kerneldir = fs.mkdtempSync(`${tempdir}${sep}kernel-`);
+		this._connectionFile = path.join(kerneldir, 'connection.json');
+		fs.writeFileSync(this._connectionFile, JSON.stringify(conn));
 
-		// Replace the {connection_file} argument with our connection file
 		const args = this._spec.argv.map((arg, _idx) => {
+			// Replace {connection_file} with the connection file path
 			if (arg === '{connection_file}') {
-				return this._file;
+				return this._connectionFile;
 			}
+
+			// Replace {log_file} with the log file path (not all kernels
+			// support this)
+			if (arg === '{log_file}') {
+				this._logFile = path.join(kerneldir, 'output.log');
+				return this._logFile;
+			}
+
 			return arg;
 		}) as Array<string>;
 
@@ -420,9 +433,12 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 	}
 
 	public dispose() {
-		// Clean up connection file
-		if (this._file) {
-			fs.rmSync(this._file);
+		// Clean up connection and log files
+		if (this._connectionFile) {
+			fs.rmSync(this._connectionFile);
+		}
+		if (this._logFile) {
+			fs.rmSync(this._logFile);
 		}
 
 		// Close sockets
