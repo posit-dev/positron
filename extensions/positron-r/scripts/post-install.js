de10a60154e3d9617f568303d2d6c424340301e0
Brian Lambert <brianlambert@gmail.com>
2022-12-05 13:20:42 -0600

    Check that arkPath exists

--- extensions/positron-r/scripts/post-install.js
+++ extensions/positron-r/scripts/post-install.js
@@ -2,6 +2,7 @@
  *  Copyright (c) Posit, PBC.
  *--------------------------------------------------------------------------------------------*/
 
+const fs = require('fs');
 const { execSync } = require('child_process');
 
 // On macOS, we use install_name_tool to fix up the link to libR.dylib.
@@ -11,14 +12,14 @@ if (process.platform === 'darwin') {
 
 	// Get the path to the ark executable.
 	const arkPath = 'amalthea/target/release/ark';
+	if (fs.existsSync(arkPath)) {
+		// Figure out what version of R that we linked to.
+		const otoolCommand = `otool -L "${arkPath}" | grep libR.dylib | awk "{ print $1 }"`;
+		const oldLibraryPath = execSync(otoolCommand, { encoding: 'utf-8' }).trim();
 
-	// Figure out what version of R that we linked to.
-	const otoolCommand = `otool -L "${arkPath}" | grep libR.dylib | awk "{ print $1 }"`;
-	const oldLibraryPath = execSync(otoolCommand, { encoding: 'utf-8' }).trim();
-
-	// Change that to use @rpath instead. We don't actually set an @rpath in the compiled
-	// executable (we inject R via DYLD_INSERT_LIBRARIES) so this is mainly just hygiene.
-	const newLibraryPath = '@rpath/libR.dylib';
-	execSync(`install_name_tool -change "${oldLibraryPath}" "${newLibraryPath}" "${arkPath}"`);
-
+		// Change that to use @rpath instead. We don't actually set an @rpath in the compiled
+		// executable (we inject R via DYLD_INSERT_LIBRARIES) so this is mainly just hygiene.
+		const newLibraryPath = '@rpath/libR.dylib';
+		execSync(`install_name_tool -change "${oldLibraryPath}" "${newLibraryPath}" "${arkPath}"`);
+	}
 }
