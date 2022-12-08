8aa55d8f82daa8846c44fb3fa1868fdce34b6b3b
Jonathan McPherson <jonathan@rstudio.com>
2022-12-08 11:10:38 -0800

    remove unnecessary mutex from zmq socket wrapper

--- extensions/positron-r/amalthea/crates/amalthea/src/socket/socket.rs
+++ extensions/positron-r/amalthea/crates/amalthea/src/socket/socket.rs
@@ -8,14 +8,9 @@
 use crate::error::Error;
 use crate::session::Session;
 use log::trace;
-use std::sync::{Arc, Mutex};
 
 /// Represents a socket that sends and receives messages that are optionally
 /// signed with a SHA-256 HMAC.
-///
-/// Internally, it wraps an ZeroMQ socket with a mutex, so Socket objects can be
-/// shared in a threadsafe way.
-#[derive(Clone)]
 pub struct Socket {
     /// The Jupyter session information associated with the socket, including
     /// the session ID and HMAC signing key
@@ -26,7 +21,7 @@ pub struct Socket {
     pub name: String,
 
     /// A ZeroMQ socket over which signed messages are to be sent/received
-    socket: Arc<Mutex<zmq::Socket>>,
+    socket: zmq::Socket,
 }
 
 impl Socket {
@@ -86,7 +81,7 @@ impl Socket {
 
         // Create a new mutex and return
         Ok(Self {
-            socket: Arc::new(Mutex::new(socket)),
+            socket: socket,
             session,
             name,
         })
@@ -94,70 +89,38 @@ impl Socket {
 
     /// Receive a message from the socket.
     ///
-    /// **Note**: This will block until the socket is available, and block again
-    /// until a message is delivered on the socket.
+    /// **Note**: This will block until a message is delivered on the socket.
     pub fn recv(&self, msg: &mut zmq::Message) -> Result<(), Error> {
-        match self.socket.lock() {
-            Ok(socket) => {
-                if let Err(err) = socket.recv(msg, 0) {
-                    Err(Error::ZmqError(self.name.clone(), err))
-                } else {
-                    Ok(())
-                }
-            }
-            Err(_) => Err(Error::CannotLockSocket(
-                self.name.clone(),
-                String::from("message send"),
-            )),
+        if let Err(err) = self.socket.recv(msg, 0) {
+            Err(Error::ZmqError(self.name.clone(), err))
+        } else {
+            Ok(())
         }
     }
 
     /// Receive a multi-part message from the socket.
     ///
-    /// **Note**: This will block until the socket is available, and block again
-    /// until a message is delivered on the socket.
+    /// **Note**: This will block until a message is delivered on the socket.
     pub fn recv_multipart(&self) -> Result<Vec<Vec<u8>>, Error> {
-        match self.socket.lock() {
-            Ok(socket) => match socket.recv_multipart(0) {
-                Ok(data) => Ok(data),
-                Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
-            },
-            Err(_) => Err(Error::CannotLockSocket(
-                self.name.clone(),
-                String::from("multipart receive"),
-            )),
+        match self.socket.recv_multipart(0) {
+            Ok(data) => Ok(data),
+            Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
         }
     }
 
     /// Send a message on the socket.
-    ///
-    /// **Note**: This will block until the socket is available.
     pub fn send(&self, msg: zmq::Message) -> Result<(), Error> {
-        match self.socket.lock() {
-            Ok(socket) => match socket.send(msg, 0) {
-                Ok(data) => Ok(data),
-                Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
-            },
-            Err(_) => Err(Error::CannotLockSocket(
-                self.name.clone(),
-                String::from("message send"),
-            )),
+        match self.socket.send(msg, 0) {
+            Ok(data) => Ok(data),
+            Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
         }
     }
 
     /// Send a multi-part message on the socket.
-    ///
-    /// **Note**: This will block until the socket is available.
     pub fn send_multipart(&self, data: &Vec<Vec<u8>>) -> Result<(), Error> {
-        match self.socket.lock() {
-            Ok(socket) => match socket.send_multipart(data, 0) {
-                Ok(data) => Ok(data),
-                Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
-            },
-            Err(_) => Err(Error::CannotLockSocket(
-                self.name.clone(),
-                String::from("multipart send"),
-            )),
+        match self.socket.send_multipart(data, 0) {
+            Ok(data) => Ok(data),
+            Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
         }
     }
 
@@ -169,15 +132,9 @@ impl Socket {
         // Currently, all SUB sockets subscribe to all topics; in theory
         // frontends could subscribe selectively, but in practice all known
         // Jupyter frontends subscribe to all topics.
-        match self.socket.lock() {
-            Ok(socket) => match socket.set_subscribe(b"") {
-                Ok(_) => Ok(()),
-                Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
-            },
-            Err(_) => Err(Error::CannotLockSocket(
-                self.name.clone(),
-                String::from("SUB socket subscribe"),
-            )),
+        match self.socket.set_subscribe(b"") {
+            Ok(_) => Ok(()),
+            Err(err) => Err(Error::ZmqError(self.name.clone(), err)),
         }
     }
 }
