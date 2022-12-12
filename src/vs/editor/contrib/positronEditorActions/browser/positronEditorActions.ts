66075ae3075adc961b5560191f4c34f76fa91e53
Kevin Ushey <kevinushey@gmail.com>
2022-12-12 16:39:50 +0800

    support arbitrary language comments

--- src/vs/editor/contrib/positronEditorActions/browser/positronEditorActions.ts
+++ src/vs/editor/contrib/positronEditorActions/browser/positronEditorActions.ts
@@ -10,11 +10,57 @@ import { Range } from 'vs/editor/common/core/range';
 import { Selection } from 'vs/editor/common/core/selection';
 import { ICommand, ICursorStateComputerData, IEditOperationBuilder } from 'vs/editor/common/editorCommon';
 import { EditorContextKeys } from 'vs/editor/common/editorContextKeys';
+import { ILanguageConfigurationService } from 'vs/editor/common/languages/languageConfigurationRegistry';
 import { ITextModel } from 'vs/editor/common/model';
 import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
 import { IsDevelopmentContext } from 'vs/platform/contextkey/common/contextkeys';
 
-export function addPositronCommentMarkers(model: ITextModel, selection: Selection): ISingleEditOperation[] {
+interface IPositronCommentMarkers {
+	startText: string;
+	endText: string;
+}
+
+function inferCommentMarkers(accessor: ServicesAccessor, editor: ICodeEditor): IPositronCommentMarkers {
+
+	const defaultCommentMarkers = <IPositronCommentMarkers>{
+		startText: '// --- Start Positron ---',
+		endText: '// --- End Positron ---',
+	};
+
+	const languageId = editor.getModel()?.getLanguageId();
+	if (!languageId) {
+		return defaultCommentMarkers;
+	}
+
+	const languageConfigurationService = accessor.get(ILanguageConfigurationService);
+	const languageConfiguration = languageConfigurationService.getLanguageConfiguration(languageId);
+	const comments = languageConfiguration.comments;
+	if (!comments) {
+		return defaultCommentMarkers;
+	}
+
+	const lineCommentToken = comments.lineCommentToken;
+	if (lineCommentToken) {
+		return <IPositronCommentMarkers>{
+			startText: `${lineCommentToken} --- Start Positron ---`,
+			endText: `${lineCommentToken} --- End Positron ---`,
+		};
+	}
+
+	const startToken = comments.blockCommentStartToken;
+	const endToken = comments.blockCommentEndToken;
+	if (startToken && endToken) {
+		return <IPositronCommentMarkers>{
+			startText: `${startToken} --- Start Positron --- ${endToken}`,
+			endText: `${startToken} --- End Positron --- ${endToken}`,
+		};
+	}
+
+	return defaultCommentMarkers;
+
+}
+
+function addPositronCommentMarkers(model: ITextModel, selection: Selection, markers: IPositronCommentMarkers): ISingleEditOperation[] {
 
 	// Compute the start position.
 	const startPosition = <IPosition>{
@@ -47,11 +93,11 @@ export function addPositronCommentMarkers(model: ITextModel, selection: Selectio
 	return [
 		{
 			range: Range.fromPositions(startPosition, startPosition),
-			text: `${indent}// --- Begin Positron ---\n`,
+			text: `${indent}${markers.startText}\n`,
 		},
 		{
 			range: Range.fromPositions(endPosition, endPosition),
-			text: `\n${indent}// --- End Positron ---\n`,
+			text: `\n${indent}${markers.endText}\n`,
 		},
 	];
 
@@ -61,15 +107,17 @@ export function addPositronCommentMarkers(model: ITextModel, selection: Selectio
 export class AddPositronCommentMarkersCommand implements ICommand {
 
 	private readonly _selection: Selection;
+	private readonly _markers: IPositronCommentMarkers;
 	private _selectionId: string | null;
 
-	constructor(selection: Selection) {
+	constructor(selection: Selection, markers: IPositronCommentMarkers) {
 		this._selection = selection;
+		this._markers = markers;
 		this._selectionId = null;
 	}
 
 	public getEditOperations(model: ITextModel, builder: IEditOperationBuilder): void {
-		const ops = addPositronCommentMarkers(model, this._selection);
+		const ops = addPositronCommentMarkers(model, this._selection, this._markers);
 		for (const op of ops) {
 			builder.addTrackedEditOperation(op.range, op.text);
 		}
@@ -95,14 +143,15 @@ export class AddPositronCommentMarkersAction extends EditorAction {
 		});
 	}
 
-	public run(_accessor: ServicesAccessor, editor: ICodeEditor, args: any): void {
+	public run(accessor: ServicesAccessor, editor: ICodeEditor, args: any): void {
 
 		const selection = editor.getSelection();
 		if (selection === null) {
 			return;
 		}
 
-		const command = new AddPositronCommentMarkersCommand(selection);
+		const commentMarkers = inferCommentMarkers(accessor, editor);
+		const command = new AddPositronCommentMarkersCommand(selection, commentMarkers);
 
 		editor.pushUndoStop();
 		editor.executeCommands(this.id, [command]);
