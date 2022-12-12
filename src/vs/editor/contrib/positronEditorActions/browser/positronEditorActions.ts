fc3ec399c35926e37160fb39b02ae280eb4dd914
Kevin Ushey <kevinushey@gmail.com>
2022-12-12 15:39:45 +0800

    some tweaks

--- src/vs/editor/contrib/positronEditorActions/browser/positronEditorActions.ts
+++ src/vs/editor/contrib/positronEditorActions/browser/positronEditorActions.ts
@@ -5,6 +5,8 @@
 import { ICodeEditor } from 'vs/editor/browser/editorBrowser';
 import { EditorAction, registerEditorAction, ServicesAccessor } from 'vs/editor/browser/editorExtensions';
 import { ISingleEditOperation } from 'vs/editor/common/core/editOperation';
+import { IPosition } from 'vs/editor/common/core/position';
+import { Range } from 'vs/editor/common/core/range';
 import { Selection } from 'vs/editor/common/core/selection';
 import { ICommand, ICursorStateComputerData, IEditOperationBuilder } from 'vs/editor/common/editorCommon';
 import { EditorContextKeys } from 'vs/editor/common/editorContextKeys';
@@ -14,24 +16,42 @@ import { IsDevelopmentContext } from 'vs/platform/contextkey/common/contextkeys'
 
 export function addPositronCommentMarkers(model: ITextModel, selection: Selection): ISingleEditOperation[] {
 
+	// Compute the start position.
+	const startPosition = <IPosition>{
+		lineNumber: selection.selectionStartLineNumber,
+		column: 0,
+	};
+
+	// Compute the end position.
+	// If the end selection ends before the start selection, assume that this is
+	// because the user had expands the selection and perhaps the end anchor of
+	// the selection lies at the start of the intended selection.
+	let endPosition;
+	if (selection.endColumn <= selection.selectionStartColumn) {
+		endPosition = <IPosition>{
+			lineNumber: selection.endLineNumber - 1,
+			column: Infinity,
+		};
+	} else {
+		endPosition = <IPosition>{
+			lineNumber: selection.endLineNumber,
+			column: Infinity,
+		};
+	}
+
+	// Inherit the indentation from the first line.
+	const startLine = model.getLineContent(startPosition.lineNumber);
+	const indent = /^\s*/.exec(startLine)?.[0];
+
+	// Return the actions to insert text around the selection.
 	return [
 		{
-			range: {
-				startLineNumber: selection.getStartPosition().lineNumber,
-				startColumn: 0,
-				endLineNumber: selection.getStartPosition().lineNumber,
-				endColumn: 0,
-			},
-			text: '// --- Begin Positron ---\n',
+			range: Range.fromPositions(startPosition, startPosition),
+			text: `${indent}// --- Begin Positron ---\n`,
 		},
 		{
-			range: {
-				startLineNumber: selection.getEndPosition().lineNumber,
-				startColumn: Infinity,
-				endLineNumber: selection.getEndPosition().lineNumber,
-				endColumn: Infinity,
-			},
-			text: '\n// --- End Positron ---\n',
+			range: Range.fromPositions(endPosition, endPosition),
+			text: `\n${indent}// --- End Positron ---\n`,
 		},
 	];
 
