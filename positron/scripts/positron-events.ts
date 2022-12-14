02353af3b6c9f5ce0fdcb44237579c920d8b381a
Jonathan McPherson <jonathan@rstudio.com>
2022-12-14 17:26:57 -0600

    Merge branch 'main' into softwarenerd
    * fix imports for language runtime events
    
    * support automatic generation of event interfaces (#71)
    
    * wire up ShowHelpUrl handling
    
    * support automatic event type generation
    
    * no longer used
    
    * comment in ts side
    
    * ensure newline at end
    
    * re-organize
    
    * move runtime events to own file
    
    Lead-authored-by: Jonathan McPherson <jonathan@rstudio.com>
    Co-authored-by: Brian Lambert <brianlambert@gmail.com>

---
+++ positron/scripts/positron-events.ts
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Posit, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import { readFileSync } from 'fs';
+import yaml from 'js-yaml';
+
+export interface PositronEventDefinitionParam {
+	name: string;
+	type: string;
+	comment: string;
+}
+
+export interface PositronEventDefinition {
+	name: string;
+	comment: string;
+	params: PositronEventDefinitionParam[];
+}
+
+const eventsYamlFile = `${__dirname}/../events.yaml`;
+const eventsYamlContents = readFileSync(eventsYamlFile, { encoding: 'utf-8' });
+export const events = yaml.load(eventsYamlContents) as PositronEventDefinition[];
