d5172d1e4b165be6e4834af5f0097de7e1524b8e
Brian Lambert <brianlambert@gmail.com>
2023-01-09 18:38:09 -0600

    Improvements to the environment and when we set / respond to the active language runtime changing

--- src/vs/workbench/contrib/positronEnvironment/browser/components/languageRuntimeSelectorMenuButton.tsx
+++ src/vs/workbench/contrib/positronEnvironment/browser/components/languageRuntimeSelectorMenuButton.tsx
@@ -4,7 +4,7 @@
 
 import 'vs/css!./languageRuntimeSelectorMenuButton';
 import * as React from 'react';
-import { IAction, Separator } from 'vs/base/common/actions';
+import { IAction } from 'vs/base/common/actions';
 import { ActionBarMenuButton } from 'vs/platform/positronActionBar/browser/components/actionBarMenuButton';
 import { usePositronEnvironmentContext } from 'vs/workbench/contrib/positronEnvironment/browser/positronEnvironmentContext';
 
@@ -33,25 +33,6 @@ export const LanguageRuntimeSelectorMenuButton = () => {
 					positronEnvironmentContext.setCurrentLanguageEnvironment(languageEnvironment);
 				}
 			});
-		});
-
-		// Add a separator.
-		if (actions.length) {
-			actions.push(new Separator());
-		}
-
-		// Add the None action.
-		actions.push({
-			id: '00000000-0000-0000-0000-000000000000',
-			label: 'None',
-			tooltip: '',
-			class: undefined,
-			enabled: true,
-			run: () => {
-				// TODO@softwarenerd - For now, the environment is used to change the active runtime.
-				positronEnvironmentContext.languageRuntimeService.activeRuntime = undefined;
-				positronEnvironmentContext.setCurrentLanguageEnvironment(undefined);
-			}
 		});
 
 		// Done. Return the actions.
