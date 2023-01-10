3ef8f45c097f42387ce8de0a2980c3e601cb6cbd
Jonathan McPherson <jonathan@rstudio.com>
2023-01-09 17:18:07 -0800

    data shaping for execution history service

---
+++ src/vs/workbench/services/executionHistory/executionHistoryService.ts
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Posit Software, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+export const IExecutionHistoryService = createDecorator<IExecutionHistoryService>('executionHistoryService');
+
+export interface IExecutionHistoryEntry {
+	/** ID of the entry */
+	id: string;
+
+	/** Time that the execution occurred, in seconds since the Epoch */
+	when: number;
+
+	/** The code that was executed, as a multi-line string */
+	input: string;
+
+	/** The type of output that was returned when the code was executed */
+	outputType: string;
+
+	/** The output itself */
+	output: any;
+
+	/** The total user time expended during the execution, in milliseconds */
+	durationMs: number;
+}
+
+export interface IExecutionHistoryService {
+	// Needed for service branding in dependency injector.
+	readonly _serviceBrand: undefined;
+
+	getEntries(runtimeId: string): Promise<IExecutionHistoryEntry[]>;
+}
