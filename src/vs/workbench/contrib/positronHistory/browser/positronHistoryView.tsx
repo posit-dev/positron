127c941c34d641f5c43a9b66c8c7b4f5afd020ad
Brian Lambert <brianlambert@gmail.com>
2023-01-13 16:02:19 -0700

    Placeholder for History

---
+++ src/vs/workbench/contrib/positronHistory/browser/positronHistoryView.tsx
@@ -0,0 +1,94 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Posit Software, PBC.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as React from 'react';
+import { IOpenerService } from 'vs/platform/opener/common/opener';
+import { IViewDescriptorService } from 'vs/workbench/common/views';
+import { IThemeService } from 'vs/platform/theme/common/themeService';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
+import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
+import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
+import { PositronReactRenderer } from 'vs/base/browser/positronReactRenderer';
+import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
+import { ViewPane, IViewPaneOptions } from 'vs/workbench/browser/parts/views/viewPane';
+import { TestContent } from 'vs/workbench/contrib/positronHistory/browser/components/testContent';
+
+/**
+ * PositronHistoryViewPane class.
+ */
+export class PositronHistoryViewPane extends ViewPane {
+	//#region Private Properties
+
+	// The PositronReactRenderer.
+	private positronReactRenderer: PositronReactRenderer | undefined;
+
+	//#endregion Private Properties
+
+	//#region Constructor & Dispose
+
+	constructor(
+		options: IViewPaneOptions,
+		@IConfigurationService configurationService: IConfigurationService,
+		@IContextKeyService contextKeyService: IContextKeyService,
+		@IContextMenuService contextMenuService: IContextMenuService,
+		@IInstantiationService instantiationService: IInstantiationService,
+		@IKeybindingService keybindingService: IKeybindingService,
+		@IOpenerService openerService: IOpenerService,
+		@ITelemetryService telemetryService: ITelemetryService,
+		@IThemeService themeService: IThemeService,
+		@IViewDescriptorService viewDescriptorService: IViewDescriptorService,
+	) {
+		super(options, keybindingService, contextMenuService, configurationService, contextKeyService, viewDescriptorService, instantiationService, openerService, themeService, telemetryService);
+		this._register(this.onDidChangeBodyVisibility(() => this.onDidChangeVisibility(this.isBodyVisible())));
+	}
+
+	public override dispose(): void {
+		if (this.positronReactRenderer) {
+			this.positronReactRenderer.destroy();
+			this.positronReactRenderer = undefined;
+		}
+
+		super.dispose();
+	}
+
+	//#endregion Constructor & Dispose
+
+	//#region Protected Overrides
+
+	protected override renderBody(container: HTMLElement): void {
+		// Call the base class's method.
+		super.renderBody(container);
+
+		// Render the component.
+		this.positronReactRenderer = new PositronReactRenderer(this.element);
+		this.positronReactRenderer.render(
+			<TestContent message='History React' />
+		);
+	}
+
+	//#endregion Protected Overrides
+
+	//#region Public Overrides
+
+	override focus(): void {
+		// Call the base class's method.
+		super.focus();
+	}
+
+	override layoutBody(height: number, width: number): void {
+		super.layoutBody(height, width);
+	}
+
+	//#endregion Public Overrides
+
+	//#region Private Methods
+
+	private onDidChangeVisibility(visible: boolean): void {
+	}
+
+	//#endregion Private Methods
+}
+
