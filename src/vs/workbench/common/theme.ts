305093a5679de3ce966a9b7d33c8b3d66e125dc1
petetronic <1178561+petetronic@users.noreply.github.com>
2023-01-18 11:28:33 -0500

    Fix hover styles on Positron top and side bars.
    Fix incorrect dark background for hover.
    HC themes use outlines, not backgrounds, to show hover.

--- src/vs/workbench/common/theme.ts
+++ src/vs/workbench/common/theme.ts
@@ -1032,10 +1032,10 @@ export const POSITRON_TOP_ACTION_BAR_SELECT_BOX_BACKGROUND = registerColor('posi
 
 // The Positron top action bar hover background color.
 export const POSITRON_TOP_ACTION_BAR_HOVER_BACKGROUND = registerColor('positronTopActionBar.hoverBackground', {
-	dark: '#dae1ea',
+	dark: '#000000',
 	light: '#dae1ea',
-	hcDark: '#dae1ea',
-	hcLight: '#dae1ea'
+	hcDark: null,
+	hcLight: null
 }, localize('positronTopActionBar.hoverBackground', "Positron top action bar hover background color."));
 
 // The Positron top action bar text input background color.
@@ -1114,10 +1114,10 @@ export const POSITRON_SIDE_ACTION_BAR_SEPARATOR = registerColor('positronSideAct
 
 // The Positron side action bar hover background color.
 export const POSITRON_SIDE_ACTION_BAR_HOVER_BACKGROUND = registerColor('positronSideActionBar.hoverBackground', {
-	dark: '#0c0c0d',
+	dark: '#3b4245',
 	light: '#dae1ea',
-	hcDark: '#0c0c0d',
-	hcLight: '#dae1ea'
+	hcDark: null,
+	hcLight: null
 }, localize('positronSideActionBar.hoverBackground', "Positron side action bar hover background color."));
 
 // The Positron side action bar text input background color.
