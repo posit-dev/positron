a79b33fc9775d936ba71498a7c78904815aeba93
Kevin Ushey <kevinushey@gmail.com>
2023-02-07 13:07:04 -0600

    update dev deps

--- extensions/jupyter-adapter/scripts/postinstall.ts
+++ extensions/jupyter-adapter/scripts/postinstall.ts
@@ -6,15 +6,21 @@ import { spawnSync } from 'child_process';
 import { arch, platform } from 'os';
 import { argv, env, exit } from 'process';
 
-// Make sure that zmq produces arm64 builds where appropriate
+// Don't do anything on Windows.
+if (platform() === 'win32') {
+	exit(0);
+}
+
+// Make sure that zmq produces arm64 builds where appropriate.
 if (platform() === 'darwin' && arch() === 'arm64') {
 	env['CMAKE_OSX_ARCHITECTURES'] = 'arm64';
 }
 
-// Ensure that zeromq is built against the right version of node
+// Ensure that zeromq is built against the right version of node.
 const result = spawnSync('electron-rebuild', ['zeromq', ...argv.slice(2)], {
 	encoding: 'utf-8',
-	stdio: 'inherit'
+	stdio: 'inherit',
+	shell: true,
 });
 
 if (result.error || result.status !== 0) {
