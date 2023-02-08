f9a301e5107b33222a6c4be7864e2de04eb56b91
Kevin Ushey <kevinushey@gmail.com>
2023-02-08 11:00:40 -0600

    force arm64 builds on arm64 macOS (#147)
    * rationalize x86_64 arch
    
    * try respecting 'npm_config_arch' envvar
    
    * force arm64 builds on arm64 macOS
    
    Lead-authored-by: Kevin Ushey <kevinushey@gmail.com>
    Co-authored-by: Jonathan McPherson <jonathan@rstudio.com>
    Signed-off-by: Kevin Ushey <kevinushey@gmail.com>

--- extensions/jupyter-adapter/bin/python.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# we use this python shim so that we can inject some required
-# compilation flags when downstream dependencies are built
-#
-# https://github.com/rstudio/positron/issues/26
-export CPPFLAGS="-D__IMMINTRIN_H"
-
-python "$@"
