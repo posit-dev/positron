5d1f86c5561422593b765f8c544cfd0db0e6f39e
Romain François <romain@posit.co>
2023-02-08 16:45:42 +0100

    detect R version major, minor, patch

--- extensions/positron-r/amalthea/crates/ark/src/version.rs
+++ extensions/positron-r/amalthea/crates/ark/src/version.rs
@@ -1,21 +1,22 @@
-// 
+//
 // version.rs
-// 
+//
 // Copyright (C) 2022 by Posit Software, PBC
-// 
-// 
+//
+//
 
 use std::process::Command;
 
+#[allow(dead_code)]
 pub struct RVersion {
     // Major version of the R installation (TODO)
-    // major: u32,
+    major: u32,
 
     // Minor version of the R installation (TODO)
-    // minor: u32,
+    minor: u32,
 
     // Patch version of the R installation (TODO)
-    // patch: u32,
+    patch: u32,
 
     // The full path on disk to the R installation -- that is, the value R_HOME
     // would have inside an R session: > R.home()
@@ -30,13 +31,37 @@ pub fn detect_r() -> RVersion {
         .expect("Failed to execute R to determine R_HOME");
 
     // Convert the output to a string
-    let output = String::from_utf8(output.stdout)
+    let r_home = String::from_utf8(output.stdout)
         .expect("Failed to convert R_HOME output to string")
         .trim()
         .to_string();
 
+    let output = Command::new("R")
+        .arg("-s")
+        .arg("-e")
+        .arg("cat(version$major, \".\", version$minor, sep = \"\")")
+        .output()
+        .expect("Failed to execute R to determine version number");
+
+    let version = String::from_utf8(output.stdout)
+        .expect("Failed to convert R version number to a string")
+        .trim()
+        .to_string();
+
+    let mut version = version.split(".");
+
+    let major : u32 = version.next().unwrap().parse().unwrap();
+    let minor : u32 = version.next().unwrap().parse().unwrap();
+    let patch : u32 = version.next().unwrap().parse().unwrap();
+
     // Execute the R script to get the home path to R
     RVersion{
-        r_home: output
+        major, minor, patch, r_home
     }
+}
+
+#[test]
+fn test_detect_r() {
+    let version = detect_r();
+    println!("{}.{}.{}", version.major, version.minor, version.patch);
 }
