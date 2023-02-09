bd36e36faff596146443ca619fbb2228f0a4e038
Brian Lambert <brianlambert@gmail.com>
2023-02-08 20:28:09 -0700

    Fix quotes

--- build/npm/positron-postinstall.js
+++ build/npm/positron-postinstall.js
@@ -6,7 +6,7 @@ const cp = require('child_process');
 const { platform } = require('os');
 
 // Based on platform, install the post-install dependencies.
-console.log(`Installing Positron post-install dependencies...`);
+console.log('Installing Positron post-install dependencies...');
 if (platform() === 'darwin') {
 	cp.execSync('./scripts/install-python-dependencies.sh', { cwd: 'extensions/positron-python' });
 } else if (platform() === 'win32') {
