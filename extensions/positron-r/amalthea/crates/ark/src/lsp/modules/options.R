9a63f937118cef4ac09ee23b76ff9522fd157a9b
Romain François <romain@posit.co>
2023-02-09 19:14:07 +0100

    set cran mirror to cran.rstudio.com (#142)
    * set cran mirror to cran.rstudio.com
    
    * comment
    
    * Update extensions/positron-r/amalthea/crates/ark/src/lsp/modules/options.R
    
    Co-authored-by: Kevin Ushey <kevin@posit.co>
    
    * handle more cases
    
    ---------
    
    Co-authored-by: Kevin Ushey <kevin@posit.co>

--- extensions/positron-r/amalthea/crates/ark/src/lsp/modules/options.R
+++ extensions/positron-r/amalthea/crates/ark/src/lsp/modules/options.R
@@ -12,3 +12,23 @@ options(help_type = "html")
 options(browser = function(url) {
     .Call("ps_browse_url", as.character(url), PACKAGE = "(embedding)")
 })
+
+# Set cran mirror
+local({
+    repos <- getOption("repos")
+    rstudio_cran <- "https://cran.rstudio.com/"
+
+    if (is.null(repos) || !is.character(repos)) {
+        options(repos = c(CRAN = rstudio_cran))
+    } else {
+        if ("CRAN" %in% names(repos)) {
+            if (identical(repos[["CRAN"]], "@CRAN@")) {
+                repos[["CRAN"]] <- rstudio_cran
+                options(repos = repos)
+            }
+        } else {
+            repos <- c(CRAN = rstudio_cran, repos)
+            options(repos = repos)
+        }
+    }
+})
