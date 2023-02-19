d836f595244b4d418d9a33bc8d556ddb1a2174a4
Pete <1178561+petetronic@users.noreply.github.com>
2023-02-18 19:12:00 -0500

    Normalize Positron copyright headers using new approved format (#190)
    * Normalize Positron copyright headers using new approved format
    Creation year was determined using a best effort with git log:
    ```
    git log --follow --format=%ad --date default <FILE> | tail -1
    ```
    
    Issue: #8
    * Also normalize rust copyright text to match approved format.

--- extensions/positron-r/amalthea/crates/amalthea/src/wire/execute_reply.rs
+++ extensions/positron-r/amalthea/crates/amalthea/src/wire/execute_reply.rs
@@ -1,7 +1,7 @@
 /*
  * execute_reply.rs
  *
- * Copyright (C) 2022 by Posit Software, PBC
+ * Copyright (C) 2022 Posit Software, PBC. All rights reserved.
  *
  */
 
--- extensions/positron-r/amalthea/crates/ark/src/interface.rs
+++ extensions/positron-r/amalthea/crates/ark/src/interface.rs
@@ -1,7 +1,7 @@
 //
 // r_interface.rs
 //
-// Copyright (C) 2022 by Posit Software, PBC
+// Copyright (C) 2022 Posit Software, PBC. All rights reserved.
 //
 //
 
