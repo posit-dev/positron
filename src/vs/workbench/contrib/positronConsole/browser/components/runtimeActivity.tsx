aa389b87d72e8aaa2efcc13b2557f4b6bfee5f98
Brian Lambert <brianlambert@gmail.com>
2023-02-22 12:23:58 -0500

    Refactoring names

---
+++ src/vs/workbench/contrib/positronConsole/browser/components/runtimeActivity.tsx
@@ -0,0 +1,54 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import 'vs/css!./runtimeActivity';
+import * as React from 'react';
+import { ActivityItem } from 'vs/workbench/services/positronConsole/common/classes/activityItem';
+import { ActivityItemError } from 'vs/workbench/services/positronConsole/common/classes/ativityItemError';
+import { ActivityItemInput } from 'vs/workbench/services/positronConsole/common/classes/activityItemInput';
+import { ActivityItemOutput } from 'vs/workbench/services/positronConsole/common/classes/activityItemOutput';
+import { RuntimeItemActivity } from 'vs/workbench/services/positronConsole/common/classes/runtimeItemActivity';
+import { ActivityInput } from 'vs/workbench/contrib/positronConsole/browser/components/activityInput';
+import { ActivityOutput } from 'vs/workbench/contrib/positronConsole/browser/components/activityOutput';
+import { ActivityError } from 'vs/workbench/contrib/positronConsole/browser/components/activityError';
+
+// RuntimeActivityProps interface.
+export interface RuntimeActivityProps {
+	runtimeItemActivity: RuntimeItemActivity;
+}
+
+/**
+ * ReplActivity component.
+ * @param props A RuntimeActivityProps that contains the component properties.
+ * @returns The rendered component.
+ */
+export const RuntimeActivity = ({ runtimeItemActivity }: RuntimeActivityProps) => {
+
+	/**
+	 * Renders am activity item.
+	 * @param activityItem The activity item.
+	 * @returns The rendered activity item.
+	 */
+	const renderActivityItem = (activityItem: ActivityItem) => {
+		if (activityItem instanceof ActivityItemInput) {
+			return <ActivityInput key={activityItem.id} activityItemInput={activityItem} />;
+		} else if (activityItem instanceof ActivityItemOutput) {
+			return <ActivityOutput key={activityItem.id} activityItemOutput={activityItem} />;
+		} else if (activityItem instanceof ActivityItemError) {
+			return <ActivityError key={activityItem.id} activityItemError={activityItem} />;
+		} else {
+			// This indicates a bug.
+			return null;
+		}
+	};
+
+	// Render.
+	return (
+		<div className='runtime-activity'>
+			{runtimeItemActivity.activityItems.map(activityItem =>
+				renderActivityItem(activityItem)
+			)}
+		</div>
+	);
+};
