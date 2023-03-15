ceb7f37faf27c077337f16b58eba7c09065e2291
Jonathan McPherson <jonathan@rstudio.com>
2023-03-15 15:31:36 -0700

    use client supplied ID (extension side)

--- extensions/positron-zed/src/positronZedLanguageRuntime.ts
+++ extensions/positron-zed/src/positronZedLanguageRuntime.ts
@@ -656,9 +656,11 @@ export class PositronZedLanguageRuntime implements positron.LanguageRuntime {
 
 	/**
 	 * Create a new instance of a client.
+	 *
+	 * @param id The ID of the client.
 	 * @param type The runtime client type.
 	 */
-	createClient(type: positron.RuntimeClientType): Promise<string> {
+	async createClient(id: string, type: positron.RuntimeClientType, _params: any) {
 		if (type === positron.RuntimeClientType.Environment) {
 			// Allocate a new ID and ZedEnvironment object for this environment backend
 			const env = new ZedEnvironment(randomUUID(), this.metadata.languageVersion);
@@ -666,13 +668,10 @@ export class PositronZedLanguageRuntime implements positron.LanguageRuntime {
 			// Connect it and save the instance to coordinate future communication
 			this.connectEnvironmentEmitter(env);
 			this._environments.set(env.id, env);
-
-			// Return the ID of the newly created environment backend
-			return Promise.resolve(env.id);
+		} else {
+			// All other types are unknown to Zed
+			throw new Error(`Unknown client type ${type}`);
 		}
-
-		// All other types are unknown to Zed
-		return Promise.reject(`Unknown client type ${type}`);
 	}
 
 	/**
