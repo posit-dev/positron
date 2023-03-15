d59ef64ac76ad72a11c41b999ab2f9542c7767b3
Jonathan McPherson <jonathan@rstudio.com>
2023-03-15 13:18:34 -0700

    send data at startup with 3 sample variables

--- extensions/positron-zed/src/positronZedEnvironment.ts
+++ extensions/positron-zed/src/positronZedEnvironment.ts
@@ -6,7 +6,18 @@ import * as vscode from 'vscode';
 import * as positron from 'positron';
 
 /**
- * ZedEnvironment is a synthetic environment backend for the Zed language.
+ * ZedVar is a simple Zed variable.
+ */
+class ZedVariable {
+	constructor(
+		readonly name: string,
+		readonly value: string,
+		readonly kind: string
+	) { }
+}
+
+/**
+ * ZedEnvironment is a synthetic environment backend for the Zed language containing a set of ZedVariables.
  */
 export class ZedEnvironment {
 
@@ -16,10 +27,57 @@ export class ZedEnvironment {
 	private readonly _onDidEmitData = new vscode.EventEmitter<object>();
 	onDidEmitData: vscode.Event<object> = this._onDidEmitData.event;
 
+	/**
+	 * A map of variable names to their respective metadata
+	 */
+	private readonly _vars = new Map<string, ZedVariable>();
+
+	/**
+	 * Creates a new ZedEnvironment backend
+	 *
+	 * @param id The ID of the environment client instance
+	 */
 	constructor(readonly id: string) {
+		// Create a few variables to start with
+		this._vars.set('z', new ZedVariable('Z', 'zed1', 'string'));
+		this._vars.set('e', new ZedVariable('Z', 'zed2', 'string'));
+		this._vars.set('d', new ZedVariable('Z', 'zed3', 'string'));
+
+		setTimeout(() => {
+			// List the environment on the first tick after startup. There's no
+			// reason we couldn't do this immediately, but waiting a tick simulates the
+			// behavior of a "real" language more accuratley.
+
+			this.emitFullList();
+		});
 	}
 
-	public handleMessage(message: object) {
-		// TODO
+	/**
+	 * Handles an incoming message from the Positron front end
+	 *
+	 * @param message The message to handle
+	 */
+	public handleMessage(message: any) {
+		switch (message.msg_type) {
+
+			// A request to refresh the environment by sending a full list to the front end
+			case 'refresh':
+				this.emitFullList();
+				break;
+		}
+	}
+
+	/**
+	 * Emits a full list of variables to the front end
+	 */
+	private emitFullList() {
+		// Create a list of all the variables in the environment
+		const vars = Array.from(this._vars.values());
+
+		// Emit the data to the front end
+		this._onDidEmitData.fire({
+			msg_type: 'list',
+			vars: vars
+		});
 	}
 }
