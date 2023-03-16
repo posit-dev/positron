18cffb7d26a9acdd1a0fa6b88ed8c959710b3f15
Brian Lambert <brianlambert@gmail.com>
2023-03-16 17:35:09 -0500

    Work on Positron Environment to store environment variables in the PositronEnvironmentInstance

---
+++ src/vs/workbench/services/positronEnvironment/common/classes/environmentVariable.ts
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+/**
+ * EnvironmentVariable class.
+ */
+export class EnvironmentVariable {
+	//#region Constructor
+
+	/**
+	 * Constructor.
+	 * @param id The identifier.
+	 * @param name The name.
+	 */
+	constructor(readonly id: string, readonly name: string) {
+	}
+
+	//#endregion Constructor
+}
