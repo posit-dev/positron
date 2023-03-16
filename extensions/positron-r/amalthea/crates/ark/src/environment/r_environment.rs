0baa76a28bd49aae4a3932e6b165d0e182dbf9c2
Romain François <romain@posit.co>
2023-03-16 17:01:20 +0100

    using select!

--- extensions/positron-r/amalthea/crates/ark/src/environment/r_environment.rs
+++ extensions/positron-r/amalthea/crates/ark/src/environment/r_environment.rs
@@ -7,6 +7,7 @@
 use std::thread;
 
 use amalthea::comm::comm_channel::CommChannelMsg;
+use crossbeam::channel::select;
 use crossbeam::channel::unbounded;
 use crossbeam::channel::Receiver;
 use crossbeam::channel::Sender;
@@ -109,6 +110,76 @@ impl REnvironment {
         // Main message processing loop; we wait here for messages from the
         // front end and loop as long as the channel is open
         loop {
+
+            select! {
+                recv(&prompt_signal_rx) -> msg => {
+                    if let Ok(()) = msg {
+                        self.update();
+                    }
+                },
+
+                recv(&self.channel_msg_rx) -> msg => {
+                    let msg = match msg {
+                        Ok(msg) => msg,
+                        Err(e) => {
+                            // We failed to receive a message from the front end. This
+                            // is usually not a transient issue and indicates that the
+                            // channel is closed, so allowing the thread to exit is
+                            // appropriate. Retrying is likely to just lead to a busy
+                            // loop.
+                            error!(
+                                "Environment: Error receiving message from front end: {:?}",
+                                e
+                            );
+
+                            break;
+                        },
+                    };
+                    debug!("Environment: Received message from front end: {:?}", msg);
+
+                    // Break out of the loop if the front end has closed the channel
+                    if msg == CommChannelMsg::Close {
+                        debug!("Environment: Closing down after receiving comm_close from front end.");
+
+                        // Remember that the user initiated the close so that we can
+                        // avoid sending a duplicate close message from the back end
+                        user_initiated_close = true;
+                        break;
+                    }
+
+                    // Process ordinary data messages
+                    if let CommChannelMsg::Data(data) = msg {
+                        let message = match serde_json::from_value::<EnvironmentMessage>(data) {
+                            Ok(m) => m,
+                            Err(err) => {
+                                error!(
+                                    "Environment: Received invalid message from front end. {}",
+                                    err
+                                );
+                                continue;
+                            },
+                        };
+
+                        // Match on the type of data received.
+                        match message {
+                            // This is a request to refresh the environment list, so
+                            // perform a full environment scan and deliver to the
+                            // front end
+                            EnvironmentMessage::Refresh => {
+                                self.refresh();
+                            },
+
+                            _ => {
+                                error!(
+                                    "Environment: Don't know how to handle message type '{:?}'",
+                                    message
+                                );
+                            },
+                        }
+                    }
+                }
+            }
+
             let mut sel = Select::new();
 
             // Listen to the comm
@@ -128,64 +199,7 @@ impl REnvironment {
                 continue;
             }
 
-            let msg = match oper.recv(&self.channel_msg_rx) {
-                Ok(msg) => msg,
-                Err(e) => {
-                    // We failed to receive a message from the front end. This
-                    // is usually not a transient issue and indicates that the
-                    // channel is closed, so allowing the thread to exit is
-                    // appropriate. Retrying is likely to just lead to a busy
-                    // loop.
-                    error!(
-                        "Environment: Error receiving message from front end: {:?}",
-                        e
-                    );
 
-                    break;
-                },
-            };
-            debug!("Environment: Received message from front end: {:?}", msg);
-
-            // Break out of the loop if the front end has closed the channel
-            if msg == CommChannelMsg::Close {
-                debug!("Environment: Closing down after receiving comm_close from front end.");
-
-                // Remember that the user initiated the close so that we can
-                // avoid sending a duplicate close message from the back end
-                user_initiated_close = true;
-                break;
-            }
-
-            // Process ordinary data messages
-            if let CommChannelMsg::Data(data) = msg {
-                let message = match serde_json::from_value::<EnvironmentMessage>(data) {
-                    Ok(m) => m,
-                    Err(err) => {
-                        error!(
-                            "Environment: Received invalid message from front end. {}",
-                            err
-                        );
-                        continue;
-                    },
-                };
-
-                // Match on the type of data received.
-                match message {
-                    // This is a request to refresh the environment list, so
-                    // perform a full environment scan and deliver to the
-                    // front end
-                    EnvironmentMessage::Refresh => {
-                        self.refresh();
-                    },
-
-                    _ => {
-                        error!(
-                            "Environment: Don't know how to handle message type '{:?}'",
-                            message
-                        );
-                    },
-                }
-            }
         }
 
         SIGNALS.console_prompt.remove(listen_id);
