67b1be569a58500320cdf3712f6b59fc085ff613
Brian Lambert <brianlambert@gmail.com>
2023-04-06 15:36:43 -0500

    Partway through some updates to how environment works

---
+++ src/vs/workbench/services/positronEnvironment/common/interfaces/environmentVariableGroup.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+/**
+ * IEnvironmentVariableGroup interface.
+ */
+export interface IEnvironmentVariableGroup {
+	id: string;
+	title: string;
+	expanded: boolean;
+}
