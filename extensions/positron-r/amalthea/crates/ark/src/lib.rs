905b67a8b38a1378352b3183ad6998b84d2bebaa
Kevin Ushey <kevinushey@gmail.com>
2023-04-07 22:02:54 -0700

    initial diagnostic work

--- extensions/positron-r/amalthea/crates/ark/src/lib.rs
+++ extensions/positron-r/amalthea/crates/ark/src/lib.rs
@@ -15,3 +15,87 @@ pub mod plots;
 pub mod request;
 pub mod shell;
 pub mod version;
+
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Default, Deserialize, Serialize)]
+pub struct Position {
+    row: usize,
+    column: usize,
+}
+
+impl From<Position> for tree_sitter::Point {
+    fn from(value: Position) -> Self {
+        Self {
+            row: value.row,
+            column: value.column,
+        }
+    }
+}
+
+impl From<tree_sitter::Point> for Position {
+    fn from(value: tree_sitter::Point) -> Self {
+        Self {
+            row: value.row,
+            column: value.column,
+        }
+    }
+}
+
+impl From<Position> for tower_lsp::lsp_types::Position {
+    fn from(value: Position) -> Self {
+        Self {
+            line: value.row as u32,
+            character: value.column as u32,
+        }
+    }
+}
+
+impl From<tower_lsp::lsp_types::Position> for Position {
+    fn from(value: tower_lsp::lsp_types::Position) -> Self {
+        Self {
+            row: value.line as usize,
+            column: value.character as usize,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Default, Deserialize, Serialize)]
+pub struct Range {
+    start: Position,
+    end: Position,
+}
+
+impl Range {
+    pub fn new(start: Position, end: Position) -> Self {
+        Self { start, end }
+    }
+}
+
+impl From<tree_sitter::Range> for Range {
+    fn from(value: tree_sitter::Range) -> Self {
+        Self {
+            start: value.start_point.into(),
+            end: value.end_point.into(),
+        }
+    }
+}
+
+impl From<Range> for tower_lsp::lsp_types::Range {
+    fn from(value: Range) -> Self {
+        Self {
+            start: value.start.into(),
+            end: value.end.into(),
+        }
+    }
+}
+
+impl From<tower_lsp::lsp_types::Range> for Range {
+    fn from(value: tower_lsp::lsp_types::Range) -> Self {
+        Self {
+            start: value.start.into(),
+            end: value.end.into(),
+        }
+    }
+}
--- extensions/positron-r/amalthea/crates/ark/src/lsp/diagnostics.rs
+++ extensions/positron-r/amalthea/crates/ark/src/lsp/diagnostics.rs
@@ -5,27 +5,117 @@
 //
 //
 
+use std::sync::atomic::AtomicI32;
+use std::time::Duration;
+
+use stdext::unwrap;
 use tower_lsp::lsp_types::Diagnostic;
-// use tower_lsp::lsp_types::Position;
-// use tower_lsp::lsp_types::Range;
 use tower_lsp::lsp_types::Url;
-use tower_lsp::Client;
+use tree_sitter::Node;
 
-use crate::lsp::documents::Document;
-// use crate::lsp::traits::cursor::TreeCursorExt;
-// use crate::lsp::traits::point::PointExt;
+use crate::lsp::backend::Backend;
+use crate::lsp::traits::cursor::TreeCursorExt;
+use crate::Range;
 
-pub async fn enqueue_diagnostics(client: Client, uri: Url, _doc: &Document) {
-    // TODO: use a timer or something similar to debounce
-    // diagnostics requests
-    let diagnostics: Vec<Diagnostic> = Vec::new();
+static VERSION: AtomicI32 = AtomicI32::new(0);
+
+pub async fn enqueue_diagnostics(backend: Backend, uri: Url, version: i32) {
+    // check diagnostics version
+    let current_version = VERSION.load(std::sync::atomic::Ordering::Acquire);
+    if version < current_version {
+        return;
+    }
+
+    // store the new version
+    VERSION.store(version, std::sync::atomic::Ordering::Release);
+
+    // spawn a new task
+    tokio::spawn(async move {
+        // wait some amount of time
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        // check for cancellation
+        let current_version = VERSION.load(std::sync::atomic::Ordering::Acquire);
+        if version != current_version {
+            return;
+        }
+
+        // okay, it's our chance to provide diagnostics
+        enqueue_diagnostics_impl(backend, uri).await;
+    });
+}
+
+async fn enqueue_diagnostics_impl(backend: Backend, uri: Url) {
+    let mut diagnostics: Vec<Diagnostic> = Vec::new();
     {
-        // TODO: do diagnostics things here
+        // get reference to document
+        let doc = unwrap!(backend.documents.get_mut(&uri), None => {
+            log::error!("diagnostics: no document associated with uri {} available", uri);
+            return;
+        });
+
+        let source = doc.contents.to_string();
+
+        // iterate through nodes and scan
+        let root = doc.ast.root_node();
+        let mut cursor = root.walk();
+        cursor.recurse(|node| {
+            check_unmatched_bracket(node, &source, &mut diagnostics);
+            true
+        });
     }
 
     if diagnostics.is_empty() {
         return;
     }
 
-    client.publish_diagnostics(uri, diagnostics, None).await;
+    backend
+        .client
+        .publish_diagnostics(uri, diagnostics, None)
+        .await;
+}
+
+fn check_unmatched_bracket(node: Node, _source: &str, diagnostics: &mut Vec<Diagnostic>) -> bool {
+    // A complete bracket node should normally have three children:
+    //
+    // - The opening bracket,
+    // - The body of the bracket statement,
+    // - The closing bracket.
+    //
+    // TODO: '[' and '[[' are special, as they require a node prior (since
+    // they're used for subsetting). Similarly, we would need a separate way to
+    // diagnose things like 'a(' where '(' is used for a call rather than just
+    // stand-alone parentheses.
+    let n = node.child_count();
+    if n == 0 {
+        return false;
+    }
+
+    let lhs = node.kind();
+    if !matches!(lhs, "{" | "(" | "[" | "[[") {
+        return false;
+    }
+
+    let rhs = match lhs {
+        "{" => "}",
+        "(" => ")",
+        "[" => "]",
+        "[[" => "]]",
+        _ => return false,
+    };
+
+    let last_child = node.child(n - 1).unwrap();
+    if last_child.kind() == rhs {
+        return false;
+    }
+
+    let child = node.child(0).unwrap();
+    let range: Range = child.range().into();
+    let diagnostic = Diagnostic::new_simple(
+        range.into(),
+        format!("unmatched opening bracket '{}'", node.kind()),
+    );
+    diagnostics.push(diagnostic);
+
+    true
 }
