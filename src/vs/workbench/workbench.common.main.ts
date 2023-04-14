2392f02e0b02e8ea5cb971057457f677e4795e46
Brian Lambert <brianlambert@gmail.com>
2023-04-14 13:43:44 -0500

    Cleanup for Private Alpha

--- src/vs/workbench/workbench.common.main.ts
+++ src/vs/workbench/workbench.common.main.ts
@@ -262,8 +262,8 @@ import 'vs/workbench/contrib/output/browser/outputView';
 // --- Start Positron ---
 import 'vs/workbench/contrib/positronEnvironment/browser/positronEnvironment.contribution';
 import 'vs/workbench/contrib/positronHelp/browser/positronHelp.contribution';
-import 'vs/workbench/contrib/positronHistory/browser/positronHistory.contribution';
-import 'vs/workbench/contrib/positronOutline/browser/positronOutline.contribution';
+// import 'vs/workbench/contrib/positronHistory/browser/positronHistory.contribution';    	Disabled for Private Alpha
+// import 'vs/workbench/contrib/positronOutline/browser/positronOutline.contribution';    	Disabled for Private Alpha
 import 'vs/workbench/contrib/positronPreview/browser/positronPreview.contribution';
 import 'vs/workbench/contrib/positronPlots/browser/positronPlots.contribution';
 // --- End Positron ---
