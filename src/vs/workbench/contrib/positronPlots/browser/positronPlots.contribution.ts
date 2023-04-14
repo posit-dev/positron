03e82fed38781c52a9e9e12c5d47ac62c40458cf
Brian Lambert <brianlambert@gmail.com>
2023-04-14 14:40:41 -0500

    Move plots to be a child of the Environment contribution

--- src/vs/workbench/contrib/positronPlots/browser/positronPlots.contribution.ts
+++ src/vs/workbench/contrib/positronPlots/browser/positronPlots.contribution.ts
@@ -10,32 +10,21 @@ import { registerIcon } from 'vs/platform/theme/common/iconRegistry';
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors';
 import { LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { ViewPaneContainer } from 'vs/workbench/browser/parts/views/viewPaneContainer';
 import { InstantiationType, registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { PositronPlotsViewPane } from 'vs/workbench/contrib/positronPlots/browser/positronPlotsView';
 import { PositronPlotsService } from 'vs/workbench/contrib/positronPlots/browser/positronPlotsService';
 import { IPositronPlotsService, POSITRON_PLOTS_VIEW_ID } from 'vs/workbench/services/positronPlots/common/positronPlots';
 import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions, IWorkbenchContribution } from 'vs/workbench/common/contributions';
-import { ViewContainer, IViewContainersRegistry, ViewContainerLocation, Extensions as ViewContainerExtensions, IViewsRegistry } from 'vs/workbench/common/views';
+import { Extensions as ViewContainerExtensions, IViewsRegistry } from 'vs/workbench/common/views';
 import { registerAction2 } from 'vs/platform/actions/common/actions';
 import { PlotsRefreshAction } from 'vs/workbench/contrib/positronPlots/browser/positronPlotsActions';
+import { VIEW_CONTAINER } from 'vs/workbench/contrib/positronEnvironment/browser/positronEnvironment.contribution';
 
 // Register the Positron plots service.
 registerSingleton(IPositronPlotsService, PositronPlotsService, InstantiationType.Delayed);
 
 // The Positron plots view icon.
 const positronPlotsViewIcon = registerIcon('positron-plots-view-icon', Codicon.positronPlotsView, nls.localize('positronPlotsViewIcon', 'View icon of the Positron plots view.'));
-
-// Register the Positron plots container.
-const VIEW_CONTAINER: ViewContainer = Registry.as<IViewContainersRegistry>(ViewContainerExtensions.ViewContainersRegistry).registerViewContainer({
-	id: POSITRON_PLOTS_VIEW_ID,
-	title: nls.localize('positron.plots', "Plots"),
-	icon: positronPlotsViewIcon,
-	order: 1,
-	ctorDescriptor: new SyncDescriptor(ViewPaneContainer, [POSITRON_PLOTS_VIEW_ID, { mergeViewWithContainerWhenSingleView: true }]),
-	storageId: POSITRON_PLOTS_VIEW_ID,
-	hideIfEmpty: true,
-}, ViewContainerLocation.AuxiliaryBar, { doNotRegisterOpenCommand: true });
 
 Registry.as<IViewsRegistry>(ViewContainerExtensions.ViewsRegistry).registerViews([{
 	id: POSITRON_PLOTS_VIEW_ID,
