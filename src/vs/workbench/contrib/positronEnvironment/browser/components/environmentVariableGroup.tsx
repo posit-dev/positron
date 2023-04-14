010a68d5b84acdbc2f671932920ce7773521a7f4
Brian Lambert <brianlambert@gmail.com>
2023-04-13 19:19:11 -0700

    Add web loading of react-window UMD (Universal Module Definition) file. Add click to select Environment item

--- src/vs/workbench/contrib/positronEnvironment/browser/components/environmentVariableGroup.tsx
+++ src/vs/workbench/contrib/positronEnvironment/browser/components/environmentVariableGroup.tsx
@@ -4,7 +4,7 @@
 
 import 'vs/css!./environmentVariableGroup';
 import * as React from 'react';
-import { CSSProperties, useRef } from 'react'; // eslint-disable-line no-duplicate-imports
+import { CSSProperties, MouseEvent, useRef } from 'react'; // eslint-disable-line no-duplicate-imports
 import { positronClassNames } from 'vs/base/common/positronUtilities';
 import { IEnvironmentVariableGroup } from 'vs/workbench/services/positronEnvironment/common/interfaces/environmentVariableGroup';
 import { IPositronEnvironmentInstance } from 'vs/workbench/services/positronEnvironment/common/interfaces/positronEnvironmentService';
@@ -17,6 +17,7 @@ interface EnvironmentVariableGroupProps {
 	selected: boolean;
 	focused: boolean;
 	style: CSSProperties;
+	onSelected: () => void;
 	positronEnvironmentInstance: IPositronEnvironmentInstance;
 }
 
@@ -30,10 +31,15 @@ export const EnvironmentVariableGroup = (props: EnvironmentVariableGroupProps) =
 	const ref = useRef<HTMLDivElement>(undefined!);
 
 	/**
-	 * Handles onClick events.
+	 * MouseDown handler.
+	 * @param e A MouseEvent<HTMLElement> that describes a user interaction with the mouse.
 	 */
-	const handleClick = () => {
+	const mouseDownHandler = (e: MouseEvent<HTMLElement>) => {
+		e.preventDefault();
+		e.stopPropagation();
+		props.onSelected();
 	};
+
 
 	// Create the class names.
 	const classNames = positronClassNames(
@@ -48,7 +54,7 @@ export const EnvironmentVariableGroup = (props: EnvironmentVariableGroupProps) =
 
 	// Render.
 	return (
-		<div ref={ref} className={classNames} onClick={handleClick} style={props.style}>
+		<div ref={ref} className={classNames} onMouseDown={mouseDownHandler} style={props.style}>
 			{props.environmentVariableGroup.expanded ?
 				<div className={`expand-collapse-icon codicon codicon-chevron-down`} /> :
 				<div className={`expand-collapse-icon codicon codicon-chevron-right`} />
