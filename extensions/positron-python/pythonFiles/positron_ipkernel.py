1db2b9609676abae6ed405c8480962d783e555cf
Pete Farland <pete.farland@posit.co>
2023-04-19 13:41:13 +0000

    Merged PR posit-dev/positron-python#67: Support positron plots comms to render figures at new sizes
    Merge pull request #67 from posit-dev/dynamic-plot-render
    
    Support positron plots comms to render figures at new sizes
    --------------------
    Commit message for posit-dev/positron-python@81b7d94dc6cd9534fca223b8cd2e1e078beaa110:
    
    Adjust dpi and close unpickled figure after render
    
    --------------------
    Commit message for posit-dev/positron-python@9f35693076261a9b4d904a6f23924c5a76516823:
    
    Register our handler without using a decorator
    
    --------------------
    Commit message for posit-dev/positron-python@e1b4fc68054daa23cd5639b0565cd0972d5d2744:
    
    Support positron plots comms to render figures at new sizes
    
    For now we focus on matplotlib based plots. Using a display publisher hook,
    we intercept the iopub based display_data message that shows a plot figure
    and establish a new comm instead. We also pickle the figure details so that
    the client can then request new renderings of the plot figure at new sizes
    through this comm. We currently only return PNG formatted images, though
    other formats could be supported.
    
    For now we try to detect if a specific aspect ratio has been configured by
    the user, and honor it when resizing plot figures.
    
    
    Authored-by: Pete Farland <pete.farland@posit.co>
    Signed-off-by: Pete Farland <pete.farland@posit.co>

--- extensions/positron-python/pythonFiles/positron_ipkernel.py
+++ extensions/positron-python/pythonFiles/positron_ipkernel.py
@@ -4,14 +4,17 @@
 
 """ Positron extensions to the iPython Kernel."""
 
+import codecs
 import enum
 import html
 import inspect
 import logging
 import numbers
+import pickle
 import pprint
 import sys
 import types
+import uuid
 
 from collections.abc import (
     Iterable,
@@ -22,9 +25,10 @@ from collections.abc import (
     Sequence,
     Set
 )
-from ipykernel.ipkernel import IPythonKernel, _get_comm_manager
+from ipykernel.ipkernel import IPythonKernel, comm
+from IPython.core.interactiveshell import InteractiveShell
 from itertools import chain
-from typing import Any
+from typing import Any, Optional
 
 
 @enum.unique
@@ -434,7 +438,10 @@ class NumpyNdarrayInspector(CustomInspector):
 
 
 POSITRON_ENVIRONMENT_COMM = 'positron.environment'
-"""The comm channel target name for Positron's Environment View"""
+"""The comm channel target_name for Positron's Environment View"""
+
+POSITRON_PLOT_COMM = 'positron.plot'
+"""The comm channel target_name for Positron's Plots View"""
 
 MAX_ITEMS = 2000
 TRUNCATE_SUMMARY_AT = 1024
@@ -459,10 +466,22 @@ class PositronIPyKernel(IPythonKernel):
         """Initializes Positron's IPython kernel."""
         super().__init__(**kwargs)
         self.env_comm = None
-        _get_comm_manager().register_target(POSITRON_ENVIRONMENT_COMM, self.environment_comm)
+        self.comm_manager.register_target(POSITRON_ENVIRONMENT_COMM, self.environment_comm)
         self.shell.events.register('pre_execute', self.handle_pre_execute)
         self.shell.events.register('post_execute', self.handle_post_execute)
         self.get_user_ns_hidden().update(POSITON_NS_HIDDEN)
+        # Register a display publisher hook to intercept display_data messages
+        # and establish a comm channel with the frontend.
+        self.display_pub_hook = PositronDisplayPublisherHook()
+        self.shell.display_pub.register_hook(self.display_pub_hook)
+
+    def do_shutdown(self, restart) -> dict:
+        """
+        Handle kernel shutdown.
+        """
+        result = super().do_shutdown(restart)
+        self.display_pub_hook.shutdown()
+        return result
 
     def environment_comm(self, comm, open_msg) -> None:
         """
@@ -1137,3 +1156,201 @@ class PositronIPyKernel(IPythonKernel):
     def get_inspector(self, value) -> CustomInspector:
         qualname = self.get_qualname(value)
         return self.CUSTOM_INSPECTORS.get(qualname, None)
+
+
+class PlotClientMessageImage(dict):
+    """
+    A message used to send rendered plot output.
+
+    Members:
+
+        data: The data for the plot image, as a base64-encoded string. We need
+        to send the plot data as a string because the underlying image file
+        exists only on the machine running the language runtime process.
+
+        mime_type: The MIME type of the image data, e.g. `image/png`. This is
+        used determine how to display the image in the UI.
+    """
+    def __init__(self, data: str, mime_type: str):
+        self['msg_type'] = 'image'
+        self['data'] = data
+        self['mime_type'] = mime_type
+
+
+class PlotClientMessageError(dict):
+    """
+    A message used to send an error message.
+
+    Members:
+
+        message: The error message.
+    """
+    def __init__(self, message: str):
+        self['msg_type'] = 'error'
+        self['message'] = message
+
+
+# Matplotlib Default Figure Size
+DEFAULT_WIDTH_IN = 6.4
+DEFAULT_HEIGHT_IN = 4.8
+BASE_DPI = 96
+
+
+class PositronDisplayPublisherHook:
+
+    def __init__(self):
+        self.comms = {}
+        self.figures = {}
+
+    def __call__(self, msg, *args, **kwargs) -> Optional[dict]:
+
+        if msg['msg_type'] == 'display_data':
+
+            # Try to pickle the current figure
+            pickled = self.pickle_current_figure()
+            if pickled is not None:
+                id = str(uuid.uuid4())
+                self.figures[id] = pickled
+
+                # Create a new comm for each plot figure to allow the client
+                # to request modifications, e.g. render at a new size
+                self.create_comm(id)
+
+                # Returning None stops the display_data message being sent via
+                # the iopub channel
+                return None
+
+        return msg
+
+    def create_comm(self, comm_id: str) -> None:
+        plot_comm = comm.create_comm(target_name=POSITRON_PLOT_COMM, comm_id=comm_id)
+        self.comms[comm_id] = plot_comm
+        plot_comm.on_msg(self.receive_message)
+
+    # Add a callback for received messages.
+    def receive_message(self, msg) -> None:
+        comm_id = msg['content']['comm_id']
+        data = msg['content']['data']
+        msg_type = data.get('msg_type', None)
+
+        if msg_type != 'render':
+            return
+
+        figure_comm = self.comms.get(comm_id, None)
+        if figure_comm is None:
+            logging.warning(f'Plot figure comm {comm_id} not found')
+            return
+
+        pickled = self.figures.get(comm_id, None)
+        if pickled is None:
+            figure_comm.send(PlotClientMessageError(f'Figure {comm_id} not found'))
+            return
+
+        width_px = data.get('width', 0)
+        height_px = data.get('height', 0)
+        pixel_ratio = data.get('pixel_ratio', 1)
+
+        if width_px != 0 and height_px != 0:
+            format_dict, md_dict = self.resize_pickled_figure(pickled, width_px, height_px, pixel_ratio)
+            data = format_dict['image/png']
+            msg_data = PlotClientMessageImage(data, 'image/png')
+            figure_comm.send(data=msg_data, metadata=md_dict)
+
+    def pickle_current_figure(self) -> str:
+
+        pickled = None
+
+        # Delay importing matplotlib until the kernel and shell has been initialized
+        # otherwise the graphics backend will be reset to the gui
+        import matplotlib.pyplot as plt
+
+        # We turn off interactive mode before accessing the plot context
+        was_interactive = plt.isinteractive()
+        plt.ioff()
+
+        # Pickle the current figure
+        figure = plt.gcf()
+        if figure is not None and not self.is_figure_empty(figure):
+            pickled = codecs.encode(pickle.dumps(figure), "base64").decode()
+
+        if was_interactive:
+            plt.ion()
+
+        return pickled
+
+    def resize_pickled_figure(self,
+                              pickled: str,
+                              new_width_px: int = 614,
+                              new_height_px: int = 460,
+                              pixel_ratio: float = 1.0,
+                              formats: list = ['image/png']) -> (dict, dict):
+
+        # Delay importing matplotlib until the kernel and shell has been
+        # initialized otherwise the graphics backend will be reset to the gui
+        import matplotlib.pyplot as plt
+
+        # Turn off interactive mode before, including before unpickling a
+        # figures (otherwise it will cause and endless loop of plot changes)
+        was_interactive = plt.isinteractive()
+        plt.ioff()
+
+        figure = pickle.loads(codecs.decode(pickled.encode(), "base64"))
+
+        # Adjust the DPI based on pixel_ratio to accommodate high
+        # resolution displays...
+        dpi = BASE_DPI * pixel_ratio
+        figure.set_dpi(dpi)
+
+        # ... but use base DPI to convert to inch based dimensions.
+        width_in, height_in = figure.get_size_inches()
+        new_width_in = new_width_px / BASE_DPI
+        new_height_in = new_height_px / BASE_DPI
+
+        # Try to determine if the figure had an explicit width or height set.
+        if width_in == DEFAULT_WIDTH_IN and height_in == DEFAULT_HEIGHT_IN:
+            # If default values are still set, apply new size, even if this
+            # resets the aspect ratio
+            width_in = new_width_in
+            height_in = new_height_in
+        else:
+            # Preserve the existing aspect ratio, constraining the scale
+            # based on the shorter dimension
+            if width_in < height_in:
+                height_in = height_in * (new_width_in / width_in)
+                width_in = new_width_in
+            else:
+                width_in = width_in * (new_height_in / height_in)
+                height_in = new_height_in
+
+        figure.set_size_inches(width_in, height_in)
+
+        format = InteractiveShell.instance().display_formatter.format
+        format_dict, md_dict = format(figure, include=formats, exclude=[])
+
+        plt.close(figure)
+
+        if was_interactive:
+            plt.ion()
+
+        return (format_dict, md_dict)
+
+    def is_figure_empty(self, figure):
+
+        children = figure.get_children()
+        if len(children) < 1:
+            return True
+
+        for child in children:
+            if child.get_visible():
+                return False
+
+        return True
+
+    def shutdown(self) -> None:
+        for figure_comm in self.comms.values():
+            try:
+                figure_comm.close()
+            except Exception:
+                pass
+        self.comms.clear()
+        self.figures.clear()
