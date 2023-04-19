1d9dd4a961831f1eb08aceee1bfb6a3313ec5426
Jonathan McPherson <jonathan@rstudio.com>
2023-04-19 13:24:51 -0700

    add an RPC layer on the ext host instance

--- src/vs/workbench/api/common/positron/extHostClientInstance.ts
+++ src/vs/workbench/api/common/positron/extHostClientInstance.ts
@@ -3,6 +3,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as positron from 'positron';
+import { DeferredPromise } from 'vs/base/common/async';
 import { ILanguageRuntimeMessageCommData, ILanguageRuntimeMessageCommOpen } from 'vs/workbench/services/languageRuntime/common/languageRuntimeService';
 import { Event, EventEmitter } from 'vscode';
 
@@ -13,6 +14,7 @@ export class ExtHostRuntimeClientInstance implements positron.RuntimeClientInsta
 	private readonly _onDidEmitData = new EventEmitter<object>();
 	private _messageCounter = 0;
 	private _state: positron.RuntimeClientState;
+	private _pendingRpcs = new Map<string, DeferredPromise<any>>();
 
 	constructor(readonly message: ILanguageRuntimeMessageCommOpen,
 		readonly sender: ExtHostClientMessageSender) {
@@ -31,7 +33,16 @@ export class ExtHostRuntimeClientInstance implements positron.RuntimeClientInsta
 	 * @param message The message to emit to the client.
 	 */
 	emitMessage(message: ILanguageRuntimeMessageCommData): void {
-		this._onDidEmitData.fire(message.data);
+		// Check to see if this is an RPC response.
+		const rpc = this._pendingRpcs.get(message.parent_id);
+		if (rpc) {
+			// It is, so complete the RPC.
+			this._pendingRpcs.delete(message.parent_id);
+			rpc.complete(message.data);
+		} else {
+			// It isn't; treat it like a regular event.
+			this._onDidEmitData.fire(message.data);
+		}
 	}
 
 	/**
@@ -42,6 +53,30 @@ export class ExtHostRuntimeClientInstance implements positron.RuntimeClientInsta
 	sendMessage(data: object): void {
 		const id = `${this.getClientId()}-${this._messageCounter++}`;
 		this.sender(id, data);
+	}
+
+	/**
+	 * Performs an RPC call to the back end.
+	 */
+	performRpc<T>(data: object): Promise<T> {
+		// Create an RPC ID and a promise to resolve when the RPC is complete.
+		const id = `${this.getClientId()}-${this._messageCounter++}`;
+		const rpc = new DeferredPromise<T>();
+
+		// Add the RPC to the pending RPCs map.
+		this._pendingRpcs.set(id, rpc);
+
+		// Time out the RPC after 10 seconds.
+		setTimeout(() => {
+			if (this._pendingRpcs.has(id)) {
+				this._pendingRpcs.delete(id);
+				rpc.error(new Error('RPC timed out'));
+			}
+		}, 10000);
+
+		// Send the RPC to the back end and return the promise.
+		this.sender(id, data);
+		return rpc.p;
 	}
 
 	onDidChangeClientState: Event<positron.RuntimeClientState>;
