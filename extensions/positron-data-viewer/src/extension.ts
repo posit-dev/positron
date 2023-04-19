2c8178c31578c95bf279660e9c02d84ecc47e1b9
Jonathan McPherson <jonathan@rstudio.com>
2023-04-19 14:05:36 -0700

    add stubs for data viewer extension

---
+++ extensions/positron-data-viewer/src/extension.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { Api } from './Api';
+
+export function activate(_context: vscode.ExtensionContext) {
+}
+
+export function deactivate() {
+}
