689f847c1d36542a55dc316653eeacd857a4847f
Jonathan McPherson <jonathan@rstudio.com>
2023-04-20 15:21:10 -0700

    tidier algorithm for loading react in dev mode

--- extensions/positron-data-viewer/src/extension.ts
+++ extensions/positron-data-viewer/src/extension.ts
@@ -7,7 +7,7 @@ import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.commands.registerCommand('positron.openDataViewer', () => {
+		vscode.commands.registerCommand('positron.openDataViewer', async () => {
 			const panel = vscode.window.createWebviewPanel(
 				'positronDataViewer',
 				'Data Viewer',
@@ -17,23 +17,30 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			);
 
-			let reactPath = vscode.Uri.file(
-				path.join(context.extensionPath, 'ui', 'node_modules', 'react', 'umd', 'react.development.js')
-			);
-			let reactUri = panel.webview.asWebviewUri(reactPath);
-			let reactHtml = `<script src="${reactUri}"></script>\n`;
+			// Check for the node_modules folder in the extension directory
+			const nodeFolder = path.join(context.extensionPath, 'ui', 'node_modules');
+			const developmentMode = await vscode.workspace.fs.stat(vscode.Uri.file(nodeFolder));
+			const scriptPaths = [];
 
-			reactPath = vscode.Uri.file(
-				path.join(context.extensionPath, 'ui', 'node_modules', 'react-dom', 'umd', 'react-dom.development.js')
-			);
-			reactUri = panel.webview.asWebviewUri(reactPath);
-			reactHtml += `<script src="${reactUri}"></script>\n`;
+			if (developmentMode) {
+				// In development mode, we use the React and ReactDOM libraries
+				// from the extension folder directly. In production mode,
+				// webpack bundles these libraries into the index.js file.
+				scriptPaths.push(path.join(nodeFolder,
+					'react', 'umd', 'react.development.js'));
+				scriptPaths.push(path.join(nodeFolder,
+					'react-dom', 'umd', 'react-dom.development.js'));
+			}
 
-			reactPath = vscode.Uri.file(
-				path.join(context.extensionPath, 'ui', 'out', 'index.js')
-			);
-			reactUri = panel.webview.asWebviewUri(reactPath);
-			reactHtml += `<script src="${reactUri}"></script>\n`;
+			// Add the main index.js file
+			scriptPaths.push(path.join(context.extensionPath,
+				'ui', 'out', 'index.js'));
+
+			// Convert each script path to a webview URI
+			const reactHtml = scriptPaths.map((scriptPath) => {
+				const scriptUri = panel.webview.asWebviewUri(vscode.Uri.file(scriptPath));
+				return `<script src="${scriptUri}"></script>`;
+			}).join('\n');
 
 			panel.webview.html = `<body><div id="root"></div></body>${reactHtml}`;
 		})
--- extensions/positron-data-viewer/ui/src/index.tsx
+++ extensions/positron-data-viewer/ui/src/index.tsx
@@ -6,3 +6,4 @@ ReactDOM.render(
 	<h1>Hello, World.</h1>,
 	document.getElementById('root')
 );
+
