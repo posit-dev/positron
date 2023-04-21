310a227243fed9f20ef607fec40987fb9f52fc2a
Brian Lambert <brianlambert@gmail.com>
2023-04-20 17:46:06 -0700

    Fix for empty environment https://github.com/rstudio/positron/issues/418

--- src/vs/workbench/contrib/positronEnvironment/browser/components/emptyEnvironment.tsx
+++ src/vs/workbench/contrib/positronEnvironment/browser/components/emptyEnvironment.tsx
@@ -5,12 +5,21 @@
 import 'vs/css!./emptyEnvironment';
 import * as React from 'react';
 
+// EmptyEnvironmentProps interface.
+interface EmptyEnvironmentProps {
+	initializing: boolean;
+}
+
 /**
  * EmptyEnvironment component.
+ * @param props A EmptyEnvironmentProps that contains the component properties.
  * @returns The rendered component.
  */
-export const EmptyEnvironment = () => {
+export const EmptyEnvironment = (props: EmptyEnvironmentProps) => {
 	return <div className='empty-environment'>
-		<div className='title'>Environment is empty</div>
+		{props.initializing ?
+			<div className='title'>...</div> :
+			<div className='title'>Environment is empty</div>
+		}
 	</div>;
 };
