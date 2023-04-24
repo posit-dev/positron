9486c985a405536e316e69437fea0a38c1936f30
Kevin Ushey <kevinushey@gmail.com>
2023-04-24 11:36:03 -0700

    handle emojis in LSP (closes #499)

--- extensions/positron-r/amalthea/crates/ark/src/lsp/traits/rope.rs
+++ extensions/positron-r/amalthea/crates/ark/src/lsp/traits/rope.rs
@@ -7,23 +7,19 @@
 
 use ropey::Rope;
 use tower_lsp::lsp_types::Position;
-use tree_sitter::Point;
 
 pub trait RopeExt {
-    fn point_to_byte(&self, point: Point) -> usize;
-    fn position_to_byte(&self, position: Position) -> usize;
+    fn position_to_byte(
+        &self,
+        position: Position,
+    ) -> usize;
 }
 
 impl RopeExt for Rope {
-
-    // TODO: We likely need to translate column positions into byte positions,
-    // to properly handle multibyte unicode characters.
-    fn point_to_byte(&self, point: Point) -> usize {
-        self.line_to_byte(point.row) + point.column
-    }
-
-    fn position_to_byte(&self, position: Position) -> usize {
+    fn position_to_byte(
+        &self,
+        position: Position,
+    ) -> usize {
         self.line_to_byte(position.line as usize) + position.character as usize
     }
-
 }
