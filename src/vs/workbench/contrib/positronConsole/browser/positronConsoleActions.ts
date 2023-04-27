20716b55de044c59e619e820f0ab5bbae61a46ea
Brian Lambert <brianlambert@gmail.com>
2023-04-27 12:32:19 -0700

    Small refactorings

--- src/vs/workbench/contrib/positronConsole/browser/positronConsoleActions.ts
+++ src/vs/workbench/contrib/positronConsole/browser/positronConsoleActions.ts
@@ -19,16 +19,24 @@ import { confirmationModalDialog } from 'vs/workbench/browser/positronModalDialo
 import { IExecutionHistoryService } from 'vs/workbench/contrib/executionHistory/common/executionHistoryService';
 import { IPositronConsoleService } from 'vs/workbench/services/positronConsole/common/interfaces/positronConsoleService';
 
+/**
+ * The Positron console view ID.
+ */
 export const POSITRON_CONSOLE_VIEW_ID = 'workbench.panel.positronConsole';
 
+/**
+ * Positron console command ID's.
+ */
 export const enum PositronConsoleCommandId {
-	New = 'workbench.action.positronConsole.new',
 	Open = 'workbench.action.positronConsole.open',
 	ClearConsole = 'workbench.action.positronConsole.clearConsole',
 	ClearInputHistory = 'workbench.action.positronConsole.clearInputHistory',
 	ExecuteCode = 'workbench.action.positronConsole.executeCode'
 }
 
+/**
+ * Positron console action category.
+ */
 export const POSITRON_CONSOLE_ACTION_CATEGORY = localize('positronConsoleCategory', "Console");
 
 /**
