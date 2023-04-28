49d294740e8ce847b9f0ef51a1b5f2f1d912a000
Brian Lambert <brianlambert@gmail.com>
2023-04-27 17:35:46 -0700

    Fix for https://github.com/rstudio/positron/issues/292

--- src/vs/workbench/contrib/positronConsole/browser/components/actionBar.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/components/actionBar.tsx
@@ -4,16 +4,18 @@
 
 import 'vs/css!./actionBar';
 import * as React from 'react';
+import { useEffect, useState } from 'react'; // eslint-disable-line no-duplicate-imports
 import { localize } from 'vs/nls';
 import { IReactComponentContainer } from 'vs/base/browser/positronReactRenderer';
 import { PositronActionBar } from 'vs/platform/positronActionBar/browser/positronActionBar';
 import { ActionBarRegion } from 'vs/platform/positronActionBar/browser/components/actionBarRegion';
 import { ActionBarButton } from 'vs/platform/positronActionBar/browser/components/actionBarButton';
 import { PositronActionBarServices } from 'vs/platform/positronActionBar/browser/positronActionBarState';
-import { ActionBarSeparator } from 'vs/platform/positronActionBar/browser/components/actionBarSeparator';
 import { usePositronConsoleContext } from 'vs/workbench/contrib/positronConsole/browser/positronConsoleContext';
 import { PositronActionBarContextProvider } from 'vs/platform/positronActionBar/browser/positronActionBarContext';
 import { ConsoleInstanceMenuButton } from 'vs/workbench/contrib/positronConsole/browser/components/consoleInstanceMenuButton';
+import { PositronConsoleState } from 'vs/workbench/services/positronConsole/common/interfaces/positronConsoleService';
+import { DisposableStore } from 'vs/base/common/lifecycle';
 
 // Constants.
 const kPaddingLeft = 8;
@@ -30,8 +32,61 @@ interface ActionBarProps {
  * @returns The rendered component.
  */
 export const ActionBar = (props: ActionBarProps) => {
-	// Hooks.
+	// Context hooks.
 	const positronConsoleContext = usePositronConsoleContext();
+
+	// State hooks.
+	const [activePositronConsoleInstance, setActivePositronConsoleInstance] =
+		useState(positronConsoleContext.positronConsoleService.activePositronConsoleInstance);
+	const [interruptable, setInterruptable] = useState(false);
+	const [interrupting, setInterrupting] = useState(false);
+
+	// Main useEffect hook.
+	useEffect(() => {
+		// Register for active Positron console instance changes.
+		positronConsoleContext.positronConsoleService.onDidChangeActivePositronConsoleInstance(
+			activePositronConsoleInstance =>
+				setActivePositronConsoleInstance(activePositronConsoleInstance)
+		);
+	}, []);
+
+	// Active Positron console instance useEffect hook.
+	useEffect(() => {
+		// Create the disposable store for cleanup.
+		const disposableStore = new DisposableStore();
+
+		// If there is an active Positron console instance, register for its onDidChangeState event.
+		if (activePositronConsoleInstance) {
+			disposableStore.add(activePositronConsoleInstance.onDidChangeState(state => {
+				// Track whether the active Positron console instance is interruptable.
+				switch (state) {
+					// The Positron console instance is interruptable.
+					case PositronConsoleState.Busy:
+						setInterruptable(true);
+						setInterrupting(false);
+						break;
+
+					// The Positron console instance is not interruptable.
+					default:
+						setInterruptable(false);
+						setInterrupting(false);
+						break;
+				}
+			}));
+		}
+
+		// Return the cleanup function that will dispose of the disposables.
+		return () => disposableStore.dispose();
+	}, [activePositronConsoleInstance]);
+
+	// Interrupt handler.
+	const interruptHandler = async () => {
+		// Set the interrupting flag to debounch the button.
+		setInterrupting(true);
+
+		// Interrupt the active Positron console instance.
+		activePositronConsoleInstance?.runtime.interrupt();
+	};
 
 	// Toggle trace handler.
 	const toggleTraceHandler = async () => {
@@ -50,10 +105,10 @@ export const ActionBar = (props: ActionBarProps) => {
 				<PositronActionBar size='small' borderTop={true} borderBottom={true} paddingLeft={kPaddingLeft} paddingRight={kPaddingRight}>
 					<ActionBarRegion align='left'>
 						<ConsoleInstanceMenuButton {...props} />
+						{interruptable && <ActionBarButton disabled={interrupting} iconId='positron-interrupt' align='left' tooltip={localize('positronInterrupt', "Interrupt")} onClick={interruptHandler} />}
 					</ActionBarRegion>
 					<ActionBarRegion align='right'>
 						<ActionBarButton iconId='positron-list' align='right' tooltip={localize('positronToggleTrace', "Toggle Trace")} onClick={toggleTraceHandler} />
-						<ActionBarSeparator />
 						<ActionBarButton iconId='positron-clean' align='right' tooltip={localize('positronClearConsole', "Clear console")} onClick={clearConsoleHandler} />
 					</ActionBarRegion>
 				</PositronActionBar>
