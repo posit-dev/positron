e520465a4d7f8602843873371e9095b590eae28d
Brian Lambert <brianlambert@gmail.com>
2023-04-27 19:35:46 -0500

    Fix for https://github.com/rstudio/positron/issues/292

--- src/vs/base/common/codicons.ts
+++ src/vs/base/common/codicons.ts
@@ -602,6 +602,7 @@ export const Codicon = {
 	positronPlots: register('positron-plots', 0xf251),
 	positronPlotsView: register('positron-plots-view', 0xf252),
 	positronEnvironmentSorting: register('positron-environment-sorting', 0xf253),
+	positronInterrupt: register('positron-interrupt', 0xf254),
 
 	// --- End Positron ---
 
