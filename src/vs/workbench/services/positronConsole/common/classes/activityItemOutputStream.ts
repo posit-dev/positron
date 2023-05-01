7c85f54dd6e804a77988257cb66d348527252f55
Brian Lambert <brianlambert@gmail.com>
2023-05-01 15:06:26 -0500

    Refactoring ActivityOutputStreamGroup and ActivityErrorStreamGroup out of existence

--- src/vs/workbench/services/positronConsole/common/classes/activityItemOutputStream.ts
+++ src/vs/workbench/services/positronConsole/common/classes/activityItemOutputStream.ts
@@ -2,10 +2,46 @@
  *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
+import { ANSIOutput, ANSIOutputLine } from 'vs/base/common/ansi/ansiOutput';
+
 /**
  * ActivityItemOutputStream class.
  */
 export class ActivityItemOutputStream {
+	//#region Private Properties
+
+	/**
+	 * Gets the ActivityItemOutputStream array.
+	 */
+	private readonly _activityItemOutputStreams: ActivityItemOutputStream[] = [];
+
+	/**
+	 * Gets or sets the ANSIOutput that is processing the ActivityItemOutputStream array.
+	 */
+	private _ansiOutput: ANSIOutput | undefined;
+
+	//#endregion Private Properties
+
+	//#region Public Properties
+
+	/**
+	 * Gets the output lines.
+	 */
+	get outputLines(): ANSIOutputLine[] {
+		// Lazily process the the ActivityItemOutputStream array.
+		if (!this._ansiOutput) {
+			this._ansiOutput = new ANSIOutput();
+			for (const activityItemOutputStream of this._activityItemOutputStreams) {
+				this._ansiOutput.processOutput(activityItemOutputStream.text);
+			}
+		}
+
+		// Return the output lines.
+		return this._ansiOutput.outputLines;
+	}
+
+	//#endregion Public Properties
+
 	//#region Constructor
 
 	/**
@@ -21,7 +57,23 @@ export class ActivityItemOutputStream {
 		readonly when: Date,
 		readonly text: string
 	) {
+		this._activityItemOutputStreams.push(this);
 	}
 
 	//#endregion Constructor
+
+	//#region Public Methods
+
+	/**
+	 * Adds an ActivityItemOutputStream to this ActivityItemOutputStream.
+	 * @param activityItemOutputStream The ActivityItemOutputStream to add.
+	 */
+	public addActivityItemOutputStream(activityItemOutputStream: ActivityItemOutputStream) {
+		this._activityItemOutputStreams.push(activityItemOutputStream);
+		if (this._ansiOutput && activityItemOutputStream.text) {
+			this._ansiOutput.processOutput(activityItemOutputStream.text);
+		}
+	}
+
+	//#endregion Public Methods
 }
