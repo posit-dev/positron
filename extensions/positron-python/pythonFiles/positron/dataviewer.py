fb49695f1bd42809b0c92d11c64962cba2687070
Pete Farland <pete.farland@posit.co>
2023-05-01 15:58:00 +0000

    Merged PR posit-dev/positron-python#79: Tidy up initial data viewer service tests
    Merge pull request #79 from posit-dev/dataviewer-tests
    
    Tidy up initial data viewer service tests
    --------------------
    Commit message for posit-dev/positron-python@a7e1381775b32b296a3a715b9fcbad48f52ddd67:
    
    Tidy up initial data viewer service tests
    
    
    Authored-by: Pete Farland <pete.farland@posit.co>
    Signed-off-by: Pete Farland <pete.farland@posit.co>

--- extensions/positron-python/pythonFiles/positron/dataviewer.py
+++ extensions/positron-python/pythonFiles/positron/dataviewer.py
@@ -8,20 +8,28 @@ from .utils import json_clean
 from ipykernel.ipkernel import comm
 
 class DataColumn(dict):
-
+    """
+    A single column of data. The viewer deals with data in columnar format since
+    that best matches the way data is stored in most data sources.
+    """
     def __init__(self, name: str, type: str, data: list):
         self['name'] = name
         self['type'] = type
         self['data'] = data
 
 class DataSet(dict):
-
+    """
+    A data set that can be displayed in the data viewer.
+    """
     def __init__(self, id: str, title: str, columns: list):
         self['id'] = id
         self['title'] = title
         self['columns'] = columns
 
 class DataViewerService:
+    """
+    A service to manage the comms and cached datasets for the client data viewer.
+    """
 
     def __init__(self, target_name: str):
         self.comms = {}
@@ -29,6 +37,10 @@ class DataViewerService:
         self.target_name = target_name
 
     def register_dataset(self, dataset: DataSet) -> None:
+        """
+        Register a dataset with the service. This will create a comm for the
+        dataset and cache the dataset for future use.
+        """
         id = dataset.get('id', str(uuid.uuid4()))
         self.datasets[id] = dataset
         self.init_comm(id, dataset)
@@ -46,16 +58,13 @@ class DataViewerService:
        pass
 
     def shutdown(self) -> None:
-        try:
-            for dataview_comm in self.comms.values():
-                try:
-                    dataview_comm.close()
-                except Exception:
-                    pass
-            self.comms.clear()
-            self.datasets.clear()
-        except Exception:
-            pass
+        for dataview_comm in self.comms.values():
+            try:
+                dataview_comm.close()
+            except Exception:
+                pass
+        self.comms.clear()
+        self.datasets.clear()
 
     # -- Private Methods --
 
