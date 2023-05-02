df381c2706f76acabd756ba8bfd1a428480ac638
Brian Lambert <brianlambert@gmail.com>
2023-05-02 16:20:44 -0500

    Prompt work

--- src/vs/workbench/contrib/positronConsole/browser/components/activityPrompt.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/components/activityPrompt.tsx
@@ -4,7 +4,7 @@
 
 import 'vs/css!./activityPrompt';
 import * as React from 'react';
-import { useEffect, useRef } from 'react'; // eslint-disable-line no-duplicate-imports
+import { KeyboardEvent, useEffect, useRef } from 'react'; // eslint-disable-line no-duplicate-imports
 import { OutputRun } from 'vs/workbench/contrib/positronConsole/browser/components/outputRun';
 import { OutputLines } from 'vs/workbench/contrib/positronConsole/browser/components/outputLines';
 import { IPositronConsoleInstance } from 'vs/workbench/services/positronConsole/common/interfaces/positronConsoleService';
@@ -23,28 +23,80 @@ export interface ActivityPromptProps {
  */
 export const ActivityPrompt = (props: ActivityPromptProps) => {
 	// Reference hooks.
-	const inputRef = useRef<HTMLDivElement>(undefined!);
+	const inputRef = useRef<HTMLInputElement>(undefined!);
 
 	// Main useEffect hook.
 	useEffect(() => {
 		// Make sure the input is scrolled into view.
 		inputRef.current?.scrollIntoView({ behavior: 'auto' });
+		inputRef.current.focus();
 	}, [inputRef]);
 
-	// onClick handler (placeholder).
-	const clickHandler = () => {
-		const answer = 'Some Value';
-		props.activityItemPrompt.state = ActivityItemPromptState.Answered;
-		props.activityItemPrompt.answer = !props.activityItemPrompt.password ? answer : '';
-		props.positronConsoleInstance.replyToPrompt(props.activityItemPrompt.id, answer);
+	// // onClick handler (placeholder).
+	// const clickHandler = () => {
+	// 	const answer = 'Some Value';
+	// 	props.activityItemPrompt.state = ActivityItemPromptState.Answered;
+	// 	props.activityItemPrompt.answer = !props.activityItemPrompt.password ? answer : '';
+	// 	props.positronConsoleInstance.replyToPrompt(props.activityItemPrompt.id, answer);
+	// };
+
+	/**
+	 * onKeyDown event handler.
+	 * @param e A KeyboardEvent<HTMLDivElement> that describes a user interaction with the keyboard.
+	 */
+	const keyDownHandler = async (e: KeyboardEvent<HTMLDivElement>) => {
+		/**
+		 * Consumes an event.
+		 */
+		const consumeEvent = () => {
+			e.preventDefault();
+			e.stopPropagation();
+		};
+
+		// Process the key.
+		switch (e.code) {
+			// Enter key.
+			case 'Enter': {
+				// Consume the event.
+				consumeEvent();
+
+				// Upate the prompt state and reply to it.
+				const value = inputRef.current?.value;
+				props.activityItemPrompt.state = ActivityItemPromptState.Answered;
+				props.activityItemPrompt.answer = !props.activityItemPrompt.password ? value : '';
+				props.positronConsoleInstance.replyToPrompt(props.activityItemPrompt.id, value);
+				break;
+			}
+
+			// C key.
+			case 'KeyC': {
+				// Handle Ctrl+C.
+				if (e.ctrlKey) {
+					// Consume the event.
+					consumeEvent();
+
+					// Upate the prompt state and interrupt it.
+					props.activityItemPrompt.state = ActivityItemPromptState.Interrupted;
+					props.positronConsoleInstance.interruptPrompt(props.activityItemPrompt.id);
+				}
+				break;
+			}
+		}
 	};
 
-	// Set the prompt.
+	// Set the prompt for the rendering.
 	let prompt;
 	switch (props.activityItemPrompt.state) {
 		// When the prompt is unanswered, render the input.
 		case ActivityItemPromptState.Unanswered:
-			prompt = <span ref={inputRef} onClick={clickHandler}>[User Input Here]</span>;
+			prompt = (
+				<input
+					ref={inputRef}
+					className='input-field'
+					type={props.activityItemPrompt.password ? 'password' : 'text'}
+					onKeyDown={keyDownHandler}
+				/>
+			);
 			break;
 
 		// When the prompt was answered, render the answer.
@@ -62,14 +114,16 @@ export const ActivityPrompt = (props: ActivityPromptProps) => {
 
 	// Render.
 	return (
-		<>
+		<div className='activity-prompt'>
 			<OutputLines outputLines={props.activityItemPrompt.outputLines.slice(0, -1)} />
-			{props.activityItemPrompt.outputLines.slice(-1).map(outputLine =>
-				outputLine.outputRuns.map(outputRun =>
-					<OutputRun key={outputRun.id} outputRun={outputRun} />
-				)
-			)}
-			{prompt}
-		</>
+			<div className='prompt-line'>
+				{props.activityItemPrompt.outputLines.slice(-1).map(outputLine =>
+					outputLine.outputRuns.map(outputRun =>
+						<OutputRun key={outputRun.id} outputRun={outputRun} />
+					)
+				)}
+				{prompt}
+			</div>
+		</div>
 	);
 };
