5c1c859b2122b75787283c4f65a3761bc01b7970
Brian Lambert <brianlambert@gmail.com>
2023-05-22 18:10:15 -0500

    Load R icon from resources/branding. Also, fix loading of react-windo… (#612)
    * Ensure that the r-icon.svg file gets copied to the release bundle
    
    * Load R icon from resources/branding. Also, fix loading of react-window for web version of the app
    
    Authored-by: Brian Lambert <brianlambert@gmail.com>
    Signed-off-by: Brian Lambert <brianlambert@gmail.com>

---
+++ extensions/positron-r/src/constants.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2022 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as path from 'path';
+
+// The extension root directory.
+export const EXTENSION_ROOT_DIR = path.join(__dirname, '..');
