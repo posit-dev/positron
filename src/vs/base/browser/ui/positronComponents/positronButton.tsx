104602e3ef36a5d0c668b0845c90db46efca9b02
Brian Lambert <brianlambert@gmail.com>
2023-06-02 15:33:49 -0500

    Adding indicator for running interpreter

--- src/vs/base/browser/ui/positronComponents/positronButton.tsx
+++ src/vs/base/browser/ui/positronComponents/positronButton.tsx
@@ -4,11 +4,14 @@
 
 import * as React from 'react';
 import { KeyboardEvent, MouseEvent, PropsWithChildren } from 'react'; // eslint-disable-line no-duplicate-imports
+import { positronClassNames } from 'vs/base/common/positronUtilities';
 
 /**
  * PositronButtonProps interface.
  */
 interface PositronButtonProps {
+	className?: string;
+	disabled?: boolean;
 	onClick: () => void;
 }
 
@@ -17,7 +20,7 @@ interface PositronButtonProps {
  * @param props A PropsWithChildren<PositronButtonProps> that contains the component properties.
  * @returns The rendered component.
  */
-export const PositronButton = ({ onClick, children }: PropsWithChildren<PositronButtonProps>) => {
+export const PositronButton = (props: PropsWithChildren<PositronButtonProps>) => {
 	/**
 	 * onKeyDown event handler.
 	 * @param e A MouseEvent<HTMLDivElement> that describes a user interaction with the mouse.
@@ -30,8 +33,10 @@ export const PositronButton = ({ onClick, children }: PropsWithChildren<Positron
 				e.preventDefault();
 				e.stopPropagation();
 
-				// Raise the click event.
-				onClick();
+				// Raise the click event if the button isn't disabled.
+				if (!props.disabled) {
+					props.onClick();
+				}
 				break;
 		}
 	};
@@ -45,14 +50,21 @@ export const PositronButton = ({ onClick, children }: PropsWithChildren<Positron
 		e.preventDefault();
 		e.stopPropagation();
 
-		// Raise the click event.
-		onClick();
+		// Raise the click event if the button isn't disabled.
+		if (!props.disabled) {
+			props.onClick();
+		}
 	};
 
 	// Render.
 	return (
-		<div className='positron-button' tabIndex={0} role='button' onKeyDown={keyDownHandler} onClick={clickHandler}>
-			{children}
+		<div
+			className={positronClassNames(props.className, { 'disabled': props.disabled })}
+			tabIndex={0}
+			role='button'
+			onKeyDown={keyDownHandler}
+			onClick={clickHandler}>
+			{props.children}
 		</div>
 	);
 };
--- src/vs/workbench/browser/parts/positronTopActionBar/modalPopups/secondaryInterpreter.tsx
+++ src/vs/workbench/browser/parts/positronTopActionBar/modalPopups/secondaryInterpreter.tsx
@@ -4,9 +4,11 @@
 
 import 'vs/css!./secondaryInterpreter';
 import * as React from 'react';
-import { MouseEvent } from 'react'; // eslint-disable-line no-duplicate-imports
+import { useEffect, useState } from 'react'; // eslint-disable-line no-duplicate-imports
+import { DisposableStore } from 'vs/base/common/lifecycle';
+import { PositronButton } from 'vs/base/browser/ui/positronComponents/positronButton';
 import { InterpreterActions } from 'vs/workbench/browser/parts/positronTopActionBar/modalPopups/interpreterActions';
-import { ILanguageRuntime, ILanguageRuntimeService } from 'vs/workbench/services/languageRuntime/common/languageRuntimeService';
+import { ILanguageRuntime, ILanguageRuntimeService, RuntimeState } from 'vs/workbench/services/languageRuntime/common/languageRuntimeService';
 
 /**
  * SecondaryInterpreterProps interface.
@@ -24,23 +26,32 @@ interface SecondaryInterpreterProps {
  * @returns The rendered component.
  */
 export const SecondaryInterpreter = (props: SecondaryInterpreterProps) => {
-	/**
-	 * onClick event handler.
-	 * @param e A MouseEvent<HTMLButtonElement> that describes a user interaction with the mouse.
-	 */
-	const clickHandler = (e: MouseEvent<HTMLDivElement>) => {
-		// Consume the event.
-		e.preventDefault();
-		e.stopPropagation();
+	// State hooks.
+	const [runtimeState, setRuntimeState] = useState(props.runtime.getRuntimeState());
 
-		// Activate the runtime.
-		props.onActivate();
-	};
+	// Main useEffect hook.
+	useEffect(() => {
+		// Create the disposable store for cleanup.
+		const disposableStore = new DisposableStore();
+
+		// Add the onDidChangeRuntimeState event handler.
+		disposableStore.add(props.runtime.onDidChangeRuntimeState(runtimeState => {
+			setRuntimeState(runtimeState);
+		}));
+
+		// Return the cleanup function that will dispose of the event handlers.
+		return () => disposableStore.dispose();
+	}, []);
 
 	// Render.
 	return (
-		<div className='secondary-interpreter' role='button' tabIndex={0} onClick={clickHandler}>
+		<PositronButton className='secondary-interpreter' onClick={props.onActivate}>
 			<div></div>
+			<div className='running-indicator'>
+				{runtimeState !== RuntimeState.Uninitialized && runtimeState !== RuntimeState.Exited &&
+					<div className='running-icon codicon codicon-circle-large-filled'></div>
+				}
+			</div>
 			<img className='icon' src={`data:image/svg+xml;base64,${props.runtime.metadata.base64EncodedIconSvg}`} />
 			<div className='info'>
 				<div className='container'>
@@ -49,6 +60,6 @@ export const SecondaryInterpreter = (props: SecondaryInterpreterProps) => {
 				</div>
 			</div>
 			<InterpreterActions runtime={props.runtime} onStart={props.onStart} />
-		</div>
+		</PositronButton>
 	);
 };
