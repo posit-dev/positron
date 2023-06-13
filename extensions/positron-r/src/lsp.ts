40731af0afda1734515991e7eaf92eef963d99ee
Lionel Henry <lionel.hry@gmail.com>
2023-06-13 12:34:21 -0500

    positron-r: Fix some race conditions in LSP handling (#732)
    * Use parameterised type for promise handles
    
    * Wait for complete initialization before stopping LSP client
    
    Calling `stop()` on an initializing client is UB
    
    * Handle kernel messages in a queue
    
    To avoid handlers running concurrently
    
    Authored-by: Lionel Henry <lionel.hry@gmail.com>
    Signed-off-by: Lionel Henry <lionel.hry@gmail.com>

--- extensions/positron-r/src/lsp.ts
+++ extensions/positron-r/src/lsp.ts
@@ -4,6 +4,7 @@
 
 import * as vscode from 'vscode';
 import * as positron from 'positron';
+import { PromiseHandles } from './util'
 
 import {
 	LanguageClient,
@@ -34,6 +35,9 @@ export class ArkLsp implements vscode.Disposable {
 	private _client?: LanguageClient;
 
 	private _state: LspState = LspState.uninitialized;
+
+	/** Promise that resolves after initialization is complete */
+	private _initializing?: Promise<void>;
 
 	public constructor(private readonly _version: string) {
 	}
@@ -113,6 +117,9 @@ export class ArkLsp implements vscode.Disposable {
 		trace(`Creating Positron R ${this._version} language client...`);
 		this._client = new LanguageClient('positron-r', `Positron R Language Server (${this._version})`, serverOptions, clientOptions);
 
+		const out = new PromiseHandles<void>();
+		this._initializing = out.promise;
+
 		this._client.onDidChangeState(event => {
 			const oldState = this._state;
 			// Convert the state to our own enum
@@ -121,9 +128,18 @@ export class ArkLsp implements vscode.Disposable {
 					this._state = LspState.starting;
 					break;
 				case State.Running:
+					if (this._initializing) {
+						trace(`ARK (R ${this._version}) language client init successful`);
+						this._initializing = undefined;
+						out.resolve();
+					}
 					this._state = LspState.running;
 					break;
 				case State.Stopped:
+					if (this._initializing) {
+						trace(`ARK (R ${this._version}) language client init failed`);
+						out.reject("Ark LSP client stopped before initialization");
+					}
 					this._state = LspState.stopped;
 					break;
 			}
@@ -131,6 +147,8 @@ export class ArkLsp implements vscode.Disposable {
 		});
 
 		context.subscriptions.push(this._client.start());
+
+		await out.promise;
 	}
 
 	/**
@@ -138,14 +156,19 @@ export class ArkLsp implements vscode.Disposable {
 	 *
 	 * @returns A promise that resolves when the client has been stopped.
 	 */
-	public deactivate(): Thenable<void> {
-		if (this._client) {
-			// Stop the client if it's running
-			return this._client.stop();
-		} else {
-			// Otherwise, no client to stop, so just resolve
-			return Promise.resolve();
+	public async deactivate() {
+		if (!this._client) {
+			// No client to stop, so just resolve
+			return;
 		}
+
+		// First wait for initialization to complete.
+		// `stop()` should not be called on a
+		// partially initialized client.
+		await this._initializing;
+
+		// Stop the client if it's running
+		await this._client.stop();
 	}
 
 	/**
