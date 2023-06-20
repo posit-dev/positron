1c8399e97ba8f71468beb29707f6882b2c3703fa
Jonathan McPherson <jonathan@rstudio.com>
2023-06-20 10:56:03 -0700

    use PromiseHandles to store connection promise

--- extensions/jupyter-adapter/src/JupyterSocket.ts
+++ extensions/jupyter-adapter/src/JupyterSocket.ts
@@ -4,10 +4,13 @@
 
 import * as zmq from 'zeromq/v5-compat';
 import * as vscode from 'vscode';
+import { PromiseHandles } from './utils';
 
 export class JupyterSocket implements vscode.Disposable {
 	private readonly _socket: zmq.Socket;
 	private readonly _title: string;
+	private _connectPromise?: PromiseHandles<void>;
+	private _connectStartTime = 0;
 	private _addr: string;
 	private _port: number;
 
@@ -25,6 +28,74 @@ export class JupyterSocket implements vscode.Disposable {
 
 		this._addr = '';
 		this._port = 0;
+
+		// Monitor the socket for events; this is necessary to get events like
+		// `connect` to fire (otherwise we just get `message` events from the
+		// socket)
+		//
+		// We also have to ignore type checking for this line because the type
+		// definitions for `zmq` insist on passing a monitoring interval to
+		// monitor(), but the underlying library ignores the interval and emits
+		// a warning if one is passed.
+		//
+		// @ts-ignore
+		this._socket.monitor();
+
+		// Bind event handlers for the socket
+		this.onConnected = this.onConnected.bind(this);
+		this._socket.on('connect', this.onConnected);
+		this.onConnected = this.onConnected.bind(this);
+		this._socket.on('connect_delay', this.onConnectDelay);
+	}
+
+	/**
+	 * Handles the `connect` event from the ZeroMQ socket
+	 *
+	 * @param _evt ZeroMQ event (ignored)
+	 * @param addr The address the socket connected to
+	 */
+	onConnected(_evt: any, addr: string) {
+		// Ignore if there's no connect promise
+		if (!this._connectPromise) {
+			return;
+		}
+		// Log the connection
+		this._logger(`${this._title} socket connected to ${addr}`);
+
+		// Resolve the promise
+		this._connectPromise.resolve();
+		this._connectPromise = undefined;
+	}
+
+	/**
+	 * Handles the `connect_delay` event from the ZeroMQ socket
+	 *
+	 * @param _evt ZeroMQ event (ignored)
+	 * @param addr The address the socket is attempting to connect to
+	 */
+	onConnectDelay(_evt: any, addr: string) {
+		// Ignore if there's no connect promise
+		if (!this._connectPromise) {
+			return;
+		}
+
+		// We give up if we've exceeded our max wait time
+		const elapsed = Math.round((Date.now() - this._connectStartTime) / 1000);
+		if (this._connectPromise.settled) {
+			// If the promise is settled, we already connected
+			// successfully, so we can stop monitoring the socket and
+			// ignore this event.
+			this._socket.unmonitor();
+		} else if (elapsed > 30) {
+			// Stop monitoring the socket so we don't get any more
+			// events, which would be redundant at this point
+			this._socket.unmonitor();
+			this._logger(`${this._title} socket failed to connect to ${addr} after ${elapsed} seconds`);
+
+			// Reject the promise
+			this._connectPromise.reject(new Error(`Failed to connect to ${addr} after ${elapsed} seconds`));
+			this._connectPromise = undefined;
+		}
 	}
 
 	/**
@@ -45,58 +116,19 @@ export class JupyterSocket implements vscode.Disposable {
 	 * @param port The port to connect to.
 	 */
 	public async connect(port: number) {
-		const maxWaitSeconds = 30;
 		this._port = port;
 		this._addr = 'tcp://127.0.0.1:' + port.toString();
 		this._logger(`${this._title} socket connecting to ${this._addr}...`);
 
-		// Monitor the socket for events; this is necessary to get events like
-		// `connect` to fire (otherwise we just get `message` events from the
-		// socket)
-		//
-		// We also have to ignore type checking for this line because the type
-		// definitions for `zmq` insist on passing a monitoring interval to
-		// monitor(), but the underlying library ignores the interval and emits
-		// a warning if one is passed.
-		//
-		// @ts-ignore
-		this._socket.monitor();
+		// Create a new promise to wait for the socket to connect
+		this._connectStartTime = Date.now();
+		this._connectPromise = new PromiseHandles<void>();
 
-		// Track how long we have been waiting to connect
-		const startTime = Date.now();
+		// Initiate the actual connection to the TCP address
+		this._socket.connect(this._addr);
 
-		// Resolve the promise when the socket connects
-		return new Promise<void>((resolve, reject) => {
-			let settled = false;
-			this._socket.on('connect', (_evt, addr) => {
-				this._logger(`${this._title} socket connected to ${addr}`);
-				settled = true;
-				resolve();
-			});
-
-			// If the socket fails to connect, reject the promise
-			this._socket.on('connect_delay', (_evt, addr) => {
-				// We give up if we've exceeded our max wait time
-				const elapsed = Math.round((Date.now() - startTime) / 1000);
-				if (settled) {
-					// If the promise is settled, we already connected
-					// successfully, so we can stop monitoring the socket and
-					// ignore this event.
-					this._socket.unmonitor();
-				} else if (elapsed > maxWaitSeconds) {
-					// Stop monitoring the socket so we don't get any more
-					// events, which would be redundant at this point
-					this._socket.unmonitor();
-					this._logger(`${this._title} socket failed to connect to ${addr} after ${elapsed} seconds`);
-
-					settled = true;
-					reject();
-				}
-			});
-
-			// Initiate the actual connection to the TCP address
-			this._socket.connect(this._addr);
-		});
+		// Return the promise; we will resolve it when the socket connects
+		return this._connectPromise.promise;
 	}
 
 	/**
@@ -139,6 +171,26 @@ export class JupyterSocket implements vscode.Disposable {
 	 * Cleans up the socket.
 	 */
 	public dispose(): void {
-		this._socket.disconnect(this._addr);
+		this._logger(`${this._title} socket disposing...`);
+
+		// If we were waiting for a connection, reject the promise
+		if (this._connectPromise) {
+			this._connectPromise.reject(new Error('Socket disposed'));
+			this._connectPromise = undefined;
+		}
+
+		// Stop monitoring the socket
+		this._socket.unmonitor();
+
+		// Clean up event handlers
+		this._socket.off('connect', this.onConnected);
+		this._socket.off('connect_delay', this.onConnectDelay);
+
+		// Close the socket if it's not already closed
+		if (!this._socket.closed) {
+			this._logger(`${this._title} socket disposed while open; closing`);
+			this._socket.disconnect(this._addr);
+			this._socket.close();
+		}
 	}
 }
