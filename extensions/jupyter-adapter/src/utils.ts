a71d8218a90a7e4b0ae8c982f0631bc1dd531b7c
Jonathan McPherson <jonathan@rstudio.com>
2023-06-22 11:12:40 -0500

    Improve reliability of restarts (#773)
    * clean out sockets on dispose/reconnect
    
    * fix promise rejection message
    
    * reject shutdown and restart commands when not online
    
    * wait for all sockets to disconnect before kernel is considered exited
    
    * no need to unmonitor since socket is closed
    
    * fully encapsulate socket creation/closure
    
    * add more guards around disconnect
    
    * after connection timeout, restore uninitialized state
    
    * Merge remote-tracking branch 'origin/main' into bugfix/jupyter-kernel-restart
    
    * add additional docs and guards
    
    * reject restarts/shutdowns unless kernel can handle them
    
    * use our own timer instead of relying on `connect_delay`
    
    * Merge remote-tracking branch 'origin/main' into bugfix/jupyter-kernel-restart
    
    * refuse to send messages to disconnect sockets
    
    * fully encapsulate zeromq socket
    
    * ensure disconnection happens on both ends
    
    * track disconnect state at socket level
    
    * use PromiseHandles to store connection promise
    
    * keep track of whether PromiseHandles are settled
    
    * ensure kernel disposal cleans up channels
    
    * Merge remote-tracking branch 'origin/main' into bugfix/jupyter-kernel-restart
    
    * stop monitoring socket after timeout
    
    Authored-by: Jonathan McPherson <jonathan@rstudio.com>
    Signed-off-by: Jonathan McPherson <jonathan@rstudio.com>

--- extensions/jupyter-adapter/src/utils.ts
+++ extensions/jupyter-adapter/src/utils.ts
@@ -2,15 +2,26 @@
  *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
+/**
+ * Similar to the `DeferredPromise` pattern in VS Code, `PromiseHandles` is a
+ * promise with exposed and imperatively callable resolve and reject methods.
+ */
 export class PromiseHandles<T> {
 	resolve!: (value: T | Promise<T>) => void;
 	reject!: (error: unknown) => void;
+	settled: boolean = false;
 	promise: Promise<T>;
 
 	constructor() {
 		this.promise = new Promise((resolve, reject) => {
-			this.resolve = resolve;
-			this.reject = reject;
+			this.resolve = (val) => {
+				this.settled = true;
+				resolve(val);
+			};
+			this.reject = (reason) => {
+				this.settled = true;
+				reject(reason);
+			};
 		});
 	}
 }
