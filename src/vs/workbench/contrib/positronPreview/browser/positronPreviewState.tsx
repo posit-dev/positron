afd2686823663b901bbb3a3049368d5375ce406d
Jonathan McPherson <jonathan@rstudio.com>
2023-06-30 10:25:07 -0700

    cleanup unused copy of preview array in React state

--- src/vs/workbench/contrib/positronPreview/browser/positronPreviewState.tsx
+++ src/vs/workbench/contrib/positronPreview/browser/positronPreviewState.tsx
@@ -2,10 +2,7 @@
  *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
-import { useEffect, useState } from 'react';
-import { DisposableStore } from 'vs/base/common/lifecycle';
 import { IPositronPreviewService } from 'vs/workbench/contrib/positronPreview/browser/positronPreview';
-import { PreviewWebview } from 'vs/workbench/contrib/positronPreview/browser/previewWebview';
 
 /**
  * PositronPreviewServices interface. Defines the set of services that are
@@ -19,7 +16,6 @@ export interface PositronPreviewServices {
  * The Positron preview pane state.
  */
 export interface PositronPreviewState extends PositronPreviewServices {
-	readonly previewWebviews: PreviewWebview[];
 }
 
 /**
@@ -28,27 +24,6 @@ export interface PositronPreviewState extends PositronPreviewServices {
  */
 export const usePositronPreviewState = (services: PositronPreviewServices): PositronPreviewState => {
 
-	// Hooks.
+	return { ...services };
 
-	// Initial set of preview items.
-	const [previewWebviews, setPreviewWebviews] = useState<PreviewWebview[]>(
-		services.positronPreviewService.previewWebviews);
-
-	// Add event handlers.
-	useEffect(() => {
-		const disposableStore = new DisposableStore();
-
-		// Listen for new preview pane items
-		disposableStore.add(services.positronPreviewService.onDidCreatePreviewWebview(item => {
-			// Add the preview instance to the list of preview instances
-			setPreviewWebviews(previewItems => {
-				return [item, ...previewItems];
-			});
-		}));
-
-		// Return the clean up for our event handlers.
-		return () => disposableStore.dispose();
-	}, []);
-
-	return { ...services, previewWebviews };
 };
