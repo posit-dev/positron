550113a8e000fe1c3aab440ddeeb02ec62ef6975
Jackie Gutman <jacqueline.gutman@rstudio.com>
2023-07-18 15:05:14 -0400

    update with major minor logic

--- scripts/node_version_check.sh
+++ scripts/node_version_check.sh
@@ -1,14 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 VERSION=$(node --version)
-MIN_VERSION=v16.17
-MAX_VERSION=v17
+CURRENT_MAJOR=$(echo $VERSION | cut -d. -f1 | grep -o "\d*")
+CURRENT_MINOR=$(echo $VERSION | cut -d. -f2)
 
-echo "Node version: $VERSION"
+RECOMMENDED_VERSION=$(cat ../.nvmrc)
+RECOMMENDED_MAJOR=$(echo $RECOMMENDED_VERSION | cut -d. -f1)
+RECOMMENDED_MINOR=$(echo $RECOMMENDED_VERSION | cut -d. -f2)
 
-if [[ "$VERSION" < "$MIN_VERSION" || "$VERSION" == "$MAX_VERSION"* ]] ; then
-	echo "Node version is not supported. Please use node version between $MIN_VERSION and $MAX_VERSION"
-	exit 1
-else
-	echo "Node version is supported."
+if [[ "$CURRENT_MAJOR" == "$RECOMMENDED_MAJOR" && "$CURRENT_MINOR" == "$RECOMMENDED_MINOR" ]] ; then
+	echo "Node version: $VERSION"
+elif [[ "$CURRENT_MAJOR" == "$RECOMMENDED_MAJOR" ]] ; then
+	echo "Node version: $VERSION ($RECOMMENDED_VERSION is recommended)"
+else # major version mismatch; print a warning in red
+	echo -e "\e[31mNode version: $VERSION may not be supported\e[0m"
+	echo -e "\e[31mConsider using node version $RECOMMENDED_VERSION\e[0m"
 fi
