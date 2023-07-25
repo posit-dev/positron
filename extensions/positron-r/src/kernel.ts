b78c2ce11de94562dc7383a4f2dbefa76dbf0bae
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-07-25 18:57:45 -0500

    Re-factor R installation discovery (#905)
    * Lean more into semver
    
    * Be more robust to weird findings when constructing an R installation
    
    * Refactor R installation discovery
    
    * Import semver and which
    
    Authored-by: Jenny Bryan <jenny.f.bryan@gmail.com>
    Signed-off-by: Jenny Bryan <jenny.f.bryan@gmail.com>

--- extensions/positron-r/src/kernel.ts
+++ extensions/positron-r/src/kernel.ts
@@ -8,6 +8,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as crypto from 'crypto';
 import * as os from 'os';
+import * as semver from 'semver';
+import * as which from 'which';
 
 import { withActiveExtension, delay } from './util';
 import { RRuntime } from './runtime';
@@ -38,110 +40,194 @@ export function adaptJupyterKernel(context: vscode.ExtensionContext, kernelPath:
 	});
 }
 
-function getRVersion(rHome: string): string {
-	// Get the version of the R installation
-	const { execSync } = require('child_process');
-	let rVersion = '';
-	try {
-		rVersion = execSync(
-			`${rHome}/bin/R --vanilla -s -e 'cat(R.Version()$major,R.Version()$minor, sep=".")'`,
-			{ shell: '/bin/bash', encoding: 'utf8' })
-			.trim();
-	} catch (e) {
-		console.error(`Error getting R version: ${e}`);
+// directory where this OS is known to keep its R installations
+function rHeadquarters(): string {
+	switch (process.platform) {
+		case 'darwin':
+			return '/Library/Frameworks/R.framework/Versions';
+		case 'linux':
+			return '/opt/R';
+		default:
+			// TODO: handle Windows
+			throw new Error('Unsupported platform');
 	}
-	return rVersion;
+}
+
+function binFragment(version: string): string {
+	switch (process.platform) {
+		case 'darwin':
+			return `${version}/Resources/bin/R`;
+		case 'linux':
+			return `${version}/bin/R`;
+		default:
+			// TODO: handle Windows
+			throw new Error('Unsupported platform');
+	}
+}
+
+function readLines(pth: string): Array<string> {
+	const bigString = fs.readFileSync(pth, 'utf8');
+	return bigString.split(/\r?\n/);
+}
+
+// extractValue('KEY=VALUE', 'KEY')      --> 'VALUE'
+// extractValue('KEY:VALUE', 'KEY', ':') --> 'VALUE'
+// extractValue('KEE:VALUE', 'KEY')      --> ''
+function extractValue(str: string, key: string, delim: string = '='): string {
+	const re = `${key}${delim}(.*)`;
+	if (!str.startsWith(key)) {
+		return '';
+	}
+	const m = str.match(re);
+	return m?.[1] ?? '';
 }
 
 export function registerArkKernel(ext: vscode.Extension<any>, context: vscode.ExtensionContext, kernelPath: string): void {
 
 	class RInstallation {
-		public readonly rHome: string;
-		public readonly rVersion: string;
+		public readonly binpath: string = '';
+		public readonly homepath: string = '';
+		// The semVersion field was added because changing the version field from a string that's
+		// "major.minor" to an instance of SemVer (conveying major.minor.patch) would have
+		// downstream consequence I don't want to take on now. But we can probably rationalize this
+		// in the future.
+		public readonly semVersion: semver.SemVer = new semver.SemVer('0.0.1');
+		public readonly version: string = '';
+		public readonly arch: string = '';
+		public readonly current: boolean = false;
+		public readonly orthogonal: boolean = false;
 
 		/**
 		 * Represents an installation of R on the user's system.
 		 *
-		 * @param rHome The path to the R installation's home folder
-		 * @param resolved Whether the path has been resolved to a canonical path
+		 * @param pth Filepath for an R "binary" (on macOS and linux, this is actually a
+		 *   shell script)
 		 */
-		constructor(rHome: string, resolved: boolean) {
-			// Form the canonical path to the R installation by asking R itself
-			// for its home directory. This may differ from the path that was
-			// passed since the discovered home path may contain symlinks.
-			if (resolved) {
-				this.rHome = rHome;
-			} else {
-				try {
-					this.rHome = execSync(
-						`${rHome}/R RHOME`, { encoding: 'utf8' })
-						.trim();
-				} catch (e) {
-					console.error(`Error running RHOME command for ${rHome}: ${e}`);
-					this.rHome = rHome;
-				}
+		constructor(pth: string, current: boolean = false) {
+			this.binpath = pth;
+			this.current = current;
+
+			const binLines = readLines(this.binpath);
+			const re = new RegExp('Shell wrapper for R executable');
+			if (!binLines.some(x => re.test(x))) {
+				return;
 			}
-			this.rVersion = getRVersion(rHome);
+			const targetLine = binLines.find(line => line.match('R_HOME_DIR'));
+			if (!targetLine) {
+				return;
+			}
+			// macOS: R_HOME_DIR=/Library/Frameworks/R.framework/Versions/4.3-arm64/Resources
+			// macOS non-orthogonal: R_HOME_DIR=/Library/Frameworks/R.framework/Resources
+			// linux: R_HOME_DIR=/opt/R/4.2.3/lib/R
+			const R_HOME_DIR = extractValue(targetLine, 'R_HOME_DIR');
+			this.homepath = R_HOME_DIR;
+			if (this.homepath === '') {
+				return;
+			}
+
+			// orthogonality is a concern specific to macOS
+			// a non-orthogonal R "binary" is hard-wired to launch the current version of R,
+			// so it only works when it actually is the current version of R
+			// learn more in https://github.com/r-lib/rig/blob/main/src/macos.rs
+			// see is_orthogonal(), make_orthogonal_()
+			const re2 = new RegExp('R[.]framework/Resources');
+			this.orthogonal = !re2.test(this.homepath);
+
+			// make sure to target a base package that contains compiled code, so the
+			// 'Built' field contains the platform info
+			const descPath = path.join(this.homepath, 'library', 'utils', 'DESCRIPTION');
+			const descLines = readLines(descPath);
+			const targetLine2 = descLines.filter(line => line.match('Built'))[0];
+			if (!targetLine2) {
+				return;
+			}
+			// macOS arm64: Built: R 4.3.1; aarch64-apple-darwin20; 2023-06-16 21:52:54 UTC; unix
+			// macOS intel: Built: R 4.3.1; x86_64-apple-darwin20; 2023-06-16 21:51:34 UTC; unix
+			// linux: Built: R 4.2.3; x86_64-pc-linux-gnu; 2023-03-15 09:03:13 UTC; unix
+			const builtField = extractValue(targetLine2, 'Built', ':');
+			const builtParts = builtField.split(new RegExp(';\\s+'));
+
+			const versionPart = builtParts[0];
+			this.semVersion = semver.coerce(versionPart) ?? new semver.SemVer('0.0.1');
+			this.version = `${semver.major(this.semVersion)}.${semver.minor(this.semVersion)}`;
+
+			const platformPart = builtParts[1];
+			const architecture = platformPart.match('^(aarch64|x86_64)');
+			this.arch = architecture ? architecture[1] : '';
 		}
 	}
-
-	const rInstallations: Array<RInstallation> = [];
-
-	const { execSync } = require('child_process');
 
 	// Check the R kernel log level setting
 	const config = vscode.workspace.getConfiguration('positron.r');
 	const logLevel = config.get<string>('kernel.logLevel') ?? 'warn';
 
-	// Discover R installations.
-	// TODO: Needs to handle Linux and Windows. See e.g. how Quarto handles this:
-	// https://github.com/quarto-dev/quarto-cli/blob/main/src/core/resources.ts#L77
-	//
-	// TODO: Needs to handle other installation locations (like RSwitch)
+	const binaries = new Set<string>();
+	let rInstallations: Array<RInstallation> = [];
 
-	// Check the R Versions folder on macOS. Typically, this only contains one
-	// version of R, but it's possible to have multiple versions installed in
-	// cases where a third-party utility such as `rig` is used to manage R
-	// installations.
-	const rVersionsFolder = '/Library/Frameworks/R.framework/Versions';
-	if (fs.existsSync(rVersionsFolder)) {
-		fs.readdirSync(rVersionsFolder).forEach((version: string) => {
-			const rHome = path.join(rVersionsFolder, version, 'Resources');
-			if (fs.existsSync(rHome)) {
-				// Ensure the R binary actually exists at this path; it's possible that
-				// the user has deleted the R installation but left the folder behind.
-				const rBin = path.join(rHome, 'bin', 'R');
-				if (fs.existsSync(rBin)) {
-					rInstallations.push(new RInstallation(rHome, false));
-				}
-			}
-		});
+	// look in the well-known place for R installations on this OS
+	const rHq = rHeadquarters();
+	if (fs.existsSync(rHq)) {
+		const versionBinaries = fs.readdirSync(rHq)
+			// 'Current', if it exists, is a symlink to an actual version. Skip it here to avoid
+			// redundant entries. This is a macOS phenomenon.
+			.filter(v => v !== 'Current')
+			.map(v => path.join(rHq, binFragment(v)))
+			// By default, macOS CRAN installer deletes previous R installations, but sometimes
+			// it doesn't do a thorough job of it and a nearly-empty version directory lingers on.
+			.filter(b => fs.existsSync(b));
+		for (const b of versionBinaries) {
+			binaries.add(b);
+		}
 	}
 
-	// Sort the R installations by version number, descending. If we don't find an R
-	// installation on the $PATH (see below), we get reasonable default behaviour, which
-	// is to use the most recent version of R available.
-	rInstallations.sort((a, b) => {
-		return b.rVersion.localeCompare(a.rVersion);
+	// other places we might find an R binary
+	const possibleBinaries = [
+		'/usr/bin/R',
+		'/usr/local/bin/R',
+		'/opt/local/bin/R',
+		'/opt/homebrew/bin/R'
+	];
+	const moreBinaries = possibleBinaries
+		.filter(b => fs.existsSync(b))
+		.map(b => fs.realpathSync(b));
+	for (const b of moreBinaries) {
+		binaries.add(b);
+	}
+
+	// make sure we include R executable found on the PATH
+	// we've probably already discovered it, but we still need to single it out, so that we mark
+	// that particular R installation as the current one
+	const whichR = which.sync('R', { nothrow: true }) as string;
+	if (whichR) {
+		const whichRCanonical = fs.realpathSync(whichR);
+		rInstallations.push(new RInstallation(whichRCanonical, true));
+		binaries.delete(whichRCanonical);
+	}
+
+	binaries.forEach((b: string) => {
+		rInstallations.push(new RInstallation(b));
 	});
 
-	// Look for an R installation on the $PATH (e.g. installed via Homebrew). If found,
-	// add or move it to the front of the list.
-	try {
-		// Try R RHOME to get the installation path; run under bash so we get $PATH
-		// set as the user would expect.
-		const rHome = execSync('R RHOME', { shell: '/bin/bash', encoding: 'utf8' }).trim();
-		if (fs.existsSync(rHome)) {
-			const loc = rInstallations.findIndex(r => r.rHome === rHome);
-			if (loc < 0) {
-				rInstallations.unshift(new RInstallation(rHome, true));
-			} else {
-				rInstallations.splice(0, 0, rInstallations.splice(loc, 1)[0]);
-			}
+	// TODO: possible future intervention re: non-orthogonal R installations
+	// * Alert the user they have R more installations?
+	// * Offer to make installations orthogonal?
+	// * Offer to switch the current version of R?
+	// for now, we drop non-orthogonal, not-current R installations
+	rInstallations = rInstallations.filter(r => r.current || r.orthogonal);
+
+	// FIXME? should I explicitly check that there is <= 1 R installation
+	// marked as 'current'?
+
+	rInstallations.sort((a, b) => {
+		if (a.current || b.current) {
+			// always put the current R version first
+			return Number(b.current) - Number(a.current);
 		}
-	} catch (err) {
-		// Just swallow this; it's okay if there's no R on the $PATH
-	}
+		// otherwise, sort by version number, descending
+		// break ties by architecture
+		// (currently taking advantage of the fact that 'aarch64' > 'x86_64')
+		return semver.compare(b.semVersion, a.semVersion) || a.arch.localeCompare(b.arch);
+	});
 
 	// Loop over the R installations and create a language runtime for each one.
 	//
@@ -164,7 +250,7 @@ export function registerArkKernel(ext: vscode.Extension<any>, context: vscode.Ex
 		const env = <Record<string, string>>{
 			'RUST_BACKTRACE': '1',
 			'RUST_LOG': logLevel,
-			'R_HOME': rHome.rHome,
+			'R_HOME': rHome.homepath,
 			'R_CLI_NUM_COLORS': '256',
 		};
 		/* eslint-enable */
@@ -172,7 +258,7 @@ export function registerArkKernel(ext: vscode.Extension<any>, context: vscode.Ex
 		if (process.platform === 'darwin') {
 
 			const dyldFallbackLibraryPaths: string[] = [];
-			dyldFallbackLibraryPaths.push(`${rHome.rHome}/lib`);
+			dyldFallbackLibraryPaths.push(`${rHome.homepath}/lib`);
 
 			const defaultDyldFallbackLibraryPath = process.env['DYLD_FALLBACK_LIBRARY_PATH'];
 			if (defaultDyldFallbackLibraryPath) {
@@ -191,17 +277,17 @@ export function registerArkKernel(ext: vscode.Extension<any>, context: vscode.Ex
 
 		// Is the runtime path within the user's home directory?
 		const homedir = os.homedir();
-		const isUserInstallation = rHome.rHome.startsWith(homedir);
+		const isUserInstallation = rHome.homepath.startsWith(homedir);
 
 		// Create the runtime path.
 		// TODO@softwarenerd - We will need to update this for Windows.
 		const runtimePath = os.platform() !== 'win32' && isUserInstallation ?
-			path.join('~', rHome.rHome.substring(homedir.length)) :
-			rHome.rHome;
+			path.join('~', rHome.homepath.substring(homedir.length)) :
+			rHome.homepath;
 
 		// Does the runtime path have 'homebrew' as a component? (we assume that
 		// it's a Homebrew installation if it does)
-		const isHomebrewInstallation = rHome.rHome.includes('/homebrew/');
+		const isHomebrewInstallation = rHome.homepath.includes('/homebrew/');
 
 		const runtimeSource = isHomebrewInstallation ? 'Homebrew' :
 			isUserInstallation ?
@@ -222,7 +308,7 @@ export function registerArkKernel(ext: vscode.Extension<any>, context: vscode.Ex
 		// Get the version of this extension from package.json so we can pass it
 		// to the adapter as the implementation version.
 		const packageJson = require('../package.json');
-		const rVersion = rHome.rVersion ?? '0.0.1';
+		const rVersion = rHome.version;
 
 		// Create a stable ID for the runtime based on the interpreter path and version.
 		const digest = crypto.createHash('sha256');
