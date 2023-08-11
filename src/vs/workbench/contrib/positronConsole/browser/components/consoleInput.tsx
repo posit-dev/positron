49fd98927d525401d915ec540ff4bcc580395cd1
Brian Lambert <brianlambert@gmail.com>
2023-08-11 16:18:41 -0400

    Undo mistaken code

--- src/vs/workbench/contrib/positronConsole/browser/components/consoleInput.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/components/consoleInput.tsx
@@ -565,7 +565,7 @@ export const ConsoleInput = forwardRef<HTMLDivElement, ConsoleInputProps>((props
 			}
 
 			// Reserve appropriate width for the prompt in case width has changed
-			editorOptions.lineNumbersMinChars = props.positronConsoleInstance.runtime.metadata.inputPrompt?.length || 1;
+			editorOptions.lineNumbersMinChars = props.positronConsoleInstance.runtime.dynState.inputPrompt.length;
 			codeEditorWidget.updateOptions({ ...editorOptions });
 
 			// Update the code editor widget options.
