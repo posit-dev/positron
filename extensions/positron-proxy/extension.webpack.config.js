203a3a98df8de3f0d015cab6853a440c0f5a7b3e
Brian Lambert <brianlambert@gmail.com>
2023-08-16 22:14:32 -0400

    I don't know what I'm doing

--- extensions/positron-proxy/extension.webpack.config.js
+++ extensions/positron-proxy/extension.webpack.config.js
@@ -1,18 +1,20 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
 //@ts-check
 
 'use strict';
 
-const path = require('path');
 const withDefaults = require('../shared.webpack.config');
+const path = require('path');
 
 module.exports = withDefaults({
 	context: __dirname,
 	entry: {
 		extension: './src/extension.ts',
 	},
+	node: {
+		__dirname: false
+	}
 });
