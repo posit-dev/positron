a2fe12063660efc2892e166d05e808457bd001d5
Julia Silge <julia.silge@gmail.com>
2023-08-17 20:25:58 -0500

    Add API to get running runtimes (#1104)
    * Merge branch 'main' into list-runtimes-api
    
    * Add API to get running runtimes
    
    Authored-by: Julia Silge <julia.silge@gmail.com>
    Signed-off-by: Julia Silge <julia.silge@gmail.com>

--- src/vs/workbench/api/common/positron/extHost.positron.protocol.ts
+++ src/vs/workbench/api/common/positron/extHost.positron.protocol.ts
@@ -16,7 +16,7 @@ export interface MainThreadLanguageRuntimeShape extends IDisposable {
 	$completeLanguageRuntimeDiscovery(): void;
 	$unregisterLanguageRuntime(handle: number): void;
 	$executeCode(languageId: string, code: string, focus: boolean): Promise<boolean>;
-
+	$getRunningRuntimes(languageId: string): Promise<ILanguageRuntimeMetadata[]>;
 	$emitLanguageRuntimeMessage(handle: number, message: ILanguageRuntimeMessage): void;
 	$emitLanguageRuntimeState(handle: number, clock: number, state: RuntimeState): void;
 }
