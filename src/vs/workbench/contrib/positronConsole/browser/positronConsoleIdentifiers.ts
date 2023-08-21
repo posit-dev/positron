8d9005e79a6a31551377a56bb4a2d4ecdb6a65a7
Brian Lambert <brianlambert@gmail.com>
2023-08-20 20:11:27 -0400

    Fix keyboard shortcut for Cut

--- src/vs/workbench/contrib/positronConsole/browser/positronConsoleIdentifiers.ts
+++ src/vs/workbench/contrib/positronConsole/browser/positronConsoleIdentifiers.ts
@@ -3,7 +3,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 // Identifiers.
-export const POSITRON_CONSOLE_CUT = 'positron.console.copy';
+export const POSITRON_CONSOLE_CUT = 'positron.console.cut';
 export const POSITRON_CONSOLE_COPY = 'positron.console.copy';
 export const POSITRON_CONSOLE_PASTE = 'positron.console.paste';
 export const POSITRON_CONSOLE_SELECT_ALL = 'positron.console.selectAll';
