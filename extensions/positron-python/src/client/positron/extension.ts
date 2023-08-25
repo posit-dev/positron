738c709deec59449845db49a3c04af262038b8a9
Wasim Lorgat <mwlorgat@gmail.com>
2023-08-25 16:36:18 +0000

    Merged PR posit-dev/positron-python#192: fix: register runtimes for newly created environments
    Merge pull request posit-dev/positron-python#192 from posit-dev/register-new-venvs
    
    fix: register runtimes for newly created environments
    --------------------
    Commit message for posit-dev/positron-python@077bf4ec5a2ea45846dbd717003027bf4b2306f9:
    
    Merge pull request posit-dev/positron-python#193 from posit-dev/cleanup-runtime-name
    
    clean up runtime name
    --------------------
    Commit message for posit-dev/positron-python@8d47e796804d05b7691e3bcff56cd0d7fb8bb0ba:
    
    clean up runtime name
    
    This shows like `Python (Venv: venv-name) 3.12.0b1` in the Console and
    Environment runtime pickers.
    
    Addresses https://github.com/rstudio/positron/issues/1119.
    
    --------------------
    Commit message for posit-dev/positron-python@40f7cb116094b3ad5b1574cb9027235843224c12:
    
    fix: register runtimes for newly created environments
    
    Slight refactor as well: I moved the positron extension activation to
    the `activatePositron` function, and ran it in the background like the
    `sendStartupTelemtry` function.
    
    I also had to extract a `createPythonRuntime` function so that we could
    use it to create and register a runtime when a new environment is
    detected. No functionality in `providers.ts` should have actually
    changed.
    
    I've found and tested two ways to add environments via the Python
    extension:
    
    - Create one manually (e.g. via `python -m venv test-venv` and the
      extension will automatically identify it and show a prompt to activate
      it (https://github.com/rstudio/positron/issues/1002).
    - Create one via Cmd+Shift+P > "Python: Create Environment...".
    
    
    Authored-by: Wasim Lorgat <mwlorgat@gmail.com>
    Signed-off-by: Wasim Lorgat <mwlorgat@gmail.com>

---
+++ extensions/positron-python/src/client/positron/extension.ts
@@ -0,0 +1,73 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+// eslint-disable-next-line import/no-unresolved
+import * as positron from 'positron';
+import { PythonExtension } from '../api/types';
+import { IDisposableRegistry } from '../common/types';
+import { IInterpreterService } from '../interpreter/contracts';
+import { IServiceContainer } from '../ioc/types';
+import { traceError } from '../logging';
+import { createPythonRuntime, pythonRuntimeProvider } from './provider';
+
+
+export async function activatePositron(
+    activatedPromise: Promise<void>,
+    pythonApi: PythonExtension,
+    serviceContainer: IServiceContainer
+): Promise<void> {
+
+    try {
+
+        // Wait for all extension components to be activated before starting Positron activation.
+        await activatedPromise;
+
+        // Map of interpreter path to language runtime metadata, used to determine the runtimeId when
+        // switching the active interpreter path.
+        const runtimes = new Map<string, positron.LanguageRuntimeMetadata>();
+
+        // Register the Python language runtime provider with positron.
+        positron.runtime.registerLanguageRuntimeProvider('python', pythonRuntimeProvider(serviceContainer, runtimes));
+
+        // If the interpreter is changed via the Python extension, select the corresponding
+        // language runtime in Positron.
+        const disposables = serviceContainer.get<IDisposableRegistry>(IDisposableRegistry);
+        disposables.push(
+            pythonApi.environments.onDidChangeActiveEnvironmentPath(async (event) => {
+                // Select the new runtime.
+                const runtimeMetadata = runtimes.get(event.path);
+                if (runtimeMetadata) {
+                    positron.runtime.selectLanguageRuntime(runtimeMetadata.runtimeId);
+                } else {
+                    throw Error(`Tried to switch to a language runtime that has not been registered: ${event.path}`);
+                }
+            }),
+        );
+
+        // If a new interpreter is added via the Python extension, create and register a
+        // corresponding language runtime.
+        disposables.push(pythonApi.environments.onDidChangeEnvironments(async (event) => {
+            if (event.type === 'add') {
+                const interpreterPath = event.env.path;
+                if (!runtimes.has(interpreterPath)) {
+                    // Get the interpreter corresponding to the new runtime.
+                    const interpreterService = serviceContainer.get<IInterpreterService>(IInterpreterService);
+                    const interpreter = await interpreterService.getInterpreterDetails(interpreterPath);
+
+                    // Create the runtime and register it with Positron.
+                    if (interpreter) {
+                        const runtime = await createPythonRuntime(interpreter, serviceContainer);
+                        const runtimeMetadata = runtime.metadata;
+                        disposables.push(positron.runtime.registerLanguageRuntime(runtime));
+                        runtimes.set(interpreterPath, runtimeMetadata);
+                    }
+                }
+            }
+        }));
+
+    } catch (ex) {
+        traceError('activatePositron() failed.', ex);
+    }
+
+}
