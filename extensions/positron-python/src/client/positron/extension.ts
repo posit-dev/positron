c5d2690e3822664321cbcd00dbddc102facd420b
Wasim Lorgat <mwlorgat@gmail.com>
2023-09-05 14:55:37 +0000

    Merged PR posit-dev/positron-python#207: add verbose logs to debug runtime registration issues
    Merge pull request #207 from posit-dev/logging
    
    add verbose logs to debug runtime registration issues
    --------------------
    Commit message for posit-dev/positron-python@9f18110d2cb2c01cfc0ec26223a9a8bab08620ac:
    
    add verbose logs to debug runtime registration issues
    
    Formatting changes are due to saving with `positron-python` workspace
    settings. That should have been done in the original commits.
    
    
    Authored-by: Wasim Lorgat <mwlorgat@gmail.com>
    Signed-off-by: Wasim Lorgat <mwlorgat@gmail.com>

--- extensions/positron-python/src/client/positron/extension.ts
+++ extensions/positron-python/src/client/positron/extension.ts
@@ -8,20 +8,18 @@ import { PythonExtension } from '../api/types';
 import { IDisposableRegistry } from '../common/types';
 import { IInterpreterService } from '../interpreter/contracts';
 import { IServiceContainer } from '../ioc/types';
-import { traceError } from '../logging';
+import { traceError, traceInfo } from '../logging';
 import { createPythonRuntime, pythonRuntimeProvider } from './provider';
 import { PythonStatementRangeProvider } from './statementRange';
-
 
 export async function activatePositron(
     activatedPromise: Promise<void>,
     pythonApi: PythonExtension,
-    serviceContainer: IServiceContainer
+    serviceContainer: IServiceContainer,
 ): Promise<void> {
-
     try {
-
         // Wait for all extension components to be activated before starting Positron activation.
+        traceInfo('activatePositron: awaiting extension activation');
         await activatedPromise;
 
         // Map of interpreter path to language runtime metadata, used to determine the runtimeId when
@@ -29,11 +27,12 @@ export async function activatePositron(
         const runtimes = new Map<string, positron.LanguageRuntimeMetadata>();
 
         // Register the Python language runtime provider with positron.
+        traceInfo('activatePositron: registering python runtime provider');
         positron.runtime.registerLanguageRuntimeProvider('python', pythonRuntimeProvider(serviceContainer, runtimes));
 
         // Register a statement range provider to detect Python statements
-        positron.languages.registerStatementRangeProvider('python',
-            new PythonStatementRangeProvider());
+        traceInfo('activatePositron: registering python statement range provider');
+        positron.languages.registerStatementRangeProvider('python', new PythonStatementRangeProvider());
 
         // If the interpreter is changed via the Python extension, select the corresponding
         // language runtime in Positron.
@@ -52,27 +51,27 @@ export async function activatePositron(
 
         // If a new interpreter is added via the Python extension, create and register a
         // corresponding language runtime.
-        disposables.push(pythonApi.environments.onDidChangeEnvironments(async (event) => {
-            if (event.type === 'add') {
-                const interpreterPath = event.env.path;
-                if (!runtimes.has(interpreterPath)) {
-                    // Get the interpreter corresponding to the new runtime.
-                    const interpreterService = serviceContainer.get<IInterpreterService>(IInterpreterService);
-                    const interpreter = await interpreterService.getInterpreterDetails(interpreterPath);
+        disposables.push(
+            pythonApi.environments.onDidChangeEnvironments(async (event) => {
+                if (event.type === 'add') {
+                    const interpreterPath = event.env.path;
+                    if (!runtimes.has(interpreterPath)) {
+                        // Get the interpreter corresponding to the new runtime.
+                        const interpreterService = serviceContainer.get<IInterpreterService>(IInterpreterService);
+                        const interpreter = await interpreterService.getInterpreterDetails(interpreterPath);
 
-                    // Create the runtime and register it with Positron.
-                    if (interpreter) {
-                        const runtime = await createPythonRuntime(interpreter, serviceContainer);
-                        const runtimeMetadata = runtime.metadata;
-                        disposables.push(positron.runtime.registerLanguageRuntime(runtime));
-                        runtimes.set(interpreterPath, runtimeMetadata);
+                        // Create the runtime and register it with Positron.
+                        if (interpreter) {
+                            const runtime = await createPythonRuntime(interpreter, serviceContainer);
+                            const runtimeMetadata = runtime.metadata;
+                            disposables.push(positron.runtime.registerLanguageRuntime(runtime));
+                            runtimes.set(interpreterPath, runtimeMetadata);
+                        }
                     }
                 }
-            }
-        }));
-
+            }),
+        );
     } catch (ex) {
         traceError('activatePositron() failed.', ex);
     }
-
 }
