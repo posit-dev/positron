0fa83aa00022dc233123478bab423ca8f99565d6
Lionel Henry <lionel.hry@gmail.com>
2023-09-06 16:07:16 +0200

    Use `clientHeight` instead of `offsetHeight`
    Addresses #1248

--- src/vs/workbench/contrib/positronConsole/browser/components/consoleInstance.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/components/consoleInstance.tsx
@@ -420,7 +420,7 @@ export const ConsoleInstance = (props: ConsoleInstanceProps) => {
 	const scrollHandler = (e: UIEvent<HTMLDivElement>) => {
 		// Determine whether the console instance is scroll locked.
 		if (Math.abs(consoleInstanceRef.current.scrollHeight -
-			consoleInstanceRef.current.offsetHeight -
+			consoleInstanceRef.current.clientHeight -
 			consoleInstanceRef.current.scrollTop) < 1) {
 			setScrollLocked(false);
 		} else {
