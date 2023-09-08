2a7a9c55c8f6346b56cb9d0e186fb0e3e0943367
Brian Lambert <brianlambert@gmail.com>
2023-09-08 18:41:50 -0500

    Updates to fix Help view display issues (#1287)
    * Updates to fix Help view display issues
    
    Authored-by: Brian Lambert <brianlambert@gmail.com>
    Signed-off-by: Brian Lambert <brianlambert@gmail.com>

--- src/vs/base/browser/positronReactRenderer.ts
+++ src/vs/base/browser/positronReactRenderer.ts
@@ -4,6 +4,7 @@
 
 import { Event } from 'vs/base/common/event';
 import { createRoot, Root } from 'react-dom/client';
+import { Disposable } from 'vs/base/common/lifecycle';
 
 /**
  * ISize interface.
@@ -77,37 +78,68 @@ export interface IReactComponentContainer {
  * PositronReactRenderer class.
  * Manages rendering a React component in the specified container HTMLElement.
  */
-export class PositronReactRenderer {
+export class PositronReactRenderer extends Disposable {
+	//#region Private Properties
+
 	/**
 	 * The root where the React element will be rendered.
 	 */
-	private _root?: Root;
+	private root?: Root;
+
+	//#endregion Private Properties
+
+	//#region Constructor & Dispose
 
 	/**
 	 * Initializes a new instance of the ReactRenderer class.
 	 * @param container The container HTMLElement where the React component will be rendered.
 	 */
 	constructor(container: HTMLElement) {
-		this._root = createRoot(container);
+		// Call the base class's constructor.
+		super();
+
+		// Create the root.
+		this.root = createRoot(container);
 	}
+
+	/**
+	 * dispose override method.
+	 */
+	public override dispose(): void {
+		// Unmount and dispose of the root.
+		if (this.root) {
+			this.root.unmount();
+			this.root = undefined;
+		}
+
+		// Call the base class's dispose method.
+		super.dispose();
+	}
+
+	//#endregion Constructor & Dispose
+
+	//#region Public Methods
 
 	/**
 	 * Renders the React component that was supplied.
 	 * @param reactElement The ReactElement to render.
 	 */
 	public render(reactElement: React.ReactElement) {
-		if (this._root) {
-			this._root.render(reactElement);
+		if (this.root) {
+			this.root.render(reactElement);
 		}
 	}
 
 	/**
 	 * Destroys the ReactRenderer.
+	 * @deprecated Use Disposable instead.
 	 */
 	public destroy() {
-		if (this._root) {
-			this._root.unmount();
-			this._root = undefined;
+		if (this.root) {
+			this.root.unmount();
+			this.root = undefined;
 		}
 	}
+
+	//#endregion Public Methods
 }
