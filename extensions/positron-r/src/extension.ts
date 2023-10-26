2ca62df4d1689356d32a760a82611787db08f2c9
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-10-26 16:36:12 -0500

    Introduce a (log) output channel for the R extension (#1677)
    * These were just helpful during development
    
    * Don't use `export default`
    
    * Remove traceOutputChannel
    
    This doesn't appear to be doing anything. I gather it is meant to send the LSP client-server tracing messages to a specific output channel.
    
    We seem to be getting fallback behaviour, which is to log such things to a channel named after the client, i.e. `Positron R Language Server (${this._version})`. Let's just go with that.
    
    BTW this has no effect on the level of tracing, e.g. `<server-id>.trace.server`. Our id is 'positron.r' and therefore the existing setup in package.json for 'positron.r.trace.server' is working as intended.
    
    * Make R installation object prettier
    
    * Wire up the log level
    
    * Strip as much away as I can
    
    * Introduce a (log) output channel for the R extension
    
    Authored-by: Jenny Bryan <jenny.f.bryan@gmail.com>
    Signed-off-by: Jenny Bryan <jenny.f.bryan@gmail.com>

--- extensions/positron-r/src/extension.ts
+++ extensions/positron-r/src/extension.ts
@@ -9,19 +9,21 @@ import { registerCommands } from './commands';
 import { providePackageTasks } from './tasks';
 import { setContexts } from './contexts';
 import { discoverTests } from './testing';
-import { initializeLogging } from './logging';
 import { rRuntimeProvider } from './provider';
 import { RRuntime } from './runtime';
 
+export const Logger = vscode.window.createOutputChannel('Positron R Extension', { log: true });
 
 export function activate(context: vscode.ExtensionContext) {
+	const onDidChangeLogLevel = (logLevel: vscode.LogLevel) => {
+		Logger.appendLine(vscode.l10n.t('Log level: {0}', vscode.LogLevel[logLevel]));
+	};
+	context.subscriptions.push(Logger.onDidChangeLogLevel(onDidChangeLogLevel));
+	onDidChangeLogLevel(Logger.logLevel);
 
 	const runtimes = new Map<string, RRuntime>();
 	positron.runtime.registerLanguageRuntimeProvider(
 		'r', rRuntimeProvider(context, runtimes));
-
-	// Initialize logging tools.
-	initializeLogging(context);
 
 	// Set contexts.
 	setContexts(context);
