8ad8d735fa808284113cddcc6d7364cdf3abf584
Jonathan McPherson <jonathan@rstudio.com>
2023-11-02 10:49:22 -0700

    eliminate usage of uuidv4 (relies on getRandomValues)

--- extensions/jupyter-adapter/src/utils.ts
+++ extensions/jupyter-adapter/src/utils.ts
@@ -29,3 +29,11 @@ export class PromiseHandles<T> {
 export function delay(ms: number) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function uuidv4() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		const r = Math.random() * 16 | 0;
+		const v = c === 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
