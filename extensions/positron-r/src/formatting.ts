005d7dc9549abb524a3d3af55680837762e642fd
Julia Silge <julia.silge@gmail.com>
2023-11-03 15:58:07 -0600

    Actually working formatter

--- extensions/positron-r/src/formatting.ts
+++ extensions/positron-r/src/formatting.ts
@@ -28,46 +28,64 @@ class FormatterProvider implements vscode.DocumentFormattingEditProvider {
 
 	public provideDocumentFormattingEdits(document: vscode.TextDocument):
 		vscode.ProviderResult<vscode.TextEdit[]> {
-		return formatDocument(document, this.runtimes);
+		return this.formatDocument(document, this.runtimes);
 	}
 
-}
-
-async function formatDocument(document: vscode.TextDocument, runtimes: Map<string, RRuntime>): Promise<vscode.TextEdit[]> {
-	if (!lastRuntimePath) {
-		throw new Error(`No running R runtime to provide R package tasks.`);
-	}
-
-	const runtime = await getRunningRRuntime(runtimes);
-
-	// We can only use styler on files right now, so write the document to a temp file
-	const source = document.getText();
-	let formattedSource = '';
-	const tempdir = os.tmpdir();
-	const fileToStyle = path.basename(document.fileName);
-	const stylerFile = path.join(tempdir, `styler-${fileToStyle}`);
-	fs.writeFileSync(stylerFile, JSON.stringify(source));
-
-
-	const id = randomUUID();
-	// Actual formatting is done by styler
-	runtime.execute(`styler::style_file('${stylerFile}')`,
-		id,
-		positron.RuntimeCodeExecutionMode.Silent,
-		positron.RuntimeErrorBehavior.Continue);
-	const disp1 = runtime.onDidReceiveRuntimeMessage(runtimeMessage => {
-		if (runtimeMessage.parent_id === id &&
-			runtimeMessage.type === positron.LanguageRuntimeMessageType.State) {
-			const runtimeMessageState = runtimeMessage as positron.LanguageRuntimeState;
-			if (runtimeMessageState.state === positron.RuntimeOnlineState.Idle) {
-				// Read the now formatted file and then delete it
-				formattedSource = fs.readFileSync(stylerFile).toString();
-				fs.promises.unlink(stylerFile);
-				disp1.dispose();
-			}
+	private async formatDocument(
+		document: vscode.TextDocument,
+		runtimes: Map<string, RRuntime>
+	): Promise<vscode.TextEdit[]> {
+		if (!lastRuntimePath) {
+			throw new Error(`No running R runtime to provide R package tasks.`);
 		}
-	});
 
-	return [vscode.TextEdit.insert(document.lineAt(0).range.start, formattedSource)];
+		const runtime = await getRunningRRuntime(runtimes);
+		const id = randomUUID();
 
+		// We can only use styler on files right now, so write the document to a temp file
+		const originalSource = document.getText();
+		const tempdir = os.tmpdir();
+		const fileToStyle = path.basename(document.fileName);
+		const stylerFile = path.join(tempdir, `styler-${fileToStyle}`);
+		fs.writeFileSync(stylerFile, originalSource);
+
+		// A promise that resolves when the runtime is idle:
+		const promise = new Promise<void>(resolve => {
+			const disp = runtime.onDidReceiveRuntimeMessage(runtimeMessage => {
+				if (runtimeMessage.parent_id === id &&
+					runtimeMessage.type === positron.LanguageRuntimeMessageType.State) {
+					const runtimeMessageState = runtimeMessage as positron.LanguageRuntimeState;
+					if (runtimeMessageState.state === positron.RuntimeOnlineState.Idle) {
+						resolve();
+						disp.dispose();
+					}
+				}
+			});
+		});
+
+		// A promise that rejects after a timeout;
+		const timeout = new Promise<void>((_, reject) => {
+			setTimeout(() => {
+				reject(new Error('Timed out after 20 seconds waiting for formatting to finish.'));
+			}, 2e4);
+		});
+
+		// Actual formatting is done by styler
+		runtime.execute(`styler::style_file('${stylerFile}')`,
+			id,
+			positron.RuntimeCodeExecutionMode.Silent,
+			positron.RuntimeErrorBehavior.Continue);
+
+		// Wait for the the runtime to be idle, or for the timeout:
+		await Promise.race([promise, timeout]);
+
+		// Read the now formatted file and then delete it
+		const formattedSource = fs.readFileSync(stylerFile).toString();
+		fs.promises.unlink(stylerFile);
+
+		// Return the formatted source
+		const fileStart = new vscode.Position(0, 0);
+		const fileEnd = document.lineAt(document.lineCount - 1).range.end;
+		return [new vscode.TextEdit(new vscode.Range(fileStart, fileEnd), formattedSource)];
+	}
 }
