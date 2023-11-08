5954cb89026ab58e93ccb952a9b39b5ee5ef8dd8
seem <mwlorgat@gmail.com>
2023-11-08 18:36:52 +0200

    run a cell via `positron.runtime.executeCode`

--- extensions/positron-editor-cells/src/codeLenseProvider.ts
+++ extensions/positron-editor-cells/src/codeLenseProvider.ts
@@ -5,47 +5,80 @@
 import * as vscode from 'vscode';
 
 
+export interface ICellRange {
+	range: vscode.Range;
+	cell_type: string;
+}
+
+
+class CellMatcher {
+	isCell(line: string): boolean {
+		return line.startsWith('# %%');
+	}
+
+	getCellType(_line: string): string {
+		return 'code';
+	}
+}
+
+
+export function generateCellRangesFromDocument(document: vscode.TextDocument): ICellRange[] {
+	// Implmentation of getCells here based on Don's Jupyter extension work
+	const matcher = new CellMatcher();
+	const cells: ICellRange[] = [];
+	for (let index = 0; index < document.lineCount; index += 1) {
+		const line = document.lineAt(index);
+		if (matcher.isCell(line.text)) {
+			if (cells.length > 0) {
+				const previousCell = cells[cells.length - 1];
+				previousCell.range = new vscode.Range(previousCell.range.start, document.lineAt(index - 1).range.end);
+			}
+
+			cells.push({
+				range: line.range,
+				cell_type: matcher.getCellType(line.text)
+			});
+		}
+	}
+
+	if (cells.length >= 1) {
+		const line = document.lineAt(document.lineCount - 1);
+		const previousCell = cells[cells.length - 1];
+		previousCell.range = new vscode.Range(previousCell.range.start, line.range.end);
+	}
+
+	return cells;
+}
+
+
 export class CodeLensProvider implements vscode.CodeLensProvider {
 
-	private codeLenses: vscode.CodeLens[] = [];
-	private regex: RegExp;
 	private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
 	public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
 
 	constructor() {
-		this.regex = /(.+)/g;
-
+		// TODO: Not sure when we're supposed to fire onDidChangeCodeLenses
 		vscode.workspace.onDidChangeConfiguration((_) => {
 			this._onDidChangeCodeLenses.fire();
 		});
 	}
 
 	public provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
-		this.codeLenses = [];
-		const regex = new RegExp(this.regex);
-		const text = document.getText();
-		let matches;
-		while ((matches = regex.exec(text)) !== null) {
-			const line = document.lineAt(document.positionAt(matches.index).line);
-			const indexOf = line.text.indexOf(matches[0]);
-			const position = new vscode.Position(line.lineNumber, indexOf);
-			const range = document.getWordRangeAtPosition(position, new RegExp(this.regex));
-			if (range) {
-				this.codeLenses.push(new vscode.CodeLens(range, { title: 'Test', command: 'foo' }));
-				// this.codeLenses.push(new vscode.CodeLens(range, { title: '$(run-above)', command: 'foo' }));
-				// this.codeLenses.push(new vscode.CodeLens(range, { title: '$(run-below)', command: 'foo' }));
-			}
+		const codeLenses: vscode.CodeLens[] = [];
+		const cellRanges = generateCellRangesFromDocument(document);
+		for (const cellRange of cellRanges) {
+			codeLenses.push(new vscode.CodeLens(cellRange.range, { title: '$(run) Run cell', command: 'positron-editor-cells.runCell', arguments: [cellRange.range] }));
 		}
-		return this.codeLenses;
+		return codeLenses;
 	}
 
 	public resolveCodeLens(codeLens: vscode.CodeLens, _token: vscode.CancellationToken): vscode.CodeLens {
-		codeLens.command = {
-			title: '$(run) Codelens provided by sample extension',
-			tooltip: 'Tooltip provided by sample extension',
-			command: 'codelens-sample.codelensAction',
-			arguments: ['Argument 1', false]
-		};
+		// codeLens.command = {
+		// 	title: '$(run) Codelens provided by sample extension',
+		// 	tooltip: 'Tooltip provided by sample extension',
+		// 	command: 'codelens-sample.codelensAction',
+		// 	arguments: ['Argument 1', false]
+		// };
 		return codeLens;
 	}
 }
--- extensions/positron-editor-cells/src/extension.ts
+++ extensions/positron-editor-cells/src/extension.ts
@@ -2,6 +2,7 @@
  *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
+import * as positron from 'positron';
 import * as vscode from 'vscode';
 import { initializeLogging } from './logging';
 import { CodeLensProvider } from './codeLenseProvider';
@@ -9,97 +10,105 @@ import { CodeLensProvider } from './codeLenseProvider';
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
 	initializeLogging();
 
+	// const document = vscode.window.activeTextEditor?.document!;
+	// const cellRanges = generateCellRangesFromDocument(document);
+
 	const codelensProvider = new CodeLensProvider();
 
 	vscode.languages.registerCodeLensProvider('*', codelensProvider);
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('positron-editor-cells.codelensAction', (args: any) => {
-			vscode.window.showInformationMessage(`CodeLens action clicked with args=${args}`);
+		vscode.commands.registerCommand('positron-editor-cells.runCell', (range: vscode.Range) => {
+			const document = vscode.window.activeTextEditor?.document!;
+
+			// Skip the cell marker
+			// TODO: Should we do this? Should this happen in the code lens provider?
+			const newRange = new vscode.Range(range.start.line + 1, 0, range.end.line, range.end.character);
+			// TODO: Should we trim?
+			const text = document.getText(newRange).trim();
+
+			positron.runtime.executeCode(document.languageId, text, true);
 		})
 	);
 
+	// let timeout: NodeJS.Timer | undefined = undefined;
 
-	console.log('decorator sample is activated');
+	// // create a decorator type that we use to decorate small numbers
+	// const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
+	// 	borderWidth: '1px',
+	// 	borderStyle: 'solid',
+	// 	overviewRulerColor: 'blue',
+	// 	overviewRulerLane: vscode.OverviewRulerLane.Right,
+	// 	light: {
+	// 		// this color will be used in light color themes
+	// 		borderColor: 'darkblue'
+	// 	},
+	// 	dark: {
+	// 		// this color will be used in dark color themes
+	// 		borderColor: 'lightblue'
+	// 	}
+	// });
 
-	let timeout: NodeJS.Timer | undefined = undefined;
+	// // create a decorator type that we use to decorate large numbers
+	// const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
+	// 	cursor: 'crosshair',
+	// 	// use a themable color. See package.json for the declaration and default values.
+	// 	backgroundColor: { id: 'myextension.largeNumberBackground' }
+	// });
 
-	// create a decorator type that we use to decorate small numbers
-	const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
-		borderWidth: '1px',
-		borderStyle: 'solid',
-		overviewRulerColor: 'blue',
-		overviewRulerLane: vscode.OverviewRulerLane.Right,
-		light: {
-			// this color will be used in light color themes
-			borderColor: 'darkblue'
-		},
-		dark: {
-			// this color will be used in dark color themes
-			borderColor: 'lightblue'
-		}
-	});
+	// let activeEditor = vscode.window.activeTextEditor;
 
-	// create a decorator type that we use to decorate large numbers
-	const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
-		cursor: 'crosshair',
-		// use a themable color. See package.json for the declaration and default values.
-		backgroundColor: { id: 'myextension.largeNumberBackground' }
-	});
+	// function updateDecorations() {
+	// 	if (!activeEditor) {
+	// 		return;
+	// 	}
+	// 	const regEx = /\d+/g;
+	// 	const text = activeEditor.document.getText();
+	// 	const smallNumbers: vscode.DecorationOptions[] = [];
+	// 	const largeNumbers: vscode.DecorationOptions[] = [];
+	// 	let match;
+	// 	while ((match = regEx.exec(text))) {
+	// 		const startPos = activeEditor.document.positionAt(match.index);
+	// 		const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+	// 		const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
+	// 		if (match[0].length < 3) {
+	// 			smallNumbers.push(decoration);
+	// 		} else {
+	// 			largeNumbers.push(decoration);
+	// 		}
+	// 	}
+	// 	activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
+	// 	activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
+	// }
 
-	let activeEditor = vscode.window.activeTextEditor;
+	// function triggerUpdateDecorations(throttle = false) {
+	// 	if (timeout) {
+	// 		clearTimeout(timeout);
+	// 		timeout = undefined;
+	// 	}
+	// 	if (throttle) {
+	// 		timeout = setTimeout(updateDecorations, 500);
+	// 	} else {
+	// 		updateDecorations();
+	// 	}
+	// }
 
-	function updateDecorations() {
-		if (!activeEditor) {
-			return;
-		}
-		const regEx = /\d+/g;
-		const text = activeEditor.document.getText();
-		const smallNumbers: vscode.DecorationOptions[] = [];
-		const largeNumbers: vscode.DecorationOptions[] = [];
-		let match;
-		while ((match = regEx.exec(text))) {
-			const startPos = activeEditor.document.positionAt(match.index);
-			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
-			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
-			if (match[0].length < 3) {
-				smallNumbers.push(decoration);
-			} else {
-				largeNumbers.push(decoration);
-			}
-		}
-		activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
-		activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
-	}
+	// if (activeEditor) {
+	// 	triggerUpdateDecorations();
+	// }
 
-	function triggerUpdateDecorations(throttle = false) {
-		if (timeout) {
-			clearTimeout(timeout);
-			timeout = undefined;
-		}
-		if (throttle) {
-			timeout = setTimeout(updateDecorations, 500);
-		} else {
-			updateDecorations();
-		}
-	}
+	// vscode.window.onDidChangeActiveTextEditor(editor => {
+	// 	activeEditor = editor;
+	// 	if (editor) {
+	// 		triggerUpdateDecorations();
+	// 	}
+	// }, null, context.subscriptions);
 
-	if (activeEditor) {
-		triggerUpdateDecorations();
-	}
-
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		activeEditor = editor;
-		if (editor) {
-			triggerUpdateDecorations();
-		}
-	}, null, context.subscriptions);
-
-	vscode.workspace.onDidChangeTextDocument(event => {
-		if (activeEditor && event.document === activeEditor.document) {
-			triggerUpdateDecorations(true);
-		}
-	}, null, context.subscriptions);
+	// vscode.workspace.onDidChangeTextDocument(event => {
+	// 	if (activeEditor && event.document === activeEditor.document) {
+	// 		triggerUpdateDecorations(true);
+	// 	}
+	// }, null, context.subscriptions);
 
 
 }
