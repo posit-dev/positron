d80c1926514830ca682a2f22d4dba54f21cd4f1c
seem <mwlorgat@gmail.com>
2023-11-09 13:07:05 +0200

    decorate cells with quarto background colors

--- extensions/positron-editor-cells/src/extension.ts
+++ extensions/positron-editor-cells/src/extension.ts
@@ -5,13 +5,10 @@
 import * as positron from 'positron';
 import * as vscode from 'vscode';
 import { initializeLogging } from './logging';
-import { CodeLensProvider } from './codeLenseProvider';
+import { CodeLensProvider, generateCellRangesFromDocument } from './codeLenseProvider';
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
 	initializeLogging();
-
-	// const document = vscode.window.activeTextEditor?.document!;
-	// const cellRanges = generateCellRangesFromDocument(document);
 
 	const codelensProvider = new CodeLensProvider();
 
@@ -31,84 +28,62 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 		})
 	);
 
-	// let timeout: NodeJS.Timer | undefined = undefined;
+	let timeout: NodeJS.Timer | undefined = undefined;
 
-	// // create a decorator type that we use to decorate small numbers
-	// const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
-	// 	borderWidth: '1px',
-	// 	borderStyle: 'solid',
-	// 	overviewRulerColor: 'blue',
-	// 	overviewRulerLane: vscode.OverviewRulerLane.Right,
-	// 	light: {
-	// 		// this color will be used in light color themes
-	// 		borderColor: 'darkblue'
-	// 	},
-	// 	dark: {
-	// 		// this color will be used in dark color themes
-	// 		borderColor: 'lightblue'
-	// 	}
-	// });
+	const cellDecorationType = vscode.window.createTextEditorDecorationType({
+		light: {
+			backgroundColor: '#E1E1E166'
+		},
+		dark: {
+			backgroundColor: '#40404066'
+		},
+		isWholeLine: true,
+	});
 
-	// // create a decorator type that we use to decorate large numbers
-	// const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
-	// 	cursor: 'crosshair',
-	// 	// use a themable color. See package.json for the declaration and default values.
-	// 	backgroundColor: { id: 'myextension.largeNumberBackground' }
-	// });
+	let activeEditor = vscode.window.activeTextEditor;
 
-	// let activeEditor = vscode.window.activeTextEditor;
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+		const cellRanges = generateCellRangesFromDocument(activeEditor.document);
+		const cells: vscode.DecorationOptions[] = [];
+		for (const cellRange of cellRanges) {
+			// const rangeTop = new vscode.Range(cellRange.range.start, cellRange.range.start);
+			const decoration = { range: cellRange.range };
+			cells.push(decoration);
+		}
+		activeEditor.setDecorations(cellDecorationType, cells);
+	}
 
-	// function updateDecorations() {
-	// 	if (!activeEditor) {
-	// 		return;
-	// 	}
-	// 	const regEx = /\d+/g;
-	// 	const text = activeEditor.document.getText();
-	// 	const smallNumbers: vscode.DecorationOptions[] = [];
-	// 	const largeNumbers: vscode.DecorationOptions[] = [];
-	// 	let match;
-	// 	while ((match = regEx.exec(text))) {
-	// 		const startPos = activeEditor.document.positionAt(match.index);
-	// 		const endPos = activeEditor.document.positionAt(match.index + match[0].length);
-	// 		const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
-	// 		if (match[0].length < 3) {
-	// 			smallNumbers.push(decoration);
-	// 		} else {
-	// 			largeNumbers.push(decoration);
-	// 		}
-	// 	}
-	// 	activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
-	// 	activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
-	// }
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
 
-	// function triggerUpdateDecorations(throttle = false) {
-	// 	if (timeout) {
-	// 		clearTimeout(timeout);
-	// 		timeout = undefined;
-	// 	}
-	// 	if (throttle) {
-	// 		timeout = setTimeout(updateDecorations, 500);
-	// 	} else {
-	// 		updateDecorations();
-	// 	}
-	// }
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
 
-	// if (activeEditor) {
-	// 	triggerUpdateDecorations();
-	// }
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
 
-	// vscode.window.onDidChangeActiveTextEditor(editor => {
-	// 	activeEditor = editor;
-	// 	if (editor) {
-	// 		triggerUpdateDecorations();
-	// 	}
-	// }, null, context.subscriptions);
-
-	// vscode.workspace.onDidChangeTextDocument(event => {
-	// 	if (activeEditor && event.document === activeEditor.document) {
-	// 		triggerUpdateDecorations(true);
-	// 	}
-	// }, null, context.subscriptions);
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations(true);
+		}
+	}, null, context.subscriptions);
 
 
 }
