2f5a64ba28f3bf89bafbb37ed7bed15a73986243
Jonathan McPherson <jonathan@rstudio.com>
2023-11-14 17:34:33 -0800

    register client handler into tree provider

--- extensions/positron-connections/src/extension.ts
+++ extensions/positron-connections/src/extension.ts
@@ -3,21 +3,32 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
+import * as positron from 'positron';
 
 /**
  * Stub implementation of a tree data provider.
  */
 class ConnectionItemsProvider implements vscode.TreeDataProvider<string> {
+	private _onDidChangeTreeData: vscode.EventEmitter<string | undefined> = new vscode.EventEmitter<string | undefined>();
+	private _connections: string[] = [];
+
+	constructor() {
+		this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+	}
+
+	onDidChangeTreeData: vscode.Event<string | undefined> | undefined;
+
 	getTreeItem(element: string): vscode.TreeItem {
 		return new vscode.TreeItem(element);
 	}
 
+	addConnection(name: string) {
+		this._connections.push(name);
+		this._onDidChangeTreeData.fire(undefined);
+	}
+
 	getChildren(element?: string): Thenable<string[]> {
-		if (element) {
-			return Promise.resolve([]);
-		} else {
-			return Promise.resolve(['stub']);
-		}
+		return Promise.resolve(this._connections);
 	}
 }
 
@@ -27,5 +38,19 @@ class ConnectionItemsProvider implements vscode.TreeDataProvider<string> {
  * @param context An ExtensionContext that contains the extention context.
  */
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.registerTreeDataProvider('connections', new ConnectionItemsProvider());
+	const connectionProvider = new ConnectionItemsProvider();
+
+	context.subscriptions.push(
+		vscode.window.registerTreeDataProvider('connections', connectionProvider));
+
+	context.subscriptions.push(
+		positron.runtime.registerClientHandler({
+			clientType: 'positron.connection',
+			callback: (client, params: object) => {
+				// Presume that the params are a connection name
+				const name = params as any as string;
+				connectionProvider.addConnection(name);
+				return true;
+			}
+		}));
 }
