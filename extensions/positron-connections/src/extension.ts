403d81f253fcd2349b139261147fca3fdf2d03fd
Jonathan McPherson <jonathan@rstudio.com>
2023-11-16 18:26:16 -0600

    Add stubs and example implementation for the Connections pane (#1840)
    * Merge remote-tracking branch 'origin/main' into feature/connections-pane-stubs
    
    * fix copy/paste error
    
    * Merge remote-tracking branch 'origin/main' into feature/connections-pane-stubs
    
    * add a connection close method to zed
    
    * track comm_close messages in ext host
    
    * remove connection when it's closed
    
    * clean up comments
    
    * impl click-to-preview in zed
    
    * add command for previewing table contents
    
    * add dev dependencies & icons for treeview
    
    * implement RPCs
    
    * super basic connection stub impl for zed
    
    * register client handler into tree provider
    
    * add connection client type
    
    * stubs for builtin 'positron-connections' extension
    
    Authored-by: Jonathan McPherson <jonathan@rstudio.com>
    Signed-off-by: Jonathan McPherson <jonathan@rstudio.com>

---
+++ extensions/positron-connections/src/extension.ts
@@ -0,0 +1,38 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import * as positron from 'positron';
+import { ConnectionItemTable, ConnectionItemsProvider } from './connection';
+
+/**
+ * Activates the extension.
+ *
+ * @param context An ExtensionContext that contains the extention context.
+ */
+export function activate(context: vscode.ExtensionContext) {
+	const connectionProvider = new ConnectionItemsProvider(context);
+
+	// Register the tree data provider that will provide the connections
+	context.subscriptions.push(
+		vscode.window.registerTreeDataProvider('connections', connectionProvider));
+
+	// Register a handler for the positron.connection client type. This client
+	// represents an active, queryable database connection.
+	context.subscriptions.push(
+		positron.runtime.registerClientHandler({
+			clientType: 'positron.connection',
+			callback: (client, params: any) => {
+				connectionProvider.addConnection(client, params.name);
+				return true;
+			}
+		}));
+
+	// Register a command to preview a table
+	context.subscriptions.push(
+		vscode.commands.registerCommand('positron.connections.previewTable',
+			(item: ConnectionItemTable) => {
+				item.preview();
+			}));
+}
---
+++ extensions/positron-zed/src/positronZedConnection.ts
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { randomUUID } from 'crypto';
+import { PositronZedLanguageRuntime } from './positronZedLanguageRuntime';
+
+/**
+ * A ZedConnection instance; simulates a real database connection.
+ */
+export class ZedConnection {
+	// The unique ID for this connection (randomly generated)
+	public readonly id;
+
+	/**
+	 * Emitter that handles outgoing messages to the front end
+	 */
+	private readonly _onDidEmitData = new vscode.EventEmitter<object>();
+	onDidEmitData: vscode.Event<object> = this._onDidEmitData.event;
+
+	constructor(readonly zed: PositronZedLanguageRuntime,
+		readonly name: string) {
+		this.id = randomUUID();
+	}
+
+	/**
+	 * Handles an incoming message from the Positron front end
+	 *
+	 * @param message The message to handle
+	 */
+	public handleMessage(message: any) {
+		switch (message.msg_type) {
+
+			// A request to list the tables
+			case 'tables_request':
+				// Emit the data to the front end
+				this._onDidEmitData.fire({
+					msg_type: 'tables_response',
+					tables: [
+						'table1',
+						'table2',
+						'table3'
+					]
+				});
+				break;
+
+			// A request to list the fields in a table
+			case 'fields_request':
+				// Emit the data to the front end
+				this._onDidEmitData.fire({
+					msg_type: 'fields_response',
+					fields: [
+						'field1',
+						'field2',
+						'field3'
+					]
+				});
+				break;
+
+			// A request to preview a table
+			case 'preview_table':
+				this.zed.createZedDataView(randomUUID(), message.table);
+				break;
+		}
+	}
+}
