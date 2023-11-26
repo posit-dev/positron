13cf1dbcecdebb8c7803b669acd5b9b6bcb224f3
Gary Ritchie <gary@rstudio.com>
2023-11-26 16:28:22 -0700

    set initial tabindex states

--- src/vs/platform/positronActionBar/browser/useRegisterWithActionBar.tsx
+++ src/vs/platform/positronActionBar/browser/useRegisterWithActionBar.tsx
@@ -5,11 +5,24 @@
 import { MutableRefObject, useEffect } from 'react';
 import { usePositronActionBarContext } from 'vs/platform/positronActionBar/browser/positronActionBarContext';
 
+/**
+ * Custom hook to register a component with the Positron Action Bar; this is to enable
+ * the roving tabindex pattern for keyboard navigation. Only one component at a time
+ * in the Action Bar is focusable (i.e. tabindex=0) and the rest have tabindex=-1.
+ * The arrow keys are used to move between the components in the Action Bar.
+ */
 export const useRegisterWithActionBar = (refs: MutableRefObject<HTMLElement>[]) => {
 	const positronActionBarContext = usePositronActionBarContext();
 
 	useEffect(() => {
-		refs.forEach(ref => positronActionBarContext.focusableComponents.add(ref.current));
+		refs.forEach(ref => {
+			if (positronActionBarContext.focusableComponents.size === 0) {
+				ref.current.tabIndex = 0; // initially the first component is focusable
+			} else {
+				ref.current.tabIndex = -1;
+			}
+			positronActionBarContext.focusableComponents.add(ref.current);
+		});
 		return () => {
 			refs.forEach(ref => positronActionBarContext.focusableComponents.delete(ref.current));
 		};
