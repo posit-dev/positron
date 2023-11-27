posit-dev/positron

commit d6e7d1ea95011ff2368e9017cb181aa02345237e

Discard test output that doesn't parse as JSON
Encountered when running gmailr tests, which first emit this before getting down to business:

ℹ Loading vscodereporter
Logged in as:
  * email: gargle-testuser@posit.co
  * num_messages: 183
  * num_threads: 183

We may want to send such output somewhere else in the future, but we have to do something with it in the meantime.

extensions/positron-r/src/testing/runner-testthat.ts
@@ -79,74 +79,80 @@ export async function runThatTest(
 		const childProcess = spawn(command, { cwd, shell: true });
 		let stdout = '';
 		const testStartDates = new WeakMap<vscode.TestItem, number>();
-		childProcess.stdout!.pipe(split2(JSON.parse)).on('data', (data: TestResult) => {
-			stdout += JSON.stringify(data);
-			Logger.info(`Received test data: ${JSON.stringify(data)}`);
-			switch (data.type) {
-				case 'start_test':
-					if (data.test !== undefined) {
-						const testItem = isSingleTest
-							? test
-							: findTestRecursively(encodeNodeId(test.uri!.fsPath, data.test), test);
-						if (testItem === undefined) {
-							reject(
-								`Test with id ${encodeNodeId(
-									test.uri!.fsPath,
-									data.test
-								)} could not be found. Please report this.`
-							);
-						}
-						testStartDates.set(testItem!, Date.now());
-						run.started(testItem!);
-					}
-					break;
-				case 'add_result':
-					if (data.result !== undefined && data.test !== undefined) {
-						const testItem = isSingleTest
-							? test
-							: findTestRecursively(encodeNodeId(test.uri!.fsPath, data.test), test);
-						if (testItem === undefined) {
-							reject(
-								`Test with id ${encodeNodeId(
-									test.uri!.fsPath,
-									data.test
-								)} could not be found. Please report this.`
-							);
-						}
-						const duration = Date.now() - testStartDates.get(testItem!)!;
-						switch (data.result) {
-							case 'success':
-							case 'warning':
-								run.passed(testItem!, duration);
-								if (data.message) {
-									run.appendOutput(data.message, undefined, testItem);
-								}
-								break;
-							case 'failure':
-								run.failed(
-									testItem!,
-									new vscode.TestMessage(data.message!),
-									duration
+		childProcess.stdout!
+			.pipe(split2((line: string) => {
+				try {
+					return JSON.parse(line);
+				} catch { }
+			}))
+			.on('data', (data: TestResult) => {
+				stdout += JSON.stringify(data);
+				Logger.info(`Received test data: ${JSON.stringify(data)}`);
+				switch (data.type) {
+					case 'start_test':
+						if (data.test !== undefined) {
+							const testItem = isSingleTest
+								? test
+								: findTestRecursively(encodeNodeId(test.uri!.fsPath, data.test), test);
+							if (testItem === undefined) {
+								reject(
+									`Test with id ${encodeNodeId(
+										test.uri!.fsPath,
+										data.test
+									)} could not be found. Please report this.`
 								);
-								break;
-							case 'skip':
-								run.skipped(testItem!);
-								if (data.message) {
-									run.appendOutput(data.message, undefined, testItem);
-								}
-								break;
-							case 'error':
-								run.errored(
-									testItem!,
-									new vscode.TestMessage(data.message!),
-									duration
-								);
-								break;
+							}
+							testStartDates.set(testItem!, Date.now());
+							run.started(testItem!);
 						}
-					}
-					break;
-			}
-		});
+						break;
+					case 'add_result':
+						if (data.result !== undefined && data.test !== undefined) {
+							const testItem = isSingleTest
+								? test
+								: findTestRecursively(encodeNodeId(test.uri!.fsPath, data.test), test);
+							if (testItem === undefined) {
+								reject(
+									`Test with id ${encodeNodeId(
+										test.uri!.fsPath,
+										data.test
+									)} could not be found. Please report this.`
+								);
+							}
+							const duration = Date.now() - testStartDates.get(testItem!)!;
+							switch (data.result) {
+								case 'success':
+								case 'warning':
+									run.passed(testItem!, duration);
+									if (data.message) {
+										run.appendOutput(data.message, undefined, testItem);
+									}
+									break;
+								case 'failure':
+									run.failed(
+										testItem!,
+										new vscode.TestMessage(data.message!),
+										duration
+									);
+									break;
+								case 'skip':
+									run.skipped(testItem!);
+									if (data.message) {
+										run.appendOutput(data.message, undefined, testItem);
+									}
+									break;
+								case 'error':
+									run.errored(
+										testItem!,
+										new vscode.TestMessage(data.message!),
+										duration
+									);
+									break;
+							}
+						}
+						break;
+				}
+			});
 		childProcess.once('exit', () => {
 			stdout += childProcess.stderr.read();
 			if (stdout.includes('Execution halted')) {

commit 4520814bade77e5fe9e8fc9ea08350b3b5b4263c

Restore a change that went missing by mistake
Originally introduced in d6e7d1ea95011ff2368e9017cb181aa02345237e, accidentally removed in f3f33da1d0488b917d0fdb904412de41314d8ab7

extensions/positron-r/src/testing/runner-testthat.ts
@@ -113,7 +113,12 @@ export async function runThatTest(
 		const childProcess = spawn(command, { cwd: wd, shell: true });
 		let stdout = '';
 		const testStartDates = new WeakMap<vscode.TestItem, number>();
-		childProcess.stdout!.pipe(split2(JSON.parse))
+		childProcess.stdout!
+			.pipe(split2((line: string) => {
+				try {
+					return JSON.parse(line);
+				} catch { }
+			}))
 			.on('data', (data: TestResult) => {
 				stdout += JSON.stringify(data);
 				Logger.info(`Received test data: ${JSON.stringify(data)}`);