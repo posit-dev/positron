30fc35aafa88c3923738c14657dcc67665e53d2f
Jackie Gutman <jacqueline.gutman@rstudio.com>
2023-11-28 13:25:45 -0600

    Improve performance, don't call table.getRowModel which is prohibitively expensive (#1877)
    * remove logging
    
    * Merge branch 'main' into debug_crashing_large_data
    
    * remove logging
    
    * turn off table debugging
    
    * Way faster performance. Calling table.getRowModel on every render for every row, visible or not, containing data or not is extremely expensive
    
    * add logging
    
    Lead-authored-by: Jackie Gutman <jacqueline.gutman@rstudio.com>
    Co-authored-by: Jacqueline Gutman <jacqueline.gutman@posit.co>
    Signed-off-by: Jackie Gutman <jacqueline.gutman@rstudio.com>

--- extensions/positron-data-viewer/ui/src/DataPanel.tsx
+++ extensions/positron-data-viewer/ui/src/DataPanel.tsx
@@ -93,79 +93,6 @@ export const DataPanel = (props: DataPanelProps) => {
 
 	const initialDataFragment: DataFragment = new DataFragment(initialData.columns, 0, Math.min(fetchSize, totalRows));
 
-	// Use a React Query infinite query to fetch data from the data model
-	const {data, fetchNextPage, fetchPreviousPage, hasNextPage, hasPreviousPage} = ReactQuery.useInfiniteQuery(
-	{
-		queryKey: ['table-data'],
-		queryFn: ({pageParam}) => fetcher.fetchNextDataFragment(pageParam, fetchSize),
-		initialPageParam: 0,
-		initialData: {
-			pages: [initialDataFragment],
-			pageParams: [0]
-		},
-		getPreviousPageParam: (_page, _pages, _firstPageParam, allPageParams) => {
-			return allPageParams.includes(scrollPages.current.top)
-				? undefined // don't refetch if we have already fetched data for this page
-				: scrollPages.current.top; // otherwise, use scroll position to determine previous page
-		},
-		getNextPageParam: (_page, _pages, _lastPageParam, allPageParams) => {
-			return allPageParams.includes(scrollPages.current.bottom)
-				? undefined // don't refetch if we have already fetched data for this page
-				: scrollPages.current.bottom; // otherwise, use scroll position to determine next page
-		},
-		// we don't need to check for active network connection before retrying a query
-		networkMode: 'always',
-		staleTime: Infinity,
-		refetchOnWindowFocus: false
-	});
-
-	const transposePage = React.useCallback((page: DataFragment) => {
-		const {rowStart, rowEnd} = page;
-		const pageSize = rowEnd - rowStart + 1;
-		return {rowStart, pageSize, data: page.transpose()};
-	}, []);
-
-	const createEmptyData = React.useCallback((highestFetchedRow: number) => {
-		const numColumns = initialData.columns.length;
-		const numRows = highestFetchedRow + 1;
-		return Array(numRows).fill(Array(numColumns).fill(undefined));
-	}, []);
-
-	// Transpose and flatten the data. The data model stores data in a column-major
-	// format, but React Table expects data in a row-major format, so we need to
-	// transpose the data. We also need to pad the array with placeholder rows
-	const flatData = React.useMemo(() => {
-		// We don't expect pages to be in order, but we do expect that there aren't duplicates
-		// That shouldn't be possible based on our implementation of getNext/PrevPageParams,
-		// but we check anyway to future-proof against changes to the query logic
-		const allPages = new Set(data.pageParams);
-		if (allPages.size !== data.pageParams.length) {
-			console.error('Duplicate pages fetched in the data');
-		}
-
-		const highestFetchedRow = Math.max(...data.pages.map(page => page.rowEnd));
-		const flatData = createEmptyData(highestFetchedRow);
-
-		data.pages.forEach(page => {
-			const {rowStart, pageSize, data} = transposePage(page);
-			flatData.splice(rowStart, pageSize, ...data);
-		});
-
-		return flatData;
-	}, [data]);
-
-	// Define the main ReactTable instance.
-	const table = ReactTable.useReactTable(
-	{
-		data: flatData,
-		columns,
-		getCoreRowModel: ReactTable.getCoreRowModel(),
-		debugTable: false,
-		enableSorting: false,
-	});
-
-	const {rows} = table.getRowModel();
-
 	// Use a virtualizer to render only the rows that are visible.
 	const rowVirtualizer = ReactVirtual.useVirtualizer(
 	{
@@ -200,8 +127,34 @@ export const DataPanel = (props: DataPanelProps) => {
 		return {firstVirtualRow, lastVirtualRow, paddingTop, paddingBottom};
 	}, [virtualRows]);
 
-	// Callback, invoked on scroll, that will fetch more data from the backend if we have reached
-	// the end of the virtualized rows by sending a new MessageRequest.
+	// Use a React Query infinite query to fetch data from the data model
+	const {data, fetchNextPage, fetchPreviousPage, hasNextPage, hasPreviousPage} = ReactQuery.useInfiniteQuery(
+	{
+		queryKey: ['table-data'],
+		queryFn: ({pageParam}) => fetcher.fetchNextDataFragment(pageParam, fetchSize),
+		initialPageParam: 0,
+		initialData: {
+			pages: [initialDataFragment],
+			pageParams: [0]
+		},
+		getPreviousPageParam: (_page, _pages, _firstPageParam, allPageParams) => {
+			return allPageParams.includes(scrollPages.current.top)
+				? undefined // don't refetch if we have already fetched data for this page
+				: scrollPages.current.top; // otherwise, use scroll position to determine previous page
+		},
+		getNextPageParam: (_page, _pages, _lastPageParam, allPageParams) => {
+			return allPageParams.includes(scrollPages.current.bottom)
+				? undefined // don't refetch if we have already fetched data for this page
+				: scrollPages.current.bottom; // otherwise, use scroll position to determine next page
+		},
+		// we don't need to check for active network connection before retrying a query
+		networkMode: 'always',
+		staleTime: Infinity,
+		refetchOnWindowFocus: false
+	});
+
+	// Callback that will fetch more data from the backend if we have scrolled outside the region of
+	// previously fetched data
 	const fetchMorePages = React.useCallback(() => {
 		if (hasNextPage) {
 			fetchNextPage({cancelRefetch: false});
@@ -228,6 +181,63 @@ export const DataPanel = (props: DataPanelProps) => {
 		fetchMorePages();
 	}, [firstVirtualRow, lastVirtualRow, fetchMorePages, rowVirtualizer.isScrolling]);
 
+	const transposePage = React.useCallback((page: DataFragment) => {
+		const {rowStart, rowEnd} = page;
+		const pageSize = rowEnd - rowStart + 1;
+		return {rowStart, pageSize, data: page.transpose()};
+	}, []);
+
+	const createEmptyData = React.useCallback((highestFetchedRow: number) => {
+		const numColumns = initialData.columns.length;
+		const numRows = highestFetchedRow + 1;
+		const emptyRow = Array(numColumns).fill(undefined);
+		return Array(numRows).fill(emptyRow) as any[][];
+	}, []);
+
+	// Transpose and flatten the data. The data model stores data in a column-major
+	// format, but React Table expects data in a row-major format, so we need to
+	// transpose the data. We also need to pad the array with placeholder rows
+	const flatData = React.useMemo(() => {
+		// We don't expect pages to be in order, but we do expect that there aren't duplicates
+		// That shouldn't be possible based on our implementation of getNext/PrevPageParams,
+		// but we check anyway to future-proof against changes to the query logic
+		const allPages = new Set(data.pageParams);
+		if (allPages.size !== data.pageParams.length) {
+			console.error('Duplicate pages fetched in the data');
+		}
+
+		const highestFetchedRow = Math.max(...data.pages.map(page => page.rowEnd));
+		const flatData = createEmptyData(highestFetchedRow);
+
+		data.pages.forEach(page => {
+			const {rowStart, pageSize, data} = transposePage(page);
+			flatData.splice(rowStart, pageSize, ...data);
+		});
+
+		return flatData;
+	}, [data.pageParams.length]);
+
+	// Define the main ReactTable instance.
+	const table = ReactTable.useReactTable(
+	{
+		data: flatData,
+		columns,
+		getCoreRowModel: ReactTable.getCoreRowModel(),
+		debugTable: false,
+		debugColumns: false,
+		debugHeaders: false,
+		enableSorting: false,
+	});
+
+	const createRow = React.useCallback((row: any[], index: number) => {
+		// table is technically a dependency here, but we use it only for its column defs
+		// so passing in columns rather than table as a dependency gives us better memoization
+		const rowId = '' + index;
+		// 0 is the depth, we don't support nested rows yet
+		// for nested rows, we'd need to update the subRows and/or parent row IDs
+		return ReactTable.createRow(table, rowId, row, index, 0);
+	}, [columns]);
+
 	const isLoading = React.useMemo(() => {
 		const columnId = columns[0].id;
 		if (!columnId) {
@@ -237,16 +247,16 @@ export const DataPanel = (props: DataPanelProps) => {
 		// Top of the screen is loading if the first virtual row is past the end of the data
 		// or in the sparse area of the data padded with undefined values
 		const topLoading = (
-			rows?.[firstVirtualRow] === undefined ||
-			rows[firstVirtualRow].getValue(columnId) === undefined
+			flatData?.[firstVirtualRow] === undefined ||
+			flatData[firstVirtualRow].every(value => value === undefined)
 		);
 		// Same for bottom of the screen, but with the last virtual row
 		const bottomLoading = (
-			rows?.[lastVirtualRow] === undefined ||
-			rows[lastVirtualRow].getValue(columnId) === undefined
+			flatData?.[lastVirtualRow] === undefined ||
+			flatData[lastVirtualRow].every(value => value === undefined)
 		);
 		return topLoading || bottomLoading;
-	}, [rows, firstVirtualRow, lastVirtualRow, columns]);
+	}, [flatData, firstVirtualRow, lastVirtualRow, columns]);
 
 	return (
 		<div
@@ -261,7 +271,9 @@ export const DataPanel = (props: DataPanelProps) => {
 						isLoading ?
 							null :
 							virtualRows.map(virtualRow => {
-								const row = rows[virtualRow.index] as ReactTable.Row<any>;
+								// We could get the rows from table.getRowModel().rows, but that would create all rows,
+								// not just the ones that we actually need to render, and is very expensive for large tables
+								const row = createRow(flatData[virtualRow.index], virtualRow.index);
 
 								return (
 								<DataRow
