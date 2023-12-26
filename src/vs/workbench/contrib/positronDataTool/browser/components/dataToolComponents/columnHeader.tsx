0a2a0ad0239ba5275d1cd1faf06ecf4d336fd5c4
Brian Lambert <brianlambert@gmail.com>
2023-12-26 16:59:25 -0500

    Some work on columns

--- src/vs/workbench/contrib/positronDataTool/browser/components/dataToolComponents/columnHeader.tsx
+++ src/vs/workbench/contrib/positronDataTool/browser/components/dataToolComponents/columnHeader.tsx
@@ -5,6 +5,7 @@
 import 'vs/css!./columnHeader';
 import * as React from 'react';
 import { usePositronDataToolContext } from 'vs/workbench/contrib/positronDataTool/browser/positronDataToolContext';
+import { PositronColumnSplitter, PositronColumnSplitterResizeResult } from 'vs/base/browser/ui/positronComponents/positronColumnSplitter';
 
 /**
  * ColumnHeaderProps interface.
@@ -22,10 +23,15 @@ export const ColumnHeader = (props: ColumnHeaderProps) => {
 	// Context hooks.
 	const context = usePositronDataToolContext();
 
+	const resizeHandler = (x: number, y: number): PositronColumnSplitterResizeResult => {
+		return PositronColumnSplitterResizeResult.Resizing;
+	};
+
 	// Render.
 	return (
-		<div className='column-header'>
-			<div className='title'>{context.instance.columns[props.index].columnSchema.name}</div>
+		<div className='column-header' style={{ width: 190 }}>
+			<div className='title'>{context.instance.columns[props.index].columnSchema.name} </div>
+			<PositronColumnSplitter width={5} showSizer={true} onResize={resizeHandler} />
 		</div>
 	);
 };
