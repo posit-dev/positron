b200d183adeae6b231b863aaac29008d85c16a54
Wes McKinney <wes@posit.co>
2024-01-03 08:41:58 -0600

    Add simple data generating mocks for data tool development, clean up comm contract (#1983)
    * first draft of simple mocks for data tool UI
    
    * Fix unit tests

---
+++ src/vs/workbench/services/positronDataTool/common/positronDataToolMocks.ts
@@ -0,0 +1,162 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import { generateUuid } from 'vs/base/common/uuid';
+import {
+	ColumnFilter,
+	ColumnFilterCompareOp,
+	ColumnFilterFilterType,
+	ColumnFilterSearchType,
+	ColumnSchema,
+	ProfileResult,
+	TableData,
+	TableSchema
+} from 'vs/workbench/services/languageRuntime/common/positronDataToolComm';
+
+const exampleTypes: [string, object][] = [
+	['int64', {}],
+	['string', {}],
+	['boolean', {}],
+	['double', {}],
+	['timestamp', { timezone: 'America/New_York' }]
+];
+
+export function getTableSchema(numRows: number = 100, numColumns: number = 10): TableSchema {
+	const columns = [];
+	for (let i = 0; i < numColumns; i++) {
+		const typeProto = exampleTypes[i % exampleTypes.length];
+		columns.push(getColumnSchema('column_' + i, typeProto[0], typeProto[1]));
+	}
+	return {
+		columns: columns,
+		num_rows: numRows
+	};
+}
+
+// If you want to add other examples, just append them to the arrays of strings.
+// They do not have to be all the same length.
+const exampleTypeData: Record<string, string[]> = {
+	'int64': ['-12345', '-1', '0', 'null', '1', '12345'],
+	'string': ['Greetings', 'Gr\u00FC\u00DFe', 'Saludos', 'null'],
+	'boolean': ['true', 'false', 'null'],
+	'double': ['1.2345', '-1.2e-17', '0.0', 'null'],
+	'timestamp': ['2021-12-20 12:34:45', '1970-01-01 00:00:00']
+};
+
+/**
+ * Generate some "random" data for the table based on a hand-written prototype, so the results
+ * are always deterministic given a particular schema's types.
+ * @param schema the schema of the table to generate data from
+ * @param rowStartIndex
+ * @param numRows
+ * @param columnIndices Columns to select by index. Can be sequetial, sparse, or random
+ */
+export function getExampleTableData(schema: TableSchema, rowStartIndex: number,
+	numRows: number, columnIndices: Array<number>): TableData {
+	const generatedColumns = [];
+
+	// Don't generate virtual data beyond the extends of the table, and if
+	// rowStartIndex is at or after end of the table, return nothing
+	numRows = Math.max(Math.min(numRows, schema.num_rows - rowStartIndex), 0);
+
+	for (const columnIndex of columnIndices) {
+		const exampleValues: string[] = exampleTypeData[schema.columns[columnIndex].type_name];
+
+		// Just repeat these values deterministically up until the "end" of the table
+		const generatedColumn = [];
+		if (numRows > 0) {
+			for (let i = rowStartIndex; i < rowStartIndex + numRows; i++) {
+				generatedColumn.push(exampleValues[i % exampleValues.length]);
+			}
+		}
+		generatedColumns.push(generatedColumn);
+	}
+	return {
+		columns: generatedColumns,
+		row_labels: []
+	};
+}
+
+export function getColumnSchema(name: string, typeName: string,
+	extraProps: object = {}): ColumnSchema {
+	return {
+		name: name,
+		type_name: typeName,
+		...extraProps
+	} as ColumnSchema;
+}
+
+export function getExampleHistogram(): ProfileResult {
+	// This example is basically made up.
+	return {
+		null_count: 10,
+		min_value: '0',
+		max_value: '100',
+		mean_value: '50',
+		histogram_bin_sizes: [4, 10, 15, 20, 7, 6, 0, 2, 50, 21],
+		histogram_bin_width: 10,
+		histogram_quantiles: [
+			{ q: 25, value: '25', exact: true },
+			{ q: 50, value: '70', exact: true },
+			{ q: 75, value: '82', exact: true }
+		],
+	} as ProfileResult;
+}
+
+export function getExampleFreqtable(): ProfileResult {
+	return {
+		null_count: 10,
+		freqtable_counts: [
+			{ value: 'foo1', count: 25 },
+			{ value: 'bar22', count: 10 },
+			{ value: 'baz3333', count: 7 },
+			{ value: 'qux444444', count: 2 }
+		],
+		freqtable_other_count: 12
+	} as ProfileResult;
+}
+
+// For filtering
+
+function _getFilterWithProps(columnName: string, filterType: ColumnFilterFilterType,
+	props: Partial<ColumnFilter> = {}): ColumnFilter {
+	return {
+		filter_id: generateUuid(),
+		filter_type: filterType,
+		column: columnName,
+		...props
+	} as ColumnFilter;
+}
+
+export function getCompareFilter(columnName: string, op: ColumnFilterCompareOp,
+	value: string) {
+	return _getFilterWithProps(columnName, ColumnFilterFilterType.Compare, {
+		compare_op: op,
+		compare_value: value
+	});
+}
+
+export function getIsNullFilter(columnName: string) {
+	return _getFilterWithProps(columnName, ColumnFilterFilterType.Isnull);
+}
+
+export function getNotNullFilter(columnName: string) {
+	return _getFilterWithProps(columnName, ColumnFilterFilterType.Notnull);
+}
+
+export function getSetMemberFilter(columnName: string, values: string[], inclusive: boolean) {
+	return _getFilterWithProps(columnName, ColumnFilterFilterType.SetMembership, {
+		set_member_values: values,
+		set_member_inclusive: inclusive
+	});
+}
+
+export function getTextSearchFilter(columnName: string, searchTerm: string,
+	searchType: ColumnFilterSearchType, caseSensitive: boolean) {
+	return _getFilterWithProps(columnName, ColumnFilterFilterType.Search, {
+		search_term: searchTerm,
+		search_type: searchType,
+		search_case_sensitive: caseSensitive
+	});
+}
