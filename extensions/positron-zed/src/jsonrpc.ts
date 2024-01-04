36a22b91090a8f9229e4f4808fa1ad3cce6f2097
Jonathan McPherson <jonathan@rstudio.com>
2024-01-03 17:06:25 -0800

    impl for zed, too

---
+++ extensions/positron-zed/src/jsonrpc.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+export enum JsonRpcErrorCode {
+	PARSE_ERROR = -32700,
+	INVALID_REQUEST = -32600,
+	METHOD_NOT_FOUND = -32601,
+	INVALID_PARAMS = -32602,
+	INTERNAL_ERROR = -32603
+}
