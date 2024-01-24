5071efe21c72cfa76af3da9f6cf89457b30de497
Jonathan McPherson <jonathan@rstudio.com>
2024-01-24 14:49:24 -0800

    allow scrolling, and always scroll to the bottom

--- src/vs/workbench/contrib/positronConsole/browser/components/historyBrowserPopup.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/components/historyBrowserPopup.tsx
@@ -6,6 +6,9 @@ import 'vs/css!./historyBrowserPopup';
 import * as React from 'react';
 import { HistoryCompletionItem } from 'vs/workbench/contrib/positronConsole/browser/components/historyCompletionItem';
 
+// eslint-disable-next-line no-duplicate-imports
+import { useEffect } from 'react';
+
 export interface HistoryBrowserPopupProps {
 	items: string[];
 	selectedIndex: number;
@@ -17,9 +20,19 @@ export interface HistoryBrowserPopupProps {
  * @returns The rendered component.
  */
 export const HistoryBrowserPopup = (props: HistoryBrowserPopupProps) => {
-	return <ul className='suggest-widget history-browser-popup'>
-		{props.items.map((item, index) => {
-			return <HistoryCompletionItem label={item} selected={props.selectedIndex === index} />;
-		})}
-	</ul>;
+	const popupRef = React.useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (popupRef.current && popupRef.current.scrollTop === 0) {
+			popupRef.current.scrollTop = popupRef.current.scrollHeight;
+		}
+	});
+
+	return <div className='suggest-widget history-browser-popup' ref={popupRef}>
+		<ul>
+			{props.items.map((item, index) => {
+				return <HistoryCompletionItem label={item} selected={props.selectedIndex === index} />;
+			})}
+		</ul>
+	</div>;
 };
