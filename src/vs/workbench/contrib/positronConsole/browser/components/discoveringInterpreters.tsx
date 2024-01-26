761f26ac201018bb1510e3705674ccb91a1700dd
Jonathan McPherson <jonathan@rstudio.com>
2024-01-26 12:40:05 -0800

    show count of discovered interpreters

--- src/vs/workbench/contrib/positronConsole/browser/components/discoveringInterpreters.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/components/discoveringInterpreters.tsx
@@ -5,6 +5,10 @@
 import 'vs/css!./discoveringInterpreters';
 import * as React from 'react';
 import { localize } from 'vs/nls';
+import { usePositronConsoleContext } from 'vs/workbench/contrib/positronConsole/browser/positronConsoleContext';
+
+// eslint-disable-next-line no-duplicate-imports
+import { useEffect, useState } from 'react';
 
 // Load localized copy for control.
 const discoveringIntrepreters = localize('positron.discoveringInterpreters', "Discovering interpreters...");
@@ -16,8 +20,23 @@ const discoveringIntrepreters = localize('positron.discoveringInterpreters', "Di
 export const DiscoveringInterpreters = () => {
 	// Context hooks.
 
+	// Hooks.
+	const positronConsoleContext = usePositronConsoleContext();
+
+	const [discovered, setDiscovered] =
+		useState(positronConsoleContext.languageRuntimeService.registeredRuntimes.length);
+
+	useEffect(() => {
+		const disposables = positronConsoleContext.languageRuntimeService.onDidRegisterRuntime(
+			_runtime => {
+				setDiscovered(
+					positronConsoleContext.languageRuntimeService.registeredRuntimes.length);
+			});
+		return () => disposables.dispose();
+	});
+
 	// Render.
 	return (
-		<h1>{discoveringIntrepreters}</h1>
+		<h1>{discoveringIntrepreters} ({discovered})</h1>
 	);
 };
