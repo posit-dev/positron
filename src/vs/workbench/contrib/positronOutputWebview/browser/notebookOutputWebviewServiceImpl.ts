0f5c48be8b32cd5654cc0a985363af822fd4c6b5
Jackie Gutman <jacqueline.gutman@rstudio.com>
2024-01-29 16:39:41 -0500

    jupyter-matplotlib dependency

--- src/vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebviewServiceImpl.ts
+++ src/vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebviewServiceImpl.ts
@@ -323,6 +323,9 @@ window.onload = function() {
 		const htmlManagerPath = asWebviewUri(
 			URI.joinPath(pythonExtension.extensionLocation, 'resources/js/@jupyter-widgets/html-manager/dist/embed-amd.js'));
 
+		const jupyterMatplotlibPath = asWebviewUri(
+			URI.joinPath(pythonExtension.extensionLocation, 'resources/js/jupyter-matplotlib/dist/index.js'));
+
 		const renderer = this._notebookService.getPreferredRenderer(MIME_TYPE_WIDGET_VIEW);
 		if (!renderer) {
 			return Promise.reject(`No renderer found for ${MIME_TYPE_WIDGET_VIEW}`);
@@ -470,6 +473,9 @@ window.onload = function() {
 
 <!-- Load the HTML manager, which is used to render the widgets -->
 <script src='${htmlManagerPath}'></script>
+
+<!-- Load jupyter-matplotlib, which is used to render matplotlib widgets -->
+<script src='${jupyterMatplotlibPath}'></script>
 
 <!-- The state of all the widget models on the page -->
 <script type="${MIME_TYPE_WIDGET_STATE}">
