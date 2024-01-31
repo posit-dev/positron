6a5fbf998d1cb3276ff3ef6ffe5934a6661d260b
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-01-31 16:14:18 -0600

    Provide richer information when backend asks about active editor (#2132)
    * Merge remote-tracking branch 'origin/main' into more-editor-context
    
    * Bump ark version
    
    * Bump positron-python
    
    * Drop '_result' from schema (and, therefore, 'Result' from types)
    
    * Add link to comment
    
    * Use snake_case in the schemas (and, therefore, in the types)
    
    * Drop the 'Ui' prefix on the schemas (and, therefore, the types)
    
    * More name tidying
    
    * Reuse interface names(s), since it seems to be technically possible
    
    * Expose document contents and line endings
    
    * Merge branch 'main' into more-editor-context
    
    * Report character position wrt to Unicode code points
    
    * Merge branch 'main' into more-editor-context
    
    * Provide richer information when backend asks about active editor
    
    Authored-by: Jenny Bryan <jenny.f.bryan@gmail.com>
    Signed-off-by: Jenny Bryan <jenny.f.bryan@gmail.com>

--- src/vs/workbench/api/common/positron/extHostMethods.ts
+++ src/vs/workbench/api/common/positron/extHostMethods.ts
@@ -4,9 +4,9 @@
 
 import * as extHostProtocol from './extHost.positron.protocol';
 import { ExtHostEditors } from '../extHostTextEditors';
-import { EditorContextResult, UiFrontendRequest } from 'vs/workbench/services/languageRuntime/common/positronUiComm';
+import { UiFrontendRequest, EditorContext } from 'vs/workbench/services/languageRuntime/common/positronUiComm';
 import { JsonRpcErrorCode } from 'vs/workbench/services/languageRuntime/common/positronBaseComm';
-
+import { EndOfLine } from '../extHostTypeConverters';
 
 type JsonRpcResponse = JsonRpcResult | JsonRpcError;
 
@@ -40,7 +40,7 @@ export class ExtHostMethods implements extHostProtocol.ExtHostMethodsShape {
 	async call(method: UiFrontendRequest, params: Record<string, any>): Promise<JsonRpcResponse> {
 		try {
 			if (!Object.values(UiFrontendRequest).includes(method)) {
-				return <JsonRpcError> {
+				return <JsonRpcError>{
 					error: {
 						code: JsonRpcErrorCode.MethodNotFound,
 						message: `Can't find method ${method}`,
@@ -71,7 +71,7 @@ export class ExtHostMethods implements extHostProtocol.ExtHostMethodsShape {
 
 			return <JsonRpcResult>({ result });
 		} catch (e) {
-			return <JsonRpcError> {
+			return <JsonRpcError>{
 				error: {
 					code: JsonRpcErrorCode.InternalError,
 					message: `Internal error: ${e}`,
@@ -80,13 +80,68 @@ export class ExtHostMethods implements extHostProtocol.ExtHostMethodsShape {
 		}
 	}
 
-	async lastActiveEditorContext(): Promise<EditorContextResult | null> {
+	async lastActiveEditorContext(): Promise<EditorContext | null> {
 		const editor = this.editors.getActiveTextEditor();
 		if (!editor) {
 			return null;
 		}
 
-		return { path: editor.document.fileName };
+		// The selections in this text editor. The primary selection is always at index 0.
+		//
+		// The gymnastics here are so that we return character positions with respect to
+		// Unicode code points. Otherwise, the native Position type provides offsets with respect to
+		// UTF-16 encoded text. That would be confusing for downstream consumers, who probably
+		// ultimately receive this text as UTF-8 and want to operate on this text in terms of
+		// as user-perceivable "characters". This only matters when the selection's neighborhood
+		// includes Unicode characters in the astral plane.
+		//
+		// Another resource that supports that what I'm doing here is desirable in Jupyter-land:
+		// https://jupyter-client.readthedocs.io/en/latest/messaging.html#notes
+		const selections = editor.selections.map(selection => {
+			const lineTextBeforeActive = editor.document
+				.lineAt(selection.active.line)
+				.text.substring(0, selection.active.character);
+			const unicodePointsBeforeActive = Array.from(lineTextBeforeActive).length;
+
+			const lineTextBeforeStart = editor.document
+				.lineAt(selection.start.line)
+				.text.substring(0, selection.start.character);
+			const unicodePointsBeforeStart = Array.from(lineTextBeforeStart).length;
+
+			const text = editor.document.getText(selection);
+			const unicodePointsInSelection = Array.from(text).length;
+
+			return {
+				active: { line: selection.active.line, character: unicodePointsBeforeActive },
+				start: { line: selection.start.line, character: unicodePointsBeforeStart },
+				end: { line: selection.end.line, character: unicodePointsBeforeStart + unicodePointsInSelection },
+				text: text
+			};
+		});
+
+		// it's surprisingly fiddly to finesse this vscode.EndOfLife enum, which we don't have
+		// direct access to here
+		const eolSequenceEnum = EndOfLine.from(editor.document.eol);
+		const eolSequence = eolSequenceEnum === 0 ? '\n' : '\r\n';
+		const documentText = editor.document.getText();
+		const lines = documentText.split(eolSequence);
+
+		return {
+			document: {
+				path: editor.document.fileName,
+				eol: eolSequence,
+				is_closed: editor.document.isClosed,
+				is_dirty: editor.document.isDirty,
+				is_untitled: editor.document.isUntitled,
+				language_id: editor.document.languageId,
+				line_count: editor.document.lineCount,
+				version: editor.document.version,
+			},
+			contents: lines,
+			// The primary selection in this text editor. Shorthand for `TextEditor.selections[0]`.
+			selection: selections[0],
+			selections: selections
+		};
 	}
 
 	async debugSleep(ms: number): Promise<null> {
@@ -99,7 +154,7 @@ export class ExtHostMethods implements extHostProtocol.ExtHostMethodsShape {
 /* Utils */
 
 function newInvalidParamsError(method: UiFrontendRequest) {
-	return <JsonRpcError> {
+	return <JsonRpcError>{
 		error: {
 			code: JsonRpcErrorCode.InvalidParams,
 			message: `Unexpected arguments for '${method}'`,
