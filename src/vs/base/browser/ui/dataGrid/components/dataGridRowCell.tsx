3d7ebe93bf8021a974d293ecaa9835f25c1557ef
Brian Lambert <brianlambert@gmail.com>
2024-02-03 09:37:59 -0600

    Data Tool range selection and automatic scrolling (#2204)
    * Fix cell selection bug
    
    * Merge branch 'main' into feature/data-tool-range-selection
    
    * Scrolling things into view.
    
    * Mouse select cell. Improved comments.
    
    * Add keyboard cell selection
    
    * Updates to selection management
    
    * Fix z-index of PositronColumnSplitetr and DataGridScrollbar to avoid Monaco Sash
    
    Lead-authored-by: Brian Lambert <brianlambert@gmail.com>
    Co-authored-by: Pete Farland <pete.farland@posit.co>
    Signed-off-by: Brian Lambert <brianlambert@gmail.com>

--- src/vs/base/browser/ui/dataGrid/components/dataGridRowCell.tsx
+++ src/vs/base/browser/ui/dataGrid/components/dataGridRowCell.tsx
@@ -14,8 +14,8 @@ import { isMacintosh } from 'vs/base/common/platform';
 import { positronClassNames } from 'vs/base/common/positronUtilities';
 import { IDataColumn } from 'vs/base/browser/ui/dataGrid/interfaces/dataColumn';
 import { useDataGridContext } from 'vs/base/browser/ui/dataGrid/dataGridContext';
-import { SelectionState } from 'vs/base/browser/ui/dataGrid/interfaces/dataGridInstance';
 import { PositronColumnSplitter } from 'vs/base/browser/ui/positronComponents/positronColumnSplitter';
+import { CellSelectionState } from 'vs/base/browser/ui/dataGrid/interfaces/dataGridInstance';
 
 /**
  * DataGridRowCellProps interface.
@@ -42,18 +42,20 @@ export const DataGridRowCell = (props: DataGridRowCellProps) => {
 	 */
 	const mouseDownHandler = (e: MouseEvent<HTMLElement>) => {
 		if (isMacintosh ? e.metaKey : e.ctrlKey) {
+			// Individual cell selection is not supported.
 		} else if (e.shiftKey) {
+			context.instance.mouseSelectCell(props.columnIndex, props.rowIndex);
 		} else {
 			context.instance.clearSelection();
+			context.instance.setCursorPosition(props.columnIndex, props.rowIndex);
 		}
-
-		// Set the cursor position.
-		context.instance.setCursorPosition(props.columnIndex, props.rowIndex);
 	};
 
 	// Get the selection states.
-	const columnSelectionState = context.instance.columnSelectionState(props.columnIndex);
-	const rowSelectionState = context.instance.rowSelectionState(props.rowIndex);
+	const cellSelectionState = context.instance.cellSelectionState(
+		props.columnIndex,
+		props.rowIndex
+	);
 
 	// Render.
 	return (
@@ -61,7 +63,7 @@ export const DataGridRowCell = (props: DataGridRowCellProps) => {
 			className={
 				positronClassNames(
 					'data-grid-row-cell',
-					{ 'selected': rowSelectionState & SelectionState.Selected || columnSelectionState & SelectionState.Selected },
+					{ 'selected': cellSelectionState & CellSelectionState.Selected },
 				)}
 			style={{
 				left: props.left,
@@ -74,11 +76,11 @@ export const DataGridRowCell = (props: DataGridRowCellProps) => {
 				className={
 					positronClassNames(
 						'data-grid-row-cell-border-overlay',
-						{ 'selected': rowSelectionState & SelectionState.Selected || columnSelectionState & SelectionState.Selected },
-						{ 'selected-top': rowSelectionState & SelectionState.FirstSelected },
-						{ 'selected-bottom': rowSelectionState & SelectionState.LastSelected },
-						{ 'selected-left': columnSelectionState & SelectionState.FirstSelected },
-						{ 'selected-right': columnSelectionState & SelectionState.LastSelected },
+						{ 'selected': cellSelectionState & CellSelectionState.Selected },
+						{ 'selected-top': cellSelectionState & CellSelectionState.SelectedTop },
+						{ 'selected-bottom': cellSelectionState & CellSelectionState.SelectedBottom },
+						{ 'selected-left': cellSelectionState & CellSelectionState.SelectedLeft },
+						{ 'selected-right': cellSelectionState & CellSelectionState.SelectedRight },
 					)}
 			>
 				{
