cef4ade8136518f8cb76cf73676a835db8e2a2b0
Wes McKinney <wes@posit.co>
2024-02-05 22:16:32 -0600

    Initial work on data caching, debouncing

--- src/vs/workbench/services/positronDataTool/browser/positronDataToolDataGridInstance.ts
+++ src/vs/workbench/services/positronDataTool/browser/positronDataToolDataGridInstance.ts
@@ -2,21 +2,152 @@
  *  Copyright (C) 2023-2024 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
-import { generateUuid } from 'vs/base/common/uuid';
 import { IColumnSortKey } from 'vs/base/browser/ui/dataGrid/interfaces/columnSortKey';
 import { DataGridInstance } from 'vs/base/browser/ui/dataGrid/classes/dataGridInstance';
 import { PositronDataToolColumn } from 'vs/workbench/services/positronDataTool/browser/positronDataToolColumn';
 import { DataToolClientInstance } from 'vs/workbench/services/languageRuntime/common/languageRuntimeDataToolClient';
 import { ColumnSortKey, TableData, TableSchema } from 'vs/workbench/services/languageRuntime/common/positronDataToolComm';
 
-interface CachedTableData {
-	firstColumnIndex: number;
-	columnIndices: number[];
+interface FetchRange {
+	/// Start index is inclusive
+	rowStartIndex: number;
 
-	firstRowIndex: number;
-	lastRowIndex: number;
+	/// End index is exclusive
+	rowEndIndex: number;
 
-	tableData: TableData;
+	/// Start index is inclusive
+	columnStartIndex: number;
+
+	/// End index is exclusive
+	columnEndIndex: number;
+}
+
+function rangeIncludes(range: FetchRange, inRange: FetchRange) {
+	return (range.rowStartIndex < inRange.rowStartIndex &&
+		range.rowEndIndex >= inRange.rowEndIndex &&
+		range.columnStartIndex < inRange.columnStartIndex &&
+		range.columnEndIndex >= inRange.columnEndIndex);
+}
+
+interface FetchResult extends FetchRange {
+	data: TableData;
+	lastUsedTime: number;
+}
+
+type FetchFunc = (req: FetchRange) => Promise<TableData>;
+
+export class PositronDataToolFetchManager {
+	private readonly ROW_CACHE_WINDOW = 100;
+	private readonly COLUMN_CACHE_WINDOW = 10;
+
+	private _cachedResults: Array<FetchResult> = [];
+
+	// Number of cells cached
+	private _cacheSize: number = 0;
+
+	// We cache up to this many cells. After that, we start dropping the
+	// least-recently used cached data
+	private readonly MAX_NUM_CACHED_CELLS = 100_000;
+
+	// The active fetch, so we can avoid spamming the backend with too many requests
+	private _pendingFetch?: { range: FetchRange; promise: Promise<FetchResult> };
+
+	// If we need to fetch more data and there is already a pending fetch,
+	// then we queue the next fetch with setTimeout, which will be immediately canceled
+	// and superseded by another fetch (e.g. if the user is moving around the waffle
+	// really fast).
+	private _debounceTimer?: any;
+
+	// We'll try a 100ms debounce timeout to start
+	private _debounceMillis = 100;
+
+	private readonly _fetcher: FetchFunc;
+
+	constructor(fetcher: FetchFunc) {
+		this._fetcher = fetcher;
+	}
+
+	/// Execute fetch request and handle throttling requests so that we do not spam the
+	/// backend with tons of requests if the user is moving around the waffle quickly.
+	fetch(range: FetchRange): Promise<FetchResult> {
+		if (this._pendingFetch) {
+			// If there is a pending fetch, clear any existing timer for a follow up request,
+			// and set a new timer
+			clearTimeout(this._debounceTimer);
+
+			let deferredResolve: (r: FetchResult) => void;
+			const deferredPromise: Promise<FetchResult> = new Promise(resolve => {
+				deferredResolve = resolve;
+			});
+			this._debounceTimer = setTimeout(async () => {
+				const promise = this.cacheRange(range);
+				this._pendingFetch = { range, promise };
+				const result = await promise;
+				deferredResolve(result);
+			}, this._debounceMillis);
+
+			return deferredPromise;
+		} else {
+			this._pendingFetch = { range, promise: this.cacheRange(range) };
+			return this._pendingFetch.promise;
+		}
+	}
+
+	private async cacheRange(range: FetchRange): Promise<FetchResult> {
+		// Determine if the range is contained in any cached result
+		for (const cachedResult of this._cachedResults) {
+			if (rangeIncludes(range, cachedResult)) {
+				return cachedResult;
+			}
+		}
+
+		// See if the range is contained in any of the cached data
+		const cacheRange = structuredClone(range);
+
+		cacheRange.rowEndIndex += this.ROW_CACHE_WINDOW;
+		cacheRange.columnEndIndex += this.COLUMN_CACHE_WINDOW;
+
+		const data = await this._fetcher(cacheRange);
+
+		// Set this based on actual number of columns returned
+		cacheRange.columnEndIndex = cacheRange.columnStartIndex + data.columns.length;
+
+		if (data.columns.length === 0) {
+			// No data was returned, so set numRows to 0
+			range.rowEndIndex = range.rowStartIndex;
+		} else {
+			range.rowEndIndex = range.rowStartIndex + data.columns[0].length;
+		}
+
+		const lastUsedTime = new Date().getTime();
+		const result: FetchResult = { data, lastUsedTime, ...cacheRange };
+
+		const getTotalCells = (range: FetchRange) => {
+			return ((range.columnEndIndex - range.columnStartIndex) *
+				(range.rowEndIndex - range.rowStartIndex));
+		};
+
+		const numCells = getTotalCells(cacheRange);
+
+		while (this._cacheSize + numCells > this.MAX_NUM_CACHED_CELLS) {
+			// Evict results from cache based on recency of use
+			let oldest = 0;
+			for (let i = 1; i < this._cachedResults.length; ++i) {
+				if (this._cachedResults[i].lastUsedTime < this._cachedResults[oldest].lastUsedTime) {
+					oldest = i;
+				}
+			}
+			this._cacheSize -= getTotalCells(this._cachedResults[oldest]);
+			this._cachedResults.splice(oldest, 1);
+		}
+		this._cachedResults.push(result);
+
+		return result;
+	}
+
+	clear() {
+		this._cachedResults = [];
+	}
 }
 
 /**
@@ -30,9 +161,9 @@ export class PositronDataToolDataGridInstance extends DataGridInstance {
 
 	private _tableSchema?: TableSchema;
 
-	private _lastFetchIdentifier = '';
+	private _fetchManager: PositronDataToolFetchManager;
 
-	private _cachedTableData?: CachedTableData;
+	private _lastFetchResult?: FetchResult;
 
 	constructor(dataToolClientInstance: DataToolClientInstance) {
 		// Call the base class's constructor.
@@ -45,6 +176,25 @@ export class PositronDataToolDataGridInstance extends DataGridInstance {
 
 		// Set the data tool client instance.
 		this._dataToolClientInstance = dataToolClientInstance;
+
+		this._fetchManager = new PositronDataToolFetchManager(async (req: FetchRange) => {
+			const start = new Date().getTime();
+
+			// Build the column indices to fetch.
+			const columnIndices: number[] = [];
+			for (let i = req.columnStartIndex; i < req.columnEndIndex; i++) {
+				columnIndices.push(i);
+			}
+
+			const data = await this._dataToolClientInstance.getDataValues(
+				req.rowStartIndex,
+				req.rowEndIndex - req.rowStartIndex,
+				columnIndices
+			);
+			const end = new Date().getTime();
+			console.log(`Fetching data took ${end - start}ms`);
+			return data;
+		});
 	}
 
 	/**
@@ -105,46 +255,27 @@ export class PositronDataToolDataGridInstance extends DataGridInstance {
 			return;
 		}
 
-		// Set the first column index and first row index.
-		const firstColumnIndex = this.firstColumnIndex;
-		const firstRowIndex = this.firstRowIndex;
+		const rangeToFetch: FetchRange = {
+			rowStartIndex: this.firstRowIndex,
+			rowEndIndex: this.firstRowIndex + this.visibleRows,
+			columnStartIndex: this.firstColumnIndex,
+			columnEndIndex: this.firstColumnIndex + this.visibleColumns + 1
+		};
 
-		// Build the column indices to fetch.
-		const columnIndices: number[] = [];
-		for (let i = this.firstColumnIndex; i < Math.min(this.firstColumnIndex + this.visibleColumns + 1, this.columns); i++) {
-			columnIndices.push(i);
+		if (this.needToFetch(rangeToFetch)) {
+			this._lastFetchResult = await this._fetchManager.fetch(rangeToFetch);
 		}
-
-		// Generate a fetch identifier.
-		const fetchIdentifier = this._lastFetchIdentifier = generateUuid();
-
-		// Fetch data.
-		const start = new Date().getTime();
-		const tableData = await this._dataToolClientInstance.getDataValues(
-			firstRowIndex,
-			this.visibleRows + 10,
-			columnIndices
-		);
-
-
-		if (fetchIdentifier !== this._lastFetchIdentifier) {
-			console.log('+++++++++++++++++++ DISCARDING FETCHED DATA');
-		}
-
-		const end = new Date().getTime();
-		console.log(`Fetching data took ${end - start}ms`);
-
-		// Set the cached data.
-		this._cachedTableData = {
-			firstColumnIndex,
-			columnIndices,
-			firstRowIndex,
-			lastRowIndex: firstRowIndex + this.visibleRows + 10,
-			tableData
-		} satisfies CachedTableData;
 
 		// Fire the onDidUpdate event.
 		this._onDidUpdateEmitter.fire();
+	}
+
+	private needToFetch(range: FetchRange) {
+		if (!this._lastFetchResult) {
+			return true;
+		} else {
+			return rangeIncludes(range, this._lastFetchResult);
+		}
 	}
 
 	fetchData() {
@@ -162,27 +293,27 @@ export class PositronDataToolDataGridInstance extends DataGridInstance {
 	 */
 	rowLabel(rowIndex: number) {
 		// If there isn't any cached data, return undefined.
-		if (!this._cachedTableData) {
+		if (!this._lastFetchResult) {
 			return undefined;
 		}
 
 		// If the row isn't cached, return undefined.
-		if (rowIndex < this._cachedTableData.firstRowIndex ||
-			rowIndex > this._cachedTableData.lastRowIndex
+		if (rowIndex < this._lastFetchResult.rowStartIndex ||
+			rowIndex > this._lastFetchResult.rowEndIndex
 		) {
 			return undefined;
 		}
 
 		// If there are no row labels, return the row index.
-		if (!this._cachedTableData.tableData.row_labels) {
+		if (!this._lastFetchResult.data.row_labels) {
 			return `${rowIndex + 1}`;
 		}
 
 		// Calculate the cached row index.
-		const cachedRowIndex = rowIndex - this._cachedTableData.firstRowIndex;
+		const cachedRowIndex = rowIndex - this._lastFetchResult.rowStartIndex;
 
 		// Return the cached row label.
-		return this._cachedTableData.tableData.row_labels[0][cachedRowIndex];
+		return this._lastFetchResult.data.row_labels[0][cachedRowIndex];
 	}
 
 	/**
@@ -193,27 +324,24 @@ export class PositronDataToolDataGridInstance extends DataGridInstance {
 	 */
 	cell(columnIndex: number, rowIndex: number): string | undefined {
 		// If there isn't any cached data, return undefined.
-		if (!this._cachedTableData) {
+		if (!this._lastFetchResult) {
 			return undefined;
 		}
 
-		// If the row isn't cached, return undefined.
-		if (rowIndex < this._cachedTableData.firstRowIndex ||
-			rowIndex > this._cachedTableData.lastRowIndex
+		// If the cell isn't cached, return undefined.
+		if (rowIndex < this._lastFetchResult.rowStartIndex ||
+			rowIndex >= this._lastFetchResult.rowEndIndex ||
+			columnIndex < this._lastFetchResult.columnStartIndex ||
+			columnIndex >= this._lastFetchResult.columnEndIndex
 		) {
 			return undefined;
 		}
 
-		// If the column isn't cached, return undefined.
-		const colIndex = this._cachedTableData.columnIndices.indexOf(columnIndex);
-		if (colIndex === -1) {
-			return undefined;
-		}
-
-		// Calculate the cached row index.
-		const cachedRowIndex = rowIndex - this._cachedTableData.firstRowIndex;
+		// Calculate the cache indices.
+		const cachedRowIndex = rowIndex - this._lastFetchResult.rowStartIndex;
+		const cachedColIndex = columnIndex - this._lastFetchResult.columnStartIndex;
 
 		// Return the cached value.
-		return this._cachedTableData.tableData.columns[colIndex][cachedRowIndex];
+		return this._lastFetchResult.data.columns[cachedColIndex][cachedRowIndex];
 	}
 }
