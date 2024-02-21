13ceb4a2fa0f8dd2a86b61efe71e934824631429
Jonathan McPherson <jonathan@rstudio.com>
2024-02-20 16:49:08 -0800

    add R runtime manager/session starter

---
+++ extensions/positron-r/src/runtime-manager.ts
@@ -0,0 +1,44 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as positron from 'positron';
+import * as vscode from 'vscode';
+import { rRuntimeDiscoverer } from './provider';
+import { RMetadataExtra } from './r-installation';
+import { RSession, createJupyterKernelExtra, createJupyterKernelSpec } from './session';
+
+export class RRuntimeManager implements positron.LanguageRuntimeManager {
+
+	constructor(private readonly _context: vscode.ExtensionContext) { }
+
+	discoverRuntimes(): AsyncGenerator<positron.LanguageRuntimeMetadata> {
+		return rRuntimeDiscoverer(this._context);
+	}
+
+	createSession(runtimeMetadata: positron.LanguageRuntimeMetadata,
+		sessionId: string): Thenable<positron.LanguageRuntimeSession> {
+
+		const metadataExtra = runtimeMetadata.extraData as RMetadataExtra;
+		const kernelExtra = createJupyterKernelExtra();
+
+		const kernelSpec = createJupyterKernelSpec(this._context,
+			metadataExtra.homepath,
+			runtimeMetadata.runtimeName);
+		const dynState: positron.LanguageRuntimeDynState = {
+			continuationPrompt: '+',
+			inputPrompt: '>',
+		};
+
+		const session = new RSession(sessionId,
+			'', // TODO!
+			positron.LanguageRuntimeSessionMode.Console, // TODO!
+			this._context,
+			kernelSpec,
+			runtimeMetadata,
+			dynState,
+			kernelExtra);
+
+		return Promise.resolve(session);
+	}
+}
