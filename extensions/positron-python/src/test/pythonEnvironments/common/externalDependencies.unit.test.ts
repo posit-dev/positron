11e778a69c0f5e03c9f797832f71db5bf361fcdb
Jacqueline Gutman <jacqueline.gutman@posit.co>
2024-02-22 17:43:30 -0600

    Separate handling for pyenv interpreters and pyenv local versioning file (posit-dev/positron-python#371)
    * Separate handling for pyenv interpreters and pyenv local versioning file
    
    * check for global pyenv version as well
    
    * fix comments
    
    * whitespace
    
    * add behavior for handling virtual env names and recursively search parent directories
    
    * clean up
    
    * fix read link path doesn't exist error
    
    * whitespace
    
    * fix symlink error issue
    
    * add unit tests
    
    * fix all unit tests
    
    * move new test to its own proper file
    
    * whitespace
    
    * fix windows unit test path separators
    
    * windows unit test path fixes

---
+++ extensions/positron-python/src/test/pythonEnvironments/common/externalDependencies.unit.test.ts
@@ -0,0 +1,52 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+import * as path from 'path';
+import * as sinon from 'sinon';
+import * as fsapi from 'fs-extra';
+import { checkParentDirs } from '../../../client/pythonEnvironments/common/externalDependencies';
+
+suite('checkParentDirs tests', () => {
+    let pathExistsSyncStub: sinon.SinonStub;
+
+    setup(() => {
+        pathExistsSyncStub = sinon.stub(fsapi, 'pathExistsSync');
+        pathExistsSyncStub
+            .withArgs('home')
+            .returns(true);
+        pathExistsSyncStub
+            .withArgs(path.join('home', 'project'))
+            .returns(true);
+        pathExistsSyncStub
+            .withArgs(path.join('home', 'project', 'file'))
+            .returns(false);
+        pathExistsSyncStub
+            .withArgs(path.join('home', 'file'))
+            .returns(true);
+        pathExistsSyncStub
+            .withArgs(path.join('home','nonexistent-file'))
+            .returns(false);
+    });
+
+    teardown(() => {
+        pathExistsSyncStub.restore();
+    });
+
+    test('checkParentDirs successfully finds the file', () => {
+        const root = path.join('home', 'project');
+        const filename = 'file';
+        const expected = path.join('home', 'file');
+        const actual = checkParentDirs(root, filename);
+        assert.strictEqual(actual, expected);
+    })
+
+    test('checkParentDirs does not find the file', () => {
+        const root = path.join('home');
+        const filename = 'nonexistent-file';
+        const expected = undefined;
+        const actual = checkParentDirs(root, filename);
+        assert.strictEqual(actual, expected);
+    })
+});
