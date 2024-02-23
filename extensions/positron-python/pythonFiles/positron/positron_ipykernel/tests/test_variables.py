b21e658a06341b13ce7a17abbd6c265c8f4f3832
Wes McKinney <wes@posit.co>
2024-02-23 10:47:19 -0600

    Add bounds for copying and deep comparisons when snapshotting/comparison user_ns before and after code execution, do not do change detection unless executing code (posit-dev/positron-python#382)
    * Add rough cost estimation for variable change detection
    
    Fixes to get change detection suppressed for large objects
    
    Revert changes to data_explorer.py
    
    Disable pytorch-related unit test for now
    
    use `pre/post_run_cell` events to avoid triggering callbacks during kernel executions
    
    Addresses https://github.com/posit-dev/positron/issues/2308.
    
    Add logging to show how much time is being spent doing namespace change detection
    
    Add logging to show how much time is being spent doing namespace change detection
    
    * format
    
    ---------
    
    Co-authored-by: Isabel Zimmerman <54685329+isabelizimm@users.noreply.github.com>

--- extensions/positron-python/pythonFiles/positron/positron_ipykernel/tests/test_variables.py
+++ extensions/positron-python/pythonFiles/positron/positron_ipykernel/tests/test_variables.py
@@ -28,6 +28,7 @@ from .utils import (
     json_rpc_response,
 )
 
+BIG_ARRAY_LENGTH = 10_000_001
 TARGET_NAME = "target_name"
 
 
@@ -125,34 +126,54 @@ def test_change_detection(
     """
     Test change detection when updating the value of the same name.
     """
+    _import_library(shell, import_code)
+    for value_code in value_codes:
+        _assert_assigned(shell, value_code, variables_comm)
+
+
+def _assert_assigned(shell: PositronShell, value_code: str, variables_comm: DummyComm):
+    # Assign the value to a variable.
+    shell.run_cell(f"x = {value_code}")
+
+    # Test that the expected `update` message was sent with the
+    # expected `assigned` value.
+    assert variables_comm.messages == [
+        json_rpc_notification(
+            "update",
+            {
+                "assigned": [not_none(_summarize_variable("x", shell.user_ns["x"])).dict()],
+                "removed": [],
+                "version": 0,
+            },
+        )
+    ]
+
+    # Clear messages for the next assignment.
+    variables_comm.messages.clear()
+
+
+def _import_library(shell: PositronShell, import_code: str):
     # Import the necessary library.
     if import_code:
         if import_code.endswith("torch"):  # temporary workaround for python 3.12
             pytest.skip()
         shell.run_cell(import_code)
 
-    for value_code in value_codes:
-        # Assign the value to a variable.
-        shell.run_cell(f"x = {value_code}")
 
-        # Test that the expected `update` message was sent with the expected `assigned` value.
-        assert variables_comm.messages == [
-            json_rpc_notification(
-                "update",
-                {
-                    "assigned": [not_none(_summarize_variable("x", shell.user_ns["x"])).dict()],
-                    "removed": [],
-                    "version": 0,
-                },
-            )
-        ]
+def test_change_detection_over_limit(shell: PositronShell, variables_comm: DummyComm):
+    _import_library(shell, "import numpy as np")
 
-        # Clear messages for the next assignment.
-        variables_comm.messages.clear()
+    big_array = f"x = np.arange({BIG_ARRAY_LENGTH})"
+    shell.run_cell(big_array)
+    variables_comm.messages.clear()
+
+    _assert_assigned(shell, big_array, variables_comm)
+    _assert_assigned(shell, big_array, variables_comm)
+    _assert_assigned(shell, big_array, variables_comm)
 
 
 def test_handle_refresh(shell: PositronShell, variables_comm: DummyComm) -> None:
-    shell.user_ns.update({"x": 3})
+    shell.user_ns["x"] = 3
 
     msg = json_rpc_request("list", comm_id="dummy_comm_id")
     variables_comm.handle_msg(msg)
@@ -214,17 +235,8 @@ def test_handle_delete(shell: PositronShell, variables_comm: DummyComm) -> None:
     assert "x" not in shell.user_ns
     assert "y" in shell.user_ns
 
-    # An update message (with the expected variable removed) is sent
     assert variables_comm.messages == [
         json_rpc_response([encode_access_key("x")]),
-        json_rpc_notification(
-            "update",
-            {
-                "assigned": [],
-                "removed": [encode_access_key("x")],
-                "version": 0,
-            },
-        ),
     ]
 
 
@@ -236,8 +248,54 @@ def test_handle_delete_error(variables_comm: DummyComm) -> None:
     assert variables_comm.messages == [json_rpc_response([])]
 
 
+def _assert_inspect(value: Any, path: List[Any], variables_comm: DummyComm) -> None:
+    encoded_path = [encode_access_key(key) for key in path]
+    msg = json_rpc_request(
+        "inspect",
+        # TODO(pyright): We shouldn't need to cast; may be a pyright bug
+        cast(JsonRecord, {"path": encoded_path}),
+        comm_id="dummy_comm_id",
+    )
+    variables_comm.handle_msg(msg)
+
+    assert len(variables_comm.messages) == 1
+
+    children = _summarize_children(value)
+    assert variables_comm.messages == [
+        json_rpc_response(
+            {
+                "children": [child.dict() for child in children],
+                "length": len(children),
+            }
+        )
+    ]
+
+    variables_comm.messages.clear()
+
+
 @pytest.mark.parametrize(
-    ("cls", "value"),
+    ("value_fn"),
+    [
+        lambda: {0: [0], "0": [1]},
+        lambda: pd.DataFrame({0: [0], "0": [1]}),
+        lambda: pl.DataFrame({"a": [1, 2], "b": ["3", "4"]}),
+        lambda: np.array([[0, 1], [2, 3]]),
+        # Inspecting large objects should not trigger update messages: https://github.com/posit-dev/positron/issues/2308.
+        lambda: np.arange(BIG_ARRAY_LENGTH),
+    ],
+)
+def test_handle_inspect(value_fn, shell: PositronShell, variables_comm: DummyComm) -> None:
+    """
+    Test that we can inspect root-level objects.
+    """
+    value = value_fn()
+    shell.user_ns["x"] = value
+
+    _assert_inspect(value, ["x"], variables_comm)
+
+
+@pytest.mark.parametrize(
+    ("cls", "data"),
     [
         # We should be able to inspect the children of a map/table with keys that have the same string representation.
         (dict, {0: [0], "0": [1]}),
@@ -250,41 +308,22 @@ def test_handle_delete_error(variables_comm: DummyComm) -> None:
     ],
 )
 def test_handle_inspect_2d(
-    cls: Type, value: Any, shell: PositronShell, variables_comm: DummyComm
+    cls: Type, data: Any, shell: PositronShell, variables_comm: DummyComm
 ) -> None:
     """
-    Test that we can inspect "two-dimensional" objects.
+    Test that we can inspect children of "two-dimensional" objects.
     """
-    x = cls(value)
-    shell.user_ns.update({"x": x})
+    value = cls(data)
+    shell.user_ns["x"] = value
 
-    keys = value.keys() if isinstance(value, dict) else range(len(value))
+    keys = data.keys() if isinstance(data, dict) else range(len(data))
     for key in keys:
-        path = [encode_access_key("x"), encode_access_key(key)]
-        # TODO: We shouldn't need to cast; may be a pyright bug
-        msg = json_rpc_request(
-            "inspect",
-            cast(JsonRecord, {"path": path}),
-            comm_id="dummy_comm_id",
-        )
-        variables_comm.handle_msg(msg)
-
-        children = _summarize_children(x[key])
-        assert variables_comm.messages == [
-            json_rpc_response(
-                {
-                    "children": [child.dict() for child in children],
-                    "length": len(children),
-                }
-            ),
-        ]
-
-        variables_comm.messages.clear()
+        _assert_inspect(value[key], ["x", key], variables_comm)
 
 
 def test_handle_inspect_error(variables_comm: DummyComm) -> None:
     path = [encode_access_key("x")]
-    # TODO: We shouldn't need to cast; may be a pyright bug
+    # TODO(pyright): We shouldn't need to cast; may be a pyright bug
     msg = json_rpc_request("inspect", cast(JsonRecord, {"path": path}), comm_id="dummy_comm_id")
     variables_comm.handle_msg(msg)
 
@@ -315,7 +354,7 @@ def test_handle_clipboard_format(shell: PositronShell, variables_comm: DummyComm
 
 def test_handle_clipboard_format_error(variables_comm: DummyComm) -> None:
     path = [encode_access_key("x")]
-    # TODO: We shouldn't need to cast; may be a pyright bug
+    # TODO(pyright): We shouldn't need to cast; may be a pyright bug
     msg = json_rpc_request(
         "clipboard_format",
         cast(JsonRecord, {"path": path, "format": "text/plain"}),
@@ -337,7 +376,7 @@ def test_handle_view(
     variables_comm: DummyComm,
     mock_dataexplorer_service: Mock,
 ) -> None:
-    shell.user_ns.update({"x": pd.DataFrame({"a": [0]})})
+    shell.user_ns["x"] = pd.DataFrame({"a": [0]})
 
     msg = json_rpc_request("view", {"path": [encode_access_key("x")]}, comm_id="dummy_comm_id")
     variables_comm.handle_msg(msg)
@@ -350,7 +389,7 @@ def test_handle_view(
 
 def test_handle_view_error(variables_comm: DummyComm) -> None:
     path = [encode_access_key("x")]
-    # TODO: We shouldn't need to cast; may be a pyright bug
+    # TODO(pyright): We shouldn't need to cast; may be a pyright bug
     msg = json_rpc_request("view", cast(JsonRecord, {"path": path}), comm_id="dummy_comm_id")
     variables_comm.handle_msg(msg)
 
