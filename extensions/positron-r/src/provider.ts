cd2e1d1c89eed1801ce78dc384db321dacc4cea7
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-03-07 01:16:08 -0600

    Fortify R installation discovery on Windows (#2031)
    * Consult PROGRAMFILES env var, then fall back
    
    * Honor "R that's on the PATH", on Windows
    
    * More robust path math on Windows
    
    On Windows, unlike *nix, path math really does seem to be the best way to get R home dir from the binary, short of actually executing `path/to/R/binary RHOME` in a child process.
    
    * Make getRHomePath() more the inverse of binFragment()
    
    * TODOs
    
    * Move discovery of binaries in OS-specific HQ into a function
    
    * Account for greater complexity on Windows
    
    "R Headquarters" is less well-defined on Windows and the directory layout bears more traces of the recent support for both 64 and 32 bit versions.
    
    * Move finding "current" R into a helper
    
    * Consult the registry for current R version on Windows
    
    * Remove a path, add a path
    
    * Account for r-devel
    
    * Manhandle rig's binpath
    
    * Make it clear this is relevant only on windows
    
    * More informative error
    
    * Update comment to match code
    
    * Improve logging of registry search

--- extensions/positron-r/src/provider.ts
+++ extensions/positron-r/src/provider.ts
@@ -10,10 +10,13 @@ import * as vscode from 'vscode';
 import * as which from 'which';
 import * as positron from 'positron';
 import * as crypto from 'crypto';
+import * as winreg from 'winreg';
 
 import { RInstallation, getRHomePath } from './r-installation';
 import { RRuntime, createJupyterKernelExtra, createJupyterKernelSpec } from './runtime';
 import { RRuntimeManager } from './runtime-manager';
+import { Logger } from './extension';
+import { readLines } from './util';
 
 const initialDynState = {
 	inputPrompt: '>',
@@ -73,20 +76,10 @@ export async function* rRuntimeDiscoverer(
 	let rInstallations: Array<RInstallation> = [];
 	const binaries = new Set<string>();
 
-	// look in the well-known place for R installations on this OS
-	const rHq = rHeadquarters();
-	if (fs.existsSync(rHq)) {
-		const versionBinaries = fs.readdirSync(rHq)
-			// 'Current', if it exists, is a symlink to an actual version. Skip it here to avoid
-			// redundant entries. This is a macOS phenomenon.
-			.filter(v => v !== 'Current')
-			.map(v => path.join(rHq, binFragment(v)))
-			// By default, macOS CRAN installer deletes previous R installations, but sometimes
-			// it doesn't do a thorough job of it and a nearly-empty version directory lingers on.
-			.filter(b => fs.existsSync(b));
-		for (const b of versionBinaries) {
-			binaries.add(b);
-		}
+	// look for R executables in the well-known place(s) for R installations on this OS
+	const hqBinaries = discoverHQBinaries();
+	for (const b of hqBinaries) {
+		binaries.add(b);
 	}
 
 	// other places we might find an R binary
@@ -103,21 +96,13 @@ export async function* rRuntimeDiscoverer(
 		binaries.add(b);
 	}
 
-	// TODO: Windows
-	// On Windows this finds `C:\Program Files\R\bin\R.BAT`, a batch file that starts
-	// the underlying version of R, i.e. try right clicking and editing the batch file to see the
-	// underlying path it ends up using. We will need some way to associate this with the `R.exe`
-	// file it ends up starting.
-	if (os.platform() !== 'win32') {
-		// make sure we include R executable found on the PATH
-		// we've probably already discovered it, but we still need to single it out, so that we mark
-		// that particular R installation as the current one
-		const whichR = await which('R', { nothrow: true }) as string;
-		if (whichR) {
-			const whichRCanonical = fs.realpathSync(whichR);
-			rInstallations.push(new RInstallation(whichRCanonical, true));
-			binaries.delete(whichRCanonical);
-		}
+	// make sure we include the "current" version of R, for some definition of "current"
+	// we've probably already discovered it, but we still want to single it out, so that we mark
+	// that particular R installation as the current one
+	const curBin = await findCurrentRBinary();
+	if (curBin) {
+		rInstallations.push(new RInstallation(curBin, true));
+		binaries.delete(curBin);
 	}
 
 	binaries.forEach((b: string) => {
@@ -125,7 +110,7 @@ export async function* rRuntimeDiscoverer(
 	});
 
 	// TODO: possible future intervention re: non-orthogonal R installations
-	// * Alert the user they have R more installations?
+	// * Alert the user they have more R installations?
 	// * Offer to make installations orthogonal?
 	// * Offer to switch the current version of R?
 	// for now, we drop non-orthogonal, not-current R installations
@@ -246,30 +231,182 @@ export async function* rRuntimeDiscoverer(
 	}
 }
 
-// directory where this OS is known to keep its R installations
-function rHeadquarters(): string {
+// directory(ies) where this OS is known to keep its R installations
+function rHeadquarters(): string[] {
 	switch (process.platform) {
 		case 'darwin':
-			return path.join('/Library', 'Frameworks', 'R.framework', 'Versions');
+			return [path.join('/Library', 'Frameworks', 'R.framework', 'Versions')];
 		case 'linux':
-			return path.join('/opt', 'R');
-		case 'win32':
-			return path.join('C:\\', 'Program Files', 'R');
+			return [path.join('/opt', 'R')];
+		case 'win32': {
+			const paths = [
+				// @kevinushey likes to install R here because it does not require administrator
+				// privileges to access
+				'C:\\R',
+				path.join(process.env['ProgramW6432'] || 'C:\\Program Files', 'R')
+			];
+			if (process.env['LOCALAPPDATA']) {
+				paths.push(path.join(process.env['LOCALAPPDATA'], 'Programs', 'R'));
+			}
+			return [...new Set(paths)];
+		}
 		default:
 			throw new Error('Unsupported platform');
 	}
 }
 
-function binFragment(version: string): string {
+function firstExisting(base: string, fragments: string[]): string {
+	const potentialPaths = fragments.map(f => path.join(base, f));
+	const existingPath = potentialPaths.find(p => fs.existsSync(p));
+	return existingPath || '';
+}
+
+function discoverHQBinaries(): string[] {
+	const hqDirs = rHeadquarters()
+		.filter(dir => fs.existsSync(dir));
+	if (hqDirs.length === 0) {
+		return [];
+	}
+
+	const versionDirs = hqDirs
+		.map(hqDir => fs.readdirSync(hqDir).map(file => path.join(hqDir, file)))
+		// Windows: rig creates 'bin/', which is a directory of .bat files (at least, for now)
+		// https://github.com/r-lib/rig/issues/189
+		.map(listing => listing.filter(path => !path.endsWith('bin')))
+		// macOS: 'Current', if it exists, is a symlink to an actual version
+		.map(listing => listing.filter(path => !path.endsWith('Current')));
+
+	// On Windows:
+	// In the case that both (1) and (2) exist we prefer (1).
+	// (1) C:\Program Files\R\R-4.3.2\bin\x64\R.exe
+	// (2) C:\Program Files\R\R-4.3.2\bin\R.exe
+	// Assuming we support R >= 4.2, we don't need to consider bin\i386\R.exe.
+	const binaries = versionDirs
+		.map(vd => vd.map(x => firstExisting(x, binFragments())))
+		.flat()
+		// macOS: By default, the CRAN installer deletes previous R installations, but sometimes
+		// it doesn't do a thorough job of it and a nearly-empty version directory lingers on.
+		.filter(b => fs.existsSync(b));
+	return binaries;
+}
+
+function binFragments(): string[] {
 	switch (process.platform) {
 		case 'darwin':
-			return path.join(version, 'Resources', 'bin', 'R');
+			return [path.join('Resources', 'bin', 'R')];
 		case 'linux':
-			return path.join(version, 'bin', 'R');
+			return [path.join('bin', 'R')];
 		case 'win32':
-			return path.join(version, 'bin', 'R.exe');
+			return [
+				path.join('bin', 'x64', 'R.exe'),
+				path.join('bin', 'R.exe')
+			];
 		default:
 			throw new Error('Unsupported platform');
+	}
+}
+
+async function findCurrentRBinary(): Promise<string | undefined> {
+	if (os.platform() === 'win32') {
+		const registryBinary = findCurrentRBinaryFromRegistry();
+		if (registryBinary) {
+			return registryBinary;
+		}
+	}
+
+	const whichR = await which('R', { nothrow: true }) as string;
+	if (whichR) {
+		if (os.platform() === 'win32') {
+			// rig puts {R Headquarters}/bin on the PATH, so that is probably how we got here.
+			// (The CRAN installer does NOT put R on the PATH.)
+			// In the rig scenario, `whichR` is anticipated to be a batch file that launches the
+			// current version of R ('default' in rig-speak):
+			// Example filepath: C:\Program Files\R\bin\R.bat
+			// Typical contents of this file:
+			// ::4.3.2
+			// @"C:\Program Files\R\R-4.3.2\bin\R" %*
+			// How it looks when r-devel is current:
+			// ::devel
+			// @"C:\Program Files\R\R-devel\bin\R" %*
+			// Note that this is not our preferred x64 binary, so we try to convert it.
+			if (path.extname(whichR).toLowerCase() === '.bat') {
+				const batLines = readLines(whichR);
+				const re = new RegExp(`^@"(.+R-(devel|[0-9]+[.][0-9]+[.][0-9]+).+)" %[*]$`);
+				const match = batLines.find((x: string) => re.test(x))?.match(re);
+				if (match) {
+					const whichRMatched = match[1];
+					const whichRHome = getRHomePath(whichRMatched);
+					if (!whichRHome) {
+						Logger.info(`Failed to get R home path from ${whichRMatched}`);
+						return undefined;
+					}
+					// we prefer the x64 binary
+					const whichRResolved = firstExisting(whichRHome, binFragments());
+					if (whichRResolved) {
+						Logger.info(`Resolved R binary at ${whichRResolved}`);
+						return whichRResolved;
+					} else {
+						Logger.info(`Can\'t find R binary within ${whichRHome}`);
+						return undefined;
+					}
+				}
+			}
+			// TODO: handle the case where whichR isn't picking up the rig case; do people do this,
+			// meaning put R on the PATH themselves, on Windows?
+		} else {
+			const whichRCanonical = fs.realpathSync(whichR);
+			Logger.info(`Resolved R binary at ${whichRCanonical}`);
+			return whichRCanonical;
+		}
+	}
+}
+
+async function findCurrentRBinaryFromRegistry(): Promise<string | undefined> {
+	const userPath = await getRegistryInstallPath(winreg.HKCU);
+	const machinePath = await getRegistryInstallPath(winreg.HKLM);
+	if (!userPath && !machinePath) {
+		return undefined;
+	}
+	const installPath = userPath || machinePath || '';
+
+	const binPath = firstExisting(installPath, binFragments());
+	if (!binPath) {
+		return undefined;
+	}
+	Logger.info(`Identified the current version of R from the registry: ${binPath}`);
+
+	return binPath;
+}
+
+async function getRegistryInstallPath(hive: string): Promise<string | undefined> {
+	try {
+		const key = new winreg({
+			hive: hive as keyof typeof winreg,
+			// 'R64' here is another place where we explicitly ignore 32-bit R
+			key: '\\Software\\R-Core\\R64',
+		});
+
+		Logger.info(`Checking for 'InstallPath' in registry key ${key.key} for hive ${key.hive}`);
+
+		const result = await new Promise<{ value: string }>((resolve, reject) => {
+			key.get('InstallPath', (error, result) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(result);
+				}
+			});
+		});
+
+		if (!result || typeof result.value !== 'string') {
+			Logger.info(`Invalid value of 'InstallPath'`);
+			return undefined;
+		}
+
+		return result.value;
+	} catch (error: any) {
+		Logger.info(`Unable to get value of 'InstallPath': ${error.message}`);
+		return undefined;
 	}
 }
 
