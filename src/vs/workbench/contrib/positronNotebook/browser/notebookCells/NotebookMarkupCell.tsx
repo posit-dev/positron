304a73d051a472c85a289badbdfdea70a9896d03
Nick Strayer <nick.strayer@rstudio.com>
2024-03-21 16:45:54 -0400

    Add render support for markdown.

--- src/vs/workbench/contrib/positronNotebook/browser/notebookCells/NotebookMarkupCell.tsx
+++ src/vs/workbench/contrib/positronNotebook/browser/notebookCells/NotebookMarkupCell.tsx
@@ -1,17 +1,27 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
+import 'vs/css!./NotebookMarkupCell';
 
 import * as React from 'react';
 import { IPositronNotebookMarkupCell } from 'vs/workbench/contrib/positronNotebook/browser/PositronNotebookCell';
 
 import { marked } from 'marked';
+import { renderHtml } from 'vs/base/browser/renderHtml';
+import { CellEditorMonacoWidget } from 'vs/workbench/contrib/positronNotebook/browser/notebookCells/CellEditorMonacoWidget';
+
 
 
 export function NotebookMarkupCell({ cell }: { cell: IPositronNotebookMarkupCell }) {
-	const htmlOfContent = marked(cell.getContent()) as string;
-
 	return <div>
-		{htmlOfContent}
+		<CellEditorMonacoWidget cell={cell} />
+		<div className='positron-notebook-markup-rendered'>
+			<RenderMarkdown content={cell.getContent()} />
+		</div>
 	</div>;
+}
+
+function RenderMarkdown({ content }: { content: string }) {
+	const htmlOfContent = marked(content) as string;
+	return <div>{renderHtml(htmlOfContent)}</div>;
 }
