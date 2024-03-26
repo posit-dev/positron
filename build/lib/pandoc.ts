888c115dfa8b492592b6f82608e174beb1870495
Jonathan McPherson <jonathan@rstudio.com>
2024-03-26 14:50:11 -0700

    clean up comments; make standalone download work

--- build/lib/pandoc.ts
+++ build/lib/pandoc.ts
@@ -7,11 +7,25 @@ import { fetchUrls } from './fetch';
 import * as es from 'event-stream';
 import { Stream } from 'stream';
 import filter = require('gulp-filter');
+import gulp = require('gulp');
+import util = require('./util');
 
+/**
+ * Get the base URL for the pandoc download
+ *
+ * @param version The version of pandoc to download
+ * @returns The base URL for the pandoc download
+ */
 function getBaseUrl(version: string): string {
 	return `https://github.com/jgm/pandoc/releases/download/${version}/`;
 }
 
+/**
+ * Gets a stream that downloads and unpacks the pandoc executable for Windows
+ *
+ * @param version The version of pandoc to download
+ * @returns A stream
+ */
 function getPandocWindows(version: string): Stream {
 	const unzip = require('gulp-unzip');
 	const basename = `pandoc-${version}-windows-x86_64`;
@@ -21,15 +35,25 @@ function getPandocWindows(version: string): Stream {
 		verbose: true,
 	})
 		.pipe(unzip({
+			// Extract only the pandoc executable
 			filter: (entry: any) => entry.path.endsWith('.exe')
 		}))
 		.pipe(rename(`pandoc.exe`));
 }
 
+/**
+ * Gets a stream that downloads and unpacks the pandoc executable for macOS
+ *
+ * @param version The version of pandoc to download
+ * @returns A stream
+ */
 function getPandocMacOS(version: string): Stream {
 	const unzip = require('gulp-unzip');
 	const rename = require('gulp-rename');
 
+	// Get the pandoc architecture for the current platform we're building. Note
+	// that this may differ from the machine's architecture since we may be
+	// cross-compiling Positron for a different platform.
 	const basename = process.env['npm_config_arch'] === 'x64' ?
 		`pandoc-${version}-x86_64-macOS` :
 		`pandoc-${version}-arm64-macOS`;
@@ -39,12 +63,20 @@ function getPandocMacOS(version: string): Stream {
 		verbose: true,
 	})
 		.pipe(unzip({
+			// Extract only the pandoc executable
 			filter: (entry: any) => entry.path.endsWith('pandoc')
 		}))
 		.pipe(rename(`pandoc`));
 }
 
+/**
+ * Gets a stream that downloads and unpacks the pandoc executable for Linux
+ *
+ * @param version The version of pandoc to download
+ * @returns A stream
+ */
 function getPandocLinux(version: string): Stream {
+	// Linux requires gunzip and untar
 	const gunzip = require('gulp-gunzip');
 	const untar = require('gulp-untar');
 	const flatmap = require('gulp-flatmap');
@@ -58,14 +90,26 @@ function getPandocLinux(version: string): Stream {
 		base: getBaseUrl(version),
 		verbose: true,
 	})
+		// Unzip, then untar
 		.pipe(flatmap((stream: Stream) => stream.pipe(gunzip()).pipe(untar())))
+
+		// Extract only the pandoc executable
 		.pipe(filter('**/pandoc'))
 		.pipe(rename(`pandoc`));
 }
 
+/**
+ * Gets a stream that downloads and unpacks the pandoc executable. Reads
+ * `npm_config_arch` to determine the architecture.
+ *
+ * @returns A stream that downloads and unpacks the pandoc executable
+ */
 export function getPandocStream(): Stream {
+	// Pandoc version
 	const version = '3.1.12.3';
+
 	fancyLog(`Synchronizing Pandoc ${version}...`);
+
 	// Get the download/unpack stream for the current platform
 	return process.platform === 'win32' ?
 		getPandocWindows(version) :
@@ -74,8 +118,16 @@ export function getPandocStream(): Stream {
 			getPandocLinux(version);
 }
 
+/**
+ * Standalone helper for downloading and unpacking pandoc; downloads Pandoc to
+ * thie `.build` folder for testing.
+ *
+ * @returns A promise that resolves when pandoc is downloaded and unpacked
+ */
 export function getPandoc(): Promise<void> {
-	const stream = getPandocStream();
+	const stream = getPandocStream()
+		.pipe(util.setExecutableBit())
+		.pipe(gulp.dest('.build/pandoc'));
 
 	return new Promise((resolve, reject) => {
 		es.merge([stream])
