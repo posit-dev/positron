e0c17f43a7be6a1b381e799d5deceae4c2605c0f
Nick Strayer <nick.strayer@rstudio.com>
2024-03-27 15:46:10 -0400

    Add ability to specify custom components for a given tag name when rendering HTML

--- src/vs/base/browser/renderHtml.tsx
+++ src/vs/base/browser/renderHtml.tsx
@@ -16,6 +16,13 @@ interface HTMLRendererOptions {
 	 * @param url The URL of the link that was clicked. Pulled from the href attribute.
 	 */
 	onLinkClick?: (url: string) => void;
+
+	/**
+	 * Component overrides for the HTML renderer.
+	 * Keyed by the node name (e.g. `'img'`) and the value is a component that can replace the
+	 * default rendering of that node.
+	 */
+	componentOverrides?: Record<string, (props: React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>) => React.ReactElement>;
 }
 
 /**
@@ -33,6 +40,12 @@ export const renderHtml = (html: string, opts: HTMLRendererOptions = {}): React.
 	// Because this code must run in a very strict security context, we cannot
 	// use parsers that rely on `innerHTML` or `DOMParser`.
 	const parsedContent = parseHtml(html);
+
+	// If there are component over-rides, use those to render the applicable elements.
+	function createElement(name: string, attrs: React.DOMAttributes<HTMLElement>, children?: (React.ReactNode | string)[] | React.ReactNode | string) {
+		const Component = opts.componentOverrides?.[name] || name;
+		return React.createElement(Component, attrs, children);
+	}
 
 	// Render the nodes into React elements.
 	const renderNode = (node: HtmlNode): React.ReactElement | undefined => {
@@ -72,24 +85,24 @@ export const renderHtml = (html: string, opts: HTMLRendererOptions = {}): React.
 			if (node.children.length === 1 && node.children[0].type === 'text') {
 				// If this is a tag with a single text child, create a React element
 				// for the tag and its text content.
-				return React.createElement(node.name!, nodeAttrs, node.children[0].content);
+				return createElement(node.name!, nodeAttrs, node.children[0].content);
 			} else {
 				if (node.children.length === 0) {
 					// If the node has no children, create a React element for
 					// the tag. For tags that cannot have children (such as
 					// <br>), React will throw an exception if an array if
 					// childen is supplied, even if the array is empty.
-					return React.createElement(node.name!, nodeAttrs);
+					return createElement(node.name!, nodeAttrs);
 				} else {
 					// Call the renderer recursively to render the children;
 					// create a React element for the tag and its children.
 					const children = node.children.map(renderNode);
-					return React.createElement(node.name!, nodeAttrs, children);
+					return createElement(node.name!, nodeAttrs, children);
 				}
 			}
 		} else if (node.type === 'tag') {
 			// Create a React element for the tag.
-			return React.createElement(node.name!, node.attrs);
+			return createElement(node.name!, node.attrs!);
 		} else {
 			// We don't render other types of nodes.
 			return undefined;
