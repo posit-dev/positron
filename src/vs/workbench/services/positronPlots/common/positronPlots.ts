75906cbfe7256d93fee8467c40e12a9129acff25
Tim Mok <timtmok@users.noreply.github.com>
2024-04-04 14:13:17 -0500

    Add UI modal dialog for saving plots (#2589)
    Preview before saving dynamic plots
    Use a progress bar when rendering
    Save plot validation
    Use save callback

--- src/vs/workbench/services/positronPlots/common/positronPlots.ts
+++ src/vs/workbench/services/positronPlots/common/positronPlots.ts
@@ -151,6 +151,11 @@ export interface IPositronPlotsService {
 	selectHistoryPolicy(policy: HistoryPolicy): void;
 
 	/**
+	 * Saves the plot.
+	 */
+	savePlot(): void;
+
+	/**
 	 * Placeholder for service initialization.
 	 */
 	initialize(): void;
