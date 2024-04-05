f3a1166ae7331d86cb679cafc23c67c9611dcfa3
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-04-05 13:19:56 -0700

    Use EXTENSION_ROOT_DIR, it's sitting right there!

--- extensions/positron-r/src/constants.ts
+++ extensions/positron-r/src/constants.ts
@@ -8,5 +8,5 @@ import * as path from 'path';
 export const EXTENSION_ROOT_DIR = path.join(__dirname, '..');
 
 // The minimum supported version of R
-const packageJson = require('../package.json');
+const packageJson = require(path.join(EXTENSION_ROOT_DIR, 'package.json'));
 export const MINIMUM_R_VERSION = packageJson.positron.minimumRVersion;
