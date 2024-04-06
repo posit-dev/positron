e6e04cfc4aba15ad913bb819a048ae82a34baed7
Brian Lambert <brianlambert@gmail.com>
2024-04-06 03:59:12 -0500

    Fix build break (#2687)

--- extensions/positron-r/src/constants.ts
+++ extensions/positron-r/src/constants.ts
@@ -3,10 +3,13 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as path from 'path';
+import * as fs from 'fs-extra';
 
 // The extension root directory.
 export const EXTENSION_ROOT_DIR = path.join(__dirname, '..');
 
-// The minimum supported version of R
-const packageJson = require(path.join(EXTENSION_ROOT_DIR, 'package.json'));
+// Read the package.json file.
+const packageJson = fs.readJSONSync(path.join(EXTENSION_ROOT_DIR, 'package.json'));
+
+// The minimum supported version of R.
 export const MINIMUM_R_VERSION = packageJson.positron.minimumRVersion;
