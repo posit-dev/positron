e050b6e58c10c9edb8d5d53858b75927b5bb0c64
isabelizimm <isabel.zimmerman@rstudio.com>
2024-04-09 20:09:12 -0400

    min matrix

--- extensions/positron-python/noxfile-positron.py
+++ extensions/positron-python/noxfile-positron.py
@@ -9,10 +9,9 @@ import nox
 @nox.session()
 @nox.parametrize('pandas', ['1.5.3'])
 @nox.parametrize('torch', ['1.12.1'])
-def pandas_tests(session, pandas, torch):
+def tests(session, pandas, torch):
     session.install("-r", "python_files/positron/pinned-test-requirements.txt")
-    if pandas != 'latest':
-        session.install('--force-reinstall', f'pandas=={pandas}')
-    if torch != 'latest':
-        session.install('--force-reinstall', f'torch=={torch}')
+    session.install('--force-reinstall', f'pandas=={pandas}')
+    session.install('--force-reinstall', f'torch=={torch}')
+
     session.run('pytest', 'python_files/positron/positron_ipykernel/tests')
