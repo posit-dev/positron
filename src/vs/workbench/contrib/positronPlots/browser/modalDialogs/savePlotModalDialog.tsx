3cab731bd148710e848e7c364c68817ba975c46f
Tim Mok <timtmok@users.noreply.github.com>
2024-04-12 17:13:35 -0400

    Refactor format saving
    Add format selector in dialog
    Browse now sets directory save location
    Add field for file name
    Validate path at save time
    Show error if saving failed

--- src/vs/workbench/contrib/positronPlots/browser/modalDialogs/savePlotModalDialog.tsx
+++ src/vs/workbench/contrib/positronPlots/browser/modalDialogs/savePlotModalDialog.tsx
@@ -7,7 +7,7 @@ import * as React from 'react';
 import { localize } from 'vs/nls';
 import { IWorkbenchLayoutService } from 'vs/workbench/services/layout/browser/layoutService';
 import { IRenderedPlot, PlotClientInstance } from 'vs/workbench/services/languageRuntime/common/languageRuntimePlotClient';
-import { IFileDialogService } from 'vs/platform/dialogs/common/dialogs';
+import { IDialogService, IFileDialogService } from 'vs/platform/dialogs/common/dialogs';
 import { URI } from 'vs/base/common/uri';
 import { ProgressBar } from 'vs/base/browser/ui/positronComponents/progressBar';
 import { LabeledTextInput } from 'vs/workbench/browser/positronComponents/positronModalDialog/components/labeledTextInput';
@@ -19,6 +19,9 @@ import { PositronModalDialog } from 'vs/workbench/browser/positronComponents/pos
 import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
 import { PositronModalReactRenderer } from 'vs/workbench/browser/positronModalReactRenderer/positronModalReactRenderer';
 import { FileFilter } from 'electron';
+import { DropDownListBox } from 'vs/workbench/browser/positronComponents/dropDownListBox/dropDownListBox';
+import { DropDownListBoxItem } from 'vs/workbench/browser/positronComponents/dropDownListBox/dropDownListBoxItem';
+import { IFileService } from 'vs/platform/files/common/files';
 
 export interface SavePlotOptions {
 	uri: string;
@@ -40,6 +43,8 @@ const BASE_DPI = 100; // matplotlib default DPI
  * Show the save plot modal dialog for dynamic plots.
  * @param layoutService the layout service for the modal
  * @param keybindingService the keybinding service to intercept shortcuts
+ * @param dialogService the dialog service to confirm the save
+ * @param fileService the file service to check if paths exist
  * @param fileDialogService the file dialog service to prompt where to save the plot
  * @param plotClient the dynamic plot client to render previews and the final image
  * @param savePlotCallback the action to take when the dialog closes
@@ -48,6 +53,8 @@ const BASE_DPI = 100; // matplotlib default DPI
 export const showSavePlotModalDialog = (
 	layoutService: IWorkbenchLayoutService,
 	keybindingService: IKeybindingService,
+	dialogService: IDialogService,
+	fileService: IFileService,
 	fileDialogService: IFileDialogService,
 	plotClient: PlotClientInstance,
 	savePlotCallback: (options: SavePlotOptions) => void,
@@ -66,7 +73,10 @@ export const showSavePlotModalDialog = (
 	renderer.render(
 		<SavePlotModalDialog
 			layoutService={layoutService}
+			dialogService={dialogService}
+			fileService={fileService}
 			fileDialogService={fileDialogService}
+			keybindingService={keybindingService}
 			renderer={renderer}
 			plotWidth={plotWidth}
 			plotHeight={plotHeight}
@@ -79,7 +89,10 @@ export const showSavePlotModalDialog = (
 
 interface SavePlotModalDialogProps {
 	layoutService: IWorkbenchLayoutService;
+	dialogService: IDialogService;
+	fileService: IFileService;
 	fileDialogService: IFileDialogService;
+	keybindingService: IKeybindingService;
 	renderer: PositronModalReactRenderer;
 	plotWidth: number;
 	plotHeight: number;
@@ -88,8 +101,16 @@ interface SavePlotModalDialogProps {
 	suggestedPath?: URI;
 }
 
+interface DirectoryState {
+	value: URI;
+	valid: boolean;
+	errorMessage?: string;
+}
+
 const SavePlotModalDialog = (props: SavePlotModalDialogProps) => {
-	const [path, setPath] = React.useState({ value: props.suggestedPath ?? URI.file(''), valid: true });
+	const [directory, setDirectory] = React.useState<DirectoryState>({ value: props.suggestedPath ?? URI.file(''), valid: true });
+	const [name, setName] = React.useState({ value: 'plot', valid: true });
+	const [format, setFormat] = React.useState(PlotFormat.PNG);
 	const [width, setWidth] = React.useState({ value: props.plotWidth, valid: true });
 	const [height, setHeight] = React.useState({ value: props.plotHeight, valid: true });
 	const [dpi, setDpi] = React.useState({ value: 100, valid: true });
@@ -107,8 +128,8 @@ const SavePlotModalDialog = (props: SavePlotModalDialogProps) => {
 	}, [props.plotClient.lastRender?.uri]);
 
 	const validateInput = React.useCallback((): boolean => {
-		return path.valid && width.valid && height.valid && dpi.valid;
-	}, [path, width, height, dpi]);
+		return directory.valid && width.valid && height.valid && dpi.valid;
+	}, [directory, width, height, dpi]);
 
 	React.useEffect(() => {
 		validateInput();
@@ -130,41 +151,58 @@ const SavePlotModalDialog = (props: SavePlotModalDialogProps) => {
 	};
 
 	const updatePath = (pathString: string) => {
-		const newPath = URI.file(pathString);
-		setPath({ value: newPath, valid: !!newPath });
+		try {
+			const newPath = URI.file(pathString);
+			props.fileService.exists(newPath).then(exists => {
+				setDirectory({
+					value: newPath, valid: exists,
+					errorMessage: exists ? undefined : localize('positron.savePlotModalDialog.pathDoesNotExist', "Path does not exist.")
+				});
+			});
+		} catch (error) {
+			setDirectory({ value: URI.file(''), valid: false, errorMessage: error.message });
+		}
 	};
 
 	const browseHandler = async () => {
-		const uri = await props.fileDialogService.showSaveDialog({
+		const uri = await props.fileDialogService.showOpenDialog({
 			title: localize('positron.savePlotModalDialog.title', "Save Plot"),
-			filters: filterEntries,
+			defaultUri: directory.value,
+			openLabel: localize('positron.savePlotModalDialog.select', "Select"),
+			canSelectFiles: false,
+			canSelectFolders: true,
+			canSelectMany: false,
 		});
 
-		if (uri?.fsPath.length) {
-			setPath({ value: uri, valid: true });
+		if (uri && uri.length > 0) {
+			updatePath(uri[0].fsPath);
 		}
 	};
 
 	const acceptHandler = async () => {
 		if (validateInput()) {
+			const fileExists = await props.fileService.exists(directory.value);
+			if (fileExists) {
+				const confirmation = await props.dialogService.confirm({
+					message: localize('positron.savePlotModalDialog.fileExists', "The file already exists. Do you want to overwrite it?"),
+					primaryButton: localize('positron.savePlotModalDialog.overwrite', "Overwrite"),
+					cancelButton: localize('positron.savePlotModalDialog.cancel', "Cancel"),
+				});
+				if (!confirmation.confirmed) {
+					return;
+				}
+			}
 			setRendering(true);
 
-			const extension = path.value.fsPath.split('.').pop()?.toLowerCase();
-			let format = extension as PlotFormat;
-
-			// default to PNG if the format is unknown
-			if (!Object.values(PlotFormat).includes(format)) {
-				format = PlotFormat.PNG;
-			}
-
-			const plotResult = await generatePreview(format);
-
-			if (plotResult) {
-				props.savePlotCallback({ uri: plotResult.uri, path: path.value });
-			}
-
-			setRendering(false);
-			props.renderer.dispose();
+			generatePreview(format)
+				.then(async (plotResult) => {
+					const filePath = URI.joinPath(directory.value, `${name.value}.${format}`);
+					props.savePlotCallback({ uri: plotResult.uri, path: filePath });
+				})
+				.finally(() => {
+					setRendering(false);
+					props.renderer.dispose();
+				});
 		}
 	};
 
@@ -211,15 +249,45 @@ const SavePlotModalDialog = (props: SavePlotModalDialogProps) => {
 						<div className='browse'>
 							<LabeledFolderInput
 								label={(() => localize(
-									'positron.savePlotModalDialog.path',
-									"Path"
+									'positron.savePlotModalDialog.directory',
+									"Directory"
 								))()}
-								value={path.value.fsPath}
+								value={directory.value.fsPath}
 								onChange={e => updatePath(e.target.value)}
 								onBrowse={browseHandler}
 								readOnlyInput={false}
-								error={!path.valid}
+								error={!directory.valid}
 								inputRef={inputRef} />
+						</div>
+						<div className='file'>
+							<LabeledTextInput
+								label={(() => localize(
+									'positron.savePlotModalDialog.name',
+									"Name"
+								))()}
+								value={name.value}
+								onChange={e => setName({ value: e.target.value, valid: !!e.target.value })}
+								error={!name.valid}
+							/>
+							<div>
+								<label>{(() => localize('positron.savePlotModalDialog.format', "Format"))()}
+									<DropDownListBox
+										title={(() => localize(
+											'positron.savePlotModalDialog.format',
+											"Format"
+										))()}
+										selectedIdentifier={format}
+										onSelectionChanged={(ext) => { setFormat(ext.options.identifier); }}
+										keybindingService={props.keybindingService}
+										layoutService={props.layoutService}
+										entries={[
+											new DropDownListBoxItem<PlotFormat, PlotFormat>({ identifier: PlotFormat.PNG, title: PlotFormat.PNG.toUpperCase(), value: PlotFormat.PNG }),
+											new DropDownListBoxItem<PlotFormat, PlotFormat>({ identifier: PlotFormat.JPEG, title: PlotFormat.JPEG.toUpperCase(), value: PlotFormat.JPEG }),
+											new DropDownListBoxItem<PlotFormat, PlotFormat>({ identifier: PlotFormat.SVG, title: PlotFormat.SVG.toUpperCase(), value: PlotFormat.SVG }),
+											new DropDownListBoxItem<PlotFormat, PlotFormat>({ identifier: PlotFormat.PDF, title: PlotFormat.PDF.toUpperCase(), value: PlotFormat.PDF }),
+										]} />
+								</label>
+							</div>
 						</div>
 						<div className='plot-input'>
 							<LabeledTextInput
@@ -258,9 +326,9 @@ const SavePlotModalDialog = (props: SavePlotModalDialogProps) => {
 							/>
 							<div className='error'>
 								<div>
-									{!path.valid && (() => localize(
-										'positron.savePlotModalDialog.noPathMessage',
-										"Specify a path."
+									{!directory.valid && (() => localize(
+										'positron.savePlotModalDialog.invalidPathError',
+										"Invalid path: {0}", directory.errorMessage
 									))()}
 								</div>
 								<div>
