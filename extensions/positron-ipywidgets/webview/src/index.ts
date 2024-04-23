5f77561636fa3d118cfef7697e13ab8bd6f6bc3c
seem <mwlorgat@gmail.com>
2024-04-23 12:49:43 +0200

    key callbacks on message id

--- extensions/positron-ipywidgets/webview/src/index.ts
+++ extensions/positron-ipywidgets/webview/src/index.ts
@@ -24,7 +24,7 @@ const comms = new Map<string, Comm>();
 class Comm implements base.IClassicComm {
 	private _on_msg: ((x: any) => void) | undefined;
 	private _on_close: ((x: any) => void) | undefined;
-	private _callbacks: base.ICallbacks | undefined;
+	private _callbacks = new Map<string, base.ICallbacks>();
 
 	constructor(
 		readonly comm_id: string,
@@ -32,23 +32,22 @@ class Comm implements base.IClassicComm {
 	) { }
 
 	open(data: JSONValue, callbacks?: base.ICallbacks | undefined, metadata?: JSONObject | undefined, buffers?: ArrayBuffer[] | ArrayBufferView[] | undefined): string {
-		this.set_callbacks(callbacks);
-
 		console.log('Comm.open', data, callbacks, metadata, buffers);
+		if (callbacks) {
+			throw new Error('Callbacks not supported in open');
+		}
 		// TODO: Move open logic here?
-		return '';
+		throw new Error('Method not implemented.');
 	}
 
 	send(data: any, callbacks?: base.ICallbacks | undefined, metadata?: JSONObject | undefined, buffers?: ArrayBuffer[] | ArrayBufferView[] | undefined): string {
-		// TODO: Support the callbacks argument
+		const msgId = UUID.uuid4();
+		console.log('Comm.send', data, callbacks, metadata, buffers, msgId);
 		// This seems to be the only requirement so far:
 		// 1. Call callbacks.iopub.status with a msg = { content: { execution_state: string } } when
 		//   the 'idle' message is received.
 		// Raise on unhandled callbacks?
-		this.set_callbacks(callbacks);
-
-		const msgId = UUID.uuid4();
-		console.log('Comm.send', data, callbacks, metadata, buffers, msgId);
+		this.set_callbacks(msgId, callbacks);
 		// This should return a string msgId. If this initiated an RPC call, the response should contain parent_header.msg_id with the same value.
 		vscode.postMessage({
 			type: 'comm_msg',
@@ -60,9 +59,10 @@ class Comm implements base.IClassicComm {
 	}
 
 	close(data?: JSONValue | undefined, callbacks?: base.ICallbacks | undefined, metadata?: JSONObject | undefined, buffers?: ArrayBuffer[] | ArrayBufferView[] | undefined): string {
-		this.set_callbacks(callbacks);
-
 		console.log('Comm.close', data, callbacks, metadata, buffers);
+		if (callbacks) {
+			throw new Error('Callbacks not supported in close');
+		}
 		vscode.postMessage({
 			type: 'comm_close',
 			content: {
@@ -82,7 +82,7 @@ class Comm implements base.IClassicComm {
 		this._on_close = callback;
 	}
 
-	set_callbacks(callbacks: base.ICallbacks | undefined): void {
+	set_callbacks(msgId: string, callbacks: base.ICallbacks | undefined): void {
 		// TODO: How are we supposed to handle multiple calls to set_callbacks?
 
 		// if (this._callbacks !== undefined) {
@@ -115,10 +115,14 @@ class Comm implements base.IClassicComm {
 			throw new Error('Unimplemented callbacks.iopub.output');
 		}
 		if (callbacks?.iopub?.status) {
-			this._callbacks = { iopub: { status: callbacks.iopub.status } };
+			if (this._callbacks.has(msgId)) {
+				throw new Error(`Callbacks already set for message id ${msgId}`);
+			}
+			this._callbacks.set(msgId, { iopub: { status: callbacks.iopub.status } });
 		}
 	}
 
+	// TODO: Use any type?
 	handle_msg(message: JSONObject): void {
 		console.log('Comm.handle_msg', message);
 		this._on_msg?.(message);
@@ -130,19 +134,21 @@ class Comm implements base.IClassicComm {
 		// setTimeout(() => {
 		// TODO: Currently this also fires when the kernel initiates the update...
 		//  In that case, I'm not sure if the iopub.status callback set earlier should fire.
-		this.handle_status({ content: { execution_state: 'idle' } } as IIOPubMessage<IOPubMessageType>);
+		const msgId = (message as any)?.parent_header?.msg_id as string;
+		if (msgId) {
+			// It's an RPC response, call callbacks.
+			const callbacks = this._callbacks.get(msgId);
+			if (callbacks) {
+				const statusMessage = { content: { execution_state: 'idle' } } as IIOPubMessage<IOPubMessageType>;
+				callbacks.iopub?.status?.(statusMessage);
+			}
+		}
 		// }, 0);
 	}
 
 	handle_close(message: JSONObject): void {
 		console.log('Comm.handle_close', message);
 		this._on_close?.(message);
-	}
-
-	handle_status(message: IIOPubMessage<IOPubMessageType>): void {
-		if (this._callbacks?.iopub?.status) {
-			this._callbacks.iopub.status(message);
-		}
 	}
 }
 
