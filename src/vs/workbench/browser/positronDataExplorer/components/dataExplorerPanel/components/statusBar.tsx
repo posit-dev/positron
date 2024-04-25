cef56941b1aa9fedc2e643414002ecc8c6ea07a5
Wes McKinney <wes@posit.co>
2024-04-25 18:48:01 -0500

    Data Explorer: Add rudimentary idle/computing/disconnected status indicator to action bar (#2881)
    * Add rudimentary status indicator to action bar in data explorer
    
    * Move status indicator to status bar with temporary CSS, address some review feedback
    
    * Add error handling so we do not get stuck in Computing state

--- src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/statusBar.tsx
+++ src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/statusBar.tsx
@@ -12,6 +12,52 @@ import { useEffect, useState } from 'react'; // eslint-disable-line no-duplicate
 // Other dependencies.
 import { DisposableStore } from 'vs/base/common/lifecycle';
 import { usePositronDataExplorerContext } from 'vs/workbench/browser/positronDataExplorer/positronDataExplorerContext';
+import { DataExplorerClientStatus } from 'vs/workbench/services/languageRuntime/common/languageRuntimeDataExplorerClient';
+
+interface ActivityIndicatorProps {
+	status: DataExplorerClientStatus;
+}
+
+const StatusBarActivityIndicator = (props: ActivityIndicatorProps) => {
+	const getStatusText = () => {
+		// TODO: localize
+		switch (props.status) {
+			case DataExplorerClientStatus.Idle:
+				return 'Idle';
+			case DataExplorerClientStatus.Computing:
+				return 'Computing';
+			case DataExplorerClientStatus.Disconnected:
+				return 'Disconnected';
+			case DataExplorerClientStatus.Error:
+				return 'Error';
+		}
+	};
+
+	const getStatusClass = () => {
+		switch (props.status) {
+			case DataExplorerClientStatus.Idle:
+				return 'idle';
+			case DataExplorerClientStatus.Computing:
+				return 'computing';
+			case DataExplorerClientStatus.Disconnected:
+				return 'disconnected';
+			case DataExplorerClientStatus.Error:
+				return 'error';
+		}
+	};
+
+	return (
+		<div className='status-bar-indicator'>
+			{/* TODO: Use different CSS  */}
+			<div className='action-bar-separator' aria-hidden='true' >
+				<div className='action-bar-separator-icon codicon codicon-positron-separator' />
+			</div>
+			<div className={`title ${getStatusClass()}`}>
+				{getStatusText()}
+			</div>
+		</div>
+	);
+};
 
 /**
  * StatusBar component.
@@ -25,6 +71,9 @@ export const StatusBar = () => {
 	const [backendState, setBackendState] = useState(
 		context.instance.dataExplorerClientInstance.cachedBackendState);
 
+	const [clientStatus, setClientStatus] = useState(
+		context.instance.dataExplorerClientInstance.status);
+
 	// Main useEffect.
 	useEffect(() => {
 		// Create the disposable store for cleanup.
@@ -33,6 +82,11 @@ export const StatusBar = () => {
 		// Add the onDidUpdateBackendState event handler.
 		disposableStore.add(context.instance.dataExplorerClientInstance.onDidUpdateBackendState(
 			state => setBackendState(state)
+		));
+
+		// Set up onDidStatusUpdate event handler.
+		disposableStore.add(context.instance.dataExplorerClientInstance.onDidStatusUpdate(
+			status => setClientStatus(status)
 		));
 
 		// Return the cleanup function that will dispose of the event handlers.
@@ -46,6 +100,7 @@ export const StatusBar = () => {
 		numRows = backendState.table_shape.num_rows;
 		numColumns = backendState?.table_shape.num_columns;
 	}
+
 	if (backendState && backendState.row_filters.length > 0) {
 		const numUnfilteredRows = backendState.table_unfiltered_shape.num_rows;
 		const pctFiltered = numUnfilteredRows === 0 ? 0 : 100 * numRows / numUnfilteredRows;
@@ -65,6 +120,9 @@ export const StatusBar = () => {
 				<span className='counter'>{numColumns.toLocaleString()}</span>
 				<span>&nbsp;</span>
 				<span className='label'>columns</span>
+				<StatusBarActivityIndicator
+					status={clientStatus}
+				/>
 			</div>
 		);
 	} else {
@@ -77,6 +135,9 @@ export const StatusBar = () => {
 				<span className='counter'>{numColumns.toLocaleString()}</span>
 				<span>&nbsp;</span>
 				<span className='label'>columns</span>
+				<StatusBarActivityIndicator
+					status={clientStatus}
+				/>
 			</div>
 		);
 	}
