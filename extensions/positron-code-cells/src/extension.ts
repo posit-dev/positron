92529491840716bce0560c963f0fc5bd61851b15
Jonathan <jonathan@rstudio.com>
2024-04-29 09:48:12 -0700

    temporarily disable code cell code folding (#2902)

--- extensions/positron-code-cells/src/extension.ts
+++ extensions/positron-code-cells/src/extension.ts
@@ -7,7 +7,6 @@ import { initializeLogging } from './logging';
 import { CellCodeLensProvider } from './codeLenses';
 import { registerCommands } from './commands';
 import { activateDecorations } from './decorations';
-import { CellFoldingRangeProvider } from './folding';
 import { activateContextKeys } from './context';
 
 export const IGNORED_SCHEMES = ['vscode-notebook-cell', 'vscode-interactive-input'];
@@ -20,7 +19,16 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 	context.subscriptions.push(
 		vscode.languages.registerCodeLensProvider('*', new CellCodeLensProvider()),
 
-		vscode.languages.registerFoldingRangeProvider('*', new CellFoldingRangeProvider()),
+		// Temporarily disabled because registering this provider causes it to
+		// become the *only* folding range provider for R and Python files,
+		// suppressing the built-in folding range provider.
+		//
+		// We can re-enable this when the R and Python language packs supply
+		// their own folding range providers.
+		//
+		// https://github.com/posit-dev/positron/issues/1908
+
+		// vscode.languages.registerFoldingRangeProvider('*', new CellFoldingRangeProvider()),
 	);
 
 	activateDecorations(context.subscriptions);
