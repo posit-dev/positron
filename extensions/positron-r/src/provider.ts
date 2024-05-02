36a959759600a59becb3a275faebdfcdb1b5b071
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-05-02 11:41:00 -0500

    Try to detect recent RStudio usage on Windows (#2966)
    * Return early for first qualifying file
    
    * Level up detection of recent RStudio usage on Windows

--- extensions/positron-r/src/provider.ts
+++ extensions/positron-r/src/provider.ts
@@ -448,25 +448,36 @@ function isRStudioUser(): boolean {
 		const filenames = fs.readdirSync(rstudioStateFolderPath());
 		const today = new Date();
 		const thirtyDaysAgo = new Date(new Date().setDate(today.getDate() - 30));
-		const recentlyModified = new Array<boolean>();
-		filenames.forEach(file => {
+		const isRecentlyModified = filenames.some(file => {
 			const stats = fs.statSync(rstudioStateFolderPath(file));
-			recentlyModified.push(stats.mtime > thirtyDaysAgo);
+			return stats.mtime > thirtyDaysAgo;
 		});
-		return recentlyModified.some(bool => bool === true);
+		return isRecentlyModified;
 	} catch { }
 	return false;
 }
 
 /**
- * Returns the path to RStudio's state folder directory. Doesn't currently work
- * on Windows; see XDG specification for the correct path there.
+ * Returns the path to RStudio's state folder directory. Currently checks only the default for each
+ * OS. A more earnest effort would require fully implementing the logic in RStudio's `userDataDir()`
+ * functions (there are implementations in both C++ and Typescript). That would add logic to
+ * check the variables RSTUDIO_DATA_HOME and XDG_DATA_HOME.
  *
  * @param pathToAppend The path to append, if any
  * @returns The path to RStudio's state folder directory.
  */
 function rstudioStateFolderPath(pathToAppend = ''): string {
-	// TODO: Windows
-	const newPath = path.join(process.env.HOME!, '.local/share/rstudio', pathToAppend);
+	let newPath: string;
+	switch (process.platform) {
+		case 'darwin':
+		case 'linux':
+			newPath = path.join(process.env.HOME!, '.local/share/rstudio', pathToAppend);
+			break;
+		case 'win32':
+			newPath = path.join(process.env.LOCALAPPDATA!, 'RStudio', pathToAppend);
+			break;
+		default:
+			throw new Error('Unsupported platform');
+	}
 	return newPath;
 }
