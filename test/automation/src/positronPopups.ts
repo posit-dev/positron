531b1326e7ed8e9b7a161af72f4a1638209fb31f
Chris Mead <chris.mead@posit.co>
2024-05-02 09:22:34 -0500

    Variables pane UI smoke test for Python & R (#2873)
    * comment updates
    
    * remove dead import
    
    * fail if bad env vars; remove explicits waits
    
    * wait for console text instead of "sleeping"
    
    * bug fix
    
    * review comment
    
    * Update test/automation/src/playwrightDriver.ts
    
    Co-authored-by: Wasim Lorgat <mwlorgat@gmail.com>
    Signed-off-by: Christopher Mead <chrisrmead@comcast.net>
    
    * rework console; try to reduce waits
    
    * enter instead of newline
    
    * slightly longer wait in R test just in case
    
    * perf fix python
    
    * Merge branch 'main' into cmead/smoke-test-basics
    
    * forgot R env var in full test
    
    * perf fix
    
    * specify R version text; get console data faster
    
    * Added R variables test
    
    * Merge branch 'main' into cmead/smoke-test-basics
    
    * Improve local performance; correctly log console contents
    
    * refactored
    
    * add screenshot to debug variables not showing up
    
    * test upload of log file as artifact & test ci bug fixes
    
    * basic test function in place; needs refactoring
    
    Lead-authored-by: Chris Mead <chris.mead@posit.co>
    Co-authored-by: Christopher Mead <chrisrmead@comcast.net>
    Signed-off-by: Chris Mead <chris.mead@posit.co>

---
+++ test/automation/src/positronPopups.ts
@@ -0,0 +1,42 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+
+import { Code } from './code';
+
+const POSITRON_MODAL_DIALOG_BOX = '.positron-modal-dialog-box';
+const POSITRON_MODAL_DIALOG_BOX_OK = '.positron-modal-dialog-box .ok-cancel-action-bar .positron-button.action-bar-button.default';
+const NOTIFICATION_TOAST = '.notification-toast';
+
+export class PositronPopups {
+
+	constructor(private code: Code) { }
+
+	async installIPyKernel() {
+
+		try {
+			await this.code.waitForElement(POSITRON_MODAL_DIALOG_BOX, undefined, 50);
+			await this.code.waitAndClick(POSITRON_MODAL_DIALOG_BOX_OK);
+			console.log('Installing ipykernel');
+			await this.waitForToastToAppear();
+			await this.waitForToastToDisappear();
+			console.log('Installed ipykernel');
+			// after toast disappears console may not yet be refreshed (still on old interpreter)
+			// TODO: make this smart later, perhaps by getting the console state from the API
+			await this.code.wait(5000);
+		} catch { }
+	}
+
+	async waitForToastToDisappear() {
+		console.log('Waiting for toast to be detacted');
+		const toastLocator = this.code.driver.getLocator(NOTIFICATION_TOAST);
+		await toastLocator.waitFor({ state: 'detached', timeout: 20000 });
+	}
+
+	async waitForToastToAppear() {
+		console.log('Waiting for toast to be attached');
+		const toastLocator = this.code.driver.getLocator(NOTIFICATION_TOAST);
+		await toastLocator.waitFor({ state: 'attached', timeout: 20000 });
+	}
+}
