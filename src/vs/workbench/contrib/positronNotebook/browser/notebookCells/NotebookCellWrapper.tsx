b7d21e3041a9b31af9b29781b05a12756430b6e2
Nick Strayer <nick.strayer@posit.co>
2024-05-02 10:09:37 -0500

    Add cell selection and basic keyboard navigation to notebooks (#2927)
    * Add logic for observable setting and observing the selected cell(s)
    
    * Add cell selection on click and visual indication of selected cell
    
    * Hook up super simple keyboard navigation for cell selection
    
    * Add differentation between editing selection and plain selection.
    
    * Switch to keeping track of currently editing cell instead of folding info into selection
    
    * First stage of selection state machine
    
    * Selection via keyboard is working, edit entering is not yet working
    
    * Basic keyboard navigation working with arrow keys
    
    * Hide action bar except on selection and hover
    
    * Selection now stays (or goes) appropriately when new cells are added
    
    * Replace xstate with vanilla class
    
    * Make sure editor is opened before focusing on markdown cells editor
    
    * Fix multicell selection.
    
    * Make focus indication outlines more subtle but logical.
    
    * Add back .selected style so multiple-select actually shows the selections
    
    * Make methods for interacting with selection state less key-specific.
    
    * Add ability to make multi-selection with mouse
    
    * Fix confusing naming convention for two state variables in favor of a single `_setState()` method.
    
    * Remove left-over xstate dependency definition
    
    * Remove left-over commented out code
    
    * Scope keyboard event listening to the notebook container rather than the entire window.
    
    * Remove DisposableObserveValue in favor of using Event.fromObservable()
    
    * Add event subscription directly to the selection state machine
    
    * More dead-code removal
    
    * Explain random settimeout
    
    * Fix out-of-date jsdoc comments.
    
    * Add log message for invalid selection in the selection method of selection machine
    
    * Dont use .tsx all willy nilly.
    
    * Switch to enum and simplify selection state type
    
    * Make the selection of the action buttons with keyboard a bit clearer.

---
+++ src/vs/workbench/contrib/positronNotebook/browser/notebookCells/NotebookCellWrapper.tsx
@@ -0,0 +1,48 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+import 'vs/css!./NotebookCellWrapper';
+
+import * as React from 'react';
+import { CellKind } from 'vs/workbench/contrib/notebook/common/notebookCommon';
+import { IPositronNotebookCell } from 'vs/workbench/contrib/positronNotebook/browser/notebookCells/interfaces';
+import { useObservedValue } from 'vs/workbench/contrib/positronNotebook/browser/useObservedValue';
+import { CellSelectionType } from 'vs/workbench/contrib/positronNotebook/browser/notebookCells/selectionMachine';
+
+export function NotebookCellWrapper({ cell, children }: { cell: IPositronNotebookCell; children: React.ReactNode }) {
+	const selected = useObservedValue(cell.selected);
+	const editing = useObservedValue(cell.editing);
+	const cellRef = React.useRef<HTMLDivElement>(null);
+
+	React.useEffect(() => {
+		if (cellRef.current) {
+			// Attach the container to the cell instance can properly control focus.
+			cell.attachContainer(cellRef.current);
+		}
+	}, [cell, cellRef]);
+
+	const selectionClass = editing ? 'editing' : selected ? 'selected' : 'unselected';
+	return <div
+		className={`positron-notebook-cell positron-notebook-${cell.kind === CellKind.Code ? 'code' : 'markdown'}-cell ${selectionClass}`}
+		ref={cellRef}
+		tabIndex={0}
+		onClick={(e) => {
+			const clickTarget = e.nativeEvent.target as HTMLElement;
+			// If any of the element or its parents have the class
+			// 'positron-cell-editor-monaco-widget' then don't run the select code as the editor
+			// widget itself handles that logic
+			const childOfEditor = clickTarget.closest('.positron-cell-editor-monaco-widget');
+			if (childOfEditor) {
+				return;
+			}
+			if (selected) {
+				cell.deselect();
+				return;
+			}
+			const addMode = e.shiftKey || e.ctrlKey || e.metaKey;
+			cell.select(addMode ? CellSelectionType.Add : CellSelectionType.Normal);
+		}}
+	>
+		{children}
+	</div>;
+}
