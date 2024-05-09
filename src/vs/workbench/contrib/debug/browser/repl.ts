0168a9b7c0b8f9484dcd9b2ee7c2bb533530a508
Brian Lambert <brianlambert@gmail.com>
2024-05-09 11:52:40 -0700

    Rename 'Clear Console' to 'Clear Debug Console'

--- src/vs/workbench/contrib/debug/browser/repl.ts
+++ src/vs/workbench/contrib/debug/browser/repl.ts
@@ -974,7 +974,11 @@ registerAction2(class extends ViewAction<Repl> {
 		super({
 			id: 'workbench.debug.panel.action.clearReplAction',
 			viewId: REPL_VIEW_ID,
-			title: localize2('clearRepl', 'Clear Console'),
+			// --- Start Positron ---
+			// Rename 'Clear Console' to 'Clear Debgug Console' to differentiate it from Positron's
+			// Clear Console command.
+			title: localize2('clearRepl', 'Clear Debug Console'),
+			// --- End Positron ---
 			metadata: {
 				description: localize2('clearRepl.descriotion', 'Clears all program output from your debug REPL')
 			},
