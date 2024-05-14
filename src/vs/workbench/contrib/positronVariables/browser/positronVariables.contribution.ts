359215e3ed41b9e7552998e9508ba4886e1744bb
Nick Strayer <nick.strayer@posit.co>
2024-05-14 09:06:01 -0500

    Make the session's pane its own seperate view container now that plots and variables go into it. (#3129)

--- src/vs/workbench/contrib/positronVariables/browser/positronVariables.contribution.ts
+++ src/vs/workbench/contrib/positronVariables/browser/positronVariables.contribution.ts
@@ -3,57 +3,25 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as nls from 'vs/nls';
-import { Codicon } from 'vs/base/common/codicons';
 import { Disposable } from 'vs/base/common/lifecycle';
 import { KeyMod, KeyCode } from 'vs/base/common/keyCodes';
 import { Registry } from 'vs/platform/registry/common/platform';
 import { registerAction2 } from 'vs/platform/actions/common/actions';
-import { registerIcon } from 'vs/platform/theme/common/iconRegistry';
 import { PositronVariablesFocused } from 'vs/workbench/common/contextkeys';
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors';
 import { LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { ViewPaneContainer } from 'vs/workbench/browser/parts/views/viewPaneContainer';
 import { PositronVariablesViewPane } from 'vs/workbench/contrib/positronVariables/browser/positronVariablesView';
 import { PositronVariablesRefreshAction } from 'vs/workbench/contrib/positronVariables/browser/positronVariablesActions';
 import { IPositronVariablesService } from 'vs/workbench/services/positronVariables/common/interfaces/positronVariablesService';
 import { ICommandAndKeybindingRule, KeybindingWeight, KeybindingsRegistry } from 'vs/platform/keybinding/common/keybindingsRegistry';
 import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions, IWorkbenchContribution } from 'vs/workbench/common/contributions';
-import { ViewContainer, IViewContainersRegistry, ViewContainerLocation, Extensions as ViewContainerExtensions, IViewsRegistry } from 'vs/workbench/common/views';
+import { Extensions as ViewContainerExtensions, IViewsRegistry } from 'vs/workbench/common/views';
 import { POSITRON_VARIABLES_COLLAPSE, POSITRON_VARIABLES_COPY_AS_HTML, POSITRON_VARIABLES_COPY_AS_TEXT, POSITRON_VARIABLES_EXPAND } from 'vs/workbench/contrib/positronVariables/browser/positronVariablesIdentifiers';
+import { POSITRON_SESSION_CONTAINER, positronSessionViewIcon } from 'vs/workbench/contrib/positronSession/browser/positronSessionContainer';
 
 // The Positron variables view identifier.
 export const POSITRON_VARIABLES_VIEW_ID = 'workbench.panel.positronVariables';
-
-// The Positron variables view icon.
-const positronVariablesViewIcon = registerIcon(
-	'positron-variables-view-icon',
-	Codicon.positronVariablesView,
-	nls.localize('positronVariablesViewIcon', 'View icon of the Positron variables view.')
-);
-
-// Register the Positron variables view container.
-export const VIEW_CONTAINER: ViewContainer = Registry.as<IViewContainersRegistry>(
-	ViewContainerExtensions.ViewContainersRegistry
-).registerViewContainer(
-	{
-		id: POSITRON_VARIABLES_VIEW_ID,
-		title: {
-			value: nls.localize('positron.session', "Session"),
-			original: 'Session'
-		},
-		icon: positronVariablesViewIcon,
-		order: 1,
-		ctorDescriptor: new SyncDescriptor(ViewPaneContainer, [POSITRON_VARIABLES_VIEW_ID, { mergeViewWithContainerWhenSingleView: true }]),
-		storageId: POSITRON_VARIABLES_VIEW_ID,
-		hideIfEmpty: false,
-	},
-	ViewContainerLocation.AuxiliaryBar,
-	{
-		doNotRegisterOpenCommand: true,
-		isDefault: true
-	}
-);
 
 // Register the Positron variables view.
 Registry.as<IViewsRegistry>(ViewContainerExtensions.ViewsRegistry).registerViews(
@@ -67,7 +35,7 @@ Registry.as<IViewsRegistry>(ViewContainerExtensions.ViewsRegistry).registerViews
 			ctorDescriptor: new SyncDescriptor(PositronVariablesViewPane),
 			canToggleVisibility: false,
 			canMoveView: true,
-			containerIcon: positronVariablesViewIcon,
+			containerIcon: positronSessionViewIcon,
 			openCommandActionDescriptor: {
 				id: 'workbench.action.positron.toggleVariables',
 				mnemonicTitle: nls.localize({ key: 'miToggleVariables', comment: ['&& denotes a mnemonic'] }, "&&Variables"),
@@ -78,7 +46,7 @@ Registry.as<IViewsRegistry>(ViewContainerExtensions.ViewsRegistry).registerViews
 			}
 		}
 	],
-	VIEW_CONTAINER
+	POSITRON_SESSION_CONTAINER
 );
 
 /**
