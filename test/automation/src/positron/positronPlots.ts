5937661347e1f9bf42671c37609e7187379f6643
Chris Mead <chris.mead@posit.co>
2024-05-15 17:39:43 -0600

    test in ci with resembljs-node

--- test/automation/src/positron/positronPlots.ts
+++ test/automation/src/positron/positronPlots.ts
@@ -5,6 +5,10 @@
 
 import { Locator } from '@playwright/test';
 import * as fs from 'fs';
+import { ComparisonOptions } from 'resemblejs';
+import compareImages = require('resemblejs/compareImages');
+import util = require('util');
+
 
 export class PositronPlots {
 
@@ -12,12 +16,28 @@ export class PositronPlots {
 
 	async compareScreenshots(plotLocator: Locator, filePath: string): Promise<boolean> {
 
-		const buffer = await plotLocator.screenshot();
-		const foundBuffer = buffer.toString('base64');
+		const options: ComparisonOptions = {
+			output: {
+				errorColor: {
+					red: 255,
+					green: 0,
+					blue: 255
+				},
+				errorType: 'movement',
+				transparency: 0.3,
+				largeImageThreshold: 1200,
+				useCrossOrigin: false,
 
-		const goldenBuffer = fs.readFileSync(filePath, 'base64');
+			},
+			scaleToSameSize: true,
+			ignore: 'antialiasing'
+		};
 
-		return goldenBuffer === foundBuffer;
+		const data = await compareImages(await plotLocator.screenshot(), await fs.promises.readFile(filePath), options);
+
+		console.log(util.inspect(data, { showHidden: false, depth: null, colors: true }));
+
+		return true;
 
 	}
 }
