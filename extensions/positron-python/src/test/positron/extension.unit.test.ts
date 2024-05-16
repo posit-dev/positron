48c778d4f50ed4c73fdf70e4b832779e1985665b
isabelizimm <isabel.zimmerman@rstudio.com>
2024-05-16 15:44:35 -0400

    format fix

--- extensions/positron-python/src/test/positron/extension.unit.test.ts
+++ extensions/positron-python/src/test/positron/extension.unit.test.ts
@@ -14,36 +14,36 @@ import { Architecture } from '../../client/common/utils/platform';
 import { EnvironmentType } from '../../client/pythonEnvironments/info';
 
 suite('Set up extension', () => {
-	let serviceContainer: TypeMoq.IMock<IServiceContainer>;
-	let interpreterService: TypeMoq.IMock<IInterpreterService>;
-	let installer: TypeMoq.IMock<IInstaller>;
-	const pythonPath = path.join('path', 'to', 'python', 'interpreter');
-	const interpreter = {
-		architecture: Architecture.Unknown,
-		path: pythonPath,
-		sysPrefix: '',
-		sysVersion: '',
-		envType: EnvironmentType.Conda,
-		// arbitrary but supported version
-		version: new SemVer('3.11.4'),
-	};
+    let serviceContainer: TypeMoq.IMock<IServiceContainer>;
+    let interpreterService: TypeMoq.IMock<IInterpreterService>;
+    let installer: TypeMoq.IMock<IInstaller>;
+    const pythonPath = path.join('path', 'to', 'python', 'interpreter');
+    const interpreter = {
+        architecture: Architecture.Unknown,
+        path: pythonPath,
+        sysPrefix: '',
+        sysVersion: '',
+        envType: EnvironmentType.Conda,
+        // arbitrary but supported version
+        version: new SemVer('3.11.4'),
+    };
 
-	setup(() => {
-		serviceContainer = TypeMoq.Mock.ofType<IServiceContainer>();
-		interpreterService = TypeMoq.Mock.ofType<IInterpreterService>();
-		installer = TypeMoq.Mock.ofType<IInstaller>();
-		serviceContainer.setup((s) => s.get(IInstaller)).returns(() => installer.object);
-		serviceContainer.setup((s) => s.get(IInterpreterService)).returns(() => interpreterService.object);
-		interpreterService
-			.setup((i) => i.getInterpreterDetails(TypeMoq.It.isAny()))
-			.returns(() => Promise.resolve(interpreter));
-		installer
-			.setup((i) => i.install(TypeMoq.It.isAny()))
-			.returns(() => Promise.resolve(InstallerResponse.Installed));
-	});
+    setup(() => {
+        serviceContainer = TypeMoq.Mock.ofType<IServiceContainer>();
+        interpreterService = TypeMoq.Mock.ofType<IInterpreterService>();
+        installer = TypeMoq.Mock.ofType<IInstaller>();
+        serviceContainer.setup((s) => s.get(IInstaller)).returns(() => installer.object);
+        serviceContainer.setup((s) => s.get(IInterpreterService)).returns(() => interpreterService.object);
+        interpreterService
+            .setup((i) => i.getInterpreterDetails(TypeMoq.It.isAny()))
+            .returns(() => Promise.resolve(interpreter));
+        installer
+            .setup((i) => i.install(TypeMoq.It.isAny()))
+            .returns(() => Promise.resolve(InstallerResponse.Installed));
+    });
 
-	test('checkAndInstallPython should install Python if necessary', async () => {
-		const result = await checkAndInstallPython(pythonPath, serviceContainer.object);
-		assert.strictEqual(result, InstallerResponse.Installed, 'Python install is not resolved');
-	});
+    test('checkAndInstallPython should install Python if necessary', async () => {
+        const result = await checkAndInstallPython(pythonPath, serviceContainer.object);
+        assert.strictEqual(result, InstallerResponse.Installed, 'Python install is not resolved');
+    });
 });
