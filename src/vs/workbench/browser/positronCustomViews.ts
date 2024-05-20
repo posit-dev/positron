c80a0b960274dbdb67bb62d8f4a24ce9735d7a93
Nick Strayer <nick.strayer@posit.co>
2024-05-20 10:45:04 -0400

    Jump-to-layout action (#3179)
    * First draft of four-pane work
    
    * Add side-by-side layout action with layout hiding included
    
    * Consolidate logic for custom layouts to helper function and make additional methods generic.
    
    * Fix layout dump display
    
    * Add comment telling about how the loadCustomViewDescriptor function is a copy of existing logic
    
    * Make names of properties on the custom layout less ambiguous
    
    * Add support for resizing panels in addition to hiding or showing them
    
    * Add support for controlling size of the shown or hidden panels.
    
    * Move logic out of layout service and into our own functions
    
    * Simplify the setting of a view state by utilizing the reactive watching of the `.viewCustomizations` property.
    
    * Add alignment for panel to description
    
    * Simplify to a single layout action with a quick-pick menu for choosing which layout to apply.
    
    * Go back to full logic for loading customizations instead of trying to trick descriptor service into doing it.
    
    * Start adding logic for order within view containers
    
    * Actually enable movement within a give view container for multi-view containers.
    
    * Use viewOrder instead of viewLocations
    
    * Remove unused view container enablement property
    
    * cleanup
    
    * Further simplify our custom layout type and add conversion functions where needed. Temporarily disable layout dump action while working on the layout type.
    
    * Loading description now also updates the order of the view containers within panes as configured by description.
    
    * Make the views within a container optional.
    
    * Simplify sizing of the panels to be a single `size` parameter that is set to the appropriate value for the panel.
    
    * Add ability to provide the size of a layout pane in terms of percentage of the viewport.
    
    * Save the new custom layout view after doing changes.
    
    * Add basic ability to size views within a compound view container
    
    * Switch to a more predictable relative sizing protocol for sized panels.
    
    * Add support for configuring which view container is open by default.
    
    * Add comments explaining why we have commented-out functions and actions.
    
    * Add side-by-side and side-by-side with no plot view to layouts
    
    * Tweak 4-pane layout to match rstudio's defaults
    
    * Cleanup missed positron blocks and extra spaces added. Also make some comment clarifications.
    
    * Split up the types and add jsdoc comments for all parts.
    
    * rename `sizeUnit` to `relativeSize` because it's a more accurate name
    
    * Quick cleanup on 4-pane layout
    
    * Make sure console is the opened container in aux bar for default side-by-side
    
    * Move console into the side-by-side layout and remove the no-plots variant.
    
    * Add ability to set a view collapsed.
    
    * Clarifying comments about conflicts between `collapsed` and `relativeSize` in view description.
    
    * Add log message if pane move fails.
    
    * Update src/vs/workbench/browser/parts/views/viewPaneContainer.ts
    
    Co-authored-by: Jonathan <jonathan@rstudio.com>
    Signed-off-by: Nick Strayer <nick.strayer@posit.co>
    
    * Extract all the types for custom layouts to the `common/` level to avoid duplication for the view service interfaces.
    
    * Remove unneccesary exports from positronCustomViews.ts
    
    * More removal of unused methods/code
    
    * Remove comments about why the layout-dumping related functions are commented out.
    
    ---------
    
    Signed-off-by: Nick Strayer <nick.strayer@posit.co>
    Co-authored-by: Jonathan <jonathan@rstudio.com>

---
+++ src/vs/workbench/browser/positronCustomViews.ts
@@ -0,0 +1,155 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ISerializableView, IViewSize } from 'vs/base/browser/ui/grid/gridview';
+import { localize } from 'vs/nls';
+import { IQuickPickItem } from 'vs/platform/quickinput/common/quickInput';
+import { CustomPositronLayoutDescription, KnownPositronLayoutParts } from 'vs/workbench/common/positronCustomViews';
+import { ViewContainerLocation } from 'vs/workbench/common/views';
+import { PanelAlignment, Parts } from 'vs/workbench/services/layout/browser/layoutService';
+
+export type PartViewInfo = {
+	partView: ISerializableView;
+	currentSize: IViewSize;
+	alignment?: PanelAlignment;
+	hidden: boolean;
+	hideFn: (hidden: boolean, skipLayout?: boolean | undefined) => void;
+};
+
+const partToViewContainerLocation: Record<KnownPositronLayoutParts, ViewContainerLocation> = {
+	[Parts.PANEL_PART]: ViewContainerLocation.Panel,
+	[Parts.SIDEBAR_PART]: ViewContainerLocation.Sidebar,
+	[Parts.AUXILIARYBAR_PART]: ViewContainerLocation.AuxiliaryBar,
+};
+
+/**
+ * Convert our custom layout description to the `IViewsCustomizations` format that the
+ * `viewDescriptorService` uses for its internal state.
+ * @param layout Positron custom layout description
+ * @returns Simplified view info in the form of viewContainerLocations and
+ * viewDescriptorCustomizations. See `IViewsCustomizations` for more info.
+ */
+export function layoutDescriptionToViewInfo(layout: CustomPositronLayoutDescription) {
+	const viewContainerLocations = new Map<string, ViewContainerLocation>();
+	const viewDescriptorCustomizations = new Map<string, string>();
+
+	for (const [part, info] of Object.entries(layout)) {
+		const viewContainers = info.viewContainers;
+		if (!viewContainers) { continue; }
+		const viewContainerLocation = partToViewContainerLocation[part as KnownPositronLayoutParts];
+
+		for (const viewContainer of viewContainers) {
+			viewContainerLocations.set(viewContainer.id, viewContainerLocation);
+
+			if (!viewContainer.views) { continue; }
+			for (const view of viewContainer.views) {
+				viewDescriptorCustomizations.set(view.id, viewContainer.id);
+			}
+		}
+	}
+
+	return {
+		viewContainerLocations,
+		viewDescriptorCustomizations,
+	};
+}
+
+
+// export function createPositronCustomLayoutDescriptor(accessor: ServicesAccessor): CustomPositronLayoutDescription {
+// 	const views = accessor.get(IViewDescriptorService).dumpViewCustomizations();
+// 	const layoutService = accessor.get(IWorkbenchLayoutService);
+
+// 	const getPartLayout = (part: KnownPositronLayoutParts) => {
+// 		const { currentSize, hidden } = layoutService.getPartViewInfo(part);
+// 		return { width: currentSize.width, height: currentSize.height, hidden };
+// 	};
+
+// 	return {
+// 		[Parts.SIDEBAR_PART]: getPartLayout(Parts.SIDEBAR_PART),
+// 		[Parts.PANEL_PART]: getPartLayout(Parts.PANEL_PART),
+// 		[Parts.AUXILIARYBAR_PART]: getPartLayout(Parts.AUXILIARYBAR_PART),
+// 	};
+// }
+
+
+type LayoutPick = IQuickPickItem & { layoutDescriptor: CustomPositronLayoutDescription };
+export const positronCustomLayoutOptions: LayoutPick[] = [
+	{
+		id: 'fourPaneDS',
+		label: localize('choseLayout.fourPaneDS', 'Four Pane Data Science'),
+		layoutDescriptor: {
+			[Parts.SIDEBAR_PART]: {
+				size: 200,
+				hidden: false,
+			},
+			[Parts.PANEL_PART]: {
+				size: '40%',
+				hidden: false,
+				alignment: 'center',
+				viewContainers: [
+					{
+						id: 'workbench.panel.positronConsole',
+						opened: true,
+					},
+					{
+						id: 'terminal',
+					},
+				]
+			},
+			[Parts.AUXILIARYBAR_PART]: {
+				size: 500, // Use pixel sizes for auxiliary bar to allow editor to take up the rest of the space
+				hidden: false,
+				viewContainers: [
+					{
+						id: 'workbench.panel.positronSession',
+						opened: true,
+						views: [
+							{
+								id: 'workbench.panel.positronVariables',
+							},
+							{
+								id: 'workbench.panel.positronPlots',
+							},
+						]
+					},
+				]
+			}
+		},
+	},
+	{
+		id: 'side-by-side',
+		label: localize('choseLayout.sideBySide', 'Side-by-side Data Science'),
+		layoutDescriptor: {
+			[Parts.PANEL_PART]: {
+				hidden: true,
+				alignment: 'center'
+			},
+			[Parts.SIDEBAR_PART]: {
+				hidden: true
+			},
+			[Parts.AUXILIARYBAR_PART]: {
+				hidden: false,
+				size: '50%',
+				viewContainers: [
+					{
+						id: 'workbench.panel.positronSession',
+						opened: true,
+						views: [
+							{
+								id: 'workbench.panel.positronConsole',
+							},
+							{
+								id: 'workbench.panel.positronVariables',
+							},
+							{
+								id: 'workbench.panel.positronPlots',
+								collapsed: true,
+							},
+						]
+					},
+				]
+			},
+		},
+	},
+];
