c58587174657f5d99351d2f56b8bbd3904ffeb67
Wasim Lorgat <mwlorgat@gmail.com>
2024-05-21 13:10:17 -0500

    Fix "Python: Select Interpreter" when choosing the selected interpreter (#3197)
    * move runtime selection lower down the stack
    
    * make python runtime manager a service
    
    * initial python runtime manager unit tests
    
    * only select runtime if updating python path does not fail

--- extensions/positron-python/src/client/positron/extension.ts
+++ extensions/positron-python/src/client/positron/extension.ts
@@ -1,89 +1,24 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2023-2024 Posit Software, PBC. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
 import { ProgressLocation, ProgressOptions } from 'vscode';
 import * as fs from 'fs';
-// eslint-disable-next-line import/no-unresolved
-import * as positron from 'positron';
-import { PythonExtension } from '../api/types';
 import { IDisposableRegistry, IInstaller, InstallerResponse, Product, ProductInstallStatus } from '../common/types';
 import { IInterpreterService } from '../interpreter/contracts';
 import { IServiceContainer } from '../ioc/types';
 import { traceError, traceInfo, traceLog } from '../logging';
-import { PythonRuntimeManager } from './manager';
-import { createPythonRuntimeMetadata } from './runtime';
 import { IPYKERNEL_VERSION, MINIMUM_PYTHON_VERSION, Commands } from '../common/constants';
 import { InstallOptions } from '../common/installer/types';
 import { EnvironmentType } from '../pythonEnvironments/info';
-import { showErrorMessage } from '../common/vscodeApis/windowApis';
 import { isProblematicCondaEnvironment } from '../interpreter/configuration/environmentTypeComparer';
-import { CreateEnv, Interpreters } from '../common/utils/localize';
+import { Interpreters } from '../common/utils/localize';
 import { IApplicationShell } from '../common/application/types';
 
-export async function activatePositron(
-    activatedPromise: Promise<void>,
-    pythonApi: PythonExtension,
-    serviceContainer: IServiceContainer,
-): Promise<void> {
+export async function activatePositron(serviceContainer: IServiceContainer): Promise<void> {
     try {
-        traceInfo('activatePositron: creating runtime manager');
-        const manager = new PythonRuntimeManager(serviceContainer, pythonApi, activatedPromise);
-
-        // Register the Python runtime discoverer (to find all available runtimes) with positron.
-        traceInfo('activatePositron: registering python runtime manager');
-        positron.runtime.registerLanguageRuntimeManager(manager);
-
-        // Wait for all extension components to be activated before registering event listeners
-        traceInfo('activatePositron: awaiting extension activation');
-        await activatedPromise;
-
-        const registerRuntime = async (interpreterPath: string) => {
-            if (!manager.registeredPythonRuntimes.has(interpreterPath)) {
-                // Get the interpreter corresponding to the new runtime.
-                const interpreterService = serviceContainer.get<IInterpreterService>(IInterpreterService);
-                const interpreter = await interpreterService.getInterpreterDetails(interpreterPath);
-                // Create the runtime and register it with Positron.
-                if (interpreter) {
-                    // Set recommendedForWorkspace to false, since we change the active runtime
-                    // in the onDidChangeActiveEnvironmentPath listener.
-                    const runtime = await createPythonRuntimeMetadata(interpreter, serviceContainer, false);
-                    // Register the runtime with Positron.
-                    manager.registerLanguageRuntime(runtime);
-                } else {
-                    traceError(`Could not register runtime due to an invalid interpreter path: ${interpreterPath}`);
-                }
-            }
-        };
-        // If the interpreter is changed via the Python extension, select the corresponding
-        // language runtime in Positron.
         const disposables = serviceContainer.get<IDisposableRegistry>(IDisposableRegistry);
-        disposables.push(
-            pythonApi.environments.onDidChangeActiveEnvironmentPath(async (event) => {
-                // Select the new runtime.
-                await registerRuntime(event.path);
-                const runtimeMetadata = manager.registeredPythonRuntimes.get(event.path);
-                if (runtimeMetadata) {
-                    positron.runtime.selectLanguageRuntime(runtimeMetadata.runtimeId);
-                } else {
-                    traceError(`Tried to switch to a language runtime that has not been registered: ${event.path}`);
-                }
-            }),
-        );
-        // If a new runtime is registered via the Python extension, create and register a corresponding language runtime.
-        disposables.push(
-            pythonApi.environments.onDidChangeEnvironments(async (event) => {
-                if (event.type === 'add') {
-                    const interpreterPath = event.env.path;
-                    await checkAndInstallPython(interpreterPath, serviceContainer);
-                    if (!fs.existsSync(interpreterPath)) {
-                        showErrorMessage(`${CreateEnv.pathDoesntExist} ${interpreterPath}`);
-                    }
-                    await registerRuntime(interpreterPath);
-                }
-            }),
-        );
         // Register a command to check if ipykernel is installed for a given interpreter.
         disposables.push(
             vscode.commands.registerCommand('python.isIpykernelInstalled', async (pythonPath: string) => {
