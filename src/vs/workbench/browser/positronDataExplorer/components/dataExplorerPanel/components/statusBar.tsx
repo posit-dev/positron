b5833dbe5655098382c7ed6440133c6be0c09a75
Brian Lambert <brianlambert@gmail.com>
2024-05-23 08:59:41 -0500

    Data Explorer: Debounce StatusBarActivityIndicator component (#3245)
    * Debounce StatusBarActivityIndicator component
    
    * Update src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/statusBar.tsx

--- src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/statusBar.tsx
+++ src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/statusBar.tsx
@@ -10,50 +10,9 @@ import * as React from 'react';
 import { useEffect, useState } from 'react'; // eslint-disable-line no-duplicate-imports
 
 // Other dependencies.
-import { localize } from 'vs/nls';
 import { DisposableStore } from 'vs/base/common/lifecycle';
 import { usePositronDataExplorerContext } from 'vs/workbench/browser/positronDataExplorer/positronDataExplorerContext';
-import { DataExplorerClientStatus } from 'vs/workbench/services/languageRuntime/common/languageRuntimeDataExplorerClient';
-
-interface ActivityIndicatorProps {
-	status: DataExplorerClientStatus;
-}
-
-const StatusBarActivityIndicator = (props: ActivityIndicatorProps) => {
-	const getStatusText = () => {
-		switch (props.status) {
-			case DataExplorerClientStatus.Idle:
-				return localize('positron.dataExplorer.idle', 'Idle');
-			case DataExplorerClientStatus.Computing:
-				return localize('positron.dataExplorer.computing', 'Computing');
-			case DataExplorerClientStatus.Disconnected:
-				return localize('positron.dataExplorer.disconnected', 'Disconnected');
-			case DataExplorerClientStatus.Error:
-				return localize('positron.dataExplorer.error', 'Error');
-		}
-	};
-
-	const getStatusClass = () => {
-		switch (props.status) {
-			case DataExplorerClientStatus.Idle:
-				return 'idle';
-			case DataExplorerClientStatus.Computing:
-				return 'computing';
-			case DataExplorerClientStatus.Disconnected:
-				return 'disconnected';
-			case DataExplorerClientStatus.Error:
-				return 'error';
-		}
-	};
-
-	return (
-		<div className='status-bar-indicator'>
-			<div className={`icon ${getStatusClass()}`}
-				title={getStatusText()}
-				aria-label={getStatusText()}></div>
-		</div>
-	);
-};
+import { StatusBarActivityIndicator } from 'vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/statusBarActivityIndicator';
 
 /**
  * StatusBar component.
@@ -65,10 +24,8 @@ export const StatusBar = () => {
 
 	// State hooks.
 	const [backendState, setBackendState] = useState(
-		context.instance.dataExplorerClientInstance.cachedBackendState);
-
-	const [clientStatus, setClientStatus] = useState(
-		context.instance.dataExplorerClientInstance.status);
+		context.instance.dataExplorerClientInstance.cachedBackendState
+	);
 
 	// Main useEffect.
 	useEffect(() => {
@@ -80,32 +37,29 @@ export const StatusBar = () => {
 			state => setBackendState(state)
 		));
 
-		// Set up onDidStatusUpdate event handler.
-		disposableStore.add(context.instance.dataExplorerClientInstance.onDidStatusUpdate(
-			status => setClientStatus(status)
-		));
-
 		// Return the cleanup function that will dispose of the event handlers.
 		return () => disposableStore.dispose();
 	}, [context.instance.dataExplorerClientInstance]);
 
-	// Render.
-	let numRows = 0;
-	let numColumns = 0;
-	if (backendState !== undefined) {
+	// Set the number of rows and number of columns.
+	let numRows;
+	let numColumns;
+	if (!backendState) {
+		numRows = 0;
+		numColumns = 0;
+	} else {
 		numRows = backendState.table_shape.num_rows;
 		numColumns = backendState?.table_shape.num_columns;
 	}
 
+	// Render.
 	if (backendState && backendState.row_filters.length > 0) {
 		const numUnfilteredRows = backendState.table_unfiltered_shape.num_rows;
 		const pctFiltered = numUnfilteredRows === 0 ? 0 : 100 * numRows / numUnfilteredRows;
 
 		return (
 			<div className='status-bar'>
-				<StatusBarActivityIndicator
-					status={clientStatus}
-				/>
+				<StatusBarActivityIndicator />
 				<span className='label'>Showing</span>
 				<span>&nbsp;</span>
 				<span className='counter'>{numRows.toLocaleString()}</span>
@@ -124,9 +78,7 @@ export const StatusBar = () => {
 	} else {
 		return (
 			<div className='status-bar'>
-				<StatusBarActivityIndicator
-					status={clientStatus}
-				/>
+				<StatusBarActivityIndicator />
 				<span className='counter'>{numRows.toLocaleString()}</span>
 				<span>&nbsp;</span>
 				<span className='label'>rows</span>
