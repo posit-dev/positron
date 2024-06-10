bc7fa91c5daa37daa533c723a3c4e27069bc06d8
Nick Strayer <nick.strayer@posit.co>
2024-06-10 09:43:31 -0500

    Add validation of basic file paths for appropriate text inputs (#3422)
    * Add validation of basic file paths for appropriate text inputs
    
    * Wrap uri parsing in try-catch in attempt to catch invalid path character errors.

--- src/vs/workbench/browser/positronModalDialogs/newFolderModalDialog.tsx
+++ src/vs/workbench/browser/positronModalDialogs/newFolderModalDialog.tsx
@@ -25,6 +25,7 @@ import { PositronModalReactRenderer } from 'vs/workbench/browser/positronModalRe
 import { LabeledTextInput } from 'vs/workbench/browser/positronComponents/positronModalDialog/components/labeledTextInput';
 import { OKCancelModalDialog } from 'vs/workbench/browser/positronComponents/positronModalDialog/positronOKCancelModalDialog';
 import { LabeledFolderInput } from 'vs/workbench/browser/positronComponents/positronModalDialog/components/labeledFolderInput';
+import { checkIfPathValid } from 'vs/workbench/browser/positronComponents/positronModalDialog/components/fileInputValidators';
 
 /**
  * Shows the new folder modal dialog.
@@ -151,6 +152,7 @@ const NewFolderModalDialog = (props: NewFolderModalDialogProps) => {
 					autoFocus
 					value={result.folder}
 					onChange={e => setResult({ ...result, folder: e.target.value })}
+					validator={checkIfPathValid}
 				/>
 				<LabeledFolderInput
 					label={(() => localize(
