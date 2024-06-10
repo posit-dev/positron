e6d6d8a92ebbae20615d51bd819cb90334e8217f
Nick Strayer <nick.strayer@posit.co>
2024-06-10 09:43:31 -0500

    Add validation of basic file paths for appropriate text inputs (#3422)
    * Add validation of basic file paths for appropriate text inputs
    
    * Wrap uri parsing in try-catch in attempt to catch invalid path character errors.

--- src/vs/workbench/browser/positronComponents/positronModalDialog/components/labeledTextInput.tsx
+++ src/vs/workbench/browser/positronComponents/positronModalDialog/components/labeledTextInput.tsx
@@ -21,6 +21,7 @@ export interface LabeledTextInputProps {
 	min?: number;
 	type?: 'text' | 'number';
 	error?: boolean;
+	validator?: (value: string | number) => string | undefined;
 	onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
@@ -28,6 +29,9 @@ export interface LabeledTextInputProps {
  * LabeledTextInput component.
  */
 export const LabeledTextInput = forwardRef<HTMLInputElement, LabeledTextInputProps>((props, ref) => {
+
+	const errorMsg = useDebouncedValidator(props);
+
 	// Render.
 	return (
 		<div className='labeled-text-input'>
@@ -35,6 +39,7 @@ export const LabeledTextInput = forwardRef<HTMLInputElement, LabeledTextInputPro
 				{props.label}
 				<input className={positronClassNames('text-input', { 'error': props.error })} ref={ref} type={props.type} value={props.value}
 					autoFocus={props.autoFocus} onChange={props.onChange} max={props.max} min={props.min} />
+				{errorMsg ? <span className='error error-msg'>{errorMsg}</span> : null}
 			</label>
 		</div>
 	);
@@ -45,3 +50,35 @@ LabeledTextInput.displayName = 'LabeledTextInput';
 LabeledTextInput.defaultProps = {
 	type: 'text'
 };
+
+
+/**
+ * A hook to debounce the validation of input values.
+*
+*/
+const DEBOUNCE_DELAY = 100;
+function useDebouncedValidator({ validator, value }: Pick<LabeledTextInputProps, 'validator' | 'value'>) {
+	const [errorMsg, setErrorMsg] = React.useState<string | undefined>(undefined);
+
+	const callbackTimeoutRef = React.useRef<NodeJS.Timeout | undefined>();
+
+	const clearCallbackTimeout = React.useCallback(() => {
+		if (!callbackTimeoutRef.current) { return; }
+		clearTimeout(callbackTimeoutRef.current);
+	}, []);
+
+	React.useEffect(() => {
+		if (!validator) { return; }
+
+		clearCallbackTimeout();
+
+		callbackTimeoutRef.current = setTimeout(() => {
+			setErrorMsg(validator(value));
+		}, DEBOUNCE_DELAY);
+
+		return clearCallbackTimeout;
+	}, [clearCallbackTimeout, validator, value]);
+
+	return errorMsg;
+}
+
