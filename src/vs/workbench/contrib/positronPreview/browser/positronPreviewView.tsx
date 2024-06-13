fcfcde3e969993ec7f14b5497dd7f383913bf6ec
Nick Strayer <nick.strayer@posit.co>
2024-06-13 15:08:51 -0400

    Bigger initial popup size for help (#3534)
    * Allow for percentage based minimum size for window and require size to be provided to enable the collapse-to-zero functionality.

--- src/vs/workbench/contrib/positronPreview/browser/positronPreviewView.tsx
+++ src/vs/workbench/contrib/positronPreview/browser/positronPreviewView.tsx
@@ -88,7 +88,7 @@ export class PositronPreviewViewPane extends PositronViewPane implements IReactC
 		@IPositronPreviewService private readonly positronPreviewService: IPositronPreviewService,
 		@IHoverService hoverService: IHoverService,
 	) {
-		super({ ...options, allowZeroMinimumBodySize: true }, keybindingService, contextMenuService, configurationService, contextKeyService, viewDescriptorService, instantiationService, openerService, themeService, telemetryService, hoverService);
+		super({ ...options, openFromCollapsedSize: '50%' }, keybindingService, contextMenuService, configurationService, contextKeyService, viewDescriptorService, instantiationService, openerService, themeService, telemetryService, hoverService);
 
 		this._register(this.onDidChangeBodyVisibility(() => this.onDidChangeVisibility(this.isBodyVisible())));
 		this._positronPreviewContainer = DOM.$('.positron-preview-container');
