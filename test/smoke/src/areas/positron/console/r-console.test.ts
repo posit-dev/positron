03e072d3caab4cc2947ebc645ac1aa676d2a3c9b
Jon Vanausdeln <jonv@live.com>
2024-06-18 17:12:11 -0700

    Added test retry

--- test/smoke/src/areas/positron/console/r-console.test.ts
+++ test/smoke/src/areas/positron/console/r-console.test.ts
@@ -13,7 +13,7 @@ export function setup(logger: Logger) {
 
 		describe('R Console Restart', () => {
 
-			before(async function () {
+			beforeEach(async function () {
 
 				const app = this.app as Application;
 				const RFixtures = new PositronRFixtures(app);
@@ -23,19 +23,21 @@ export function setup(logger: Logger) {
 
 			it('Verify restart button inside the console', async function () {
 				// TestRail #377917
+				this.retries(1);
 				const app = this.app as Application;
 				// Need to make console bigger to see all bar buttons
 				await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
 				await app.workbench.positronConsole.barClearButton.click();
 				await app.workbench.positronConsole.barPowerButton.click();
 				await app.workbench.positronConsole.consoleRestartButton.click();
-				await app.workbench.positronConsole.waitForReady('>');
+				await app.workbench.positronConsole.waitForReady('<');
 				await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('restarted')));
 				await app.workbench.positronConsole.consoleRestartButton.isNotVisible();
 			});
 
 			it('Verify restart button on console bar', async function () {
 				// TestRail #620636
+				this.retries(1);
 				const app = this.app as Application;
 				// Need to make console bigger to see all bar buttons
 				await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
