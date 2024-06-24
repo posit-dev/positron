bb70cd3a687a9c167bb838e56d188e2b8eda449e
sharon <sharon-wang@users.noreply.github.com>
2024-06-24 22:18:08 +0200

    Run detect-secrets in precommit hook (#3618)
    <!-- Thank you for submitting a pull request.
    Please ensure that the code is up-to-date with the `main` branch.
    -->
    
    ### Intent
    
    - Addresses: https://github.com/posit-dev/positron/issues/3413
    
    ### Approach
    
    - port the run-detect-secrets shell script to js
    - add secret detection to the hygiene and precommit workflows
    - update the secrets README with new paths
    - specify exit codes so the hygiene task can fail with helpful
    information
    
    ### QA Notes
    
    I have tested on Mac. It'd be great to have others verify this on their
    Linux and Windows dev machines!
    
    #### Test Cases
    
    ##### Running the script directly `node build/detect-secrets.js`
    - running the script from a location other than the root directory of
    the positron repo results in an error message
    - `node ./build/detect-secrets.js init-baseline`
        - if no existing baseline file, a .secrets.baseline file is created
    - if there is an existing baseline file, user is prompted to accept or
    deny overwriting the existing baseline file
            - if accept, secrets are scanned in the entire repo
            - if deny, the script exists
    - `node ./build/detect-secrets.js audit-baseline`
    - if no existing baseline file, a message is printed and the script
    exits
    - if there is an existing baseline file, user is prompted to mark any
    unmarked false positives
    - `node ./build/detect-secrets.js update-baseline`
    - if no existing baseline file, a message is printed and the script
    exits
    - if there is an existing baseline file, the entire repo is scanned to
    update the baseline file with any new findings or line number changes
    - `node ./build/detect-secrets.js generate-report`
    - if no existing baseline file, a message is printed and the script
    exits
    - if there is an existing baseline file, a JSON report is generated from
    that baseline file
    - `node ./build/detect-secrets.js run-hook`
        - if no staged files, the hook exits
        - if staged files
            - if staged files contain secrets, fail the script
            - if staged files don't contain secrets, script is successful
    
    ##### Running the script via `yarn hygiene`/`yarn gulp hygiene`/`npm run
    hygiene` or `yarn precommit`/`npm run precommit` (from the root of the
    repo)
    - should error if any secrets were found or if any other error occurred
    - should pass if no secrets or errors
    
    <details><summary>Example secrets for testing secret scanning</summary>
    
    All of the secrets below are randomly generated strings that match the
    expected string format for each secret type.
    
    #### Github secrets
    https://github.blog/2021-04-05-behind-githubs-new-authentication-token-formats
    GITHUB_PERSONAL=ghp_65CCDtH2W4OzBcxpUSr31wgbzR73sba1IIxA
    GITHUB_OAUTH=gho_AuVpltgWc8g1gi6AZpuSvLiWBH7FDSNZtPZl
    GITHUB_USER_TO_SERVER=ghu_TNnUL0oB4edIzcnV5tZDMIFclzxmGATTKgHY
    GITHUB_SERVER_TO_USER=ghs_i1kVrdswVFCGSSDvsPZp3C85nhQzifqsTqAS
    GITHUB_REFRESH=ghr_wXjrwkALbBMusNhbrN8Kfx8PiW8a6GevVlkA
    
    #### AWS secrets
    https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html
    AWS_ACCESS_KEY_ID=AKIA4GD12P9SQQZW8CIN
    AWS_ACCESS_KEY_SECRET=BKucTwucNPxCa/I1WDGRG/73aJOHWfQE94HEMZNV
    - Note that the `AWS_ACCESS_KEY_SECRET` will only be flagged as an AWS
    secret if network verification is enabled and the secret is the matching
    secret for a valid access key id `AWS_ACCESS_KEY_ID`. As such, this
    secret will not be flagged.
    
    </details>

---
+++ build/detect-secrets-hook.js
@@ -0,0 +1,41 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+const es = require('event-stream');
+const vfs = require('vinyl-fs');
+const child_process = require('child_process');
+
+module.exports = detectSecretsHook;
+
+function detectSecretsHook(reporter) {
+	try {
+		const result = child_process.execSync('node build/detect-secrets.js run-hook', { encoding: 'utf8' });
+	} catch (error) {
+		let message = '';
+		// See ExitCodes in build/detect-secrets.js
+		switch (error.status) {
+			case 1:
+				message = 'detect-secrets found secrets in the staged files or there was an issue with the .secrets.baseline file';
+				break;
+			case 2:
+				message = 'detect-secrets wrapper script encountered an error while running the hook';
+				break;
+			default:
+				message = 'detect-secrets encountered an error while running the hook';
+				break;
+		}
+		reporter(message, true);
+		throw new Error(message);
+	}
+	return es.through(function () { /* noop, important for the stream to end */ });
+}
+
+// We'll need this if we add "detect-secrets-hook": "node build/detect-secrets-hook" to package.json
+// if (require.main === module) {
+// 	detectSecretsHook().on('error', (err) => {
+// 		console.error();
+// 		console.error(err);
+// 		process.exit(1);
+// 	});
+// }
