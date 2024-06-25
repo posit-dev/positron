ac15b7f8b8ccf6af678f38982cc3949a5f3ac0fb
Nick Strayer <nick.strayer@rstudio.com>
2024-06-25 12:04:45 -0400

    Add new smoke test for collapsing and opening help panel

--- test/automation/src/positron/positronHelp.ts
+++ test/automation/src/positron/positronHelp.ts
@@ -3,12 +3,14 @@
  *--------------------------------------------------------------------------------------------*/
 
 
-import { FrameLocator } from '@playwright/test';
+import { FrameLocator, Locator } from '@playwright/test';
 import { Code } from '../code';
 
 const OUTER_FRAME = '.webview';
 const MIDDLE_FRAME = '#active-frame';
 const INNER_FRAME = '#help-iframe';
+const HELP_CONTAINER = '.positron-help-container';
+const RESIZE_SASH = '.monaco-sash.mac.horizontal:not(.disabled)';
 
 export class PositronHelp {
 
@@ -19,5 +21,29 @@ export class PositronHelp {
 		const innerFrame = outerFrame.frameLocator(MIDDLE_FRAME);
 		const innerInnerFrame = innerFrame.frameLocator(INNER_FRAME);
 		return innerInnerFrame;
+	}
+
+	getHelpContainer(): Locator {
+		return this.code.driver.getAuxilaryBar().locator(HELP_CONTAINER);
+	}
+
+	getHelpHeader(): Locator {
+		return this.code.driver.getAuxilaryBar().getByRole('button', { name: 'Help Section' });
+	}
+
+	async resizeHelpPanel(delta: { x?: number; y?: number }): Promise<void> {
+		const sashLocator = this.code.driver.getAuxilaryBar().locator(RESIZE_SASH);
+		const sashLocation = await sashLocator.boundingBox();
+		if (!sashLocation) {
+			throw new Error('Could not find sash');
+		}
+
+		const middleOfSash = { x: sashLocation.x + sashLocation.width / 2, y: sashLocation.y + sashLocation.height / 2 };
+
+		// Select the sash to resize
+		await this.code.driver.clickAndDrag({
+			from: middleOfSash,
+			delta
+		});
 	}
 }
--- test/smoke/src/areas/positron/help/help.test.ts
+++ test/smoke/src/areas/positron/help/help.test.ts
@@ -63,5 +63,65 @@ export function setup(logger: Logger) {
 
 			});
 		});
+
+		describe('Collapse behavior', () => {
+
+			it.only('Verifies help panel can be opened when empty and also can be resized smaller and remember resize height', async function () {
+
+				const app = this.app as Application;
+				const positronHelp = app.workbench.positronHelp;
+				// How close should our heights be? It's not totally clear why this isn't always
+				// exact, but it's likely due to rounding errors or other factors. We'll allow
+				// a small margin of error.
+				const sizePrecision = 5;
+				await expect(async () => {
+					// Enter layout with help pane docked in session panel
+					await app.workbench.quickaccess.runCommand('workbench.action.positronHelpPaneDocked');
+
+					const helpContainerLocator = positronHelp.getHelpContainer();
+					const helpPanelHeaderLocator = positronHelp.getHelpHeader();
+
+					// Help panel starts collapsed thanks to the above command
+					await expect(helpContainerLocator).not.toBeVisible();
+
+					// Clicking the header opens it
+					await helpPanelHeaderLocator.click();
+					// Wait to allow the open animation to complete
+					await app.code.wait(1000);
+
+					await expect(helpContainerLocator).toBeVisible();
+					// Get the height of the help panel
+					const helpPanelHeight = (await helpContainerLocator.boundingBox())?.height ?? -1;
+					// Make sure that an empty help panel actually expands to a visible size.
+					expect(helpPanelHeight).toBeGreaterThan(100);
+
+					// Now resize the help panel smaller than the pop-open size and make sure that
+					// when we collapse and reopen it doesn't pop back to the full size again.
+
+					// We'll make it roughly two thirds the size of the original height
+					const resize_delta = helpPanelHeight / 3;
+					await positronHelp.resizeHelpPanel({ y: resize_delta });
+
+					// Verify that the height has changed by the expected amount
+					const helpPanelHeightAfter = (await helpContainerLocator.boundingBox())?.height ?? -1;
+					expect(helpPanelHeight - helpPanelHeightAfter - resize_delta).toBeLessThan(sizePrecision);
+
+					// Now collapse the panel again
+					await helpPanelHeaderLocator.click();
+					await expect(helpContainerLocator).not.toBeVisible();
+
+					// Reopen the panel
+					await helpPanelHeaderLocator.click();
+					await app.code.wait(1000);
+
+					// Make sure that the panel is smaller than it was before after opening up.
+					// Should be roughly the same size it was before we collapsed it. Allow for
+					// small deviations due to rounding errors etc..
+					const helpPanelHeightAfterReopen = (await helpContainerLocator.boundingBox())?.height ?? -1;
+					expect(Math.abs(helpPanelHeightAfterReopen - helpPanelHeightAfter)).toBeLessThan(sizePrecision);
+				}).toPass();
+
+			});
+		});
 	});
 }
