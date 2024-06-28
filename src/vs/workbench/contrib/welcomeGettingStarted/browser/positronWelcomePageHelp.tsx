a57cacb53b336524bd184e1ec1e357eabc5a3e90
Cameron Pfiffer <cameron@pfiffer.org>
2024-06-28 15:13:09 -0700

    revert formatting

--- src/vs/workbench/contrib/welcomeGettingStarted/browser/positronWelcomePageHelp.tsx
+++ src/vs/workbench/contrib/welcomeGettingStarted/browser/positronWelcomePageHelp.tsx
@@ -4,47 +4,32 @@
  *--------------------------------------------------------------------------------------------*/
 
 // CSS.
-import "vs/css!./media/positronGettingStarted";
+import 'vs/css!./media/positronGettingStarted';
 
 // React.
-import * as React from "react";
-import { PropsWithChildren } from "react"; // eslint-disable-line no-duplicate-imports
+import * as React from 'react';
+import { PropsWithChildren } from 'react'; // eslint-disable-line no-duplicate-imports
 
-import { ExternalLink } from "vs/base/browser/ui/ExternalLink/ExternalLink";
-import { IOpenerService } from "vs/platform/opener/common/opener";
-import { localize } from "vs/nls";
+import { ExternalLink } from 'vs/base/browser/ui/ExternalLink/ExternalLink';
+import { IOpenerService } from 'vs/platform/opener/common/opener';
+import { localize } from 'vs/nls';
 
 export interface PositronWelcomePageHelpProps {
 	openerService: IOpenerService;
 }
 
-export const PositronWelcomePageHelp = (
-	props: PropsWithChildren<PositronWelcomePageHelpProps>
-) => {
+export const PositronWelcomePageHelp = (props: PropsWithChildren<PositronWelcomePageHelpProps>) => {
 	const buildLinks = () => {
 		return (
-			<div className="welcome-help-links">
+			<div className='welcome-help-links'>
 				<ExternalLink
-					href="https://github.com/posit-dev/positron/wiki"
+					href='https://github.com/posit-dev/positron-beta/wiki'
 					openerService={props.openerService}
 				>
-					{localize(
-						"positron.welcome.positronDocumentation",
-						"Positron Documentation"
-					)}
+					{localize('positron.welcome.positronDocumentation', "Positron Documentation")}
 				</ExternalLink>
-				<ExternalLink
-					href="https://github.com/posit-dev/positron/discussions"
-					openerService={props.openerService}
-				>
-					Positron Community
-				</ExternalLink>
-				<ExternalLink
-					href="https://github.com/posit-dev/positron/issues"
-					openerService={props.openerService}
-				>
-					Report a bug
-				</ExternalLink>
+				<ExternalLink href='https://github.com/posit-dev/positron/discussions' openerService={props.openerService}>Positron Community</ExternalLink>
+				<ExternalLink href='https://github.com/posit-dev/positron/issues' openerService={props.openerService}>Report a bug</ExternalLink>
 			</div>
 		);
 	};
@@ -53,7 +38,7 @@ export const PositronWelcomePageHelp = (
 
 	// Render.
 	return (
-		<div className="positron-welcome-page-help welcome-page-section">
+		<div className='positron-welcome-page-help welcome-page-section'>
 			<h2>Help</h2>
 			{links}
 		</div>
