5d3146dfcb87adb110a1f97f36e1e1eb06144e5d
Jonathan McPherson <jonathan@rstudio.com>
2024-06-27 17:00:41 -0700

    use full version in download URL

--- extensions/open-remote-ssh/src/serverConfig.ts
+++ extensions/open-remote-ssh/src/serverConfig.ts
@@ -1,39 +1,42 @@
 import * as vscode from 'vscode';
+import * as positron from 'positron';
 import * as fs from 'fs';
 import * as path from 'path';
 
 let vscodeProductJson: any;
 async function getVSCodeProductJson() {
-    if (!vscodeProductJson) {
-        const productJsonStr = await fs.promises.readFile(path.join(vscode.env.appRoot, 'product.json'), 'utf8');
-        vscodeProductJson = JSON.parse(productJsonStr);
-    }
+	if (!vscodeProductJson) {
+		const productJsonStr = await fs.promises.readFile(path.join(vscode.env.appRoot, 'product.json'), 'utf8');
+		vscodeProductJson = JSON.parse(productJsonStr);
+	}
 
-    return vscodeProductJson;
+	return vscodeProductJson;
 }
 
 export interface IServerConfig {
-    version: string;
-    commit: string;
-    quality: string;
-    release?: string; // vscodium-like specific
-    serverApplicationName: string;
-    serverDataFolderName: string;
-    serverDownloadUrlTemplate?: string; // vscodium-like specific
+	version: string;
+	commit: string;
+	quality: string;
+	release?: string; // vscodium-like specific
+	serverApplicationName: string;
+	serverDataFolderName: string;
+	serverDownloadUrlTemplate?: string; // vscodium-like specific
 }
 
 export async function getVSCodeServerConfig(): Promise<IServerConfig> {
-    const productJson = await getVSCodeProductJson();
+	const productJson = await getVSCodeProductJson();
 
-    const customServerBinaryName = vscode.workspace.getConfiguration('remote.SSH.experimental').get<string>('serverBinaryName', '');
+	const customServerBinaryName = vscode.workspace.getConfiguration('remote.SSH.experimental').get<string>('serverBinaryName', '');
 
-    return {
-        version: vscode.version.replace('-insider',''),
-        commit: productJson.commit,
-        quality: productJson.quality,
-        release: productJson.release,
-        serverApplicationName: customServerBinaryName || productJson.serverApplicationName,
-        serverDataFolderName: productJson.serverDataFolderName,
-        serverDownloadUrlTemplate: productJson.serverDownloadUrlTemplate
-    };
+	const version = `${positron.version}-${positron.buildNumber}`;
+
+	return {
+		version,
+		commit: productJson.commit,
+		quality: productJson.quality,
+		release: productJson.release,
+		serverApplicationName: customServerBinaryName || productJson.serverApplicationName,
+		serverDataFolderName: productJson.serverDataFolderName,
+		serverDownloadUrlTemplate: productJson.serverDownloadUrlTemplate
+	};
 }
