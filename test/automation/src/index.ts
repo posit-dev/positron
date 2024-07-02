14dbb6dbeb6096891b27ef110d82828da04f831c
Chris Mead <chris.mead@posit.co>
2024-07-02 16:07:11 -0600

    changes to accomodate windows

--- test/automation/src/index.ts
+++ test/automation/src/index.ts
@@ -41,6 +41,5 @@ export * from './positron/positronNotebooks';
 export * from './positron/positronNewProjectWizard';
 export * from './positron/positronConnections';
 export * from './positron/positronHelp';
-export * from './positron/positronClipboardUtils';
 // --- End Positron ---
 export { getDevElectronPath, getBuildElectronPath, getBuildVersion } from './electron';
--- test/smoke/src/areas/positron/console/consoleClipboard.test.ts
+++ test/smoke/src/areas/positron/console/consoleClipboard.test.ts
@@ -4,10 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 
-import { Application, Logger, PositronPythonFixtures, PositronRFixtures, readClipboard } from '../../../../../automation';
+import { Application, Logger, PositronPythonFixtures, PositronRFixtures } from '../../../../../automation';
 import { installAllHandlers } from '../../../utils';
 import * as os from 'os';
-import { expect } from '@playwright/test';
 
 export function setup(logger: Logger) {
 	describe('Console', () => {
@@ -17,6 +16,34 @@ export function setup(logger: Logger) {
 		const isMac = os.platform() === 'darwin';
 		const modifier = isMac ? 'Meta' : 'Control';
 
+		async function testBody(app: Application) {
+				// TestRail 608100
+
+				const activeConsole = app.workbench.positronConsole.getActiveConsole();
+				await activeConsole?.click();
+
+				await app.workbench.positronConsole.typeToConsole('a = 1');
+
+				const page = activeConsole!.page();
+				await page.keyboard.press(`${modifier}+A`);
+				await page.keyboard.press(`${modifier}+C`);
+
+				await app.workbench.positronConsole.sendEnterKey();
+
+				await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('a = 1')) );
+
+				await app.workbench.positronConsole.barClearButton.click();
+
+				await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.length === 0 );
+
+				await page.keyboard.press(`${modifier}+V`);
+				await app.workbench.positronConsole.sendEnterKey();
+
+				await app.workbench.positronConsole.waitForConsoleContents((contents) =>
+					contents.some((line) => line.includes('a = 1'))
+				);
+		}
+
 		describe('Console Clipboard - Python', () => {
 			before(async function () {
 				const app = this.app as Application;
@@ -24,34 +51,8 @@ export function setup(logger: Logger) {
 				await pythonFixtures.startPythonInterpreter();
 			});
 
-			it('Python - Copy from console & paste to console', async function () {
-				// TestRail 608100
-				const app = this.app as Application;
-
-				logger.log('Sending code to console');
-				await app.workbench.positronConsole.executeCode('Python', 'a = b', '>>>');
-
-				await app.workbench.positronConsole.waitForConsoleContents((contents) =>
-					contents.some((line) => line.includes('NameError'))
-				);
-
-				const activeConsole = app.workbench.positronConsole.getActiveConsole();
-				await activeConsole?.click();
-
-				const page = activeConsole!.page();
-				await page.keyboard.press(`${modifier}+A`);
-				await page.keyboard.press(`${modifier}+C`);
-
-				const inClipboard = readClipboard();
-
-				expect(inClipboard).toContain('NameError');
-
-				await page.keyboard.press(`${modifier}+V`);
-				await app.workbench.positronConsole.sendEnterKey();
-
-				await app.workbench.positronConsole.waitForConsoleContents((contents) =>
-					contents.some((line) => line.includes('invalid syntax'))
-				);
+			it.only('Python - Copy from console & paste to console', async function () {
+				await testBody(this.app);
 			});
 		});
 
@@ -62,34 +63,8 @@ export function setup(logger: Logger) {
 				await pythonFixtures.startRInterpreter();
 			});
 
-			it('R - Copy from console & paste to console', async function () {
-				// TestRail 608100
-				const app = this.app as Application;
-
-				logger.log('Sending code to console');
-				await app.workbench.positronConsole.executeCode('R', 'a = b', '>');
-
-				await app.workbench.positronConsole.waitForConsoleContents((contents) =>
-					contents.some((line) => line.includes("object 'b' not found"))
-				);
-
-				const activeConsole = app.workbench.positronConsole.getActiveConsole();
-				await activeConsole?.click();
-
-				const page = activeConsole!.page();
-				await page.keyboard.press(`${modifier}+A`);
-				await page.keyboard.press(`${modifier}+C`);
-
-				const inClipboard = readClipboard();
-
-				expect(inClipboard).toContain("object 'b' not found");
-
-				await page.keyboard.press(`${modifier}+V`);
-				await app.workbench.positronConsole.sendEnterKey();
-
-				await app.workbench.positronConsole.waitForConsoleContents((contents) =>
-					contents.some((line) => line.includes('unexpected numeric constant in "R 4.3"'))
-				);
+			it.only('R - Copy from console & paste to console', async function () {
+				await testBody(this.app);
 			});
 		});
 	});
