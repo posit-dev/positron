posit-dev/positron

commit c2f48ab40ac228f876af469b3a5d324b6b760682

Add layout service and full-sized part layouts (#3616)
Addresses  #3586 and #3577

### Intent

- Add a service that manages dispatching the positron custom layouts for
us.
- Add a few testing-related layouts like `"Maximized Sidebar Layout"`
that can configure positron layouts for easier smoke-testing.

Right now it is simple but provides a foundation in the future for more
advanced behavior like queing updates when workbench isn't ready or
making tweaks to existing layouts etc..

### Approach
Took the logic for the previous layout work that was spread across
methods on two existing services (`IWorkbenchLayoutService` and
`IViewDescriptorService`) and merged the details for how those methods
are called into an encapsulating positron layout service
(`IPositronLayoutService`).

At the same time, this provided a nice home for all the layout logic to
live so the previous scripts were moved into the new folder at
`src/vs/workbench/services/positronLayout/`

Also added three more layouts (not hooked up to quick-pick ui) for
maximizing specific parts of the UI: `workbench.action.fullSizedPanel`,
`workbench.action.fullSizedAuxiliaryBar`, and
`workbench.action.fullSizedSidebar`. These can be used to let the user
focus on a specific part of the page or for making automated testing
easier. (ala #3577)

### QA Notes

Existing layouts should work exactly as before. New layouts such as
`workbench.action.fullSizedAuxiliaryBar` should do what's printed on the
tin. Note that it's not possible for them to fully take up the entire
ide width or height because there's a minimum size protection for the
editor window.

src/vs/workbench/services/positronLayout/browser/layouts/maximizedPartLayouts.ts
@@ -1,0 +1,52 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import { localize2 } from 'vs/nls';
+import { registerAction2 } from 'vs/platform/actions/common/actions';
+import { Parts } from 'vs/workbench/services/layout/browser/layoutService';
+import { PositronLayoutAction } from './layoutAction';
+import { KnownPositronLayoutParts, CustomPositronLayoutDescription } from 'vs/workbench/services/positronLayout/common/positronCustomViews';
+
+// Layouts that maximize a single part as much as possible.
+function makeMaximizedPartLayout(part: KnownPositronLayoutParts): CustomPositronLayoutDescription {
+	return {
+		[Parts.PANEL_PART]: { hidden: true },
+		[Parts.SIDEBAR_PART]: { hidden: true },
+		[Parts.AUXILIARYBAR_PART]: { hidden: true },
+		[part]: { hidden: false, size: '100%' },
+	};
+}
+
+registerAction2(class extends PositronLayoutAction {
+	constructor() {
+		super({
+			id: 'workbench.action.fullSizedSidebar',
+			label: localize2('chooseLayout.fullSizedSidebarLayout', 'Maximized Sidebar Layout'),
+			hideFromPalette: true,
+			layoutDescriptor: makeMaximizedPartLayout(Parts.SIDEBAR_PART),
+		});
+	}
+});
+
+registerAction2(class extends PositronLayoutAction {
+	constructor() {
+		super({
+			id: 'workbench.action.fullSizedPanel',
+			label: localize2('chooseLayout.fullSizedPanelLayout', 'Maximized Panel Layout'),
+			hideFromPalette: true,
+			layoutDescriptor: makeMaximizedPartLayout(Parts.PANEL_PART),
+		});
+	}
+});
+
+registerAction2(class extends PositronLayoutAction {
+	constructor() {
+		super({
+			id: 'workbench.action.fullSizedAuxiliaryBar',
+			label: localize2('chooseLayout.fullSizedAuxiliaryBarLayout', 'Maximized Auxiliary Bar Layout'),
+			hideFromPalette: true,
+			layoutDescriptor: makeMaximizedPartLayout(Parts.AUXILIARYBAR_PART),
+		});
+	}
+});

src/vs/workbench/browser/actions/layoutActions.ts
@@ -35,7 +35,7 @@ import { TitlebarStyle } from 'vs/platform/window/common/window';
 // --- Start Positron ---
 import { PositronToggleTopActionBarVisibilityAction } from 'vs/workbench/browser/parts/positronTopActionBar/positronTopActionBarActions';
 import { PositronTopActionBarVisibleContext } from 'vs/workbench/common/contextkeys';  // eslint-disable-line no-duplicate-imports
-import { positronCustomLayoutOptions } from 'vs/workbench/browser/positronCustomViews';
+import { positronCustomLayoutOptions } from 'vs/workbench/services/positronLayout/browser/positronCustomViews';
 // --- End Positron ---
 
 // Register Icons

src/vs/workbench/browser/layout.ts
@@ -52,9 +52,9 @@ import { CustomTitleBarVisibility } from '../../platform/window/common/window';
 
 // --- Start Positron ---
 import { IPositronTopActionBarService } from 'vs/workbench/services/positronTopActionBar/browser/positronTopActionBarService';
-import { PartViewInfo } from 'vs/workbench/browser/positronCustomViews';
+import { PartViewInfo } from 'vs/workbench/services/positronLayout/browser/interfaces/positronLayoutService';
 import { AbstractPaneCompositePart } from 'vs/workbench/browser/parts/paneCompositePart';
-import { CustomPositronLayoutDescription, KnownPositronLayoutParts, PartLayoutDescription } from 'vs/workbench/common/positronCustomViews';
+import { CustomPositronLayoutDescription, KnownPositronLayoutParts, PartLayoutDescription } from 'vs/workbench/services/positronLayout/common/positronCustomViews';
 import { clamp } from 'vs/base/common/numbers';
 // --- End Positron ---
 

src/vs/workbench/browser/positronCustomViews.ts
@@ -1,265 +1,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
- *--------------------------------------------------------------------------------------------*/
-
-
-import { ISerializableView, IViewSize } from 'vs/base/browser/ui/grid/gridview';
-import { ILocalizedString, localize2 } from 'vs/nls';
-import { Categories } from 'vs/platform/action/common/actionCommonCategories';
-import { Action2, registerAction2 } from 'vs/platform/actions/common/actions';
-import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
-import { IQuickPickItem } from 'vs/platform/quickinput/common/quickInput';
-import { CustomPositronLayoutDescription, KnownPositronLayoutParts } from 'vs/workbench/common/positronCustomViews';
-import { IViewDescriptorService, ViewContainerLocation } from 'vs/workbench/common/views';
-import { IWorkbenchLayoutService, PanelAlignment, Parts } from 'vs/workbench/services/layout/browser/layoutService';
-
-
-export type PartViewInfo = {
-	partView: ISerializableView;
-	currentSize: IViewSize;
-	alignment?: PanelAlignment;
-	hidden: boolean;
-	hideFn: (hidden: boolean, skipLayout?: boolean | undefined) => void;
-};
-
-const partToViewContainerLocation: Record<KnownPositronLayoutParts, ViewContainerLocation> = {
-	[Parts.PANEL_PART]: ViewContainerLocation.Panel,
-	[Parts.SIDEBAR_PART]: ViewContainerLocation.Sidebar,
-	[Parts.AUXILIARYBAR_PART]: ViewContainerLocation.AuxiliaryBar,
-};
-
-/**
- * Convert our custom layout description to the `IViewsCustomizations` format that the
- * `viewDescriptorService` uses for its internal state.
- * @param layout Positron custom layout description
- * @returns Simplified view info in the form of viewContainerLocations and
- * viewDescriptorCustomizations. See `IViewsCustomizations` for more info.
- */
-export function layoutDescriptionToViewInfo(layout: CustomPositronLayoutDescription) {
-	const viewContainerLocations = new Map<string, ViewContainerLocation>();
-	const viewDescriptorCustomizations = new Map<string, string>();
-
-	for (const [part, info] of Object.entries(layout)) {
-		const viewContainers = info.viewContainers;
-		if (!viewContainers) { continue; }
-		const viewContainerLocation = partToViewContainerLocation[part as KnownPositronLayoutParts];
-
-		for (const viewContainer of viewContainers) {
-			viewContainerLocations.set(viewContainer.id, viewContainerLocation);
-
-			if (!viewContainer.views) { continue; }
-			for (const view of viewContainer.views) {
-				viewDescriptorCustomizations.set(view.id, viewContainer.id);
-			}
-		}
-	}
-
-	return {
-		viewContainerLocations,
-		viewDescriptorCustomizations,
-	};
-}
-
-type PositronLayoutInfo = {
-	id: string;
-	codicon: string;
-	label: ILocalizedString;
-	layoutDescriptor: CustomPositronLayoutDescription;
-};
-
-export const positronFourPaneDsLayout: PositronLayoutInfo = {
-	id: 'workbench.action.positronFourPaneDataScienceLayout',
-	codicon: 'positron-four-pane-ds-layout',
-	label: localize2('choseLayout.stacked', 'Stacked Layout'),
-	layoutDescriptor: {
-		[Parts.SIDEBAR_PART]: {
-			size: '15%',
-			hidden: false,
-		},
-		[Parts.PANEL_PART]: {
-			size: '40%',
-			hidden: false,
-			alignment: 'center',
-			viewContainers: [
-				{
-					id: 'workbench.panel.positronConsole',
-					opened: true,
-				},
-				{
-					id: 'terminal',
-				},
-			]
-		},
-		[Parts.AUXILIARYBAR_PART]: {
-			size: '30%',
-			hidden: false,
-			viewContainers: [
-				{
-					id: 'workbench.panel.positronSession',
-					opened: true,
-					views: [
-						{
-							id: 'workbench.panel.positronVariables',
-						},
-						{
-							id: 'workbench.panel.positronPlots',
-						},
-					]
-				},
-			]
-		}
-	},
-};
-
-export const positronTwoPaneLayout: PositronLayoutInfo = {
-	id: 'workbench.action.positronTwoPaneDataScienceLayout',
-	codicon: 'positron-two-pane-ds-layout',
-	label: localize2('choseLayout.sideBySide', 'Side-By-Side Layout'),
-	layoutDescriptor: {
-		[Parts.PANEL_PART]: {
-			hidden: true,
-			alignment: 'center'
-		},
-		[Parts.SIDEBAR_PART]: {
-			hidden: true
-		},
-		[Parts.AUXILIARYBAR_PART]: {
-			hidden: false,
-			size: '40%',
-			viewContainers: [
-				{
-					id: 'workbench.panel.positronSession',
-					opened: true,
-					views: [
-						{
-							id: 'workbench.panel.positronConsole',
-						},
-						{
-							id: 'workbench.panel.positronVariables',
-							collapsed: true,
-						},
-						{
-							id: 'workbench.panel.positronPlots',
-							collapsed: true,
-						},
-					]
-				},
-				{
-					id: 'terminal',
-				}
-			]
-		},
-	},
-};
-
-export const positronNotebookLayout: PositronLayoutInfo = {
-	id: 'workbench.action.positronNotebookLayout',
-	codicon: 'positron-notebook-layout',
-	label: localize2('chooseLayout.notebookLayout', 'Notebook Layout'),
-	layoutDescriptor: {
-		[Parts.PANEL_PART]: {
-			size: '40%',
-			hidden: true,
-			alignment: 'center',
-			viewContainers: [
-				{
-					id: 'workbench.panel.positronConsole',
-					opened: true,
-				},
-				{
-					id: 'terminal',
-				},
-			]
-		},
-		[Parts.SIDEBAR_PART]: {
-			hidden: false,
-			size: '15%',
-			minSize: 180,
-			hideIfBelowMinSize: true,
-		},
-		[Parts.AUXILIARYBAR_PART]: {
-			hidden: true,
-			size: '20%',
-			minSize: 180,
-		},
-	},
-};
-
-
-abstract class PositronLayoutAction extends Action2 {
-	private _layout: CustomPositronLayoutDescription;
-	constructor(
-		layoutInfo: PositronLayoutInfo
-	) {
-		super({
-			id: layoutInfo.id,
-			title: layoutInfo.label,
-			category: Categories.View,
-			f1: true,
-		});
-
-		this._layout = layoutInfo.layoutDescriptor;
-	}
-	run(accessor: ServicesAccessor): void {
-		const layoutService = accessor.get(IWorkbenchLayoutService);
-		const viewDescriptorService = accessor.get(IViewDescriptorService);
-
-		viewDescriptorService.loadCustomViewDescriptor(this._layout);
-		// Run the layout service action after the view descriptor has been loaded.
-		// This is needed so that the changing of the contents of the parts doesn't
-		// break the currently open view container that is set by the layoutService.
-		layoutService.enterCustomLayout(this._layout);
-	}
-}
-registerAction2(class extends PositronLayoutAction {
-	constructor() {
-		super(positronNotebookLayout);
-	}
-});
-
-registerAction2(class extends PositronLayoutAction {
-	constructor() {
-		super(positronTwoPaneLayout);
-	}
-});
-
-registerAction2(class extends PositronLayoutAction {
-	constructor() {
-		super(positronFourPaneDsLayout);
-	}
-});
-
-
-// Action to dump json of the current layout to the console for creation of a custom layout.
-// registerAction2(class DumpViewCustomizations extends Action2 {
-
-// 	constructor() {
-// 		super({
-// 			id: 'workbench.action.dumpViewCustomizations',
-// 			title: localize2('dumpViewCustomizations', "Dump view customizations to console"),
-// 			category: Categories.View,
-// 			f1: true,
-// 		});
-// 	}
-
-// 	run(accessor: ServicesAccessor): void {
-// 		console.log(
-// 			JSON.stringify(createPositronCustomLayoutDescriptor(accessor), null, 2)
-// 		);
-// 	}
-// });
-
-type LayoutPick = IQuickPickItem & { layoutDescriptor: CustomPositronLayoutDescription };
-
-export const positronCustomLayoutOptions: LayoutPick[] = [
-	positronFourPaneDsLayout,
-	positronTwoPaneLayout,
-	positronNotebookLayout
-].map(function positronLayoutInfoToQuickPick(layoutInfo: PositronLayoutInfo): LayoutPick {
-	return {
-		id: layoutInfo.id,
-		label: `$(${layoutInfo.codicon}) ${layoutInfo.label.value}`,
-		layoutDescriptor: layoutInfo.layoutDescriptor,
-	};
-});
-

src/vs/workbench/common/views.ts
@@ -27,7 +27,7 @@ import { CancellationToken } from 'vs/base/common/cancellation';
 import { VSDataTransfer } from 'vs/base/common/dataTransfer';
 import { ILocalizedString } from 'vs/platform/action/common/action';
 // --- Start Positron ---
-import { CustomPositronLayoutDescription } from 'vs/workbench/common/positronCustomViews';
+import { CustomPositronLayoutDescription } from 'vs/workbench/services/positronLayout/common/positronCustomViews';
 // --- End Positron ---
 
 export const VIEWS_LOG_ID = 'views';

src/vs/workbench/services/layout/browser/layoutService.ts
@@ -16,8 +16,8 @@ import { isFullscreen, isWCOEnabled } from 'vs/base/browser/browser';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { IDisposable } from 'vs/base/common/lifecycle';
 // --- Start Positron ---
-import { PartViewInfo } from 'vs/workbench/browser/positronCustomViews';
-import { CustomPositronLayoutDescription, KnownPositronLayoutParts } from 'vs/workbench/common/positronCustomViews';
+import { PartViewInfo } from 'vs/workbench/services/positronLayout/browser/interfaces/positronLayoutService';
+import { CustomPositronLayoutDescription, KnownPositronLayoutParts } from 'vs/workbench/services/positronLayout/common/positronCustomViews';
 // --- End Positron ---
 
 export const IWorkbenchLayoutService = refineServiceDecorator<ILayoutService, IWorkbenchLayoutService>(ILayoutService);

src/vs/workbench/services/positronLayout/browser/interfaces/positronLayoutService.ts
@@ -1,0 +1,40 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import { CustomPositronLayoutDescription } from 'vs/workbench/services/positronLayout/common/positronCustomViews';
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+import { ISerializableView, IViewSize } from 'vs/base/browser/ui/grid/gridview';
+import { PanelAlignment } from 'vs/workbench/services/layout/browser/layoutService';
+
+export const IPositronLayoutService = createDecorator<IPositronLayoutService>('positronLayoutService');
+
+/**
+ * IPositronLayoutService interface.
+ */
+export interface IPositronLayoutService {
+	/**
+	 * Needed for service branding in dependency injector.
+	 */
+	readonly _serviceBrand: undefined;
+
+	/**
+	 * Placeholder that gets called to "initialize" the PositronLayoutService.
+	 */
+	initialize(): void;
+
+	/**
+	 * Set the layout for the editor.
+	 * @param layout Layout description to set.
+	 */
+	setLayout(layout: CustomPositronLayoutDescription): void;
+
+}
+
+export type PartViewInfo = {
+	partView: ISerializableView;
+	currentSize: IViewSize;
+	alignment?: PanelAlignment;
+	hidden: boolean;
+	hideFn: (hidden: boolean, skipLayout?: boolean | undefined) => void;
+};

src/vs/workbench/services/positronLayout/browser/layouts/layoutAction.ts
@@ -1,0 +1,44 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ILocalizedString } from 'vs/nls';
+import { Categories } from 'vs/platform/action/common/actionCommonCategories';
+import { Action2 } from 'vs/platform/actions/common/actions';
+import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
+import { IPositronLayoutService } from 'vs/workbench/services/positronLayout/browser/interfaces/positronLayoutService';
+import { CustomPositronLayoutDescription } from 'vs/workbench/services/positronLayout/common/positronCustomViews';
+
+
+export type PositronLayoutInfo = {
+	id: string;
+	codicon?: string;
+	label: ILocalizedString;
+	layoutDescriptor: CustomPositronLayoutDescription;
+	/**
+	 * If true, the layout will not be shown in the command pallete and will need to be called
+	 * programmatically.
+	 */
+	hideFromPalette?: boolean;
+};
+
+export abstract class PositronLayoutAction extends Action2 {
+	private _layout: CustomPositronLayoutDescription;
+	constructor(
+		layoutInfo: PositronLayoutInfo
+	) {
+		const showInPalette = layoutInfo.hideFromPalette ? false : true;
+		super({
+			id: layoutInfo.id,
+			title: layoutInfo.label,
+			category: Categories.View,
+			f1: showInPalette
+		});
+
+		this._layout = layoutInfo.layoutDescriptor;
+	}
+	run(accessor: ServicesAccessor): void {
+		const positronLayoutService = accessor.get(IPositronLayoutService);
+		positronLayoutService.setLayout(this._layout);
+	}
+}

commit a8469d11d6b4ce5d753da0576804e1fa00d8ce36

Make maximized layouts available to use in smoke tests (#3807)
Fixes issue where the maximized part layouts introduced in #3616, made
specifically for testing, were not available to be used in the tests due
to being hidden from the palette.

I was mistaken in thinking that the method
`app.workbench.quickaccess.runCommand()` used some other method of
running commands than the command palette. Hence, I hide the maximized
layouts to not clutter the user's palette.

This PR just sets the maximized layouts:
`workbench.action.fullSizedPanel`, `workbench.action.fullSizedSidebar`,
and `Maximized Auxiliary Bar Layout` to be available in the command
palette.

### QA Notes
Hopefully will make life easier for smoke test users.

src/vs/workbench/services/positronLayout/browser/layouts/maximizedPartLayouts.ts
@@ -24,7 +24,7 @@ registerAction2(class extends PositronLayoutAction {
 		super({
 			id: 'workbench.action.fullSizedSidebar',
 			label: localize2('chooseLayout.fullSizedSidebarLayout', 'Maximized Sidebar Layout'),
-			hideFromPalette: true,
+			hideFromPalette: false,
 			layoutDescriptor: makeMaximizedPartLayout(Parts.SIDEBAR_PART),
 		});
 	}
@@ -35,7 +35,7 @@ registerAction2(class extends PositronLayoutAction {
 		super({
 			id: 'workbench.action.fullSizedPanel',
 			label: localize2('chooseLayout.fullSizedPanelLayout', 'Maximized Panel Layout'),
-			hideFromPalette: true,
+			hideFromPalette: false,
 			layoutDescriptor: makeMaximizedPartLayout(Parts.PANEL_PART),
 		});
 	}
@@ -46,7 +46,7 @@ registerAction2(class extends PositronLayoutAction {
 		super({
 			id: 'workbench.action.fullSizedAuxiliaryBar',
 			label: localize2('chooseLayout.fullSizedAuxiliaryBarLayout', 'Maximized Auxiliary Bar Layout'),
-			hideFromPalette: true,
+			hideFromPalette: false,
 			layoutDescriptor: makeMaximizedPartLayout(Parts.AUXILIARYBAR_PART),
 		});
 	}