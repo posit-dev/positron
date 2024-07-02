cee7cfe39a94cd5304c3a77c129aa03a1c7ef52b
Daniel Falbel <dfalbel@gmail.com>
2024-07-02 14:41:09 -0300

    Remove it.only

--- test/smoke/src/areas/positron/connections/dbConnections.test.ts
+++ test/smoke/src/areas/positron/connections/dbConnections.test.ts
@@ -115,7 +115,7 @@ export function setup(logger: Logger) {
 				await app.workbench.positronConnections.reconnectButton.waitforVisible();
 			});
 
-			it.only('R - Connections are update after adding a database', async function () {
+			it('R - Connections are update after adding a database', async function () {
 
 				const app = this.app as Application;
 
