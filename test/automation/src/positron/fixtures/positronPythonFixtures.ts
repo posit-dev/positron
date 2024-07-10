fb3df872b18dea888154694cfa396cf9876363ef
sharon wang <sharon-wang@users.noreply.github.com>
2024-07-10 16:02:08 +0200

    add support for installing ipykernel if a dialog opens when selecting an interpreter

--- test/automation/src/positron/fixtures/positronPythonFixtures.ts
+++ test/automation/src/positron/fixtures/positronPythonFixtures.ts
@@ -14,13 +14,21 @@ export class PositronPythonFixtures {
 
 	constructor(private app: Application) { }
 
-	async startPythonInterpreter() {
+	async startPythonInterpreter(installIPyKernelIfPrompted: boolean = false) {
 
 		const desiredPython = process.env.POSITRON_PY_VER_SEL;
 		if (desiredPython === undefined) {
 			fail('Please be sure to set env var POSITRON_PY_VER_SEL to the UI text corresponding to the Python version for the test');
 		}
 		await this.app.workbench.positronConsole.selectInterpreter(InterpreterType.Python, desiredPython);
+
+		if (await this.app.workbench.positronPopups.popupCurrentlyOpen()) {
+			if (installIPyKernelIfPrompted) {
+				await this.app.workbench.positronPopups.installIPyKernel();
+			} else {
+				fail('The ipykernel popup was opened, but the test was not configured to install it.')
+			}
+		}
 
 		await this.app.workbench.positronConsole.waitForReady('>>>');
 
