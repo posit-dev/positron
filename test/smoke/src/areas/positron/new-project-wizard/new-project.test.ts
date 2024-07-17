d5adc18d0500d0b4981f9121efdcfd6bc0fc40fc
sharon wang <sharon-wang@users.noreply.github.com>
2024-07-17 18:42:37 +0200

    update renv install test comment

--- test/smoke/src/areas/positron/new-project-wizard/new-project.test.ts
+++ test/smoke/src/areas/positron/new-project-wizard/new-project.test.ts
@@ -218,19 +218,17 @@ export function setup(logger: Logger) {
 					// Interact with the modal to install renv
 					await app.workbench.positronPopups.installRenv();
 
-					// If the test is running on Windows, we may need to interact with the
-					// Console to allow the renv installation to complete. It doesn't always happen,
-					// so for now this code is commented out. We've seen it once, but not again:
-					// https://github.com/posit-dev/positron/pull/3881#issuecomment-2211123610.
-					// For some reason, we don't need to do this on Mac or Linux -- or at least we
-					// haven't seen it yet!
-					// if (process.platform === 'win32') {
-					// 	await app.workbench.positronConsole.waitForConsoleContents((contents) =>
-					// 		contents.some((line) => line.includes('Do you want to proceed?'))
-					// 	);
-					// 	await app.workbench.positronConsole.typeToConsole('y');
-					// 	await app.workbench.positronConsole.sendEnterKey();
-					// }
+					// If this test is running on a machine that is using Renv for the first time, we
+					// may need to interact with the Console to allow the renv installation to complete
+					// An example: https://github.com/posit-dev/positron/pull/3881#issuecomment-2211123610.
+
+					// You should either manually interact with the Console to proceed with the Renv
+					// install or temporarily uncomment the code below to automate the interaction.
+					// await app.workbench.positronConsole.waitForConsoleContents((contents) =>
+					// 	contents.some((line) => line.includes('Do you want to proceed?'))
+					// );
+					// await app.workbench.positronConsole.typeToConsole('y');
+					// await app.workbench.positronConsole.sendEnterKey();
 
 					// Verify renv files are present
 					expect(async () => {
