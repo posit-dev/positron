2c325ce376dd92178553859abe3af581662293ed
Christopher Mead <chrisrmead@comcast.net>
2024-07-18 07:32:19 -0600

    Notebook Variables Smoke Test (#4035)
    Very much like the existing variables test except for notebook sessions.
    Tests Python and R notebooks' variables.
    
    ### QA Notes
    
    All smoke tests should pass.

--- test/smoke/src/areas/positron/variables/notebookVariables.test.ts
+++ test/smoke/src/areas/positron/variables/notebookVariables.test.ts
@@ -3,103 +3,98 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
-
 import { expect } from '@playwright/test';
 import { Application, Logger, PositronPythonFixtures, PositronRFixtures } from '../../../../../automation';
 import { installAllHandlers } from '../../../utils';
 
 /*
- * Notebook creation test cases
+ * Variables Pane test cases
  */
 export function setup(logger: Logger) {
-
-	describe('Notebooks', () => {
+	describe('Variables Pane - Notebook', () => {
 
 		// Shared before/after handling
 		installAllHandlers(logger);
 
-		describe('Python Notebooks', () => {
+		describe('Python Notebook Variables Pane', () => {
 
 			before(async function () {
-
 				await PositronPythonFixtures.SetupFixtures(this.app as Application);
-
 			});
 
 			after(async function () {
 
 				const app = this.app as Application;
 				await app.workbench.positronNotebooks.closeNotebookWithoutSaving();
+
+				await app.workbench.positronLayouts.enterLayout('stacked');
 			});
 
-			it('Python - Basic notebook creation and execution (code) [C628631]', async function () {
+			it('Verifies Variables pane basic function for notebook with python interpreter [C669188]', async function () {
 				const app = this.app as Application;
 
 				await app.workbench.positronNotebooks.createNewNotebook();
 
 				await app.workbench.positronNotebooks.selectInterpreter('Python Environments', process.env.POSITRON_PY_VER_SEL!);
 
-				await app.workbench.positronNotebooks.executeInFirstCell('eval("8**2")');
-				expect(await app.workbench.positronNotebooks.getPythonCellOutput()).toBe('64');
+				await app.workbench.positronNotebooks.executeInFirstCell('y = [2, 3, 4, 5]');
+
+				const interpreter = await app.code.waitForElement('.positron-variables-container .action-bar-button-text');
+
+				expect(interpreter.textContent).toBe('Untitled-1.ipynb');
+
+				await app.workbench.positronLayouts.enterLayout('fullSizedAuxBar');
+
+				const variablesMap = await app.workbench.positronVariables.getFlatVariables();
+
+				expect(variablesMap.get('y')).toStrictEqual({ value: '[2, 3, 4, 5]', type: 'list [4]' });
 
 			});
 
-			it('Python - Basic notebook creation and execution (markdown) [C628632]', async function () {
-				const app = this.app as Application;
-
-				await app.workbench.notebook.insertNotebookCell('markdown');
-
-				await app.workbench.notebook.waitForTypeInEditor('## hello2! ');
-				await app.workbench.notebook.stopEditingCell();
-
-				expect(await app.workbench.positronNotebooks.getMarkdownText('h2')).toBe('hello2!');
-
-			});
 		});
+
 	});
 
-	describe('Notebooks', () => {
+	describe('Variables Pane - Notebook', () => {
 
 		// Shared before/after handling
 		installAllHandlers(logger);
 
-		describe('R Notebooks', () => {
+		describe('R Notebook Variables Pane', () => {
 
 			before(async function () {
-
 				await PositronRFixtures.SetupFixtures(this.app as Application);
-
 			});
 
 			after(async function () {
 
 				const app = this.app as Application;
 				await app.workbench.positronNotebooks.closeNotebookWithoutSaving();
+
+				await app.workbench.positronLayouts.enterLayout('stacked');
 			});
 
-			it('R - Basic notebook creation and execution (code) [C628629]', async function () {
+			it('Verifies Variables pane basic function for notebook with R interpreter [C669189]', async function () {
 				const app = this.app as Application;
 
 				await app.workbench.positronNotebooks.createNewNotebook();
 
 				await app.workbench.positronNotebooks.selectInterpreter('R Environments', process.env.POSITRON_R_VER_SEL!);
 
-				await app.workbench.positronNotebooks.executeInFirstCell('eval(parse(text="8**2"))');
-				expect(await app.workbench.positronNotebooks.getRCellOutput()).toBe('[1] 64');
+				await app.workbench.positronNotebooks.executeInFirstCell('y <- c(2, 3, 4, 5)');
+
+				const interpreter = await app.code.waitForElement('.positron-variables-container .action-bar-button-text');
+
+				expect(interpreter.textContent).toBe('Untitled-1.ipynb');
+
+				await app.workbench.positronLayouts.enterLayout('fullSizedAuxBar');
+
+				const variablesMap = await app.workbench.positronVariables.getFlatVariables();
+
+				expect(variablesMap.get('y')).toStrictEqual({ value: '2 3 4 5', type: 'dbl [4]' });
 
 			});
 
-			it('R - Basic notebook creation and execution (markdown) [C628630]', async function () {
-				const app = this.app as Application;
-
-				await app.workbench.notebook.insertNotebookCell('markdown');
-
-				await app.workbench.notebook.waitForTypeInEditor('## hello2! ');
-				await app.workbench.notebook.stopEditingCell();
-
-				expect(await app.workbench.positronNotebooks.getMarkdownText('h2')).toBe('hello2!');
-
-			});
 		});
 	});
 }
--- test/smoke/src/main.ts
+++ test/smoke/src/main.ts
@@ -45,6 +45,7 @@ import { setup as setupHelpTest } from './areas/positron/help/help.test';
 import { setup as setupClipboardTest } from './areas/positron/console/consoleClipboard.test';
 import { setup as setupLayoutTest } from './areas/positron/layouts/layouts.test';
 import { setup as setupTopActionBarTest } from './areas/positron/top-action-bar/top-action-bar.test';
+import { setup as setupNotebookVariablesTest} from './areas/positron/variables/notebookVariables.test';
 import { setup as setupConsoleInputTest } from './areas/positron/console/consoleInput.test';
 import { setup as setupConsoleOutputLogTest } from './areas/positron/output/consoleOutputLog.test';
 // --- End Positron ---
@@ -448,6 +449,7 @@ describe(`VSCode Smoke Tests (${opts.web ? 'Web' : 'Electron'})`, () => {
 	setupClipboardTest(logger);
 	setupTopActionBarTest(logger);
 	setupLayoutTest(logger);
+	setupNotebookVariablesTest(logger);
 	setupConsoleInputTest(logger);
 	setupConsoleOutputLogTest(logger);
 	// --- End Positron ---
