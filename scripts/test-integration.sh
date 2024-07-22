0eef8a950e9ea3d840aa671dc28d66b73e2f6e57
Chris Mead <chris.mead@posit.co>
2024-07-22 12:31:59 -0600

    run all integration tests nightly but only positron tests on PR

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -33,92 +33,11 @@ fi
 echo "Storing crash reports into '$VSCODECRASHDIR'."
 echo "Storing log files into '$VSCODELOGSDIR'."
 
-
-# Tests standalone (AMD)
-
-echo
-echo "### node.js integration tests"
-echo
-./scripts/test.sh --runGlob **/*.integrationTest.js "$@"
-
-
-# Tests in the extension host
-
-API_TESTS_EXTRA_ARGS="--disable-telemetry --skip-welcome --skip-release-notes --crash-reporter-directory=$VSCODECRASHDIR --logsPath=$VSCODELOGSDIR --no-cached-data --disable-updates --use-inmemory-secretstorage --disable-extensions --disable-workspace-trust --user-data-dir=$VSCODEUSERDATADIR"
-
-if [ -z "$INTEGRATION_TEST_APP_NAME" ]; then
-	kill_app() { true; }
-else
-	kill_app() { killall $INTEGRATION_TEST_APP_NAME || true; }
-fi
-
-echo
-echo "### API tests (folder)"
-echo
-"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS $ROOT/extensions/vscode-api-tests/testWorkspace --enable-proposed-api=vscode.vscode-api-tests --extensionDevelopmentPath=$ROOT/extensions/vscode-api-tests --extensionTestsPath=$ROOT/extensions/vscode-api-tests/out/singlefolder-tests $API_TESTS_EXTRA_ARGS
-kill_app
-
-echo
-echo "### API tests (workspace)"
-echo
-"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS $ROOT/extensions/vscode-api-tests/testworkspace.code-workspace --enable-proposed-api=vscode.vscode-api-tests --extensionDevelopmentPath=$ROOT/extensions/vscode-api-tests --extensionTestsPath=$ROOT/extensions/vscode-api-tests/out/workspace-tests $API_TESTS_EXTRA_ARGS
-kill_app
-
-echo
-echo "### Colorize tests"
-echo
-yarn test-extension -l vscode-colorize-tests
-kill_app
-
-echo
-echo "### TypeScript tests"
-echo
-"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS $ROOT/extensions/typescript-language-features/test-workspace --extensionDevelopmentPath=$ROOT/extensions/typescript-language-features --extensionTestsPath=$ROOT/extensions/typescript-language-features/out/test/unit $API_TESTS_EXTRA_ARGS
-kill_app
-
-echo
-echo "### Markdown tests"
-echo
-yarn test-extension -l markdown-language-features
-kill_app
-
-echo
-echo "### Emmet tests"
-echo
-"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS $ROOT/extensions/emmet/test-workspace --extensionDevelopmentPath=$ROOT/extensions/emmet --extensionTestsPath=$ROOT/extensions/emmet/out/test $API_TESTS_EXTRA_ARGS
-kill_app
-
-echo
-echo "### Git tests"
-echo
-"$INTEGRATION_TEST_ELECTRON_PATH" $LINUX_EXTRA_ARGS $(mktemp -d 2>/dev/null) --extensionDevelopmentPath=$ROOT/extensions/git --extensionTestsPath=$ROOT/extensions/git/out/test $API_TESTS_EXTRA_ARGS
-kill_app
-
-echo
-echo "### Ipynb tests"
-echo
-yarn test-extension -l ipynb
-kill_app
-
-echo
-echo "### Notebook Output tests"
-echo
-yarn test-extension -l notebook-renderers
-kill_app
-
-echo
-echo "### Configuration editing tests"
-echo
-yarn test-extension -l configuration-editing
-kill_app
-
-echo
-echo "### GitHub Authentication tests"
-echo
-yarn test-extension -l github-authentication
-kill_app
-
 # --- Start Positron ---
+
+# note that some tests from upstream have been deleted at this location
+# those tests are now in test-integration-nightly.sh
+
 # Positron Extensions
 
 echo
@@ -133,20 +52,10 @@ echo
 yarn test-extension -l positron-r
 kill_app
 
+# note that some tests from upstream have been deleted at this location
+# those tests are now in test-integration-nightly.sh
+
 # --- End Positron ---
-
-# Tests standalone (CommonJS)
-
-echo
-echo "### CSS tests"
-echo
-cd $ROOT/extensions/css-language-features/server && $ROOT/scripts/node-electron.sh test/index.js
-
-echo
-echo "### HTML tests"
-echo
-cd $ROOT/extensions/html-language-features/server && $ROOT/scripts/node-electron.sh test/index.js
-
 
 # Cleanup
 
