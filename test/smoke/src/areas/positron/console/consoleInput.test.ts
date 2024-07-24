21eef941f8a7e3642cb38349f3f8c749bcc3fd3c
Daniel Falbel <dfalbel@gmail.com>
2024-07-24 12:27:11 -0300

    Add R console additional tests

--- test/smoke/src/areas/positron/console/consoleInput.test.ts
+++ test/smoke/src/areas/positron/console/consoleInput.test.ts
@@ -7,6 +7,7 @@
 import { expect } from '@playwright/test';
 import { Application, Logger, PositronPythonFixtures, PositronRFixtures } from '../../../../../automation';
 import { installAllHandlers } from '../../../utils';
+import { join } from 'path';
 
 export function setup(logger: Logger) {
 	describe('Console Input', () => {
@@ -35,7 +36,7 @@ print(f'Hello {val}!')`;
 
 					await app.workbench.positronConsole.sendEnterKey();
 
-					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Enter your name:')) );
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Enter your name:')));
 
 					// slight wait before starting to type
 					await app.code.wait(200);
@@ -44,8 +45,8 @@ print(f'Hello {val}!')`;
 
 					await app.workbench.positronConsole.sendEnterKey();
 
-					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Hello John Doe!')) );
-				}).toPass({timeout: 60000});
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Hello John Doe!')));
+				}).toPass({ timeout: 60000 });
 
 			});
 		});
@@ -72,7 +73,7 @@ cat(sprintf('Hello %s!\n', val))`;
 
 					await app.workbench.positronConsole.sendEnterKey();
 
-					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Enter your name:')) );
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Enter your name:')));
 
 					// slight wait before starting to type
 					await app.code.wait(200);
@@ -81,9 +82,61 @@ cat(sprintf('Hello %s!\n', val))`;
 
 					await app.workbench.positronConsole.sendEnterKey();
 
-					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Hello John Doe!')) );
-				}).toPass({timeout: 60000});
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Hello John Doe!')));
+				}).toPass({ timeout: 60000 });
 
+			});
+
+			it('R - Can use `menu` to select alternatives', async function () {
+				const app = this.app as Application;
+				const inputCode = `x <- menu(letters)`;
+
+				await expect(async () => {
+					await app.workbench.positronConsole.pasteCodeToConsole(inputCode);
+					await app.workbench.positronConsole.sendEnterKey();
+
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('Selection:')));
+
+					// slight wait before starting to type
+					await app.code.wait(200);
+
+					await app.workbench.positronConsole.typeToConsole('1');
+					await app.workbench.positronConsole.sendEnterKey();
+
+					// slight wait before starting to type
+					await app.code.wait(200);
+
+					await app.workbench.positronConsole.typeToConsole('x');
+					await app.workbench.positronConsole.sendEnterKey();
+
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('[1] 1')));
+				}).toPass({ timeout: 60000 });
+			});
+
+			it("R - Can produce clickable links via cli", async function () {
+				const app = this.app as Application;
+
+				// Can be any file on the workkspace. We use .gitignore as it's probably
+				// always there.
+				const fileName = '.gitignore';
+				const filePath = join(app.workspacePathOrFolder, fileName);
+				const inputCode = `cli::cli_inform("{.file ${filePath}}")`;
+
+				await expect(async () => {
+					await app.workbench.positronConsole.pasteCodeToConsole(inputCode);
+					await app.workbench.positronConsole.sendEnterKey();
+
+					const activeConsole = app.workbench.positronConsole.activeConsole;
+
+					// Locate the link and click on it
+					const link = activeConsole.locator('.output-run-hyperlink');
+					expect(link).toHaveText(fileName, { useInnerText: true });
+
+					await link.click();
+					await app.code.wait(200);
+
+					await app.workbench.editors.waitForActiveTab(fileName);
+				}).toPass({ timeout: 60000 });
 			});
 		});
 	});
