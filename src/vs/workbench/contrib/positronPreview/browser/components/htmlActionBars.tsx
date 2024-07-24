a9c6230724225e84416b46f60286fe62943c8847
Jonathan McPherson <jonathan@rstudio.com>
2024-07-24 11:12:59 -0700

    show title of content in Viewer pane

--- src/vs/workbench/contrib/positronPreview/browser/components/htmlActionBars.tsx
+++ src/vs/workbench/contrib/positronPreview/browser/components/htmlActionBars.tsx
@@ -5,7 +5,7 @@
 
 import 'vs/css!./actionBars';
 import * as React from 'react';
-import { PropsWithChildren } from 'react'; // eslint-disable-line no-duplicate-imports
+import { PropsWithChildren, useEffect } from 'react'; // eslint-disable-line no-duplicate-imports
 import { localize } from 'vs/nls';
 import { PositronActionBar } from 'vs/platform/positronActionBar/browser/positronActionBar';
 import { PositronActionBarContextProvider } from 'vs/platform/positronActionBar/browser/positronActionBarContext';
@@ -14,6 +14,7 @@ import { PreviewHtml } from 'vs/workbench/contrib/positronPreview/browser/previe
 import { ActionBarRegion } from 'vs/platform/positronActionBar/browser/components/actionBarRegion';
 import { ActionBarButton } from 'vs/platform/positronActionBar/browser/components/actionBarButton';
 import { ActionBarSeparator } from 'vs/platform/positronActionBar/browser/components/actionBarSeparator';
+import { DisposableStore } from 'vs/base/common/lifecycle';
 
 const reload = localize('positron.preview.html.reload', "Reload the content");
 const clear = localize('positron.preview.html.clear', "Clear the content");
@@ -29,6 +30,8 @@ export interface HtmlActionBarsProps extends PreviewActionBarsProps {
 }
 
 export const HtmlActionBars = (props: PropsWithChildren<HtmlActionBarsProps>) => {
+
+	const [title, setTitle] = React.useState<string>(props.preview.html.kind);
 
 	// Handler for the reload button.
 	const reloadHandler = () => {
@@ -49,13 +52,24 @@ export const HtmlActionBars = (props: PropsWithChildren<HtmlActionBarsProps>) =>
 			{ openExternal: true, fromUserGesture: true });
 	};
 
+	// Main use effect.
+	useEffect(() => {
+		// Create the disposable store for cleanup.
+		const disposableStore = new DisposableStore();
+		disposableStore.add(props.preview.webview.onDidLoad((title) => {
+			setTitle(title);
+		}));
+		return () => disposableStore.dispose();
+	});
+
 	// Render.
 	return (
 		<PositronActionBarContextProvider {...props}>
 			<div className='action-bars preview-action-bar'>
 				<PositronActionBar size='small' borderTop={true} borderBottom={true} paddingLeft={kPaddingLeft} paddingRight={kPaddingRight}>
 					<ActionBarRegion location='left'>
-						{props.preview.html.kind}
+						<span className='codicon codicon-file'></span>
+						<span className='preview-title'>{title}</span>
 					</ActionBarRegion>
 					<ActionBarRegion location='center'>
 					</ActionBarRegion>
