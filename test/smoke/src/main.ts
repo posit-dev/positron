2622ee557b74177358fc6d070e1e54a443756709
Chris Mead <chris.mead@posit.co>
2024-07-26 16:03:48 -0600

    Fix main.ts

--- test/smoke/src/main.ts
+++ test/smoke/src/main.ts
@@ -50,6 +50,8 @@ import { setup as setupConsoleInputTest } from './areas/positron/console/console
 import { setup as setupConsoleANSITest } from './areas/positron/console/consoleANSI.test';
 import { setup as setupConsoleOutputLogTest } from './areas/positron/output/consoleOutputLog.test';
 import { setup as setupBasicRMarkdownTest } from './areas/positron/rmarkdown/rmarkdown.test';
+import { setup as setupWelcomeTest } from './areas/positron/welcome/welcome.test';
+import { setup as setupConsoleHistoryTest } from './areas/positron/console/consoleHistory.test';
 // --- End Positron ---
 
 const rootPath = path.join(__dirname, '..', '..', '..');
@@ -456,5 +458,7 @@ describe(`VSCode Smoke Tests (${opts.web ? 'Web' : 'Electron'})`, () => {
 	setupConsoleANSITest(logger);
 	setupConsoleOutputLogTest(logger);
 	setupBasicRMarkdownTest(logger);
+	setupWelcomeTest(logger);
+	setupConsoleHistoryTest(logger);
 	// --- End Positron ---
 });
