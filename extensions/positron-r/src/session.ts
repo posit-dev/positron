8b2b509062d72f91c76d4400a39e9a35b905ee4d
Lionel Henry <lionel.hry@gmail.com>
2024-07-29 13:25:31 +0200

    Update extensions/positron-r/src/session.ts
    Co-authored-by: Davis Vaughan <davis@rstudio.com>
    Signed-off-by: Lionel Henry <lionel.hry@proton.me>

--- extensions/positron-r/src/session.ts
+++ extensions/positron-r/src/session.ts
@@ -698,6 +698,8 @@ export function createJupyterKernelSpec(
 		// https://github.com/posit-dev/positron/issues/1619#issuecomment-1971552522
 		env['LD_LIBRARY_PATH'] = rHomePath + '/lib';
 	} else if (process.platform === 'darwin') {
+	        // Workaround for
+	        // https://github.com/posit-dev/positron/issues/3732
 		env['DYLD_LIBRARY_PATH'] = rHomePath + '/lib';
 	}
 
