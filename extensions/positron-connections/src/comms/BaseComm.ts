posit-dev/positron

commit c8e3bb9ba91a64aff767f6db81ab516ac433d376

Support binary data in comm messages via `buffers` (#4208)
Addresses https://github.com/posit-dev/positron/issues/3974 and
https://github.com/posit-dev/positron/issues/4173.

This PR plumbs `buffers` from jupyter-adapter messages through to the
main thread and specifically to our IPyWidgets notebook renderer. This
unlocks a bunch of widget types (thanks to @isabelizimm for these
examples):

#### `ipywidgets.Image`

```python
import ipywidgets
with open('image.png', 'rb') as f: image = f.read()  # you'll need an actual image path
display(ipywidgets.Image(value=image, format='png'))
```

#### `ipympl` (matplotlib's interactive backend)

Note: Save doesn't work yet. We'll need to handle the `clicked-data-url`
webview message in our `NotebookOutputWebview`.

```python
%matplotlib ipympl
import matplotlib.pyplot as plt
import numpy as np

fig, ax = plt.subplots()
x = np.linspace(0, 2*np.pi, 100)
y = np.sin(3*x)
ax.plot(x, y)
```

#### `ipydatagrid`

```python
import pandas as pd
from ipydatagrid import DataGrid
data = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]}, index=["One", "Two", "Three"])
w = DataGrid(data, selection_mode="cell", editable=True)
display(w)
# Now change the value of a cell, then run:
w.data
# It should reflect the updated value.
# The next line should update the value in the widget frontend:
w.set_cell_value("A", "One", 3)
```

#### `bqplot`

There appears to be a console warning here, we can address that in a
follow-up.

```py
import bqplot.pyplot as bplt
import numpy as np

x = np.linspace(-10, 10, 100)
y = np.sin(x)
axes_opts = {"x": {"label": "X"}, "y": {"label": "Y"}}

fig = bplt.figure(title="Line Chart")
line = bplt.plot(
    x=x, y=y, axes_options=axes_opts
)
bplt.show()
```

extensions/positron-connections/src/comms/BaseComm.ts
@@ -151,7 +151,7 @@ export class PositronBaseComm {
 		// Perform the RPC
 		let response = {} as any;
 		try {
-			response = await this.clientInstance.performRpc(request);
+			response = await this.clientInstance.performRpc<any>(request);
 		} catch (err: any) {
 			// Convert the error to a runtime method error. This handles errors
 			// that occur while performing the RPC; if the RPC is successfully

extensions/jupyter-adapter/src/JupyterKernel.ts
@@ -959,6 +959,7 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 			type: 'jupyter-message',
 			message: msg.content,
 			metadata: msg.metadata,
+			buffers: msg.buffers,
 			msgId: msg.header.msg_id,
 			msgType: msg.header.msg_type,
 			when: msg.header.date,
@@ -1286,6 +1287,7 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 	private async sendToSocket(id: string, type: string, dest: JupyterSocket,
 		parent: JupyterMessageHeader, message: JupyterMessageSpec, metadata: object = {}) {
 		const msg: JupyterMessage = {
+			// TODO: Do comms ever try to send buffers back to the kernel?
 			buffers: [],
 			content: message,
 			header: this.generateMessageHeader(id, type),

extensions/jupyter-adapter/src/JupyterMessage.ts
@@ -26,5 +26,5 @@ export interface JupyterMessage {
 	content: JupyterMessageSpec;
 
 	/** Additional binary data */
-	buffers: Array<Buffer>;
+	buffers: Array<Uint8Array>;
 }

extensions/jupyter-adapter/src/JupyterMessageDeserializer.ts
@@ -40,6 +40,9 @@ export function deserializeJupyterMessage(message: any[], key: string, channel: 
 	const parent = message[2]!.toString();
 	const metadata = message[3]!.toString();
 	const content = message[4]!.toString();
+	// Remaining buffers, if there are any, may be used to transfer raw binary data e.g.
+	// in some ipywidgets comm messages
+	const buffers = message.slice(5);
 
 	// Double-check signature
 	const hmac = createHmac('sha256', key);
@@ -58,9 +61,7 @@ export function deserializeJupyterMessage(message: any[], key: string, channel: 
 		parent_header: JSON.parse(parent), // eslint-disable-line
 		metadata: JSON.parse(metadata),
 		content: JSON.parse(content),
-
-		// TODO: Do we need to pass buffers?
-		buffers: []
+		buffers,
 	};
 
 	return msg;

extensions/jupyter-adapter/src/JupyterMessagePacket.ts
@@ -36,4 +36,7 @@ export interface JupyterMessagePacket {
 
 	/** Additional metadata, if any */
 	metadata?: Map<any, any>;
+
+	/** Additional binary data, if any */
+	buffers?: Array<Uint8Array>;
 }

extensions/jupyter-adapter/src/LanguageRuntimeAdapter.ts
@@ -686,6 +686,7 @@ export class LanguageRuntimeSessionAdapter
 			comm_id: msg.comm_id,
 			data: msg.data,
 			metadata: message.metadata,
+			buffers: message.buffers,
 		} as positron.LanguageRuntimeCommMessage);
 	}
 
@@ -960,7 +961,8 @@ export class LanguageRuntimeSessionAdapter
 
 		// Handle events from the DAP
 		const comm = this._comms.get(clientId)!;
-		this._disposables.push(comm.onDidReceiveCommMsg(msg => {
+		this._disposables.push(comm.onDidReceiveCommMsg(e => {
+			const msg = e.data;
 			switch (msg.msg_type) {
 				// The runtime is in control of when to start a debug session.
 				// When this happens, we attach automatically to the runtime

extensions/jupyter-adapter/src/RuntimeClientAdapter.ts
@@ -12,6 +12,11 @@ import { JupyterCommMsg } from './JupyterCommMsg';
 import { JupyterCommClose } from './JupyterCommClose';
 import { PromiseHandles, delay, uuidv4 } from './utils';
 
+interface IRuntimeClientOutput {
+	data: Record<string, any>;
+	buffers?: Array<Uint8Array>;
+}
+
 /**
  * Adapts a Positron Language Runtime client widget to a Jupyter kernel.
  */
@@ -28,9 +33,9 @@ export class RuntimeClientAdapter {
 	 * and the value is a promise that will be resolved when the RPC response is
 	 * received.
 	 */
-	private _pendingRpcs = new Map<string, PromiseHandles<any>>();
+	private _pendingRpcs = new Map<string, PromiseHandles<IRuntimeClientOutput>>();
 
-	private readonly _messageEmitter = new vscode.EventEmitter<{ [key: string]: any }>();
+	private readonly _messageEmitter = new vscode.EventEmitter<IRuntimeClientOutput>();
 	readonly onDidReceiveCommMsg = this._messageEmitter.event;
 
 	constructor(
@@ -203,7 +208,7 @@ export class RuntimeClientAdapter {
 	/**
 	 * Perform an RPC call over the comm channel.
 	 */
-	performRpc(request: any): Promise<any> {
+	performRpcWithBuffers(request: any): Promise<IRuntimeClientOutput> {
 		// Create a random ID for this request
 		const id = uuidv4();
 
@@ -214,6 +219,16 @@ export class RuntimeClientAdapter {
 		// Send the request and return the promise
 		this._kernel.sendCommMessage(this._id, id, request);
 		return out.promise;
+	}
+
+	/**
+	 * Perform an RPC call over the comm channel.
+	 *
+	 * This method is a convenience wrapper around {@link performRpcWithBuffers} that returns
+	 * only the data portion of the RPC response.
+	 */
+	async performRpc(request: any): Promise<any> {
+		return (await this.performRpcWithBuffers(request)).data;
 	}
 
 	/**
@@ -255,7 +270,7 @@ export class RuntimeClientAdapter {
 					`RPC already settled (timed out?)`);
 			} else {
 				// Otherwise, resolve the promise with the message data.
-				promise.resolve(message.data);
+				promise.resolve({ data: message.data, buffers: msg.buffers });
 			}
 
 			// Remove the RPC from the pending list
@@ -263,7 +278,7 @@ export class RuntimeClientAdapter {
 		} else {
 
 			// Not an RPC response, so emit the message
-			this._messageEmitter.fire(message.data);
+			this._messageEmitter.fire({ data: message.data, buffers: msg.buffers });
 		}
 	}
 

extensions/positron-ipywidgets/renderer/src/comm.ts
@@ -74,14 +74,14 @@ export class Comm implements base.IClassicComm, Disposable {
 	 * @param data The data to send.
 	 * @param callbacks Callbacks to handle the response.
 	 * @param _metadata Metadata to send with the message - not currently used.
-	 * @param _buffers Buffers to send with the message - not currently used.
+	 * @param buffers Buffers to send with the message - not currently used.
 	 * @returns The message ID.
 	 */
 	send(
 		data: JSONValue,
 		callbacks?: base.ICallbacks,
 		_metadata?: JSONObject,
-		_buffers?: ArrayBuffer[] | ArrayBufferView[]
+		buffers?: ArrayBuffer[] | ArrayBufferView[]
 	): string {
 
 		// Callbacks are used to handle responses from the runtime.
@@ -103,6 +103,10 @@ export class Comm implements base.IClassicComm, Disposable {
 
 		if (callbacks?.iopub?.output) {
 			throw new Error('Callback iopub.output not implemented');
+		}
+
+		if (buffers && buffers.length > 0) {
+			console.warn(`Comm tried to send message with buffers:`, data, buffers);
 		}
 
 		const msgId = UUID.uuid4();
@@ -216,6 +220,9 @@ export class Comm implements base.IClassicComm, Disposable {
 					comm_id: this.comm_id,
 					data: message.data as JSONObject,
 				},
+				// Some widget libraries (e.g. ipydatagrid) may try to create an Int32Array which
+				// could fail if the original buffer is not correctly aligned, so create a new buffer.
+				buffers: message.buffers?.map(buffer => new Uint8Array(buffer)),
 				// Stub the rest of the interface - these are not currently used by widget libraries.
 				channel: 'iopub',
 				header: {

commit ccd1e75b0c17d7454c6700a319e0bcc4088d2e37

Fix connections base comm events (#4242)
Fixes the `R - Connections are update after adding a database,[C663724]`
smoke test, which was broken in
https://github.com/posit-dev/positron/pull/4208.

extensions/positron-connections/src/comms/BaseComm.ts
@@ -213,7 +213,8 @@ export class PositronBaseComm {
 		name: string,
 		properties: string[]
 	): Event<T> {
-		const event = this.clientInstance.onDidSendEvent((event: any) => {
+		const event = this.clientInstance.onDidSendEvent((output) => {
+			const event = output.data as any;
 			if (event.method === name) {
 				const args = event.params;
 				const namedArgs: any = {};