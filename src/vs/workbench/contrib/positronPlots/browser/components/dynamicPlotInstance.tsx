4f1c022e55439e5f73c9900651c35ddc8315fc6b
Tim Mok <timtmok@users.noreply.github.com>
2024-08-06 11:04:04 -0400

    Fix sizing re-render
    Updates pan and zoom with new image size
    Fix multi-window bug

--- src/vs/workbench/contrib/positronPlots/browser/components/dynamicPlotInstance.tsx
+++ src/vs/workbench/contrib/positronPlots/browser/components/dynamicPlotInstance.tsx
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as React from 'react';
+import * as DOM from 'vs/base/browser/dom';
 import { useEffect, useState } from 'react'; // eslint-disable-line no-duplicate-imports
 import { ProgressBar } from 'vs/base/browser/ui/progressbar/progressbar';
 import { DisposableStore } from 'vs/base/common/lifecycle';
@@ -42,7 +43,7 @@ export const DynamicPlotInstance = (props: DynamicPlotInstanceProps) => {
 	const plotsContext = usePositronPlotsContext();
 
 	useEffect(() => {
-		const ratio = window.devicePixelRatio;
+		const ratio = DOM.getActiveWindow().devicePixelRatio;
 		const disposables = new DisposableStore();
 
 		// If the plot is already rendered, use the old image until the new one is ready.
@@ -121,7 +122,7 @@ export const DynamicPlotInstance = (props: DynamicPlotInstanceProps) => {
 					// to be done.
 					const started = Date.now();
 					progressBar.total(props.plotClient.renderEstimateMs);
-					progressTimer = window.setInterval(() => {
+					progressTimer = DOM.getActiveWindow().setInterval(() => {
 						// Every 100ms, update the progress bar.
 						progressBar?.setWorked(Date.now() - started);
 					}, 100);
@@ -135,7 +136,7 @@ export const DynamicPlotInstance = (props: DynamicPlotInstanceProps) => {
 				// When the render completes, clean up the progress bar and
 				// timers if they exist.
 				if (progressTimer) {
-					window.clearTimeout(progressTimer);
+					DOM.getActiveWindow().clearTimeout(progressTimer);
 					progressTimer = undefined;
 				}
 				if (progressBar) {
@@ -148,7 +149,7 @@ export const DynamicPlotInstance = (props: DynamicPlotInstanceProps) => {
 		return () => {
 			disposables.dispose();
 		};
-	});
+	}, [props.plotClient, props.height, props.width, plotsContext.positronPlotsService, plotsContext.notificationService]);
 
 	// Render method for the plot image.
 	const renderedImage = () => {
