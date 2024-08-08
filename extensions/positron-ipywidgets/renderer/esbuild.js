4bc25cd512cce3dd82140c499d3b33d50e38b78d
Wasim Lorgat <mwlorgat@gmail.com>
2024-08-08 15:53:58 +0200

    Fix positron-ipywidgets build (#4285)
    Second attempt to fix the positron-ipywidgets build following #4211.
    
    The base esbuild script was only using the `path.basename` of the output
    directory, so it was building to `positron-ipywidgets/media` instead of
    `positron-ipywidgets/renderer/media`:
    
    https://github.com/posit-dev/positron/blob/5053aa2682f6e2b95ea5aa33093acdcacd057298/extensions/esbuild-webview-common.js#L70
    
    This PR attempts to fix it by moving the renderer's esbuild script into
    the `renderer` folder, where I think it belongs anyway.

--- extensions/positron-ipywidgets/renderer/esbuild.js
+++ extensions/positron-ipywidgets/renderer/esbuild.js
@@ -6,10 +6,10 @@
 // @ts-check
 const path = require('path');
 
-const srcDir = path.join(__dirname, 'renderer/src');
-const outDir = path.join(__dirname, 'renderer/media');
+const srcDir = path.join(__dirname, 'src');
+const outDir = path.join(__dirname, 'media');
 
-require('../esbuild-webview-common').run({
+require('../../esbuild-webview-common').run({
 	entryPoints: [
 		path.join(srcDir, 'index.ts'),
 	],
