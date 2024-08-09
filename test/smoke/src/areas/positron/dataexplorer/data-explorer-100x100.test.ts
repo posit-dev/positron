96b9819a57ab3deab66fb89e2749a1813b46816e
Chris Mead <chris.mead@posit.co>
2024-08-09 09:15:59 -0600

    Move incorrectly placed skip for test case needing work

--- test/smoke/src/areas/positron/dataexplorer/data-explorer-100x100.test.ts
+++ test/smoke/src/areas/positron/dataexplorer/data-explorer-100x100.test.ts
@@ -219,7 +219,7 @@ export function setupDataExplorer100x100Test(logger: Logger) {
 		/**
 		 * Data Explorer 100x100 - R.
 		 */
-		describe('Data Explorer 100x100 - R', function () {
+		describe.skip('Data Explorer 100x100 - R', function () {
 			/**
 			 * Before hook.
 			 */
@@ -240,7 +240,7 @@ export function setupDataExplorer100x100Test(logger: Logger) {
 			/**
 			 * Data Explorer 100x100 - R - Smoke Test.
 			 */
-			it.skip('Data Explorer 100x100 - R - Smoke Test [C674521]', async function () {
+			it('Data Explorer 100x100 - R - Smoke Test [C674521]', async function () {
 				// Get the app.
 				const app = this.app as Application;
 
