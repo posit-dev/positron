faaec868c84fa658efe6765533140562435c1efb
Chris Mead <chris.mead@posit.co>
2024-08-09 15:01:48 -0600

    Test fully implemented

--- test/smoke/src/areas/positron/editor/fast-execution.test.ts
+++ test/smoke/src/areas/positron/editor/fast-execution.test.ts
@@ -3,8 +3,10 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { join } from 'path';
 import { Application, Logger, PositronRFixtures } from '../../../../../automation';
 import { installAllHandlers } from '../../../utils';
+import { expect } from '@playwright/test';
 
 /*
  * R console tests
@@ -23,12 +25,27 @@ export function setup(logger: Logger) {
 
 			});
 
-			it('Verify fast execution is not out of order [C...]', async function () {
+			it('Verify fast execution is not out of order [C712539]', async function () {
 				const app = this.app as Application;
+				await app.workbench.quickaccess.openFile(join(app.workspacePathOrFolder, 'workspaces', 'fast-statement-execution', 'fast-execution.r'));
 
+				for (let i = 0; i < 11; i++) {
+					await app.code.driver.getKeyboard().press('Control+Enter');
+				}
+
+				await app.code.wait(1000);
+
+				const variablesMap = await app.workbench.positronVariables.getFlatVariables();
+
+				await app.workbench.positronLayouts.enterLayout('fullSizedAuxBar');
+
+				expect(variablesMap.get('x')).toStrictEqual({ value: '1', type: 'dbl' });
+				expect(variablesMap.get('y')).toStrictEqual({ value: '1', type: 'dbl' });
+				expect(variablesMap.get('z')).toStrictEqual({ value: '1', type: 'dbl' });
+				expect(variablesMap.get('a')).toStrictEqual({ value: '1', type: 'dbl' });
+				expect(variablesMap.get('b')).toStrictEqual({ value: '1', type: 'dbl' });
+				expect(variablesMap.get('c')).toStrictEqual({ value: '1', type: 'dbl' });
 			});
-
 		});
-
 	});
 }
--- test/smoke/src/main.ts
+++ test/smoke/src/main.ts
@@ -53,6 +53,7 @@ import { setup as setupBasicRMarkdownTest } from './areas/positron/rmarkdown/rma
 import { setup as setupWelcomeTest } from './areas/positron/welcome/welcome.test';
 import { setup as setupConsoleHistoryTest } from './areas/positron/console/consoleHistory.test';
 import { setup as setupShinyTest } from './areas/positron/apps/shiny.test';
+import { setup as setupFastExecutionTest } from './areas/positron/editor/fast-execution.test';
 // --- End Positron ---
 
 const rootPath = path.join(__dirname, '..', '..', '..');
@@ -462,5 +463,6 @@ describe(`VSCode Smoke Tests (${opts.web ? 'Web' : 'Electron'})`, () => {
 	setupWelcomeTest(logger);
 	setupConsoleHistoryTest(logger);
 	setupShinyTest(logger);
+	setupFastExecutionTest(logger);
 	// --- End Positron ---
 });
