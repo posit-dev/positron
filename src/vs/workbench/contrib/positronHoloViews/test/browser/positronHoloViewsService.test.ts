11b656a47e00e127117bc3c6d6ef1a0abca243b4
Nick Strayer <nick.strayer@rstudio.com>
2024-08-14 12:56:23 -0400

    Remove duplication from test script

--- src/vs/workbench/contrib/positronHoloViews/test/browser/positronHoloViewsService.test.ts
+++ src/vs/workbench/contrib/positronHoloViews/test/browser/positronHoloViewsService.test.ts
@@ -5,15 +5,11 @@
 import assert from 'assert';
 import { timeout } from 'vs/base/common/async';
 import { ensureNoDisposablesAreLeakedInTestSuite } from 'vs/base/test/common/utils';
-import { ExtensionIdentifier } from 'vs/platform/extensions/common/extensions';
-import { INotebookEditorService } from 'vs/workbench/contrib/notebook/browser/services/notebookEditorService';
-import { NotebookEditorWidgetService } from 'vs/workbench/contrib/notebook/browser/services/notebookEditorServiceImpl';
 import { NotebookRendererMessagingService } from 'vs/workbench/contrib/notebook/browser/services/notebookRendererMessagingServiceImpl';
-import { INotebookRendererInfo, INotebookStaticPreloadInfo } from 'vs/workbench/contrib/notebook/common/notebookCommon';
-import { NotebookOutputRendererInfo } from 'vs/workbench/contrib/notebook/common/notebookOutputRenderer';
 import { INotebookRendererMessagingService } from 'vs/workbench/contrib/notebook/common/notebookRendererMessagingService';
 import { INotebookService } from 'vs/workbench/contrib/notebook/common/notebookService';
 import { PositronHoloViewsService } from 'vs/workbench/contrib/positronHoloViews/browser/positronHoloViewsService';
+import { TestNotebookService } from 'vs/workbench/contrib/positronIPyWidgets/test/browser/positronIPyWidgetsService.test';
 import { IPositronNotebookOutputWebviewService } from 'vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebviewService';
 import { PositronNotebookOutputWebviewService } from 'vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebviewServiceImpl';
 import { WebviewPlotClient } from 'vs/workbench/contrib/positronPlots/browser/webviewPlotClient';
@@ -25,23 +21,6 @@ import { TestLanguageRuntimeSession } from 'vs/workbench/services/runtimeSession
 import { TestRuntimeSessionService } from 'vs/workbench/services/runtimeSession/test/common/testRuntimeSessionService';
 import { workbenchInstantiationService } from 'vs/workbench/test/browser/workbenchTestServices';
 
-class TestNotebookService implements Partial<INotebookService> {
-	getRenderers(): INotebookRendererInfo[] {
-		return [];
-	}
-
-	getPreferredRenderer(_mimeType: string): NotebookOutputRendererInfo | undefined {
-		// Doesn't matter what the renderer is, just that it exists.
-		return <NotebookOutputRendererInfo>{
-			id: 'positron-ipywidgets',
-			extensionId: new ExtensionIdentifier('vscode.positron-ipywidgets'),
-		};
-	}
-
-	*getStaticPreloads(_viewType: string): Iterable<INotebookStaticPreloadInfo> {
-		// Yield nothing.
-	}
-}
 
 const hvPreloadMessage1 = {
 	kind: RuntimeOutputKind.HoloViews,
@@ -72,13 +51,10 @@ suite('Positron - PositronHoloViewsService', () => {
 
 	let positronHoloViewsService: PositronHoloViewsService;
 	let runtimeSessionService: TestRuntimeSessionService;
-	let notebookEditorService: INotebookEditorService;
 
 	setup(() => {
 		const instantiationService = workbenchInstantiationService(undefined, disposables);
 		instantiationService.stub(INotebookRendererMessagingService, disposables.add(instantiationService.createInstance(NotebookRendererMessagingService)));
-		notebookEditorService = disposables.add(instantiationService.createInstance(NotebookEditorWidgetService));
-		instantiationService.stub(INotebookEditorService, notebookEditorService);
 		instantiationService.stub(INotebookService, new TestNotebookService());
 		instantiationService.stub(IWebviewService, disposables.add(new WebviewService(instantiationService)));
 		instantiationService.stub(IPositronNotebookOutputWebviewService, instantiationService.createInstance(PositronNotebookOutputWebviewService));
