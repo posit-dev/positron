c5be1eff2135b23b36056d1c65f58ced11102abc
Lionel Henry <lionel.hry@gmail.com>
2024-08-14 05:18:04 -0700

    Use custom tools for universal binary reconciliation
    `filesToSkip` no longer works for us. It is not consulted for x64-only files,
    which should be ignored via `x64ArchFiles`, which only accepts a single glob
    pattern. So we now have our own procedure to stash away such files and move them
    back into the universal bundle at the end.
    
    We also create a new set of files to reconcile following
    this upstream change:
    https://github.com/microsoft/vscode/commit/f6f90e016301f288ac5b36936d15ff8af9d70923
    
    Localised strings are now stored in an array and the strings are replaced at
    build time by references to this array. Somehow this array differs across arch
    distributions, so we fix that up by copying from the relevant files from the x64
    dist to the arm64 one.

--- build/darwin/create-universal-app.ts
+++ build/darwin/create-universal-app.ts
@@ -11,6 +11,150 @@ import { spawn } from '@malept/cross-spawn-promise';
 
 const root = path.dirname(path.dirname(__dirname));
 
+// --- Start Positron ---
+
+import * as os from 'os';
+
+// The merging procedure will fail if:
+//
+// (a) It finds x64 files that don't have an arm64 equivalent. For instance the
+//     pydevd folder contains precompiled single arch libraries.
+//
+// (b) It finds binaries that are already univeral, for instance ark.
+//     (Note that in more recent versions of vscode-universal-bundler (>= 0.1.2), it
+//     won't be necessary to worry about universal binaries as those will be
+//     ignored by the bundler.)
+//
+// `makeUniversalApp()` has arguments to handle some of these cases but these
+// are not flexible enough for our purposes. For instance, x64-only files need
+// to be specified in `x64ArchFiles` which only accepts a single glob pattern.
+// `filesToSkipComparison` is not consulted in this case. So we stash away all
+// problematic files and restore them once the merging procedure has completed.
+//
+// The files to stash away are determined with minimatch patterns. Folders are
+// excluded from matches, so to match a whole folder use `myfolder/**`.
+//
+const stashPatterns = [
+	// Exclusions from ZeroMQ node module
+	'**/electron.napi.node',    // ZeroMQ Electron architecture-specific pre-built binary
+	'**/node.napi.node',        // ZeroMQ Electron architecture-specific pre-built binary
+	'**/node.napi.glibc.node',  // ZeroMQ Electron architecture-specific pre-built binary
+	// Exclusions from remote-ssh
+	'**/cpufeatures.node',
+	'**/sshcrypto.node',
+	// Case-sensitivity issues
+	'**/HTML.icns',
+	'**/html.icns',
+	// Exclusions from Python language pack (positron-python)
+	'**/pydevd/**',             // Cython pre-built binaries for Python debugging
+	// Exclusions from R language pack (positron-r)
+	'**/ark',                   // Compiled R kernel and LSP
+	// Exclusions from Quarto
+	'**/quarto/bin/tools/**',
+];
+
+// Some generated files may end up being different in both distributions.
+// `reconciliationFiles` contains relative paths of files that should be copied
+// from the x64 bundle to the arm64 one so they don't cause a mismatch error.
+const reconciliationFiles = [
+	'Contents/Resources/app/product.json',
+	// Definitions of localized strings
+	'Contents/Resources/app/out/nls.messages.json',
+	'Contents/Resources/app/out/nls.keys.json',
+	// Consumers of localised strings, found by grepping for `nls_1.localize`
+	'Contents/Resources/app/out/vs/platform/profiling/electron-sandbox/profileAnalysisWorker.js',
+	'Contents/Resources/app/out/vs/platform/files/node/watcher/watcherMain.js',
+	'Contents/Resources/app/out/vs/platform/terminal/node/ptyHostMain.js',
+	'Contents/Resources/app/out/vs/code/node/cli.js',
+	'Contents/Resources/app/out/vs/code/node/cliProcessMain.js',
+	'Contents/Resources/app/out/vs/code/electron-sandbox/processExplorer/processExplorerMain.js',
+	'Contents/Resources/app/out/vs/code/node/sharedProcess/sharedProcessMain.js',
+	'Contents/Resources/app/out/vs/code/electron-main/main.js',
+	'Contents/Resources/app/out/vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.js',
+	'Contents/Resources/app/out/vs/workbench/contrib/issue/electron-sandbox/issueReporterMain.js',
+	'Contents/Resources/app/out/vs/base/worker/workerMain.js',
+	'Contents/Resources/app/out/vs/workbench/api/worker/extensionHostWorker.js',
+	'Contents/Resources/app/out/vs/workbench/api/node/extensionHostProcess.js',
+	'Contents/Resources/app/out/vs/workbench/workbench.desktop.main.js',
+];
+
+function readFiles(dir: string): string[] {
+	let files: string[] = [];
+	const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+	for (const entry of entries) {
+		const fullPath = path.join(dir, entry.name);
+
+		// Recurse into directories and only return files
+		if (entry.isDirectory()) {
+			files = files.concat(readFiles(fullPath));
+		} else if (entry.isFile()) {
+			files.push(fullPath);
+		}
+	}
+
+	return files;
+}
+
+function ensureDir(file: string): string {
+	const dir = path.dirname(file);
+
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir, { recursive: true });
+	}
+
+	return dir;
+}
+
+function stashFiles(stash: string, x64Path: string, arm64Path: string) {
+	const x64Stash = path.join(stash, 'x64');
+	const arm64Stash = path.join(stash, 'arm64');
+	fs.mkdirSync(x64Stash);
+	fs.mkdirSync(arm64Stash);
+
+	const matches = readFiles(x64Path).
+		filter((file) => stashPatterns.some(pat => minimatch(file, pat)));
+
+	for (const x64Source of matches) {
+		const rel = path.relative(x64Path, x64Source);
+
+		const arm64Source = path.join(arm64Path, rel);
+		if (!fs.existsSync(arm64Source)) {
+			throw new Error(`Cannot find '${rel}' in arm64 source`);
+		}
+
+		const x64Dest = path.join(x64Stash, rel);
+		const arm64Dest = path.join(arm64Stash, rel);
+
+		ensureDir(x64Dest);
+		ensureDir(arm64Dest);
+
+		fs.renameSync(x64Source, x64Dest);
+		fs.renameSync(arm64Source, arm64Dest);
+	}
+}
+
+// Copy files to reconcile from the x64 build to the arm64 one
+function reconcileFiles(x64Path: string, arm64Path: string) {
+	for (const file of reconciliationFiles) {
+		const src = path.join(x64Path, file);
+		const dest = path.join(arm64Path, file);
+		fs.copyFileSync(src, dest);
+	}
+}
+
+function restoreFromStash(stash: string, destRoot: string): void {
+	for (const src of readFiles(stash)) {
+		const rel = path.relative(stash, src);
+		const dest = path.join(destRoot, rel);
+
+		ensureDir(dest);
+		fs.renameSync(src, dest);
+	}
+}
+
+// --- End Positron ---
+
 async function main(buildDir?: string) {
 	const arch = process.env['VSCODE_ARCH'];
 
@@ -29,30 +173,42 @@ async function main(buildDir?: string) {
 	const filesToSkip = [
 		'**/CodeResources',
 		'**/Credits.rtf',
-		// --- Start Positron ---
-		// Exclusions from ZeroMQ node module
-		'electron.napi.node', // ZeroMQ Electron architecture-specific pre-built binary
-		'node.napi.node',     // ZeroMQ Electron architecture-specific pre-built binary
-		'node.napi.glibc.node', // ZeroMQ Electron architecture-specific pre-built binary
-		// Exclusions from remote-ssh
-		'cpufeatures.node',
-		'sshcrypto.node',
-		// Case-sensitivity issues
-		'HTML.icns',
-		'html.icns',
-		// Exclusions from Python language pack (positron-python)
-		'pydevd',             // Cython pre-built binaries for Python debugging
-		// Exclusions from R language pack (positron-r)
-		'ark',                // Compiled R kernel and LSP
-		// Exclusions from Quarto
-		'dart',
-		'deno',
-		'esbuild',
-		'pandoc',
-		'sass',
-		'typst',
-		// --- End Positron ---
 	];
+
+	// --- Start Positron ---
+
+	// We split the original main function in two parts so that we can call the
+	// second part in a try block without causing the formatter to increase the
+	// indentation, which would increase the chance of merge conflicts
+
+	const stash = fs.mkdtempSync(path.join(os.tmpdir(), 'positron-create-universal'));
+
+	try {
+		reconcileFiles(x64AppPath, arm64AppPath);
+		stashFiles(stash, x64AppPath, arm64AppPath);
+		await origMain(
+			x64AppPath,
+			arm64AppPath,
+			asarRelativePath,
+			outAppPath,
+			filesToSkip,
+			productJsonPath
+		);
+		restoreFromStash(path.join(stash, 'x64'), outAppPath);
+	} finally {
+		fs.rmSync(stash, { recursive: true, force: true });
+	}
+}
+
+async function origMain(
+	x64AppPath: string,
+	arm64AppPath: string,
+	asarRelativePath: string,
+	outAppPath: string,
+	filesToSkip: string[],
+	productJsonPath: string,
+) {
+	// --- End Positron ---
 
 	await makeUniversalApp({
 		x64AppPath,
