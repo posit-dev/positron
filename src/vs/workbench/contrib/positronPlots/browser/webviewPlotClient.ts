6487a7c43a16a899737077eb54a0305ee540fecf
seem <mwlorgat@gmail.com>
2024-08-20 15:54:57 +0200

    use type guard

--- src/vs/workbench/contrib/positronPlots/browser/webviewPlotClient.ts
+++ src/vs/workbench/contrib/positronPlots/browser/webviewPlotClient.ts
@@ -63,8 +63,8 @@ export abstract class WebviewPlotClient extends Disposable implements IPositronP
 	}
 
 	/** Whether the plot's underlying webview is active. */
-	isActive(): boolean {
-		return !!this._webview.value;
+	isActive(): this is { _webview: { value: IOverlayWebview } } {
+		return Boolean(this._webview.value);
 	}
 
 	/**
@@ -107,7 +107,7 @@ export abstract class WebviewPlotClient extends Disposable implements IPositronP
 	 * @param claimant The object taking ownership.
 	 */
 	public claim(claimant: any) {
-		if (!this._webview.value) {
+		if (!this.isActive()) {
 			throw new Error('No webview to claim');
 		}
 		this._webview.value.claim(claimant, DOM.getWindow(this._element), undefined);
@@ -120,7 +120,7 @@ export abstract class WebviewPlotClient extends Disposable implements IPositronP
 	 * @param ele The element over which to position the webview.
 	 */
 	public layoutWebviewOverElement(ele: HTMLElement) {
-		if (!this._webview.value) {
+		if (!this.isActive()) {
 			throw new Error('No webview to layout');
 		}
 		this._element = ele;
@@ -133,7 +133,7 @@ export abstract class WebviewPlotClient extends Disposable implements IPositronP
 	 * @param claimant The object releasing ownership.
 	 */
 	public release(claimant: any) {
-		if (!this._webview.value) {
+		if (!this.isActive()) {
 			throw new Error('No webview to release');
 		}
 		this._webview.value.release(claimant);
@@ -149,7 +149,7 @@ export abstract class WebviewPlotClient extends Disposable implements IPositronP
 	 * Electron APIs in desktop mode) as PNG.
 	 */
 	private renderThumbnail() {
-		if (!this._webview.value) {
+		if (!this.isActive()) {
 			throw new Error('No webview to render thumbnail');
 		}
 		this._webview.value.captureContentsAsPng().then(data => {
