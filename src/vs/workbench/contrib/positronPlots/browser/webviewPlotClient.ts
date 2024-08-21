posit-dev/positron

commit 8e8026de459739b664591a28bd22ae9b78ce2183

Improve interactive plot webview lifecycle management (#4391)
This PR improves interactive plots by:

1. Retaining webview state (e.g. the plot's zoom level).
2. Reusing the service worker process.
3. Cleaning up unnecessary webviews:
    1. When the maximum number of active webview plots (currently 5) is
exceeded, the least recently selected plot's webview is disposed.
    2. When a plot has not been selected for a while (currently 2 minutes),
its webview is disposed.

Addresses #1713.

### Implementation

The following changes were required:

1. Enable `retainContextWhenHidden` for plot webviews, so that the
   webview context/state is retained when the plot loses focus.
2. Specify an explicit webview `origin` (currently the window's origin),
   so that the same service worker is reused.
3. Moved the creation of underlying webviews into `WebviewPlotClient`
   subclasses, so that we can dispose and recreate webviews as needed.

### QA Notes

Might be best to manually change the `WebviewPlotInactiveTimeout`
constant to a shorter time to test that out.

When a webview is deactivated, there will be a log message. You'll also
lose any state (e.g. the plot's zoom level).

src/vs/workbench/contrib/positronPlots/browser/webviewPlotClient.ts
@@ -6,7 +6,7 @@
 import * as DOM from 'vs/base/browser/dom';
 import { VSBuffer, encodeBase64 } from 'vs/base/common/buffer';
 import { Emitter, Event } from 'vs/base/common/event';
-import { Disposable } from 'vs/base/common/lifecycle';
+import { Disposable, MutableDisposable } from 'vs/base/common/lifecycle';
 import { IOverlayWebview } from 'vs/workbench/contrib/webview/browser/webview';
 import { IPositronPlotMetadata } from 'vs/workbench/services/languageRuntime/common/languageRuntimePlotClient';
 import { IPositronPlotClient } from 'vs/workbench/services/positronPlots/common/positronPlots';
@@ -14,10 +14,13 @@ import { IPositronPlotClient } from 'vs/workbench/services/positronPlots/common/
 /**
  * A Positron plot instance that is backed by a webview.
  */
-export class WebviewPlotClient extends Disposable implements IPositronPlotClient {
+export abstract class WebviewPlotClient extends Disposable implements IPositronPlotClient {
 
+	protected readonly _webview = this._register(new MutableDisposable<IOverlayWebview>());
 
 	private _thumbnail: VSBuffer | undefined;
+
+	private _onDidActivate: Emitter<void>;
 
 	private _onDidRenderThumbnail: Emitter<string>;
 
@@ -27,21 +30,19 @@ export class WebviewPlotClient extends Disposable implements IPositronPlotClient
 
 	private _element: HTMLElement | undefined;
 
+	private _pendingActivation?: Promise<void>;
+
 	/**
-	 * Creates a new WebPlotClient, which wraps a notebook output webview in
-	 * an object that can be displayed in the Plots pane.
+	 * Creates a new NotebookOutputPlotClient, which manages the lifecycle of a
+	 * webview, wrapped in an object that can be displayed in the Plots pane.
 	 *
-	 * @param webview The webview to wrap.
-	 * @param message The output message from which the webview was created.
-	 * @param code The code that generated the webview (if known)
+	 * @param metadata The metadata associated with the plot.
 	 */
-	constructor(
-		public readonly metadata: IPositronPlotMetadata,
-		public readonly webview: IOverlayWebview) {
+	constructor(public readonly metadata: IPositronPlotMetadata) {
 		super();
 
-		// Ensure that the webview is disposed when the plot client is disposed.
-		this._register(webview);
+		this._onDidActivate = this._register(new Emitter<void>());
+		this.onDidActivate = this._onDidActivate.event;
 
 		this._onDidRenderThumbnail = this._register(new Emitter<string>());
 		this.onDidRenderThumbnail = this._onDidRenderThumbnail.event;
@@ -63,13 +64,67 @@ export class WebviewPlotClient extends Disposable implements IPositronPlotClient
 		return undefined;
 	}
 
+	/** Whether the plot's underlying webview is active. */
+	isActive(): this is { _webview: { value: IOverlayWebview } } {
+		return Boolean(this._webview.value);
+	}
+
+	/**
+	 * Creates the underlying webview.
+	 **/
+	protected abstract createWebview(): Promise<IOverlayWebview>;
+
+	/**
+	 * Disposes the underlying webview.
+	 **/
+	protected abstract disposeWebview(): void;
+
+	/**
+	 * Activates the plot, creating the underlying webview if needed.
+	 **/
+	public activate() {
+		// If we're already active, do nothing.
+		if (this.isActive()) {
+			return Promise.resolve();
+		}
+
+		// If we're already activating, return the existing promise.
+		if (this._pendingActivation) {
+			return this._pendingActivation;
+		}
+
+		// Otherwise, create the webview and fire the activation event.
+		this._pendingActivation = this.createWebview().then((webview) => {
+			this._webview.value = webview;
+			this._onDidActivate.fire();
+		}).finally(() => {
+			this._pendingActivation = undefined;
+		});
+		return this._pendingActivation;
+	}
+
+	/**
+	 * Deactivates the plot, disposing the underlying webview if needed.
+	 **/
+	public deactivate() {
+		if (!this.isActive()) {
+			// Already inactive, do nothing.
+			return;
+		}
+		this.disposeWebview();
+		this._webview.clear();
+	}
+
 	/**
 	 * Claims the underlying webview.
 	 *
 	 * @param claimant The object taking ownership.
 	 */
 	public claim(claimant: any) {
-		this.webview.claim(claimant, DOM.getWindow(this._element), undefined);
+		if (!this.isActive()) {
+			throw new Error('No webview to claim');
+		}
+		this._webview.value.claim(claimant, DOM.getWindow(this._element), undefined);
 		this._claimed = true;
 	}
 
@@ -79,8 +134,11 @@ export class WebviewPlotClient extends Disposable implements IPositronPlotClient
 	 * @param ele The element over which to position the webview.
 	 */
 	public layoutWebviewOverElement(ele: HTMLElement) {
+		if (!this.isActive()) {
+			throw new Error('No webview to layout');
+		}
 		this._element = ele;
-		this.webview.layoutWebviewOverElement(ele);
+		this._webview.value.layoutWebviewOverElement(ele);
 	}
 
 	/**
@@ -89,7 +147,10 @@ export class WebviewPlotClient extends Disposable implements IPositronPlotClient
 	 * @param claimant The object releasing ownership.
 	 */
 	public release(claimant: any) {
-		this.webview.release(claimant);
+		if (!this.isActive()) {
+			throw new Error('No webview to release');
+		}
+		this._webview.value.release(claimant);
 		this._claimed = false;
 
 		// We can't render a thumbnail while the webview isn't showing, so cancel the
@@ -102,7 +163,10 @@ export class WebviewPlotClient extends Disposable implements IPositronPlotClient
 	 * Electron APIs in desktop mode) as PNG.
 	 */
 	private renderThumbnail() {
-		this.webview.captureContentsAsPng().then(data => {
+		if (!this.isActive()) {
+			throw new Error('No webview to render thumbnail');
+		}
+		this._webview.value.captureContentsAsPng().then(data => {
 			if (data) {
 				this._thumbnail = data;
 				this._onDidRenderThumbnail.fire(this.asDataUri(data));
@@ -136,6 +200,11 @@ export class WebviewPlotClient extends Disposable implements IPositronPlotClient
 	private asDataUri(buffer: VSBuffer) {
 		return `data:image/png;base64,${encodeBase64(buffer)}`;
 	}
+
+	/**
+	 * Fires when the plot has been activated.
+	 */
+	public readonly onDidActivate: Event<void>;
 
 	/**
 	 * Fires when the plot thumbnail has been rendered. The event's data is the

src/vs/workbench/contrib/positronHoloViews/browser/positronHoloViewsService.ts
@@ -3,13 +3,13 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 import { Disposable, DisposableStore } from 'vs/base/common/lifecycle';
-import { Emitter, Event } from 'vs/base/common/event';
+import { Emitter } from 'vs/base/common/event';
 import { ILanguageRuntimeMessageOutput, ILanguageRuntimeMessageWebOutput, LanguageRuntimeSessionMode, RuntimeOutputKind } from 'vs/workbench/services/languageRuntime/common/languageRuntimeService';
-import { NotebookOutputPlotClient } from 'vs/workbench/contrib/positronPlots/browser/notebookOutputPlotClient';
 import { IPositronHoloViewsService, MIME_TYPE_HOLOVIEWS_EXEC } from 'vs/workbench/services/positronHoloViews/common/positronHoloViewsService';
 import { ILanguageRuntimeSession, IRuntimeSessionService } from 'vs/workbench/services/runtimeSession/common/runtimeSessionService';
 import { InstantiationType, registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { IPositronNotebookOutputWebviewService } from 'vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebviewService';
+import { NotebookMultiMessagePlotClient } from 'vs/workbench/contrib/positronPlots/browser/notebookMultiMessagePlotClient';
 
 const MIME_TYPE_HTML = 'text/html';
 const MIME_TYPE_PLAIN = 'text/plain';
@@ -32,10 +32,10 @@ export class PositronHoloViewsService extends Disposable implements IPositronHol
 	private _sessionToDisposablesMap = new Map<string, DisposableStore>();
 
 	/** The emitter for the onDidCreatePlot event */
-	private readonly _onDidCreatePlot = this._register(new Emitter<NotebookOutputPlotClient>());
+	private readonly _onDidCreatePlot = this._register(new Emitter<NotebookMultiMessagePlotClient>());
 
 	/** Emitted when a new HoloViews webview is created. */
-	onDidCreatePlot: Event<NotebookOutputPlotClient> = this._onDidCreatePlot.event;
+	onDidCreatePlot = this._onDidCreatePlot.event;
 
 	constructor(
 		@IRuntimeSessionService private _runtimeSessionService: IRuntimeSessionService,
@@ -138,28 +138,17 @@ export class PositronHoloViewsService extends Disposable implements IPositronHol
 		runtime: ILanguageRuntimeSession,
 		displayMessage: ILanguageRuntimeMessageWebOutput,
 	) {
-		const storedMessages = this._messagesBySessionId.get(runtime.sessionId) ?? [];
-		const outputWebview = await this._notebookOutputWebviewService.createMultiMessageWebview({
-			runtime,
-			preReqMessages: storedMessages,
-			displayMessage: displayMessage,
-			viewType: 'jupyter-notebook'
-		});
-
-		if (!outputWebview) {
-			throw new Error(`PositronHoloViewsService: Could not create webview for HoloViews message: ${JSON.stringify(displayMessage)}`);
-		}
-
 		// Grab disposables for this session
 		const disposables = this._sessionToDisposablesMap.get(runtime.sessionId);
 		if (!disposables) {
 			throw new Error(`PositronHoloViewsService: Could not find disposables for session ${runtime.sessionId}`);
 		}
 
-		disposables.add(outputWebview);
-
 		// Create a plot client and fire event letting plots pane know it's good to go.
-		const client = disposables.add(new NotebookOutputPlotClient(outputWebview, displayMessage));
+		const storedMessages = this._messagesBySessionId.get(runtime.sessionId) ?? [];
+		const client = disposables.add(new NotebookMultiMessagePlotClient(
+			this._notebookOutputWebviewService, runtime, storedMessages, displayMessage,
+		));
 		this._onDidCreatePlot.fire(client);
 	}
 }

src/vs/workbench/contrib/positronIPyWidgets/browser/positronIPyWidgetsService.ts
@@ -111,19 +111,14 @@ export class PositronIPyWidgetsService extends Disposable implements IPositronIP
 				return;
 			}
 
-			// Create a webview to display the widget.
-			const webview = await this._notebookOutputWebviewService.createNotebookOutputWebview(
-				session, message, 'jupyter-notebook');
-
-			if (!webview) {
-				throw new Error(`Could not create webview for IPyWidget message: ${JSON.stringify(message)}`);
-			}
-
-			disposables.add(webview);
+			// Create the plot client.
+			const client = disposables.add(new NotebookOutputPlotClient(
+				this._notebookOutputWebviewService, session, message
+			));
 
 			// Create the ipywidgets instance.
 			const messaging = disposables.add(new IPyWidgetsWebviewMessaging(
-				webview.id, this._notebookRendererMessagingService
+				client.id, this._notebookRendererMessagingService
 			));
 			const ipywidgetsInstance = disposables.add(
 				new IPyWidgetsInstance(session, messaging, this._logService)
@@ -138,7 +133,6 @@ export class PositronIPyWidgetsService extends Disposable implements IPositronIP
 			// TODO: We probably need to dispose in more cases...
 
 			// Fire the onDidCreatePlot event.
-			const client = disposables.add(new NotebookOutputPlotClient(webview, message));
 			this._onDidCreatePlot.fire(client);
 		};
 

src/vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebview.ts
@@ -24,7 +24,6 @@ interface NotebookOutputWebviewOptions<WType extends IOverlayWebview | IWebviewE
 	readonly id: string;
 	readonly sessionId: string;
 	readonly webview: WType;
-	readonly render?: () => void;
 	rendererMessaging?: IScopedRendererMessaging;
 }
 
@@ -40,7 +39,6 @@ export class NotebookOutputWebview<WType extends IOverlayWebview | IWebviewEleme
 	readonly id: string;
 	readonly sessionId: string;
 	readonly webview: WType;
-	readonly render?: () => void;
 
 	/**
 	 * Create a new notebook output webview.
@@ -49,8 +47,6 @@ export class NotebookOutputWebview<WType extends IOverlayWebview | IWebviewEleme
 	 *   that created it.
 	 * @param sessionId The ID of the runtime that owns this webview.
 	 * @param webview The underlying webview.
-	 * @param render Optional method to render the output in the webview rather than doing so
-	 *   directly in the HTML content.
 	 * @param rendererMessaging Optional scoped messaging instance for communicating between a
 	 *   runtime and the renderer.
 	 */
@@ -59,7 +55,6 @@ export class NotebookOutputWebview<WType extends IOverlayWebview | IWebviewEleme
 			id,
 			sessionId,
 			webview,
-			render,
 			rendererMessaging
 		}: NotebookOutputWebviewOptions<WType>,
 		@IFileDialogService private _fileDialogService: IFileDialogService,
@@ -76,7 +71,6 @@ export class NotebookOutputWebview<WType extends IOverlayWebview | IWebviewEleme
 		this.id = id;
 		this.sessionId = sessionId;
 		this.webview = webview;
-		this.render = render;
 		this.onDidRender = this._onDidRender.event;
 		this._register(this._onDidRender);
 		this._register(this._onDidReceiveMessage);

src/vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebviewService.ts
@@ -28,12 +28,6 @@ export interface INotebookOutputWebview<WType extends IOverlayWebview | IWebview
 
 	/** Fired when the content completes rendering */
 	onDidRender: Event<void>;
-
-	/**
-	 * Optional method to render the output in the webview rather than doing so
-	 * directly in the HTML content
-	 */
-	render?(): void;
 }
 
 export enum WebviewType {
@@ -49,6 +43,8 @@ export interface IPositronNotebookOutputWebviewService {
 	/**
 	 * Create a new notebook output webview from an output message.
 	 *
+	 * @param id A unique ID for this webview; typically the ID of the message
+	 *  that created it.
 	 * @param runtime The runtime that emitted the output
 	 * @param output The message containing the contents to be rendered in the webview.
 	 * @param viewType The view type of the notebook e.g 'jupyter-notebook', if known. Used to
@@ -57,6 +53,7 @@ export interface IPositronNotebookOutputWebviewService {
 	 *   output does not have a suitable renderer.
 	 */
 	createNotebookOutputWebview(
+		id: string,
 		runtime: ILanguageRuntimeSession,
 		output: ILanguageRuntimeMessageOutput,
 		viewType?: string,

src/vs/workbench/contrib/positronOutputWebview/browser/notebookOutputWebviewServiceImpl.ts
@@ -3,6 +3,7 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as DOM from 'vs/base/browser/dom';
 import { VSBuffer } from 'vs/base/common/buffer';
 import { Schemas } from 'vs/base/common/network';
 import { URI } from 'vs/base/common/uri';
@@ -120,6 +121,7 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 	}
 
 	async createNotebookOutputWebview(
+		id: string,
 		runtime: ILanguageRuntimeSession,
 		output: ILanguageRuntimeMessageWebOutput,
 		viewType?: string,
@@ -137,7 +139,7 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 			const renderer = this._notebookService.getPreferredRenderer(mimeType);
 			if (renderer) {
 				return this.createNotebookRenderOutput({
-					id: output.id,
+					id,
 					runtime,
 					displayMessageInfo: { mimeType, renderer, output },
 					viewType
@@ -150,7 +152,7 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 		for (const mimeType of Object.keys(output.data)) {
 			if (mimeType === 'text/html') {
 				return this.createRawHtmlOutput({
-					id: output.id,
+					id,
 					runtimeOrSessionId: runtime,
 					html: output.data[mimeType],
 					webviewType: WebviewType.Overlay
@@ -291,6 +293,11 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 
 		// Create the metadata for the webview
 		const webviewInitInfo: WebviewInitInfo = {
+			// TODO: This is what the Viewer pane does. The back layer webview creates a UUID
+			//       per viewType. Not sure what we should do.
+			// Use the active window's origin. All webviews with the same origin will reuse the same
+			// service worker.
+			origin: DOM.getActiveWindow().origin,
 			contentOptions: {
 				allowScripts: true,
 				// Needed since we use the API ourselves, and it's also used by
@@ -302,7 +309,9 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 				// Just choose last renderer for now. This may be insufficient in the future.
 				id: displayMessageInfo.renderer.extensionId,
 			},
-			options: {},
+			options: {
+				retainContextWhenHidden: true,
+			},
 			title: '',
 		};
 
@@ -328,28 +337,26 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 				</body>
 					`);
 
-		const render = () => {
-			// Loop through all the messages and render them in the webview
-			for (const { output: message, mimeType, renderer } of messagesInfo) {
-				const data = message.data[mimeType];
-				// Send a message to the webview to render the output.
-				const valueBytes = typeof (data) === 'string' ? VSBuffer.fromString(data) :
-					VSBuffer.fromString(JSON.stringify(data));
-				// TODO: We may need to pass valueBytes.buffer (or some version of it) as the `transfer`
-				//   argument to postMessage.
-				const transfer: ArrayBuffer[] = [];
-				const webviewMessage: IPositronRenderMessage = {
-					type: 'positronRender',
-					outputId: message.id,
-					elementId: 'container',
-					rendererId: renderer.id,
-					mimeType,
-					metadata: message.metadata,
-					valueBytes: valueBytes.buffer,
-				};
-				webview.postMessage(webviewMessage, transfer);
-			}
-		};
+		// Loop through all the messages and render them in the webview
+		for (const { output: message, mimeType, renderer } of messagesInfo) {
+			const data = message.data[mimeType];
+			// Send a message to the webview to render the output.
+			const valueBytes = typeof (data) === 'string' ? VSBuffer.fromString(data) :
+				VSBuffer.fromString(JSON.stringify(data));
+			// TODO: We may need to pass valueBytes.buffer (or some version of it) as the `transfer`
+			//   argument to postMessage.
+			const transfer: ArrayBuffer[] = [];
+			const webviewMessage: IPositronRenderMessage = {
+				type: 'positronRender',
+				outputId: message.id,
+				elementId: 'container',
+				rendererId: renderer.id,
+				mimeType,
+				metadata: message.metadata,
+				valueBytes: valueBytes.buffer,
+			};
+			webview.postMessage(webviewMessage, transfer);
+		}
 
 		const scopedRendererMessaging = this._notebookRendererMessagingService.getScoped(id);
 
@@ -359,7 +366,6 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 				id,
 				sessionId: runtime.sessionId,
 				webview,
-				render,
 				rendererMessaging: scopedRendererMessaging
 			},
 		);
@@ -383,6 +389,9 @@ export class PositronNotebookOutputWebviewService implements IPositronNotebookOu
 
 		// Create the metadata for the webview
 		const webviewInitInfo: WebviewInitInfo = {
+			// Use the active window's origin. All webviews with the same origin will reuse the same
+			// service worker.
+			origin: DOM.getActiveWindow().origin,
 			contentOptions: {
 				allowScripts: true,
 				localResourceRoots: [jupyterExtension.extensionLocation]

src/vs/workbench/contrib/positronPlots/browser/components/webviewPlotInstance.tsx
@@ -26,23 +26,38 @@ interface WebviewPlotInstanceProps {
  */
 export const WebviewPlotInstance = (props: WebviewPlotInstanceProps) => {
 	const webviewRef = React.useRef<HTMLDivElement>(null);
+	const [clientIsClaimed, setClientIsClaimed] = React.useState(false);
 
 	useEffect(() => {
 		const client = props.plotClient;
 		// Only claim if the plot is visible to avoid rendering the webview when
 		// the parent view pane is collapsed.
 		if (props.visible) {
-			client.claim(this);
+			client.activate().then(() => {
+				client.claim(this);
+				setClientIsClaimed(true);
+			});
 		}
 		return () => {
 			client.release(this);
+			setClientIsClaimed(false);
 		};
 	}, [props.plotClient, props.visible]);
 
 	useEffect(() => {
-		if (webviewRef.current) {
-			props.plotClient.layoutWebviewOverElement(webviewRef.current);
+		// If the client is not claimed, do nothing.
+		// This is to avoid activating the client when it isn't claimed, which could happen
+		// if the previous effect is cleaned up before this one runs.
+		if (!clientIsClaimed) {
+			return;
 		}
+
+		const client = props.plotClient;
+		client.activate().then(() => {
+			if (webviewRef.current) {
+				client.layoutWebviewOverElement(webviewRef.current);
+			}
+		});
 	});
 
 	const style = {

src/vs/workbench/contrib/positronPlots/browser/htmlPlotClient.ts
@@ -3,38 +3,75 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { DisposableStore, MutableDisposable } from 'vs/base/common/lifecycle';
+import { URI } from 'vs/base/common/uri';
 import { WebviewPlotClient } from 'vs/workbench/contrib/positronPlots/browser/webviewPlotClient';
+import { IPositronPreviewService } from 'vs/workbench/contrib/positronPreview/browser/positronPreviewSevice';
 import { PreviewHtml } from 'vs/workbench/contrib/positronPreview/browser/previewHtml';
+import { WebviewExtensionDescription } from 'vs/workbench/contrib/webview/browser/webview';
+import { IShowHtmlUriEvent } from 'vs/workbench/services/languageRuntime/common/languageRuntimeUiClient';
+import { ILanguageRuntimeSession } from 'vs/workbench/services/runtimeSession/common/runtimeSessionService';
 
 /**
  * A Positron plot instance that contains content from an HTML file.
  */
 export class HtmlPlotClient extends WebviewPlotClient {
 
+	private readonly _html = this._register(new MutableDisposable<PreviewHtml>());
+
+	private readonly _htmlEvents = this._register(new DisposableStore());
+
 	private static _nextId = 0;
 
 	/**
-	 * Creates a new HtmlPlotClient, which wraps an HTML preview webview in an
-	 * object that can be displayed in the Plots pane.
+	 * Creates a new HtmlPlotClient, which manages the lifecycle of an HTML preview webview,
+	 * wrapped in an object that can be displayed in the Plots pane.
 	 *
-	 * @param html The webview to wrap.
+	 * @param _positronPreviewService The preview service.
+	 * @param _session The runtime session that emitted the output.
+	 * @param _event The event that triggered the preview.
 	 */
-	constructor(public readonly html: PreviewHtml) {
+	constructor(
+		private readonly _positronPreviewService: IPositronPreviewService,
+		private readonly _session: ILanguageRuntimeSession,
+		private readonly _event: IShowHtmlUriEvent) {
 		// Create the metadata for the plot.
 		super({
 			id: `plot-${HtmlPlotClient._nextId++}`,
 			parent_id: '',
 			created: Date.now(),
-			session_id: html.sessionId,
+			session_id: _session.sessionId,
 			code: '',
-		}, html.webview.webview);
+		});
+	}
 
-		// Ensure that the preview is disposed when the plot client is disposed.
-		this._register(html);
+	get uri(): URI {
+		return this._event.uri;
+	}
+
+	protected override async createWebview() {
+		if (this._html.value) {
+			throw new Error('Webview already created. Dispose the existing webview first.');
+		}
+		// Create the webview.
+		const extension = this._session.runtimeMetadata.extensionId;
+		const webviewExtension: WebviewExtensionDescription = {
+			id: extension
+		};
+		const html = this._positronPreviewService.createHtmlWebview(this._session.sessionId,
+			webviewExtension, this._event);
+		this._html.value = html;
 
 		// Render the thumbnail when the webview loads.
-		this._register(this.html.webview.onDidLoad(e => {
+		this._htmlEvents.add(html.webview.onDidLoad(e => {
 			this.nudgeRenderThumbnail();
 		}));
+
+		return html.webview.webview;
+	}
+
+	protected override disposeWebview() {
+		this._html.clear();
+		this._htmlEvents.clear();
 	}
 }

commit e34587df3bcb9ae20bb0ac699b4bc4637c40ee35

Fix webview plot disposal (#4419)
Fixes a bug introduced in #4391 where an error was thrown when trying to
`release` a webview plot client since it was already disposed. We don't
need to throw the error in that case.

src/vs/workbench/contrib/positronPlots/browser/webviewPlotClient.ts
@@ -148,7 +148,8 @@ export abstract class WebviewPlotClient extends Disposable implements IPositronP
 	 */
 	public release(claimant: any) {
 		if (!this.isActive()) {
-			throw new Error('No webview to release');
+			// Webview is already disposed so there's nothing to release.
+			return;
 		}
 		this._webview.value.release(claimant);
 		this._claimed = false;