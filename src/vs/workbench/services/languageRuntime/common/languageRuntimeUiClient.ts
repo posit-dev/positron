posit-dev/positron

commit 240a81d86244160a275539c3c0e71adf70bbcbf6

Overhaul R HTML widgets and HTML support in Viewer (#4151)
This change holistically addresses a large number of issues around
viewing R HTML widgets, and HTML in general, in the Viewer and Plots
panes. Specifically, it addresses the following issues:

https://github.com/posit-dev/positron/issues/2023
https://github.com/posit-dev/positron/issues/2145
https://github.com/posit-dev/positron/issues/2559
https://github.com/posit-dev/positron/issues/2888
https://github.com/posit-dev/positron/issues/3018
https://github.com/posit-dev/positron/issues/3678
https://github.com/posit-dev/positron/issues/3756
https://github.com/posit-dev/positron/issues/3783
https://github.com/posit-dev/positron/issues/3914
https://github.com/posit-dev/positron/issues/3968
https://github.com/posit-dev/positron/issues/4057

Prior to this change, R HTML widgets were emitted from Ark as notebook
outputs in format compatible with the [VS Code Notebook Renderer
API](https://code.visualstudio.com/api/extension-guides/notebook#notebook-renderer).
This approach was chosen because we need to be able to render these
kinds of outputs in any case (other kernels emit them), and for minimal
friction with the existing infrastructure.

However, R HTML widgets aren't really a good match for Notebook Renderer
API, and the abstraction was very leaky. In particular, the API is
really designed to be used by widgets directly -- for example, plotly
has an output renderer -- not as a layer on top of another widget
framework. It also can't be used to just render HTML output directly,
which is what a lot of R code does.

The approach taken here largely supersedes the notebook output approach
(for console sessions). It extends our existing help proxy with a new
file proxy. R HTML content is rendered to a temporary path on disk, and
then we create a proxy that serves content from the path on disk.

```mermaid
graph TD
proxy[Positron Proxy] --HTML --> pos[Positron]

src/vs/workbench/services/languageRuntime/common/languageRuntimeUiClient.ts
@@ -4,10 +4,16 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Disposable } from 'vs/base/common/lifecycle';
-import { Event } from 'vs/base/common/event';
+import { Emitter, Event } from 'vs/base/common/event';
 import { IRuntimeClientInstance } from 'vs/workbench/services/languageRuntime/common/languageRuntimeClientInstance';
-import { BusyEvent, ClearConsoleEvent, UiFrontendEvent, OpenEditorEvent, OpenWorkspaceEvent, PromptStateEvent, ShowMessageEvent, WorkingDirectoryEvent, ExecuteCommandEvent, ShowUrlEvent, SetEditorSelectionsEvent } from './positronUiComm';
+import { BusyEvent, ClearConsoleEvent, UiFrontendEvent, OpenEditorEvent, OpenWorkspaceEvent, PromptStateEvent, ShowMessageEvent, WorkingDirectoryEvent, ExecuteCommandEvent, ShowUrlEvent, SetEditorSelectionsEvent, ShowHtmlFileEvent } from './positronUiComm';
 import { PositronUiCommInstance } from 'vs/workbench/services/languageRuntime/common/positronUiCommInstance';
+import { IOpenerService } from 'vs/platform/opener/common/opener';
+import { URI } from 'vs/base/common/uri';
+import { ICommandService } from 'vs/platform/commands/common/commands';
+import { ILogService } from 'vs/platform/log/common/log';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
+import { POSITRON_PREVIEW_PLOTS_IN_VIEWER } from 'vs/workbench/contrib/positronPreview/browser/positronPreview.contribution';
 
 
 /**
@@ -52,6 +58,12 @@ export interface IUiClientMessageOutputEvent
 	extends IUiClientMessageOutput, IRuntimeClientEvent {
 }
 
+
+export interface IShowHtmlUriEvent {
+	uri: URI;
+	event: ShowHtmlFileEvent;
+}
+
 /**
  * A UI client instance. This client instance represents the global Positron window, and
  * its lifetime is tied to the lifetime of the Positron window.
@@ -73,6 +85,13 @@ export class UiClientInstance extends Disposable {
 	onDidWorkingDirectory: Event<WorkingDirectoryEvent>;
 	onDidExecuteCommand: Event<ExecuteCommandEvent>;
 	onDidShowUrl: Event<ShowUrlEvent>;
+	onDidShowHtmlFile: Event<IShowHtmlUriEvent>;
+
+	/** Emitter wrapper for Show URL events */
+	private readonly _onDidShowUrlEmitter = this._register(new Emitter<ShowUrlEvent>());
+
+	/** Emitter wrapper for Show HTML File events */
+	private readonly _onDidShowHtmlFileEmitter = this._register(new Emitter<IShowHtmlUriEvent>());
 
 	/**
 	 * Creates a new frontend client instance.
@@ -82,6 +101,10 @@ export class UiClientInstance extends Disposable {
 	 */
 	constructor(
 		private readonly _client: IRuntimeClientInstance<any, any>,
+		private readonly _commandService: ICommandService,
+		private readonly _logService: ILogService,
+		private readonly _openerService: IOpenerService,
+		private readonly _configurationService: IConfigurationService,
 	) {
 		super();
 		this._register(this._client);
@@ -96,6 +119,68 @@ export class UiClientInstance extends Disposable {
 		this.onDidPromptState = this._comm.onDidPromptState;
 		this.onDidWorkingDirectory = this._comm.onDidWorkingDirectory;
 		this.onDidExecuteCommand = this._comm.onDidExecuteCommand;
-		this.onDidShowUrl = this._comm.onDidShowUrl;
+		this.onDidShowUrl = this._onDidShowUrlEmitter.event;
+		this.onDidShowHtmlFile = this._onDidShowHtmlFileEmitter.event;
+
+		// Wrap the ShowUrl event to resolve incoming external URIs from the
+		// backend before broadcasting them to the frontend.
+		this._register(this._comm.onDidShowUrl(async e => {
+			try {
+				let uri = URI.parse(e.url);
+				try {
+					const resolvedUri = await this._openerService.resolveExternalUri(uri);
+					uri = resolvedUri.resolved;
+				} catch {
+					// Noop; use the original URI
+				}
+				const resolvedEvent: ShowUrlEvent = {
+					url: uri.toString(),
+				};
+				this._onDidShowUrlEmitter.fire(resolvedEvent);
+			} catch {
+				this._onDidShowUrlEmitter.fire(e);
+			}
+		}));
+
+		// Wrap the ShowHtmlFile event to start a proxy server for the HTML file.
+		this._register(this._comm.onDidShowHtmlFile(async e => {
+			try {
+				const url = await this._commandService.executeCommand<string>(
+					'positronProxy.startHtmlProxyServer',
+					e.path
+				);
+
+				if (!url) {
+					throw new Error('Failed to start HTML file proxy server');
+				}
+
+				let uri = URI.parse(url);
+				try {
+					const resolvedUri = await this._openerService.resolveExternalUri(uri);
+					uri = resolvedUri.resolved;
+				} catch {
+					// Noop; use the original URI
+				}
+
+				if (e.is_plot) {
+					// Check the configuration to see if we should open the plot
+					// in the Viewer tab. If so, clear the `is_plot` flag so that
+					// we open the file in the Viewer.
+					const openInViewer = this._configurationService.getValue<boolean>(POSITRON_PREVIEW_PLOTS_IN_VIEWER);
+					if (openInViewer) {
+						e.is_plot = false;
+					}
+				}
+
+				const resolvedEvent: IShowHtmlUriEvent = {
+					uri,
+					event: e,
+				};
+
+				this._onDidShowHtmlFileEmitter.fire(resolvedEvent);
+			} catch (error) {
+				this._logService.error(`Failed to show HTML file ${e.path}: ${error}`);
+			}
+		}));
 	}
 }

extensions/positron-proxy/src/extension.ts
@@ -1,10 +1,12 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2022 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2022-2024 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
+import * as positron from 'positron';
 import { PositronProxy } from './positronProxy';
+import path from 'path';
 
 /**
  * ProxyServerStyles type.
@@ -27,6 +29,14 @@ export function activate(context: vscode.ExtensionContext) {
 		)
 	);
 
+	// Register the positronProxy.startHtmlProxyServer command and add its disposable.
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'positronProxy.startHtmlProxyServer',
+			async (targetPath: string) => await positronProxy.startHtmlProxyServer(targetPath)
+		)
+	);
+
 	// Register the positronProxy.stopHelpProxyServer command and add its disposable.
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
@@ -41,6 +51,15 @@ export function activate(context: vscode.ExtensionContext) {
 			'positronProxy.setHelpProxyServerStyles',
 			(styles: ProxyServerStyles) => positronProxy.setHelpProxyServerStyles(styles)
 		)
+	);
+
+	// Register the positronProxy.showHtmlPreview command and add its disposable.
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'positronProxy.showHtmlPreview',
+			(path: vscode.Uri) => {
+				positron.window.previewHtml(path.toString());
+			})
 	);
 
 	// Add the PositronProxy object disposable.

extensions/positron-proxy/src/htmlProxy.ts
@@ -1,0 +1,96 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import express from 'express';
+import path = require('path');
+import fs = require('fs');
+
+import { Disposable, Uri } from 'vscode';
+import { PromiseHandles } from './util';
+import { isAddressInfo } from './positronProxy';
+
+/**
+ * HtmlProxyServer class.
+ *
+ * HtmlProxyServer wraps an Express server that serves static HTML content. All
+ * the content is served by the same server, but each piece of content is served
+ * from a unique path.
+ */
+export class HtmlProxyServer implements Disposable {
+	private readonly _app = express();
+	private readonly _server;
+
+	private readonly _paths = new Map<string, string>();
+	private readonly _ready: PromiseHandles<void> = new PromiseHandles();
+
+	/**
+	 * Construct a new HtmlProxyServer; creates the server and listens on a
+	 * random port on localhost.
+	 */
+	constructor() {
+		this._server = this._app.listen(0, 'localhost', () => {
+			this._ready.resolve();
+		});
+	}
+
+	/**
+	 * Creates a unique URL that serves the content at the specified path.
+	 *
+	 * @param targetPath The path to the content to serve. May be specified as a
+	 * file URI or a plain path, and may be a file or a directory. When a file
+	 * is specified, the parent directory is served and the filename is appended
+	 * to the URL.
+	 * @returns A URL that serves the content at the specified path.
+	 */
+	public async createHtmlProxy(targetPath: string): Promise<string> {
+		// Wait for the server to be ready.
+		await this._ready.promise;
+
+		// The targetPath may be specified as a file URI or a file path. If it's
+		// a file URI, convert it to a file path first.
+		try {
+			const uri = Uri.parse(targetPath);
+			if (uri.scheme === 'file') {
+				targetPath = uri.fsPath;
+			}
+		} catch {
+			// Ignore parse failures; expected when the target path is not a
+			// URI.
+		}
+
+		// Ensure the target path exists.
+		if (!fs.existsSync(targetPath)) {
+			throw new Error(`Path does not exist: ${targetPath}`);
+		}
+
+		// Generate a random 8-character hex string to use as the path, and
+		// ensure it's unique.
+		let serverPath = '';
+		do {
+			serverPath = Math.random().toString(16).substring(2, 10);
+		} while (this._paths.has(serverPath));
+
+		// Is the target path a file, or a directory? If it's a file, we'll
+		// serve the parent directory and then amend the filename to the URL.
+		let filename = '';
+		const isFile = fs.statSync(targetPath).isFile();
+		if (isFile) {
+			filename = path.basename(targetPath);
+			targetPath = path.dirname(targetPath);
+		}
+
+		// Create a new path entry.
+		this._app.use(`/${serverPath}`, express.static(targetPath));
+		const address = this._server.address();
+		if (!isAddressInfo(address)) {
+			throw new Error(`Server address is not available; cannot serve ${targetPath}`);
+		}
+		return `http://${address.address}:${address.port}/${serverPath}/${filename}`;
+	}
+
+	dispose() {
+		this._server.close();
+	}
+}

extensions/positron-proxy/src/positronProxy.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2022 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2022-2024 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -10,6 +10,7 @@ import { AddressInfo, Server } from 'net';
 import { ProxyServerStyles } from './extension';
 import { Disposable, ExtensionContext } from 'vscode';
 import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
+import { HtmlProxyServer } from './htmlProxy';
 
 /**
  * Constants.
@@ -55,7 +56,7 @@ type ContentRewriter = (
  * @param addressInfo The value.
  * @returns true if the value is aAddressInfo AddressInfo; otherwise, false.
  */
-const isAddressInfo = (addressInfo: string | AddressInfo | null): addressInfo is AddressInfo =>
+export const isAddressInfo = (addressInfo: string | AddressInfo | null): addressInfo is AddressInfo =>
 	(addressInfo as AddressInfo).address !== undefined &&
 	(addressInfo as AddressInfo).family !== undefined &&
 	(addressInfo as AddressInfo).port !== undefined;
@@ -68,13 +69,11 @@ export class ProxyServer implements Disposable {
 	 * Constructor.
 	 * @param serverOrigin The server origin.
 	 * @param targetOrigin The target origin.
-	 * @param type The type. (Right now, only help is supported.)
 	 * @param server The server.
 	 */
 	constructor(
 		readonly serverOrigin: string,
 		readonly targetOrigin: string,
-		private readonly type: 'help',
 		private readonly server: Server,
 	) {
 	}
@@ -123,6 +122,12 @@ export class PositronProxy implements Disposable {
 	 */
 	private _proxyServers = new Map<string, ProxyServer>();
 
+	/**
+	 * The HTML proxy server. There's only ever one of these; it serves all raw
+	 * HTML content.
+	 */
+	private _htmlProxyServer?: HtmlProxyServer;
+
 	//#endregion Private Properties
 
 	//#region Constructor & Dispose
@@ -161,6 +166,9 @@ export class PositronProxy implements Disposable {
 		this._proxyServers.forEach(proxyServer => {
 			proxyServer.dispose();
 		});
+		if (this._htmlProxyServer) {
+			this._htmlProxyServer.dispose();
+		}
 	}
 
 	//#endregion Constructor & Dispose
@@ -241,6 +249,18 @@ export class PositronProxy implements Disposable {
 	}
 
 	/**
+	 * Starts a proxy server to server local HTML content.
+	 * @param targetPath The target path
+	 * @returns The server URL.
+	 */
+	async startHtmlProxyServer(targetPath: string) {
+		if (!this._htmlProxyServer) {
+			this._htmlProxyServer = new HtmlProxyServer();
+		}
+		return this._htmlProxyServer.createHtmlProxy(targetPath);
+	}
+
+	/**
 	 * Sets the help proxy server styles.
 	 * @param styles The help proxy server styles.
 	 */
@@ -290,7 +310,6 @@ export class PositronProxy implements Disposable {
 				this._proxyServers.set(targetOrigin, new ProxyServer(
 					serverOrigin,
 					targetOrigin,
-					'help',
 					server
 				));
 

extensions/positron-proxy/src/util.ts
@@ -1,0 +1,24 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+
+/**
+ * PromiseHandles is a class that represents a promise that can be resolved or
+ * rejected externally.
+ */
+export class PromiseHandles<T> {
+	resolve!: (value: T | Promise<T>) => void;
+
+	reject!: (error: unknown) => void;
+
+	promise: Promise<T>;
+
+	constructor() {
+		this.promise = new Promise((resolve, reject) => {
+			this.resolve = resolve;
+			this.reject = reject;
+		});
+	}
+}

extensions/positron-python/python_files/positron/positron_ipykernel/ui_comm.py
@@ -219,6 +219,9 @@ class UiFrontendEvent(str, enum.Enum):
     # Show a URL in Positron's Viewer pane
     ShowUrl = "show_url"
 
+    # Show an HTML file in Positron
+    ShowHtmlFile = "show_html_file"
+
 
 class BusyParams(BaseModel):
     """
@@ -422,6 +425,28 @@ class ShowUrlParams(BaseModel):
     )
 
 
+class ShowHtmlFileParams(BaseModel):
+    """
+    Show an HTML file in Positron
+    """
+
+    path: StrictStr = Field(
+        description="The fully qualified filesystem path to the HTML file to display",
+    )
+
+    title: StrictStr = Field(
+        description="A title to be displayed in the viewer. May be empty, and can be superseded by the title in the HTML file.",
+    )
+
+    is_plot: StrictBool = Field(
+        description="Whether the HTML file is a plot-like object",
+    )
+
+    height: StrictInt = Field(
+        description="The desired height of the HTML viewer, in pixels. The special value 0 indicates that no particular height is desired, and -1 indicates that the viewer should be as tall as possible.",
+    )
+
+
 EditorContext.update_forward_refs()
 
 TextDocument.update_forward_refs()
@@ -465,3 +490,5 @@ SetEditorSelectionsParams.update_forward_refs()
 ModifyEditorSelectionsParams.update_forward_refs()
 
 ShowUrlParams.update_forward_refs()
+
+ShowHtmlFileParams.update_forward_refs()

src/positron-dts/positron.d.ts
@@ -1070,6 +1070,17 @@ declare module 'positron' {
 		export function previewUrl(url: vscode.Uri): PreviewPanel;
 
 		/**
+		 * Create and show a new preview panel for an HTML file. This is a
+		 * convenience method that creates a new webview panel and sets its
+		 * content to that of the given file.
+		 *
+		 * @param path The fully qualified path to the HTML file to preview
+		 *
+		 * @return New preview panel.
+		 */
+		export function previewHtml(path: string): PreviewPanel;
+
+		/**
 		 * Create a log output channel from raw data.
 		 *
 		 * Variant of `createOutputChannel()` that creates a "raw log" output channel.

src/vs/workbench/api/browser/positron/mainThreadPreviewPanel.ts
@@ -120,9 +120,14 @@ export class MainThreadPreviewPanel extends Disposable implements extHostProtoco
 
 		const extension = reviveWebviewExtension(extensionData);
 		const targetUri = URI.revive(uri);
-		const origin = this.webviewOriginStore.getOrigin('positron.previewUrl', extension.id);
-		const preview = this._positronPreviewService.openUri(handle, origin, extension, targetUri);
+		const preview = this._positronPreviewService.openUri(handle, extension, targetUri);
 
+		this.attachPreview(handle, preview);
+	}
+
+	async $previewHtml(extensionData: WebviewExtensionDescription, handle: string, path: string): Promise<void> {
+		const extension = reviveWebviewExtension(extensionData);
+		const preview = await this._positronPreviewService.openHtml(handle, extension, path);
 		this.attachPreview(handle, preview);
 	}
 
@@ -185,7 +190,7 @@ export class MainThreadPreviewPanel extends Disposable implements extHostProtoco
 
 	public addWebview(handle: extHostProtocol.PreviewHandle, preview: PreviewWebview): void {
 		this._previews.add(handle, preview);
-		this._mainThreadWebviews.addWebview(handle, preview.webview,
+		this._mainThreadWebviews.addWebview(handle, preview.webview.webview,
 			{
 				// This is the standard for extensions built for VS Code
 				// 1.57.0 and above (see `shouldSerializeBuffersForPostMessage`).

commit 9faaed123852aa8b2e2b1ac35273abf4bbb1a84a

Open file urls in external browser (#4468)
This change causes certain URLs to HTML files to open in an external
browser rather than opening up the HTML file in Positron's editor,
addressing a regression from #4151.

In the aforementioned change, we added some machinery that automatically
transforms and proxies URLs that arrive from language runtimes. This is
intended for http URLs, but also runs for file URLs, resulting in those
files being opened in the editor instead of an external browser.
Probably OK if e.g. a runtime asks us to show a `.cpp` file, but not
cool for HTML content.

The fix is to check for this situation specifically; when we're handed a
file URI that looks like HTML, we start a proxy for it and then open it
externally.

I'm not sure it's always right to open these in an external browser
(maybe sometimes we'll want the Viewer pane?) but it's what people
expect for scenarios where this is broken now, so we can start here and
see how it feels.

Addresses https://github.com/posit-dev/positron/issues/4297.

### QA Notes

- It's worth double checking a couple of the scenarios from #4151 to
make sure all that good stuff still works.
- This change is also intended to work on Workbench and on remote SSH
scenarios where we can't just load the file URL directly since it's on
the remote host.

src/vs/workbench/services/languageRuntime/common/languageRuntimeUiClient.ts
@@ -125,6 +125,23 @@ export class UiClientInstance extends Disposable {
 		this._register(this._comm.onDidShowUrl(async e => {
 			try {
 				let uri = URI.parse(e.url);
+
+				// If this is an HTML file URI, then treat it as a local file to
+				// be opened in a browser
+				if (uri.scheme === 'file') {
+					// Does the URI point to a plain directory (presumably
+					// containing an index.html of some kind), or a specific
+					// HTML file?  (lowercase to be case-insensitive)
+					const uriPath = uri.path.toLowerCase();
+					if (uriPath.endsWith('/') ||
+						uriPath.endsWith('.html') ||
+						uriPath.endsWith('.htm')) {
+						this.openHtmlFile(e.url);
+					}
+					return;
+				}
+
+				// Resolve the URI if it is an external URI
 				try {
 					const resolvedUri = await this._openerService.resolveExternalUri(uri);
 					uri = resolvedUri.resolved;
@@ -143,22 +160,8 @@ export class UiClientInstance extends Disposable {
 		// Wrap the ShowHtmlFile event to start a proxy server for the HTML file.
 		this._register(this._comm.onDidShowHtmlFile(async e => {
 			try {
-				const url = await this._commandService.executeCommand<string>(
-					'positronProxy.startHtmlProxyServer',
-					e.path
-				);
-
-				if (!url) {
-					throw new Error('Failed to start HTML file proxy server');
-				}
-
-				let uri = URI.parse(url);
-				try {
-					const resolvedUri = await this._openerService.resolveExternalUri(uri);
-					uri = resolvedUri.resolved;
-				} catch {
-					// Noop; use the original URI
-				}
+				// Start an HTML proxy server for the file
+				const uri = await this.startHtmlProxyServer(e.path);
 
 				if (e.is_plot) {
 					// Check the configuration to see if we should open the plot
@@ -180,5 +183,48 @@ export class UiClientInstance extends Disposable {
 				this._logService.error(`Failed to show HTML file ${e.path}: ${error}`);
 			}
 		}));
+	}
+
+	/**
+	 * Opens a file URI in an external browser.
+	 *
+	 * @param url The URL to open in the browser
+	 */
+	private async openHtmlFile(url: string): Promise<void> {
+		// Start an HTML proxy server for the file
+		const resolved = await this.startHtmlProxyServer(url);
+
+		// Open the resolved URI in the external browser. (Consider: should
+		// _all_ file URIs be opened in the external browser?)
+		this._openerService.open(resolved.toString(), {
+			openExternal: true,
+		});
+	}
+
+	/**
+	 * Starts an HTML proxy server for the given HTML file.
+	 *
+	 * @param htmlPath The path to the HTML file to open
+	 * @returns A URI representing the HTML file
+	 */
+	private async startHtmlProxyServer(htmlPath: string): Promise<URI> {
+		const url = await this._commandService.executeCommand<string>(
+			'positronProxy.startHtmlProxyServer',
+			htmlPath
+		);
+
+		if (!url) {
+			throw new Error('Failed to start HTML file proxy server');
+		}
+
+		let uri = URI.parse(url);
+		try {
+			const resolvedUri = await this._openerService.resolveExternalUri(uri);
+			uri = resolvedUri.resolved;
+		} catch {
+			// Noop; use the original URI
+		}
+
+		return uri;
 	}
 }