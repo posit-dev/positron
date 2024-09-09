acd89fd99de4a7962074573ca162ca342d6078c7
seem <mwlorgat@gmail.com>
2024-09-09 19:49:56 +0200

    Revert "wip: terminal app service"
    This reverts commit 27d4c48fb3b4f694273b5859daa843e5b0426689.

--- src/vs/workbench/api/browser/positron/mainThreadLanguageRuntime.ts
+++ src/vs/workbench/api/browser/positron/mainThreadLanguageRuntime.ts
@@ -40,7 +40,6 @@ import { IRuntimeStartupService, RuntimeStartupPhase } from 'vs/workbench/servic
 import { SerializableObjectWithBuffers } from 'vs/workbench/services/extensions/common/proxyIdentifier';
 import { isWebviewReplayMessage } from 'vs/workbench/contrib/positronWebviewPreloads/browser/utils';
 import { IPositronWebviewPreloadService } from 'vs/workbench/services/positronWebviewPreloads/common/positronWebviewPreloadService';
-import { IPositronTerminalAppService } from 'vs/workbench/services/positronTerminalApp/common/positronTerminalAppService';
 
 /**
  * Represents a language runtime event (for example a message or state change)
@@ -1136,7 +1135,6 @@ export class MainThreadLanguageRuntime
 		@IPositronPlotsService private readonly _positronPlotService: IPositronPlotsService,
 		@IPositronIPyWidgetsService private readonly _positronIPyWidgetsService: IPositronIPyWidgetsService,
 		@IPositronWebviewPreloadService private readonly _positronWebviewPreloadService: IPositronWebviewPreloadService,
-		@IPositronTerminalAppService private readonly _positronTerminalAppService: IPositronTerminalAppService,
 		@INotificationService private readonly _notificationService: INotificationService,
 		@ILogService private readonly _logService: ILogService,
 		@ICommandService private readonly _commandService: ICommandService,
@@ -1153,7 +1151,6 @@ export class MainThreadLanguageRuntime
 		this._positronPlotService.initialize();
 		this._positronIPyWidgetsService.initialize();
 		this._positronWebviewPreloadService.initialize();
-		this._positronTerminalAppService.initialize();
 		this._proxy = extHostContext.getProxy(ExtHostPositronContext.ExtHostLanguageRuntime);
 		this._id = MainThreadLanguageRuntime.MAX_ID++;
 
