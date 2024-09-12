3fca1e26e8444778cdeafe02e55efe9fe4b50f46
Chris Mead <chris.mead@posit.co>
2024-09-12 11:28:31 -0600

    Test in CI

--- test/smoke/src/areas/positron/dataexplorer/veryLargeDataFrame.test.ts
+++ test/smoke/src/areas/positron/dataexplorer/veryLargeDataFrame.test.ts
@@ -8,7 +8,7 @@ import { expect } from '@playwright/test';
 import { Application, Logger, PositronPythonFixtures, PositronRFixtures } from '../../../../../automation';
 import { installAllHandlers } from '../../../utils';
 import { join } from 'path';
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand, GetObjectCommandOutput } from "@aws-sdk/client-s3";
 import { createWriteStream } from "fs";
 import { pipeline } from "stream";
 import { promisify } from "util";
@@ -18,10 +18,49 @@ import { promisify } from "util";
  */
 export function setup(logger: Logger) {
 
+	// AWS Configuration
+	const region = "us-west-2";
+	const bucketName = "positron-qa-data-files";
+	const objectKey = "largeParquet.parquet";
+
 	describe('Data Explorer - Very Large Data Frame', () => {
 
 		// Shared before/after handling
 		installAllHandlers(logger);
+
+		before(async function () {
+
+			// debug locally - set to your profile name
+			// process.env.AWS_PROFILE = 'my-dev-profile';
+
+			const localFilePath = join(this.app.workspacePathOrFolder, "data-files", objectKey);
+
+			const s3 = new S3Client({ region });
+
+			const command = new GetObjectCommand({
+				Bucket: bucketName,
+				Key: objectKey,
+			});
+
+			let response: GetObjectCommandOutput = {
+				$metadata: {}
+			};
+			try {
+				response = await s3.send(command);
+			} catch (error) {
+				console.error("Error:", (error as any).message, (error as any).stack);
+			}
+
+			if (!response.Body || !("pipe" in response.Body)) {
+				throw new Error("Unexpected response from S3: Body is not a stream");
+			}
+
+			const fileStream = createWriteStream(localFilePath);
+			const streamPipeline = promisify(pipeline);
+			await streamPipeline(response.Body, fileStream);
+
+		});
+
 
 		describe('Python Data Explorer (Very Large Data Frame)', () => {
 
@@ -86,48 +125,6 @@ export function setup(logger: Logger) {
 			it('R - Verifies data explorer functionality with very large data frame [C...] #pr', async function () {
 				const app = this.app as Application;
 				await app.workbench.quickaccess.openFile(join(app.workspacePathOrFolder, 'workspaces', 'performance', 'loadBigParquet.r'));
-
-
-				//debug
-				process.env.AWS_PROFILE = 'my-dev-profile';
-
-				// Configuration
-				const region = "us-west-2";
-				const bucketName = "positron-qa-data-files";
-				const objectKey = "largeParquet.parquet";
-				const localFilePath = join(app.workspacePathOrFolder, "data-files", "largeParquet.parquet");
-
-				// Create an S3 client and specify the SSO profile via environment variables
-				const s3 = new S3Client({
-					region
-				});
-
-				// Create the S3 GetObject command
-				const command = new GetObjectCommand({
-					Bucket: bucketName,
-					Key: objectKey,
-				});
-
-				// Execute the command and handle the response
-				let response;
-				try {
-					response = await s3.send(command);
-				} catch (error) {
-					console.error("Error:", (error as any).message, (error as any).stack);
-				}
-
-				// Check if the Body is a stream (for large files)
-				if (!response.Body || !("pipe" in response.Body)) {
-					throw new Error("Unexpected response from S3: Body is not a stream");
-				}
-
-				// Create a write stream to the local file
-				const fileStream = createWriteStream(localFilePath);
-
-				// Use pipeline to pipe the response stream to the file stream
-				const streamPipeline = promisify(pipeline);
-				await streamPipeline(response.Body, fileStream);
-				console.log("File downloaded successfully!");
 
 				const startTime = performance.now();
 
