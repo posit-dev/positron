70bed381c7bb389baa88a133c144f9ff933b83a8
Julia Silge <julia.silge@gmail.com>
2024-09-17 16:00:41 -0600

    Change name of env var to `POSITRON_MODE`

--- extensions/jupyter-adapter/src/JupyterKernel.ts
+++ extensions/jupyter-adapter/src/JupyterKernel.ts
@@ -730,7 +730,7 @@ export class JupyterKernel extends EventEmitter implements vscode.Disposable {
 			POSITRON: '1',
 			POSITRON_VERSION: positron.version,
 			POSITRON_LONG_VERSION: `${positron.version}+${positron.buildNumber}`,
-			POSITRON_UI_KIND: vscode.env.uiKind === vscode.UIKind.Desktop ? 'desktop' : 'web',
+			POSITRON_MODE: vscode.env.uiKind === vscode.UIKind.Desktop ? 'desktop' : 'web',
 		};
 		Object.assign(env, process.env, this._spec.env);
 
