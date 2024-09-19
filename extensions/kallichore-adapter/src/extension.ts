f8b63be64609447bb08c2819ce74768378abe5e1
Jonathan McPherson <jonathan@rstudio.com>
2024-09-18 17:12:07 -0700

    start kcserver at startup

--- extensions/kallichore-adapter/src/extension.ts
+++ extensions/kallichore-adapter/src/extension.ts
@@ -4,8 +4,30 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
+import * as positron from 'positron';
+import * as os from 'os';
 
-export function activate(_context: vscode.ExtensionContext) {
+import path = require('path');
+
+export function activate(context: vscode.ExtensionContext) {
+	// TODO: re-use existing terminal instead of opening a new one every time;
+	// can do this by attempting a network reconnect
+	const kcExeName = os.platform() === 'win32' ? 'kcserver.exe' : 'kcserver';
+	const shellPath = path.join(context.extensionPath, 'resources', 'kallichore', kcExeName);
+	const env = {
+		POSITRON: '1',
+		POSITRON_VERSION: positron.version,
+		RUST_LOG: 'debug'
+	};
+	vscode.window.createTerminal(<vscode.TerminalOptions>{
+		name: 'Kallichore',
+		shellPath: shellPath,
+		shellArgs: [],
+		env,
+		message: '',
+		hideFromUser: false,
+		isTransient: false
+	});
 }
 
 export function deactivate() {
