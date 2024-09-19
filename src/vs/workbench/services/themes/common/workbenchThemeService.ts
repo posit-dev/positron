4022fb5864b23ff123b3796cd7caf3d4fc24ab33
Brian Lambert <brianlambert@gmail.com>
2024-09-19 16:40:03 -0700

    Updates for Positron Light and Positron Dark themes

--- src/vs/workbench/services/themes/common/workbenchThemeService.ts
+++ src/vs/workbench/services/themes/common/workbenchThemeService.ts
@@ -44,8 +44,10 @@ export enum ThemeSettings {
 }
 
 export enum ThemeSettingDefaults {
-	COLOR_THEME_DARK = 'Default Dark Modern',
-	COLOR_THEME_LIGHT = 'Default Light Modern',
+	// --- Start Positron ---
+	COLOR_THEME_DARK = 'Default Positron Dark',
+	COLOR_THEME_LIGHT = 'Default Positron Light',
+	// --- End Positron ---
 	COLOR_THEME_HC_DARK = 'Default High Contrast',
 	COLOR_THEME_HC_LIGHT = 'Default High Contrast Light',
 
