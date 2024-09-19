3dcbefed8fea90100283b2151961bc39209e9b1f
Jonathan McPherson <jonathan@rstudio.com>
2024-09-19 14:40:41 -0700

    pick an unused port to run kallichore

--- extensions/kallichore-adapter/src/extension.ts
+++ extensions/kallichore-adapter/src/extension.ts
@@ -8,8 +8,20 @@ import * as positron from 'positron';
 import * as os from 'os';
 
 import path = require('path');
+import { DefaultApi } from './kcclient/api';
+import { findAvailablePort } from './PortFinder';
 
 export function activate(context: vscode.ExtensionContext) {
+	const log = vscode.window.createOutputChannel('Kallichore Adapter', { log: true });
+	log.debug('Kallichore Adapter activated');
+	startKallichore(context, log).then(() => {
+		log.debug('Kallichore started');
+	});
+}
+
+async function startKallichore(context: vscode.ExtensionContext,
+	log: vscode.LogOutputChannel
+) {
 	// TODO: re-use existing terminal instead of opening a new one every time;
 	// can do this by attempting a network reconnect
 	const kcExeName = os.platform() === 'win32' ? 'kcserver.exe' : 'kcserver';
@@ -19,15 +31,28 @@ export function activate(context: vscode.ExtensionContext) {
 		POSITRON_VERSION: positron.version,
 		RUST_LOG: 'debug'
 	};
+
+
+	// Find a port for the server to listen on
+	const port = await findAvailablePort([], 10);
+
+	log.info(`Starting Kallichore server ${shellPath} on port ${port}`);
 	vscode.window.createTerminal(<vscode.TerminalOptions>{
 		name: 'Kallichore',
 		shellPath: shellPath,
-		shellArgs: [],
+		shellArgs: ['--port', port.toString()],
 		env,
 		message: '',
 		hideFromUser: false,
 		isTransient: false
 	});
+	// wait 1s for the server to start up
+	setTimeout(() => {
+		const api = new DefaultApi(`http://localhost:${port}`);
+		api.listSessions().then(sessions => {
+			log.info(`Kallichore server online with ${sessions.body.total} sessions`);
+		});
+	}, 1000);
 }
 
 export function deactivate() {
