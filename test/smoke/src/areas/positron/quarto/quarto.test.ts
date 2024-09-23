a84ddb04887c0aee5e052f443a3b8118efebfc4f
Marie Idleman <marie.idleman@posit.co>
2024-09-23 10:17:04 -0500

    tinytex in ci

--- test/smoke/src/areas/positron/quarto/quarto.test.ts
+++ test/smoke/src/areas/positron/quarto/quarto.test.ts
@@ -3,14 +3,14 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Application, Logger, TerminalCommandId } from '../../../../../automation';
+import { Application, Logger } from '../../../../../automation';
 import { installAllHandlers } from '../../../utils';
 import { expect } from '@playwright/test';
 const path = require('path');
 const fs = require('fs-extra');
 
 export function setup(logger: Logger) {
-	describe('Quarto', () => {
+	describe('Quarto #pr', () => {
 
 		installAllHandlers(logger);
 
@@ -19,7 +19,6 @@ export function setup(logger: Logger) {
 		before(async function () {
 			app = this.app as Application;
 
-			await installTinyTex(app);
 			await app.workbench.quickaccess.openFile(path.join(app.workspacePathOrFolder, 'workspaces', 'quarto_basic', 'quarto_basic.qmd'));
 		});
 
@@ -81,12 +80,4 @@ const deleteGeneratedFiles = async (app: Application) => {
 const fileExists = (app: Application, file: String) => {
 	const filePath = path.join(app.workspacePathOrFolder, 'workspaces', 'quarto_basic', file);
 	return fs.pathExists(filePath);
-};
-
-const installTinyTex = async (app: Application) => {
-	// ensure tinytex is installed (needed for LaTeX rendering)
-	await app.workbench.quickaccess.runCommand('terminal.focus.view');
-	await app.workbench.terminal.runCommandInTerminal('quarto install tinytex');
-	await app.workbench.terminal.waitForTerminalText(buffer => buffer.some(line => line.includes('Installation successful') || line.includes('tinytex is already installed')));
-	await app.workbench.terminal.runCommand(TerminalCommandId.KillAll);
 };
