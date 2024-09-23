90b6806947f3f7056798b98f547cfe20f437621b
Wasim Lorgat <mwlorgat@gmail.com>
2024-09-23 22:01:57 +0200

    Support local Python application development (#4662)
    This PR adds support for local Python application development for the
    following frameworks:
    
    * Streamlit
    * Dash
    * Gradio
    * FastAPI
    * Flask
    
    This is facilitated by a new extension: `positron-run-app`.
    
    Addresses https://github.com/posit-dev/positron/issues/4557,
    https://github.com/posit-dev/positron/issues/4555, #3023, #3027, #3024.
    
    https://github.com/user-attachments/assets/d72e5028-a8c9-4aab-b927-4413c4e74b24
    
    ### QA notes
    
    1. Open an application developed using one of the supported frameworks.
    2. Run the relevant "Python: Run X App in Terminal" command. It should
    also be the default action if you click the run button in the editor
    menu.
    3. It should create a terminal named after the framework, and
    automatically show the app in the Viewer pane once its ready.
    
    Here are example apps for all of the supported frameworks:
    
    #### Streamlit
    
    ```python
    import streamlit as st
    x = st.slider('x')
    st.write(x, 'squared is', x * x)
    ```
    
    #### Dash
    
    ```python
    from dash import Dash, html
    app = Dash()
    app.layout = [html.Div(children='Hello World')]
    if __name__ == '__main__':
        app.run(debug=True)
    ```
    
    #### Gradio
    
    ```python
    import gradio as gr
    def image_classifier(inp):
        return {'cat': 0.3, 'dog': 0.7}
    demo = gr.Interface(fn=image_classifier, inputs="image", outputs="label")
    demo.launch()
    ```
    
    #### FastAPI
    
    ```python
    from fastapi import FastAPI
    app = FastAPI()
    @app.get("/")
    async def root():
        return {"message": "Hello World"}
    ```
    
    #### Flask
    
    ```python
    from flask import Flask
    app = Flask(__name__)
    @app.route("/")
    def hello_world():
        return "<p>Hello, World!</p>"
    ```
    
    ### Next steps: Workbench integration
    
    To support Workbench, we'll need to update `runApplication` to detect if
    we're in Workbench, and if so, find a free port and corresponding
    proxied URL (possibly related to
    https://github.com/posit-dev/positron/issues/4274) and pass them both to
    `getTerminalOptions`.

---
+++ extensions/positron-python/src/client/positron/webAppCommands.ts
@@ -0,0 +1,220 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as path from 'path';
+import * as vscode from 'vscode';
+import { PositronRunApp, RunAppTerminalOptions } from '../positron-run-app.d';
+import { IServiceContainer } from '../ioc/types';
+import { IInterpreterService } from '../interpreter/contracts';
+import { IInstaller, Product } from '../common/types';
+import { traceError } from '../logging';
+import { Commands } from '../common/constants';
+
+export function activateWebAppCommands(serviceContainer: IServiceContainer, disposables: vscode.Disposable[]): void {
+    disposables.push(
+        vscode.commands.registerCommand(Commands.Exec_Dash_In_Terminal, async () => {
+            const runAppApi = await getPositronRunAppApi();
+            await runAppApi.runApplication({
+                name: 'Dash',
+                getTerminalOptions(runtime, document, port, urlPrefix) {
+                    const terminalOptions: RunAppTerminalOptions = {
+                        commandLine: [runtime.runtimePath, document.uri.fsPath].join(' '),
+                    };
+                    if (port || urlPrefix) {
+                        terminalOptions.env = {};
+                        if (port) {
+                            terminalOptions.env.DASH_PORT = port;
+                        }
+                        if (urlPrefix) {
+                            terminalOptions.env.DASH_URL_PREFIX = urlPrefix;
+                        }
+                    }
+                    return terminalOptions;
+                },
+            });
+        }),
+
+        vscode.commands.registerCommand(Commands.Exec_FastAPI_In_Terminal, async () => {
+            const runAppApi = await getPositronRunAppApi();
+            await runAppApi.runApplication({
+                name: 'FastAPI',
+                async getTerminalOptions(runtime, document, port, urlPrefix) {
+                    let hasFastapiCli = false;
+
+                    const interpreterService = serviceContainer.get<IInterpreterService>(IInterpreterService);
+                    const interpreter = await interpreterService.getInterpreterDetails(runtime.runtimePath);
+                    if (interpreter) {
+                        const installer = serviceContainer.get<IInstaller>(IInstaller);
+                        hasFastapiCli = await installer.isInstalled(Product.fastapiCli, interpreter);
+                    } else {
+                        traceError(
+                            `Could not check if fastapi-cli is installed due to an invalid interpreter path: ${runtime.runtimePath}`,
+                        );
+                    }
+
+                    let args: string[];
+                    if (hasFastapiCli) {
+                        args = [runtime.runtimePath, '-m', 'fastapi', 'dev', document.uri.fsPath];
+                    } else {
+                        const appName = await getAppName(document, 'FastAPI');
+                        if (!appName) {
+                            return undefined;
+                        }
+                        args = [
+                            runtime.runtimePath,
+                            '-m',
+                            'uvicorn',
+                            '--reload',
+                            `${pathToModule(document.uri.fsPath)}:${appName}`,
+                        ];
+                    }
+
+                    if (port) {
+                        args.push('--port', port);
+                    }
+                    if (urlPrefix) {
+                        args.push('--root-path', urlPrefix);
+                    }
+                    return { commandLine: args.join(' ') };
+                },
+                urlPath: '/docs',
+            });
+        }),
+
+        vscode.commands.registerCommand(Commands.Exec_Flask_In_Terminal, async () => {
+            const runAppApi = await getPositronRunAppApi();
+            await runAppApi.runApplication({
+                name: 'Flask',
+                async getTerminalOptions(runtime, document, port, urlPrefix) {
+                    const args = [runtime.runtimePath, '-m', 'flask', '--app', document.uri.fsPath, 'run'];
+                    if (port) {
+                        args.push('--port', port);
+                    }
+                    const terminalOptions: RunAppTerminalOptions = { commandLine: args.join(' ') };
+                    if (urlPrefix) {
+                        terminalOptions.env = { SCRIPT_NAME: urlPrefix };
+                    }
+                    return terminalOptions;
+                },
+            });
+        }),
+
+        vscode.commands.registerCommand(Commands.Exec_Gradio_In_Terminal, async () => {
+            const runAppApi = await getPositronRunAppApi();
+            await runAppApi.runApplication({
+                name: 'Gradio',
+                getTerminalOptions(runtime, document, port, urlPrefix) {
+                    const terminalOptions: RunAppTerminalOptions = {
+                        commandLine: [runtime.runtimePath, document.uri.fsPath].join(' '),
+                    };
+                    if (port || urlPrefix) {
+                        terminalOptions.env = {};
+                        if (port) {
+                            terminalOptions.env.GRADIO_SERVER_PORT = port;
+                        }
+                        if (urlPrefix) {
+                            terminalOptions.env.GRADIO_ROOT_PATH = urlPrefix;
+                        }
+                    }
+                    return terminalOptions;
+                },
+            });
+        }),
+
+        vscode.commands.registerCommand(Commands.Exec_Streamlit_In_Terminal, async () => {
+            const runAppApi = await getPositronRunAppApi();
+            await runAppApi.runApplication({
+                name: 'Streamlit',
+                getTerminalOptions(runtime, document, port, _urlPrefix) {
+                    const args = [
+                        runtime.runtimePath,
+                        '-m',
+                        'streamlit',
+                        'run',
+                        document.uri.fsPath,
+                        // Enable headless mode to avoid opening a browser window since it
+                        // will already be previewed in the viewer pane.
+                        '--server.headless',
+                        'true',
+                    ];
+                    if (port) {
+                        args.push('--port', port);
+                    }
+                    return { commandLine: args.join(' ') };
+                },
+            });
+        }),
+    );
+}
+
+/**
+ * Convert a file path string to Python module format.
+ * For example `path/to/module.py` becomes `path.to.module`.
+ */
+function pathToModule(p: string): string {
+    // Get the path's directory relative to the workspace root.
+    const relativePath = vscode.workspace.asRelativePath(p);
+    let relativeDir = path.dirname(relativePath);
+
+    // Get the name of the module.
+    const mod = path.parse(p).name;
+
+    // If the module is in the workspace root, return it as is.
+    if (relativeDir === '.') {
+        return mod;
+    }
+    // If the module is in a parent directory, remove one '.' as expected by the Python module format.
+    if (relativeDir.match(/\.+/)) {
+        relativeDir = relativeDir.slice(0, -1);
+    }
+
+    // Otherwise, convert the path to a Python module format.
+    const parts = relativeDir.split(path.sep);
+
+    return parts.concat(mod).join('.');
+}
+
+/**
+ * Get an ASGI application object's name from a document, prompting the user if necessary.
+ *
+ * @param document The Python application document.
+ * @param className The name of the ASGI application class e.g. 'FastAPI' or 'Flask'.
+ * @returns The name of the ASGI application object, or `undefined` if it could not be determined.
+ */
+async function getAppName(document: vscode.TextDocument, className: string): Promise<string | undefined> {
+    const text = document.getText();
+    let appName = text.match(new RegExp(`([^\\s]+)\\s*=\\s*${className}\\(`))?.[1];
+    if (!appName) {
+        appName = await vscode.window.showInputBox({
+            prompt: vscode.l10n.t(
+                'No {0} object found in your application code. Please enter the name manually.',
+                className,
+            ),
+            validateInput(value) {
+                if (!value.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/)) {
+                    return vscode.l10n.t('Invalid {0} app object name.', className);
+                }
+                return undefined;
+            },
+        });
+        if (!appName) {
+            vscode.window.showErrorMessage(
+                vscode.l10n.t('No {0} application object name provided, aborting. Please try again.', className),
+            );
+            return undefined;
+        }
+    }
+    return appName;
+}
+
+/** Get the Positron Run App extension's API. */
+async function getPositronRunAppApi(): Promise<PositronRunApp> {
+    const runAppExt = vscode.extensions.getExtension<PositronRunApp>('vscode.positron-run-app');
+    if (!runAppExt) {
+        throw new Error('vscode.positron-run-app extension not found');
+    }
+    const runAppApi = await runAppExt.activate();
+    return runAppApi;
+}
---
+++ extensions/positron-run-app/src/extension.ts
@@ -0,0 +1,310 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import * as positron from 'positron';
+import { PositronRunApp, RunAppOptions } from './positron-run-app';
+import { raceTimeout, SequencerByKey } from './utils';
+
+const localUrlRegex = /http:\/\/(localhost|127\.0\.0\.1):(\d{1,5})/;
+
+export const log = vscode.window.createOutputChannel('Positron Run App', { log: true });
+
+export async function activate(context: vscode.ExtensionContext): Promise<PositronRunApp> {
+	context.subscriptions.push(log);
+
+	return new PositronRunAppApiImpl(context);
+}
+
+export class PositronRunAppApiImpl implements PositronRunApp {
+	private readonly _runApplicationSequencerByName = new SequencerByKey<string>();
+
+	constructor(private readonly _context: vscode.ExtensionContext) { }
+
+	private isShellIntegrationSupported(): boolean {
+		return this._context.globalState.get('shellIntegrationSupported', true);
+	}
+
+	setShellIntegrationSupported(supported: boolean): Thenable<void> {
+		return this._context.globalState.update('shellIntegrationSupported', supported);
+	}
+
+	async runApplication(options: RunAppOptions): Promise<void> {
+		// If there's no active text editor, do nothing.
+		const document = vscode.window.activeTextEditor?.document;
+		if (!document) {
+			return;
+		}
+
+		if (this._runApplicationSequencerByName.has(options.name)) {
+			vscode.window.showErrorMessage(vscode.l10n.t('{0} application is already starting.', options.name));
+			return;
+		}
+
+		return this.queueRunApplication(document, options);
+	}
+
+	private queueRunApplication(document: vscode.TextDocument, options: RunAppOptions): Promise<void> {
+		return this._runApplicationSequencerByName.queue(
+			options.name,
+			() => Promise.resolve(vscode.window.withProgress({
+				location: vscode.ProgressLocation.Notification,
+				title: vscode.l10n.t(`Running ${options.name} application`),
+			},
+				(progress) => this.doRunApplication(document, options, progress),
+			)),
+		);
+	}
+
+	private async doRunApplication(document: vscode.TextDocument, options: RunAppOptions, progress: vscode.Progress<{ message?: string }>): Promise<void> {
+		// Save the active document if it's dirty.
+		if (document.isDirty) {
+			await document.save();
+		}
+
+		// Get the preferred runtime for the document's language.
+		progress.report({ message: vscode.l10n.t('Getting interpreter information...') });
+		let runtime: positron.LanguageRuntimeMetadata;
+		try {
+			runtime = await positron.runtime.getPreferredRuntime(document.languageId);
+		} catch (error) {
+			vscode.window.showErrorMessage(
+				vscode.l10n.t(
+					"Failed to get '{0}' interpreter information. Error: {1}",
+					document.languageId,
+					error.message
+				),
+			);
+			return;
+		}
+
+		// Get the terminal options for the application.
+		// TODO: If we're in Posit Workbench find a free port and corresponding URL prefix.
+		//       Some application frameworks need to know the URL prefix when running behind a proxy.
+		progress.report({ message: vscode.l10n.t('Getting terminal options...') });
+		const port = undefined;
+		const urlPrefix = undefined;
+		const terminalOptions = await options.getTerminalOptions(runtime, document, port, urlPrefix);
+		if (!terminalOptions) {
+			return;
+		}
+
+		// Get existing terminals with the application's name.
+		const existingTerminals = vscode.window.terminals.filter((t) => t.name === options.name);
+
+		// Create a new terminal for the application.
+		const terminal = vscode.window.createTerminal({
+			name: options.name,
+			env: terminalOptions.env,
+		});
+
+		// Reveal the new terminal.
+		terminal.show(true);
+
+		// Wait for existing terminals to close, or a timeout.
+		progress.report({ message: vscode.l10n.t('Closing existing terminals...') });
+		await raceTimeout(
+			Promise.allSettled(existingTerminals.map((terminal) => {
+				// Create a promise that resolves when the terminal is closed.
+				// Note that the application process may still be running once this promise resolves.
+				const terminalDidClose = new Promise<void>((resolve) => {
+					const disposable = vscode.window.onDidCloseTerminal((closedTerminal) => {
+						if (closedTerminal === terminal) {
+							disposable.dispose();
+							resolve();
+						}
+					});
+				});
+
+				// Close the terminal.
+				terminal.dispose();
+
+				return terminalDidClose;
+			})),
+			5000,
+			() => {
+				log.warn('Timed out waiting for existing terminals to close. Proceeding anyway');
+			}
+		);
+
+		// Replace the contents of the viewer pane with a blank page while the app is loading.
+		positron.window.previewUrl(vscode.Uri.parse('about:blank'));
+
+		const shellIntegrationConfig = vscode.workspace.getConfiguration('terminal.integrated.shellIntegration');
+		const runAppConfig = vscode.workspace.getConfiguration('positron.runApplication');
+
+		let shellIntegration: vscode.TerminalShellIntegration | undefined;
+		if (shellIntegrationConfig.get('enabled')) {
+			// Shell integration may have already been injected into the terminal.
+			shellIntegration = terminal.shellIntegration;
+
+			// If it hasn't yet been injected...
+			if (!shellIntegration) {
+
+				// Create a promise that resolves with the terminal's shell integration once it's injected.
+				const shellIntegrationPromise = new Promise<vscode.TerminalShellIntegration>((resolve) => {
+					const disposable = vscode.window.onDidChangeTerminalShellIntegration(async (e) => {
+						if (e.terminal === terminal) {
+							disposable.dispose();
+							resolve(e.shellIntegration);
+
+							// Remember that shell integration is supported in this terminal.
+							await this.setShellIntegrationSupported(true);
+						}
+					});
+				});
+
+				if (this.isShellIntegrationSupported()) {
+					// Shell integration was detected as supported in a previous run.
+					// Wait for it to be injected, or a timeout.
+					progress.report({ message: vscode.l10n.t('Activating terminal shell integration...') });
+
+					shellIntegration = await raceTimeout(shellIntegrationPromise, 5000, () => {
+						log.warn('Timed out waiting for terminal shell integration. Proceeding without shell integration');
+
+						// Remember that shell integration is not supported in this terminal,
+						// so that we don't wait for it to be injected next time.
+						this.setShellIntegrationSupported(false);
+
+						// Show the shell integration not supported message, if enabled.
+						if (runAppConfig.get('showShellIntegrationNotSupportedMessage')) {
+							showShellIntegrationNotSupportedMessage()
+								.catch(error => log.error(`Error showing shell integration not supported message: ${error}`));
+						}
+					});
+				} else {
+					// Shell integration was detected as not supported in a previous run.
+					// Don't wait for it to be injected, and show a warning if enabled.
+					log.warn('Shell integration is not supported in this terminal');
+					if (runAppConfig.get('showShellIntegrationNotSupportedMessage')) {
+						showShellIntegrationNotSupportedMessage()
+							.catch(error => log.error(`Error showing shell integration not supported message: ${error}`));
+					}
+				}
+			}
+		} else if (runAppConfig.get('showEnableShellIntegrationMessage')) {
+			// Shell integration is disabled. Proceed without it, but give the user the option to
+			// enable it and to rerun the application.
+			showEnableShellIntegrationMessage()
+				.catch(error => {
+					log.error(`Error during shell integration prompt: ${error}`);
+					return { rerunApplication: false };
+				})
+				.then(({ rerunApplication }) => {
+					if (rerunApplication) {
+						this.queueRunApplication(document, options);
+					}
+				});
+		}
+
+
+		if (shellIntegration) {
+			progress.report({ message: vscode.l10n.t('Starting application server...') });
+			const execution = shellIntegration.executeCommand(terminalOptions.commandLine);
+
+			// Wait for the server URL to appear in the terminal output, or a timeout.
+			const stream = execution.read();
+			const url = await raceTimeout(
+				(async () => {
+					for await (const data of stream) {
+						const match = data.match(localUrlRegex)?.[0];
+						if (match) {
+							return new URL(match);
+						}
+					}
+					log.warn('URL not found in terminal output');
+					return undefined;
+				})(),
+				5000,
+				() => {
+					log.warn('Timed out waiting for server URL in terminal output');
+				}
+			);
+
+			if (url) {
+				// Convert the url to an external URI.
+				const localBaseUri = vscode.Uri.parse(url.toString());
+				const localUri = options.urlPath ?
+					vscode.Uri.joinPath(localBaseUri, options.urlPath) : localBaseUri;
+				const externalUri = await vscode.env.asExternalUri(localUri);
+
+				// Open the server URL in the viewer pane.
+				positron.window.previewUrl(externalUri);
+			}
+		} else {
+			// No shell integration support, just run the command.
+			terminal.sendText(terminalOptions.commandLine);
+
+			// TODO: If a port was provided, we could poll the server until it responds,
+			//       then open the URL in the viewer pane.
+		}
+	}
+}
+
+interface IShellIntegrationPromptResult {
+	rerunApplication: boolean;
+}
+
+async function showEnableShellIntegrationMessage(): Promise<IShellIntegrationPromptResult> {
+	// Prompt the user to enable shell integration.
+	const enableShellIntegration = vscode.l10n.t('Enable Shell Integration');
+	const notNow = vscode.l10n.t('Not Now');
+	const dontAskAgain = vscode.l10n.t('Don\'t Ask Again');
+	const selection = await vscode.window.showInformationMessage(
+		vscode.l10n.t(
+			'Shell integration is disabled. Would you like to enable shell integration for this ' +
+			'workspace to automatically preview your application in the Viewer pane?',
+		),
+		enableShellIntegration,
+		notNow,
+		dontAskAgain,
+	);
+
+	if (selection === enableShellIntegration) {
+		// Enable shell integration.
+		const shellIntegrationConfig = vscode.workspace.getConfiguration('terminal.integrated.shellIntegration');
+		await shellIntegrationConfig.update('enabled', true, vscode.ConfigurationTarget.Workspace);
+
+		// Prompt the user to rerun the application.
+		const rerunApplication = vscode.l10n.t('Rerun Application');
+		const notNow = vscode.l10n.t('Not Now');
+		const selection = await vscode.window.showInformationMessage(
+			vscode.l10n.t('Shell integration is now enabled. Would you like to rerun the application?'),
+			rerunApplication,
+			notNow,
+		);
+		return { rerunApplication: selection === rerunApplication };
+	} else if (selection === dontAskAgain) {
+		// Disable the prompt for future runs.
+		const runAppConfig = vscode.workspace.getConfiguration('positron.runApplication');
+		await runAppConfig.update('showShellIntegrationPrompt', false, vscode.ConfigurationTarget.Global);
+	}
+
+	return { rerunApplication: false };
+}
+
+async function showShellIntegrationNotSupportedMessage(): Promise<void> {
+	const learnMore = vscode.l10n.t('Learn More');
+	const dismiss = vscode.l10n.t('Dismiss');
+	const dontShowAgain = vscode.l10n.t('Don\'t Show Again');
+	const selection = await vscode.window.showWarningMessage(
+		vscode.l10n.t(
+			'Shell integration isn\'t supported in this terminal, ' +
+			'so automatic preview in the Viewer pane isn\'t available. ' +
+			'To use this feature, please switch to a terminal that supports shell integration.'
+		),
+		learnMore,
+		dismiss,
+		dontShowAgain,
+	);
+
+	if (selection === learnMore) {
+		await vscode.env.openExternal(vscode.Uri.parse('https://code.visualstudio.com/docs/terminal/shell-integration'));
+	} else if (selection === dontShowAgain) {
+		// Disable the prompt for future runs.
+		const runAppConfig = vscode.workspace.getConfiguration('positron.runApplication');
+		await runAppConfig.update('showShellIntegrationNotSupportedMessage', false, vscode.ConfigurationTarget.Global);
+	}
+}
