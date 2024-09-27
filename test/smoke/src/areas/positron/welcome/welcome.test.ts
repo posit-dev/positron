81bd869546491e34de0279c9edf25b3448071f88
Marie Idleman <marie.idleman@posit.co>
2024-09-27 09:41:20 -0500

    test refactor

--- test/smoke/src/areas/positron/welcome/welcome.test.ts
+++ test/smoke/src/areas/positron/welcome/welcome.test.ts
@@ -13,7 +13,7 @@ import { installAllHandlers } from '../../../utils';
  */
 
 export function setup(logger: Logger) {
-	describe.skip('Welcome Page', () => {
+	describe('Welcome Page', () => {
 		installAllHandlers(logger);
 
 		beforeEach(async function () {
