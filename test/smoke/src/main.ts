d357870c5cc9e4dd6927057b93c718aad46d4ea5
Jon Vanausdeln <jonv@live.com>
2024-09-30 12:12:33 -0700

    Turn on 100x100 data explorer

--- test/smoke/src/main.ts
+++ test/smoke/src/main.ts
@@ -449,7 +449,7 @@ describe(`VSCode Smoke Tests (${opts.web ? 'Web' : 'Electron'})`, () => {
 	// if (!opts.web && !opts.remote) { setupLaunchTests(logger); }
 	setupVariablesTest(logger);
 	setupDataExplorerTest(logger);
-	if (!opts.web && process.platform !== 'win32') { setupDataExplorer100x100Test(logger); } // skipping for web since it's slow
+	if (!opts.web) { setupDataExplorer100x100Test(logger); } // skipping for web since it's slow
 	if (!opts.web && process.platform !== 'win32') { setupPlotsTest(logger); } // bugs 4800 & 4804
 	if (!opts.web) { setupPythonConsoleTest(logger); }
 	setupRConsoleTest(logger)
