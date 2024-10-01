12f685e3ed97cfaf5fdb4718ef30f32071e571f6
Christopher Mead <chrisrmead@comcast.net>
2024-10-01 09:44:13 -0600

    More web test cases (#4852)
    Enabling more web smoke tests.
    
    Remaining tests that are not enabled for web are commented with issue
    number or note.
    
    Also, added better handling for console tests and conditional (on web)
    turn off of auto save for top action bar tests.
    
    ### QA Notes
    
    All smoke tests pass.

--- test/smoke/src/areas/positron/console/python-console.test.ts
+++ test/smoke/src/areas/positron/console/python-console.test.ts
@@ -3,6 +3,7 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { expect } from '@playwright/test';
 import { Application, Logger, PositronPythonFixtures } from '../../../../../automation';
 import { installAllHandlers } from '../../../utils';
 
@@ -17,6 +18,11 @@ export function setup(logger: Logger) {
 
 		describe('Python Console Restart', () => {
 
+			before(async function () {
+				// Need to make console bigger to see all bar buttons
+				await this.app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
+			});
+
 			beforeEach(async function () {
 
 				await PositronPythonFixtures.SetupFixtures(this.app as Application);
@@ -24,27 +30,27 @@ export function setup(logger: Logger) {
 			});
 
 			it('Verify restart button inside the console [C377918]', async function () {
-				this.retries(1);
+
 				const app = this.app as Application;
-				// Need to make console bigger to see all bar buttons
-				await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
-				await app.workbench.positronConsole.barClearButton.click();
-				await app.workbench.positronConsole.barPowerButton.click();
-				await app.workbench.positronConsole.consoleRestartButton.click();
-				await app.workbench.positronConsole.waitForReady('>>>');
-				await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('restarted')));
-				await app.workbench.positronConsole.consoleRestartButton.isNotVisible();
+				await expect(async () => {
+					await app.workbench.positronConsole.barClearButton.click();
+					await app.workbench.positronConsole.barPowerButton.click();
+					await app.workbench.positronConsole.consoleRestartButton.click();
+					await app.workbench.positronConsole.waitForReady('>>>');
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('restarted')));
+					await app.workbench.positronConsole.consoleRestartButton.isNotVisible();
+				}).toPass();
 			});
 
 			it('Verify restart button on console bar [C617464]', async function () {
-				this.retries(1);
+
 				const app = this.app as Application;
-				// Need to make console bigger to see all bar buttons
-				await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
-				await app.workbench.positronConsole.barClearButton.click();
-				await app.workbench.positronConsole.barRestartButton.click();
-				await app.workbench.positronConsole.waitForReady('>>>');
-				await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('restarted')));
+				await expect(async () => {
+					await app.workbench.positronConsole.barClearButton.click();
+					await app.workbench.positronConsole.barRestartButton.click();
+					await app.workbench.positronConsole.waitForReady('>>>');
+					await app.workbench.positronConsole.waitForConsoleContents((contents) => contents.some((line) => line.includes('restarted')));
+				}).toPass();
 			});
 
 		});
