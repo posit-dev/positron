9c8fe001eb0a956ad0c0d2c1b0a13c806a462c1e
Daniel Falbel <dfalbel@gmail.com>
2024-10-03 18:06:47 -0300

    Hello world rendering

---
+++ src/vs/workbench/contrib/positronConnections/browser/positronConnections.tsx
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { PropsWithChildren } from 'react';
+import * as React from 'react';
+
+export interface PositronConnectionsProps {
+}
+
+export const PositronConnections = (props: PropsWithChildren<PositronConnectionsProps>) => {
+	return <p>Hello world</p>;
+};
--- src/vs/workbench/contrib/positronConnections/browser/positronConnectionsView.tsx
+++ src/vs/workbench/contrib/positronConnections/browser/positronConnectionsView.tsx
@@ -6,18 +6,37 @@
 import {
 	IReactComponentContainer,
 	ISize,
+	PositronReactRenderer,
 } from 'vs/base/browser/positronReactRenderer';
 import { Emitter, Event } from 'vs/base/common/event';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
+import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
+import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
+import { IHoverService } from 'vs/platform/hover/browser/hover';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
+import { IOpenerService } from 'vs/platform/opener/common/opener';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
+import { IThemeService } from 'vs/platform/theme/common/themeService';
+import { IViewPaneOptions } from 'vs/workbench/browser/parts/views/viewPane';
 import { PositronViewPane } from 'vs/workbench/browser/positronViewPane/positronViewPane';
+import { IViewDescriptorService } from 'vs/workbench/common/views';
+import * as DOM from 'vs/base/browser/dom';
+import { PositronConnections } from 'vs/workbench/contrib/positronConnections/browser/positronConnections';
+import * as React from 'react';
 
 export class PositronConnectionsView
 	extends PositronViewPane
 	implements IReactComponentContainer {
+
 	private onSizeChangedEmitter = this._register(new Emitter<ISize>());
 	private onVisibilityChangedEmitter = this._register(new Emitter<boolean>());
 	private onSaveScrollPositionEmitter = this._register(new Emitter<void>());
 	private onRestoreScrollPositionEmitter = this._register(new Emitter<void>());
 	private onFocusedEmitter = this._register(new Emitter<void>());
+
+	private positronConnectionsContainer!: HTMLElement;
+	private positronReactRenderer?: PositronReactRenderer;
 
 	onFocused: Event<void> = this.onFocusedEmitter.event;
 	onSizeChanged: Event<ISize> = this.onSizeChangedEmitter.event;
@@ -38,5 +57,68 @@ export class PositronConnectionsView
 		return false;
 	}
 
-	takeFocus() { }
+	takeFocus() {
+		this.focus();
+	}
+
+	constructor(
+		options: IViewPaneOptions,
+		@IConfigurationService configurationService: IConfigurationService,
+		@IContextKeyService contextKeyService: IContextKeyService,
+		@IContextMenuService contextMenuService: IContextMenuService,
+		@IHoverService hoverService: IHoverService,
+		@IInstantiationService instantiationService: IInstantiationService,
+		@IKeybindingService keybindingService: IKeybindingService,
+		@IOpenerService openerService: IOpenerService,
+		@ITelemetryService telemetryService: ITelemetryService,
+		@IThemeService themeService: IThemeService,
+		@IViewDescriptorService viewDescriptorService: IViewDescriptorService,
+	) {
+		super(
+			options,
+			keybindingService,
+			contextMenuService,
+			configurationService,
+			contextKeyService,
+			viewDescriptorService,
+			instantiationService,
+			openerService,
+			themeService,
+			telemetryService,
+			hoverService
+		);
+
+		// Register the onDidChangeBodyVisibility event handler.
+		this._register(this.onDidChangeBodyVisibility(visible => {
+			// The browser will automatically set scrollTop to 0 on child components that have been
+			// hidden and made visible. (This is called "desperate" elsewhere in Visual Studio Code.
+			// Search for that word and you'll see other examples of hacks that have been added to
+			// to fix this problem.) IReactComponentContainers can counteract this behavior by
+			// firing onSaveScrollPosition and onRestoreScrollPosition events to have their child
+			// components save and restore their scroll positions.
+			if (!visible) {
+				this.onSaveScrollPositionEmitter.fire();
+			} else {
+				this.onRestoreScrollPositionEmitter.fire();
+			}
+			this.onVisibilityChangedEmitter.fire(visible);
+		}));
+	}
+
+	protected override renderBody(container: HTMLElement): void {
+		// Call the base class's method.
+		super.renderBody(container);
+
+		// Create and append the Positron variables container.
+		this.positronConnectionsContainer = DOM.$('.positron-variables-container');
+		container.appendChild(this.positronConnectionsContainer);
+
+
+		// Create the PositronReactRenderer for the PositronVariables component and render it.
+		this.positronReactRenderer = new PositronReactRenderer(this.positronConnectionsContainer);
+		this._register(this.positronReactRenderer);
+		this.positronReactRenderer.render(
+			<PositronConnections />
+		);
+	}
 }
