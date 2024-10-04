062a984f9de346986a636c919d5865ce11380c05
Jonathan McPherson <jonathan@rstudio.com>
2024-10-04 11:08:37 -0700

    infrastructure for tests

---
+++ extensions/kallichore-adapter/src/test/kernels.test.ts
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+
+suite('Kernels', () => {
+	test('Stub test', async () => {
+		assert.strictEqual(1, 1);
+	});
+});
