f620f2f166b6c9b32dab943e5e1f49110132f96c
Daniel Falbel <dfalbel@gmail.com>
2024-10-04 12:11:01 -0300

    Add more elements and details

--- src/vs/workbench/contrib/positronConnections/browser/positronConnections.tsx
+++ src/vs/workbench/contrib/positronConnections/browser/positronConnections.tsx
@@ -30,29 +30,137 @@ export const PositronConnections = (props: React.PropsWithChildren<PositronConne
 			<ActionBar {...props}></ActionBar>
 			<div className='connections-items-container'>
 				<PositronConnectionsItem
-					name='SQLite Connection 1'
+					name='Snowflake Connection 1: R'
 					icon='database'
+					expanded={ConnectionItemExpanded.Expanded}
+					level={0}
 				></PositronConnectionsItem>
 				<PositronConnectionsItem
-					name='SQLite Connection 2'
+					name='Snowflake Connection 2: Python'
 					icon='database'
+					expanded={ConnectionItemExpanded.Expanded}
+					level={0}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='PostgreSQL Connection: Python'
+					icon='database'
+					expanded={ConnectionItemExpanded.Expanded}
+					level={0}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='rds'
+					icon='database'
+					expanded={ConnectionItemExpanded.Expanded}
+					level={1}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='content'
+					icon='database'
+					expanded={ConnectionItemExpanded.Expanded}
+					level={2}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='bike_model_data'
+					icon='database'
+					expanded={ConnectionItemExpanded.NotExpanded}
+					level={3}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='bike_predict_metrics'
+					icon='database'
+					expanded={ConnectionItemExpanded.NotExpanded}
+					level={3}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='bike_raw_data'
+					icon='database'
+					expanded={ConnectionItemExpanded.NotExpanded}
+					level={3}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='bike_raw_dataset'
+					icon='database'
+					expanded={ConnectionItemExpanded.NotExpanded}
+					level={3}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='bike_station_info'
+					icon='database'
+					expanded={ConnectionItemExpanded.Expanded}
+					level={3}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='station_id'
+					icon='database'
+					expanded={ConnectionItemExpanded.None}
+					level={4}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='name'
+					icon='database'
+					expanded={ConnectionItemExpanded.None}
+					level={4}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='lat'
+					icon='database'
+					expanded={ConnectionItemExpanded.None}
+					level={4}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='lon'
+					icon='database'
+					expanded={ConnectionItemExpanded.None}
+					level={4}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='bike_test_data'
+					icon='database'
+					expanded={ConnectionItemExpanded.NotExpanded}
+					level={3}
+				></PositronConnectionsItem>
+				<PositronConnectionsItem
+					name='Spark Connection: Python'
+					icon='database'
+					expanded={ConnectionItemExpanded.NotExpanded}
+					level={0}
 				></PositronConnectionsItem>
 			</div>
 		</div>
 	);
 };
 
+enum ConnectionItemExpanded {
+	Expanded,
+	NotExpanded,
+	None
+}
+
 interface PositronConnectionsItemProps {
 	name: string;
 	icon: string;
+	expanded: ConnectionItemExpanded;
+	level: number; // How nested the item is.
 }
 
 const PositronConnectionsItem = (props: React.PropsWithChildren<PositronConnectionsItemProps>) => {
+
+	// If the connection is not expandable, we add some more padding.
+	const padding = props.level * 10 + (props.expanded === ConnectionItemExpanded.None ? 26 : 0);
+
 	return (
 		<div className='connections-item'>
-			<div className='expand-collapse-area'>
-				<div className='codicon codicon-chevron-right'></div>
-			</div>
+			<div className='nesting' style={{ width: `${padding}px` }}></div>
+			{
+				props.expanded === ConnectionItemExpanded.None ?
+					<></> :
+					<div className='expand-collapse-area'>
+						<div
+							className={`codicon codicon-chevron-${props.expanded === ConnectionItemExpanded.Expanded ? 'down' : 'right'}`}
+						>
+						</div>
+					</div>
+			}
 			<div className='connections-name'>
 				{props.name}
 			</div>
