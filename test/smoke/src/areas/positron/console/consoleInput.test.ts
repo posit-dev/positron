af3c8088ea323bf347d5bb3ba90bd7c4f51d1b47
Jon Vanausdeln <jonv@live.com>
2024-10-15 15:17:17 -0700

    Re-enable R console input tests (#5032)
    ### Intent
    
    Turn cnosole input test again as #4901 has been fixed in `main`
    
    ### Approach
    
    Removed `skip`. Also note that these tests will now run as part of the PR jobs as requested by dev.
    
    ## QA Notes
    
    Tests pass in CI

--- test/smoke/src/areas/positron/console/consoleInput.test.ts
+++ test/smoke/src/areas/positron/console/consoleInput.test.ts
@@ -48,8 +48,7 @@ print(f'Hello {val}!')`;
 		});
 	});
 
-	//Skipping due to https://github.com/posit-dev/positron/issues/4901
-	describe.skip('Console Input - R', () => {
+	describe('Console Input - R', () => {
 		before(async function () {
 			await PositronRFixtures.SetupFixtures(this.app as Application);
 			await this.app.workbench.positronLayouts.enterLayout('fullSizedPanel');
