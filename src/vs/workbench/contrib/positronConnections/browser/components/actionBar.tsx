0ec4f4eee00e2402b69834b1f49a6c0cdfe643ed
Daniel Falbel <dfalbel@gmail.com>
2024-10-16 15:39:00 -0300

    Move connections pane into core positron (#4967)
    Addresses #4874 
    
    Needs codicons from:
    https://github.com/posit-dev/positron-codicons/pull/6
    Needs ark side: https://github.com/posit-dev/ark/pull/589
    
    This PR is an initial implementation of the connections pane into
    Positron core. It's still missing a lot of functionality, but will
    eventually be equivalent to the current positron-connections extension.
    
    TODO:
    
    - [x] Workspace storage of previous connections
    - [x] Setting to opt-in for the new implementation
    - [x] Button to open data explorer for a `Table` or `View`
    - [x] Codicons for most common DB data types (schema, collections, etc)
    and for creating a new connection
    - [x] Review event listeners disposing
    - [x] Implement Python side of new GetMetadata RPC
    - [x] Implement refresh connections
    - [x] Display the field type 
    - [x] Display the language for each connection
    - [x] Review error handling
    - [x] Fix smoke tests (smoke tests are passing locally)
    - [x] Fix integration tests
    - [x] Rename 'Connections Core' to just 'Connections'
    - [x] Support for the focus event
    
    
    Demo:
    
    
    
    https://github.com/user-attachments/assets/9e7b7905-3c1b-421d-a9e4-6c8e5d3b3b94
    
    Must be enabled with:
    
    
    ![image](https://github.com/user-attachments/assets/b3870a00-9fe2-4144-b54e-d74b7521945f)

---
+++ src/vs/workbench/contrib/positronConnections/browser/components/actionBar.tsx
@@ -0,0 +1,113 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as React from 'react';
+
+import { ActionBarButton } from 'vs/platform/positronActionBar/browser/components/actionBarButton';
+import { ActionBarRegion } from 'vs/platform/positronActionBar/browser/components/actionBarRegion';
+import { ActionBarSeparator } from 'vs/platform/positronActionBar/browser/components/actionBarSeparator';
+import { PositronActionBar } from 'vs/platform/positronActionBar/browser/positronActionBar';
+import { PositronActionBarContextProvider } from 'vs/platform/positronActionBar/browser/positronActionBarContext';
+
+import { ICommandService } from 'vs/platform/commands/common/commands';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
+import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
+import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
+import { IHoverService } from 'vs/platform/hover/browser/hover';
+import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
+import { ActionBarSearch } from 'vs/platform/positronActionBar/browser/components/actionBarSearch';
+
+import 'vs/css!./actionBar';
+import { IPositronConnectionEntry } from 'vs/workbench/services/positronConnections/browser/positronConnectionsCache';
+
+const ACTION_BAR_PADDING_LEFT = 8;
+const ACTION_BAR_PADDING_RIGHT = 8;
+export const ACTION_BAR_HEIGHT = 32;
+
+interface ActionBarProps {
+	readonly commandService: ICommandService;
+	readonly configurationService: IConfigurationService;
+	readonly contextKeyService: IContextKeyService;
+	readonly contextMenuService: IContextMenuService;
+	readonly hoverService: IHoverService;
+	readonly keybindingService: IKeybindingService;
+}
+
+interface ConnectionActionBarProps extends ActionBarProps {
+	selectedEntry: IPositronConnectionEntry | undefined;
+	clearAllHandler: () => void;
+}
+
+export const ActionBar = (props: React.PropsWithChildren<ConnectionActionBarProps>) => {
+
+	// We only enable the disconnect button if:
+	// 1. there's some connection selected
+	// 2. it's the root of a connection (level == 0).
+	// 3. the connection is active.
+	const disconnectDisabled = props.selectedEntry === undefined ||
+		props.selectedEntry.level !== 0 ||
+		!props.selectedEntry.active;
+
+	// We only enable the connect button if:
+	// 1. there's some connection selected
+	// 2. it's the root of a connection (level == 0).
+	// 3. the connection is not active.
+	// 4. it implements a 'connect' method.
+	const connectDisabled = props.selectedEntry === undefined ||
+		props.selectedEntry.level !== 0 ||
+		props.selectedEntry.active ||
+		props.selectedEntry.connect === undefined;
+
+	return (
+		<div style={{ height: ACTION_BAR_HEIGHT }}>
+			<PositronActionBarContextProvider {...props}>
+				<PositronActionBar
+					size='small'
+					borderTop={true}
+					borderBottom={true}
+					paddingLeft={ACTION_BAR_PADDING_LEFT}
+					paddingRight={ACTION_BAR_PADDING_RIGHT}
+				>
+					<ActionBarRegion location='left'>
+						<ActionBarButton
+							align='left'
+							// TODO: should have a connect-icon
+							iconId='positron-new-connection'
+							tooltip={() => 'Connect'}
+							disabled={connectDisabled}
+							onPressed={() => props.selectedEntry?.connect?.()}
+						/>
+						<ActionBarSeparator />
+						<ActionBarButton
+							align='left'
+							iconId='positron-disconnect-connection'
+							text='Disconnect'
+							disabled={disconnectDisabled}
+							onPressed={() => props.selectedEntry?.disconnect?.()}
+						/>
+						<ActionBarSeparator />
+						<ActionBarButton
+							align='left'
+							iconId='refresh'
+							onPressed={() => props.selectedEntry?.refresh?.()}
+							disabled={props.selectedEntry === undefined || props.selectedEntry.refresh === undefined || !props.selectedEntry.active}
+						/>
+						<ActionBarSeparator />
+						<ActionBarButton
+							align='left'
+							iconId='clear-all'
+							onPressed={() => props.clearAllHandler()}
+						/>
+					</ActionBarRegion>
+					<ActionBarRegion location='right'>
+						<div className='action-bar-disabled'>
+							<ActionBarSearch placeholder='filter'></ActionBarSearch>
+						</div>
+					</ActionBarRegion>
+				</PositronActionBar>
+			</PositronActionBarContextProvider>
+		</div>
+	);
+};
---
+++ src/vs/workbench/services/positronConnections/browser/interfaces/positronConnectionsInstance.ts
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Emitter } from 'vs/base/common/event';
+
+export interface ConnectionMetadata {
+	name: string;
+	language_id: string;
+	host?: string;
+	type?: string;
+	code?: string;
+	icon?: string;
+}
+
+/***
+ * A Connection Instance represents the root of a connection to a data
+ * source. Children of a connection instance are tables, views, and other
+ * objects that can be queried and are represented by Connection Items.
+ */
+export interface IPositronConnectionInstance extends IPositronConnectionItem {
+	language_id: string;
+	active: boolean;
+	metadata: ConnectionMetadata;
+
+	connect?(): Promise<void>;
+	disconnect?(): Promise<void>;
+	refresh?(): Promise<void>;
+}
+
+/***
+ * A connection item represents a child object of a connection instance, such as a schema,
+ * catalog, table, or view.
+ */
+export interface IPositronConnectionItem {
+	id: string; // An id is essential for rendering with React
+	name: string; // Every item needs a name in order for it to be displayed
+	kind: string; // The kind of the item, eg. table, view, schema, catalog, etc.
+	dtype?: string; // The data type of the item, usually only implemented if kind == field
+	icon?: string; // The icon that should be displayed next to the item
+	error?: string; // Any initialization error for the item.
+
+	expanded: boolean | undefined; // Wether the item is currently expanded
+
+	/**
+	 * Front-end may fire this event whenever the user clicks the
+	 * toggle expand button. Must be implemented if the item is
+	 * expandable.
+	 */
+	onToggleExpandEmitter?: Emitter<void>;
+
+	/**
+	 * Items fire this event whenever their data has changed.
+	 * Eg. The connections is turned off, or some child was expanded.
+	 * It's used to notify the renderer that the item has changed.
+	 */
+	onDidChangeDataEmitter: Emitter<void>;
+
+	/**
+	 * If the item can be previewed, it should implement this method.
+	 */
+	preview?(): Promise<void>;
+
+	hasChildren?(): Promise<boolean>;
+	getChildren?(): Promise<IPositronConnectionItem[]>;
+}
