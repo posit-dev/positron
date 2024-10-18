582072901ce26cfacd7eecf5de1c49d7f1efb707
sharon wang <sharon-wang@users.noreply.github.com>
2024-10-18 09:21:48 -0400

    update compiled JS files

--- build/lib/fetch.js
+++ build/lib/fetch.js
@@ -48,8 +48,8 @@ async function fetchUrl(url, options, retries = 10, retryDelay = 1000) {
         }
         const controller = new AbortController();
         // --- Start Positron ---
-        // Increase the timeout to 90 seconds to accommodate slower downloads.
-        const timeout = setTimeout(() => controller.abort(), 90 * 1000);
+        const timeoutSeconds = options.timeoutSeconds ?? 30;
+        const timeout = setTimeout(() => controller.abort(), timeoutSeconds * 1000);
         // --- End Positron ---
         try {
             const response = await fetch(url, {
