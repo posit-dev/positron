18fe1b32ddaf9234f2e15622d99f71095926b893
Jon Vanausdeln <jonv@live.com>
2024-10-23 15:51:16 -0700

    fix up some out of order layout changes

--- test/smoke/src/areas/positron/dataexplorer/dataexplorer.test.ts
+++ test/smoke/src/areas/positron/dataexplorer/dataexplorer.test.ts
@@ -216,7 +216,7 @@ df2 = pd.DataFrame(data)`;
 			const app = this.app as Application;
 
 			await app.workbench.positronDataExplorer.closeDataExplorer();
-			await app.workbench.positronVariables.openVariables();
+			// await app.workbench.positronVariables.openVariables();
 			await app.workbench.quickaccess.runCommand('workbench.action.closeAllEditors', { keepOpen: false });
 
 		});
@@ -258,7 +258,7 @@ df2 = pd.DataFrame(data)`;
 			const app = this.app as Application;
 			this.timeout(120000);
 
-			// await app.workbench.positronLayouts.enterLayout('notebook');
+			await app.workbench.positronDataExplorer.expandSummary();
 
 			expect(await app.workbench.positronDataExplorer.getColumnMissingPercent(1)).toBe('0%');
 			expect(await app.workbench.positronDataExplorer.getColumnMissingPercent(2)).toBe('0%');
@@ -267,7 +267,6 @@ df2 = pd.DataFrame(data)`;
 			expect(await app.workbench.positronDataExplorer.getColumnMissingPercent(5)).toBe('33%');
 			expect(await app.workbench.positronDataExplorer.getColumnMissingPercent(6)).toBe('33%');
 
-			await app.workbench.positronDataExplorer.expandSummary();
 
 			const col1ProfileInfo = await app.workbench.positronDataExplorer.getColumnProfileInfo(1);
 			expect(col1ProfileInfo).toStrictEqual({ 'Missing': '0', 'Min': '1.00', 'Median': '2.00', 'Mean': '2.00', 'Max': '3.00', 'SD': '1.00' });
@@ -287,8 +286,9 @@ df2 = pd.DataFrame(data)`;
 			const col6ProfileInfo = await app.workbench.positronDataExplorer.getColumnProfileInfo(6);
 			expect(col6ProfileInfo).toStrictEqual({ 'Missing': '1', 'True': '1', 'False': '1' });
 
-			await app.workbench.positronLayouts.enterLayout('stacked');
-			await app.workbench.positronSideBar.closeSecondarySideBar();
+			// await app.workbench.positronLayouts.enterLayout('stacked');
+			// await app.workbench.positronSideBar.closeSecondarySideBar();
+			await app.workbench.positronDataExplorer.collapseSummary();
 
 		});
 
@@ -347,6 +347,8 @@ df2 = pd.DataFrame(data)`;
 				expect(tableData.length).toBe(2);
 			}).toPass({ timeout: 60000 });
 
+			await app.workbench.positronLayouts.enterLayout('stacked');
+
 		});
 	});
 
@@ -377,7 +379,7 @@ df2 = pd.DataFrame(data)`;
 				await app.code.driver.getLocator('.label-name:has-text("Data: Data_Frame")').innerText();
 			}).toPass();
 
-			await app.workbench.positronLayouts.enterLayout('notebook');
+			await app.workbench.positronDataExplorer.maximizeDataExplorer(true);
 
 			await expect(async () => {
 				const tableData = await app.workbench.positronDataExplorer.getDataExplorerTableData();
@@ -388,7 +390,6 @@ df2 = pd.DataFrame(data)`;
 				expect(tableData.length).toBe(3);
 			}).toPass({ timeout: 60000 });
 
-			await app.workbench.positronLayouts.enterLayout('stacked');
 
 
 		});
@@ -396,6 +397,8 @@ df2 = pd.DataFrame(data)`;
 
 			const app = this.app as Application;
 			this.timeout(120000);
+
+			await app.workbench.positronDataExplorer.expandSummary();
 
 			expect(await app.workbench.positronDataExplorer.getColumnMissingPercent(1)).toBe('0%');
 			expect(await app.workbench.positronDataExplorer.getColumnMissingPercent(2)).toBe('33%');
