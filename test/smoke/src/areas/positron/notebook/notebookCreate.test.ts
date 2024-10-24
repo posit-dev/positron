fc8ccdbfab1365e9360ae4eaa1573b08f6c89a03
Marie Idleman <marie.idleman@posit.co>
2024-10-24 09:14:57 -0500

    update test

--- test/smoke/src/areas/positron/notebook/notebookCreate.test.ts
+++ test/smoke/src/areas/positron/notebook/notebookCreate.test.ts
@@ -3,13 +3,46 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
-
-import { expect } from '@playwright/test';
 import { Application, PositronNotebooks, PositronPythonFixtures, PositronRFixtures } from '../../../../../automation';
 import { setupAndStartApp } from '../../../test-runner/test-hooks';
 
 describe('Notebooks #pr #web #win', () => {
 	setupAndStartApp();
+
+	describe('R Notebooks', () => {
+		let app: Application;
+		let notebooks: PositronNotebooks;
+
+		before(async function () {
+			app = this.app as Application;
+			notebooks = app.workbench.positronNotebooks;
+			await PositronRFixtures.SetupFixtures(this.app as Application);
+		});
+
+		beforeEach(async function () {
+			await notebooks.createNewNotebook();
+			await notebooks.selectInterpreter('R Environments', process.env.POSITRON_R_VER_SEL!);
+		});
+
+		afterEach(async function () {
+			await notebooks.closeNotebookWithoutSaving();
+		});
+
+		it('R - Basic notebook creation and execution (code) [C628629]', async function () {
+			await notebooks.addCodeToFirstCell('eval(parse(text="8**2"))');
+			await notebooks.executeCodeInCell();
+			await notebooks.assertRCellOutput('[1] 64');
+		});
+
+		it('R - Basic notebook creation and execution (markdown) [C628630]', async function () {
+			const randomText = Math.random().toString(36).substring(7);
+
+			await app.workbench.notebook.insertNotebookCell('markdown');
+			await app.workbench.notebook.waitForTypeInEditor(`## ${randomText} `);
+			await app.workbench.notebook.stopEditingCell();
+			await notebooks.assertMarkdownText('h2', randomText);
+		});
+	});
 
 	describe('Python Notebooks', () => {
 		let app: Application;
@@ -19,7 +52,6 @@ describe('Notebooks #pr #web #win', () => {
 			app = this.app as Application;
 			notebooks = app.workbench.positronNotebooks;
 			await PositronPythonFixtures.SetupFixtures(app);
-			await app.workbench.positronLayouts.enterLayout('notebook');
 		});
 
 		beforeEach(async function () {
@@ -47,44 +79,5 @@ describe('Notebooks #pr #web #win', () => {
 		});
 	});
 
-	describe('R Notebooks', () => {
-		let app: Application;
-		let notebooks: PositronNotebooks;
 
-		before(async function () {
-			app = this.app as Application;
-			notebooks = app.workbench.positronNotebooks;
-			await PositronRFixtures.SetupFixtures(this.app as Application);
-		});
-
-		beforeEach(async function () {
-			await notebooks.createNewNotebook();
-			await notebooks.selectInterpreter('R Environments', process.env.POSITRON_R_VER_SEL!);
-		});
-
-		afterEach(async function () {
-			await notebooks.closeNotebookWithoutSaving();
-		});
-
-		it('R - Basic notebook creation and execution (code) [C628629]', async function () {
-			await notebooks.addCodeToFirstCell('eval(parse(text="8**2"))');
-			await expect(async () => {
-				await notebooks.executeCodeInCell();
-				await notebooks.assertRCellOutput('[1] 64');
-			}).toPass({ timeout: 15000 });
-
-
-			await notebooks.executeCodeInCell();
-			await notebooks.assertRCellOutput('[1] 64');
-		});
-
-		it('R - Basic notebook creation and execution (markdown) [C628630]', async function () {
-			const randomText = Math.random().toString(36).substring(7);
-
-			await app.workbench.notebook.insertNotebookCell('markdown');
-			await app.workbench.notebook.waitForTypeInEditor(`## ${randomText} `);
-			await app.workbench.notebook.stopEditingCell();
-			await notebooks.assertMarkdownText('h2', randomText);
-		});
-	});
 });
