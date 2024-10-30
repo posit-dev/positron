14c397a49a30a4a403c2fcf34b1586076efe33a4
Marie Idleman <marie.idleman@posit.co>
2024-10-30 14:05:47 -0500

    tag annot

--- test/smoke/src/e2e/examples/annotations.test.ts
+++ test/smoke/src/e2e/examples/annotations.test.ts
@@ -31,6 +31,7 @@ test.describe('annotations and tags', () => {
 			{ type: 'performance', description: 'very slow test!' },
 			{ type: 'test case', description: 'https://posit.testrail.io/index.php?/cases/view/534454' }
 		],
+		tag: ['@pr'],
 	}, async () => {
 		// This test is annotated with multiple annotations.
 	});
