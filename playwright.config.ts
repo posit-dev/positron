472de25759cf672e61a1d3a21bd07044b77dbca2
Marie Idleman <marie.idleman@posit.co>
2024-10-31 14:08:03 -0500

    env tweaks

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,9 +26,9 @@ export default defineConfig<TestOptions>({
 	testMatch: '*.test.ts',
 	fullyParallel: false,
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 0 : 0,
 	workers: 3, // Number of parallel workers (tests will run in parallel)
-	reporter: [['html', { open: 'always' }]],
+	reporter: [['list'], ['html', { open: 'always' }]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
