78bf619dc2e6216fca394fdf72cf6d7b9e5e7ef3
Chris Mead <chris.mead@posit.co>
2024-11-06 10:57:10 -0700

    Test in CI

--- test/smoke/src/areas/positron/notebook/largePythonNotebook.test.ts
+++ test/smoke/src/areas/positron/notebook/largePythonNotebook.test.ts
@@ -8,7 +8,7 @@ import { Application, PositronNotebooks, PositronPythonFixtures } from '../../..
 import { setupAndStartApp } from '../../../test-runner/test-hooks';
 import { expect } from '@playwright/test';
 
-
+// This test is too time consuming to pass on web
 describe('Large Notebooks', () => {
 	setupAndStartApp();
 
@@ -24,7 +24,7 @@ describe('Large Notebooks', () => {
 
 		it('Python - Large notebook execution [C...]', async function () {
 
-			this.timeout(160000);
+			this.timeout(120_000);
 
 			await app.workbench.positronQuickaccess.openDataFile(join(app.workspacePathOrFolder, 'workspaces', 'large_py_notebook', 'spotify.ipynb'));
 
@@ -35,36 +35,36 @@ describe('Large Notebooks', () => {
 			const stopExecutionLocator = app.code.driver.page.locator('a').filter({ hasText: 'Stop Execution' });
 
 			await expect(stopExecutionLocator).toBeVisible();
-			await expect(stopExecutionLocator).not.toBeVisible({ timeout: 30000 });
+			await expect(stopExecutionLocator).not.toBeVisible({ timeout: 60000 });
 
 			await app.workbench.quickaccess.runCommand('notebook.focusTop');
 
 			await app.code.driver.page.locator('span').filter({ hasText: 'import pandas as pd' }).locator('span').first().click();
 
 			const allFigures: any[] = [];
-			const uniqueFigureParents = new Set<string>();
+			const uniqueLocators = new Set<string>();
 
-			for (let i = 0; i < 500; i++) {
-				await app.code.driver.page.mouse.wheel(0, 1000);
+			for (let i = 0; i < 6; i++) {
+
+				for (let j = 0; j < 100; j++) {
+					await app.code.driver.page.mouse.wheel(0, 1);
+					await app.code.wait(100);
+				}
 
 				const figureLocator = app.workbench.positronNotebooks.frameLocator.locator('.plot-container');
 				const figures = await figureLocator.all();
 
 				if (figures!.length > 0) {
 					for (const figure of figures!) {
-						const figureParentLocator = figure.locator('..');
-						const figureParent = await figureParentLocator.textContent();
-
-						if (figureParent && !uniqueFigureParents.has(figureParent)) {
-							uniqueFigureParents.add(figureParent);
+						if (!uniqueLocators.has(figure.toString())) {
 							allFigures.push(figure);
+							uniqueLocators.add(figure.toString());
 						}
 					}
 				}
 			}
 
-			console.log('debug');
-
+			expect(allFigures.length).toBeGreaterThan(20);
 
 		});
 	});
