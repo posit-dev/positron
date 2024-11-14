64f6c57d158c546af5ad43e723f19d925102bccd
Marie Idleman <marie.idleman@posit.co>
2024-11-14 15:22:42 -0600

    add script and yml

---
+++ scripts/slack-skipped-tests.js
@@ -0,0 +1,38 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+const { execSync } = require('child_process');
+
+const slackSkippedTests = (slackWebhookUrl) => {
+	try {
+		const skippedTests = execSync(
+			`grep -r --include \\*.test.ts -E "describe\\.skip|it\\.skip" test/smoke/src/areas/positron | sed 's/\\.test\\.ts.*$/.test.ts/'`
+		).toString();
+
+		const slackMessage = {
+			attachments: [
+				{
+					mrkdwn_in: ['text'],
+					color: skippedTests === '' ? '#CCCCCC' : '#FF0000',
+					pretext: ':skipping:*Skipped Tests*',
+					text: skippedTests === '' ? 'There are no skipped tests. :tada:' : skippedTests,
+				},
+			],
+		};
+
+		console.log(skippedTests);
+		console.log(JSON.stringify(slackMessage, null, 2));
+
+		execSync(
+			`curl -X POST -H 'Content-type: application/json' --data '${JSON.stringify(
+				slackMessage
+			)}' ${slackWebhookUrl}`
+		);
+	} catch (error) {
+		console.error(`Error: ${error}`);
+	}
+};
+
+slackSkippedTests(process.argv[2]);
