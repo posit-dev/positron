bd00a9a3c5ba9735978cf9c38c1783d30f82b6d8
Marie Idleman <marie.idleman@posit.co>
2024-11-18 15:20:56 -0600

    update python-apps for terminal url

--- test/smoke/src/areas/positron/apps/python-apps.test.ts
+++ test/smoke/src/areas/positron/apps/python-apps.test.ts
@@ -42,8 +42,12 @@ test.describe('Python Applications', { tag: ['@pr'] }, () => {
 			const viewer = app.workbench.positronViewer;
 
 			await app.workbench.quickaccess.openFile(join(app.workspacePathOrFolder, 'workspaces', 'python_apps', 'gradio_example', 'gradio_example.py'));
+			await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
+			await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
 			await app.workbench.positronEditor.pressPlay();
+			await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
 			await expect(viewer.getViewerFrame().getByRole('button', { name: 'Submit' })).toBeVisible({ timeout: 45000 });
+			await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
 		});
 
 		test('Python - Verify Basic Streamlit App [C903308]', { tag: ['@web', '@win'] }, async function ({ app, python }) {
