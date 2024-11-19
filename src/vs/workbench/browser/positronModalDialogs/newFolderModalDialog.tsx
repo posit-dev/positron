95ee4c8fcc6eefc1cd9a0a3951108e50f8c64d57
Jonathan McPherson <jonathan@rstudio.com>
2024-11-19 13:38:12 -0800

    update css ref (one of many)

--- src/vs/workbench/browser/positronModalDialogs/newFolderModalDialog.tsx
+++ src/vs/workbench/browser/positronModalDialogs/newFolderModalDialog.tsx
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 // CSS.
-import 'vs/css!./newFolderModalDialog';
+import './newFolderModalDialog.css';
 
 // React.
 import * as React from 'react';
