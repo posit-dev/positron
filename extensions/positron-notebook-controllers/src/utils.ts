e6be6a584f78bf406c3defe588d972160ce3088c
seem <mwlorgat@gmail.com>
2024-12-05 16:56:07 +0200

    remove unnecessary function

--- extensions/positron-notebook-controllers/src/utils.ts
+++ extensions/positron-notebook-controllers/src/utils.ts
@@ -3,9 +3,6 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as positron from 'positron';
-import * as vscode from 'vscode';
-
 export function delay(ms: number) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -15,17 +12,4 @@ export function formatCount(count: number, unit: string): string {
 		return `${count} ${unit}`;
 	}
 	return `${count} ${unit}s`;
-}
-
-export async function getRunningNotebookSession(notebookUri: vscode.Uri): Promise<positron.LanguageRuntimeSession | undefined> {
-	// TODO: Use getSessions()?
-	// TODO: Check that it's the expected runtime?
-	return positron.runtime.getNotebookSession(notebookUri);
-	// const state = session?.state;
-	// if (state === positron.RuntimeState.Uninitialized
-	// 	|| state === positron.RuntimeState.Exiting
-	// 	|| state === positron.RuntimeState.Exited) {
-	// 	return undefined;
-	// }
-	// return session;
 }
