50ddc102cc4000c754cbf71f0eb7103027671fee
Chris Mead <chris.mead@posit.co>
2024-12-06 15:44:53 -0700

    windows workarounds

--- test/e2e/features/help/f1.test.ts
+++ test/e2e/features/help/f1.test.ts
@@ -37,13 +37,17 @@ test.describe('F1 Help #web #win', {
 
 	});
 
-
 	test('R - Verifies basic F1 editor help functionality [C1062994]', async function ({ app, r }) {
 		await app.workbench.quickaccess.openFile(join(app.workspacePathOrFolder, 'workspaces', 'generate-data-frames-r', 'generate-data-frames.r'));
 
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
+		await app.workbench.quickaccess.runCommand('workbench.action.togglePanel');
 		await app.code.driver.page.locator('span').filter({ hasText: 'colnames(df) <- paste0(\'col\', 1:num_cols)' }).locator('span').first().dblclick();
-
 		await app.workbench.positronConsole.sendKeyboardKey('F1');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
+		await app.workbench.quickaccess.runCommand('workbench.action.togglePanel');
 
 		await expect(async () => {
 			const helpFrame = await app.workbench.positronHelp.getHelpFrame(0);
@@ -67,7 +71,7 @@ test.describe('F1 Help #web #win', {
 			const helpFrame = await app.workbench.positronHelp.getHelpFrame(1);
 
 			await expect(helpFrame.locator('h2').first()).toContainText('Options Settings', { timeout: 2000 });
-		}).toPass({ timeout: 90000 });
+		}).toPass({ timeout: 30000 });
 
 	});
 
@@ -89,9 +93,15 @@ test.describe('F1 Help #web #win', {
 	test('Python - Verifies basic F1 editor help functionality [C1062995]', async function ({ app, python }) {
 		await app.workbench.quickaccess.openFile(join(app.workspacePathOrFolder, 'workspaces', 'generate-data-frames-py', 'generate-data-frames.py'));
 
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
+		await app.workbench.quickaccess.runCommand('workbench.action.togglePanel');
 		await app.code.driver.page.locator('span').filter({ hasText: 'df = pd.DataFrame(data)' }).locator('span').first().dblclick();
 
 		await app.workbench.positronConsole.sendKeyboardKey('F1');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
+		await app.workbench.quickaccess.runCommand('workbench.action.togglePanel');
 
 		await expect(async () => {
 			const helpFrame = await app.workbench.positronHelp.getHelpFrame(0);
@@ -103,16 +113,27 @@ test.describe('F1 Help #web #win', {
 	test('Python - Verifies basic F1 notebook help functionality [C1062997]', async function ({ app, python }) {
 		await app.workbench.positronQuickaccess.openDataFile(join(app.workspacePathOrFolder, 'workspaces', 'large_py_notebook', 'spotify.ipynb'));
 
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
+		await app.workbench.quickaccess.runCommand('workbench.action.togglePanel');
 		await app.code.driver.page.locator('span').filter({ hasText: 'warnings.filterwarnings(\'ignore\')' }).locator('span').first().dblclick();
 
+		// need to wait for notebook to be ready and cannot put this in retry loop as we are also
+		// in the middle of a windows workaround
+		await app.code.wait(10000);
+
+		await app.workbench.positronConsole.sendKeyboardKey('F1');
+
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleAuxiliaryBar');
+		await app.workbench.quickaccess.runCommand('workbench.action.toggleSidebarVisibility');
+		await app.workbench.quickaccess.runCommand('workbench.action.togglePanel');
 		await expect(async () => {
-			await app.workbench.positronConsole.sendKeyboardKey('F1');
 
 			// Note that we are getting help frame 1 instead of 0 because the notbook structure matches the same locators as help
 			const helpFrame = await app.workbench.positronHelp.getHelpFrame(1);
 
 			await expect(helpFrame.locator('body').first()).toContainText('warnings.filterwarnings', { timeout: 2000 });
-		}).toPass({ timeout: 90000 });
+		}).toPass({ timeout: 30000 });
 
 	});
 
