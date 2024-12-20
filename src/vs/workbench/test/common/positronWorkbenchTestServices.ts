998aa3a11909878c02eb248c39e4f2ee403f7c28
seem <mwlorgat@gmail.com>
2024-12-20 15:21:12 +0200

    test notebook execution status

--- src/vs/workbench/test/common/positronWorkbenchTestServices.ts
+++ src/vs/workbench/test/common/positronWorkbenchTestServices.ts
@@ -7,12 +7,40 @@ import { Emitter, Event } from '../../../base/common/event.js';
 import { IDisposable } from '../../../base/common/lifecycle.js';
 import { URI } from '../../../base/common/uri.js';
 import { ICommandService, ICommandEvent, CommandsRegistry } from '../../../platform/commands/common/commands.js';
+import { IContextKeyService } from '../../../platform/contextkey/common/contextkey.js';
 import { IInstantiationService } from '../../../platform/instantiation/common/instantiation.js';
 import { IOpenerService, IOpener, IValidator, IExternalUriResolver, IExternalOpener, OpenInternalOptions, OpenExternalOptions, ResolveExternalUriOptions, IResolvedExternalUri } from '../../../platform/opener/common/opener.js';
+import { NotebookCellTextModel } from '../../contrib/notebook/common/model/notebookCellTextModel.js';
+import { INotebookTextModel } from '../../contrib/notebook/common/notebookCommon.js';
+import { ICellExecutionParticipant, IDidEndNotebookCellsExecutionEvent, IDidStartNotebookCellsExecutionEvent, INotebookExecutionService } from '../../contrib/notebook/common/notebookExecutionService.js';
 import { ILanguageRuntimeMetadata } from '../../services/languageRuntime/common/languageRuntimeService.js';
 import { IPositronModalDialogsService, ShowConfirmationModalDialogOptions, IModalDialogPromptInstance } from '../../services/positronModalDialogs/common/positronModalDialogs.js';
 import { ILanguageRuntimeSessionManager, IRuntimeSessionMetadata, ILanguageRuntimeSession } from '../../services/runtimeSession/common/runtimeSessionService.js';
 import { TestLanguageRuntimeSession } from '../../services/runtimeSession/test/common/testLanguageRuntimeSession.js';
+
+export class TestNotebookExecutionService implements INotebookExecutionService {
+	declare readonly _serviceBrand: undefined;
+	public readonly onDidStartNotebookCellsExecutionEmitter = new Emitter<IDidStartNotebookCellsExecutionEvent>();
+	public readonly onDidEndNotebookCellsExecutionEmitter = new Emitter<IDidEndNotebookCellsExecutionEvent>();
+	public readonly onDidStartNotebookCellsExecution = this.onDidStartNotebookCellsExecutionEmitter.event;
+	public readonly onDidEndNotebookCellsExecution = this.onDidEndNotebookCellsExecutionEmitter.event;
+
+	async executeNotebookCells(notebook: INotebookTextModel, cells: Iterable<NotebookCellTextModel>, contextKeyService: IContextKeyService): Promise<void> {
+		throw new Error('cancelNotebookCellHandles not implemented.');
+	}
+
+	async cancelNotebookCellHandles(notebook: INotebookTextModel, cells: Iterable<number>): Promise<void> {
+		throw new Error('cancelNotebookCellHandles not implemented.');
+	}
+
+	async cancelNotebookCells(notebook: INotebookTextModel, cells: Iterable<NotebookCellTextModel>): Promise<void> {
+		throw new Error('cancelNotebookCells not implemented.');
+	}
+
+	registerExecutionParticipant(participant: ICellExecutionParticipant): IDisposable {
+		throw new Error('registerExecutionParticipant not implemented.');
+	}
+}
 
 export class TestOpenerService implements IOpenerService {
 	_serviceBrand: undefined;
