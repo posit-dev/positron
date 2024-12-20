f70968ec440cab6b0fca2d924c0ceb28a77ef376
seem <mwlorgat@gmail.com>
2024-12-20 15:39:47 +0200

    add initial tests + simplify

--- src/vs/workbench/contrib/runtimeNotebookKernel/tests/browser/notebookExecutionStatus.test.ts
+++ src/vs/workbench/contrib/runtimeNotebookKernel/tests/browser/notebookExecutionStatus.test.ts
@@ -7,40 +7,67 @@ import assert from 'assert';
 import sinon from 'sinon';
 import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../../../base/test/common/utils.js';
 import { TestConfigurationService } from '../../../../../platform/configuration/test/common/testConfigurationService.js';
-import { IStatusbarEntryAccessor, IStatusbarService, } from '../../../../services/statusbar/browser/statusbar.js';
+import { IStatusbarService, } from '../../../../services/statusbar/browser/statusbar.js';
 import { PositronTestServiceAccessor, positronWorkbenchInstantiationService } from '../../../../test/browser/positronWorkbenchTestServices.js';
 import { TestNotebookExecutionService } from '../../../../test/common/positronWorkbenchTestServices.js';
 import { NotebookExecutionStatus } from '../../browser/notebookExecutionStatus.js';
 import { NOTEBOOK_EXPERIMENTAL_SHOW_EXECUTION_INFO_KEY } from '../../common/runtimeNotebookKernelConfig.js';
+import { TestInstantiationService } from '../../../../../platform/instantiation/test/common/instantiationServiceMock.js';
 
 suite('NotebookExecutionStatus', () => {
 	const disposables = ensureNoDisposablesAreLeakedInTestSuite();
+	let instantiationService: TestInstantiationService;
 	let configurationService: TestConfigurationService;
 	let notebookExecutionService: TestNotebookExecutionService;
 	let statusbarService: IStatusbarService;
-	let entry: sinon.SinonSpiedInstance<IStatusbarEntryAccessor>;
 
 	setup(() => {
-		const instantiationService = positronWorkbenchInstantiationService(disposables);
+		instantiationService = positronWorkbenchInstantiationService(disposables);
 		const accessor = instantiationService.createInstance(PositronTestServiceAccessor);
 		configurationService = accessor.configurationService;
 		notebookExecutionService = accessor.notebookExecutionService;
 		statusbarService = accessor.statusbarService;
-
-		const notebookExecutionStatus = disposables.add(instantiationService.createInstance(NotebookExecutionStatus));
-		entry = sinon.spy(notebookExecutionStatus.entry);
 	});
 
-	test('becomes visible when configuration is enabled', () => {
-		assert.strictEqual(statusbarService.isEntryVisible(NotebookExecutionStatus.ID), false);
+	function createEntry() {
+		const notebookExecutionStatus = disposables.add(instantiationService.createInstance(NotebookExecutionStatus));
+		return sinon.spy(notebookExecutionStatus.entry);
+	}
 
-		configurationService.setUserConfiguration(NOTEBOOK_EXPERIMENTAL_SHOW_EXECUTION_INFO_KEY, true);
+	function setShowExecutionInfo(value: boolean) {
+		configurationService.setUserConfiguration(NOTEBOOK_EXPERIMENTAL_SHOW_EXECUTION_INFO_KEY, value);
+	}
+
+	function assertEntryIsVisible(visible: boolean) {
+		assert.strictEqual(statusbarService.isEntryVisible(NotebookExecutionStatus.ID), visible);
+	}
+
+	test('initially hidden', () => {
+		createEntry();
+
+		assertEntryIsVisible(false);
+	});
+
+	test('initially shown', () => {
+		setShowExecutionInfo(true);
+
+		createEntry();
+
+		assertEntryIsVisible(true);
+	});
+
+	test('show on config enabled', () => {
+		createEntry();
+
+		setShowExecutionInfo(true);
 		configurationService.onDidChangeConfigurationEmitter.fire({ affectsConfiguration: () => true } as any);
 
-		assert.strictEqual(statusbarService.isEntryVisible(NotebookExecutionStatus.ID), true);
+		assertEntryIsVisible(true);
 	});
 
-	test('updates text when notebook cells execution starts', async () => {
+	test('update on execution start', async () => {
+		const entry = createEntry();
+
 		notebookExecutionService.onDidStartNotebookCellsExecutionEmitter.fire({ cellHandles: [1] });
 
 		const text = 'Executing 1 cell';
@@ -51,7 +78,9 @@ suite('NotebookExecutionStatus', () => {
 		});
 	});
 
-	test('updates text when notebook cells execution ends', async () => {
+	test('update on execution end', async () => {
+		const entry = createEntry();
+
 		notebookExecutionService.onDidEndNotebookCellsExecutionEmitter.fire({ cellHandles: [1], duration: 100 });
 
 		const text = 'Executed 1 cell in 100ms';
