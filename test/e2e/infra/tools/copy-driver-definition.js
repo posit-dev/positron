ea78cbebb179558fcaf5bca4019782bdec55f8f6
Marie Idleman <marie.idleman@posit.co>
2025-01-06 15:50:53 -0600

    add copy-driver-def back

---
+++ test/e2e/infra/tools/copy-driver-definition.js
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+//@ts-check
+'use strict';
+
+const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const root = cp.execSync('git rev-parse --show-toplevel').toString().trim();
+const driverPath = path.join(root, 'src/vs/workbench/services/driver/common/driver.ts');
+
+let contents = fs.readFileSync(driverPath, 'utf8');
+// @ts-ignore
+contents = /\/\/\*START([\s\S]*)\/\/\*END/mi.exec(contents)[1].trim();
+contents = contents.replace(/\bTPromise\b/g, 'Promise');
+
+contents = `/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+${contents}
+`;
+
+const srcPath = path.join(path.dirname(__dirname));
+const outPath = path.join(path.dirname(__dirname), 'out');
+
+if (!fs.existsSync(outPath)) {
+	fs.mkdirSync(outPath);
+}
+fs.writeFileSync(path.join(srcPath, 'driver.d.ts'), contents);
+fs.writeFileSync(path.join(outPath, 'driver.d.ts'), contents);
