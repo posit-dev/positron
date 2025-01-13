d786dc54f134975ad491635cb85e663b0815ecb4
Jon Vanausdeln <jonv@live.com>
2025-01-13 08:36:58 -0800

    Add windows and browser job to PR workflow (#5940)
    Addresses #5935 and #5955
    This adds windows and web tests to the PR workflow.  They are by default turned off, but can be added with the `win` and `web` tags.  
        
    ### QA Notes
    I tested with and without the win and web tags. It also works in conjunction with other tags.
    
    @:win @:web
    <!--
      Add additional information for QA on how to validate the change,
      paying special attention to the level of risk, adjacent areas that
      could be affected by the change, and any important contextual
      information not present in the linked issues.
    -->

--- test/e2e/infra/test-runner/test-tags.ts
+++ test/e2e/infra/test-runner/test-tags.ts
@@ -9,9 +9,10 @@
  * Each tag corresponds to a specific feature, functionality, or area of the application. Use them
  * thoughtfully to ensure your tests align with the intended scope.
  *
- * Avoid using `@:web` and `@:win` tags, as these are reserved for web and Windows platform-specific tests,
- * which are not currently configured to run in PR workflows.
- */
+ * Add `@:win` tag to enable the tests to run on windows. Add the `@:web` tag to enable a web run.
+ * The default will only run Linux electron e2e tests.
+ *
+*/
 export enum TestTags {
 	EDITOR_ACTION_BAR = '@editor-action-bar',
 	APPS = '@apps',
