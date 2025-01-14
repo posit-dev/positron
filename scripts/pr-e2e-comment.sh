fe59d1b1a196f08e969c1cc72800c480f6c14ec1
Marie Idleman <marie.idleman@posit.co>
2025-01-14 13:42:26 -0600

    add link to tags

--- scripts/pr-e2e-comment.sh
+++ scripts/pr-e2e-comment.sh
@@ -44,7 +44,7 @@ FORMATTED_TAGS=$(echo "$TAGS" | sed 's/,/` `/g' | sed 's/^/`/' | sed 's/$/`/')
 RED_ALERT_NOTE="<!-- \n🚨 RED ALERT! ✋ Rule breaker detected! Tags don’t go here—they belong above ^ in the PR description using this proper format: \`@:tag\`. Changing them here is a no-go (trust us, we’ve tried). Confused? Check out the link below before the sirens get louder!\n-->"
 
 # Build the new comment body with proper newlines
-NEW_COMMENT=$(printf "${COMMENT_MARKER}\n${RED_ALERT_NOTE}\n\n**E2E Tests** 🚀 &nbsp;<sup>[?](https://github.com/posit-dev/positron/blob/main/test/e2e/README.md#pull-requests-and-test-tags)</sup>\nThis PR will run tests tagged with: %s" "$FORMATTED_TAGS")
+NEW_COMMENT=$(printf "${COMMENT_MARKER}\n${RED_ALERT_NOTE}\n\n**E2E Tests** 🚀 This PR will run tests tagged with: %s\n\n<sup>[readme](https://github.com/posit-dev/positron/blob/main/test/e2e/README.md#pull-requests-and-test-tags)</sup>&nbsp;&nbsp;<sup>[valid tags](https://github.com/posit-dev/positron/blob/main/test/e2e/infra/test-runner/test-tags.ts)</sup>" "$FORMATTED_TAGS")
 
 if [ -n "$COMMENT_ID" ]; then
   # Update the existing comment
