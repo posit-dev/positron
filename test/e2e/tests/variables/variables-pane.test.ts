2a86596fadfd9051406935cd356527fb2fc17f31
Jon Vanausdeln <jonv@live.com>
2025-01-16 11:39:33 -0800

    finished out tests

--- test/e2e/tests/variables/variables-pane.test.ts
+++ test/e2e/tests/variables/variables-pane.test.ts
@@ -36,6 +36,17 @@ test.describe('Variables Pane', {
 
 	});
 
+	test.skip('Python - Verifies only 1 entery per environment', {
+		annotation: [{ type: 'issue', description: 'https://github.com/posit-dev/positron/issues/5887' }],
+	}, async function ({ app, logger, python }) {
+		await app.workbench.console.barClearButton.click();
+		await app.workbench.console.barRestartButton.click();
+		await app.workbench.console.waitForReady('>>>');
+		await app.workbench.console.waitForConsoleContents('restarted');
+		const groupList = app.workbench.variables.getVariablesGroupList();
+		expect((await groupList).length).toBe(1);
+	});
+
 	test('R - Verifies Variables pane basic function [C628635]', async function ({ app, logger, r }) {
 		const executeCode = async (code: string) => {
 			await app.workbench.console.executeCode('R', code, '>');
@@ -55,20 +66,16 @@ test.describe('Variables Pane', {
 		expect(variablesMap.get('z')).toStrictEqual({ value: '100', type: 'dbl' });
 	});
 
-	test('Python - Verifies only 1 entery per environment', {
+
+	test.skip('R - Verifies only 1 entery per environment', {
 		annotation: [{ type: 'issue', description: 'https://github.com/posit-dev/positron/issues/5887' }],
-	}, async function ({ app, logger, python }) {
-		const executeCode = async (code: string) => {
-			await app.workbench.console.executeCode('Python', code, '>>>');
-		};
-
-		await executeCode('x=1');
-		await executeCode('y=10');
-		await executeCode('z=100');
+	}, async function ({ app, logger, r }) {
+		await app.workbench.console.barClearButton.click();
+		await app.workbench.console.barRestartButton.click();
+		await app.workbench.console.waitForReady('>');
+		await app.workbench.console.waitForConsoleContents('restarted');
 		const groupList = app.workbench.variables.getVariablesGroupList();
-		expect(groupList).toHaveLength(1);
-
-
+		expect((await groupList).length).toBe(1);
 	});
 });
 
