344b32dbec54646280533d3e7726bb5e854e8f59
Dhruvi Sompura <dhruvi.sompura@posit.co>
2025-01-17 10:57:25 -0800

    Simplify confirm delete dialog

---
+++ src/vs/workbench/browser/positronComponents/positronModalDialog/confirmDeleteModalDialog.tsx
@@ -0,0 +1,70 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// CSS.
+import './positronModalDialog.css';
+import './confirmDeleteModalDialog.css';
+
+// React.
+import React, { PropsWithChildren } from 'react';
+
+// Other dependencies.
+import { localize } from '../../../../nls.js';
+import { Button } from '../../../../base/browser/ui/positronComponents/button/button.js';
+import { ContentArea } from './components/contentArea.js';
+import { PositronModalDialog, PositronModalDialogProps } from './positronModalDialog.js';
+import { PlatformNativeDialogActionBar } from './components/platformNativeDialogActionBar.js';
+
+/**
+ * ConfirmDeleteModalDialogProps interface.
+ */
+export interface ConfirmDeleteModalDialogProps extends PositronModalDialogProps {
+	title: string;
+	cancelButtonTitle?: string;
+	deleteActionTitle?: string
+	onCancel: () => (void | Promise<void>);
+	onDeleteAction: () => (void | Promise<void>);
+}
+
+/**
+ * ConfirmDeleteModalDialog component.
+ * @param props A PropsWithChildren<ConfirmDeleteModalDialogProps> that contains the component
+ * properties.
+ * @returns The rendered component.
+ */
+export const ConfirmDeleteModalDialog = (props: PropsWithChildren<ConfirmDeleteModalDialogProps>) => {
+	const cancelButton = (
+		<Button
+			className='action-bar-button default'
+			onPressed={async () => await props.onCancel()}
+		>
+			{props.cancelButtonTitle ?? localize('positron.cancel', "Cancel")}
+		</Button>
+	);
+	const deleteButton = (
+		<Button
+			className='action-bar-button destructive'
+			onPressed={async () => await props.onDeleteAction()}
+		>
+			{props.deleteActionTitle ?? localize('positron.delete', "Delete")}
+		</Button>
+	);
+
+	// Render.
+	return (
+		<PositronModalDialog {...props}>
+			<ContentArea>
+				{props.children}
+			</ContentArea>
+			<div className='action-bar top-separator'>
+				<div className='left-actions'>
+				</div>
+				<div className='right-actions'>
+					<PlatformNativeDialogActionBar secondaryButton={deleteButton} primaryButton={cancelButton} />
+				</div>
+			</div>
+		</PositronModalDialog>
+	);
+};
