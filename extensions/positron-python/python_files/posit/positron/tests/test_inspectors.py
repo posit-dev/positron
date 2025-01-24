fbefc9630a8b4677289953bef6722a1baa5b929b
isabelizimm <isabel.zimmerman@posit.co>
2025-01-24 12:12:26 -0500

    add overlay and xfail test

--- extensions/positron-python/python_files/posit/positron/tests/test_inspectors.py
+++ extensions/positron-python/python_files/posit/positron/tests/test_inspectors.py
@@ -910,14 +910,12 @@ def test_get_child(value: Any, key: Any, expected: Any) -> None:
     child = get_inspector(value).get_child(key)
     assert get_inspector(child).equals(expected)
 
-
+# TODO: 3.13 maint. run this test once ibis is available in 3.13
+@pytest.mark.xfail(sys.version_info > (3, 12), reason="ibis not available in 3.13", strict=True)
 @pytest.mark.skipif(sys.version_info < (3, 10), reason="requires Python 3.10 or higher")
 def test_inspect_ibis_exprs() -> None:
-    try:
-        import ibis  # type: ignore [reportMissingImports] for 3.13
-    except ImportError:
-        return  # skip this test if ibis is not installed
-
+    import ibis
+    
     # Make sure we don't return an executed repr
     ibis.options.interactive = True
 
