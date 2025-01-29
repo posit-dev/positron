24e3c76607f0da89aad6e67943cc61cefc7d9e54
Jonathan <jonathan@rstudio.com>
2025-01-29 07:09:23 -0800

    Don't veto shutdown in web mode (2025.01 backport of https://github.com/posit-dev/positron/pull/6091)  (#6092)
    This PR is a backport of https://github.com/posit-dev/positron/pull/6091
    to the 2025.01 release branch; see that PR for notes.
    
    ---------
    
    Co-authored-by: petetronic <1178561+petetronic@users.noreply.github.com>

--- src/vs/workbench/services/runtimeStartup/common/runtimeStartup.ts
+++ src/vs/workbench/services/runtimeStartup/common/runtimeStartup.ts
@@ -26,6 +26,7 @@ import { ICommandService } from '../../../../platform/commands/common/commands.j
 import { URI } from '../../../../base/common/uri.js';
 import { IWorkspaceContextService, WorkbenchState } from '../../../../platform/workspace/common/workspace.js';
 import { IPositronNewProjectService } from '../../positronNewProject/common/positronNewProject.js';
+import { isWeb } from '../../../../base/common/platform.js';
 
 interface ILanguageRuntimeProviderMetadata {
 	languageId: string;
@@ -306,7 +307,14 @@ export class RuntimeStartupService extends Disposable implements IRuntimeStartup
 				// storage, but it is possible that this workspace will be
 				// re-opened without an interleaving quit (e.g. if multiple
 				// Positron windows are open).
-				e.veto(this.clearWorkspaceSessions(), 'positron.runtimeStartup.clearWorkspaceSessions');
+				//
+				// We don't do this in web mode because async shutdown
+				// operations aren't supported on the web, and if used will
+				// trigger a browser warning when the user attempts to navigate
+				// away.
+				if (!isWeb) {
+					e.veto(this.clearWorkspaceSessions(), 'positron.runtimeStartup.clearWorkspaceSessions');
+				}
 			}
 		}));
 	}
