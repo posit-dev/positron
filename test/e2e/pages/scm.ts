15c339939a905375917042866ac9021f9b6150d5
Marie Idleman <marie.idleman@posit.co>
2025-01-30 11:28:10 -0600

    simplify

--- test/e2e/pages/scm.ts
+++ test/e2e/pages/scm.ts
@@ -13,7 +13,6 @@ import { Layouts } from './layouts';
 
 const VIEWLET = 'div[id="workbench.view.scm"]';
 const SCM_INPUT_TEXTAREA = `${VIEWLET} .scm-editor textarea`;
-const SCM_RESOURCE = `${VIEWLET} .monaco-list-row .resource`;
 const SCM_RESOURCE_CLICK = (name: string) => `${VIEWLET} .monaco-list-row .resource .monaco-icon-label[aria-label*="${name}"] .label-name`;
 const SCM_RESOURCE_ACTION_CLICK = (name: string, actionName: string) => `.monaco-list-row .resource .monaco-icon-label[aria-label*="${name}"] .actions .action-label[aria-label="${actionName}"]`;
 const COMMIT_COMMAND = `div[id="workbench.parts.sidebar"] .actions-container a.action-label[aria-label="Commit"]`;
@@ -29,33 +28,16 @@ export class SCM {
 	}
 
 	async waitForChange(name: string, type: string): Promise<void> {
-		await expect(async () => {
+		await this.layout.enterLayout('fullSizedSidebar');
 
-			await this.layout.enterLayout('fullSizedSidebar');
+		const tooltip = type === 'Staged' ? 'Index Modified' : 'Modified';
+		const locator = this.code.driver.page
+			.getByLabel('Source Control Management')
+			.locator(`[data-tooltip="${tooltip}"] .file-icon`)
+			.filter({ hasText: name });
 
-			const scmResources = await this.code.driver.page.locator(SCM_RESOURCE).all();
-
-			const resources: { name: string; type: string }[] = [];
-			for (const resource of scmResources) {
-				const text = await resource.locator('.label-name').textContent();
-				const tooltip = await resource.getAttribute('data-tooltip') || '';
-
-				if (text !== null) {
-					resources.push({ name: text.trim(), type: tooltip.trim() });
-				}
-			}
-
-			// debug
-			resources.forEach(resource => this.code.logger.log(`Name: ${resource.name}, Type: ${resource.type}`));
-
-			// Check if at least one resource matches both name and type
-			expect(resources).toEqual(
-				expect.arrayContaining([{ name, type }])
-			);
-
-			await this.layout.enterLayout('stacked');
-
-		}).toPass({ timeout: 20000 });
+		await expect(locator).toBeVisible();
+		await this.layout.enterLayout('stacked');
 	}
 
 	async openChange(name: string): Promise<void> {
