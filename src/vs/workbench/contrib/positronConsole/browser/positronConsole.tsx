15c2e7eac09642f3af23001c631306d5301b371c
Brian Lambert <brianlambert@gmail.com>
2025-01-30 19:46:49 -0700

    Adjust dependencies

--- src/vs/workbench/contrib/positronConsole/browser/positronConsole.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/positronConsole.tsx
@@ -50,7 +50,7 @@ export const PositronConsole = (props: PropsWithChildren<PositronConsoleProps>) 
 
 		// Return the cleanup function that will dispose of the event handlers.
 		return () => disposableStore.dispose();
-	}, []);
+	}, [props.reactComponentContainer]);
 
 	// Render.
 	return (
