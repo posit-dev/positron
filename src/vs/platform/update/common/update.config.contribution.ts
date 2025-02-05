afaf9bfca14b8bb99c5e2c7d4efdaf25fac86f8f
Tim Mok <timtmok@users.noreply.github.com>
2025-02-05 15:42:22 -0500

    Enable update checks and allow auto-updates (#6194)

--- src/vs/platform/update/common/update.config.contribution.ts
+++ src/vs/platform/update/common/update.config.contribution.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { isWeb, isWindows } from '../../../base/common/platform.js';
+import { isMacintosh, isWeb, isWindows } from '../../../base/common/platform.js';
 import { localize } from '../../../nls.js';
 import { ConfigurationScope, Extensions as ConfigurationExtensions, IConfigurationRegistry } from '../../configuration/common/configurationRegistry.js';
 import { Registry } from '../../registry/common/platform.js';
@@ -31,15 +31,16 @@ configurationRegistry.registerConfiguration({
 			],
 			policy: {
 				name: 'UpdateMode',
-				minimumVersion: '2025.1.0',
+				minimumVersion: '2025.02.0',
 			}
 		},
-		'update.autoUpdateExperimental': {
+		'update.autoUpdate': {
 			type: 'boolean',
 			default: false,
 			scope: ConfigurationScope.APPLICATION,
-			description: localize('experimentalAutoUpdate', "CAUTION: Enable automatic update checking. Requires a restart after change to take effect."),
+			description: localize('autoUpdateEnable', "Enable automatic updates. Requires a restart after change to take effect."),
 			tags: ['usesOnlineServices'],
+			included: (isWindows || isMacintosh) && !isWeb
 		},
 		'update.channel': {
 			type: 'string',
