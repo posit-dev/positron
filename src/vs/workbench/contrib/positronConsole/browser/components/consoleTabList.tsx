4abc9b71495fa54f886814a53a69d50e47b0c7f3
Sam Clark <sam.clark@posit.co>
2025-02-26 08:47:10 -0600

    Update console tab list to use state to track button disablement

--- src/vs/workbench/contrib/positronConsole/browser/components/consoleTabList.tsx
+++ src/vs/workbench/contrib/positronConsole/browser/components/consoleTabList.tsx
@@ -7,12 +7,63 @@
 import './consoleTabList.css';
 
 // React.
-import React from 'react';
+import React, { useState } from 'react';
 
 // Other dependencies.
 import { usePositronConsoleContext } from '../positronConsoleContext.js';
 import { ConsoleInstanceState } from './consoleInstanceState.js';
 import { IPositronConsoleInstance } from '../../../../services/positronConsole/browser/interfaces/positronConsoleService';
+
+
+interface ConsoleTabProps {
+	positronConsoleInstance: IPositronConsoleInstance;
+	onClick: (instance: IPositronConsoleInstance) => void;
+}
+
+const ConsoleTab = ({ positronConsoleInstance, onClick }: ConsoleTabProps) => {
+	const positronConsoleContext = usePositronConsoleContext();
+	const [deleteDisabled, setDeleteDisabled] = useState(false);
+
+	const handleTabDeleteClick = async (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>, consoleInstance: IPositronConsoleInstance) => {
+		// evt.preventDefault();
+		evt.stopPropagation();
+
+		// Prevent the button from being clicked multiple times
+		setDeleteDisabled(true);
+		try {
+			await positronConsoleContext.runtimeSessionService.deleteSession(consoleInstance.session.sessionId);
+		} catch (error) {
+			positronConsoleContext.notificationService.error(`Failed to delete session: ${error}`);
+		} finally {
+			// Re-enable the button when done deleting (should not exist anymore)
+			setDeleteDisabled(false);
+		}
+	}
+
+	return (<div
+		key={`tab-${positronConsoleInstance.session.sessionId}`}
+		aria-label={positronConsoleInstance.session.metadata.sessionName}
+		aria-labelledby={`console-panel-${positronConsoleInstance.session.sessionId}`}
+		aria-selected={positronConsoleContext.activePositronConsoleInstance?.session.sessionId === positronConsoleInstance.session.sessionId}
+		className={`tab-button ${positronConsoleContext.activePositronConsoleInstance?.session.sessionId === positronConsoleInstance.session.sessionId && 'tab-button--active'}`}
+		data-testid={`console-tab-${positronConsoleInstance.session.sessionId}`}
+		role='tab'
+		onClick={() => onClick(positronConsoleInstance)}
+	>
+		<ConsoleInstanceState positronConsoleInstance={positronConsoleInstance} />
+		<img
+			className='icon'
+			src={`data:image/svg+xml;base64,${positronConsoleInstance.session.runtimeMetadata.base64EncodedIconSvg}`}
+		/>
+		<p className='session-name'>
+			{positronConsoleInstance.session.metadata.sessionName}
+		</p>
+		<button className='close-button' disabled={deleteDisabled} onClick={evt => handleTabDeleteClick(evt, positronConsoleInstance)}>
+			<span className='codicon codicon-trash' />
+		</button>
+	</div>)
+}
+
 
 // ConsoleCoreProps interface.
 interface ConsoleTabListProps {
@@ -44,19 +95,6 @@ export const ConsoleTabList = (props: ConsoleTabListProps) => {
 		onChangeForegroundSession(sessionId);
 	};
 
-	const handleTabDeleteClick = async (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>, consoleInstance: IPositronConsoleInstance) => {
-		evt.preventDefault();
-		evt.stopPropagation();
-
-		// Prevent the button from being clicked multiple times
-		evt.currentTarget.disabled = true;
-
-		await positronConsoleContext.runtimeSessionService.deleteSession(consoleInstance.session.sessionId);
-
-		// Re-enable the button when done deleting (should not exist anymore)
-		evt.currentTarget.disabled = false;
-	}
-
 	// Sort console sessions by created time, oldest to newest
 	const consoleInstances = Array.from(positronConsoleContext.positronConsoleInstances.values()).sort((a, b) => {
 		return a.session.metadata.createdTimestamp - b.session.metadata.createdTimestamp;
@@ -69,30 +107,7 @@ export const ConsoleTabList = (props: ConsoleTabListProps) => {
 			role='tablist'
 			style={{ height: props.height, width: props.width }}
 		>
-			{consoleInstances.map((positronConsoleInstance) => (
-				<div
-					key={`tab-${positronConsoleInstance.session.sessionId}`}
-					aria-label={positronConsoleInstance.session.metadata.sessionName}
-					aria-labelledby={`console-panel-${positronConsoleInstance.session.sessionId}`}
-					aria-selected={positronConsoleContext.activePositronConsoleInstance?.session.sessionId === positronConsoleInstance.session.sessionId}
-					className={`tab-button ${positronConsoleContext.activePositronConsoleInstance?.session.sessionId === positronConsoleInstance.session.sessionId && 'tab-button--active'}`}
-					data-testid={`console-tab-${positronConsoleInstance.session.sessionId}`}
-					role='tab'
-					onClick={() => handleTabClick(positronConsoleInstance.session.sessionId)}
-				>
-					<ConsoleInstanceState positronConsoleInstance={positronConsoleInstance} />
-					<img
-						className='icon'
-						src={`data:image/svg+xml;base64,${positronConsoleInstance.session.runtimeMetadata.base64EncodedIconSvg}`}
-					/>
-					<p className='session-name'>
-						{positronConsoleInstance.session.metadata.sessionName}
-					</p>
-					<button className='close-button' onClick={evt => handleTabDeleteClick(evt, positronConsoleInstance)}>
-						<span className='codicon codicon-trash' />
-					</button>
-				</div>
-			))}
+			{consoleInstances.map((positronConsoleInstance) => <ConsoleTab key={positronConsoleInstance.session.sessionId} positronConsoleInstance={positronConsoleInstance} onClick={() => handleTabClick(positronConsoleInstance.session.sessionId)} />)}
 		</div>
 	)
 }
