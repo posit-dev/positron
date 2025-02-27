ba1033d15b937df01279eddaf20a73e7c7eb3ca4
Dianyi Yang <105025148+kv9898@users.noreply.github.com>
2025-02-27 23:34:16 +0800

    Add layout settings

--- src/vs/workbench/services/positronDataExplorer/common/positronDataExplorerSummary.ts
+++ src/vs/workbench/services/positronDataExplorer/common/positronDataExplorerSummary.ts
@@ -12,10 +12,13 @@ import {
 } from '../../../../platform/configuration/common/configurationRegistry.js';
 import { Registry } from '../../../../platform/registry/common/platform.js';
 import { positronConfigurationNodeBase } from '../../languageRuntime/common/languageRuntime.js';
+import { PositronDataExplorerLayout } from '../browser/interfaces/positronDataExplorerService.js';
 
 // Key for the configuration setting
 export const USE_POSITRON_DATA_EXPLORER_SUMMARY_COLLAPSE_KEY =
 	'positron.dataExplorerSummaryCollapse';
+export const USE_POSITRON_DATA_EXPLORER_SUMMARY_LAYOUT_KEY =
+	'positron.dataExplorerSummaryLayout';
 
 /**
  * Retrieves the value of the configuration setting that determines whether to enable
@@ -32,11 +35,23 @@ export function DataExplorerSummaryCollapseEnabled(
 	);
 }
 
+export function DefaultDataExplorerSummaryLayout(
+	configurationService: IConfigurationService
+) {
+	if (String(
+		configurationService.getValue(USE_POSITRON_DATA_EXPLORER_SUMMARY_LAYOUT_KEY)
+	) === 'Left') {
+		return PositronDataExplorerLayout.SummaryOnLeft;
+	} else {
+		return PositronDataExplorerLayout.SummaryOnRight;
+	}
+}
+
 // Register the configuration setting
 const configurationRegistry = Registry.as<IConfigurationRegistry>(
 	Extensions.Configuration
 );
-configurationRegistry.registerConfiguration({
+configurationRegistry.registerConfiguration({ // for summary collapse
 	...positronConfigurationNodeBase,
 	scope: ConfigurationScope.MACHINE_OVERRIDABLE,
 	properties: {
@@ -46,6 +61,21 @@ configurationRegistry.registerConfiguration({
 			markdownDescription: localize(
 				'positron.enablePositronDataExplorerSummaryCollapse',
 				'Collapse Data Explorer Summary Panel by default.'
+			),
+		},
+	},
+});
+configurationRegistry.registerConfiguration({ // for summary layout
+	...positronConfigurationNodeBase,
+	scope: ConfigurationScope.MACHINE_OVERRIDABLE,
+	properties: {
+		[USE_POSITRON_DATA_EXPLORER_SUMMARY_LAYOUT_KEY]: {
+			type: 'string',
+			default: 'Left', // Default value (can be "left" or "right")
+			enum: ['Left', 'Right'], // Define possible values
+			markdownDescription: localize(
+				'positron.dataExplorerSummaryLayout',
+				'Select the position of the Data Explorer Summary Panel (left or right).'
 			),
 		},
 	},
