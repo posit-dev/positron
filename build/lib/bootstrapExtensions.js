posit-dev/positron

commit ef7ceff4c8dfccc9190d2067fc9184354e59f02f

Pre package extensions and install them on startup for users (#6403
Addresses #6302, #6301, #6305, #5315

Introduces the concept of "bootstrapped extensions". Bundles the
following extensions as VSIX files with Positron:
- Black
- Posit Publisher
- Shiny(Workbench only)
- Quarto
- ms-toolsai.jupyter-keymap
- ms-toolsai.vscode-jupyter-cell-tags
- ms-toolsai.vscode-jupyter-slideshow
- ms-toolsai.jupyter
- ms-pyright.pyright
- ms-python.debugpy

When a user starts up Positron after first installing it or a version
change (upgrade or downgrade), Positron will install the bootstrapped
extensions for the users. Users will then be able to manage the
extensions (upgrade, downgrade, uninstall, disable) themselves. Positron
will always prefer whichever version of the extension in newer (bundled
or user). When a user downgrades the extension, Positron will attempt to
upgrade to the latest packaged version on the next upgrade, but not next
Positron launch.

I changed the approach slightly from my initial spec. The initial spec
built on top of the existing
[defaultExtensionInitializer](https://github.com/posit-dev/positron/blob/main/src/vs/code/electron-utility/sharedProcess/contrib/defaultExtensionsInitializer.ts).
This approach is heavily inspired by that class but provides the server
and desktop logic in
src/vs/platform/extensionManagement/node/positronBootstrapExtensionsInitializer.ts.

### Release Notes

#### New Features

- Packages several extensions that were previously shipped a built-in
extensions as user managed extensions that are installed on first
launch.

### QA Notes
@:web

build/lib/bootstrapExtensions.js
@@ -1,0 +1,139 @@
+"use strict";
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getBootstrapExtensionStream = getBootstrapExtensionStream;
+exports.getBootstrapExtensions = getBootstrapExtensions;
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const rimraf = require("rimraf");
+const es = require("event-stream");
+const rename = require("gulp-rename");
+const vfs = require("vinyl-fs");
+const ext = require("./extensions");
+const fancyLog = require("fancy-log");
+const ansiColors = require("ansi-colors");
+const root = path.dirname(path.dirname(__dirname));
+const productjson = JSON.parse(fs.readFileSync(path.join(__dirname, '../../product.json'), 'utf8'));
+const ENABLE_LOGGING = !process.env['VSCODE_BUILD_BOOTSTRAP_EXTENSIONS_SILENCE_PLEASE'];
+const bootstrapExtensions = productjson.bootstrapExtensions || [];
+const controlFilePath = path.join(os.homedir(), '.vscode-oss-dev', 'extensions', 'bootstrap-control.json');
+function log(...messages) {
+    if (ENABLE_LOGGING) {
+        fancyLog(...messages);
+    }
+}
+function getExtensionPath(extension) {
+    return path.join(root, '.build', 'bootstrapExtensions', extension.name);
+}
+function isUpToDate(extension) {
+    const packagePath = path.join(getExtensionPath(extension), 'package.json');
+    if (!fs.existsSync(packagePath)) {
+        return false;
+    }
+    const packageContents = fs.readFileSync(packagePath, { encoding: 'utf8' });
+    try {
+        const diskVersion = JSON.parse(packageContents).version;
+        return (diskVersion === extension.version);
+    }
+    catch (err) {
+        return false;
+    }
+}
+function getExtensionDownloadStream(extension) {
+    const url = extension.metadata.multiPlatformServiceUrl || productjson.extensionsGallery?.serviceUrl;
+    return (url ? ext.fromMarketplace(url, extension, true) : ext.fromGithub(extension))
+        .pipe(rename(p => { p.basename = `${extension.name}-${extension.version}.vsix`; }));
+}
+function getBootstrapExtensionStream(extension) {
+    // if the extension exists on disk, use those files instead of downloading anew
+    if (isUpToDate(extension)) {
+        log('[extensions]', `${extension.name}@${extension.version} up to date`, ansiColors.green('✔︎'));
+        return vfs.src(['**'], { cwd: getExtensionPath(extension), dot: true })
+            .pipe(rename(p => p.dirname = `${extension.name}/${p.dirname}`));
+    }
+    return getExtensionDownloadStream(extension);
+}
+function syncMarketplaceExtension(extension) {
+    const galleryServiceUrl = productjson.extensionsGallery?.serviceUrl;
+    const source = ansiColors.blue(galleryServiceUrl ? '[marketplace]' : '[github]');
+    if (isUpToDate(extension)) {
+        log(source, `${extension.name}@${extension.version}`, ansiColors.green('✔︎'));
+        return es.readArray([]);
+    }
+    rimraf.sync(getExtensionPath(extension));
+    return getExtensionDownloadStream(extension)
+        .pipe(vfs.dest('.build/bootstrapExtensions'))
+        .on('end', () => log(source, extension.name, ansiColors.green('✔︎')));
+}
+function syncExtension(extension, controlState) {
+    if (extension.platforms) {
+        const platforms = new Set(extension.platforms);
+        if (!platforms.has(process.platform)) {
+            log(ansiColors.gray('[skip]'), `${extension.name}@${extension.version}: Platform '${process.platform}' not supported: [${extension.platforms}]`, ansiColors.green('✔︎'));
+            return es.readArray([]);
+        }
+    }
+    switch (controlState) {
+        case 'disabled':
+            log(ansiColors.blue('[disabled]'), ansiColors.gray(extension.name));
+            return es.readArray([]);
+        case 'marketplace':
+            return syncMarketplaceExtension(extension);
+        default:
+            if (!fs.existsSync(controlState)) {
+                log(ansiColors.red(`Error: Bootstrap extension '${extension.name}' is configured to run from '${controlState}' but that path does not exist.`));
+                return es.readArray([]);
+            }
+            else if (!fs.existsSync(path.join(controlState, 'package.json'))) {
+                log(ansiColors.red(`Error: Bootstrap extension '${extension.name}' is configured to run from '${controlState}' but there is no 'package.json' file in that directory.`));
+                return es.readArray([]);
+            }
+            log(ansiColors.blue('[local]'), `${extension.name}: ${ansiColors.cyan(controlState)}`, ansiColors.green('✔︎'));
+            return es.readArray([]);
+    }
+}
+function readControlFile() {
+    try {
+        return JSON.parse(fs.readFileSync(controlFilePath, 'utf8'));
+    }
+    catch (err) {
+        return {};
+    }
+}
+function writeControlFile(control, filePath) {
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    fs.writeFileSync(filePath, JSON.stringify(control, null, 2));
+}
+function getBootstrapExtensions() {
+    const control = readControlFile();
+    const streams = [];
+    for (const extension of [...bootstrapExtensions]) {
+        const controlState = control[extension.name] || 'marketplace';
+        control[extension.name] = controlState;
+        // Discard extensions intended for the web. The 'type' field isn't a
+        // formal part of the extension definition but a custom field we use to
+        // filter out web-only extensions (i.e. Posit Workbench)
+        // @ts-ignore
+        if (extension.type === 'reh-web') {
+            continue;
+        }
+        streams.push(syncExtension(extension, controlState));
+    }
+    writeControlFile(control, controlFilePath);
+    return new Promise((resolve, reject) => {
+        es.merge(streams)
+            .on('error', reject)
+            .on('end', resolve);
+    });
+}
+if (require.main === module) {
+    getBootstrapExtensions().then(() => process.exit(0)).catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+}
+//# sourceMappingURL=bootstrapExtensions.js.map

build/lib/bootstrapExtensions.ts
@@ -1,0 +1,169 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+import * as rimraf from 'rimraf';
+import * as es from 'event-stream';
+import * as rename from 'gulp-rename';
+import * as vfs from 'vinyl-fs';
+import * as ext from './extensions';
+import * as fancyLog from 'fancy-log';
+import * as ansiColors from 'ansi-colors';
+import { Stream } from 'stream';
+import { IExtensionDefinition } from './builtInExtensions';
+
+const root = path.dirname(path.dirname(__dirname));
+const productjson = JSON.parse(fs.readFileSync(path.join(__dirname, '../../product.json'), 'utf8'));
+const ENABLE_LOGGING = !process.env['VSCODE_BUILD_BOOTSTRAP_EXTENSIONS_SILENCE_PLEASE'];
+
+const bootstrapExtensions = <IExtensionDefinition[]>productjson.bootstrapExtensions || [];
+const controlFilePath = path.join(os.homedir(), '.vscode-oss-dev', 'extensions', 'bootstrap-control.json');
+
+function log(...messages: string[]): void {
+	if (ENABLE_LOGGING) {
+		fancyLog(...messages);
+	}
+}
+
+function getExtensionPath(extension: IExtensionDefinition): string {
+	return path.join(root, '.build', 'bootstrapExtensions', extension.name);
+}
+
+function isUpToDate(extension: IExtensionDefinition): boolean {
+	const packagePath = path.join(getExtensionPath(extension), 'package.json');
+
+	if (!fs.existsSync(packagePath)) {
+		return false;
+	}
+
+	const packageContents = fs.readFileSync(packagePath, { encoding: 'utf8' });
+
+	try {
+		const diskVersion = JSON.parse(packageContents).version;
+		return (diskVersion === extension.version);
+	} catch (err) {
+		return false;
+	}
+}
+
+function getExtensionDownloadStream(extension: IExtensionDefinition) {
+	const url = extension.metadata.multiPlatformServiceUrl || productjson.extensionsGallery?.serviceUrl;
+	return (url ? ext.fromMarketplace(url, extension, true) : ext.fromGithub(extension))
+		.pipe(rename(p => { p.basename = `${extension.name}-${extension.version}.vsix`; }));
+}
+
+export function getBootstrapExtensionStream(extension: IExtensionDefinition) {
+	// if the extension exists on disk, use those files instead of downloading anew
+	if (isUpToDate(extension)) {
+		log('[extensions]', `${extension.name}@${extension.version} up to date`, ansiColors.green('✔︎'));
+		return vfs.src(['**'], { cwd: getExtensionPath(extension), dot: true })
+			.pipe(rename(p => p.dirname = `${extension.name}/${p.dirname}`));
+	}
+
+	return getExtensionDownloadStream(extension);
+}
+
+function syncMarketplaceExtension(extension: IExtensionDefinition): Stream {
+	const galleryServiceUrl = productjson.extensionsGallery?.serviceUrl;
+	const source = ansiColors.blue(galleryServiceUrl ? '[marketplace]' : '[github]');
+	if (isUpToDate(extension)) {
+		log(source, `${extension.name}@${extension.version}`, ansiColors.green('✔︎'));
+		return es.readArray([]);
+	}
+
+	rimraf.sync(getExtensionPath(extension));
+
+	return getExtensionDownloadStream(extension)
+		.pipe(vfs.dest('.build/bootstrapExtensions'))
+		.on('end', () => log(source, extension.name, ansiColors.green('✔︎')));
+}
+
+function syncExtension(extension: IExtensionDefinition, controlState: 'disabled' | 'marketplace'): Stream {
+	if (extension.platforms) {
+		const platforms = new Set(extension.platforms);
+
+		if (!platforms.has(process.platform)) {
+			log(ansiColors.gray('[skip]'), `${extension.name}@${extension.version}: Platform '${process.platform}' not supported: [${extension.platforms}]`, ansiColors.green('✔︎'));
+			return es.readArray([]);
+		}
+	}
+
+	switch (controlState) {
+		case 'disabled':
+			log(ansiColors.blue('[disabled]'), ansiColors.gray(extension.name));
+			return es.readArray([]);
+
+		case 'marketplace':
+			return syncMarketplaceExtension(extension);
+
+		default:
+			if (!fs.existsSync(controlState)) {
+				log(ansiColors.red(`Error: Bootstrap extension '${extension.name}' is configured to run from '${controlState}' but that path does not exist.`));
+				return es.readArray([]);
+
+			} else if (!fs.existsSync(path.join(controlState, 'package.json'))) {
+				log(ansiColors.red(`Error: Bootstrap extension '${extension.name}' is configured to run from '${controlState}' but there is no 'package.json' file in that directory.`));
+				return es.readArray([]);
+			}
+
+			log(ansiColors.blue('[local]'), `${extension.name}: ${ansiColors.cyan(controlState)}`, ansiColors.green('✔︎'));
+			return es.readArray([]);
+	}
+}
+
+interface IControlFile {
+	[name: string]: 'disabled' | 'marketplace';
+}
+
+function readControlFile(): IControlFile {
+	try {
+		return JSON.parse(fs.readFileSync(controlFilePath, 'utf8'));
+	} catch (err) {
+		return {};
+	}
+}
+
+function writeControlFile(control: IControlFile, filePath: string): void {
+	fs.mkdirSync(path.dirname(filePath), { recursive: true });
+	fs.writeFileSync(filePath, JSON.stringify(control, null, 2));
+}
+
+export function getBootstrapExtensions(): Promise<void> {
+
+	const control = readControlFile();
+	const streams: Stream[] = [];
+
+	for (const extension of [...bootstrapExtensions]) {
+		const controlState = control[extension.name] || 'marketplace';
+		control[extension.name] = controlState;
+
+		// Discard extensions intended for the web. The 'type' field isn't a
+		// formal part of the extension definition but a custom field we use to
+		// filter out web-only extensions (i.e. Posit Workbench)
+		// @ts-ignore
+		if (extension.type === 'reh-web') {
+			continue;
+		}
+
+		streams.push(syncExtension(extension, controlState));
+	}
+
+	writeControlFile(control, controlFilePath);
+
+	return new Promise((resolve, reject) => {
+		es.merge(streams)
+			.on('error', reject)
+			.on('end', resolve);
+	});
+}
+
+if (require.main === module) {
+	getBootstrapExtensions().then(() => process.exit(0)).catch(err => {
+		console.error(err);
+		process.exit(1);
+	});
+}

build/gulpfile.extensions.js
@@ -300,12 +300,19 @@ exports.cleanExtensionsBuildTask = cleanExtensionsBuildTask;
  */
 const bundleMarketplaceExtensionsBuildTask = task.define('bundle-marketplace-extensions-build', () => ext.packageMarketplaceExtensionsStream(false).pipe(gulp.dest('.build')));
 
+// --- Start Positron ---
+const bundleBootstrapExtensionsBuildTask = task.define('bundle-bootstrap-extensions-build', () => ext.packageBootstrapExtensionsStream().pipe(gulp.dest('.build')));
+// --- End Positron ---
+
 /**
  * Compiles the non-native extensions for the build
  * @note this does not clean the directory ahead of it. See {@link cleanExtensionsBuildTask} for that.
  */
 const compileNonNativeExtensionsBuildTask = task.define('compile-non-native-extensions-build', task.series(
 	bundleMarketplaceExtensionsBuildTask,
+	// --- Start Positron ---
+	bundleBootstrapExtensionsBuildTask,
+	// --- End Positron ---
 	task.define('bundle-non-native-extensions-build', () => ext.packageNonNativeLocalExtensionsStream().pipe(gulp.dest('.build')))
 ));
 gulp.task(compileNonNativeExtensionsBuildTask);
@@ -326,6 +333,9 @@ exports.compileNativeExtensionsBuildTask = compileNativeExtensionsBuildTask;
 const compileAllExtensionsBuildTask = task.define('compile-extensions-build', task.series(
 	cleanExtensionsBuildTask,
 	bundleMarketplaceExtensionsBuildTask,
+	// --- Start Positron ---
+	bundleBootstrapExtensionsBuildTask,
+	// --- End Positron ---
 	task.define('bundle-extensions-build', () => ext.packageAllLocalExtensionsStream(false, false).pipe(gulp.dest('.build'))),
 	// --- Start Positron ---
 	copyExtensionBinariesTask
@@ -341,6 +351,9 @@ gulp.task(task.define('extensions-ci', task.series(compileNonNativeExtensionsBui
 const compileExtensionsBuildPullRequestTask = task.define('compile-extensions-build-pr', task.series(
 	cleanExtensionsBuildTask,
 	bundleMarketplaceExtensionsBuildTask,
+	// --- Start Positron ---
+	bundleBootstrapExtensionsBuildTask,
+	// --- End Positron ---
 	task.define('bundle-extensions-build-pr', () => ext.packageAllLocalExtensionsStream(false, true).pipe(gulp.dest('.build'))),
 ));
 gulp.task(compileExtensionsBuildPullRequestTask);

build/gulpfile.reh.js
@@ -320,7 +320,20 @@ function packageTask(type, platform, arch, sourceFolderName, destinationFolderNa
 		const extensionPaths = [...localWorkspaceExtensions, ...marketplaceExtensions]
 			.map(name => `.build/extensions/${name}/**`);
 
-		const extensions = gulp.src(extensionPaths, { base: '.build', dot: true });
+		//const extensions = gulp.src(extensionPaths, { base: '.build', dot: true });
+		// --- Start Positron ---
+
+		const bootstrapExtensions = JSON.parse(fs.readFileSync(path.join(REPO_ROOT, 'product.json'), 'utf8')).bootstrapExtensions
+			.filter(entry => !entry.platforms || new Set(entry.platforms).has(platform))
+			.filter(entry => !entry.type || entry.type === type)
+			.map(entry => entry.name);
+		const bootstrapExtensionPaths = [...bootstrapExtensions]
+			.map(name => `.build/extensions/bootstrap/${name}*.vsix`);
+
+		const extensions = gulp.src([...extensionPaths, ...bootstrapExtensionPaths], { base: '.build', dot: true });
+
+		// --- End Positron ---
+
 		const extensionsCommonDependencies = gulp.src('.build/extensions/node_modules/**', { base: '.build', dot: true });
 		const sources = es.merge(src, extensions, extensionsCommonDependencies)
 			.pipe(filter(['**', '!**/*.js.map'], { dot: true }));

build/lib/extensions.js
@@ -11,6 +11,7 @@ exports.packageNonNativeLocalExtensionsStream = packageNonNativeLocalExtensionsS
 exports.packageNativeLocalExtensionsStream = packageNativeLocalExtensionsStream;
 exports.packageAllLocalExtensionsStream = packageAllLocalExtensionsStream;
 exports.packageMarketplaceExtensionsStream = packageMarketplaceExtensionsStream;
+exports.packageBootstrapExtensionsStream = packageBootstrapExtensionsStream;
 exports.scanBuiltinExtensions = scanBuiltinExtensions;
 exports.translatePackageJSON = translatePackageJSON;
 exports.webpackExtensions = webpackExtensions;
@@ -34,6 +35,7 @@ const buffer = require('gulp-buffer');
 const jsoncParser = require("jsonc-parser");
 const dependencies_1 = require("./dependencies");
 const builtInExtensions_1 = require("./builtInExtensions");
+const bootstrapExtensions_1 = require("./bootstrapExtensions");
 const getVersion_1 = require("./getVersion");
 const fetch_1 = require("./fetch");
 // --- Start Positron ---
@@ -234,7 +236,9 @@ const baseHeaders = {
     'User-Agent': userAgent,
     'X-Market-User-Id': '291C1CD0-051A-4123-9B4B-30D60EF52EE2',
 };
-function fromMarketplace(serviceUrl, { name: extensionName, version, sha256, metadata }) {
+// --- Start Positron ---
+function fromMarketplace(serviceUrl, { name: extensionName, version, sha256, metadata }, bootstrap = false) {
+    // --- End Positron ---
     const json = require('gulp-json-editor');
     const [publisher, name] = extensionName.split('.');
     // --- Start Positron ---
@@ -263,20 +267,34 @@ function fromMarketplace(serviceUrl, { name: extensionName, version, sha256, met
     // --- End Positron ---
     fancyLog('Downloading extension:', ansiColors.yellow(`${extensionName}@${version}`), '...');
     const packageJsonFilter = filter('package.json', { restore: true });
-    return (0, fetch_1.fetchUrls)('', {
-        base: url,
-        nodeFetchOptions: {
-            headers: baseHeaders
-        },
-        checksumSha256: sha256
-    })
-        .pipe(vzip.src())
-        .pipe(filter('extension/**'))
-        .pipe(rename(p => p.dirname = p.dirname.replace(/^extension\/?/, '')))
-        .pipe(packageJsonFilter)
-        .pipe(buffer())
-        .pipe(json({ __metadata: metadata }))
-        .pipe(packageJsonFilter.restore);
+    // --- Start Positron ---
+    if (bootstrap) {
+        return (0, fetch_1.fetchUrls)('', {
+            base: url,
+            nodeFetchOptions: {
+                headers: baseHeaders
+            },
+            checksumSha256: sha256
+        })
+            .pipe(buffer());
+    }
+    else {
+        return (0, fetch_1.fetchUrls)('', {
+            base: url,
+            nodeFetchOptions: {
+                headers: baseHeaders
+            },
+            checksumSha256: sha256
+        })
+            .pipe(vzip.src())
+            .pipe(filter('extension/**'))
+            .pipe(rename(p => p.dirname = p.dirname.replace(/^extension\/?/, '')))
+            .pipe(packageJsonFilter)
+            .pipe(buffer())
+            .pipe(json({ __metadata: metadata }))
+            .pipe(packageJsonFilter.restore);
+    }
+    // --- End Positron ---
 }
 // --- Start PWB: Bundle PWB extension ---
 function fromPositUrl({ name: extensionName, version, sha256, positUrl, metadata }) {
@@ -348,6 +366,9 @@ const marketplaceWebExtensionsExclude = new Set([
 ]);
 const productJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../../product.json'), 'utf8'));
 const builtInExtensions = productJson.builtInExtensions || [];
+// --- Start Positron ---
+const bootstrapExtensions = productJson.bootstrapExtensions || [];
+// --- End Positron ---
 const webBuiltInExtensions = productJson.webBuiltInExtensions || [];
 /**
  * Loosely based on `getExtensionKind` from `src/vs/workbench/services/extensions/common/extensionManifestPropertiesService.ts`
@@ -479,6 +500,16 @@ function packageMarketplaceExtensionsStream(forWeb) {
     })));
     return (marketplaceExtensionsStream
         .pipe(util2.setExecutableBit(['**/*.sh'])));
+}
+// --- Start Positron ---
+function packageBootstrapExtensionsStream() {
+    return es.merge(...bootstrapExtensions
+        .map(extension => {
+        const src = (0, bootstrapExtensions_1.getBootstrapExtensionStream)(extension).pipe(rename(p => {
+            p.dirname = `extensions/bootstrap`;
+        }));
+        return src;
+    }));
 }
 function scanBuiltinExtensions(extensionsRoot, exclude = []) {
     const scannedExtensions = [];

commit 32d20f863749b5a329ec1962df4cbb8d490edd3b

Prevent bootstrap extensions from being downloaded with every dev build (#6619)
Bug introduced in https://github.com/posit-dev/positron/pull/6403

Rewrite the `isUpToDate` and `getExtensionsPath` functions to properly
identify the vsix files so they don't get downloaded if they already
exist. Also did some parameter clean up on another function.


### Release Notes


#### Bug Fixes

- Prevent bootstrap extensions from being downloaded on every dev build

build/lib/bootstrapExtensions.js
@@ -27,16 +27,21 @@ function log(...messages) {
     }
 }
 function getExtensionPath(extension) {
-    return path.join(root, '.build', 'bootstrapExtensions', extension.name);
+    return path.join(root, '.build', 'bootstrapExtensions', `${extension.name}-${extension.version}.vsix`);
 }
 function isUpToDate(extension) {
-    const packagePath = path.join(getExtensionPath(extension), 'package.json');
-    if (!fs.existsSync(packagePath)) {
+    const regex = new RegExp(`^${extension.name}-(\\d+\\.\\d+\\.\\d+)\\.vsix$`);
+    if (!fs.existsSync(path.join(root, '.build', 'bootstrapExtensions'))) {
         return false;
     }
-    const packageContents = fs.readFileSync(packagePath, { encoding: 'utf8' });
+    const files = fs.readdirSync(path.join(root, '.build', 'bootstrapExtensions'));
+    const vsixPath = files.find(f => regex.test(f));
+    if (!vsixPath) {
+        return false;
+    }
     try {
-        const diskVersion = JSON.parse(packageContents).version;
+        const match = vsixPath.match(regex);
+        const diskVersion = match ? match[1] : null;
         return (diskVersion === extension.version);
     }
     catch (err) {
@@ -104,9 +109,9 @@ function readControlFile() {
         return {};
     }
 }
-function writeControlFile(control, filePath) {
-    fs.mkdirSync(path.dirname(filePath), { recursive: true });
-    fs.writeFileSync(filePath, JSON.stringify(control, null, 2));
+function writeControlFile(control) {
+    fs.mkdirSync(path.dirname(controlFilePath), { recursive: true });
+    fs.writeFileSync(controlFilePath, JSON.stringify(control, null, 2));
 }
 function getBootstrapExtensions() {
     const control = readControlFile();
@@ -116,7 +121,7 @@ function getBootstrapExtensions() {
         control[extension.name] = controlState;
         streams.push(syncExtension(extension, controlState));
     }
-    writeControlFile(control, controlFilePath);
+    writeControlFile(control);
     return new Promise((resolve, reject) => {
         es.merge(streams)
             .on('error', reject)

build/lib/bootstrapExtensions.ts
@@ -30,20 +30,24 @@ function log(...messages: string[]): void {
 }
 
 function getExtensionPath(extension: IExtensionDefinition): string {
-	return path.join(root, '.build', 'bootstrapExtensions', extension.name);
+	return path.join(root, '.build', 'bootstrapExtensions', `${extension.name}-${extension.version}.vsix`);
 }
 
 function isUpToDate(extension: IExtensionDefinition): boolean {
-	const packagePath = path.join(getExtensionPath(extension), 'package.json');
+	const regex = new RegExp(`^${extension.name}-(\\d+\\.\\d+\\.\\d+)\\.vsix$`);
+	if (!fs.existsSync(path.join(root, '.build', 'bootstrapExtensions'))) {
+		return false;
+	}
+	const files = fs.readdirSync(path.join(root, '.build', 'bootstrapExtensions'));
+	const vsixPath = files.find(f => regex.test(f));
 
-	if (!fs.existsSync(packagePath)) {
+	if (!vsixPath) {
 		return false;
 	}
 
-	const packageContents = fs.readFileSync(packagePath, { encoding: 'utf8' });
-
 	try {
-		const diskVersion = JSON.parse(packageContents).version;
+		const match = vsixPath.match(regex);
+		const diskVersion = match ? match[1] : null;
 		return (diskVersion === extension.version);
 	} catch (err) {
 		return false;
@@ -127,9 +131,9 @@ function readControlFile(): IControlFile {
 	}
 }
 
-function writeControlFile(control: IControlFile, filePath: string): void {
-	fs.mkdirSync(path.dirname(filePath), { recursive: true });
-	fs.writeFileSync(filePath, JSON.stringify(control, null, 2));
+function writeControlFile(control: IControlFile): void {
+	fs.mkdirSync(path.dirname(controlFilePath), { recursive: true });
+	fs.writeFileSync(controlFilePath, JSON.stringify(control, null, 2));
 }
 
 export function getBootstrapExtensions(): Promise<void> {
@@ -144,7 +148,7 @@ export function getBootstrapExtensions(): Promise<void> {
 		streams.push(syncExtension(extension, controlState));
 	}
 
-	writeControlFile(control, controlFilePath);
+	writeControlFile(control);
 
 	return new Promise((resolve, reject) => {
 		es.merge(streams)