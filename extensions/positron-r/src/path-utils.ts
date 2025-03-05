896e6f35e9b164f5fe7ad5176fb79f4b1f2b96d9
sharon wang <sharon-wang@users.noreply.github.com>
2025-03-05 16:50:14 -0500

    implement `positron.r.interpreters.override` handling

--- extensions/positron-r/src/path-utils.ts
+++ extensions/positron-r/src/path-utils.ts
@@ -5,6 +5,7 @@
 
 import * as path from 'path';
 import * as os from 'os';
+import * as fs from 'fs';
 
 /**
  * Returns true if given file path exists within the given parent directory, false otherwise.
@@ -41,4 +42,49 @@ export function arePathsSame(path1: string, path2: string): boolean {
  */
 export function untildify(path: string): string {
 	return path.replace(/^~($|\/|\\)/, `${os.homedir()}$1`);
+}
+
+/**
+ * Check if a path is a file.
+ * A combination of isFile and resolveSymbolicLink in extensions/positron-python/src/client/pythonEnvironments/common/externalDependencies.ts.
+ * @param filePath The path to check
+ * @returns Whether the path is a file
+ */
+export function isFile(filePath: string): boolean {
+	try {
+		const stats = fs.lstatSync(filePath);
+		if (stats.isSymbolicLink()) {
+			let resolvedPath = fs.realpathSync(filePath);
+			// Stop at a maximum depth of 5 symbolic links to avoid infinite loops
+			const maximumDepth = 5;
+			for (let i = 0; i < maximumDepth; i++) {
+				const resolvedStats = fs.lstatSync(resolvedPath);
+				if (resolvedStats.isFile()) {
+					return true;
+				}
+				resolvedPath = fs.realpathSync(resolvedPath);
+			}
+			console.error(`[isFile] Detected a potential symbolic link loop at ${filePath}, terminating resolution.`);
+			return false;
+		}
+		return stats.isFile();
+	} catch (error) {
+		console.error(`[isFile] Error checking if path is a file: ${error}`);
+		return false;
+	}
+}
+
+/**
+ * Check if a path is a directory.
+ * @param filePath The path to check
+ * @returns Whether the path is a directory
+ */
+export function isDirectory(filePath: string): boolean {
+	try {
+		const stats = fs.lstatSync(filePath);
+		return stats.isDirectory();
+	} catch (error) {
+		console.error(`[isDirectory] Error checking if path is a directory: ${error}`);
+		return false;
+	}
 }
