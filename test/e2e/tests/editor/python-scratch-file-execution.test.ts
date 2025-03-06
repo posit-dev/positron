227ba82b4050fb5c174694127ab1d399de05af6f
Chris Mead <chris.mead@posit.co>
2025-03-06 11:33:23 -0700

    review comments addressed

--- test/e2e/tests/editor/python-scratch-file-execution.test.ts
+++ test/e2e/tests/editor/python-scratch-file-execution.test.ts
@@ -15,18 +15,21 @@ test.describe('Python Scratch File', { tag: [tags.WEB, tags.EDITOR, tags.WIN] },
 		const filename = 'Untitled-1';
 		await test.step('Create a new python scratch file with code and a magic', async () => {
 			await runCommand('python.createNewFile');
-			await app.workbench.editor.waitForTypeInEditor(filename, 'import pandas as pd');
+			await app.workbench.editor.waitForTypeInEditor(filename, 'print("test")');
 			await app.code.driver.page.keyboard.press('Enter');
 			await app.code.driver.page.keyboard.press('Enter');
-			await app.workbench.editor.waitForTypeInEditor(filename, 'pip install pyarrow');
+			await app.workbench.editor.waitForTypeInEditor(filename, '%pip install pyarrow');
 		});
 
 		await test.step('Exexcute first line of code', async () => {
-			await app.workbench.editor.clickOnTerm(filename, 'import', 1, true);
+			await app.workbench.editor.clickOnTerm(filename, 'print', 1, true);
 			await app.code.driver.page.keyboard.press('ArrowLeft');
 
 			await app.code.driver.page.keyboard.press('Control+Enter');
 		});
+
+		// ensure code execution worked
+		await app.workbench.console.waitForConsoleContents('test', { expectedCount: 2 });
 
 		// Ensure "deprecated" does not appear in the console
 		await app.workbench.console.waitForConsoleContents('deprecated', { timeout: 5000, expectedCount: 0 });
