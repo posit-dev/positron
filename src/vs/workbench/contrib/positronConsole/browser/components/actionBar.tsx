posit-dev/positron

commit 4ee3c31e8b6e0059d4dc1ff530e2d93449bc0f2a

Restore console history at startup (#6741)
This change persists the contents of the Console tab, so that reloading
the window or resuming your session on Posit Workbench restores the
history of the commands you have executed and their results.


https://github.com/user-attachments/assets/2e1118ce-7cec-47b0-9175-de0a5733b5dd

A few related improvements are made, too:

- Now that we have console contents, we show them right away at startup
when reloading or reconnecting -- even before extensions are started,
etc. (see video) This replaces the flickering "starting..." or
"preparing..." screens and makes reconnection feel snappier.
- Input history has been revamped; we now have separate histories for
navigation inside a session (e.g. up/down) vs. searching (e.g. cmd/ctrl
up). The former will take you to literally the previous thing you typed
in that session's window, the latter to all the code you've executed in
the matching language.

Under the covers, the execution history is stored and recorded
separately from the console, by a separate set of listeners to the
messages emitted from the runtimes.

Addresses #4925. 

### Release Notes

<!--
Optionally, replace `N/A` with text to be included in the next release
notes.
The `N/A` bullets are ignored. If you refer to one or more Positron
issues,
these issues are used to collect information about the feature or
bugfix, such
as the relevant language pack as determined by Github labels of type
`lang: `.
  The note will automatically be tagged with the language.

These notes are typically filled by the Positron team. If you are an
external
  contributor, you may ignore this section.
-->

#### New Features

- Console contents are retained when reconnecting to existing sessions
(#4925)

#### Bug Fixes

- N/A


### QA Notes

The contents are stored in a format that is somewhat optimized for speed
and compactness, and is intended to preserve the record of code you have

src/vs/workbench/contrib/positronConsole/browser/components/actionBar.tsx
@@ -126,8 +126,8 @@ export const ActionBar = (props: ActionBarProps) => {
 				setActivePositronConsoleInstance(activePositronConsoleInstance);
 				setInterruptible(activePositronConsoleInstance?.state === PositronConsoleState.Busy);
 				setInterrupting(false);
-				setCanShutdown(activePositronConsoleInstance?.session.getRuntimeState() !== RuntimeState.Exited);
-				setCanStart(activePositronConsoleInstance?.session.getRuntimeState() === RuntimeState.Exited);
+				setCanShutdown(activePositronConsoleInstance?.attachedRuntimeSession?.getRuntimeState() !== RuntimeState.Exited);
+				setCanStart(activePositronConsoleInstance?.attachedRuntimeSession?.getRuntimeState() === RuntimeState.Exited);
 			}
 		);
 	}, [positronConsoleContext.positronConsoleService]);
@@ -249,11 +249,15 @@ export const ActionBar = (props: ActionBarProps) => {
 			const session = activePositronConsoleInstance.attachedRuntimeSession;
 			if (session) {
 				attachRuntime(session);
+			} else {
+				// If no session yet, we can at least show the directory label
+				// while it reconnects
+				setDirectoryLabel(activePositronConsoleInstance.initialWorkingDirectory);
 			}
 
 			// Register for runtime changes.
 			disposableConsoleStore.add(
-				activePositronConsoleInstance.onDidAttachRuntime(attachRuntime));
+				activePositronConsoleInstance.onDidAttachSession(attachRuntime));
 		}
 
 		// Return the cleanup function that will dispose of the disposables.
@@ -269,7 +273,7 @@ export const ActionBar = (props: ActionBarProps) => {
 		setInterrupting(true);
 
 		// Interrupt the active Positron console instance.
-		activePositronConsoleInstance?.session.interrupt();
+		activePositronConsoleInstance?.attachedRuntimeSession?.interrupt();
 	};
 
 	// Toggle trace event handler.
@@ -290,7 +294,8 @@ export const ActionBar = (props: ActionBarProps) => {
 	// Power cycle (start or stop) console event handler.
 	const powerCycleConsoleHandler = async () => {
 		// Get the current session the console is bound to and its state.
-		const session = positronConsoleContext.activePositronConsoleInstance?.session;
+		const session =
+			positronConsoleContext.activePositronConsoleInstance?.attachedRuntimeSession;
 		if (!session) {
 			return;
 		}
@@ -324,7 +329,7 @@ export const ActionBar = (props: ActionBarProps) => {
 			return;
 		}
 		positronConsoleContext.runtimeSessionService.restartSession(
-			activePositronConsoleInstance!.session.sessionId,
+			activePositronConsoleInstance!.sessionId,
 			'User-requested restart from console action bar');
 	};
 
@@ -333,7 +338,8 @@ export const ActionBar = (props: ActionBarProps) => {
 			return;
 		}
 
-		await positronConsoleContext.runtimeSessionService.deleteSession(positronConsoleContext.activePositronConsoleInstance.session.sessionId);
+		await positronConsoleContext.runtimeSessionService.deleteSession(
+			positronConsoleContext.activePositronConsoleInstance.sessionId);
 	};
 
 	// Render.

src/vs/workbench/contrib/positronConsole/browser/components/consoleInstanceMenuButton.tsx
@@ -14,7 +14,7 @@ import { IAction } from '../../../../../base/common/actions.js';
 import { DisposableStore } from '../../../../../base/common/lifecycle.js';
 import { IReactComponentContainer } from '../../../../../base/browser/positronReactRenderer.js';
 import { ActionBarMenuButton } from '../../../../../platform/positronActionBar/browser/components/actionBarMenuButton.js';
-import { ILanguageRuntimeSession } from '../../../../services/runtimeSession/common/runtimeSessionService.js';
+import { IRuntimeSessionMetadata } from '../../../../services/runtimeSession/common/runtimeSessionService.js';
 import { usePositronConsoleContext } from '../positronConsoleContext.js';
 
 // ConsoleInstanceMenuButtonProps interface.
@@ -32,9 +32,9 @@ export const ConsoleInstanceMenuButton = (props: ConsoleInstanceMenuButtonProps)
 	const positronConsoleContext = usePositronConsoleContext();
 
 	// Helper method to calculate the label for a runtime session.
-	const labelForSession = (session?: ILanguageRuntimeSession): string => {
+	const labelForSession = (session?: IRuntimeSessionMetadata): string => {
 		if (session) {
-			return session.metadata.sessionName;
+			return session.sessionName;
 		}
 		return 'None';
 	};
@@ -42,14 +42,14 @@ export const ConsoleInstanceMenuButton = (props: ConsoleInstanceMenuButtonProps)
 	// State.
 	const [activeRuntimeLabel, setActiveRuntimeLabel] =
 		React.useState(labelForSession(
-			positronConsoleContext.activePositronConsoleInstance?.session));
+			positronConsoleContext.activePositronConsoleInstance?.sessionMetadata));
 
 	// useEffect hook to update the runtime label when the environment changes.
 	React.useEffect(() => {
 		const disposables = new DisposableStore();
 		const consoleService = positronConsoleContext.positronConsoleService;
 		disposables.add(consoleService.onDidChangeActivePositronConsoleInstance(e => {
-			setActiveRuntimeLabel(labelForSession(e?.session));
+			setActiveRuntimeLabel(labelForSession(e?.sessionMetadata));
 		}));
 		return () => disposables.dispose();
 	}, [positronConsoleContext.activePositronConsoleInstance, positronConsoleContext.positronConsoleService]);
@@ -60,14 +60,14 @@ export const ConsoleInstanceMenuButton = (props: ConsoleInstanceMenuButtonProps)
 		const actions: IAction[] = [];
 		positronConsoleContext.positronConsoleInstances.map(positronConsoleInstance => {
 			actions.push({
-				id: positronConsoleInstance.session.sessionId,
-				label: positronConsoleInstance.session.metadata.sessionName,
+				id: positronConsoleInstance.sessionId,
+				label: positronConsoleInstance.sessionMetadata.sessionName,
 				tooltip: '',
 				class: undefined,
 				enabled: true,
 				run: () => {
 					positronConsoleContext.runtimeSessionService.foregroundSession =
-						positronConsoleInstance.session;
+						positronConsoleInstance.attachedRuntimeSession
 					setTimeout(() => {
 						props.reactComponentContainer.takeFocus();
 					}, 0);

src/vs/workbench/services/positronConsole/browser/interfaces/positronConsoleService.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2023-2024 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2023-2025 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -7,10 +7,11 @@ import { Event } from '../../../../../base/common/event.js';
 import { ICodeEditor } from '../../../../../editor/browser/editorBrowser.js';
 import { createDecorator } from '../../../../../platform/instantiation/common/instantiation.js';
 import { RuntimeItem } from '../classes/runtimeItem.js';
-import { ILanguageRuntimeSession } from '../../../runtimeSession/common/runtimeSessionService.js';
+import { ILanguageRuntimeSession, IRuntimeSessionMetadata } from '../../../runtimeSession/common/runtimeSessionService.js';
 import { ActivityItemPrompt } from '../classes/activityItemPrompt.js';
-import { RuntimeCodeExecutionMode, RuntimeErrorBehavior } from '../../../languageRuntime/common/languageRuntimeService.js';
+import { ILanguageRuntimeMetadata, RuntimeCodeExecutionMode, RuntimeErrorBehavior } from '../../../languageRuntime/common/languageRuntimeService.js';
 import { IDisposable } from '../../../../../base/common/lifecycle.js';
+import { IExecutionHistoryEntry } from '../../../positronHistory/common/executionHistoryService.js';
 
 // Create the decorator for the Positron console service (used in dependency injection).
 export const IPositronConsoleService = createDecorator<IPositronConsoleService>('positronConsoleService');
@@ -73,6 +74,28 @@ export interface IPositronConsoleService {
 	 * The onDidChangeActivePositronConsoleInstance event.
 	 */
 	readonly onDidChangeActivePositronConsoleInstance: Event<IPositronConsoleInstance | undefined>;
+
+	/**
+	 * Set the active console instance to the one with the given session ID.
+	 *
+	 * Typically the active console instance should follow the global
+	 * foreground session; this method should only be used when the active
+	 * console instance needs to be set to a specific session.
+	 *
+	 * @param sessionId The session ID of the console to activate.
+	 */
+	setActivePositronConsoleSession(sessionId: string): void;
+
+	/**
+	 * Remove the console instance with the given session ID.
+	 *
+	 * As with setActivePositronConsoleSession, this is only used to remove
+	 * provisional instances that aren't tied to a session. Typically, session
+	 * deletion should be handled by the runtime session service.
+	 *
+	 * @param sessionId
+	 */
+	deletePositronConsoleSession(sessionId: string): void;
 
 	/**
 	 * The onDidChangeConsoleWidth event.
@@ -142,14 +165,24 @@ export interface ILanguageRuntimeCodeExecutedEvent {
  */
 export interface IPositronConsoleInstance {
 	/**
-	 * Gets the runtime session for the Positron console instance.
-	 */
-	readonly session: ILanguageRuntimeSession;
-
-	/**
 	 * Gets the state.
 	 */
 	readonly state: PositronConsoleState;
+
+	/**
+	 * Gets the metadata for the runtime session itself.
+	 */
+	readonly sessionMetadata: IRuntimeSessionMetadata;
+
+	/**
+	 * Gets the metadata of the runtime associated with the session.
+	 */
+	readonly runtimeMetadata: ILanguageRuntimeMetadata;
+
+	/**
+	 * Gets the session ID.
+	 */
+	readonly sessionId: string;
 
 	/**
 	 * Gets a value which indicates whether trace is enabled.
@@ -258,10 +291,10 @@ export interface IPositronConsoleInstance {
 	readonly onDidRequestRestart: Event<void>;
 
 	/**
-	 * The onDidAttachRuntime event. Fires both when a runtime is attached and
+	 * The onDidAttachSession event. Fires both when a session is attached and
 	 * when one is detached (in which case the parameter is undefined)
 	 */
-	readonly onDidAttachRuntime: Event<ILanguageRuntimeSession | undefined>;
+	readonly onDidAttachSession: Event<ILanguageRuntimeSession | undefined>;
 
 	/**
 	 * The onDidChangeWidthInChars event.
@@ -325,6 +358,21 @@ export interface IPositronConsoleInstance {
 	interrupt(code: string): void;
 
 	/**
+	 * Replays execution history entries, adding their input and output to the
+	 * console and preparing the console to reconnect to the runtime.
+	 *
+	 * @param entry The entry to replay.
+	 */
+	replayExecutions(entries: IExecutionHistoryEntry<any>[]): void;
+
+	/**
+	 * Gets or sets the initial working directory displayed in the console.
+	 *
+	 * This does not actually change the working directory of the runtime session.
+	 */
+	initialWorkingDirectory: string;
+
+	/**
 	 * Enqueues code to be executed.
 	 * @param code The code to enqueue.
 	 * @param allowIncomplete Whether to bypass runtime code completeness checks. If true, the `code`
@@ -356,7 +404,15 @@ export interface IPositronConsoleInstance {
 	interruptPrompt(activityItemPrompt: ActivityItemPrompt): void;
 
 	/**
-	 * Sets the currently attached runtime, or undefined if none.
+	 * Attaches a runtime session to the console.
+	 *
+	 * @param session The session to attach.
+	 * @param mode The session attach mode.
+	 */
+	attachRuntimeSession(session: ILanguageRuntimeSession | undefined, mode: SessionAttachMode): void;
+
+	/**
+	 * Gets the currently attached runtime, or undefined if none.
 	 */
 	attachedRuntimeSession: ILanguageRuntimeSession | undefined;
 }

src/vs/workbench/api/browser/positron/mainThreadConsole.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2024-2025 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -12,7 +12,7 @@ export class MainThreadConsole {
 	}
 
 	getLanguageId(): string {
-		return this._console.session.runtimeMetadata.languageId;
+		return this._console.runtimeMetadata.languageId;
 	}
 
 	pasteText(text: string): void {

src/vs/workbench/api/browser/positron/mainThreadConsoleService.ts
@@ -46,7 +46,7 @@ export class MainThreadConsoleService implements MainThreadConsoleServiceShape {
 		// in the main thread too
 		this._disposables.add(
 			this._positronConsoleService.onDidStartPositronConsoleInstance((console) => {
-				const sessionId = console.session.sessionId;
+				const sessionId = console.sessionMetadata.sessionId;
 
 				// First update ext host
 				this._proxy.$addConsole(sessionId);

src/vs/workbench/contrib/executionHistory/common/executionHistory.ts
@@ -1,129 +1,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2023-2024 Posit Software, PBC. All rights reserved.
- *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
- *--------------------------------------------------------------------------------------------*/
-
-import { IExecutionHistoryEntry, IExecutionHistoryService, IInputHistoryEntry } from './executionHistoryService.js';
-import { Disposable } from '../../../../base/common/lifecycle.js';
-import { ILanguageRuntimeSession, IRuntimeSessionService } from '../../../services/runtimeSession/common/runtimeSessionService.js';
-import { IStorageService, StorageScope } from '../../../../platform/storage/common/storage.js';
-import { ILogService } from '../../../../platform/log/common/log.js';
-import { InstantiationType, registerSingleton } from '../../../../platform/instantiation/common/extensions.js';
-import { RuntimeExecutionHistory } from './runtimeExecutionHistory.js';
-import { LanguageInputHistory } from './languageInputHistory.js';
-import { IConfigurationService } from '../../../../platform/configuration/common/configuration.js';
-import { IWorkspaceContextService, WorkbenchState } from '../../../../platform/workspace/common/workspace.js';
-
-/**
- * Service that manages execution histories for all runtimes.
- */
-export class ExecutionHistoryService extends Disposable implements IExecutionHistoryService {
-	// Required for service branding in dependency injector.
-	_serviceBrand: undefined;
-
-	// Map of runtime ID to execution history
-	private readonly _executionHistories: Map<string, RuntimeExecutionHistory> = new Map();
-
-	// Map of language ID to input history
-	private readonly _inputHistories: Map<string, LanguageInputHistory> = new Map();
-
-	constructor(
-		@IRuntimeSessionService private readonly _runtimeSessionService: IRuntimeSessionService,
-		@IStorageService private readonly _storageService: IStorageService,
-		@ILogService private readonly _logService: ILogService,
-		@IConfigurationService private readonly _configurationService: IConfigurationService,
-		@IWorkspaceContextService private readonly _workspaceContextService: IWorkspaceContextService
-	) {
-		super();
-
-		// Start recording history for all currently active runtimes
-		this._runtimeSessionService.activeSessions.forEach(session => {
-			this.beginRecordingHistory(session);
-		});
-
-		// Listen for runtimes to start; when they do, begin recording
-		// executions
-		this._register(this._runtimeSessionService.onDidStartRuntime(runtime => {
-			this.beginRecordingHistory(runtime);
-		}));
-	}
-
-	/**
-	 * Clear the input history for the given language (remove all entries)
-	 *
-	 * @param languageId Language ID to clear input history for
-	 */
-	clearInputEntries(languageId: string): void {
-		this.getInputHistory(languageId).clear();
-	}
-
-	/**
-	 * Get the input history for the given language.
-	 *
-	 * @param languageId Language ID to get input history for
-	 * @returns Input history for the given language, as an array of input history entries.
-	 */
-	getInputEntries(languageId: string): IInputHistoryEntry[] {
-		return this.getInputHistory(languageId).getInputHistory();
-	}
-
-	private beginRecordingHistory(runtime: ILanguageRuntimeSession): void {
-		// Create a new history for the runtime if we don't already have one
-		if (!this._executionHistories.has(runtime.runtimeMetadata.runtimeId)) {
-			const history = new RuntimeExecutionHistory(runtime, this._storageService, this._logService);
-			this._executionHistories.set(runtime.runtimeMetadata.runtimeId, history);
-			this._register(history);
-		}
-
-		// Attach the runtime to an input history recorder for the language,
-		// creating one if necessary
-		this.getInputHistory(runtime.runtimeMetadata.languageId).attachToRuntime(runtime);
-	}
-
-	getExecutionEntries(runtimeId: string): IExecutionHistoryEntry<any>[] {
-		// Return the history entries for the given runtime, if known.
-		if (this._executionHistories.has(runtimeId)) {
-			return this._executionHistories.get(runtimeId)?.entries!;
-		} else {
-			throw new Error(`Can't get entries; unknown runtime ID: ${runtimeId}`);
-		}
-	}
-
-	clearExecutionEntries(runtimeId: string): void {
-		// Return the history entries for the given runtime, if known.
-		if (this._executionHistories.has(runtimeId)) {
-			this._executionHistories.get(runtimeId)?.clear();
-		} else {
-			throw new Error(`Can't get entries; unknown runtime ID: ${runtimeId}`);
-		}
-	}
-
-	/**
-	 * Gets the input history for the given language ID, creating it if necessary.
-	 *
-	 * @param languageId The language ID for which to get the input history
-	 * @param callback The callback to execute with the input history
-	 */
-	private getInputHistory(languageId: string): LanguageInputHistory {
-		if (this._inputHistories.has(languageId)) {
-			return this._inputHistories.get(languageId)!;
-		}
-
-		// If we're in an empty workspace, use the profile storage scope; otherwise,
-		// use the workspace scope.
-		const storageScope =
-			this._workspaceContextService.getWorkbenchState() === WorkbenchState.EMPTY ?
-				StorageScope.PROFILE :
-				StorageScope.WORKSPACE;
-		const history = new LanguageInputHistory(languageId,
-			this._storageService,
-			storageScope,
-			this._logService,
-			this._configurationService);
-		this._inputHistories.set(languageId, history);
-		this._register(history);
-		return history;
-	}
-}
-
-registerSingleton(IExecutionHistoryService, ExecutionHistoryService, InstantiationType.Delayed);

commit 4c58ae47d1ae3fd37c8a23e823513a98794a9717

Fix Reticulate web tests (#6760)
This change attempts to fix the Reticulate web tests, which were
recently broken by #6741.

test: `@:web`, `@:console`, `@:reticulate`

src/vs/workbench/contrib/positronConsole/browser/components/actionBar.tsx
@@ -293,31 +293,39 @@ export const ActionBar = (props: ActionBarProps) => {
 
 	// Power cycle (start or stop) console event handler.
 	const powerCycleConsoleHandler = async () => {
-		// Get the current session the console is bound to and its state.
-		const session =
-			positronConsoleContext.activePositronConsoleInstance?.attachedRuntimeSession;
-		if (!session) {
+		// Ensure we're acting on a valid console instance.
+		if (!positronConsoleContext.activePositronConsoleInstance) {
 			return;
 		}
-		const state = session.getRuntimeState();
+
+		// Get the current session the console is bound to and its state.
+		const consoleInstance = positronConsoleContext.activePositronConsoleInstance;
+		const session = consoleInstance.attachedRuntimeSession;
+
+		// If no session, treat state as uninitialized.
+		const state = session ? session.getRuntimeState() : RuntimeState.Uninitialized;
 
 		if (state === RuntimeState.Exited || state === RuntimeState.Uninitialized) {
+
+			const runtimeMetadata = consoleInstance.runtimeMetadata;
+			const sessionMetadata = consoleInstance.sessionMetadata;
+
 			// Start a new session if the current session has exited, or never
 			// started (e.g. retrying after a startup failure)
 			positronConsoleContext.runtimeSessionService.startNewRuntimeSession(
-				session.runtimeMetadata.runtimeId,
-				session.metadata.sessionName,
-				session.metadata.sessionMode,
-				session.metadata.notebookUri,
+				runtimeMetadata.runtimeId,
+				sessionMetadata.sessionName,
+				sessionMetadata.sessionMode,
+				sessionMetadata.notebookUri,
 				`User-requested new session from console action bar ` +
-				`after session ${session.metadata.sessionId} exited.`,
+				`after session ${sessionMetadata.sessionId} exited.`,
 				RuntimeStartMode.Starting,
 				false
 			);
 			return;
 		} else {
 			// Shutdown the current session.
-			session.shutdown(
+			session?.shutdown(
 				RuntimeExitReason.Shutdown
 			);
 		}

src/vs/workbench/contrib/positronConsole/browser/components/consoleInstanceMenuButton.tsx
@@ -16,6 +16,7 @@ import { IReactComponentContainer } from '../../../../../base/browser/positronRe
 import { ActionBarMenuButton } from '../../../../../platform/positronActionBar/browser/components/actionBarMenuButton.js';
 import { IRuntimeSessionMetadata } from '../../../../services/runtimeSession/common/runtimeSessionService.js';
 import { usePositronConsoleContext } from '../positronConsoleContext.js';
+import { IPositronConsoleInstance } from '../../../../services/positronConsole/browser/interfaces/positronConsoleService.js';
 
 // ConsoleInstanceMenuButtonProps interface.
 interface ConsoleInstanceMenuButtonProps {
@@ -54,6 +55,21 @@ export const ConsoleInstanceMenuButton = (props: ConsoleInstanceMenuButtonProps)
 		return () => disposables.dispose();
 	}, [positronConsoleContext.activePositronConsoleInstance, positronConsoleContext.positronConsoleService]);
 
+	// Switch to the given console.
+	const switchToConsole = (positronConsoleInstance: IPositronConsoleInstance) => {
+		const session = positronConsoleInstance.attachedRuntimeSession;
+		if (session) {
+			// For attached sessions, we need to set the foreground session.
+			positronConsoleContext.runtimeSessionService.foregroundSession = session;
+		} else {
+			// For detached sessions, set the session in just the console service.
+			positronConsoleContext.positronConsoleService.setActivePositronConsoleSession(positronConsoleInstance.sessionId);
+		}
+		setTimeout(() => {
+			props.reactComponentContainer.takeFocus();
+		}, 0);
+	}
+
 	// Builds the actions.
 	const actions = () => {
 		// Build the actions for the available console repl instances.
@@ -66,11 +82,7 @@ export const ConsoleInstanceMenuButton = (props: ConsoleInstanceMenuButtonProps)
 				class: undefined,
 				enabled: true,
 				run: () => {
-					positronConsoleContext.runtimeSessionService.foregroundSession =
-						positronConsoleInstance.attachedRuntimeSession
-					setTimeout(() => {
-						props.reactComponentContainer.takeFocus();
-					}, 0);
+					switchToConsole(positronConsoleInstance);
 				}
 			});
 		});

src/vs/workbench/services/positronConsole/browser/positronConsoleService.ts
@@ -140,11 +140,6 @@ export class PositronConsoleService extends Disposable implements IPositronConso
 	private readonly _positronConsoleInstancesByLanguageId = new Map<string, PositronConsoleInstance>();
 
 	/**
-	 * A map of the Positron console instances by runtime ID.
-	 */
-	private readonly _positronConsoleInstancesByRuntimeId = new Map<string, PositronConsoleInstance[]>();
-
-	/**
 	 * A map of the Positron console instances by session ID.
 	 */
 	private readonly _positronConsoleInstancesBySessionId = new Map<string, PositronConsoleInstance>();
@@ -319,10 +314,7 @@ export class PositronConsoleService extends Disposable implements IPositronConso
 				 * a new one. This is problematic because the user's intention was to creat a new console
 				 * instance for the new session.
 				 */
-				const positronConsoleInstances = this._positronConsoleInstancesByRuntimeId.get(e.session.runtimeMetadata.runtimeId);
-
-				const positronConsoleInstance = positronConsoleInstances?.find(
-					console => console.sessionId === e.session.sessionId);
+				const positronConsoleInstance = this._positronConsoleInstancesBySessionId.get(e.session.sessionId);
 
 				if (positronConsoleInstance) {
 					this._positronConsoleInstancesBySessionId.delete(positronConsoleInstance.sessionId);
@@ -337,14 +329,7 @@ export class PositronConsoleService extends Disposable implements IPositronConso
 
 		// Register the onDidStartRuntime event handler so we activate the new Positron console instance when the runtime starts up.
 		this._register(this._runtimeSessionService.onDidStartRuntime(session => {
-			const multiSessionsEnabled = multipleConsoleSessionsFeatureEnabled(this._configurationService);
-
-			let positronConsoleInstance: PositronConsoleInstance | undefined;
-			if (!multiSessionsEnabled) {
-				positronConsoleInstance = this._positronConsoleInstancesBySessionId.get(session.runtimeMetadata.runtimeId);
-			} else {
-				positronConsoleInstance = this._positronConsoleInstancesBySessionId.get(session.sessionId);
-			}
+			const positronConsoleInstance = this._positronConsoleInstancesBySessionId.get(session.sessionId);
 
 			if (positronConsoleInstance) {
 				positronConsoleInstance.setState(PositronConsoleState.Ready);
@@ -353,15 +338,7 @@ export class PositronConsoleService extends Disposable implements IPositronConso
 
 		// Register the onDidFailStartRuntime event handler so we activate the new Positron console instance when the runtime starts up.
 		this._register(this._runtimeSessionService.onDidFailStartRuntime(session => {
-			const multiSessionsEnabled = multipleConsoleSessionsFeatureEnabled(this._configurationService);
-
-			let positronConsoleInstance: PositronConsoleInstance | undefined;
-			if (!multiSessionsEnabled) {
-				positronConsoleInstance = this._positronConsoleInstancesBySessionId.get(session.runtimeMetadata.runtimeId);
-
-			} else {
-				positronConsoleInstance = this._positronConsoleInstancesBySessionId.get(session.sessionId);
-			}
+			const positronConsoleInstance = this._positronConsoleInstancesBySessionId.get(session.sessionId);
 
 			if (positronConsoleInstance) {
 				positronConsoleInstance.setState(PositronConsoleState.Exited);
@@ -646,16 +623,8 @@ export class PositronConsoleService extends Disposable implements IPositronConso
 				runtimeMetadata.languageId,
 				positronConsoleInstance
 			);
-		} else {
-			// Add the Positron console instance.
-			const positronConsoleInstancesForRuntime =
-				this._positronConsoleInstancesByRuntimeId.get(runtimeMetadata.runtimeId) || [];
-			positronConsoleInstancesForRuntime.push(positronConsoleInstance);
-			this._positronConsoleInstancesByRuntimeId.set(
-				runtimeMetadata.runtimeId,
-				positronConsoleInstancesForRuntime
-			);
 		}
+
 		this._positronConsoleInstancesBySessionId.set(
 			sessionMetadata.sessionId,
 			positronConsoleInstance