5221be236ce12aa89dbba76d14c13af41d58c75c
Melissa Barca <5323711+melissa-barca@users.noreply.github.com>
2025-03-14 11:33:33 -0400

    move salt into a subfolder, implement DeferredPromise pattern and add context to missing functions, remove fallback salt browser storage, and improve logging

--- src/vs/workbench/services/encryption/browser/browserEncryptionService.ts
+++ src/vs/workbench/services/encryption/browser/browserEncryptionService.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { mainWindow } from '../../../../base/browser/window.js';
+import { DeferredPromise } from '../../../../base/common/async.js';
 import { URI } from '../../../../base/common/uri.js';
 import { joinPath } from '../../../../base/common/resources.js';
 import { VSBuffer } from '../../../../base/common/buffer.js';
@@ -21,9 +22,8 @@ export class BrowserEncryptionService implements IEncryptionService {
 	_serviceBrand: undefined;
 
 	private _encryptionKey: CryptoKey | undefined;
-	private _keyInitPromise: Promise<CryptoKey> | undefined;
+	private _keyInitPromise: DeferredPromise<CryptoKey> | undefined;
 	private readonly _storageKeyName = 'browser.encryptionKeyWrapped';
-	private readonly _storageSaltKey = 'browser.encryptionSalt';
 
 	constructor(
 		@IStorageService private readonly storageService: IStorageService,
@@ -32,60 +32,73 @@ export class BrowserEncryptionService implements IEncryptionService {
 		@IEnvironmentService private readonly environmentService: IEnvironmentService
 	) {
 		this.logService.info('[BrowserEncryptionService] Initializing browser encryption service');
-		this._keyInitPromise = this.initializeEncryptionKey();
+		this.initializeEncryptionKey();
 	}
 
 	private async initializeEncryptionKey(): Promise<CryptoKey> {
 		// TO DO: Swap some info logging for trace logging
-		this.logService.info('[BrowserEncryptionService] Initializing encryption key...');
-		try {
-			const storedKeyData = this.storageService.get(this._storageKeyName, StorageScope.APPLICATION);
+		this.logService.trace('[BrowserEncryptionService] Initializing encryption key');
+		if (!this._keyInitPromise) {
+			this._keyInitPromise = new DeferredPromise<CryptoKey>();
 
-			if (storedKeyData && storedKeyData !== '{}') {
-				this.logService.info('[BrowserEncryptionService] Found stored key data.');
+			try {
+				const storedKeyData = this.storageService.get(this._storageKeyName, StorageScope.APPLICATION);
+
+				if (storedKeyData && storedKeyData !== '{}') {
+					this.logService.trace('[BrowserEncryptionService] Found stored key data');
+					try {
+						// unwrap key using iv and salt
+						const wrappedKeyData = JSON.parse(storedKeyData);
+						const iv = new Uint8Array(wrappedKeyData.iv);
+						const wrappedKey = new Uint8Array(wrappedKeyData.data);
+
+						const salt = await this.getSaltFromFileSystem();
+						const wrappingKey = await this.deriveKeyWrappingKey(salt);
+
+						const unwrappedKeyBuffer = await mainWindow.crypto.subtle.decrypt(
+							{ name: 'AES-GCM', iv },
+							wrappingKey,
+							wrappedKey
+						);
+
+						const decoder = new TextDecoder();
+						const decryptedKeyString = decoder.decode(unwrappedKeyBuffer);
+						const keyData = JSON.parse(decryptedKeyString);
+
+						this._encryptionKey = await mainWindow.crypto.subtle.importKey(
+							'jwk',
+							keyData,
+							{ name: 'AES-GCM' },
+							true,
+							['encrypt', 'decrypt']
+						);
+						this.logService.trace('[BrowserEncryptionService] Unwrapped encryption key');
+						this._keyInitPromise.complete(this._encryptionKey);
+					} catch (error) {
+						this.logService.error('[BrowserEncryptionService] Error unwrapping encrytption key:', error);
+						const key = await this.generateAndStoreEncryptionKey();
+						this._keyInitPromise.complete(key);
+					}
+				} else {
+					this.logService.info('[BrowserEncryptionService] No stored key data found');
+					const key = await this.generateAndStoreEncryptionKey();
+					this._keyInitPromise.complete(key);
+				}
+			} catch (error) {
+				this.logService.error('[BrowserEncryptionService] Error initializing encryption key:', error);
 				try {
-					// unwrap key using iv and salt
-					const wrappedKeyData = JSON.parse(storedKeyData);
-					const iv = new Uint8Array(wrappedKeyData.iv);
-					const wrappedKey = new Uint8Array(wrappedKeyData.data);
-
-					const salt = await this.getSaltFromFileSystem();
-					const wrappingKey = await this.deriveKeyWrappingKey(salt);
-
-					const unwrappedKeyBuffer = await mainWindow.crypto.subtle.decrypt(
-						{ name: 'AES-GCM', iv },
-						wrappingKey,
-						wrappedKey
-					);
-
-					const decoder = new TextDecoder();
-					const decryptedKeyString = decoder.decode(unwrappedKeyBuffer);
-					const keyData = JSON.parse(decryptedKeyString);
-
-					this._encryptionKey = await mainWindow.crypto.subtle.importKey(
-						'jwk',
-						keyData,
-						{ name: 'AES-GCM' },
-						true,
-						['encrypt', 'decrypt']
-					);
-					this.logService.info('[BrowserEncryptionService] Unwrapped encryption key.');
-					return this._encryptionKey;
-				} catch (error) {
-					this.logService.error('[BrowserEncryptionService] Error unwrapping encrytption key:', error);
-					return await this.generateAndStoreEncryptionKey();
+					const key = await this.generateAndStoreEncryptionKey();
+					this._keyInitPromise.complete(key);
+				} catch (finalError) {
+					this._keyInitPromise.error(finalError);
 				}
 			}
-			this.logService.info('[BrowserEncryptionService] No stored key data found.');
-			return this.generateAndStoreEncryptionKey();
-		} catch (error) {
-			this.logService.error('[BrowserEncryptionService] Error initializing encryption key:', error);
-			return this.generateAndStoreEncryptionKey();
 		}
+		return this._keyInitPromise.p;
 	}
 
 	private async generateAndStoreEncryptionKey(): Promise<CryptoKey> {
-		this.logService.info('[BrowserEncryptionService] Generating encryption key...');
+		this.logService.info('[BrowserEncryptionService] Generating encryption key');
 		try {
 			this._encryptionKey = await mainWindow.crypto.subtle.generateKey(
 				{ name: 'AES-GCM', length: 256 },
@@ -93,7 +106,7 @@ export class BrowserEncryptionService implements IEncryptionService {
 				['encrypt', 'decrypt']
 			);
 			await this.wrapAndStoreKey(this._encryptionKey);
-			this.logService.info('[BrowserEncryptionService] Generated encryption key.');
+			this.logService.info('[BrowserEncryptionService] Generated encryption key');
 			return this._encryptionKey;
 		} catch (error) {
 			this.logService.error('[BrowserEncryptionService] Error generating encryption key:', error);
@@ -128,7 +141,7 @@ export class BrowserEncryptionService implements IEncryptionService {
 			// Store in storageService
 			this.storageService.store(this._storageKeyName, JSON.stringify(wrappedKeyData), StorageScope.APPLICATION, StorageTarget.MACHINE);
 
-			this.logService.info('[BrowserEncryptionService] Wrapped and stored key.');
+			this.logService.trace('[BrowserEncryptionService] Wrapped and stored key');
 		} catch (error) {
 			this.logService.error('[BrowserEncryptionService] Error wrapping and storing key:', error);
 			throw error;
@@ -146,47 +159,26 @@ export class BrowserEncryptionService implements IEncryptionService {
 
 				if (saltData && saltData.length > 0) {
 					salt = new Uint8Array(JSON.parse(saltData));
-					this.logService.info('[BrowserEncryptionService] Retrieved salt from file system.');
+					this.logService.trace('[BrowserEncryptionService] Retrieved salt from file system');
 					return salt;
 				}
 			} catch (error) {
-				this.logService.info('[BrowserEncryptionService] No existing salt found, generating new salt.');
+				this.logService.trace('[BrowserEncryptionService] No existing salt found, generating new salt');
 			}
 			salt = mainWindow.crypto.getRandomValues(new Uint8Array(16));
 
 			const saltContent = JSON.stringify(Array.from(salt));
 			await this.fileService.writeFile(this.getSaltFilePath(saltFileName), VSBuffer.fromString(saltContent));
-			this.logService.info('[BrowserEncryptionService] Wrote new salt to file system.');
+			this.logService.trace('[BrowserEncryptionService] Wrote new salt to file system');
 			return salt;
 		} catch (error) {
 			this.logService.error('[BrowserEncryptionService] Error getting salt from file system:', error);
-			// If error, store salt in browser storage instead
-			return this.getSaltFromStorage();
+			throw new Error('[BrowserEncryptionService] Cannot access file system for encryption salt. Encryption unavailable');
 		}
-	}
-
-	private getSaltFromStorage(): Promise<Uint8Array> {
-		const storedSalt = this.storageService.get(this._storageSaltKey, StorageScope.APPLICATION);
-		let salt: Uint8Array;
-
-		if (storedSalt) {
-			try {
-				salt = new Uint8Array(JSON.parse(storedSalt));
-				this.logService.info('[BrowserEncryptionService] Retrieved salt from storage.');
-				return Promise.resolve(salt);
-			} catch (error) {
-				this.logService.error('[BrowserEncryptionService] Error parsing stored salt:', error);
-			}
-		}
-
-		salt = mainWindow.crypto.getRandomValues(new Uint8Array(16));
-		this.storageService.store(this._storageSaltKey, JSON.stringify(Array.from(salt)), StorageScope.APPLICATION, StorageTarget.MACHINE);
-		this.logService.info('[BrowserEncryptionService] Generated and stored new salt.');
-		return Promise.resolve(salt);
 	}
 
 	private getSaltFilePath(saltFileName: string): URI {
-		return joinPath(this.environmentService.userRoamingDataHome, saltFileName);
+		return joinPath(this.environmentService.userRoamingDataHome, 'positron', saltFileName);
 	}
 
 	private async deriveKeyWrappingKey(salt: Uint8Array): Promise<CryptoKey> {
@@ -229,16 +221,13 @@ export class BrowserEncryptionService implements IEncryptionService {
 		if (this._encryptionKey) {
 			return this._encryptionKey;
 		}
-
-		if (!this._keyInitPromise) {
-			this._keyInitPromise = this.initializeEncryptionKey();
-		}
-
-		return this._keyInitPromise;
+		const key = await this.initializeEncryptionKey();
+		this._encryptionKey = key;
+		return key;
 	}
 
 	async encrypt(value: string): Promise<string> {
-		this.logService.info('[BrowserEncryptionService] Encrypting value...');
+		this.logService.trace('[BrowserEncryptionService] Encrypting value');
 
 		try {
 			const encoder = new TextEncoder();
@@ -257,21 +246,21 @@ export class BrowserEncryptionService implements IEncryptionService {
 				iv: Array.from(iv)
 			});
 
-			this.logService.info('[BrowserEncryptionService] Encrypted value.');
+			this.logService.trace('[BrowserEncryptionService] Encrypted value');
 			return result;
 		} catch (e) {
-			this.logService.error(e);
+			this.logService.error('[BrowserEncryptionService] Error encrypting value:', e);
 			throw e;
 		}
 	}
 
 	async decrypt(value: string): Promise<string> {
-		this.logService.info('[BrowserEncryptionService] Decrypting value...');
+		this.logService.trace('[BrowserEncryptionService] Decrypting value');
 
 		try {
 			const { data, iv } = JSON.parse(value);
 			if (!data || !iv) {
-				throw new Error('[BrowserEncryptionService Invalid encrypted value');
+				throw new Error('[BrowserEncryptionService] Invalid encrypted value');
 			}
 
 			const key = await this.getEncryptionKey();
@@ -287,7 +276,7 @@ export class BrowserEncryptionService implements IEncryptionService {
 			const decoder = new TextDecoder();
 			const decrypted = decoder.decode(decryptedBuffer);
 
-			this.logService.info('[BrowserEncryptionService] Decrypted value.');
+			this.logService.trace('[BrowserEncryptionService] Decrypted value');
 			return decrypted;
 		} catch (e) {
 			this.logService.error('[BrowserEncryptionService] Error decrypting value:', e);
@@ -297,17 +286,19 @@ export class BrowserEncryptionService implements IEncryptionService {
 
 	async isEncryptionAvailable(): Promise<boolean> {
 		const available = !!(mainWindow?.crypto?.subtle);
-		this.logService.info('[BrowserEncryptionService] Encryption is available:', available);
+		this.logService.trace('[BrowserEncryptionService] Encryption is available:', available);
 		return available;
 	}
 
 	getKeyStorageProvider(): Promise<KnownStorageProvider> {
-		this.logService.info('[BrowserEncryptionService] Getting key storage provider:', KnownStorageProvider.basicText);
+		this.logService.trace('[BrowserEncryptionService] Getting key storage provider:', KnownStorageProvider.basicText);
 		return Promise.resolve(KnownStorageProvider.basicText);
 	}
 
+	// This function exists to satisfy the IEncryptionService interface,
+	// but it is not valid in the browser as there is not a backup encryption method
 	setUsePlainTextEncryption(): Promise<void> {
-		this.logService.info('[BrowserEncryptionService] Setting use plain text encryption');
+		this.logService.trace('[BrowserEncryptionService] Setting use plain text encryption is not valid in the browser');
 		return Promise.resolve(undefined);
 	}
 }
