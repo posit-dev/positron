4d8fdb78b89b07a8abe3b04925a04a4c78e28369
Wes McKinney <wes@posit.co>
2025-03-19 16:32:57 -0500

    Correct bin edges in DuckDB histogram

--- extensions/positron-duckdb/src/extension.ts
+++ extensions/positron-duckdb/src/extension.ts
@@ -541,7 +541,7 @@ class ColumnProfileEvaluator {
 			result.toArray().map(entry => [entry.bin_id, entry.bin_count])
 		);
 		for (let i = 0; i < numBins; ++i) {
-			output.bin_edges.push((binWidth * i).toString());
+			output.bin_edges.push((minValue + binWidth * i).toString());
 			output.bin_counts.push(Number(histEntries.get(i) ?? 0));
 		}
 
@@ -549,7 +549,7 @@ class ColumnProfileEvaluator {
 		output.bin_counts[numBins - 1] += Number(histEntries.get(numBins) ?? 0);
 
 		// Compute the push the last bin
-		output.bin_edges.push((binWidth * numBins).toString());
+		output.bin_edges.push((minValue + binWidth * numBins).toString());
 		return output;
 	}
 
