a0aa8384beebc8430d1b1e32fccff7c66db8a315
seem <mwlorgat@gmail.com>
2025-03-19 13:39:48 +0200

    pin numpy version; ruff autoformat

--- extensions/positron-python/noxfile-positron.py
+++ extensions/positron-python/noxfile-positron.py
@@ -8,18 +8,20 @@ import nox
 
 
 @nox.session()
-@nox.parametrize('pandas', ['1.5.3'])
-@nox.parametrize('torch', ['1.12.1'])
-@nox.parametrize('lightning', ['2.1.4'])
-def test_minimum_reqs(session, pandas, torch, lightning):
+@nox.parametrize("pandas", ["1.5.3"])
+@nox.parametrize("numpy", ["1.24.4"])
+@nox.parametrize("torch", ["1.12.1"])
+@nox.parametrize("lightning", ["2.1.4"])
+def test_minimum_reqs(session, pandas, numpy, torch, lightning):
     session.install("-r", "python_files/posit/pinned-test-requirements.txt")
-    session.install('--force-reinstall', f'pandas=={pandas}')
-    session.install('--force-reinstall', f'torch=={torch}')
-    session.install('--force-reinstall', f'lightning=={lightning}')
+    session.install("--force-reinstall", f"pandas=={pandas}")
+    session.install("--force-reinstall", f"numpy=={numpy}")
+    session.install("--force-reinstall", f"torch=={torch}")
+    session.install("--force-reinstall", f"lightning=={lightning}")
 
     if session.posargs:
         test_args = session.posargs
     else:
         test_args = []
 
-    session.run('pytest', *test_args)
+    session.run("pytest", *test_args)
