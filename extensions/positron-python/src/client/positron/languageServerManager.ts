4d288bb2e4095150a13e6adae82fd7c70b41979e
seem <mwlorgat@gmail.com>
2025-03-24 13:20:56 +0200

    dont need anymore

--- extensions/positron-python/src/client/positron/languageServerManager.ts
+++ extensions/positron-python/src/client/positron/languageServerManager.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-classes-per-file */
 /*---------------------------------------------------------------------------------------------
  *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
