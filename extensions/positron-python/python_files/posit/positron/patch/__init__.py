0467ca1081613e535d0394c4d4b97913d7c4e564
Nick Strayer <nick.strayer@posit.co>
2025-03-25 16:03:35 -0400

    Remove unneccesary code in __init__.py

--- extensions/positron-python/python_files/posit/positron/patch/__init__.py
+++ extensions/positron-python/python_files/posit/positron/patch/__init__.py
@@ -4,14 +4,3 @@
 #
 
 """Patch modules for external libraries compatibility with Positron."""
-
-from .bokeh import handle_bokeh_output, patch_bokeh_no_access
-from .haystack import patch_haystack_is_in_jupyter
-from .holoviews import set_holoviews_extension
-
-__all__ = [
-    "handle_bokeh_output",
-    "patch_bokeh_no_access",
-    "patch_haystack_is_in_jupyter",
-    "set_holoviews_extension",
-]
