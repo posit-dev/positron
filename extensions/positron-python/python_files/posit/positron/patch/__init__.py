d238e9a5176f69471a7fbc157869be50fe789218
Nick Strayer <nick.strayer@posit.co>
2025-03-25 19:25:42 -0400

    remove leftover comment

--- extensions/positron-python/python_files/posit/positron/patch/__init__.py
+++ extensions/positron-python/python_files/posit/positron/patch/__init__.py
@@ -2,5 +2,3 @@
 # Copyright (C) 2024 Posit Software, PBC. All rights reserved.
 # Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
 #
-
-"""Patch modules for external libraries compatibility with Positron."""
