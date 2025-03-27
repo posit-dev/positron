ad53400ef2e758a11ee4b1605f9bf77c05a3263d
Marie Idleman <marie.idleman@posit.co>
2025-03-27 14:25:10 -0500

    e2e-test: session outline test updates (#7020)
    ### Summary 
    
    Updated the session outline test toggle between files (in addition to
    between sessions) to verify outline displays as expected. And to ensure
    proper behavior when foreground session doesn't match editor.
    
    ### QA Notes
    
    @:sessions

--- test/e2e/tests/sessions/session-outline.test.ts
+++ test/e2e/tests/sessions/session-outline.test.ts
@@ -3,6 +3,7 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { Outline } from '../../pages/outline.js';
 import { test, tags } from '../_test.setup';
 
 test.use({
@@ -17,63 +18,69 @@ test.describe('Session: Outline', {
 		await userSettings.set([['console.multipleConsoleSessions', 'true']], true);
 	});
 
-	test('Python - Verify outline is per session', async function ({ app, openFile, sessions }) {
-		const { variables, outline, console } = app.workbench;
-
-		const [pySession, pyAltSession] = await sessions.start(['python', 'pythonAlt']);
-
-		// Focus outline view and open Python file
+	test('Verify outline is based on editor and per session', async function ({ app, openFile, sessions }) {
+		const { variables, outline, console, editor } = app.workbench;
 		await variables.togglePane('hide');
 		await outline.focus();
+
 		await openFile('workspaces/outline/basic-outline-with-vars.py');
-
-		// Session 1 - verify only expected outline elements
-		await sessions.select(pySession.id);
-		await console.typeToConsole('global_variable="goodbye"', true);
-		await outline.expectOutlineElementCountToBe(2);
-		await outline.expectOutlineElementToBeVisible('global_variable = "hello"');
-		await outline.expectOutlineElementToBeVisible('def demonstrate_scope');
-
-		// Python Alt Session - verify only expected outline elements
-		await sessions.select(pyAltSession.id);
-		await console.typeToConsole('global_variable="goodbye2"', true);
-		await outline.expectOutlineElementCountToBe(2);
-		await outline.expectOutlineElementToBeVisible('global_variable = "hello"');
-		await outline.expectOutlineElementToBeVisible('def demonstrate_scope');
-	});
-
-	test('R - Verify outline is per session', async function ({ app, openFile, sessions }) {
-		const { variables, outline, console } = app.workbench;
-
-		const [rSession1, rSession2, rSessionAlt] = await sessions.start(['r', 'r', 'rAlt'], { reuse: false });
-
-		// Focus outline view and open Python file
-		await variables.togglePane('hide');
-		await outline.focus();
 		await openFile('workspaces/outline/basic-outline-with-vars.r');
 
-		// Session 1 - verify only expected outline elements
+		// No active session - verify no outlines
+		await editor.selectTab('basic-outline-with-vars.py');
+		await outline.expectOutlineToBeEmpty();
+		await editor.selectTab('basic-outline-with-vars.r');
+		await outline.expectOutlineToBeEmpty();
+
+		// Start sessions
+		const [pySession1, pySession2, rSession1, rSession2] = await sessions.start(['python', 'pythonAlt', 'r', 'rAlt']);
+
+		// Select Python file
+		await editor.selectTab('basic-outline-with-vars.py');
+		await verifyPythonOutline(outline);
+
+		// Select R Session 1 - verify Python outline
+		// Use last-active Python session's LSP for Python files, even if foreground session is R.
 		await sessions.select(rSession1.id);
-		await console.typeToConsole('x<-"goodbye"', true);
-		await outline.expectOutlineElementCountToBe(3);
-		await outline.expectOutlineElementToBeVisible('demonstrate_scope');
-		await outline.expectOutlineElementToBeVisible('global_variable');
-		await outline.expectOutlineElementToBeVisible('local_variable');
+		await verifyPythonOutline(outline);
 
-		// Session 2 - verify only expected outline elements
+		// Select Python Session 1 - verify Python outline
+		await sessions.select(pySession1.id);
+		await console.typeToConsole('global_variable="goodbye"', true);
+		await verifyPythonOutline(outline);
+
+		// Select R file
+		await editor.selectTab('basic-outline-with-vars.r');
+		await verifyROutline(outline);
+
+		// Select R Session 1 - verify R outline
+		await sessions.select(rSession1.id);
+		await verifyROutline(outline);
+
+		// Select R Session 2 - verify R outline
 		await sessions.select(rSession2.id);
-		await console.typeToConsole('x<-"goodbye2"', true);
-		await outline.expectOutlineElementCountToBe(3);
-		await outline.expectOutlineElementToBeVisible('demonstrate_scope');
-		await outline.expectOutlineElementToBeVisible('global_variable');
-		await outline.expectOutlineElementToBeVisible('local_variable');
+		await verifyROutline(outline);
 
-		// Session 3 (Alt) - verify only expected outline elements
-		await sessions.select(rSessionAlt.id);
-		await console.typeToConsole('x<-"goodbye3"', true);
-		await outline.expectOutlineElementCountToBe(3);
-		await outline.expectOutlineElementToBeVisible('demonstrate_scope');
-		await outline.expectOutlineElementToBeVisible('global_variable');
-		await outline.expectOutlineElementToBeVisible('local_variable');
+		// Select Python file - verify Python outline
+		await editor.selectTab('basic-outline-with-vars.py');
+		await verifyPythonOutline(outline);
+
+		// Python Session 2 - verify Python outline
+		await sessions.select(pySession2.id);
+		await console.typeToConsole('global_variable="goodbye2"', true);
+		await verifyPythonOutline(outline);
 	});
 });
+
+async function verifyPythonOutline(outline: Outline) {
+	await outline.expectOutlineElementCountToBe(2); // ensure no dupes from multisessions
+	await outline.expectOutlineElementToBeVisible('global_variable = "hello"');
+	await outline.expectOutlineElementToBeVisible('def demonstrate_scope');
+}
+
+async function verifyROutline(outline: Outline) {
+	await outline.expectOutlineElementCountToBe(3); // ensure no dupes from multisessions
+	await outline.expectOutlineElementToBeVisible('demonstrate_scope');
+	await outline.expectOutlineElementToBeVisible('global_variable');
+	await outline.expectOutlineElementToBeVisible('local_variable');
+}
