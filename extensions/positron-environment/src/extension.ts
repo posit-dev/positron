9dedf49ba8845ed9ebeeea7f58b637063c778d40
Jonathan McPherson <jonathan@rstudio.com>
2025-03-31 13:36:21 -0700

    richer set of mutations; re apply when settings change

--- extensions/positron-environment/src/extension.ts
+++ extensions/positron-environment/src/extension.ts
@@ -6,22 +6,75 @@
 import * as vscode from 'vscode';
 import * as positron from 'positron';
 
+/**
+ * Interface for the environment variable action. Mirrors the definition in the
+ * configuration schema.
+ */
+interface EnvironmentVariableAction {
+	/** The action to take */
+	action: 'replace' | 'append' | 'prepend';
 
-export function activate(context: vscode.ExtensionContext) {
+	/** The name of the variable */
+	name: string;
+
+	/** The value to replace, append, or remove */
+	value: string;
+};
+
+/**
+ * Apply the configuration for the environment variables.
+ *
+ * @param context The extension context.
+ */
+function applyConfiguration(context: vscode.ExtensionContext) {
+
 	const vars = vscode.workspace.getConfiguration('positron.environment');
 
 	// Set the vars using the environment variable collection
-	if (vars.get('enabled')) {
-		const collection = context.environmentVariableCollection;
-		collection.description = 'Global Positron environment variables';
-		const variables = vars.get<Record<string, string>>('variables') ?? {};
+	if (!vars.get('enabled')) {
+		return;
+	}
 
-		// Iterate through the configured environment variables and set them
-		for (const key in variables) {
-			if (Object.prototype.hasOwnProperty.call(variables, key)) {
-				const value = variables[key];
-				collection.replace(key, value);
-			}
+	const collection = context.environmentVariableCollection;
+
+	// Clear the initial collection to remove any old values and set the description
+	collection.clear();
+	collection.description = 'Global Positron environment variables';
+
+	// Get the configured environment variables
+	const actions = vars.get<Array<EnvironmentVariableAction>>('variables') ?? [];
+	for (const action of actions) {
+		switch (action.action) {
+			case 'replace':
+				collection.replace(action.name, action.value);
+				break;
+			case 'append':
+				collection.append(action.name, action.value);
+				break;
+			case 'prepend':
+				collection.prepend(action.name, action.value);
+				break;
 		}
 	}
+}
+
+/**
+ * Activate the extension. Main entry point; called when the extension is
+ * activated.
+ *
+ * @param context The extension context.
+ */
+export function activate(context: vscode.ExtensionContext) {
+	// Apply the initial configuration values.
+	applyConfiguration(context);
+
+	// Register a listener for when the configuration changes and reapply
+	// the configuration.
+	const disposable = vscode.workspace.onDidChangeConfiguration((e) => {
+		if (e.affectsConfiguration('positron.environment')) {
+			applyConfiguration(context);
+		}
+	});
+
+	context.subscriptions.push(disposable);
 }
