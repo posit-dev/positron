2f2c27086a004ef9b09cfc892879ef9496d168bc
Marie Idleman <marie.idleman@posit.co>
2025-04-01 15:48:41 -0500

    move delete test with others

--- test/e2e/tests/sessions/session-delete.test.ts
+++ test/e2e/tests/sessions/session-delete.test.ts
@@ -31,6 +31,30 @@ test.describe('Sessions: Delete', {
 		await sessions.expectSessionCountToBe(0);
 	});
 
+	test('Validate session picker and variables after delete', {
+		tag: [tags.VARIABLES]
+	}, async function ({ app, sessions }) {
+		const { variables } = app.workbench;
+		await sessions.deleteAll();
+
+		// Ensure sessions exist and are idle
+		const [pySession, rSession] = await sessions.start(['python', 'r']);
+
+		// Delete 1st session and verify active sessions and runtime in session picker
+		await sessions.delete(pySession.id);
+		await sessions.expectSessionPickerToBe(rSession);
+		await sessions.expectSessionCountToBe(1);
+		await sessions.expectActiveSessionListsToMatch();
+		await variables.expectRuntimeToBe('visible', rSession.name);
+
+		// Delete 2nd session and verify no active sessions or runtime in session picker
+		await sessions.delete(rSession.id);
+		await sessions.expectSessionPickerToBe('Start Session');
+		await sessions.expectSessionCountToBe(0);
+		await sessions.expectActiveSessionListsToMatch();
+		await variables.expectRuntimeToBe('not.visible', `${rSession.name}|${pySession.name}|None`);
+	});
+
 	test('Python & R - Validate can delete multiple sessions', async function ({ sessions }) {
 		await sessions.start(['python', 'r', 'python', 'pythonAlt', 'pythonAlt', 'r', 'rAlt', 'rAlt']);
 		await sessions.expectSessionCountToBe(8);
