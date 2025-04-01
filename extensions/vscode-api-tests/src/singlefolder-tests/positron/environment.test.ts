895de3dba1378ddb1c18e520855f26f0a7bef898
Jonathan McPherson <jonathan@rstudio.com>
2025-04-01 15:17:58 -0700

    comprehensive action checking

--- extensions/vscode-api-tests/src/singlefolder-tests/positron/environment.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/positron/environment.test.ts
@@ -45,9 +45,15 @@ suite('positron API - environment', () => {
 		assert.ok(contributedActions, 'Contributed actions should be defined');
 		assert.strictEqual(contributedActions.length, 3, 'Should have 3 actions');
 
-		// Spot check first action
+		// Check all the actions
 		assert.strictEqual(contributedActions[0].action, vscode.EnvironmentVariableMutatorType.Append, 'First action should be append');
 		assert.strictEqual(contributedActions[0].name, 'test', 'First action name should be "test"');
 		assert.strictEqual(contributedActions[0].value, 'value', 'First action value should be "value"');
+		assert.strictEqual(contributedActions[1].action, vscode.EnvironmentVariableMutatorType.Prepend, 'Second action should be prepend');
+		assert.strictEqual(contributedActions[1].name, 'test2', 'Second action name should be "test2"');
+		assert.strictEqual(contributedActions[1].value, 'value2', 'Second action value should be "value2"');
+		assert.strictEqual(contributedActions[2].action, vscode.EnvironmentVariableMutatorType.Replace, 'Third action should be replace');
+		assert.strictEqual(contributedActions[2].name, 'test3', 'Third action name should be "test3"');
+		assert.strictEqual(contributedActions[2].value, 'value3', 'Third action value should be "value3"');
 	});
 });
