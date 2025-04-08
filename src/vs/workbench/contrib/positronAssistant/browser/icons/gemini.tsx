50c968979b98623475905497fa24e90abb69669f
Tim Mok <timtmok@users.noreply.github.com>
2025-04-08 09:49:14 -0400

    Fix icon contrast for Gemini

--- src/vs/workbench/contrib/positronAssistant/browser/icons/gemini.tsx
+++ src/vs/workbench/contrib/positronAssistant/browser/icons/gemini.tsx
@@ -18,22 +18,9 @@ export const Gemini = (props: SVGProps<SVGSVGElement>) => (
 		xmlns='http://www.w3.org/2000/svg'
 		{...props}
 	>
-		<defs>
-			<linearGradient
-				id='gemini-color_svg__a'
-				x1='0%'
-				x2='68.73%'
-				y1='100%'
-				y2='30.395%'
-			>
-				<stop offset='0%' stopColor='#1C7DFF' />
-				<stop offset='52.021%' stopColor='#1C69FF' />
-				<stop offset='100%' stopColor='#F0DCD6' />
-			</linearGradient>
-		</defs>
 		<path
 			d='M12 24A14.3 14.3 0 0 0 0 12 14.3 14.3 0 0 0 12 0a14.305 14.305 0 0 0 12 12 14.305 14.305 0 0 0-12 12'
-			fill='url(#gemini-color_svg__a)'
+			fill='#1C7DFF'
 		/>
 	</svg>
 );
