6ee1087398f5c1c01bae01662e968e34a8f6c0a7
Jonathan McPherson <jonathan@rstudio.com>
2025-04-09 09:25:52 -0700

    clarify server start in node server

--- src/server-main.ts
+++ src/server-main.ts
@@ -317,34 +317,44 @@ function prompt(question: string): Promise<boolean> {
 }
 
 // --- Start Positron ---
+/**
+ * Start a Positron Kernel Supervisor process. This process is shared among all
+ * the windows that connect to this server. We start it here so that it'll be warm
+ * when the first window connects.
+ */
 async function startKernelSupervisor() {
 	// Create the connection and log file paths. We put these in the user data
 	// path rather than the temporary directory since some environments clean up
 	// the temporary directory aggressively.
 	const userDataPath = getUserDataPath(parsedArgs, product.nameShort || 'positron');
-	const tempFile = path.join(userDataPath, `positron-supervisor-${process.pid}.json`);
+	const connectionFile = path.join(userDataPath, `positron-supervisor-${process.pid}.json`);
 	const logFile = path.join(userDataPath, `positron-supervisor-${process.pid}.log`);
 
 	// Unlikely, but if the files already exist, delete them; they are stale.
-	if (fs.existsSync(tempFile)) {
-		fs.unlinkSync(tempFile);
+	if (fs.existsSync(connectionFile)) {
+		fs.unlinkSync(connectionFile);
 	}
 	if (fs.existsSync(logFile)) {
 		fs.unlinkSync(logFile);
 	}
 
 	// Pass the connection file path to the supervisor extension.
-	process.env['POSITRON_SUPERVISOR_CONNECTION_FILE'] = tempFile;
+	process.env['POSITRON_SUPERVISOR_CONNECTION_FILE'] = connectionFile;
 
 	// Search local paths for the supervisor
 	const supervisorPaths = [
+		// Dev build of the supervisor in the kallichore repository (debug version)
 		path.join(__dirname, '..', '..',
 			'kallichore', 'target', 'debug', 'kcserver'),
+		// Dev build of the supervisor in the kallichore repository (release version)
 		path.join(__dirname, '..', '..',
 			'kallichore', 'target', 'release', 'kcserver'),
+		// Release build of the supervisor, or a dev build in the extensions folder
 		path.join(__dirname, '..',
 			'extensions', 'positron-supervisor', 'resources', 'kallichore', 'kcserver'),
 	];
+
+	// Find the first of these paths that exists.
 	const supervisorPath = supervisorPaths.find((p) => fs.existsSync(p));
 	if (!supervisorPath) {
 		process.stderr.write('The Positron Kernel Supervisor was not found and will not be started.\n');
@@ -354,7 +364,7 @@ async function startKernelSupervisor() {
 	// Start the supervisor process.
 	process.stdout.write(`\nStarting Positron Kernel Supervisor (${supervisorPath})...\n`);
 	const supervisorProcess = spawn(supervisorPath, [
-		'--connection-file', tempFile, '--log-file', logFile,]);
+		'--connection-file', connectionFile, '--log-file', logFile,]);
 	supervisorProcess.stdout.on('data', (data) => {
 		process.stdout.write(data);
 	});
