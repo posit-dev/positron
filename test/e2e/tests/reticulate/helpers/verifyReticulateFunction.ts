26d5f097447ec6d100998c673ff0cf46c2aad019
Chris Mead <chris.mead@posit.co>
2025-04-11 13:55:22 -0600

    multiple reticulate sessions

--- test/e2e/tests/reticulate/helpers/verifyReticulateFunction.ts
+++ test/e2e/tests/reticulate/helpers/verifyReticulateFunction.ts
@@ -6,11 +6,18 @@
 import { expect } from '@playwright/test';
 import { Application } from '../../../infra/index.js';
 
-export async function verifyReticulateFunctionality(app: Application, rSessionId: string, pythonSessionId: string = 'Python (reticulate)') {
-
+export async function verifyReticulateFunctionality(
+	app: Application,
+	rSessionId: string,
+	pythonSessionId: string = 'Python (reticulate)',
+	value = '200',
+	value2 = '400',
+	value3 = '6'): Promise<void> {
+	// Verify that reticulate is installed
+	// Create a variable in Python and expect to be able to access it from R
 	await app.workbench.sessions.select(pythonSessionId);
 
-	await app.workbench.console.pasteCodeToConsole('x=100');
+	await app.workbench.console.pasteCodeToConsole(`x=${value}`);
 	await app.workbench.console.sendEnterKey();
 
 	await app.workbench.console.clearButton.click();
@@ -26,27 +33,27 @@ export async function verifyReticulateFunctionality(app: Application, rSessionId
 
 	await expect(async () => {
 		const variablesMap = await app.workbench.variables.getFlatVariables();
-		expect(variablesMap.get('y')).toStrictEqual({ value: '100', type: 'int' });
+		expect(variablesMap.get('y')).toStrictEqual({ value: value, type: 'int' });
 	}).toPass({ timeout: 10000 });
 
 	await app.workbench.layouts.enterLayout('stacked');
 
 	// Create a variable in R and expect to be able to access it from Python
-	await app.workbench.console.pasteCodeToConsole('y <- 200L');
+	await app.workbench.console.pasteCodeToConsole(`y <- ${value2}L`);
 	await app.workbench.console.sendEnterKey();
 
 	// Executing reticulate::repl_python() should not start a new interpreter
 	// but should move focus to the reticulate interpreter
-	await app.workbench.console.pasteCodeToConsole('reticulate::repl_python(input = "z = 3")');
+	await app.workbench.console.pasteCodeToConsole(`reticulate::repl_python(input = "z = ${value3}")`);
 	await app.workbench.console.sendEnterKey();
 
 	// Expect that focus changed to the reticulate console
 	await app.workbench.sessions.expectSessionPickerToBe(pythonSessionId);
 	await app.workbench.console.pasteCodeToConsole('print(r.y)');
 	await app.workbench.console.sendEnterKey();
-	await app.workbench.console.waitForConsoleContents('200');
+	await app.workbench.console.waitForConsoleContents(value2);
 
 	await app.workbench.console.pasteCodeToConsole('print(z)');
 	await app.workbench.console.sendEnterKey();
-	await app.workbench.console.waitForConsoleContents('200');
+	await app.workbench.console.waitForConsoleContents(value3);
 }
