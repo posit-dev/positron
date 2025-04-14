5e8c91aa3b9b3e049a0581484e1974d23d9691a6
Sam Clark <sam.clark@posit.co>
2025-04-14 11:20:57 -0500

    Implements proper settings diff view

--- src/vs/workbench/contrib/positronWelcome/browser/actions.ts
+++ src/vs/workbench/contrib/positronWelcome/browser/actions.ts
@@ -8,15 +8,16 @@ import { URI } from '../../../../base/common/uri.js';
 import { ServicesAccessor } from '../../../../editor/browser/editorExtensions.js';
 import { localize } from '../../../../nls.js';
 import { Action2 } from '../../../../platform/actions/common/actions.js';
+import { ICommandService } from '../../../../platform/commands/common/commands.js';
 import { ConfigurationTarget } from '../../../../platform/configuration/common/configuration.js';
 import { IFileService } from '../../../../platform/files/common/files.js';
 import { IStorageService } from '../../../../platform/storage/common/storage.js';
-import { IResourceDiffEditorInput } from '../../../common/editor.js';
+import { DiffEditorInput } from '../../../common/editor/diffEditorInput.js';
 import { IEditorService } from '../../../services/editor/common/editorService.js';
 import { IPathService } from '../../../services/path/common/pathService.js';
 import { IPreferencesService } from '../../../services/preferences/common/preferences.js';
+import { COMPARE_WITH_SAVED_COMMAND_ID } from '../../files/browser/fileConstants.js';
 import { setImportWasPrompted } from './helpers.js';
-
 
 export class PositronImportSettings extends Action2 {
 	/**
@@ -45,9 +46,10 @@ export class PositronImportSettings extends Action2 {
 	 */
 	override async run(accessor: ServicesAccessor): Promise<void> {
 		const pathService = accessor.get(IPathService);
+		const commandService = accessor.get(ICommandService);
+		const prefService = accessor.get(IPreferencesService);
 		const fileService = accessor.get(IFileService);
 		const editorService = accessor.get(IEditorService);
-		const prefService = accessor.get(IPreferencesService);
 
 		const positronSettingsPath = await prefService.getEditableSettingsURI(ConfigurationTarget.USER);
 		if (!positronSettingsPath) {
@@ -55,54 +57,29 @@ export class PositronImportSettings extends Action2 {
 			return;
 		}
 
-		const positronSettingsContent = await fileService
-			.readFile(positronSettingsPath)
-			.then(content => content.value.toString());
-		const positronData = JSON.parse(positronSettingsContent);
-
-		if (!positronData) {
-			alert('No Positron settings found');
+		const codeSettingsPath = await this.getCodeSettingsPath(pathService);
+		if (!codeSettingsPath) {
+			alert('No Visual Studio Code settings found');
 			return;
 		}
 
-		const codeSettingsPath = await this.getCodeSettingsPath(pathService);
 		const codeSettingsContent = await fileService
 			.readFile(codeSettingsPath)
 			.then(content => content.value.toString());
-		const codeData = JSON.parse(codeSettingsContent);
 
-		if (!codeData) {
-			alert('No Code settings found');
-			return;
+
+		await commandService.executeCommand(COMPARE_WITH_SAVED_COMMAND_ID, positronSettingsPath);
+		const editor = editorService.activeEditor;
+
+		if (editor instanceof DiffEditorInput) {
+			const model = editorService.activeTextEditorControl?.getModel();
+			if (model) {
+				if ('original' in model && 'modified' in model) {
+					model.modified.setValue('// Settings imported from Visual Studio Code\n' + codeSettingsContent);
+				}
+			}
 		}
-
-		const orderedPositronData = Object.keys(positronData).sort().reduce(
-			(obj, key) => {
-				obj[key] = positronData[key];
-				return obj;
-			},
-			{} as any
-		);
-
-		const orderedCodeData = Object.keys(codeData).sort().reduce(
-			(obj, key) => {
-				obj[key] = codeData[key];
-				return obj;
-			},
-			{} as any
-		);
-
-
-
-		const input: IResourceDiffEditorInput = {
-			original: { resource: undefined, contents: JSON.stringify(orderedPositronData, null, 2), label: 'Positron Settings', options: { transient: true } },
-			modified: { resource: undefined, contents: JSON.stringify(orderedCodeData, null, 2), label: 'Visual Studio Code Settings', options: { transient: true } },
-		};
-
-		await editorService.openEditor(input);
-
 	}
-
 
 
 	private async getCodeSettingsPath(pathService: IPathService): Promise<URI> {
