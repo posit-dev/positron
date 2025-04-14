d776433f88f2c44eff00c971ba01d7f6f9f6bbd9
Aaron Jacobs <aaron.jacobs@posit.co>
2025-04-14 19:10:51 -0400

    Initial commit. Scaffold a new extension.
    Signed-off-by: Aaron Jacobs <aaron.jacobs@posit.co>

---
+++ extensions/positron-catalog-explorer/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('"positron-catalogs" is now active!');
+}
+
+export function deactivate() {}
