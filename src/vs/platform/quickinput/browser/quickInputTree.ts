143c27ca5fa885b9ddab364513eb91cb6a72159e
Isabel Zimmerman <54685329+isabelizimm@users.noreply.github.com>
2025-04-16 15:12:00 -0400

    add flags to quickpickitem (#7278)
    thanks @austin3dickey for the catch post-merge on #7115 😬 adding flags
    to Positron specific code

--- src/vs/platform/quickinput/browser/quickInputTree.ts
+++ src/vs/platform/quickinput/browser/quickInputTree.ts
@@ -1406,7 +1406,9 @@ export class QuickInputTree extends Disposable {
 					element.labelHighlights = labelHighlights;
 					element.descriptionHighlights = descriptionHighlights;
 					element.detailHighlights = detailHighlights;
+					// --- Start Positron ---
 					element.hidden = element.item.neverShowWhenFiltered || false;
+					// --- End Positron ---
 				} else {
 					element.labelHighlights = undefined;
 					element.descriptionHighlights = undefined;
