291599b25cf6d3a8f36d2f9b921a28ce5b165045
Chris Mead <chris.mead@posit.co>
2025-04-18 12:00:35 -0600

    add R test

--- test/e2e/tests/notebook/notebook-large-r.test.ts
+++ test/e2e/tests/notebook/notebook-large-r.test.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -8,32 +8,37 @@ import { test, expect, tags } from '../_test.setup';
 
 test.use({
 	suiteId: __filename,
+	snapshots: false
 });
 
-test.describe('Large Python Notebook', {
+
+test.describe('Large R Notebook', {
 	tag: [tags.NOTEBOOKS, tags.WIN, tags.WEB]
 }, () => {
 
-	test('Python - Large notebook execution', async function ({ app, python }) {
+	test('R - Large notebook execution', async function ({ app, r }) {
 		test.slow();
 		const notebooks = app.workbench.notebooks;
 
-		await app.workbench.quickaccess.openDataFile(join(app.workspacePathOrFolder, 'workspaces', 'large_py_notebook', 'spotify.ipynb'));
-		await notebooks.selectInterpreter('Python');
+		await app.workbench.quickaccess.openDataFile(join(app.workspacePathOrFolder, 'workspaces', 'large_r_notebook', 'spotify.ipynb'));
+		await notebooks.selectInterpreter('R');
 
 		await notebooks.runAllCells(120000);
 
 		await app.workbench.quickaccess.runCommand('notebook.focusTop');
-		await app.code.driver.page.locator('span').filter({ hasText: 'import pandas as pd' }).locator('span').first().click();
+		await app.code.driver.page.locator('span').filter({ hasText: 'library(dplyr)' }).locator('span').first().click();
 
 		const allFigures: any[] = [];
 		const uniqueLocators = new Set<string>();
 
-		for (let i = 0; i < 12; i++) {
+		for (let i = 0; i < 6; i++) {
 
-			await app.code.driver.page.keyboard.press('PageDown');
+			for (let j = 0; j < 100; j++) {
+				// second param to mouse.wheel is not processed correctly so loop is needed
+				await app.code.driver.page.mouse.wheel(0, 1);
+			}
 
-			const figureLocator = app.workbench.notebooks.frameLocator.locator('.plot-container');
+			const figureLocator = app.workbench.notebooks.frameLocator.locator('.output_container');
 			const figures = await figureLocator.all();
 
 			if (figures!.length > 0) {
@@ -46,6 +51,6 @@ test.describe('Large Python Notebook', {
 			}
 		}
 
-		expect(allFigures.length).toBeGreaterThan(20);
+		expect(allFigures.length).toBeGreaterThan(10);
 	});
 });
