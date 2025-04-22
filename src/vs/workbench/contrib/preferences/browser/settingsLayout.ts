348c4db6865071c6f4a19fee0810aac5267047e9
Brian Lambert <brianlambert@gmail.com>
2025-04-22 12:06:50 -0700

    Updates to Data Explorer settings

--- src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
+++ src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
@@ -182,6 +182,11 @@ export const tocData: ITOCEntry<string> = {
 				},
 				// --- Start Positron ---
 				{
+					id: 'features/dataExplorer',
+					label: localize('dataExplorer', "Data Explorer"),
+					settings: ['dataExplorer.*']
+				},
+				{
 					id: 'features/environment',
 					label: localize('environment', "Environment"),
 					settings: ['environment.*']
