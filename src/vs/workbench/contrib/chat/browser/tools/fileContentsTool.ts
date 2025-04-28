4ce83a0727a92a68bb8cc7d498a6daa04599e164
sharon wang <sharon-wang@users.noreply.github.com>
2025-04-28 14:57:52 -0400

    add clickable file reference for file contents tool

--- src/vs/workbench/contrib/chat/browser/tools/fileContentsTool.ts
+++ src/vs/workbench/contrib/chat/browser/tools/fileContentsTool.ts
@@ -11,6 +11,8 @@ import { localize } from '../../../../../nls.js';
 import { IWorkspaceContextService } from '../../../../../platform/workspace/common/workspace.js';
 import { GroupsOrder, IEditorGroupsService } from '../../../../services/editor/common/editorGroupsService.js';
 import { ITextFileService } from '../../../../services/textfile/common/textfiles.js';
+import { ChatModel } from '../../common/chatModel.js';
+import { IChatService } from '../../common/chatService.js';
 import { CountTokensCallback, IPreparedToolInvocation, IToolData, IToolImpl, IToolInvocation, IToolResult } from '../../common/languageModelToolsService.js';
 import { IToolInputProcessor } from '../../common/tools/tools.js';
 
@@ -44,6 +46,7 @@ export const FileContentsToolData: IToolData = {
 
 export class FileContentsTool implements IToolImpl {
 	constructor(
+		@IChatService private readonly _chatService: IChatService,
 		@ITextFileService private readonly _textFileService: ITextFileService,
 		@IEditorGroupsService private readonly _editorGroupsService: IEditorGroupsService,
 		@IWorkspaceContextService private readonly _workspaceContextService: IWorkspaceContextService,
@@ -76,6 +79,16 @@ export class FileContentsTool implements IToolImpl {
 
 		// The file is in the workspace, so grab the file contents
 		const { value, size, encoding } = await this._textFileService.read(uri);
+
+		// If we have a chat context, create a clickable file reference
+		if (invocation.context) {
+			const model = this._chatService.getSession(invocation.context?.sessionId) as ChatModel;
+			const request = model.getRequests().at(-1)!;
+			model.acceptResponseProgress(request, {
+				kind: 'inlineReference',
+				inlineReference: uri,
+			});
+		}
 
 		return {
 			content: [{ kind: 'text', value: JSON.stringify({ contents: value, size, encoding, }) }],
