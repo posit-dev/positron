017a78eb2b91dabbaa4a3b8c3176016e54c14a1d
Marie Idleman <marie.idleman@posit.co>
2025-05-02 06:13:07 -0500

    e2e-test: python console flake on executing code (#7494)
    ### Summary
    Switching out method to enter code in console to reduce flake.
    
    ### QA Notes
    
    @:console

--- test/e2e/tests/console/console-python.test.ts
+++ test/e2e/tests/console/console-python.test.ts
@@ -13,13 +13,13 @@ test.describe('Console Pane: Python', { tag: [tags.WEB, tags.CONSOLE, tags.WIN] 
 
 	test('Python - queue user input while interpreter is starting', async function ({ app, sessions }) {
 		await sessions.startAndSkipMetadata({ language: 'Python', waitForReady: false });
-		await app.workbench.console.executeCode('Python', 'import time; time.sleep(5); print("done");');
+		await app.workbench.console.executeCode('Python', 'import time; time.sleep(5); print("done");',);
 		await app.workbench.console.waitForConsoleContents('done', { expectedCount: 2, timeout: 30000 });
 	});
 
 	test('Python - Verify cancel button on console bar', async function ({ app, python }) {
-		await app.workbench.console.typeToConsole('import time', true);
-		await app.workbench.console.typeToConsole('time.sleep(10)', true);
+		await app.workbench.console.executeCode('Python', 'import time');
+		await app.workbench.console.executeCode('Python', 'time.sleep(10)', { waitForReady: false });
 		await app.workbench.console.interruptExecution();
 	});
 });
@@ -35,7 +35,7 @@ test.describe('Console Pane: Alternate Python', () => {
 	test('Verify alternate python can skip bundled ipykernel', async ({ app, sessions }) => {
 		await sessions.start('pythonAlt');
 		await app.workbench.console.clearButton.click();
-		await app.workbench.console.pasteCodeToConsole(`import ipykernel; ipykernel.__file__`, true);
+		await app.workbench.console.executeCode('Python', 'import ipykernel; ipykernel.__file__');
 		await app.workbench.console.waitForConsoleContents('site-packages');
 	});
 
