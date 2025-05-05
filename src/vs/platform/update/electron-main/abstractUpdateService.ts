3981d394be3036ac436a8f58e7dad36a4e31243a
Tim Mok <timtmok@users.noreply.github.com>
2025-05-05 13:34:06 -0400

    Add preference for release channel

--- src/vs/platform/update/electron-main/abstractUpdateService.ts
+++ src/vs/platform/update/electron-main/abstractUpdateService.ts
@@ -22,13 +22,6 @@ import { IUpdate } from '../common/update.js';
 import { hasUpdate } from '../electron-main/positronVersion.js';
 import { INativeHostMainService } from '../../native/electron-main/nativeHostMainService.js';
 
-export const enum UpdateChannel {
-	Releases = 'releases',
-	Prereleases = 'prereleases',
-	Dailies = 'dailies',
-	Staging = 'staging',
-}
-
 export function createUpdateURL(platform: string, channel: string, productService: IProductService): string {
 	return `${productService.updateUrl}/${channel}/${platform}`;
 	//--- End Positron ---
@@ -93,7 +86,7 @@ export abstract class AbstractUpdateService implements IUpdateService {
 	*/
 	protected async initialize(): Promise<void> {
 		// --- Start Positron ---
-		const updateChannel = process.env.POSITRON_UPDATE_CHANNEL ?? UpdateChannel.Prereleases;
+		const updateChannel = process.env.POSITRON_UPDATE_CHANNEL ?? this.configurationService.getValue<string>('update.positron.channel');
 		this.enableAutoUpdate = this.configurationService.getValue<boolean>('update.autoUpdate');
 
 		if (this.environmentMainService.disableUpdates) {
