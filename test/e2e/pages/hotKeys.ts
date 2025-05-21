40a8ecb0767eaf8d3fa522a887c80398acad1a90
Christopher Mead <chrisrmead@comcast.net>
2025-05-21 10:57:40 -0600

    E2E test: basic air test (#7778)
    Just a quick test to make sure the lights are on with Air.
    
    ### QA Notes
    
    @:web @:win @:extensions

--- test/e2e/pages/hotKeys.ts
+++ test/e2e/pages/hotKeys.ts
@@ -85,13 +85,13 @@ export class HotKeys {
 	 * Note: Supports multiple key sequences separated by spaces.
 	 * @param keyCombo the hotkeys to press (e.g. "Cmd+Shift+P").
 	 */
-	private async pressHotKeys(keyCombo: string, options: { platformAdjust?: boolean } = {}) {
-		const { platformAdjust = true } = options;
+	private async pressHotKeys(keyCombo: string) {
 		const modifierKey = this.getModifierKey();
 
 		await test.step(`Press hotkeys: ${keyCombo}`, async () => {
 			// Replace "Cmd" with the platform-appropriate modifier key
-			const keySequences = keyCombo.split(' ').map(keys => keys.replace(/Cmd/g, platformAdjust ? modifierKey : 'Cmd'));
+			// and (for Windows and Ubuntu) replace "Option" with "Alt"
+			const keySequences = keyCombo.split(' ').map(keys => keys.replace(/Cmd/g, modifierKey));
 
 			for (const key of keySequences) {
 				await this.code.driver.page.keyboard.press(key);
