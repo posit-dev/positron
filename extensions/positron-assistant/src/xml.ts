9d018af693c032cca90e989eddf4110bd3a5ac42
seem <mwlorgat@gmail.com>
2025-05-22 13:31:39 +0200

    clarify xml validity

--- extensions/positron-assistant/src/xml.ts
+++ extensions/positron-assistant/src/xml.ts
@@ -3,6 +3,13 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+/**
+ * NOTE: The goal of this module is to use XML-like syntax to structure information
+ *       in LLM conversations, NOT to create valid XML. We may drift significantly
+ *       from XML syntax if it produces better LLM results. This is also why we decided
+ *       to not use a third party library.
+ */
+
 function xmlAttributes(attributes: Record<string, any>): string {
 	return Object.entries(attributes)
 		.map(([key, val]) => `${key}="${val}"`)
@@ -10,7 +17,9 @@ function xmlAttributes(attributes: Record<string, any>): string {
 }
 
 /**
- * Create an XML node.
+ * Create an XML-like node for structuring information in LLM prompts.
+ *
+ * NOTE: Do not use this function where valid XML is required.
  *
  * @param name The name of the node.
  * @param content The content of the node.
@@ -31,7 +40,9 @@ export function node(name: string, content?: string, attributes?: Record<string,
 }
 
 /**
- * Create a self-closing XML node.
+ * Create a self-closing XML node for structuring information in LLM prompts.
+ *
+ * NOTE: Do not use this function where valid XML is required.
  *
  * @param name The name of the node.
  * @param attributes The attributes of the node.
