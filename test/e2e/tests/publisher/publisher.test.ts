34fa8a6ac02c5e445e798331a81b91c5c9f83477
Rodrigo Silva Ferreira <rodrigo.ferreira@posit.co>
2025-05-28 11:45:40 -0400

    commenting out-of-scope aspects of test

--- test/e2e/tests/publisher/publisher.test.ts
+++ test/e2e/tests/publisher/publisher.test.ts
@@ -5,15 +5,17 @@
 
 import { test, tags, expect } from '../_test.setup';
 
-
 test.use({
 	suiteId: __filename
 });
 
 test.describe('Publisher - Positron', { tag: [tags.WEB, tags.WIN, tags.PUBLISHER] }, () => {
+	/*
+	Temporarily disabled file deletion from Connect after test runs for the same reason below.
 	test.afterAll('Delete file from Posit Connect', async function ({ app }) {
 		await app.workbench.positConnect.deleteUserContent();
 	});
+	*/
 
 	test('Verify Publisher functionality in Positron with Shiny app deployment as example', async function ({ app, page, openFile }) {
 		test.slow();
@@ -49,30 +51,41 @@ test.describe('Publisher - Positron', { tag: [tags.WEB, tags.WIN, tags.PUBLISHER
 			await app.workbench.positConnect.selectFilesForDeploy(files);
 		});
 
-		// This step was tricky due to button being inside iframe --> iframe --> shadow DOM (for any left pane interactivity, check this approach)
 		const outerFrame = page.frameLocator('iframe.webview.ready');
 		const innerFrame = outerFrame.frameLocator('iframe#active-frame');
 		const deployButton = innerFrame.locator('vscode-button[data-automation="deploy-button"] >>> button');
 
-		await test.step('Click on Deploy Your Project button', async () => {
-			await deployButton.click();
-		});
-
-		await test.step('Click on View Log', async () => {
-			const viewLogLink = innerFrame.locator('a.webview-link', { hasText: 'View Log' });
-			await viewLogLink.waitFor({ state: 'visible' });
-			await viewLogLink.click();
-		});
-
-		// This step verifies deployment process kicks in. See discussion in #connect channel, posted 05/23.
-		await test.step('Verify deployments process gets kicked in by Publisher (out of scope: whether deployment succeeds', async () => {
-			// Not needed thanks to PR 7840, but this is an example of implementation
-			// await app.workbench.popups.closeSpecificToast('Import your settings from Visual Studio Code into Positron?');
-			await page.getByRole('button', { name: 'Maximize Panel' }).click();
-			// The next two await expects are a bit redundant on purpose. If "Deploy Bundle" isn't visible, test should quickly fail (5secs).
-			await expect(page.locator('span.monaco-highlighted-label:has-text("Deploy Bundle")')).toBeVisible({ timeout: 5000 });
-			// Then, checkmark next to it might take a bit long to appear, which is expected.
-			await expect(page.locator('.monaco-list-row:has-text("Deploy Bundle") .custom-view-tree-node-item-icon.codicon.codicon-check')).toBeVisible({ timeout: 90000 });
+		await test.step('Expect Deploy button to appear', async () => {
+			await expect(deployButton).toBeVisible();
 		});
 	});
 });
+
+/*
+Temporarily commenting this out, but keeping the code because later we may have a more feature-rich integration with Publisher.
+// This step was tricky due to button being inside iframe --> iframe --> shadow DOM (for any left pane interactivity, check this approach)
+const outerFrame = page.frameLocator('iframe.webview.ready');
+const innerFrame = outerFrame.frameLocator('iframe#active-frame');
+const deployButton = innerFrame.locator('vscode-button[data-automation="deploy-button"] >>> button');
+
+await test.step('Click on Deploy Your Project button', async () => {
+	await deployButton.click();
+});
+
+await test.step('Click on View Log', async () => {
+	const viewLogLink = innerFrame.locator('a.webview-link', { hasText: 'View Log' });
+	await viewLogLink.waitFor({ state: 'visible' });
+	await viewLogLink.click();
+});
+
+// This step verifies deployment process kicks in. See discussion in #connect channel, posted 05/23.
+await test.step('Verify deployments process gets kicked in by Publisher (out of scope: whether deployment succeeds', async () => {
+	// Not needed thanks to PR 7840, but this is an example of implementation
+	// await app.workbench.popups.closeSpecificToast('Import your settings from Visual Studio Code into Positron?');
+	await page.getByRole('button', { name: 'Maximize Panel' }).click();
+	// The next two await expects are a bit redundant on purpose. If "Deploy Bundle" isn't visible, test should quickly fail (5secs).
+	await expect(page.locator('span.monaco-highlighted-label:has-text("Deploy Bundle")')).toBeVisible({ timeout: 5000 });
+	// Then, checkmark next to it might take a bit long to appear, which is expected.
+	await expect(page.locator('.monaco-list-row:has-text("Deploy Bundle") .custom-view-tree-node-item-icon.codicon.codicon-check')).toBeVisible({ timeout: 90000 });
+});
+*/
