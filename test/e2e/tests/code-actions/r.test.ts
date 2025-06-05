842bb0c04a86b524075cff0fa8c22e0407d99cec
Christopher Mead <chrisrmead@comcast.net>
2025-06-05 14:41:31 -0600

    E2E test: R code folding (#7992)
    R Code folding test
    
    ### QA Notes
    
    @:web @:win @:editor

--- test/e2e/tests/code-actions/r.test.ts
+++ test/e2e/tests/code-actions/r.test.ts
@@ -3,15 +3,22 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { join } from 'path';
+import path, { join } from 'path';
 import { test, tags } from '../_test.setup';
 import { expect } from '@playwright/test';
+import { fail } from 'assert';
 
 test.use({
 	suiteId: __filename
 });
 
-test.describe('R Code Actions', { tag: [tags.CONSOLE, tags.WIN, tags.WEB] }, () => {
+test.describe('R Code Actions', { tag: [tags.EDITOR, tags.WIN, tags.WEB] }, () => {
+
+	test.afterEach(async function ({ app, hotKeys, cleanup }) {
+		await hotKeys.closeAllEditors();
+		await cleanup.discardAllChanges();
+	});
+
 
 	test("R - Can insert a Roxygen skeleton", async function ({ app, r, openFile }) {
 
@@ -45,5 +52,91 @@ test.describe('R Code Actions', { tag: [tags.CONSOLE, tags.WIN, tags.WEB] }, () 
 		expect(line12).toBe('#\' @examples');
 
 	});
+
+
+	test("R - Can fold code", async function ({ app, r, hotKeys }) {
+
+		const fileName = 'folding.R';
+		await test.step('Create test file', async () => {
+
+			await app.workbench.quickaccess.runCommand('workbench.action.files.newUntitledFile', { keepOpen: false });
+
+			await hotKeys.save();
+
+			await app.workbench.quickInput.waitForQuickInputOpened();
+
+			await app.workbench.quickInput.type(path.join(app.workspacePathOrFolder, fileName));
+
+			await app.workbench.quickInput.clickOkButton();
+
+			await app.workbench.quickInput.waitForQuickInputClosed();
+
+			await app.workbench.editor.selectTabAndType(fileName, collapseText);
+		});
+
+		await test.step('Single hash collpase', async () => {
+			await app.code.driver.page.locator('.codicon-folding-expanded').first().click();
+
+			await expect(app.code.driver.page.locator('.codicon-folding-collapsed')).toHaveCount(1);
+
+			try {
+				const line2 = await app.workbench.editor.getLine(fileName, 2);
+				fail(`Expected line 2 to be folded, but got: ${line2}`);
+			} catch { } // expected error when line is folded
+
+			await app.code.driver.page.locator('.codicon-folding-collapsed').first().click();
+
+			await expect(app.code.driver.page.locator('.codicon-folding-expanded')).toHaveCount(4);
+		});
+
+		await test.step('Double hash collpase', async () => {
+
+			await app.code.driver.page.locator('.codicon-folding-expanded').nth(1).click();
+
+			await expect(app.code.driver.page.locator('.codicon-folding-collapsed')).toHaveCount(1);
+
+			try {
+				const line4 = await app.workbench.editor.getLine(fileName, 4);
+				fail(`Expected line 4 to be folded, but got: ${line4}`);
+			} catch { } // expected error when line is folded
+
+			const line9 = await app.workbench.editor.getLine(fileName, 9);
+			expect(line9).toBe('## Section 1.2 ----');
+
+			await app.code.driver.page.locator('.codicon-folding-collapsed').first().click();
+
+			await expect(app.code.driver.page.locator('.codicon-folding-expanded')).toHaveCount(4);
+		});
+
+		await test.step('Triple hash collpase', async () => {
+			await app.code.driver.page.locator('.codicon-folding-expanded').nth(2).click();
+
+			await expect(app.code.driver.page.locator('.codicon-folding-collapsed')).toHaveCount(1);
+
+			try {
+				const line6 = await app.workbench.editor.getLine(fileName, 6);
+				fail(`Expected line 6 to be folded, but got: ${line6}`);
+			} catch { } // expected error when line is folded
+
+			await app.code.driver.page.locator('.codicon-folding-collapsed').first().click();
+
+			await expect(app.code.driver.page.locator('.codicon-folding-expanded')).toHaveCount(4);
+
+			const line7 = await app.workbench.editor.getLine(fileName, 7);
+			expect(line7).toBe('#### Section 1.1.1.1 ----');
+		});
+
+
+	});
 });
 
+const collapseText =
+	`# Section 1 ----
+
+## Section 1.1 ----
+
+### Section 1.1.1 ----
+
+#### Section 1.1.1.1 ----
+
+## Section 1.2 ----`;
