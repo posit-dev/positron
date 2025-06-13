62792a9b6e7adbb4b3cfc10c2492246b5d955026
Rodrigo Silva Ferreira <rodrigo.ferreira@posit.co>
2025-06-13 16:57:46 -0400

    cleanup

--- test/e2e/tests/data-explorer/data-columns.test.ts
+++ test/e2e/tests/data-explorer/data-columns.test.ts
@@ -10,15 +10,7 @@ test.use({
 	suiteId: __filename
 });
 
-test.describe('Verify data columns using UI', { tag: [tags.WEB, tags.WIN, tags.DATA_EXPLORER] }, () => {
-	test.afterEach(async ({ app, page }) => {
-		await page.getByRole('button', { name: 'Select Column' }).focus();
-		await page.keyboard.press('Escape');
-		await page.keyboard.press('Escape');
-		await expect(page.locator('.positron-modal-popup')).toHaveCount(0);
-		await app.workbench.console.clearButton.click();
-		await app.workbench.sessions.deleteAll();
-	});
+test.describe('Data Explorer: Column Names', { tag: [tags.WEB, tags.WIN, tags.DATA_EXPLORER] }, () => {
 
 	test('Verify data columns - Python', async function ({ app, python, openDataFile }) {
 		await openDataFile('data-files/data_explorer/data_columns.csv');
@@ -29,17 +21,13 @@ test.describe('Verify data columns using UI', { tag: [tags.WEB, tags.WIN, tags.D
 
 });
 
-/* This actually tests pandas, not Positron, so I'm commenting it out
-test.describe('Verify data columns using data frame through console (with Python only)', { tag: [tags.WEB, tags.WIN, tags.DATA_EXPLORER] }, () => {
-	test.afterEach(async ({ app }) => {
+/* Add this after test.describe if there is a need to escape from the filter and delete all sessions for clean-up.
+	test.afterEach(async ({ app, page }) => {
+		await page.getByRole('button', { name: 'Select Column' }).focus();
+		await page.keyboard.press('Escape');
+		await page.keyboard.press('Escape');
+		await expect(page.locator('.positron-modal-popup')).toHaveCount(0);
 		await app.workbench.console.clearButton.click();
 		await app.workbench.sessions.deleteAll();
 	});
-
-	test('Python can read CSV and verify column names', async ({ app, python }) => {
-		await app.workbench.console.pasteCodeToConsole(pyColumnComparison);
-		await app.workbench.console.sendEnterKey();
-		await app.workbench.console.waitForConsoleContents('True');
-	});
-});
 */
