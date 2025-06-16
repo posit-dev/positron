5073800f24c843a5b1a6e80832954f2ded2b43f9
Rodrigo Silva Ferreira <rodrigo.ferreira@posit.co>
2025-06-16 09:42:35 -0400

    remaining tests

--- test/e2e/tests/interpreters/interpreter-commands.test.ts
+++ test/e2e/tests/interpreters/interpreter-commands.test.ts
@@ -8,6 +8,8 @@ Summary:
 - This test suite verifies the functionality of interpreter commands via Force Quit, Interrupt, and Shutdown for both Python and R.
 - Tests confirm that each quick input command triggers the expected behavior, verified through console outputs (see Table below).
 - After each test, console is cleared and session is fully deleted. Doing both for each test makes the tests more robust indeed.
+- Additional tests, as shown below, have been included for verifying clear interpreter, rename active session, and show interpreter
+output for both Python and R.
 
  * |Command   |Language|Targeted Console Output    |
  * |----------|--------|---------------------------|
@@ -20,7 +22,9 @@ Summary:
  * |Clear     |Python  |'int... has been cleared'  |
  * |Clear     |R       |'int... has been cleared'  |
  * |Rename Act|Python  |'RenamedActive_Python'     |
- * |Rename Act|R.      |'RenamedActive_R'          |
+ * |Rename Act|R       |'RenamedActive_R'          |
+ * |ShowOutput|Python  |'[Python]'                 |
+ * |ShowOutput|R       |'[R]'                      |
  */
 
 import { test, tags, expect } from '../_test.setup';
@@ -29,7 +33,7 @@ test.use({
 	suiteId: __filename
 });
 
-test.describe('Interpreter Commands (Force Quit, Interrupt, and Shutdown', {
+test.describe('Interpreter Commands (Force Quit, Interrupt, Shutdown, Clear Interpreter, and Rename Active Session', {
 	tag: [tags.WEB, tags.INTERPRETER]
 }, () => {
 
@@ -122,15 +126,23 @@ test.describe('Interpreter Commands (Force Quit, Interrupt, and Shutdown', {
 	});
 });
 
+test.describe('Interpreter Commands: Show Active Interpreter Session Output', {
+	tag: [tags.WEB, tags.INTERPRETER, tags.WIN]
+}, () => {
+
+	test('Verify Show Active Interpreter Session Output command works (→ [Python]) - Python', async function ({ app, python, page }) {
+		await app.workbench.quickaccess.runCommand('workbench.action.languageRuntime.showOutput', { keepOpen: false });
+		await expect(page.getByText('[Python] [', { exact: false }).first()).toBeVisible();
+	});
+
+	test('Verify Show Active Interpreter Session Output command works (→ [R]) - R', async function ({ app, r, page }) {
+		await app.workbench.quickaccess.runCommand('workbench.action.languageRuntime.showOutput', { keepOpen: false });
+		await expect(page.getByText('[R] [', { exact: false }).first()).toBeVisible();
+	});
+
+});
+
 /*
 A couple questions for next week:
 1. What would be relevant to add to the POM here? Any suggestions are welcome. Or anything I used that I could have adopted from the codebase?
-2. Having trouble with session output test, to find the output pane... I've tried multiple ways, but the structure seems complex. Any ideas?
-	test('Verify Show Active Interpreter Session Output command works (→ [Python]) - Python', { tag: [tags.WIN] }, async function ({ app, python, page }) {
-		await app.workbench.quickaccess.runCommand('workbench.action.languageRuntime.showOutput', { keepOpen: false });
-	});
-
-	test('Verify Show Active Interpreter Session Output command works (→ [R]?) - R', { tag: [tags.WIN] }, async function ({ app, r, page }) {
-		await app.workbench.quickaccess.runCommand('workbench.action.languageRuntime.showOutput', { keepOpen: false });
-	});
 */
