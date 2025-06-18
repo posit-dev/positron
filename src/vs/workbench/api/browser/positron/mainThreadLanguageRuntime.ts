posit-dev/positron

commit e4b17538c074cba50805050dd9efeab59468a203

Update console logic to detect disconnections (#6832)
Fixing a recent regression.

Console logic is now updated to properly show/hide extensions restarting
flag.


Addresses #6822

src/vs/workbench/services/positronConsole/browser/positronConsoleService.ts
@@ -2090,6 +2090,11 @@ class PositronConsoleInstance extends Disposable implements IPositronConsoleInst
 			// Clear any starting item still present.
 			this.clearStartingItem();
 
+			if (exit.reason === RuntimeExitReason.ExtensionHost) {
+				this.setState(PositronConsoleState.Disconnected);
+				return;
+			}
+
 			// Add a message explaining that the exit occurred, and why.
 			let message = this.formatExit(exit);
 			if (exit.message) {

src/vs/workbench/services/positronConsole/browser/interfaces/positronConsoleService.ts
@@ -31,7 +31,8 @@ export const enum PositronConsoleState {
 	Ready = 'Ready',
 	Offline = 'Offline',
 	Exiting = 'Exiting',
-	Exited = 'Exited'
+	Exited = 'Exited',
+	Disconnected = 'Disconnected'
 }
 
 /**

src/vs/workbench/contrib/positronConsole/browser/components/consoleInstance.tsx
@@ -89,6 +89,7 @@ export const ConsoleInstance = (props: ConsoleInstanceProps) => {
 	const [marker, setMarker] = useState(generateUuid());
 	const [runtimeAttached, setRuntimeAttached] = useState(props.positronConsoleInstance.runtimeAttached);
 	const [, setIgnoreNextScrollEvent, ignoreNextScrollEventRef] = useStateRef(false);
+	const [disconnected, setDisconnected] = useState(false);
 
 	// Determines whether the console is scrollable.
 	const scrollable = () =>
@@ -257,6 +258,8 @@ export const ConsoleInstance = (props: ConsoleInstanceProps) => {
 				// https://github.com/posit-dev/positron/issues/2807
 				scrollToBottom();
 			}
+
+			setDisconnected(state === PositronConsoleState.Disconnected);
 		}));
 
 		// Add the onDidChangeTrace event handler.
@@ -627,6 +630,7 @@ export const ConsoleInstance = (props: ConsoleInstanceProps) => {
 			>
 				<ConsoleInstanceItems
 					consoleInputWidth={consoleInputWidth}
+					disconnected={disconnected}
 					editorFontInfo={editorFontInfo}
 					positronConsoleInstance={props.positronConsoleInstance}
 					runtimeAttached={runtimeAttached}

src/vs/workbench/contrib/positronConsole/browser/components/consoleInstanceItems.tsx
@@ -46,6 +46,7 @@ interface ConsoleInstanceItemsProps {
 	readonly trace: boolean;
 	readonly runtimeAttached: boolean;
 	readonly consoleInputWidth: number;
+	readonly disconnected: boolean;
 	readonly onSelectAll: () => void;
 }
 /**
@@ -76,9 +77,6 @@ export class ConsoleInstanceItems extends Component<ConsoleInstanceItemsProps> {
 	 * @returns The rendered component.
 	 */
 	override render() {
-
-		let extensionHostDisconnected = false;
-
 		return (
 			<>
 				<div className='top-spacer' />
@@ -88,10 +86,8 @@ export class ConsoleInstanceItems extends Component<ConsoleInstanceItemsProps> {
 					} else if (runtimeItem instanceof RuntimeItemPendingInput) {
 						return <RuntimePendingInput key={runtimeItem.id} fontInfo={this.props.editorFontInfo} runtimeItemPendingInput={runtimeItem} />;
 					} else if (runtimeItem instanceof RuntimeItemStartup) {
-						extensionHostDisconnected = false;
 						return <RuntimeStartup key={runtimeItem.id} runtimeItemStartup={runtimeItem} />;
 					} else if (runtimeItem instanceof RuntimeItemReconnected) {
-						extensionHostDisconnected = false;
 						return null;
 					} else if (runtimeItem instanceof RuntimeItemStarting) {
 						return <RuntimeStarting key={runtimeItem.id} runtimeItemStarting={runtimeItem} />;
@@ -100,10 +96,6 @@ export class ConsoleInstanceItems extends Component<ConsoleInstanceItemsProps> {
 					} else if (runtimeItem instanceof RuntimeItemOffline) {
 						return <RuntimeOffline key={runtimeItem.id} runtimeItemOffline={runtimeItem} />;
 					} else if (runtimeItem instanceof RuntimeItemExited) {
-						if (runtimeItem.reason === 'extensionHost') {
-							extensionHostDisconnected = true;
-							return null;
-						}
 						return <RuntimeExited key={runtimeItem.id} runtimeItemExited={runtimeItem} />;
 					} else if (runtimeItem instanceof RuntimeItemRestartButton) {
 						return <RuntimeRestartButton key={runtimeItem.id} positronConsoleInstance={this.props.positronConsoleInstance} runtimeItemRestartButton={runtimeItem} />;
@@ -116,7 +108,7 @@ export class ConsoleInstanceItems extends Component<ConsoleInstanceItemsProps> {
 						return null;
 					}
 				})}
-				{extensionHostDisconnected ?
+				{this.props.disconnected ?
 					(<div className='console-item-starting'>
 						<span className='codicon codicon-loading codicon-modifier-spin'></span>
 						<span>{localize(

src/vs/workbench/contrib/positronConsole/browser/components/consoleInstanceState.tsx
@@ -27,6 +27,7 @@ const statusIconClassNameToColor = {
 
 const consoleStateToStatusIcon = {
 	[PositronConsoleState.Uninitialized]: StatusIconClassName.DISCONNECTED,
+	[PositronConsoleState.Disconnected]: StatusIconClassName.DISCONNECTED,
 	[PositronConsoleState.Starting]: StatusIconClassName.ACTIVE,
 	[PositronConsoleState.Busy]: StatusIconClassName.ACTIVE,
 	[PositronConsoleState.Ready]: StatusIconClassName.IDLE,

commit 572a37b9b646af0b813dbe4d424bc9bfa1629646

Fix frozen console after extension host restart (#8137)
This change addresses a few problems that have accumulated in the
"restart extension host" scenario.

- The console is frozen. This is a regression from
https://github.com/posit-dev/positron/pull/6832; which returns early
before detaching the session; because we never detached the existing
runtime session, the new session was not reattached and console stayed
attached to the old (disconnected) session.
- It is no longer possible to open data explorers from the Variables
pane. As far as I can tell this never worked; fixed by reattaching event
handlers to the new session.
- The logs contain errors about registered runtimes existing on the main
thread but not the extension host, which happens because restarting the
extension host causes it to forget its runtimes (of course). After
auditing the code it appears that it isn't actually necessary for the
extension host to keep a copy of the registration list; fixed by having
this knowledge exist only on the main thread, to be queried by the
extension host when required.
- Various errors about leaked disposables; addressed by making the
session adapter disposable and making sure event handlers get registered
for disposal.

I also found a couple of places where exceptions were not propagated,
which is why the console looks frozen instead of popping up an error. I
added notifications to those places, so if the bug were to regress the
user would get an error instead of nothing at all.

We should also at some point think about whether there is a better way
to manage `ILanguageRuntimeSession` objects. Right now an extension host
restart results in a new `ILanguageRuntimeSession` object with the same
session ID as the previous one; it is imperative that everyone working
with the old one switch to the new one, but this requires verbose and
error-prone code. We could make this better (but not before RC).

Addresses https://github.com/posit-dev/positron/issues/8096

src/vs/workbench/api/browser/positron/mainThreadLanguageRuntime.ts
@@ -91,7 +91,7 @@ class QueuedRuntimeStateEvent extends QueuedRuntimeEvent {
 
 // Adapter class; presents an ILanguageRuntime interface that connects to the
 // extension host proxy to supply language features.
-class ExtHostLanguageRuntimeSessionAdapter implements ILanguageRuntimeSession {
+class ExtHostLanguageRuntimeSessionAdapter extends Disposable implements ILanguageRuntimeSession {
 
 	private readonly _stateEmitter = new Emitter<RuntimeState>();
 	private readonly _startupEmitter = new Emitter<ILanguageRuntimeInfo>();
@@ -144,6 +144,8 @@ class ExtHostLanguageRuntimeSessionAdapter implements ILanguageRuntimeSession {
 		private readonly _editorService: IEditorService,
 		private readonly _proxy: ExtHostLanguageRuntimeShape) {
 
+		super();
+
 		// Save handle
 		this.handle = initialState.handle;
 		this.dynState = {
@@ -162,7 +164,7 @@ class ExtHostLanguageRuntimeSessionAdapter implements ILanguageRuntimeSession {
 		this.onDidEndSession = this._exitEmitter.event;
 
 		// Listen to state changes and track the current state
-		this.onDidChangeRuntimeState((state) => {
+		this._register(this.onDidChangeRuntimeState((state) => {
 			this._currentState = state;
 
 			if (state === RuntimeState.Exited) {
@@ -181,14 +183,14 @@ class ExtHostLanguageRuntimeSessionAdapter implements ILanguageRuntimeSession {
 				// Remove all clients; none can send or receive data any more
 				this._clients.clear();
 			}
-		});
+		}));
 
 		// Listen for changes to the foreground session and notify the extension host
-		this._runtimeSessionService.onDidChangeForegroundSession((session) => {
+		this._register(this._runtimeSessionService.onDidChangeForegroundSession((session) => {
 			this._proxy.$notifyForegroundSessionChanged(session?.sessionId);
-		});
+		}));
 
-		this._runtimeSessionService.onDidReceiveRuntimeEvent(globalEvent => {
+		this._register(this._runtimeSessionService.onDidReceiveRuntimeEvent(globalEvent => {
 			// Ignore events for other sessions.
 			if (globalEvent.session_id !== this.sessionId) {
 				return;
@@ -250,7 +252,7 @@ class ExtHostLanguageRuntimeSessionAdapter implements ILanguageRuntimeSession {
 
 			// Propagate event
 			this._onDidReceiveRuntimeMessageClientEventEmitter.fire(ev);
-		});
+		}));
 	}
 
 	onDidChangeRuntimeState: Event<RuntimeState>;
@@ -988,7 +990,8 @@ class ExtHostLanguageRuntimeSessionAdapter implements ILanguageRuntimeSession {
 	}
 	static clientCounter = 0;
 
-	dispose(): void {
+	override dispose(): void {
+		super.dispose();
 		// Cleanup (dispose) the ExtHost side of the session.
 		this._proxy.$disposeLanguageRuntime(this.handle);
 	}
@@ -1352,7 +1355,7 @@ export class MainThreadLanguageRuntime
 
 	private readonly _sessions: Map<number, ExtHostLanguageRuntimeSessionAdapter> = new Map();
 
-	private readonly _registeredRuntimes: Map<number, ILanguageRuntimeMetadata> = new Map();
+	private readonly _registeredRuntimes: Map<string, ILanguageRuntimeMetadata> = new Map();
 
 	/**
 	 * Instance counter
@@ -1469,8 +1472,8 @@ export class MainThreadLanguageRuntime
 	}
 
 	// Called by the extension host to register a language runtime
-	$registerLanguageRuntime(handle: number, metadata: ILanguageRuntimeMetadata): void {
-		this._registeredRuntimes.set(handle, metadata);
+	$registerLanguageRuntime(metadata: ILanguageRuntimeMetadata): void {
+		this._registeredRuntimes.set(metadata.runtimeId, metadata);
 		this._languageRuntimeService.registerRuntime(metadata);
 	}
 
@@ -1503,6 +1506,11 @@ export class MainThreadLanguageRuntime
 		return this._runtimeSessionService.selectRuntime(
 			runtimeId,
 			'Extension-requested runtime selection via Positron API');
+	}
+
+	// Called by the extension host to get a list of all registered runtimes
+	$getRegisteredRuntimes(): Promise<ILanguageRuntimeMetadata[]> {
+		return Promise.resolve(Array.from(this._registeredRuntimes.values()));
 	}
 
 	// Called by the extension host to start a previously registered language runtime
@@ -1580,11 +1588,12 @@ export class MainThreadLanguageRuntime
 		this._runtimeStartupService.completeDiscovery(this._id);
 	}
 
-	$unregisterLanguageRuntime(handle: number): void {
-		const runtime = this._registeredRuntimes.get(handle);
+	// Called by the extension host to unregister a language runtime
+	$unregisterLanguageRuntime(runtimeId: string): void {
+		const runtime = this._registeredRuntimes.get(runtimeId);
 		if (runtime) {
-			this._languageRuntimeService.unregisterRuntime(runtime.runtimeId);
-			this._registeredRuntimes.delete(handle);
+			this._languageRuntimeService.unregisterRuntime(runtimeId);
+			this._registeredRuntimes.delete(runtimeId);
 		}
 	}
 

src/vs/workbench/api/common/positron/extHost.positron.protocol.ts
@@ -38,13 +38,14 @@ export interface RuntimeInitialState {
 
 // This is the interface that the main process exposes to the extension host
 export interface MainThreadLanguageRuntimeShape extends IDisposable {
-	$registerLanguageRuntime(handle: number, metadata: ILanguageRuntimeMetadata): void;
+	$registerLanguageRuntime(metadata: ILanguageRuntimeMetadata): void;
 	$selectLanguageRuntime(runtimeId: string): Promise<void>;
 	$startLanguageRuntime(runtimeId: string, sessionName: string, sessionMode: LanguageRuntimeSessionMode, notebookUri: URI | undefined): Promise<string>;
 	$completeLanguageRuntimeDiscovery(): void;
-	$unregisterLanguageRuntime(handle: number): void;
+	$unregisterLanguageRuntime(runtimeId: string): void;
 	$executeCode(languageId: string, extensionId: string, code: string, focus: boolean, allowIncomplete?: boolean, mode?: RuntimeCodeExecutionMode, errorBehavior?: RuntimeErrorBehavior, executionId?: string): Promise<string>;
 	$getPreferredRuntime(languageId: string): Promise<ILanguageRuntimeMetadata | undefined>;
+	$getRegisteredRuntimes(): Promise<ILanguageRuntimeMetadata[]>;
 	$getActiveSessions(): Promise<RuntimeSessionMetadata[]>;
 	$getForegroundSession(): Promise<string | undefined>;
 	$getNotebookSession(notebookUri: URI): Promise<string | undefined>;

src/vs/workbench/api/common/positron/extHostLanguageRuntime.ts
@@ -14,7 +14,7 @@ import { RuntimeClientState, RuntimeClientType } from './extHostTypes.positron.j
 import { ExtHostRuntimeClientInstance } from './extHostClientInstance.js';
 import { ExtensionIdentifier, IExtensionDescription } from '../../../../platform/extensions/common/extensions.js';
 import { URI } from '../../../../base/common/uri.js';
-import { DeferredPromise, retry } from '../../../../base/common/async.js';
+import { DeferredPromise } from '../../../../base/common/async.js';
 import { IRuntimeSessionMetadata } from '../../../services/runtimeSession/common/runtimeSessionService.js';
 import { ILogService } from '../../../../platform/log/common/log.js';
 import { SerializableObjectWithBuffers } from '../../../services/extensions/common/proxyIdentifier.js';
@@ -187,8 +187,6 @@ interface LanguageRuntimeManager {
 export class ExtHostLanguageRuntime implements extHostProtocol.ExtHostLanguageRuntimeShape {
 
 	private readonly _proxy: extHostProtocol.MainThreadLanguageRuntimeShape;
-
-	private readonly _registeredRuntimes = new Array<positron.LanguageRuntimeMetadata>();
 
 	private readonly _runtimeManagersByRuntimeId = new Map<string, LanguageRuntimeManager>();
 
@@ -967,27 +965,11 @@ export class ExtHostLanguageRuntime implements extHostProtocol.ExtHostLanguageRu
 	}
 
 	public getRegisteredRuntimes(): Promise<positron.LanguageRuntimeMetadata[]> {
-		return Promise.resolve(this._registeredRuntimes);
+		return this._proxy.$getRegisteredRuntimes();
 	}
 
 	public async getPreferredRuntime(languageId: string): Promise<positron.LanguageRuntimeMetadata | undefined> {
-		const metadata = await this._proxy.$getPreferredRuntime(languageId);
-		if (!metadata) {
-			return undefined;
-		}
-
-		// If discovery is in progress, a runtime may exist on the main thread but not
-		// the extension host, so retry a bunch of times. Retrying is more likely to return
-		// faster than waiting for the entire discovery phase to complete since runtimes are
-		// discovered concurrently across languages.
-		return retry(async () => {
-			const runtime = this._registeredRuntimes.find(runtime => runtime.runtimeId === metadata.runtimeId);
-			if (!runtime) {
-				this._logService.warn(`Could not find runtime ${metadata.runtimeId} on extension host. Waiting 2 seconds and retrying.`);
-				throw new Error(`Runtime exists on main thread but not extension host: ${metadata.runtimeId}`);
-			}
-			return runtime;
-		}, 2000, 5);
+		return this._proxy.$getPreferredRuntime(languageId);
 	}
 
 	public async getActiveSessions(): Promise<positron.LanguageRuntimeSession[]> {
@@ -1085,17 +1067,6 @@ export class ExtHostLanguageRuntime implements extHostProtocol.ExtHostLanguageRu
 			if (index >= 0) {
 				this._runtimeManagers.splice(index, 1);
 			}
-
-			// Remove its associated runtimes
-			this._runtimeManagersByRuntimeId.forEach((m, runtimeId) => {
-				if (m.manager === manager) {
-					this._runtimeManagersByRuntimeId.delete(runtimeId);
-					const index = this._registeredRuntimes.findIndex(runtime => runtime.runtimeId === runtimeId);
-					if (index >= 0) {
-						this._registeredRuntimes.splice(index, 1);
-					}
-				}
-			});
 		});
 	}
 
@@ -1104,25 +1075,19 @@ export class ExtHostLanguageRuntime implements extHostProtocol.ExtHostLanguageRu
 		manager: positron.LanguageRuntimeManager,
 		runtime: positron.LanguageRuntimeMetadata): IDisposable {
 
-		// Create a handle and register the runtime with the main thread
-		const handle = this._registeredRuntimes.length;
-
 		// Register the runtime with the main thread
-		this._proxy.$registerLanguageRuntime(handle, {
+		this._proxy.$registerLanguageRuntime({
 			extensionId: extension.identifier,
 			...runtime
 		});
 		this._onDidRegisterRuntimeEmitter.fire(runtime);
-
-		// Add this runtime to the set of registered runtimes
-		this._registeredRuntimes.push(runtime);
 
 		// Save the manager associated with this runtime, too; we'll need to use
 		// it to create a runtime session later
 		this._runtimeManagersByRuntimeId.set(runtime.runtimeId, { manager, languageId: runtime.languageId, extension });
 
 		return new Disposable(() => {
-			this._proxy.$unregisterLanguageRuntime(handle);
+			this._proxy.$unregisterLanguageRuntime(runtime.runtimeId);
 		});
 	}
 

src/vs/workbench/contrib/positronConsole/browser/components/consoleInput.tsx
@@ -47,6 +47,7 @@ import { IPositronConsoleInstance, PositronConsoleState } from '../../../../serv
 import { ContentHoverController } from '../../../../../editor/contrib/hover/browser/contentHoverController.js';
 import { IInputHistoryEntry } from '../../../../services/positronHistory/common/executionHistoryService.js';
 import { CodeAttributionSource, IConsoleCodeAttribution } from '../../../../services/positronConsole/common/positronConsoleCodeExecution.js';
+import { localize } from '../../../../../nls.js';
 
 // Position enumeration.
 const enum Position {
@@ -164,8 +165,26 @@ export const ConsoleInput = (props: ConsoleInputProps) => {
 		if (!session) {
 			return false;
 		}
-		// Check on whether the code is complete and can be executed.
-		switch (await session.isCodeFragmentComplete(code)) {
+
+		// Determine whether the code is complete, incomplete, invalid, or
+		// unknown. We handle errors here since callers don't handle them.
+		let status = RuntimeCodeFragmentStatus.Unknown;
+		try {
+			status = await session.isCodeFragmentComplete(code);
+		} catch (err) {
+			if (err instanceof Error) {
+				positronConsoleContext.notificationService.error(
+					localize('positronConsole.incompleteError', 'Cannot execute code: {0} ({1})', err.name, err.message)
+				);
+			} else {
+				positronConsoleContext.notificationService.error(
+					localize('positronConsole.incompleteUnknownError', 'Cannot execute code: {0}', JSON.stringify(err))
+				);
+			}
+			return false;
+		}
+
+		switch (status) {
 			// If the code fragment is complete, execute it.
 			case RuntimeCodeFragmentStatus.Complete:
 				break;

src/vs/workbench/services/positronConsole/browser/positronConsoleService.ts
@@ -2223,6 +2223,9 @@ class PositronConsoleInstance extends Disposable implements IPositronConsoleInst
 
 			if (exit.reason === RuntimeExitReason.ExtensionHost) {
 				this.setState(PositronConsoleState.Disconnected);
+				// The runtime must be detached, so that when it is reconnected, it is reattached
+				// to the new extension host.
+				this.detachRuntime();
 				return;
 			}
 

src/vs/workbench/services/positronDataExplorer/browser/positronDataExplorerService.ts
@@ -20,13 +20,15 @@ import { InstantiationType, registerSingleton } from '../../../../platform/insta
 import { PositronDataExplorerUri } from '../common/positronDataExplorerUri.js';
 import { DataExplorerClientInstance, DataExplorerUiEvent } from '../../languageRuntime/common/languageRuntimeDataExplorerClient.js';
 import { PositronDataExplorerInstance } from './positronDataExplorerInstance.js';
-import { ILanguageRuntimeSession, IRuntimeSessionService, RuntimeClientType } from '../../runtimeSession/common/runtimeSessionService.js';
+import { ILanguageRuntimeSession, IRuntimeClientInstance, IRuntimeSessionService, RuntimeClientType } from '../../runtimeSession/common/runtimeSessionService.js';
 import { IPositronDataExplorerService } from './interfaces/positronDataExplorerService.js';
 import { IPositronDataExplorerInstance } from './interfaces/positronDataExplorerInstance.js';
 import { PositronDataExplorerComm } from '../../languageRuntime/common/positronDataExplorerComm.js';
 import { PositronDataExplorerDuckDBBackend } from '../common/positronDataExplorerDuckDBBackend.js';
 import { ServicesAccessor } from '../../../../editor/browser/editorExtensions.js';
 import { URI } from '../../../../base/common/uri.js';
+import { RuntimeState } from '../../languageRuntime/common/languageRuntimeService.js';
+import { ILogService } from '../../../../platform/log/common/log.js';
 
 /**
  * DataExplorerRuntime class.
@@ -155,6 +157,7 @@ class PositronDataExplorerService extends Disposable implements IPositronDataExp
 	 * @param _hoverService The hover service.
 	 * @param _keybindingService The keybinding service.
 	 * @param _layoutService The layout service.
+	 * @pararm _logService The log service.
 	 * @param _notificationService The notification service.
 	 * @param _runtimeSessionService The language runtime session service.
 	 */
@@ -167,6 +170,7 @@ class PositronDataExplorerService extends Disposable implements IPositronDataExp
 		@IHoverService private readonly _hoverService: IHoverService,
 		@IKeybindingService private readonly _keybindingService: IKeybindingService,
 		@ILayoutService private readonly _layoutService: ILayoutService,
+		@ILogService private readonly _logService: ILogService,
 		@INotificationService private readonly _notificationService: INotificationService,
 		@IRuntimeSessionService private readonly _runtimeSessionService: IRuntimeSessionService
 	) {
@@ -180,12 +184,12 @@ class PositronDataExplorerService extends Disposable implements IPositronDataExp
 
 		// Add a data explorer runtime for each running runtime.
 		this._runtimeSessionService.activeSessions.forEach(async session => {
-			await this.addDataExplorerSession(session);
+			await this.attachSession(session);
 		});
 
 		// Register the onWillStartSession event handler.
 		this._register(this._runtimeSessionService.onWillStartSession(async e => {
-			await this.addDataExplorerSession(e.session);
+			await this.attachSession(e.session);
 		}));
 
 		// Register the onDidStartRuntime event handler.
@@ -335,6 +339,31 @@ class PositronDataExplorerService extends Disposable implements IPositronDataExp
 	//#region Private Methods
 
 	/**
+	 * Attach a session to the Positron data explorer service.
+	 *
+	 * If the session is being reattached, we wait for it to become idle before
+	 * adding it.
+	 *
+	 * @param session
+	 */
+	private async attachSession(session: ILanguageRuntimeSession) {
+		if (this._dataExplorerRuntimes.has(session.sessionId) &&
+			session.getRuntimeState() !== RuntimeState.Idle) {
+			const disposable = this._register(
+				session.onDidChangeRuntimeState(async newState => {
+					if (newState === RuntimeState.Idle) {
+						// If the runtime is idle, we can add the data explorer session.
+						disposable.dispose();
+						await this.addDataExplorerSession(session);
+					}
+				}));
+		} else {
+			// If the runtime is already idle, we can add the data explorer session.
+			await this.addDataExplorerSession(session);
+		}
+	}
+
+	/**
 	 * Adds a data explorer runtime.
 	 *
 	 * @param session The runtime session.
@@ -343,7 +372,12 @@ class PositronDataExplorerService extends Disposable implements IPositronDataExp
 		// If the runtime has already been added, check if we need to open a Data Explorer client.
 		if (this._dataExplorerRuntimes.has(session.sessionId)) {
 			// Get the Data Explorer clients for the session.
-			const sessionClients = await session.listClients(RuntimeClientType.DataExplorer);
+			const sessionClients: Array<IRuntimeClientInstance<any, any>> = [];
+			try {
+				sessionClients.push(...await session.listClients(RuntimeClientType.DataExplorer));
+			} catch (err) {
+				this._logService.error('Error listing Data Explorer clients:', err);
+			}
 
 			// For each client, check if we already have a Data Explorer client instance.
 			for (const client of sessionClients) {
@@ -355,7 +389,14 @@ class PositronDataExplorerService extends Disposable implements IPositronDataExp
 					this.openEditor(session.runtimeMetadata.languageName, dataExplorerClientInstance);
 				}
 			}
-			return;
+
+			// Dispose the old DataExplorerRuntime if it exists and create a new
+			// one below; even if the session IDs match, the session's event
+			// handlers may need to be reattached (e.g. if the extension host
+			// restarted)
+			const dataExplorerRuntime = this._dataExplorerRuntimes.get(session.sessionId);
+			dataExplorerRuntime?.dispose();
+			this._dataExplorerRuntimes.delete(session.sessionId);
 		}
 
 		// Create and add the data explorer runtime.

src/vs/workbench/services/runtimeSession/common/runtimeSession.ts
@@ -223,11 +223,28 @@ export class RuntimeSessionService extends Disposable implements IRuntimeSession
 						// Attempt to reconnect the session.
 						this._logService.debug(`Extension ${extensionId.value} has been reloaded; ` +
 							`attempting to reconnect session ${session.sessionId}`);
-						this.restoreRuntimeSession(session.runtimeMetadata, session.metadata, session.dynState.sessionName, false);
+						try {
+							this.restoreRuntimeSession(session.runtimeMetadata,
+								session.metadata,
+								session.dynState.sessionName,
+								false);
+							this._logService.debug(`Completed reconnection for session ${session.sessionId}`);
+						} catch (err) {
+							if (err instanceof Error) {
+								this._notificationService.error(
+									localize('positron.runtimeSession.restoreFailed', 'Failed to restore session {0} for extension {1}: {2}', session.sessionId, extensionId.value, err.message)
+								);
+							} else {
+								this._notificationService.error(
+									localize('positron.runtimeSession.restoreFailedUnkown', 'Unknown error restoring session {0} for extension {1}: {2}', session.metadata.sessionId, extensionId.value, JSON.stringify(err))
+								);
+							}
+						}
 					}
 				}
 			}
-		}));
+		}
+		));
 
 		// Changing the application storage scope causes disconnected sessions
 		// to become unusable, since the information needed to reconnect to them
@@ -467,7 +484,7 @@ export class RuntimeSessionService extends Disposable implements IRuntimeSession
 	focusSession(sessionId: string): void {
 		const session = this.getSession(sessionId);
 		if (!session) {
-			throw new Error(`Could not find session with id {sessionId}.`);
+			throw new Error(`Could not find session with id ${sessionId}.`);
 		}
 
 		if (session.metadata.sessionMode === LanguageRuntimeSessionMode.Console) {