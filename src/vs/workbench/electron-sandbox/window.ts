531e11485e36920aa06e7cc54cd5936d76236212
Julia Silge <julia.silge@gmail.com>
2025-06-20 15:38:35 -0600

    Use a block comment

--- src/vs/workbench/electron-sandbox/window.ts
+++ src/vs/workbench/electron-sandbox/window.ts
@@ -249,11 +249,11 @@ export class NativeWindow extends BaseWindow {
 					label: localize('downloadArmBuild', "Download"),
 					run: () => {
 						// --- Start Positron ---
-						// const quality = this.productService.quality;
-						// const stableURL = 'https://code.visualstudio.com/docs/?dv=osx';
-						// const insidersURL = 'https://code.visualstudio.com/docs/?dv=osx&build=insiders';
-						// this.openerService.open(quality === 'stable' ? stableURL : insidersURL);
-						this.openerService.open('https://positron.posit.co/download');
+/* 						const quality = this.productService.quality;
+						const stableURL = 'https://code.visualstudio.com/docs/?dv=osx';
+						const insidersURL = 'https://code.visualstudio.com/docs/?dv=osx&build=insiders';
+						this.openerService.open(quality === 'stable' ? stableURL : insidersURL);
+ */						this.openerService.open('https://positron.posit.co/download');
 						// --- End Positron ---
 					}
 				}],
