b40fdd2a079832c49f63b94c0f709744a006be6d
Rodrigo Silva Ferreira <rodrigo.ferreira@posit.co>
2025-06-25 11:27:05 -0400

    remove web and put all code in one line

--- test/e2e/tests/extensions/restart-host-ext.test.ts
+++ test/e2e/tests/extensions/restart-host-ext.test.ts
@@ -3,32 +3,32 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { test, tags, expect } from '../_test.setup';
+import { test, tags } from '../_test.setup';
 
 test.use({
 	suiteId: __filename
 });
 
 
-test.describe('Restart Host Extension', {
-	tag: [tags.EXTENSIONS, tags.WEB, tags.WIN],
-}, () => {
+test.describe('Restart Host Extension', { tag: [tags.EXTENSIONS, tags.WIN] }, () => {
+
+	test.afterEach(async ({ app }) => {
+		await app.workbench.sessions.deleteAll();
+	});
 
 	test('Verify Restart Extension Host command works - R', async function ({ app, r }) {
 		await app.workbench.quickaccess.runCommand('workbench.action.restartExtensionHost');
 		await app.workbench.console.waitForConsoleContents('Extensions restarting...');
-		await expect(app.workbench.console.activeConsole.locator('.current-line')).toBeVisible();
-		await app.workbench.console.pasteCodeToConsole('x=1; y=x+100', true);
-		await app.workbench.console.pasteCodeToConsole('y', true);
+		await app.workbench.console.waitForReady('>');
+		await app.workbench.console.pasteCodeToConsole('x=1; y=x+100; y', true);
 		await app.workbench.console.waitForConsoleContents('101');
 	});
 
 	test('Verify Restart Extension Host command works - Python', async function ({ app, python }) {
 		await app.workbench.quickaccess.runCommand('workbench.action.restartExtensionHost');
 		await app.workbench.console.waitForConsoleContents('Extensions restarting...');
-		await expect(app.workbench.console.activeConsole.locator('.current-line')).toBeVisible();
-		await app.workbench.console.pasteCodeToConsole('x=1; y=x+100', true);
-		await app.workbench.console.pasteCodeToConsole('print(y)', true);
+		await app.workbench.console.waitForReady('>>>');
+		await app.workbench.console.pasteCodeToConsole('x=1; y=x+100; print(y)', true);
 		await app.workbench.console.waitForConsoleContents('101');
 	});
 
