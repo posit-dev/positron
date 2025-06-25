80ace2f575b4e22a755aa95dd8f3bb9ae21a5921
Lionel Henry <lionel.hry@proton.me>
2025-06-25 11:04:52 +0200

    Add note about falsy `viewerId`

--- src/vs/workbench/contrib/positronVariables/browser/components/variableItem.tsx
+++ src/vs/workbench/contrib/positronVariables/browser/components/variableItem.tsx
@@ -137,7 +137,11 @@ export const VariableItem = (props: VariableItemProps) => {
 				));
 			}
 
-			// If a binding was returned, save the binding between the viewer and the variable item.
+			// If a binding was returned, save the binding between the viewer and the
+			// variable item. Note that we intentionally treat `""` as falsy here so
+			// that backends can return an empty ID if no comm was open. This happens
+			// with Ark when the user views a function object: a virtual document is
+			// opened in an editor but is not managed by a comm.
 			if (viewerId) {
 				explorerService.setInstanceForVar(viewerId, item.id);
 			}
