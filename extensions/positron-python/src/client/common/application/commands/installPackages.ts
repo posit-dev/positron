c49f15a135b22fe10fe7dfd6e4ff48d5b39e6b3a
Nick Strayer <nick.strayer@posit.co>
2025-06-25 12:22:33 -0400

    Add missing copyright header

--- extensions/positron-python/src/client/common/application/commands/installPackages.ts
+++ extensions/positron-python/src/client/common/application/commands/installPackages.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { injectable, inject } from 'inversify';
 import { IExtensionSingleActivationService } from '../../../activation/types';
 import { Commands } from '../../constants';
