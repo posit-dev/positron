06723e14b478de4afd79c88066001fc3cc89bfab
Wasim Lorgat <mwlorgat@gmail.com>
2025-07-18 20:36:01 +0200

    Python: Safely and efficiently compute variable display values (#8545)
    Addresses #8245. The approach is inspired by pydevd which backs the
    debugger used by most Python IDEs today.
    
    The focus is on safely (i.e. no hangs) and efficiently calculating and
    sending display values over the wire, _not_ on perfectly formatting
    those display values, hence the large max character values. The idea is
    that further formatting would be handled in the UI, although we can
    change this if needed.
    
    I've mostly left the display formatting of the larger third party
    objects as is (numpy, pandas, etc). The lower level changes here should
    make refinements to those easier in future.
    
    ### Release Notes
    
    #### New Features
    
    - N/A
    
    #### Bug Fixes
    
    - Improved the performance of Python variable updates (#8245).
    
    ### QA Notes
    
    See the repro steps in #8245 as well as the new unit tests.
    
    @:variables @:data-explorer @:console
    
    ---------
    
    Signed-off-by: Wasim Lorgat <mwlorgat@gmail.com>
    Co-authored-by: Isabel Zimmerman <54685329+isabelizimm@users.noreply.github.com>

--- extensions/positron-python/python_files/posit/positron/utils.py
+++ extensions/positron-python/python_files/posit/positron/utils.py
@@ -8,14 +8,9 @@ import concurrent.futures
 import functools
 import inspect
 import logging
-import numbers
-import pprint
 import sys
 import threading
-import types
 import uuid
-from binascii import b2a_base64
-from datetime import datetime
 from pathlib import Path
 from typing import (
     Any,
@@ -25,7 +20,6 @@ from typing import (
     List,
     Optional,
     Set,
-    Tuple,
     TypeVar,
     Union,
     cast,
@@ -122,103 +116,7 @@ def is_numpy_ufunc(object_: Any) -> bool:
     return object_type.__module__ == "numpy" and object_type.__name__ == "ufunc"
 
 
-def pretty_format(
-    value,
-    print_width: Optional[int] = None,
-    truncate_at: Optional[int] = None,
-) -> Tuple[str, bool]:
-    if print_width is not None:
-        s = pprint.pformat(value, width=print_width, compact=True)
-    else:
-        s = str(value)
-
-    # TODO: Add type aware truncation
-    if truncate_at is not None:
-        return truncate_string(s, truncate_at)
-
-    return s, False
-
-
-def truncate_string(value: str, max_: int) -> Tuple[str, bool]:
-    if len(value) > max_:
-        return (value[:max_], True)
-    else:
-        return (value, False)
-
-
 ISO8601 = "%Y-%m-%dT%H:%M:%S.%f"
-
-
-# We can't use ipykernel's json_clean function directly as it has since been
-# deactivated. JSON message cleaning in jupyter_client will also be removed in
-# the near future. We keep a copy below and adjust it for display-only use.
-#
-# The original function is available in the ipykernel module and was made
-# available under the following license:
-#
-# Copyright (c) IPython Development Team.
-# Distributed under the terms of the Modified BSD License.
-#
-def json_clean(obj):
-    # types that are 'atomic' and ok in json as-is.
-    atomic_ok = (str, type(None))
-
-    # containers that we need to convert into lists
-    container_to_list = (tuple, set, types.GeneratorType)
-
-    # Since bools are a subtype of Integrals, which are a subtype of Reals,
-    # we have to check them in that order.
-
-    if isinstance(obj, bool):
-        return obj
-
-    if isinstance(obj, numbers.Integral):
-        # cast int to int, in case subclasses override __str__ (e.g. boost enum, #4598)
-        return int(obj)
-
-    if isinstance(obj, numbers.Real):
-        # use string repr to avoid precision issues with JSON
-        return repr(obj)
-
-    if isinstance(obj, atomic_ok):
-        return obj
-
-    if isinstance(obj, bytes):
-        # unanmbiguous binary data is base64-encoded
-        # (this probably should have happened upstream)
-        return b2a_base64(obj, newline=False).decode("ascii")
-
-    if isinstance(obj, container_to_list) or (
-        hasattr(obj, "__iter__") and hasattr(obj, "__next__")
-    ):
-        obj = list(obj)
-
-    if isinstance(obj, list):
-        return [json_clean(x) for x in obj]
-
-    if isinstance(obj, dict):
-        # First, validate that the dict won't lose data in conversion due to
-        # key collisions after stringification.  This can happen with keys like
-        # True and 'true' or 1 and '1', which collide in JSON.
-        nkeys = len(obj)
-        nkeys_collapsed = len(set(map(str, obj)))
-        if nkeys != nkeys_collapsed:
-            msg = (
-                "dict cannot be safely converted to JSON: "
-                "key collision would lead to dropped values"
-            )
-            raise ValueError(msg)
-        # If all OK, proceed by making the new dict that will be json-safe
-        out = {}
-        for k, v in obj.items():
-            out[str(k)] = json_clean(v)
-        return out
-
-    if isinstance(obj, datetime):
-        return obj.strftime(ISO8601)
-
-    # we don't understand it, it's probably an unserializable object
-    raise ValueError(f"Can't clean for JSON: {obj!r}")
 
 
 def create_task(coro: Coroutine, pending_tasks: Set[asyncio.Task], **kwargs) -> asyncio.Task:
