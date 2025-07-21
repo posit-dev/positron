ba3800c9cd28245eefdce55edce63d34a955a46a
Dhruvi Sompura <dhruvi.sompura@posit.co>
2025-07-21 14:16:15 -0700

    Render stubs for search bar and sort dropdown

---
+++ src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/summaryRowFilterBar/summaryRowSortDropdown.tsx
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import React from 'react';
+
+import './summaryRowSortDropdown.css';
+
+export const SummaryRowSortDropdown = () => {
+	return (
+		<div className='summary-row-sort-dropdown-container'>
+			<span>Sort by original</span>
+		</div>
+	);
+}
