3d316e540679fba9e8d562115736890d62252d5d
sharon wang <sharon-wang@users.noreply.github.com>
2025-07-22 10:51:23 -0400

    require variables for the data summary tool

--- extensions/positron-assistant/src/participants.ts
+++ extensions/positron-assistant/src/participants.ts
@@ -280,9 +280,10 @@ abstract class PositronAssistantParticipant implements IPositronAssistantPartici
 						return inChatPane && (isEditMode || isAgentMode);
 					// Only include the getTableSummary tool for Python sessions until supported in R
 					case PositronAssistantToolName.GetTableSummary:
-						// TODO: Remove this restriction when the tool is supported in R https://github.com/posit-dev/positron/issues/8343
+						// TODO: Remove the python-specific restriction when the tool is supported in R https://github.com/posit-dev/positron/issues/8343
 						// The logic above with TOOL_TAG_REQUIRES_ACTIVE_SESSION will handle checking for active sessions once this is removed.
-						return activeSessions.has('python');
+						// We'll still want to check that variables are defined.
+						return activeSessions.has('python') && hasVariables;
 					// Only include the getPlot tool if there is a plot available.
 					case PositronAssistantToolName.GetPlot:
 						return positronContext.plots?.hasPlots === true;
