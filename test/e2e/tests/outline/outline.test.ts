9c8d131e5d1232b804c63961f4c0239eccfb6406
Lionel Henry <lionel.hry@proton.me>
2025-07-24 12:25:34 +0200

    Only extract symbol name from the outline

--- test/e2e/tests/outline/outline.test.ts
+++ test/e2e/tests/outline/outline.test.ts
@@ -142,11 +142,11 @@ test.describe('Outline', { tag: [tags.WEB, tags.WIN, tags.OUTLINE] }, () => {
 		test('Python - Verify Outline Contents', async function ({ app, python, openFile }) {
 			await openFile(join('workspaces', 'chinook-db-py', 'chinook-sqlite.py'));
 			await app.workbench.outline.expectOutlineToContain([
-				'data_file_pathdata_file_path = os.path.join(os.getcwd(), \'data-files\', \'chinook\', \'chinook.db\')',
-				'connconn = sqlite3.connect(data_file_path)',
-				'curcur = conn.cursor()',
-				'rowsrows = cur.fetchall()',
-				'dfdf = pd.DataFrame(rows)'
+				'data_file_path',
+				'conn',
+				'cur',
+				'rows',
+				'df'
 			]);
 		});
 
