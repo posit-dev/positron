0922e57df87cf4ac62cbf54e63b96df23264325d
Dhruvi Sompura <dhruvi.sompura@posit.co>
2025-07-24 13:54:59 -0700

    Use ActionBarFilter component for searching

--- src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/summaryRowFilterBar/summaryRowActionBar.tsx
+++ src/vs/workbench/browser/positronDataExplorer/components/dataExplorerPanel/components/summaryRowFilterBar/summaryRowActionBar.tsx
@@ -5,49 +5,66 @@
 
 import './summaryRowActionBar.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { SummaryRowSortDropdown } from './summaryRowSortDropdown.js';
-import { SummaryRowFilterInput } from './summaryRowFilterInput.js';
 import { TableSummaryDataGridInstance } from '../../../../../../services/positronDataExplorer/browser/tableSummaryDataGridInstance.js';
 import { PositronActionBar } from '../../../../../../../platform/positronActionBar/browser/positronActionBar.js';
 import { PositronActionBarContextProvider } from '../../../../../../../platform/positronActionBar/browser/positronActionBarContext.js';
+import { ActionBarRegion } from '../../../../../../../platform/positronActionBar/browser/components/actionBarRegion.js';
+import { ActionBarFilter, ActionBarFilterHandle } from '../../../../../../../platform/positronActionBar/browser/components/actionBarFilter.js';
+
+// This is the debounce time for the search input in milliseconds.
+// It allows the user to type without triggering a search on every keystroke.
+const SEARCH_DEBOUNCE_TIMEOUT = 500;
 
 export interface SummaryRowActionBarProps {
 	instance: TableSummaryDataGridInstance
 }
 
 export const SummaryRowActionBar = ({ instance }: SummaryRowActionBarProps) => {
-	const [summarySearchText, setSummarySearchText] = useState('');
+	const filterRef = useRef<ActionBarFilterHandle>(null);
+	// State to hold the search text typed by the user
+	const [searchText, setSearchText] = useState('');
+	// State to hold the debounced search text that we use to filter data.
+	const [debouncedSearchText, setDebouncedSearchText] = useState('');
 
 	/**
-	 * Handler for when the summary search text changes.
-	 * @param searchText The new search text.
+	 * Update the debounced search text after a delay.
+	 * This is to prevent excessive search requests while the user is typing.
 	 */
-	const handleSummarySearchTextChanged = async (searchText: string) => {
-		setSummarySearchText(searchText);
+	useEffect(() => {
+		const debounce = setTimeout(() => {
+			setDebouncedSearchText(searchText);
+		}, SEARCH_DEBOUNCE_TIMEOUT);
 
-		// Only update the search text if it has changed
-		if (searchText !== summarySearchText) {
-			// Apply the column name search filter
-			await instance.setSearchText(searchText);
-		}
-	};
+		return () => clearTimeout(debounce);
+	}, [searchText, instance]);
 
-	const a = (
-		<div className='summary-row-filter-bar'>
-			<SummaryRowSortDropdown />
-			<SummaryRowFilterInput
-				searchText={summarySearchText}
-				onSearchTextChanged={handleSummarySearchTextChanged}
-			/>
-		</div>
-	)
+	/**
+	 * Every time the debounced search text changes (every SEARCH_DEBOUNCE_TIMEOUT milliseconds),
+	 * we request the summary data grid instance to filter its data.
+	 */
+	useEffect(() => {
+		const search = async () => {
+			await instance.setSearchText(debouncedSearchText);
+		};
+		search();
+	}, [debouncedSearchText, instance]);
+
 
 	return (
 		<PositronActionBarContextProvider>
 			<PositronActionBar>
-
+				<ActionBarRegion location='left'>
+					<SummaryRowSortDropdown />
+				</ActionBarRegion>
+				<ActionBarRegion location='right'>
+					<ActionBarFilter
+						ref={filterRef}
+						width={150}
+						onFilterTextChanged={filterText => setSearchText(filterText)} />
+				</ActionBarRegion>
 			</PositronActionBar>
 		</PositronActionBarContextProvider>
 
