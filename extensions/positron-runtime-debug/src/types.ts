d9488d614966b61a7eea946a237afb6300a265b6
seem <mwlorgat@gmail.com>
2025-08-04 17:48:30 +0200

    cleaning up

---
+++ extensions/positron-runtime-debug/src/types.ts
@@ -0,0 +1,43 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { DebugProtocol } from '@vscode/debugprotocol';
+
+export interface DumpCellArguments {
+	code: string;
+}
+
+export interface DumpCellResponseBody {
+	sourcePath: string;
+}
+
+/**
+ * Represents the response body containing debugger information.
+ */
+export interface DebugInfoResponseBody {
+	/* Indicates whether the debugger is started. */
+	isStarted: boolean;
+	/* The hash method used for code cells. Default is 'Murmur2'. */
+	hashMethod: string;
+	/* The seed for hashing code cells. */
+	hashSeed: number;
+	/* Prefix for temporary file names. */
+	tmpFilePrefix: string;
+	/* Suffix for temporary file names. */
+	tmpFileSuffix: string;
+	/* Breakpoints currently registered in the debugger. */
+	breakpoints: {
+		/* Source file. */
+		source: string;
+		/* List of breakpoints for that source file. */
+		breakpoints: DebugProtocol.Breakpoint[];
+	}[];
+	/* Threads in which the debugger is currently in a stopped state. */
+	stoppedThreads: number[];
+	/* Whether the debugger supports rich rendering of variables. */
+	richRendering: boolean;
+	/* Exception names used to match leaves or nodes in a tree of exception. */
+	exceptionPaths: string[];
+}
--- extensions/positron-runtime-debug/src/util.ts
+++ extensions/positron-runtime-debug/src/util.ts
@@ -2,7 +2,9 @@
  *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
+
 import * as vscode from 'vscode';
+import { DebugProtocol } from '@vscode/debugprotocol';
 
 export class DisposableStore implements vscode.Disposable {
 
@@ -19,5 +21,25 @@ export class DisposableStore implements vscode.Disposable {
 		}
 
 		this._disposables.clear();
+	}
+}
+
+export function formatDebugMessage(message: DebugProtocol.ProtocolMessage): string {
+	switch (message.type) {
+		case 'request': {
+			const request = message as DebugProtocol.Request;
+			return `${request.command} #${request.seq}: ${JSON.stringify(request.arguments)}`;
+		}
+		case 'event': {
+			const event = message as DebugProtocol.Event;
+			return `${event.event}: ${JSON.stringify(event.body)}`;
+		}
+		case 'response': {
+			const response = message as DebugProtocol.Response;
+			return `${response.command} #${response.request_seq}: ${JSON.stringify(response.body)}`;
+		}
+		default: {
+			return `[${message.type}]: ${JSON.stringify(message)}`;
+		}
 	}
 }
