4e77f5caa724f29c9b24829c790c58a47cf73606
Lionel Henry <lionel.hry@proton.me>
2025-08-05 14:04:05 +0200

    Add `openTextDocument()` with disposable

--- extensions/positron-r/src/test/utils.ts
+++ extensions/positron-r/src/test/utils.ts
@@ -3,10 +3,36 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
 export function mock<T>(obj: Partial<T>): T {
 	return obj as T;
 }
 
 export function createUniqueId(): string {
 	return Math.floor(Math.random() * 0x100000000).toString(16);
+}
+
+/**
+ * Normalize a file path for robust comparison (realpath, normalize, lower-case).
+ */
+export function normalizePath(p: string): string {
+	// `realPathSync` takes care of expanding e.g. `runner~1` to `runneradmin`
+	const real = fs.realpathSync.native(path.normalize(p));
+
+	// On Windows, paths are not case sensitive and we might get mixups. On mac it
+	// depends but let's only lowercase if we find it's needed.
+	return process.platform === 'win32' ? real.toLowerCase() : real;
+}
+
+/**
+ * Normalize a vscode.Uri for robust comparison.
+ * For file URIs, uses `normalizePath()`; otherwise, uses `uri.toString()`.
+ */
+export function normalizeUri(uri: vscode.Uri): string {
+	return uri.scheme === 'file'
+		? normalizePath(uri.fsPath)
+		: uri.toString();
 }
