128000f6993dfffa41f4614dc6bb573865209547
Lionel Henry <lionel.hry@proton.me>
2025-08-06 15:37:20 -0400

    Rewrite session tests without mocking

---
+++ extensions/positron-r/src/test/lsp.unit.test.ts
@@ -0,0 +1,60 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import './mocha-setup'
+
+import * as assert from 'assert';
+import { withDisposables } from './utils-disposables';
+import { startR } from './utils-session';
+import { State } from 'vscode-languageclient/node';
+import { delay } from '../util';
+import { pollForSuccess } from './utils-assertions';
+
+suite('Session manager', () => {
+	test('should deactivate non-foreground session before activating foreground session', async () => {
+		await withDisposables(async (disposables) => {
+			const [_ses1, ses1Disposable, ses1Lsp] = await startR();
+			disposables.push(ses1Disposable);
+
+			assert.strictEqual(ses1Lsp.client!.state, State.Running);
+
+			// Array of [sessionNumber, from, to] states
+			let states: [number, State, State][] = [];
+
+			ses1Lsp.client!.onDidChangeState((event) => {
+				states.push([1, event.oldState, event.newState]);
+			});
+
+			const [_ses2, ses2Disposable, ses2Lsp] = await startR();
+
+			// Session 1 now offline
+			assert.deepStrictEqual(states.pop(), [1, State.Running, State.Stopped]);
+
+			// Session 2 now online
+			assert.strictEqual(ses2Lsp.client!.state, State.Running);
+
+			ses2Lsp.client!.onDidChangeState((event) => {
+				states.push([2, event.oldState, event.newState]);
+			});
+
+			// Delete session 2 to put session 1 back to foreground
+			await ses2Disposable.dispose();
+
+			// Session 2 now offline
+			assert.deepStrictEqual(states.pop(), [2, State.Running, State.Stopped]);
+
+			// The LSP of the first session eventually goes back online
+			pollForSuccess(() => {
+				assert.strictEqual(ses1Lsp.client!.state, State.Running);
+			})
+
+			// We would expect the following but currently we start the LSP client
+			// anew on each activation, so the event handler is no longer active.
+			// This test would allow us to robustly check for event ordering.
+
+			// assert.deepStrictEqual(states.pop(), [1, State.Stopped, State.Running]);
+		});
+	});
+});
