f7ce4c78222b4b91233cc7a6e678b6294e3683ed
seem <mwlorgat@gmail.com>
2025-08-06 10:27:06 +0200

    finalizing debug protocol transformer interface

--- extensions/positron-jupyter-debugger/src/runtimeDebugAdapter.ts
+++ extensions/positron-jupyter-debugger/src/runtimeDebugAdapter.ts
@@ -8,22 +8,61 @@ import { randomUUID } from 'crypto';
 import * as positron from 'positron';
 import * as vscode from 'vscode';
 import { DebugInfoResponseBody, DumpCellArguments, DumpCellResponseBody } from './jupyterDebugProtocol.js';
-import { DisposableStore } from './util.js';
-import { formatDebugMessage } from './debugProtocol.js';
+import { DisposableStore, formatDebugMessage } from './util.js';
 import { murmurhash2_32 } from './murmur.js';
+
+export interface CodeIdOptions {
+	/* Indicates whether the debugger is started. */
+	// isStarted: boolean;
+
+	/* The hash method used for code cells. Default is 'Murmur2'. */
+	hashMethod: string;
+
+	/* The seed for hashing code cells. */
+	hashSeed: number;
+
+	/* Prefix for temporary file names. */
+	tmpFilePrefix: string;
+
+	/* Suffix for temporary file names. */
+	tmpFileSuffix: string;
+
+	/* Breakpoints currently registered in the debugger. */
+	// breakpoints: {
+	// 	/* Source file. */
+	// 	source: string;
+
+	// 	/* List of breakpoints for that source file. */
+	// 	breakpoints: DebugProtocol.Breakpoint[];
+	// }[];
+
+	/* Threads in which the debugger is currently in a stopped state. */
+	// stoppedThreads: number[];
+
+	/* Whether the debugger supports rich rendering of variables. */
+	// richRendering: boolean;
+
+	/* Exception names used to match leaves or nodes in a tree of exception. */
+	// exceptionPaths: string[];
+}
 
 export class JupyterRuntimeDebugAdapter implements vscode.DebugAdapter, vscode.Disposable {
 	private readonly _disposables = new DisposableStore();
 	private readonly _onDidSendMessage = this._disposables.add(new vscode.EventEmitter<vscode.DebugProtocolMessage>());
 	private readonly _onDidCompleteConfiguration = this._disposables.add(new vscode.EventEmitter<void>());
+	private readonly _onDidUpdateCodeIdOptions = this._disposables.add(new vscode.EventEmitter<void>());
 	private readonly _pendingRequestIds = new Set<string>();
 	private sequence = 1;
+
+	public codeIdOptions?: CodeIdOptions;
 
 	/** Event emitted when a debug protocol message is sent to the client. */
 	public readonly onDidSendMessage = this._onDidSendMessage.event;
 
 	/** Event emitted when the debugger has completed its configuration. */
 	public readonly onDidCompleteConfiguration = this._onDidCompleteConfiguration.event;
+
+	public readonly onDidUpdateCodeIdOptions = this._onDidUpdateCodeIdOptions.event;
 
 	constructor(
 		public readonly log: vscode.LogOutputChannel,
@@ -42,6 +81,22 @@ export class JupyterRuntimeDebugAdapter implements vscode.DebugAdapter, vscode.D
 				// TODO: Do we also need to handle debug requests from the runtime?
 			}
 		}));
+
+		this.restoreState().catch((error) => {
+			this.log.error('[runtime] Error restoring debug state', error);
+		});
+	}
+
+	private async restoreState(): Promise<void> {
+		// TODO: Might need a way to synchronize with this method in subclasses.
+		const debugInfo = await this.debugInfo();
+		this.codeIdOptions = {
+			hashMethod: debugInfo.hashMethod,
+			hashSeed: debugInfo.hashSeed,
+			tmpFilePrefix: debugInfo.tmpFilePrefix,
+			tmpFileSuffix: debugInfo.tmpFileSuffix,
+		};
+		this._onDidUpdateCodeIdOptions.fire();
 	}
 
 	private onDidReceiveDebugEvent(debugEvent: positron.LanguageRuntimeDebugEvent): void {
@@ -77,26 +132,30 @@ export class JupyterRuntimeDebugAdapter implements vscode.DebugAdapter, vscode.D
 		return await this.debugSession.customRequest('dumpCell', args) as DumpCellResponseBody;
 	}
 
-	// TODO: Maybe this should be sent on startup?...
-	public async debugInfo(): Promise<DebugInfoResponseBody> {
+	private async debugInfo(): Promise<DebugInfoResponseBody> {
 		return await this.debugSession.customRequest('debugInfo') as DebugInfoResponseBody;
 	}
 
-	// TODO: Align naming: id, hashCode, tempFilePath, etc.
-	public hashCode(code: string, hashMethod: string, hashSeed: number): string {
-		switch (hashMethod) {
+	private hash(code: string): string {
+		if (!this.codeIdOptions) {
+			throw new Error('Cannot hash code before debug options are initialized');
+		}
+
+		switch (this.codeIdOptions.hashMethod) {
 			case 'murmur2':
-				return murmurhash2_32(code, hashSeed).toString();
+				return murmurhash2_32(code, this.codeIdOptions.hashSeed).toString();
 			default:
-				throw new Error(`Unsupported hash method: ${hashMethod}`);
+				throw new Error(`Unsupported hash method: ${this.codeIdOptions.hashMethod}`);
 		}
 	}
 
-	// TODO: Should this perhaps just take code, and we handle the debug info request internally?
-	//       We can track an internal promise and block this and hashCode requests until it resolves.
-	public getTempFilePath(code: string, debugInfo: DebugInfoResponseBody): string {
-		const id = this.hashCode(code, debugInfo.hashMethod, debugInfo.hashSeed);
-		return `${debugInfo.tmpFilePrefix}${id}${debugInfo.tmpFileSuffix}`;
+	public getCodeId(code: string): string {
+		if (!this.codeIdOptions) {
+			throw new Error('Cannot get code ID before debug options are initialized');
+		}
+
+		const hashed = this.hash(code);
+		return `${this.codeIdOptions.tmpFilePrefix}${hashed}${this.codeIdOptions.tmpFileSuffix}`;
 	}
 
 	private sendMessage<P extends DebugProtocol.ProtocolMessage>(message: Omit<P, 'seq'>): void {
