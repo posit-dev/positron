4604ea18d032d197fecdabd9bfe88face0ef89fa
Lionel Henry <lionel.hry@proton.me>
2025-08-06 13:27:00 +0200

    Add common suite setup

--- extensions/positron-r/src/test/hyperlink.test.ts
+++ extensions/positron-r/src/test/hyperlink.test.ts
@@ -3,6 +3,8 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import './mocha-setup'
+
 import * as assert from 'assert';
 import { matchRunnable } from '../hyperlink';
 
---
+++ extensions/positron-r/src/test/mocha-setup.ts
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { closeAllEditors } from './utils-vscode';
+
+// This will run once per import in each test files
+
+suiteSetup(async () => {
+	// Set Ark log level to TRACE for easier debugging of tests
+	await vscode.workspace.getConfiguration().update('positron.r.kernel.logLevel', 'trace', vscode.ConfigurationTarget.Global);
+
+	// To be safe
+	await closeAllEditors();
+});
