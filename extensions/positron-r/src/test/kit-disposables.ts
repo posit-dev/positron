cd287e3b23caf484b387c691cb2c2adb22da0334
Lionel Henry <lionel.hry@proton.me>
2025-08-07 03:04:23 -0400

    Reexport all utils from central file

--- extensions/positron-r/src/test/kit-disposables.ts
+++ extensions/positron-r/src/test/kit-disposables.ts
@@ -7,7 +7,7 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as os from 'os';
 import * as fs from 'fs';
-import { createUniqueId, normalizePath } from './utils';
+import { createUniqueId, normalizePath } from './kit';
 import { delay } from '../util';
 
 /**
