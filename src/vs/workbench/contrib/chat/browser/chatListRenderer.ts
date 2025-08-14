32637f7d6ec66a332fdfd9615322b16f694267a7
sharon wang <sharon-wang@users.noreply.github.com>
2025-08-14 17:28:07 -0600

    use `element.isComplete` instead of `element.model.isInProgress.get()`

--- src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
+++ src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
@@ -977,7 +977,7 @@ export class ChatListItemRenderer extends Disposable implements ITreeRenderer<Ch
 			// Show working progress if the last part is markdown content and the response is in progress
 			// This is especially helpful when running in Agent mode, when the executeCode code block is still being constructed
 			// This causes the in progress indicator to show in any mode, while markdown text is being rendered and the response is not complete
-			(lastPart.kind === 'markdownContent' && element.model.isInProgress.get()) ||
+			(lastPart.kind === 'markdownContent' && element.isComplete) ||
 			// --- End Positron ---
 			((lastPart.kind === 'textEditGroup' || lastPart.kind === 'notebookEditGroup') && lastPart.done && !partsToRender.some(part => part.kind === 'toolInvocation' && !part.isComplete)) ||
 			(lastPart.kind === 'progressTask' && lastPart.deferred.isSettled) ||
