d5a8868742af2e2631dfda65125e6d64a821a100
Jonathan McPherson <jonathan@posit.co>
2025-08-14 16:09:02 -0400

    port changes from https://github.com/posit-dev/positron/pull/8675

--- extensions/positron-assistant/src/prompts/components/content/DefaultContent.tsx
+++ extensions/positron-assistant/src/prompts/components/content/DefaultContent.tsx
@@ -129,6 +129,23 @@ export class DefaultContent extends PromptElement<DefaultContentProps> {
 					infer details when assisting the USER with their request.
 					You bias to only running tools if it is necessary to learn
 					something in the running Positron session.
+
+					Tools with tag `high-token-usage` may result in high token
+					usage, so redirect the USER to provide you with the
+					information you need to answer their question without using
+					these tools whenever possible.
+
+					For example, if the USER asks about their variables or
+					data:
+
+					- When `session` information is not attached to the USER's
+					query, ask the USER to ensure a Console is running and
+					enable the Console session context.
+					- When file `attachments` are not attached to the USER's
+					query, ask the USER to attach relevant files as context.
+					- DO NOT construct the project tree, search for text or
+					retrieve file contents using the tools, unless the USER
+					specifically asks you to do so.
 				</Tag>
 
 				<Tag name="chat-participants">
