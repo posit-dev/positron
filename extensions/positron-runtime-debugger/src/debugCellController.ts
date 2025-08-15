a701b637506b88b7369298ef4bdd9575820677ab
seem <mwlorgat@gmail.com>
2025-08-15 18:48:49 +0200

    rename cell controller; remove refresh state and depend directly on path encoder

--- extensions/positron-runtime-debugger/src/debugCellController.ts
+++ extensions/positron-runtime-debugger/src/debugCellController.ts
@@ -10,7 +10,7 @@ import { Disposable } from './util.js';
 /**
  * Controls the execution and lifecycle of a notebook cell during debugging.
  */
-export class CellDebugController extends Disposable {
+export class DebugCellController extends Disposable {
 	/* Tracks the runtime execution ID of the current cell. */
 	private _executionId?: string;
 
@@ -39,9 +39,7 @@ export class CellDebugController extends Disposable {
 
 		// Track the runtime execution ID when the cell is first executed.
 		const executeDisposable = this._register(positron.runtime.onDidExecuteCode((event) => {
-			// TODO: restrict to cell and session ID as well?
 			if (event.attribution.source === positron.CodeAttributionSource.Notebook &&
-				// TODO: what does this look like for untitled/unsaved files?
 				event.attribution.metadata?.notebook === this._cell.notebook.uri.fsPath) {
 				executeDisposable.dispose();
 				this._executionId = event.executionId;
@@ -50,14 +48,12 @@ export class CellDebugController extends Disposable {
 
 		// End the debug session when the cell execution is complete.
 		const messageDisposable = this._register(this._runtimeSession.onDidReceiveRuntimeMessage(async (message) => {
-			// TODO: Throw or wait if execution ID is not set?
 			if (this._executionId &&
 				message.parent_id === this._executionId &&
 				message.type === positron.LanguageRuntimeMessageType.State &&
 				(message as positron.LanguageRuntimeState).state === positron.RuntimeOnlineState.Idle) {
 				messageDisposable.dispose();
 				await vscode.debug.stopDebugging(this._debugSession);
-				// TODO: this.dispose()? Or ensure its disposed elsewhere?
 			}
 		}));
 	}
--- extensions/positron-runtime-debugger/src/notebookDebugAdapterFactory.ts
+++ extensions/positron-runtime-debugger/src/notebookDebugAdapterFactory.ts
@@ -6,8 +6,8 @@
 import * as positron from 'positron';
 import * as vscode from 'vscode';
 import { log } from './extension.js';
-import { CellDebugController } from './cellDebugController.js';
-import { createDebuggerOutputChannel, Disposable } from './util.js';
+import { DebugCellController } from './debugCellController.js';
+import { ContextKey, createDebuggerOutputChannel, Disposable } from './util.js';
 import { RuntimeDebugAdapter } from './runtimeDebugAdapter.js';
 import { PathEncoder } from './pathEncoder.js';
 import { NotebookLocationMapper } from './notebookLocationMapper.js';
@@ -21,6 +21,8 @@ export class NotebookDebugAdapterFactory extends Disposable implements vscode.De
 
 	/* Maps runtime session IDs to their debug output channels. */
 	private readonly _outputChannelByRuntimeSessionId = new Map<string, vscode.LogOutputChannel>();
+
+	private readonly _notebookDebugDocuments = new ContextKey<vscode.Uri[]>('notebookDebugResources');
 
 	constructor() {
 		super();
@@ -51,22 +53,11 @@ export class NotebookDebugAdapterFactory extends Disposable implements vscode.De
 		const pathEncoder = new PathEncoder();
 		const locationMapper = this._register(new NotebookLocationMapper(pathEncoder, notebook));
 		const adapter = this._register(new RuntimeDebugAdapter({
-			locationMapper, outputChannel, debugSession, runtimeSession
+			locationMapper, outputChannel, pathEncoder, debugSession, runtimeSession
 		}));
 
-		// TODO: Where should this disposable live? In the adapter?
-		// TODO: Do we need a refresh state event or can we just call debugInfo here?
-		this._register(adapter.onDidRefreshState((debugInfo) => {
-			pathEncoder.setOptions({
-				hashMethod: debugInfo.hashMethod,
-				hashSeed: debugInfo.hashSeed,
-				tmpFilePrefix: debugInfo.tmpFilePrefix,
-				tmpFileSuffix: debugInfo.tmpFileSuffix,
-			});
-		}));
-
-		// Create a debug cell manager to handle the cell execution and debugging.
-		const debugCellManager = this._register(new CellDebugController(adapter, debugSession, runtimeSession, cell));
+		// Create a debug cell controller to handle the cell execution and debugging.
+		const debugCellController = this._register(new DebugCellController(adapter, debugSession, runtimeSession, cell));
 
 		// TODO: Move below to JupyterRuntimeDebugAdapter?
 		// TODO: stop debugging when:
@@ -88,7 +79,7 @@ export class NotebookDebugAdapterFactory extends Disposable implements vscode.De
 			vscode.debug.onDidTerminateDebugSession((session) => {
 				if (session.id === debugSession.id) {
 					stateDisposable.dispose();
-					debugCellManager.dispose();
+					debugCellController.dispose();
 					adapter.dispose();
 				}
 			})
