158f3f019d40ca06b3de9e6b206848e2d38d1e3f
Nick Strayer <nick.strayer@posit.co>
2025-08-21 14:38:26 -0400

    Add cell execution info available on hover

--- src/vs/workbench/contrib/positronNotebook/browser/PositronNotebookCells/PositronNotebookCodeCell.ts
+++ src/vs/workbench/contrib/positronNotebook/browser/PositronNotebookCells/PositronNotebookCodeCell.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2024-2025 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -14,20 +14,34 @@ import { IPositronNotebookCodeCell, NotebookCellOutputs } from '../../../../serv
 import { IPositronWebviewPreloadService } from '../../../../services/positronWebviewPreloads/browser/positronWebviewPreloadService.js';
 import { pickPreferredOutputItem } from './notebookOutputUtils.js';
 import { getWebviewMessageType } from '../../../../services/positronIPyWidgets/common/webviewPreloadUtils.js';
+import { INotebookExecutionStateService, NotebookExecutionType } from '../../../notebook/common/notebookExecutionStateService.js';
 
 export class PositronNotebookCodeCell extends PositronNotebookCellGeneral implements IPositronNotebookCodeCell {
 	override kind: CellKind.Code = CellKind.Code;
 	outputs: ISettableObservable<NotebookCellOutputs[]>;
+
+	// Execution timing observables
+	lastExecutionDuration: ISettableObservable<number | undefined>;
+	lastExecutionOrder: ISettableObservable<number | undefined>;
+	lastRunSuccess: ISettableObservable<boolean | undefined>;
+	lastRunEndTime: ISettableObservable<number | undefined>;
 
 	constructor(
 		cellModel: NotebookCellTextModel,
 		private instance: PositronNotebookInstance,
 		@ITextModelService _textModelResolverService: ITextModelService,
 		@IPositronWebviewPreloadService private _webviewPreloadService: IPositronWebviewPreloadService,
+		@INotebookExecutionStateService private _executionStateService: INotebookExecutionStateService
 	) {
 		super(cellModel, instance, _textModelResolverService);
 
 		this.outputs = observableValue<NotebookCellOutputs[], void>('cellOutputs', this.parseCellOutputs());
+
+		// Initialize execution timing observables
+		this.lastExecutionDuration = observableValue<number | undefined, void>('positronNotebookCodeCell.lastExecutionDuration', this.calculateExecutionDuration());
+		this.lastExecutionOrder = observableValue<number | undefined, void>('positronNotebookCodeCell.lastExecutionOrder', cellModel.internalMetadata.executionOrder);
+		this.lastRunSuccess = observableValue<boolean | undefined, void>('positronNotebookCodeCell.lastRunSuccess', cellModel.internalMetadata.lastRunSuccess ?? undefined);
+		this.lastRunEndTime = observableValue<number | undefined, void>('positronNotebookCodeCell.lastRunEndTime', cellModel.internalMetadata.runEndTime ?? undefined);
 
 		// Listen for changes to the cell outputs and update the observable
 		this._register(
@@ -35,6 +49,38 @@ export class PositronNotebookCodeCell extends PositronNotebookCellGeneral implem
 				this.outputs.set(this.parseCellOutputs(), undefined);
 			})
 		);
+
+		// Listen for changes to the internal metadata to update execution timing
+		this._register(
+			this.cellModel.onDidChangeInternalMetadata(() => {
+				this.updateExecutionInfo();
+			})
+		);
+
+		// Listen for execution state changes
+		this._register(
+			this._executionStateService.onDidChangeExecution(e => {
+				if (e.type === NotebookExecutionType.cell && e.affectsCell(this.cellModel.uri)) {
+					this.updateExecutionInfo();
+				}
+			})
+		);
+	}
+
+	private calculateExecutionDuration(): number | undefined {
+		const { runStartTime, runEndTime } = this.cellModel.internalMetadata;
+		if (typeof runStartTime === 'number' && typeof runEndTime === 'number') {
+			return Math.max(0, runEndTime - runStartTime);
+		}
+		return undefined;
+	}
+
+	private updateExecutionInfo(): void {
+		const metadata = this.cellModel.internalMetadata;
+		this.lastExecutionDuration.set(this.calculateExecutionDuration(), undefined);
+		this.lastExecutionOrder.set(metadata.executionOrder ?? undefined, undefined);
+		this.lastRunSuccess.set(metadata.lastRunSuccess ?? undefined, undefined);
+		this.lastRunEndTime.set(metadata.runEndTime ?? undefined, undefined);
 	}
 
 	/**
