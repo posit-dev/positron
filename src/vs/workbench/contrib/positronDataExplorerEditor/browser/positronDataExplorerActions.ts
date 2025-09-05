c8c27b244c06eb093af7c3f38baee0ffaa9d3577
isabel zimmerman <isabel.zimmerman@posit.co>
2025-09-05 17:25:51 -0400

    use shift+c+v as kb shortcut

--- src/vs/workbench/contrib/positronDataExplorerEditor/browser/positronDataExplorerActions.ts
+++ src/vs/workbench/contrib/positronDataExplorerEditor/browser/positronDataExplorerActions.ts
@@ -817,7 +817,7 @@ class PositronDataExplorerConvertToCodeModalAction extends Action2 {
 						POSITRON_DATA_EXPLORER_IS_ROW_FILTERING)
 				),
 				weight: KeybindingWeight.EditorContrib,
-				primary: KeyMod.Alt | KeyMod.Shift | KeyCode.KeyQ,
+				primary: KeyMod.Shift | KeyCode.KeyC | KeyCode.KeyV,
 			},
 			icon: Codicon.code,
 			menu: [
