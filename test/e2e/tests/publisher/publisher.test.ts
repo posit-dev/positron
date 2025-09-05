3fa4798f58d32cd641dc6d1dac4a54dfe042b1fc
Jon Vanausdeln <jon.vanausdeln@posit.co>
2025-09-04 20:06:09 -0700

    Quick Input to keep open true

--- test/e2e/tests/publisher/publisher.test.ts
+++ test/e2e/tests/publisher/publisher.test.ts
@@ -75,7 +75,7 @@ test.describe('Publisher - Positron', { tag: [tags.WEB, tags.WIN, tags.PUBLISHER
 		});
 
 		await test.step('Select Posit Connect as deployment target', async () => {
-			await app.workbench.quickInput.selectQuickInputElement(1, false);
+			await app.workbench.quickInput.selectQuickInputElement(1, true);
 			await app.workbench.quickInput.type(`${process.env.E2E_CONNECT_SERVER}`);
 		});
 
