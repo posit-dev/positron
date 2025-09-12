3384b16092cd1af26165f72a986dc27f7d17b900
seem <mwlorgat@gmail.com>
2025-09-12 14:59:39 +0200

    remove `map` - its unused, and we can use `observable.map` instead

--- src/vs/workbench/contrib/positronNotebook/browser/useObservedValue.tsx
+++ src/vs/workbench/contrib/positronNotebook/browser/useObservedValue.tsx
@@ -12,24 +12,20 @@ import { autorun, IObservable } from '../../../../base/common/observable.js';
 /**
  * Automatically updates the component when the observable changes.
  * @param observable Observable value with value to be extracted
- * @param map Optional mapping function to transform the observable value into a different value.
  * @returns The current value of the observable.
  */
-export function useObservedValue<T>(observable: IObservable<T>): T;
-export function useObservedValue<T, M extends (x: T) => unknown>(observable: IObservable<T>, map: M): M extends (x: T) => infer Out ? Out : never;
-export function useObservedValue<T, M>(observable: IObservable<T>, map?: (x: T) => unknown): T | undefined | M extends (x: T) => infer Out ? Out : never {
-
-	const [value, setValue] = React.useState(() => typeof map === 'function' ? map(observable.get()) : observable.get());
+export function useObservedValue<T>(observable: IObservable<T>): T {
+	const [value, setValue] = React.useState(observable.get());
 
 	React.useEffect(() => {
 		const disposable = autorun(reader => {
 			const val = observable.read(reader);
-			setValue(typeof map === 'function' ? map(val) : val);
+			setValue(val);
 		});
 		return () => {
 			disposable.dispose();
 		};
-	}, [map, observable]);
+	}, [observable]);
 
-	return value as T | M extends (x: T) => infer Out ? Out : never;
+	return value;
 }
