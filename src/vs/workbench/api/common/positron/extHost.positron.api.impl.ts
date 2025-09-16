869659faf6ba8fec496b749f6c043a45b187cd10
Jonathan <jonathan@rstudio.com>
2025-09-16 10:20:21 -0700

    Add support for `rstudioapi::showPrompt()` (#9424)
    This change adds support for `rstudioapi::showPrompt()` to Positron. 
    
    Addresses https://github.com/posit-dev/positron/issues/9099.
    
    Requires https://github.com/posit-dev/ark/pull/920 (see notes in that
    PR).
    
    
    ### Release Notes
    
    <!--
    Optionally, replace `N/A` with text to be included in the next release
    notes.
    The `N/A` bullets are ignored. If you refer to one or more Positron
    issues,
    these issues are used to collect information about the feature or
    bugfix, such
    as the relevant language pack as determined by Github labels of type
    `lang: `.
      The note will automatically be tagged with the language.
    
    These notes are typically filled by the Positron team. If you are an
    external
      contributor, you may ignore this section.
    -->
    
    #### New Features
    
    - Add support for `rstudioapi::showPrompt` (#9099)
    
    #### Bug Fixes
    
    - N/A
    
    
    ### QA Notes
    
    <!--
    Positron team members: please add relevant e2e test tags, so the tests
    can be
      run when you open this pull request.
    
    - Instructions:
    https://github.com/posit-dev/positron/blob/main/test/e2e/README.md#pull-requests-and-test-tags
    - Available tags:
    https://github.com/posit-dev/positron/blob/main/test/e2e/infra/test-runner/test-tags.ts
    -->
    
    
    <!--
      Add additional information for QA on how to validate the change,
      paying special attention to the level of risk, adjacent areas that
      could be affected by the change, and any important contextual
      information not present in the linked issues.
    -->

--- src/vs/workbench/api/common/positron/extHost.positron.api.impl.ts
+++ src/vs/workbench/api/common/positron/extHost.positron.api.impl.ts
@@ -192,6 +192,9 @@ export function createPositronApiFactoryAndRegisterActors(accessor: ServicesAcce
 			showSimpleModalDialogMessage(title: string, message: string, okButtonTitle?: string): Thenable<null> {
 				return extHostModalDialogs.showSimpleModalDialogMessage(title, message, okButtonTitle);
 			},
+			showSimpleModalDialogInputPrompt(title: string, message: string, placeholder?: string): Thenable<string | undefined> {
+				return extHostModalDialogs.showSimpleModalDialogInput(title, message, undefined, placeholder).then(result => result === null ? undefined : result);
+			},
 			getConsoleForLanguage(languageId: string) {
 				return extHostConsoleService.getConsoleForLanguage(languageId);
 			},
