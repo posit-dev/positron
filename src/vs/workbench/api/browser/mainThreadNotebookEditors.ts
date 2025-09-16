dc95c0e0ffea16a7d9174ad30013be1174d3cead
seem <mwlorgat@gmail.com>
2025-09-16 21:55:35 +0200

    part of mocking notebook editor api

--- src/vs/workbench/api/browser/mainThreadNotebookEditors.ts
+++ src/vs/workbench/api/browser/mainThreadNotebookEditors.ts
@@ -115,6 +115,15 @@ export class MainThreadNotebookEditors implements MainThreadNotebookEditorsShape
 		};
 
 		const editorPane = await this._editorService.openEditor({ resource: URI.revive(resource), options: editorOptions }, columnToEditorGroup(this._editorGroupService, this._configurationService, options.position));
+		// --- Start Positron ---
+		if (editorPane?.getId() === POSITRON_NOTEBOOK_EDITOR_ID) {
+			// Positron notebook is already open, just return a synthetic ID
+			// We can't return the actual notebook editor ID because Positron notebooks
+			// don't implement INotebookEditor interface yet
+			const uri = URI.revive(resource);
+			return `positron-notebook-${uri.toString()}`;
+		}
+		// --- End Positron ---
 		const notebookEditor = getNotebookEditorFromEditorPane(editorPane);
 
 		if (notebookEditor) {
