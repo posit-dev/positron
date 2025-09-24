bb6265a4ef2e9ef3c4735dbfe5c7be76716029df
Sam Clark <sam.clark@posit.co>
2025-09-24 11:01:45 -0500

    Updates default max input tokens to 100,000

--- extensions/positron-assistant/src/constants.ts
+++ extensions/positron-assistant/src/constants.ts
@@ -16,7 +16,7 @@ export const MARKDOWN_DIR = path.join(EXTENSION_ROOT_DIR, 'src', 'md');
 export const ALL_DOCUMENTS_SELECTOR: DocumentSelector = [{ scheme: '*' }];
 
 /** The default max token input if a model's maximum is unknown */
-export const DEFAULT_MAX_TOKEN_INPUT = 0; // TODO: This is 0 right now, but should it be something else? Copilot tools won't work.
+export const DEFAULT_MAX_TOKEN_INPUT = 100_000;
 
 /** The default max token output if a model's maximum is unknown */
 export const DEFAULT_MAX_TOKEN_OUTPUT = 4_096;
