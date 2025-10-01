82c444be0bd26dd93c793c437e21077cdd484941
Jon Vanausdeln <jon.vanausdeln@posit.co>
2025-10-01 11:54:56 -0700

    Added copywright header to file.

--- test/assistant-inspect-ai/inspect_result_parser.py
+++ test/assistant-inspect-ai/inspect_result_parser.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+# ---------------------------------------------------------------------------------------------
+# Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+# Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+# ---------------------------------------------------------------------------------------------
 """
 Inspect AI Result Parser
 
