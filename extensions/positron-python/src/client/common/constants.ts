e573d7e85ae87f4f26fae31f459bf9c7a066f9f3
Julia Silge <julia.silge@gmail.com>
2025-10-08 16:21:53 -0600

    More removing of Shiny support

--- extensions/positron-python/src/client/common/constants.ts
+++ extensions/positron-python/src/client/common/constants.ts
@@ -57,7 +57,6 @@ export namespace Commands {
     export const Exec_FastAPI_In_Terminal = 'python.execFastAPIInTerminal';
     export const Exec_Flask_In_Terminal = 'python.execFlaskInTerminal';
     export const Exec_Gradio_In_Terminal = 'python.execGradioInTerminal';
-    export const Exec_Shiny_In_Terminal = 'python.execShinyInTerminal';
     export const Exec_Streamlit_In_Terminal = 'python.execStreamlitInTerminal';
     export const Exec_In_Console = 'python.execInConsole';
     export const Exec_Selection_In_Console = 'python.execSelectionInConsole';
@@ -65,7 +64,6 @@ export namespace Commands {
     export const Debug_FastAPI_In_Terminal = 'python.debugFastAPIInTerminal';
     export const Debug_Flask_In_Terminal = 'python.debugFlaskInTerminal';
     export const Debug_Gradio_In_Terminal = 'python.debugGradioInTerminal';
-    export const Debug_Shiny_In_Terminal = 'python.debugShinyInTerminal';
     export const Debug_Streamlit_In_Terminal = 'python.debugStreamlitInTerminal';
     // --- End Positron ---
     export const Exec_In_REPL = 'python.execInREPL';
