b6e6b70a3885c7b01a857fc720214e3c34f92fb4
Julia Silge <julia.silge@gmail.com>
2025-10-10 15:31:46 -0600

    Add support for Databricks-style code cells in both Python and R

--- extensions/positron-code-cells/src/parser.ts
+++ extensions/positron-code-cells/src/parser.ts
@@ -63,10 +63,11 @@ const pythonIsCellStartRegExp = new RegExp(/^#\s*%%/);
 const pythonMarkdownRegExp = new RegExp(/^#\s*%%[^[]*\[markdown\]/);
 const rIsCellStartRegExp = new RegExp(/^#\s*(%%|\+)/);
 const rIsSectionHeaderRegExp = new RegExp(/^#+.*[-=]{4,}\s*$/);
+const databricksIsCellStartRegExp = new RegExp(/^# COMMAND ----------$/);
 
 // TODO: Expose an API to let extensions register parsers
 const pythonCellParser: CellParser = {
-	isCellStart: (line) => pythonIsCellStartRegExp.test(line),
+	isCellStart: (line) => pythonIsCellStartRegExp.test(line) || databricksIsCellStartRegExp.test(line),
 	isCellEnd: (_line) => false,
 	getCellType: (line) => pythonMarkdownRegExp.test(line) ? CellType.Markdown : CellType.Code,
 	getCellText: (cell, document) =>
@@ -77,8 +78,8 @@ const pythonCellParser: CellParser = {
 };
 
 const rCellParser: CellParser = {
-	isCellStart: (line) => rIsCellStartRegExp.test(line),
-	isCellEnd: (_line) => rIsSectionHeaderRegExp.test(_line),
+	isCellStart: (line) => rIsCellStartRegExp.test(line) || databricksIsCellStartRegExp.test(line),
+	isCellEnd: (_line) => !databricksIsCellStartRegExp.test(_line) && rIsSectionHeaderRegExp.test(_line),
 	getCellType: (_line) => CellType.Code,
 	getCellText: getCellText,
 	newCell: () => '\n# %%\n'
