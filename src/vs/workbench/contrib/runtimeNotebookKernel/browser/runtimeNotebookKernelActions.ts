408f4b520aaabf27a3e7b8118226721b91d3ba04
seem <mwlorgat@gmail.com>
2025-10-20 16:27:32 +0200

    fix runtime kernel actions; add restart keybinding

--- src/vs/workbench/contrib/runtimeNotebookKernel/browser/runtimeNotebookKernelActions.ts
+++ src/vs/workbench/contrib/runtimeNotebookKernel/browser/runtimeNotebookKernelActions.ts
@@ -4,11 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Codicon } from '../../../../base/common/codicons.js';
+import { KeyChord, KeyCode } from '../../../../base/common/keyCodes.js';
 import { isUriComponents, URI } from '../../../../base/common/uri.js';
 import { localize, localize2 } from '../../../../nls.js';
 import { Action2, MenuId, registerAction2 } from '../../../../platform/actions/common/actions.js';
 import { ContextKeyExpr } from '../../../../platform/contextkey/common/contextkey.js';
 import { ServicesAccessor } from '../../../../platform/instantiation/common/instantiation.js';
+import { KeybindingWeight } from '../../../../platform/keybinding/common/keybindingsRegistry.js';
 import { INotificationService } from '../../../../platform/notification/common/notification.js';
 import { IProgressService, ProgressLocation } from '../../../../platform/progress/common/progress.js';
 import { IEditorService } from '../../../services/editor/common/editorService.js';
@@ -17,6 +19,8 @@ import { INotebookLanguageRuntimeSession, IRuntimeSessionService } from '../../.
 import { NotebookEditorWidget } from '../../notebook/browser/notebookEditorWidget.js';
 import { NOTEBOOK_KERNEL } from '../../notebook/common/notebookContextKeys.js';
 import { isNotebookEditorInput } from '../../notebook/common/notebookEditorInput.js';
+import { POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED } from '../../positronNotebook/browser/ContextKeysManager.js';
+import { POSITRON_NOTEBOOK_EDITOR_INPUT_ID } from '../../positronNotebook/common/positronNotebookCommon.js';
 import { ActiveNotebookHasRunningRuntime } from '../common/activeRuntimeNotebookContextManager.js';
 import { POSITRON_RUNTIME_NOTEBOOK_KERNELS_EXTENSION_ID } from '../common/runtimeNotebookKernelConfig.js';
 
@@ -74,22 +78,15 @@ abstract class BaseRuntimeNotebookKernelAction extends Action2 {
 				debugMessage: `Restart notebook kernel command ${RuntimeNotebookKernelRestartAction.ID} executed`,
 			};
 			const activeEditor = editorService.activeEditor;
-			if (!isNotebookEditorInput(activeEditor)) {
+			if (!(isNotebookEditorInput(activeEditor) ||
+				// TODO: Wasn't there a function for this?
+				(activeEditor?.typeId === POSITRON_NOTEBOOK_EDITOR_INPUT_ID))) {
 				throw new Error('No active notebook. This command should only be available when a notebook is active.');
 			}
 			notebookUri = activeEditor.resource;
 		}
 
-		// If no context was provided, try to get the active notebook URI.
-		if (!notebookUri) {
-			const activeEditor = editorService.activeEditor;
-			if (!isNotebookEditorInput(activeEditor)) {
-				throw new Error('No active notebook. This command should only be available when a notebook is active.');
-			}
-			notebookUri = activeEditor.resource;
-		}
-
-		const runtimeSession = runtimeSessionService.getNotebookSessionForNotebookUri(notebookUri);
+		const runtimeSession = notebookUri && runtimeSessionService.getNotebookSessionForNotebookUri(notebookUri);
 
 		return this.runWithContext(accessor, { runtimeSession, source });
 	}
@@ -111,6 +108,11 @@ export class RuntimeNotebookKernelRestartAction extends BaseRuntimeNotebookKerne
 			f1: true,
 			category,
 			precondition: ActiveNotebookHasRunningRuntime,
+			keybinding: {
+				weight: KeybindingWeight.WorkbenchContrib,
+				primary: KeyChord(KeyCode.Digit0, KeyCode.Digit0),
+				when: POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED,
+			},
 			menu: [
 				// VSCode notebooks
 				{
