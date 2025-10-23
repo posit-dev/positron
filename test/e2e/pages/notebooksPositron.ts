ea9e20557807581e7d08b7cbf0e0931dd295c1a7
Marie Idleman <marie.idleman@posit.co>
2025-10-23 13:34:23 -0500

    try standard click

--- test/e2e/pages/notebooksPositron.ts
+++ test/e2e/pages/notebooksPositron.ts
@@ -381,10 +381,11 @@ export class PositronNotebooks extends Notebooks {
 				this.code.logger.log(`Clicking kernel status badge to select: ${desiredKernel}`);
 				await expect(async () => {
 					// we shouldn't need to retry this, but the input closes immediately sometimes
-					await this.contextMenu.triggerAndClick({
-						menuTrigger: this.kernelStatusBadge,
-						menuItemLabel: /Change Kernel/
-					});
+					// await this.contextMenu.triggerAndClick({
+					// 	menuTrigger: this.kernelStatusBadge,
+					// 	menuItemLabel: /Change Kernel/
+					// });
+					await this.kernelStatusBadge.click();
 					// this is a short wait because for some reason, 1st click always gets auto-closed in playwright :shrug:
 					await this.quickinput.waitForQuickInputOpened({ timeout: 1000 });
 					await this.quickinput.selectQuickInputElementContaining(desiredKernel, { timeout: 1000, force: false });
