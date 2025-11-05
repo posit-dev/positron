ebc6500165c5e6ff2a56c82494bfb508aa1cd67b
petetronic <1178561+petetronic@users.noreply.github.com>
2025-11-05 15:41:16 -0500

    Add NOTICE to reh and web distributions

--- build/gulpfile.reh.js
+++ build/gulpfile.reh.js
@@ -382,7 +382,7 @@ function packageTask(type, platform, arch, sourceFolderName, destinationFolderNa
 				this.emit('data', file);
 			}));
 
-		const license = gulp.src(['remote/LICENSE'], { base: 'remote', allowEmpty: true });
+		const license = gulp.src(['remote/LICENSE', 'NOTICE'], { base: '.', allowEmpty: true });
 
 		const jsFilter = util.filter(data => !data.isDirectory() && /\.js$/.test(data.path));
 
