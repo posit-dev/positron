955824c4ec1c9b64383c53ed34d2342538ac5692
Dhruvi Sompura <dhruvi.sompura@posit.co>
2025-11-12 16:09:53 -0800

    Add comment explaining context key nuance

--- src/vs/workbench/contrib/positronNotebook/browser/positronNotebook.contribution.ts
+++ src/vs/workbench/contrib/positronNotebook/browser/positronNotebook.contribution.ts
@@ -1354,6 +1354,10 @@ MenuRegistry.appendMenuItem(MenuId.EditorContext, {
 		// output panel (which is a monaco editor).
 		ContextKeyExpr.or(
 			POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED,
+			// Need to include this context key to ensure the menu shows up
+			// when right-clicking inside a cell editor. This is because the
+			// POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED key gets set to false
+			// when user is editing a cell.
 			POSITRON_NOTEBOOK_CELL_EDITOR_FOCUSED
 		)
 	),
