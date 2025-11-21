a2aa36479733b3fd5dcce7a1394185c3db6b2099
Marie Idleman <marie.idleman@posit.co>
2025-11-21 15:23:06 -0600

    more tests

--- test/e2e/pages/notebooksPositron.ts
+++ test/e2e/pages/notebooksPositron.ts
@@ -204,6 +204,7 @@ export class PositronNotebooks extends Notebooks {
 		if (codeCells > 0) {
 			for (let i = 0; i < codeCells; i++) {
 				await this.addCodeToCell(i, `# Cell ${i}`);
+				await this.expectCellCountToBe(totalCellsAdded + 1);
 				totalCellsAdded++;
 			}
 		}
@@ -212,10 +213,10 @@ export class PositronNotebooks extends Notebooks {
 			for (let i = 0; i < markdownCells; i++) {
 				await this.addCell('markdown');
 				await keyboard.type(`### Cell ${totalCellsAdded}`);
+				await this.expectCellCountToBe(totalCellsAdded + 1);
 				totalCellsAdded++;
 			}
 		}
-		await this.expectCellCountToBe(codeCells + markdownCells);
 	}
 
 	/**
@@ -455,6 +456,7 @@ export class PositronNotebooks extends Notebooks {
 					await this.code.driver.page.keyboard.press('KeyV');
 					break;
 				case 'undo':
+					// await this.hotKeys.undo();
 					await this.code.driver.page.keyboard.press('KeyZ');
 					break;
 				case 'redo':
