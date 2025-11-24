b703ebedce58bec51a958278d084367d59965af4
Chris Mead <chris.mead@posit.co>
2025-11-24 12:30:06 -0700

    E2E test: skip testthat test - https://github.com/posit-dev/positron/issues/10682

--- test/e2e/tests/test-explorer/test-explorer.test.ts
+++ test/e2e/tests/test-explorer/test-explorer.test.ts
@@ -23,8 +23,9 @@ test.describe('Test Explorer', { tag: [tags.TEST_EXPLORER, tags.WEB] }, () => {
 		}
 	});
 
-	test('R - Verify Basic Test Explorer Functionality', {
-		tag: [tags.ARK]
+	test.skip('R - Verify Basic Test Explorer Functionality', {
+		tag: [tags.ARK],
+		annotation: [{ type: 'issue', description: 'https://github.com/posit-dev/positron/issues/10682' }]
 	}, async function ({ app, openFolder }) {
 
 		// Open R package embedded in qa-example-content
