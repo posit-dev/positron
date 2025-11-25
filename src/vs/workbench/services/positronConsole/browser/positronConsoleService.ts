58eefe07a338bc2425c2d36ed7d322c54c0a32cb
Lionel Henry <lionel.hry@proton.me>
2025-11-25 16:45:25 +0100

    Add code location to `execute()` method of language runtimes

--- src/vs/workbench/services/positronConsole/browser/positronConsoleService.ts
+++ src/vs/workbench/services/positronConsole/browser/positronConsoleService.ts
@@ -3030,7 +3030,9 @@ class PositronConsoleInstance extends Disposable implements IPositronConsoleInst
 			code,
 			id,
 			mode,
-			errorBehavior);
+			errorBehavior,
+			attribution,
+		);
 
 		// Create and fire the onDidExecuteCode event.
 		const event: ILanguageRuntimeCodeExecutedEvent = {
