793ebade7346b0deb59fb4df4ed3d9f3ef60b231
Rodrigo Silva Ferreira <129895386+rodrigosf672@users.noreply.github.com>
2025-12-05 16:07:24 -0500

    Update clear command in Python app tests
    Signed-off-by: Rodrigo Silva Ferreira <129895386+rodrigosf672@users.noreply.github.com>

--- test/e2e/tests/apps/python-apps.test.ts
+++ test/e2e/tests/apps/python-apps.test.ts
@@ -48,7 +48,7 @@ test.describe('Python Applications', {
 		});
 
 		await test.step('Verify Clear Current URL clears Viewer', async () => {
-			await app.workbench.quickaccess.runCommand('positron.preview.clear');
+			await app.workbench.locator('.codicon-clear-all').click();
 
 			await expect(async () => {
 				const iframeLocator = app.web
