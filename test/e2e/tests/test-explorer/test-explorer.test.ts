ed9225432fdc999a650c36cf0c19c072d34b4b33
Marie Idleman <marie.idleman@posit.co>
2025-12-05 15:19:19 -0600

    Revert "remove tagged issue"
    This reverts commit d4be79ab78989339ade72c2a61b3895536874cbf.

--- test/e2e/tests/test-explorer/test-explorer.test.ts
+++ test/e2e/tests/test-explorer/test-explorer.test.ts
@@ -25,6 +25,7 @@ test.describe('Test Explorer', { tag: [tags.TEST_EXPLORER, tags.WEB] }, () => {
 
 	test('R - Verify Basic Test Explorer Functionality', {
 		tag: [tags.ARK],
+		annotation: [{ type: 'issue', description: 'https://github.com/posit-dev/positron/issues/10682' }]
 	}, async function ({ app, openFolder }) {
 
 		// Open R package embedded in qa-example-content
