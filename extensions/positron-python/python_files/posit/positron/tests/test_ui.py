088f8f01a441b2cdaabf719ad1fcab1191c6870a
Jonathan McPherson <jonathan@posit.co>
2025-12-11 11:28:23 -0800

    update python

--- extensions/positron-python/python_files/posit/positron/tests/test_ui.py
+++ extensions/positron-python/python_files/posit/positron/tests/test_ui.py
@@ -66,9 +66,9 @@ def show_url_event(url: str) -> Dict[str, Any]:
     return json_rpc_notification(UiFrontendEvent.ShowUrl, {"url": url, "source": None})
 
 
-def show_html_file_event(path: str, *, is_plot: bool) -> Dict[str, Any]:
+def show_html_file_event(path: str, *, destination: bool) -> Dict[str, Any]:
     return json_rpc_notification(
-        "show_html_file", {"path": path, "is_plot": is_plot, "height": 0, "title": ""}
+        "show_html_file", {"path": path, "destination": destination, "height": 0, "title": ""}
     )
 
 
@@ -178,12 +178,12 @@ def test_shutdown(ui_service: UiService, ui_comm: DummyComm) -> None:
         # Unix path
         (
             "file://hello/my/friend.html",
-            [show_html_file_event("file://hello/my/friend.html", is_plot=False)],
+            [show_html_file_event("file://hello/my/friend.html", destination=ShowHtmlFileDestination.Viewer)],
         ),
         # Windows path
         (
             "file:///C:/Users/username/Documents/index.htm",
-            [show_html_file_event("file:///C:/Users/username/Documents/index.htm", is_plot=False)],
+            [show_html_file_event("file:///C:/Users/username/Documents/index.htm", destination=ShowHtmlFileDestination.Viewer)],
         ),
         # Not a local html file
         ("http://example.com/page.html", []),
@@ -233,7 +233,7 @@ show(p)
     assert len(ui_comm.messages) == 1
     params = ui_comm.messages[0]["data"]["params"]
     assert params["title"] == ""
-    assert params["is_plot"]
+    assert params["destination"] == "plot"
     assert params["height"] == 0
     # default behavior should be writing to temppath
     # not wherever the process is running (see patch.bokeh)
@@ -281,11 +281,11 @@ fig
     assert len(ui_comm.messages) == 2
     params = ui_comm.messages[0]["data"]["params"]
     assert params["title"] == ""
-    assert params["is_plot"]
+    assert params["destination"] == "plot"
     assert params["height"] == 0
     params = ui_comm.messages[1]["data"]["params"]
     assert params["title"] == ""
-    assert params["is_plot"]
+    assert params["destination"] == "plot"
     assert params["height"] == 0
 
 
@@ -296,7 +296,7 @@ def test_is_not_plot_url_events(
     """
     Test that opening a URL that is not a plot sends the expected UI events.
 
-    Checks that the `is_plot` parameter is not sent or is `False`.
+    Checks that the `destination` parameter is not set to "plot".
     """
     shell.run_cell(
         """\
@@ -311,8 +311,9 @@ webbrowser.open("file://file.html")
     assert len(ui_comm.messages) == 2
     params = ui_comm.messages[0]["data"]["params"]
     assert params["url"] == "http://127.0.0.1:8000"
-    assert "is_plot" not in params
+    assert "destination" not in params
 
     params = ui_comm.messages[1]["data"]["params"]
     assert params["path"] == "file.html" if sys.platform == "win32" else "file://file.html"
-    assert params["is_plot"] is False
+    assert params["destination"] is not "plot"
+
