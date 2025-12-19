cf8fe86e3e143e8fa6ef862fbacce157defe25ce
seem <mwlorgat@gmail.com>
2025-12-19 12:37:06 +0200

    fix notebook find stealing focus from output pane

--- src/vs/workbench/contrib/positronNotebook/browser/contrib/find/actions.ts
+++ src/vs/workbench/contrib/positronNotebook/browser/contrib/find/actions.ts
@@ -5,19 +5,21 @@
 
 import { KeyCode, KeyMod } from '../../../../../../base/common/keyCodes.js';
 import { ICodeEditor } from '../../../../../../editor/browser/editorBrowser.js';
+import { EditorActionImplementation } from '../../../../../../editor/browser/editorExtensions.js';
 import { EditorContextKeys } from '../../../../../../editor/common/editorContextKeys.js';
 import { NextMatchFindAction, PreviousMatchFindAction, StartFindAction } from '../../../../../../editor/contrib/find/browser/findController.js';
 import { CONTEXT_FIND_WIDGET_VISIBLE } from '../../../../../../editor/contrib/find/browser/findModel.js';
 import { localize2 } from '../../../../../../nls.js';
-import { registerAction2 } from '../../../../../../platform/actions/common/actions.js';
+import { IAction2Options, registerAction2 } from '../../../../../../platform/actions/common/actions.js';
 import { ContextKeyExpr } from '../../../../../../platform/contextkey/common/contextkey.js';
 import { ServicesAccessor } from '../../../../../../platform/instantiation/common/instantiation.js';
 import { KeybindingWeight } from '../../../../../../platform/keybinding/common/keybindingsRegistry.js';
 import { IEditorService } from '../../../../../services/editor/common/editorService.js';
-import { POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED } from '../../ContextKeysManager.js';
+import { POSITRON_NOTEBOOK_CELL_EDITOR_FOCUSED, POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED } from '../../ContextKeysManager.js';
 import { IPositronNotebookInstance } from '../../IPositronNotebookInstance.js';
 import { NotebookAction2 } from '../../NotebookAction2.js';
 import { getNotebookInstanceFromActiveEditorPane } from '../../notebookUtils.js';
+import { getActiveCell } from '../../selectionMachine.js';
 import { PositronNotebookFindController } from './controller.js';
 
 abstract class PositronNotebookFindAction extends NotebookAction2 {
@@ -31,35 +33,75 @@ abstract class PositronNotebookFindAction extends NotebookAction2 {
 	abstract runFindAction(controller: PositronNotebookFindController): Promise<void>;
 }
 
-registerAction2(class extends PositronNotebookFindAction {
-	constructor() {
-		super({
-			id: 'positron.notebook.find',
-			title: localize2('positron.notebook.find.title', 'Find in Notebook'),
-			keybinding: {
-				when: ContextKeyExpr.and(
-					POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED,
-					EditorContextKeys.focus.toNegated()
-				),
-				primary: KeyCode.KeyF | KeyMod.CtrlCmd,
-				weight: KeybindingWeight.WorkbenchContrib
-			}
-		});
-	}
+function registerPositronNotebookFindAction(
+	options: IAction2Options & { run: (controller: PositronNotebookFindController) => void | Promise<void> },
+) {
+	return registerAction2(class extends PositronNotebookFindAction {
+		constructor() {
+			super(options);
+		}
 
-	override async runFindAction(controller: PositronNotebookFindController): Promise<void> {
-		controller.start();
-	}
+		override async runFindAction(controller: PositronNotebookFindController): Promise<void> {
+			return options.run(controller);
+		}
+	});
+}
+
+registerPositronNotebookFindAction({
+	id: 'positron.notebook.find.focus',
+	title: localize2('positron.notebook.find.focus.title', 'Focus Find'),
+	keybinding: {
+		when: ContextKeyExpr.and(
+			POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED,
+			EditorContextKeys.focus.toNegated()
+		),
+		primary: KeyCode.KeyF | KeyMod.CtrlCmd,
+		weight: KeybindingWeight.WorkbenchContrib
+	},
+	run: (controller) => controller.start()
 });
 
+function findEditorActionImplementation(handler: (controller: PositronNotebookFindController) => void): EditorActionImplementation {
+	return (accessor: ServicesAccessor, codeEditor: ICodeEditor, _args: unknown) => {
+		// Get the active notebook instance
+		const editorService = accessor.get(IEditorService);
+		const notebook = getNotebookInstanceFromActiveEditorPane(editorService);
+		if (!notebook) {
+			return false;
+		}
+
+		// Confirm that the code editor matches the notebook's active cell
+		const activeCell = getActiveCell(notebook.selectionStateMachine.state.get());
+		if (activeCell?.currentEditor !== codeEditor) {
+			return false;
+		}
+
+		// Get the find controller
+		const controller = PositronNotebookFindController.get(notebook);
+		if (!controller) {
+			return false;
+		}
+
+		// Invoke the handler
+		handler(controller);
+		return true;
+	};
+}
+
+// Invoked when Cmd+F is pressed while editing a notebook cell
+StartFindAction.addImplementation(100, findEditorActionImplementation((controller) => controller.start()));
+
 registerAction2(class extends PositronNotebookFindAction {
 	constructor() {
 		super({
-			id: 'positron.notebook.hideFind',
-			title: localize2('positron.notebook.hideFind.title', 'Hide Find in Notebook'),
+			id: 'positron.notebook.find.hide',
+			title: localize2('positron.notebook.find.hide.title', 'Hide Find'),
 			keybinding: {
 				when: ContextKeyExpr.and(
-					POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED,
+					ContextKeyExpr.or(
+						POSITRON_NOTEBOOK_EDITOR_CONTAINER_FOCUSED,
+						POSITRON_NOTEBOOK_CELL_EDITOR_FOCUSED,
+					),
 					CONTEXT_FIND_WIDGET_VISIBLE,
 				),
 				primary: KeyCode.Escape,
@@ -73,51 +115,12 @@ registerAction2(class extends PositronNotebookFindAction {
 	}
 });
 
-NextMatchFindAction.addImplementation(0, (accessor: ServicesAccessor, _codeEditor: ICodeEditor, _args: unknown) => {
-	const editorService = accessor.get(IEditorService);
-	const notebook = getNotebookInstanceFromActiveEditorPane(editorService);
-	if (!notebook) {
-		return false;
-	}
+// registerAction2(class extends PositronNotebookFindAction {
+// 	constructor() {
+// 		super({
+// 			id: 'positron.notebook.findNext',
+// 			title: localize2('positron.notebook.findNext.title', 'Find Next'),
 
-	const controller = PositronNotebookFindController.get(notebook);
-	if (!controller) {
-		return false;
-	}
+NextMatchFindAction.addImplementation(0, findEditorActionImplementation((controller) => controller.findNext));
 
-	controller.findNext();
-	return true;
-});
-
-PreviousMatchFindAction.addImplementation(0, (accessor: ServicesAccessor, _codeEditor: ICodeEditor, _args: unknown) => {
-	const editorService = accessor.get(IEditorService);
-	const notebook = getNotebookInstanceFromActiveEditorPane(editorService);
-	if (!notebook) {
-		return false;
-	}
-
-	const controller = PositronNotebookFindController.get(notebook);
-	if (!controller) {
-		return false;
-	}
-
-	controller.findPrevious();
-	return true;
-});
-
-// Invoked when Cmd+F is pressed while editing a notebook cell
-StartFindAction.addImplementation(100, (accessor: ServicesAccessor, _codeEditor: ICodeEditor, _args: unknown) => {
-	const editorService = accessor.get(IEditorService);
-	const notebook = getNotebookInstanceFromActiveEditorPane(editorService);
-	if (!notebook) {
-		return false;
-	}
-
-	const controller = PositronNotebookFindController.get(notebook);
-	if (!controller) {
-		return false;
-	}
-
-	controller.start();
-	return true;
-});
+PreviousMatchFindAction.addImplementation(0, findEditorActionImplementation((controller) => controller.findPrevious));
