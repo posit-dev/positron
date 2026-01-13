0744b6d84d9c8de33880b76d9427b89ee3fa6265
Julia Silge <julia.silge@gmail.com>
2026-01-12 18:41:09 -0700

    Adjust fences around upstream code again

--- src/vs/workbench/services/extensionManagement/common/extensionManagementService.ts
+++ src/vs/workbench/services/extensionManagement/common/extensionManagementService.ts
@@ -554,13 +554,12 @@ export class ExtensionManagementService extends CommontExtensionManagementServic
 	}
 
 	async installFromGallery(gallery: IGalleryExtension, installOptions?: InstallOptions, servers?: IExtensionManagementServer[]): Promise<ILocalExtension> {
-		// --- Start Positron ---
-		// Check compatibility using the manifest (which includes engines.positron if specified)
 		const manifest = await this.extensionGalleryService.getManifest(gallery, CancellationToken.None);
 		if (!manifest) {
 			throw new Error(localize('Manifest is not found', "Installing Extension {0} failed: Manifest is not found.", gallery.displayName || gallery.name));
 		}
-
+		// --- Start Positron ---
+		// Check compatibility using the manifest (which includes engines.positron if specified)
 		const compat = positronExtensionCompatibility(manifest, this.productService);
 		if (!compat.compatible) {
 			return Promise.reject(positronExtensionCompatibilityError(compat.reason));
