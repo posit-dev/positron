a93063b6770a02cf8c6be980221ec223d6e6aff4
Dhruvi Sompura <dhruvi.sompura@posit.co>
2026-01-16 12:05:54 -0800

    Replace vertical border with cell border

--- src/vs/workbench/contrib/positronNotebook/browser/notebookCells/NotebookCodeCell.tsx
+++ src/vs/workbench/contrib/positronNotebook/browser/notebookCells/NotebookCodeCell.tsx
@@ -30,7 +30,7 @@ interface CellOutputsSectionProps {
 
 function CellOutputsSection({ outputs }: CellOutputsSectionProps) {
 	return (
-		<div className={`positron-notebook-code-cell-outputs positron-notebook-cell-outputs ${outputs.length > 0 ? 'has-outputs' : 'no-outputs'}`} data-testid='cell-output'>
+		<div className={`positron-notebook-code-cell-outputs positron-notebook-cell-outputs ${outputs.length > 0 ? '' : 'no-outputs'}`} data-testid='cell-output'>
 			<div className='positron-notebook-code-cell-outputs-inner'>
 				{outputs?.map((output) => (
 					<CellOutput key={output.outputId} {...output} />
