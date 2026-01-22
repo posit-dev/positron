ec7fe7db76cab0a53f7488ed79e61d6bd602f624
Chris Mead <chris.mead@posit.co>
2026-01-22 15:43:34 -0700

    E2E test: skip "Verify Restart Extension Host command works - Python" for now

--- test/e2e/tests/extensions/restart-host-ext.test.ts
+++ test/e2e/tests/extensions/restart-host-ext.test.ts
@@ -30,6 +30,9 @@ test.describe('Restart Host Extension', { tag: [tags.EXTENSIONS, tags.WIN] }, ()
 	});
 
 	test('Verify Restart Extension Host command works - Python', async function ({ app, python }) {
+		// FIXME: Disabled for https://github.com/posit-dev/positron/pull/11407
+		test.skip(process.platform === 'win32', 'Skip on Windows');
+
 		await app.workbench.quickaccess.runCommand('workbench.action.restartExtensionHost');
 		await app.workbench.console.waitForConsoleContents('Extensions restarting...');
 		await app.workbench.console.waitForReady('>>>');
