2fe38537efe7e88d2029b66e06cd554f1a7f8a54
Jonathan McPherson <jonathan@posit.co>
2026-01-28 15:45:21 -0800

    improve centering of gutter decoration

--- src/vs/workbench/contrib/positronQuarto/browser/quartoExecutionDecorations.ts
+++ src/vs/workbench/contrib/positronQuarto/browser/quartoExecutionDecorations.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2026 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -48,7 +48,7 @@ const queuedDecorationOptions: IModelDecorationOptions = {
 	stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
 	overviewRuler: {
 		color: themeColorFromId(quartoExecutionQueued),
-		position: OverviewRulerLane.Left,
+		position: OverviewRulerLane.Full,
 	},
 };
 
@@ -63,7 +63,7 @@ const runningDecorationOptions: IModelDecorationOptions = {
 	stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
 	overviewRuler: {
 		color: themeColorFromId(quartoExecutionRunning),
-		position: OverviewRulerLane.Left,
+		position: OverviewRulerLane.Full,
 	},
 };
 
