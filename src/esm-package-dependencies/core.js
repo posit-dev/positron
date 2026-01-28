24ae71d418e7adbad0f7647b7d7d704f21b5aea2
Nick Strayer <nick.strayer@posit.co>
2026-01-28 15:45:58 -0500

    chore(notebooks): Remove vendored dnd-kit library
    The custom drag-and-drop implementation is now complete and tested.
    This removes the no-longer-needed dnd-kit dependency including:
    - Package dependencies from package.json
    - Vendored library files from esm-package-dependencies
    - Import map entries from workbench HTML/TS files
    - License entry from ThirdPartyNotices.txt

--- src/esm-package-dependencies/core.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-// @dnd-kit/core - MIT License - https://github.com/clauderic/dnd-kit
-import "./v135/@dnd-kit/utilities@3.2.2/es2022/utilities.mjs";
-import "./v135/@dnd-kit/accessibility@3.1.1/es2022/accessibility.mjs";
-export * from "./v135/@dnd-kit/core@6.3.0/es2022/core.mjs";
