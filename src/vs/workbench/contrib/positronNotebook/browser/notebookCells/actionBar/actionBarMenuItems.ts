b9b7d07583e79869da051cbc8d30e9daed8fafc9
Dhruvi Sompura <dhruvi.sompura@posit.co>
2026-02-03 12:45:39 -0800

    Positron Notebooks: show cell action bar on hover (#11544)

--- src/vs/workbench/contrib/positronNotebook/browser/notebookCells/actionBar/actionBarMenuItems.ts
+++ src/vs/workbench/contrib/positronNotebook/browser/notebookCells/actionBar/actionBarMenuItems.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2025-2026 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -8,14 +8,28 @@ import { CustomContextMenuEntry } from '../../../../../../workbench/browser/posi
 import { CustomContextMenuSeparator } from '../../../../../browser/positronComponents/customContextMenu/customContextMenuSeparator.js';
 import { MenuItemAction, SubmenuItemAction } from '../../../../../../platform/actions/common/actions.js';
 import { ThemeIcon } from '../../../../../../base/common/themables.js';
+import { IPositronNotebookCell } from '../../PositronNotebookCells/IPositronNotebookCell.js';
+import { IPositronNotebookInstance } from '../../IPositronNotebookInstance.js';
+import { CellSelectionType } from '../../selectionMachine.js';
 
 /**
  * Build the menu entries for the "more actions" dropdown menu.
  * This provides a clean extension point for adding new actions via the registry.
+ *
+ * Note: Like the CellActionButton component, we need to ensure the cell is selected
+ * before running any action. This is done via the onWillSelect callback which runs
+ * before the command is executed.
+ *
+ * @param cell The cell that the menu actions will operate on
+ * @param menuActions The grouped menu actions to display
+ * @param instance The notebook instance (needed to select cell before action runs)
  */
 export function buildMoreActionsMenuItems(
+	cell: IPositronNotebookCell,
 	menuActions: [string, (MenuItemAction | SubmenuItemAction)[]][],
+	instance: IPositronNotebookInstance,
 ): CustomContextMenuEntry[] {
+
 	// Get the groups
 	const entriesByGroup = new Map<string, CustomContextMenuEntry[]>();
 	menuActions.forEach(([group, groupActions]) => {
@@ -26,6 +40,10 @@ export function buildMoreActionsMenuItems(
 			commandId: action.id,
 			label: action.label,
 			icon: ThemeIcon.isThemeIcon(action.item.icon) ? action.item.icon.id : undefined,
+			onWillSelect: () => {
+				// Select cell BEFORE command runs to keep notebook selection in sync
+				instance.selectionStateMachine.selectCell(cell, CellSelectionType.Normal);
+			},
 			onSelected: () => {
 				// No op as command is executed via the commandId argument
 			}
