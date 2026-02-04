e93d02eef2172fa4ef25695d85075133296c8142
seem <mwlorgat@gmail.com>
2026-02-04 19:09:11 +0200

    start/stop in parallel

--- scripts/build-start.sh
+++ scripts/build-start.sh
@@ -7,11 +7,20 @@ run() {
 	local cmd="$@"
 	if ! result=$($cmd 2>&1); then
 		echo "$result"
-		exit 1
+		return 1
 	fi
 	echo "[$name] started"
 }
 
-run watch-client npx deemon -- --detach npm run watch-client
-run watch-extensions npx deemon -- --detach npm run watch-extensions
-run watch-e2e npx deemon -- --detach npm run watch-e2e
+run watch-client npx deemon -- --detach npm run watch-client &
+pid1=$!
+run watch-extensions npx deemon -- --detach npm run watch-extensions &
+pid2=$!
+run watch-e2e npx deemon -- --detach npm run watch-e2e &
+pid3=$!
+
+failed=0
+wait $pid1 || failed=1
+wait $pid2 || failed=1
+wait $pid3 || failed=1
+exit $failed
--- scripts/build-stop.sh
+++ scripts/build-stop.sh
@@ -7,11 +7,20 @@ run() {
 	local cmd="$@"
 	if ! result=$($cmd 2>&1); then
 		echo "$result"
-		exit 1
+		return 1
 	fi
 	echo "[$name] stopped"
 }
 
-run watch-client npx deemon -- --kill npm run watch-client
-run watch-extensions npx deemon -- --kill npm run watch-extensions
-run watch-e2e npx deemon -- --kill npm run watch-e2e
+run watch-client npx deemon -- --kill npm run watch-client &
+pid1=$!
+run watch-extensions npx deemon -- --kill npm run watch-extensions &
+pid2=$!
+run watch-e2e npx deemon -- --kill npm run watch-e2e &
+pid3=$!
+
+failed=0
+wait $pid1 || failed=1
+wait $pid2 || failed=1
+wait $pid3 || failed=1
+exit $failed
