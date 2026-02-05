109533883e599083102b474f3fcc2c9f030dc3a5
Brian Lambert <brianlambert@gmail.com>
2026-02-05 15:24:36 -0700

    Fix build issue

--- build/npm/dirs.ts
+++ build/npm/dirs.ts
@@ -31,6 +31,7 @@ export let dirs = [
 	'extensions/positron-ipywidgets',
 	'extensions/positron-javascript',
 	'extensions/positron-notebooks',
+	'extensions/positron-pdf-server',
 	'extensions/positron-r',
 	'extensions/positron-reticulate',
 	'extensions/positron-run-app',
