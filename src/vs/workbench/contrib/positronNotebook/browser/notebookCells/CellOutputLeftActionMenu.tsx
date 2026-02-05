a909693a9f5f69cf4bd7a4d49b820218804ae200
Dhruvi Sompura <dhruvi.sompura@posit.co>
2026-02-05 15:07:01 -0800

    Use native menu

--- src/vs/workbench/contrib/positronNotebook/browser/notebookCells/CellOutputLeftActionMenu.tsx
+++ src/vs/workbench/contrib/positronNotebook/browser/notebookCells/CellOutputLeftActionMenu.tsx
@@ -7,17 +7,21 @@
 import './CellOutputLeftActionMenu.css';
 
 // React.
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // Other dependencies.
 import { localize } from '../../../../../nls.js';
+import { ActionRunner } from '../../../../../base/common/actions.js';
 import { MenuId } from '../../../../../platform/actions/common/actions.js';
-import { useMenu } from '../useMenu.js';
-import { useMenuActions } from '../useMenuActions.js';
+import { ActionButton } from '../utilityComponents/ActionButton.js';
 import { useNotebookInstance } from '../NotebookInstanceProvider.js';
 import { useCellScopedContextKeyService } from './CellContextKeyServiceProvider.js';
-import { NotebookCellMoreActionsMenu } from './actionBar/NotebookCellMoreActionsMenu.js';
 import { PositronNotebookCodeCell } from '../PositronNotebookCells/PositronNotebookCodeCell.js';
+import { usePositronReactServicesContext } from '../../../../../base/browser/positronReactRendererContext.js';
+import { CellSelectionType } from '../selectionMachine.js';
+import { useObservedValue } from '../useObservedValue.js';
+import { Icon } from '../../../../../platform/positronActionBar/browser/components/icon.js';
+import { Codicon } from '../../../../../base/common/codicons.js';
 
 const cellOutputActions = localize('cellOutputActions', 'Cell Output Actions');
 
@@ -27,32 +31,74 @@ interface CellOutputLeftActionMenuProps {
 
 /**
  * The left action menu for notebook cell output actions.
+ * Uses the native context menu service to display actions registered to
+ * MenuId.PositronNotebookCellOutputActionLeft.
  * @param cell The cell that the menu actions will operate on
  */
 export function CellOutputLeftActionMenu({ cell }: CellOutputLeftActionMenuProps) {
 	const instance = useNotebookInstance();
 	const contextKeyService = useCellScopedContextKeyService();
+	const { contextMenuService } = usePositronReactServicesContext();
 
+	const buttonRef = useRef<HTMLButtonElement>(null);
+	const actionRunnerRef = useRef<ActionRunner>();
 	const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-	const menu = useMenu(MenuId.PositronNotebookCellOutputActionLeft, contextKeyService);
-	const menuActions = useMenuActions(menu);
 
-	// Don't render if there are no actions
-	if (menuActions.length === 0) {
+	// Check if there are outputs to determine if we should render the menu
+	const outputs = useObservedValue(cell.outputs);
+	const hasOutputs = outputs.length > 0;
+
+	// Create and setup the action runner
+	useEffect(() => {
+		const actionRunner = new ActionRunner();
+
+		// Select the cell before any action runs to ensure notebook selection is in sync
+		const onWillRunDisposable = actionRunner.onWillRun(() => {
+			instance.selectionStateMachine.selectCell(cell, CellSelectionType.Normal);
+		});
+
+		actionRunnerRef.current = actionRunner;
+
+		return () => {
+			onWillRunDisposable.dispose();
+			actionRunner.dispose();
+		};
+	}, [cell, instance]);
+
+	const showContextMenu = () => {
+		if (!buttonRef.current || !actionRunnerRef.current) {
+			return;
+		}
+
+		setIsMenuOpen(true);
+
+		contextMenuService.showContextMenu({
+			menuId: MenuId.PositronNotebookCellOutputActionLeft,
+			contextKeyService,
+			getAnchor: () => buttonRef.current!,
+			actionRunner: actionRunnerRef.current,
+			onHide: () => setIsMenuOpen(false),
+		});
+	};
+
+	// Don't render if the cell has no outputs
+	if (!hasOutputs) {
 		return null;
 	}
 
 	return (
 		<div className='cell-output-left-action-menu'>
-			<NotebookCellMoreActionsMenu
+			<ActionButton
+				ref={buttonRef}
+				aria-expanded={isMenuOpen}
+				aria-haspopup='menu'
 				ariaLabel={cellOutputActions}
-				cell={cell}
 				hoverManager={instance.hoverManager}
-				instance={instance}
-				isMenuOpen={isMenuOpen}
-				menuActions={menuActions}
-				setIsMenuOpen={setIsMenuOpen}
-			/>
+				tooltip={cellOutputActions}
+				onPressed={showContextMenu}
+			>
+				<Icon className='button-icon' icon={Codicon.ellipsis} />
+			</ActionButton>
 		</div>
 	);
 }
