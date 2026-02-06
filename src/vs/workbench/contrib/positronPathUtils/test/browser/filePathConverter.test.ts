160b336cc8c8b0e8436f8dc355c142cd2c589dbd
Jenny Bryan <jenny.f.bryan@gmail.com>
2026-02-06 15:11:24 -0800

    Refactor tests now that formatPathForCode() exists
    Most of the existing tests of convertClipboardFiles() were really about functionality that is now exposed in formatPathForCode(). Split into 2 suites.

--- src/vs/workbench/contrib/positronPathUtils/test/browser/filePathConverter.test.ts
+++ src/vs/workbench/contrib/positronPathUtils/test/browser/filePathConverter.test.ts
@@ -1,10 +1,10 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2026 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import assert from 'assert';
-import { convertClipboardFiles } from '../../common/filePathConverter.js';
+import { formatPathForCode, convertClipboardFiles } from '../../common/filePathConverter.js';
 import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../../../base/test/common/utils.js';
 import { isWindows } from '../../../../../base/common/platform.js';
 import { URI } from '../../../../../base/common/uri.js';
@@ -13,136 +13,109 @@ suite('File Path Converter Tests', () => {
 
 	ensureNoDisposablesAreLeakedInTestSuite();
 
-	// Inputs largely based on real-world examples captured in the debugger on Windows and macOS
+	suite('formatPathForCode', () => {
 
-	test('Convert a single Windows file path', () => {
-		const uriListData = 'file:///c%3A/Users/test/file.txt';
-		const result = convertClipboardFiles(uriListData);
-		assert.deepStrictEqual(result, ['"c:/Users/test/file.txt"']);
+		test('Basic quoting of a simple path', () => {
+			const result = formatPathForCode('c:/Users/test/file.txt');
+			assert.strictEqual(result, '"c:/Users/test/file.txt"');
+		});
+
+		test('Backslashes are normalized to forward slashes', () => {
+			const result = formatPathForCode('c:\\Users\\test\\file.txt');
+			assert.strictEqual(result, '"c:/Users/test/file.txt"');
+		});
+
+		test('File path with quotes is escaped correctly', () => {
+			// Double quotes are allowed in filenames on macOS/Linux (but not Windows)
+			const result = formatPathForCode('/Users/test/test"file.txt');
+			assert.strictEqual(result, '"/Users/test/test\\"file.txt"');
+		});
+
+		test('Workspace-relative path when file is inside workspace', () => {
+			const result = formatPathForCode('c:/Users/test/project/src/components/file.txt', [
+				{ uri: URI.file('c:/Users/test/project') }
+			]);
+			assert.strictEqual(result, '"src/components/file.txt"');
+		});
+
+		test('Absolute path when file is outside workspace', () => {
+			const result = formatPathForCode('c:/Users/test/other-project/file.txt', [
+				{ uri: URI.file('c:/Users/test/project') }
+			]);
+			assert.strictEqual(result, '"c:/Users/test/other-project/file.txt"');
+		});
+
+		test('Home-relative path when file is in home directory but outside workspace', () => {
+			const result = formatPathForCode('c:/Users/test/Documents/file.txt', [
+				{ uri: URI.file('c:/Users/test/project') },
+				{ uri: URI.file('c:/Users/test'), prefix: '~/' }
+			]);
+			assert.strictEqual(result, '"~/Documents/file.txt"');
+		});
+
+		test('Workspace-relative takes priority over home-relative', () => {
+			const result = formatPathForCode('c:/Users/test/project/src/file.txt', [
+				{ uri: URI.file('c:/Users/test/project') },
+				{ uri: URI.file('c:/Users/test'), prefix: '~/' }
+			]);
+			assert.strictEqual(result, '"src/file.txt"');
+		});
+
+		test('POSIX paths: workspace-relative', () => {
+			const result = formatPathForCode('/home/user/project/src/file.txt', [
+				{ uri: URI.file('/home/user/project') }
+			]);
+			assert.strictEqual(result, '"src/file.txt"');
+		});
+
+		test('POSIX paths: home-relative', () => {
+			const result = formatPathForCode('/home/user/Documents/file.txt', [
+				{ uri: URI.file('/home/user/project') },
+				{ uri: URI.file('/home/user'), prefix: '~/' }
+			]);
+			assert.strictEqual(result, '"~/Documents/file.txt"');
+		});
+
 	});
 
-	test('Convert multiple Windows file paths', () => {
-		const uriListData = 'file:///c%3A/Users/test/file1.txt\r\nfile:///c%3A/Users/test/file2.txt';
-		const result = convertClipboardFiles(uriListData);
-		assert.deepStrictEqual(result, ['"c:/Users/test/file1.txt"', '"c:/Users/test/file2.txt"']);
-	});
+	suite('convertClipboardFiles', () => {
 
-	test('File path with quotes is escaped correctly', () => {
-		// Double quotes are allowed in filenames on macOS/Linux (but not Windows)
-		// Example URI captured from real macOS file copy: test"file.txt
-		const uriListData = 'file:///Users/test/test%22file.txt';
-		const result = convertClipboardFiles(uriListData);
-		assert.deepStrictEqual(result, ['"/Users/test/test\\"file.txt"']);
-	});
+		test('Returns null for empty input', () => {
+			const result = convertClipboardFiles('');
+			assert.strictEqual(result, null);
+		});
 
-	// The isUNC() utility used to detect UNC paths literally only works on
-	// Windows.
-	(isWindows ? test : test.skip)('UNC path is not converted [can only be tested on Windows]', () => {
-		// This tests the real-world scenario where \\localhost\C$\path becomes localhost/C$/path after URI decoding
-		const uriListData = 'file://localhost/C$/Users/test/file.txt';
-		const result = convertClipboardFiles(uriListData);
-		assert.strictEqual(result, null);
-	});
+		// The isUNC() utility used to detect UNC paths literally only works on
+		// Windows.
+		(isWindows ? test : test.skip)('UNC path is not converted [can only be tested on Windows]', () => {
+			// This tests the real-world scenario where \\localhost\C$\path becomes localhost/C$/path after URI decoding
+			const uriListData = 'file://localhost/C$/Users/test/file.txt';
+			const result = convertClipboardFiles(uriListData);
+			assert.strictEqual(result, null);
+		});
 
-	(isWindows ? test : test.skip)('No conversion for mix of regular and UNC paths [can only be tested on Windows]', () => {
-		const uriListData = 'file:///c%3A/Users/test/file.txt\r\nfile://localhost/C$/Users/test/file.txt';
-		const result = convertClipboardFiles(uriListData);
-		assert.strictEqual(result, null);
-	});
+		(isWindows ? test : test.skip)('No conversion for mix of regular and UNC paths [can only be tested on Windows]', () => {
+			const uriListData = 'file:///c%3A/Users/test/file.txt\r\nfile://localhost/C$/Users/test/file.txt';
+			const result = convertClipboardFiles(uriListData);
+			assert.strictEqual(result, null);
+		});
 
-	test('Returns null for empty input', () => {
-		const result = convertClipboardFiles('');
-		assert.strictEqual(result, null);
-	});
+		test('Convert multiple Windows file paths', () => {
+			const uriListData = 'file:///c%3A/Users/test/file1.txt\r\nfile:///c%3A/Users/test/file2.txt';
+			const result = convertClipboardFiles(uriListData);
+			assert.deepStrictEqual(result, ['"c:/Users/test/file1.txt"', '"c:/Users/test/file2.txt"']);
+		});
 
-	test('Absolute path when no bases provided', () => {
-		const uriListData = 'file:///c%3A/Users/test/project/file.txt';
-		const result = convertClipboardFiles(uriListData, []);
-		assert.deepStrictEqual(result, ['"c:/Users/test/project/file.txt"']);
-	});
-
-	test('Workspace-relative path when file is inside workspace', () => {
-		const uriListData = 'file:///c%3A/Users/test/project/src/components/file.txt';
-		const result = convertClipboardFiles(uriListData, [{ uri: URI.file('c:/Users/test/project') }]);
-		assert.deepStrictEqual(result, ['"src/components/file.txt"']);
-	});
-
-	test('Absolute path when file is outside workspace', () => {
-		const uriListData = 'file:///c%3A/Users/test/other-project/file.txt';
-		const result = convertClipboardFiles(uriListData, [{ uri: URI.file('c:/Users/test/project') }]);
-		assert.deepStrictEqual(result, ['"c:/Users/test/other-project/file.txt"']);
-	});
-
-	test('Home-relative path when file is in home directory but outside workspace', () => {
-		const uriListData = 'file:///c%3A/Users/test/Documents/file.txt';
-		const result = convertClipboardFiles(uriListData, [
-			{ uri: URI.file('c:/Users/test/project') },
-			{ uri: URI.file('c:/Users/test'), prefix: '~/' }
-		]);
-		assert.deepStrictEqual(result, ['"~/Documents/file.txt"']);
-	});
-
-	test('Workspace-relative takes priority over home-relative', () => {
-		const uriListData = 'file:///c%3A/Users/test/project/src/file.txt';
-		const result = convertClipboardFiles(uriListData, [
-			{ uri: URI.file('c:/Users/test/project') },
-			{ uri: URI.file('c:/Users/test'), prefix: '~/' }
-		]);
-		// Should use workspace-relative, not ~/project/src/file.txt
-		assert.deepStrictEqual(result, ['"src/file.txt"']);
-	});
-
-	test('Multiple files with mixed relative paths', () => {
-		const uriListData = 'file:///c%3A/Users/test/project/src/file1.txt\r\nfile:///c%3A/Users/test/other/file2.txt';
-		const result = convertClipboardFiles(uriListData, [
-			{ uri: URI.file('c:/Users/test/project') },
-			{ uri: URI.file('c:/Users/test'), prefix: '~/' }
-		]);
-		assert.deepStrictEqual(result, [
-			'"src/file1.txt"',  // workspace-relative
-			'"~/other/file2.txt"'  // home-relative
-		]);
-	});
-
-	test('Absolute path when file is outside both workspace and home', () => {
-		const uriListData = 'file:///d%3A/external/file.txt';
-		const result = convertClipboardFiles(uriListData, [
-			{ uri: URI.file('c:/Users/test/project') },
-			{ uri: URI.file('c:/Users/test'), prefix: '~/' }
-		]);
-		assert.deepStrictEqual(result, ['"d:/external/file.txt"']);
-	});
-
-	test('Home-relative when only home base is provided', () => {
-		const uriListData = 'file:///c%3A/Users/test/Documents/file.txt';
-		const result = convertClipboardFiles(uriListData, [{ uri: URI.file('c:/Users/test'), prefix: '~/' }]);
-		assert.deepStrictEqual(result, ['"~/Documents/file.txt"']);
-	});
-
-	test('Absolute path when bases array is empty', () => {
-		const uriListData = 'file:///c%3A/Users/test/file.txt';
-		const result = convertClipboardFiles(uriListData, []);
-		assert.deepStrictEqual(result, ['"c:/Users/test/file.txt"']);
-	});
-
-	test('POSIX paths: workspace-relative', () => {
-		const uriListData = 'file:///home/user/project/src/file.txt';
-		const result = convertClipboardFiles(uriListData, [{ uri: URI.file('/home/user/project') }]);
-		assert.deepStrictEqual(result, ['"src/file.txt"']);
-	});
-
-	test('POSIX paths: home-relative', () => {
-		const uriListData = 'file:///home/user/Documents/file.txt';
-		const result = convertClipboardFiles(uriListData, [
-			{ uri: URI.file('/home/user/project') },
-			{ uri: URI.file('/home/user'), prefix: '~/' }
-		]);
-		assert.deepStrictEqual(result, ['"~/Documents/file.txt"']);
-	});
-
-	test('Relative path with spaces preserved', () => {
-		const uriListData = 'file:///c%3A/Users/test/project/My%20Documents/file.txt';
-		const result = convertClipboardFiles(uriListData, [{ uri: URI.file('c:/Users/test/project') }]);
-		assert.deepStrictEqual(result, ['"My Documents/file.txt"']);
+		test('Multiple files with mixed relative paths', () => {
+			const uriListData = 'file:///c%3A/Users/test/project/src/file1.txt\r\nfile:///c%3A/Users/test/other/file2.txt';
+			const result = convertClipboardFiles(uriListData, [
+				{ uri: URI.file('c:/Users/test/project') },
+				{ uri: URI.file('c:/Users/test'), prefix: '~/' }
+			]);
+			assert.deepStrictEqual(result, [
+				'"src/file1.txt"',  // workspace-relative
+				'"~/other/file2.txt"'  // home-relative
+			]);
+		});
 	});
 });
