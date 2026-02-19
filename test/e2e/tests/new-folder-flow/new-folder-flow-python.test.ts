b28c1158385e6d72c71c23558d19bb5b0f350046
Austin Dickey <austin3spammy@gmail.com>
2026-02-19 11:59:08 -0600

    delete the annotation too

--- test/e2e/tests/new-folder-flow/new-folder-flow-python.test.ts
+++ test/e2e/tests/new-folder-flow/new-folder-flow-python.test.ts
@@ -13,7 +13,6 @@ test.use({
 
 // Not running conda test on windows because conda reeks havoc on selecting the correct python interpreter
 test.describe('New Folder Flow: Python Project', {
-	annotation: [{ type: 'issue', description: 'https://github.com/posit-dev/positron/issues/9999' }],
 	tag: [tags.MODAL, tags.NEW_FOLDER_FLOW, tags.WEB]
 }, () => {
 	const folderTemplate = FolderTemplate.PYTHON_PROJECT;
