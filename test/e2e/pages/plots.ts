e921a1157694a0f7b0a0cba87e9b76f1af3f9ff6
Dhruvi Sompura <dhruvi.sompura@posit.co>
2026-02-26 17:04:13 -0800

    Add overflow menu selection support to openPlotIn

--- test/e2e/pages/plots.ts
+++ test/e2e/pages/plots.ts
@@ -19,6 +19,8 @@ const PLOT_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .pos
 const SAVE_PLOT_FROM_PLOTS_PANE_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Save plot"]';
 const COPY_PLOT_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Copy plot to clipboard"]';
 const ZOOM_PLOT_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Fit"]';
+const OPEN_IN_EDITOR_DROPDOWN_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Select where to open plot"]';
+const OVERFLOW_MENU_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="overflow"]';
 const OUTER_WEBVIEW_FRAME = '.webview';
 const INNER_WEBVIEW_FRAME = '#active-frame';
 
@@ -176,11 +178,36 @@ export class Plots {
 			'editor tab to the side': /Open in editor tab to the Side$/
 		};
 		await test.step(`Open plot in: ${plotLocation}`, async () => {
-			await this.contextMenu.triggerAndClick({
-				menuTrigger: this.code.driver.page.getByRole('button', { name: 'Select where to open plot' }),
-				menuItemLabel: menuItemRegex[plotLocation],
-				menuItemType: 'menuitemcheckbox'
-			});
+			// The "Open in Editor" button may be visible in the action bar or overflowed into the overflow menu.
+			// First check if the dropdown button is visible, otherwise use the overflow menu.
+
+			const openInEditorButton = this.code.driver.page.locator(OPEN_IN_EDITOR_DROPDOWN_BUTTON);
+			const overflowButton = this.code.driver.page.locator(OVERFLOW_MENU_BUTTON);
+
+			if (await openInEditorButton.isVisible()) {
+				// Button is visible in action bar - use the dropdown
+				await this.contextMenu.triggerAndClick({
+					menuTrigger: openInEditorButton,
+					menuItemLabel: menuItemRegex[plotLocation],
+					menuItemType: 'menuitemcheckbox'
+				});
+			} else if (await overflowButton.isVisible()) {
+				// Button overflowed - use the overflow menu and its submenu
+				await overflowButton.click();
+				const overflowMenu = this.code.driver.page.locator('.custom-context-menu-items');
+				await expect(overflowMenu).toBeVisible();
+
+				// Click on the "Open in Editor" menu option to see submenu entries
+				const openInEditorSubmenu = overflowMenu.getByText('Open in Editor');
+				await openInEditorSubmenu.click();
+
+				// Wait for submenu to appear and click the appropriate item
+				const submenuItem = this.code.driver.page.locator('.custom-context-menu-items').last().getByText(menuItemRegex[plotLocation]);
+				await expect(submenuItem).toBeVisible();
+				await submenuItem.click();
+			} else {
+				throw new Error('Could not find "Open in Editor" button in action bar or overflow menu');
+			}
 		});
 	}
 
