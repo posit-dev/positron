afd776cd33e58c87539aa2cd419666dce84870bf
Sam Clark <sam.clark@posit.co>
2026-03-09 13:16:58 -0500

    Updates EmbeddedLink component to handle custom links (e.g., commands)

--- src/vs/base/browser/ui/positronComponents/embeddedLink/EmbeddedLink.tsx
+++ src/vs/base/browser/ui/positronComponents/embeddedLink/EmbeddedLink.tsx
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2025-2026 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -7,14 +7,23 @@ import './embeddedLink.css';
 
 import React from 'react';
 
+interface EmbeddedLinkProps {
+	/** Called after any link (regular or command) is clicked. */
+	onLinkClick?: (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+}
+
 /**
  * A component that displays a string and converts any markdown links in the string to a href links.
  * It also handles paragraphs by splitting the input text by newlines and wrapping each paragraph
  * in appropriate HTML elements.
  *
+ * Links whose URL starts with `command:` are rendered as `<button>` elements and handled via
+ * the `onCommandLink` callback. All other links open in a new tab.
+ *
  * @param props the string to display containing markdown links to convert to a link
  */
-export const EmbeddedLink = (props: React.PropsWithChildren) => {
+export const EmbeddedLink = (props: React.PropsWithChildren<EmbeddedLinkProps>) => {
+	const { onLinkClick } = props;
 	// capture markdown links in the format [text](url)
 	const regex = /\[([^\]]*)\]\(([^)]+)\)/g;
 	const text = props.children as string;
@@ -33,7 +42,7 @@ export const EmbeddedLink = (props: React.PropsWithChildren) => {
 				const link = parts[index + 1];
 				const linkText = part || link; // Use URL as text if no text provided
 				return (
-					<a key={index} href={link} rel='noreferrer' target='_blank'>
+					<a key={index} href={link} rel='noreferrer' target='_blank' onClick={(e) => onLinkClick?.(e)}>
 						{linkText}
 					</a>
 				);
