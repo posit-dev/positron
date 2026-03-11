9d9f53c19f0758eb16f855b38a7584fccd14137e
Jonathan <jonathan@rstudio.com>
2026-03-11 13:41:02 -0700

    Remember choice in plot 'open in editor' action (#12378)
    Fixes persistence of the user's choice in the Open in Editor action in
    the Plots pane. The problem was pretty simple; all the commands were
    mapped to the same ID.
    
    Also adds a test to protect this code path going forward.
    
    Addresses https://github.com/posit-dev/positron/issues/11654.
    
    ### Release Notes
    
    <!--
    Optionally, replace `N/A` with text to be included in the next release
    notes.
    The `N/A` bullets are ignored. If you refer to one or more Positron
    issues,
    these issues are used to collect information about the feature or
    bugfix, such
    as the relevant language pack as determined by Github labels of type
    `lang: `.
      The note will automatically be tagged with the language.
    
    These notes are typically filled by the Positron team. If you are an
    external
      contributor, you may ignore this section.
    -->
    
    #### New Features
    
    - N/A
    
    #### Bug Fixes
    
    - Fix issue persisting selection in the Open in Editor menu in the Plots
    pane (#11654)
    
    
    ### QA Notes
    
    Easy to repro using notes in #11654. 
    
    Test tags: `@:plots`

--- test/e2e/pages/plots.ts
+++ test/e2e/pages/plots.ts
@@ -19,6 +19,7 @@ const PLOT_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .pos
 const SAVE_PLOT_FROM_PLOTS_PANE_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Save plot"]';
 const COPY_PLOT_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Copy plot to clipboard"]';
 const ZOOM_PLOT_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Fit"]';
+const OPEN_IN_EDITOR_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Open in editor tab"]';
 const OPEN_IN_EDITOR_DROPDOWN_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="Select where to open plot"]';
 const OVERFLOW_MENU_BUTTON = '.positron-plots-container .positron-dynamic-action-bar .positron-button[aria-label="overflow"]';
 const ORIGIN_FILE_BUTTON = '.plot-origin-file';
@@ -233,6 +234,31 @@ export class Plots {
 			} else {
 				throw new Error('Could not find "Open in Editor" button in action bar or overflow menu');
 			}
+		});
+	}
+
+	async clickOpenInEditorButton() {
+		await test.step('Click the main Open in Editor button', async () => {
+			const openInEditorButton = this.code.driver.page.locator(OPEN_IN_EDITOR_BUTTON);
+			await openInEditorButton.click();
+		});
+	}
+
+	async verifyOpenPlotDropdownCheckedOption(expectedOption: PlotLocations) {
+		const menuItemLabels: Record<PlotLocations, string> = {
+			'editor': 'Open in editor tab',
+			'new window': 'Open in new window',
+			'editor tab to the side': 'Open in editor tab to the Side'
+		};
+		await test.step(`Verify dropdown checked option is: ${expectedOption}`, async () => {
+			const openInEditorButton = this.code.driver.page.locator(OPEN_IN_EDITOR_DROPDOWN_BUTTON);
+			await this.contextMenu.triggerAndVerifyMenuItems({
+				menuTrigger: openInEditorButton,
+				menuItemStates: Object.entries(menuItemLabels).map(([key, label]) => ({
+					label,
+					checked: key === expectedOption
+				}))
+			});
 		});
 	}
 
