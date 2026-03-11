bf3abcc49d9a64919157022a804a1205ac25592b
Nick Strayer <nick.strayer@posit.co>
2026-03-11 16:55:43 -0400

    refactor: migrate @dnd-kit packages from vendored ESM to npm build pipeline
    Replace hand-vendored esm.sh bundles in src/esm-package-dependencies/
    with proper npm dependencies built through the ESM package build system.
    Removes the build filter exclusion and updates workbench HTML files to
    load the packages from the build output.

--- src/esm-package-dependencies/sortable.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-// @dnd-kit/sortable - MIT License - https://github.com/clauderic/dnd-kit
-import "./v135/@dnd-kit/utilities@3.2.2/es2022/utilities.mjs";
-import "./core.js";
-export * from "./v135/@dnd-kit/sortable@10.0.0/es2022/sortable.mjs";
