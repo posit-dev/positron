6f1d6e222c29e76e9bd10d64dede32b1871befaf
seem <mwlorgat@gmail.com>
2026-03-13 16:01:02 +0200

    refactor: move styles to cellactionbutton so we can reuse it in both action bars

--- src/vs/workbench/contrib/positronNotebook/browser/notebookCells/actionBar/CellActionButton.tsx
+++ src/vs/workbench/contrib/positronNotebook/browser/notebookCells/actionBar/CellActionButton.tsx
@@ -3,6 +3,8 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import './CellActionButton.css';
+
 import { CellSelectionType } from '../../selectionMachine.js';
 import { useNotebookInstance } from '../../NotebookInstanceProvider.js';
 import { ActionButton } from '../../utilityComponents/ActionButton.js';
@@ -10,6 +12,7 @@ import { IPositronNotebookCell } from '../../PositronNotebookCells/IPositronNote
 import { MenuItemAction, SubmenuItemAction } from '../../../../../../platform/actions/common/actions.js';
 import { DevErrorIcon, Icon } from '../../../../../../platform/positronActionBar/browser/components/icon.js';
 import { IHoverManager } from '../../../../../../platform/hover/browser/hoverManager.js';
+import { positronClassNames } from '../../../../../../base/common/positronUtilities.js';
 
 
 interface CellActionButtonProps {
@@ -46,7 +49,9 @@ export function CellActionButton({ action, cell, hoverManager, showSeparator }: 
 		<ActionButton
 			key={action.id}
 			ariaLabel={action.label}
-			className={showSeparator ? 'separator-after' : ''}
+			className={positronClassNames('cell-action-button', {
+				'separator-after': showSeparator,
+			})}
 			hoverManager={hoverManager}
 			// Match VSCode behavior: prefer tooltip but default to label
 			tooltip={action.tooltip && action.tooltip.length > 0 ? action.tooltip : action.label}
