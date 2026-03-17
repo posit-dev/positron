93b9ca3491a76bbdbefad46b4b7e9fe74d650475
Jonathan McPherson <jonathan@posit.co>
2026-03-16 17:45:16 -0700

    i don't know how to make it any clearer pyright

--- extensions/positron-python/python_files/posit/positron/positron_ipkernel.py
+++ extensions/positron-python/python_files/posit/positron/positron_ipkernel.py
@@ -228,8 +228,8 @@ class PositronMagics(Magics):
 
         kernel = self.shell.kernel
         parent = kernel.get_parent("shell")
-        content: dict = parent.get("content", {})
-        positron_meta: dict = content.get("positron", {})
+        content: dict = cast(dict, parent.get("content", {}))
+        positron_meta: dict = cast(dict, content.get("positron", {}))
         print(_json.dumps(positron_meta, sort_keys=True))
 
 
