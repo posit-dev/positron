1f9af50052212f9fd247f0ab4145783b291d9c6c
isabel zimmerman <isabel.zimmerman@posit.co>
2026-03-18 10:14:17 -0400

    use replace instead of insert

--- build/darwin/sign.ts
+++ build/darwin/sign.ts
@@ -97,7 +97,7 @@ async function main(buildDir?: string): Promise<void> {
 	// universal will get its copy from the x64 build.
 	if (arch !== 'universal') {
 		await spawn('plutil', [
-			'-insert',
+			'-replace',
 			'NSAppleEventsUsageDescription',
 			'-string',
 			'An application in Visual Studio Code wants to use AppleScript.',
