777defe77bd122c21a90eb116f5262ef91531b40
Brian Lambert <brianlambert@gmail.com>
2026-04-07 19:08:02 -0700

    Refactoring some comments

--- src/vs/workbench/contrib/positronDataConnections/browser/dialogs/newDataConnectionModalDialog.tsx
+++ src/vs/workbench/contrib/positronDataConnections/browser/dialogs/newDataConnectionModalDialog.tsx
@@ -2,3 +2,41 @@
  *  Copyright (C) 2026 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
+
+// CSS.
+import './newDataConnectionModalDialog.css';
+
+// Other dependencies.
+import { PositronModalReactRenderer } from '../../../../../base/browser/positronModalReactRenderer.js';
+import { ContentArea } from '../../../../browser/positronComponents/positronModalDialog/components/contentArea.js';
+import { PositronModalDialog } from '../../../../browser/positronComponents/positronModalDialog/positronModalDialog.js';
+import { OKCancelActionBar } from '../../../../browser/positronComponents/positronModalDialog/components/okCancelActionBar.js';
+
+/**
+ * NewDataConnectionModalDialogProps interface.
+ */
+interface NewDataConnectionModalDialogProps {
+	renderer: PositronModalReactRenderer;
+}
+
+/**
+ * NewDataConnectionModalDialog component.
+ * @param props The component properties.
+ * @returns The rendered component.
+ */
+export const NewDataConnectionModalDialog = (props: NewDataConnectionModalDialogProps) => {
+	// Render.
+	return (
+		<PositronModalDialog height={400} renderer={props.renderer} title='Yes' width={400} >
+			<ContentArea>
+				<div>Hello</div>
+			</ContentArea>
+			<OKCancelActionBar
+				cancelButtonTitle='Cancel'
+				okButtonTitle='OK'
+				onAccept={() => console.log('OK')}
+				onCancel={() => console.log('Cancel')}
+			/>
+		</PositronModalDialog>
+	);
+};
