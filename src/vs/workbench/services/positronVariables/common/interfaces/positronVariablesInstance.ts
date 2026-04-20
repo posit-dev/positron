0feec223f2e6f96fb6e432f933eafb67c4cd7083
Julia Silge <julia.silge@gmail.com>
2026-04-20 15:14:23 -0600

    Expose `variableItems` on interface

--- src/vs/workbench/services/positronVariables/common/interfaces/positronVariablesInstance.ts
+++ src/vs/workbench/services/positronVariables/common/interfaces/positronVariablesInstance.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2023-2024 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2023-2026 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -69,6 +69,11 @@ export interface IPositronVariablesInstance {
 	 * Gets the runtime session.
 	 */
 	readonly session: ILanguageRuntimeSession;
+
+	/**
+	 * Gets the top-level variable items currently known for this session.
+	 */
+	readonly variableItems: IVariableItem[];
 
 	/**
 	 * Gets the state.
