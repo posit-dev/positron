16ce4de9939ac3e0417439cc875651214460fa13
Marie Idleman <marie.idleman@posit.co>
2026-06-10 15:43:54 -0500

    quarto resize smaller

--- test/e2e/release-screenshots/quarto/quarto-hello-python.screenshot.ts
+++ test/e2e/release-screenshots/quarto/quarto-hello-python.screenshot.ts
@@ -54,7 +54,7 @@ test.describe('Release Screenshots - Quarto', () => {
 	test('Release Screenshot - quarto-hello-python.png', async ({ app, page, openFile, python }) => {
 		const { editorActionBar, viewer, hotKeys, layouts, sessions } = app.workbench;
 
-		await setScreenshotWindowSize(app, { width: 1240, height: 800 });
+		await setScreenshotWindowSize(app, { width: 900, height: 600 });
 		await sessions.expectAllSessionsToBeReady();
 
 		writeFileSync(join(app.workspacePathOrFolder, 'hello.qmd'), HELLO_QMD);
