3841f7e449884fe6ee069e8ca9a4622cb068baa2
Rodrigo Silva Ferreira <129895386+rodrigosf672@users.noreply.github.com>
2026-06-12 16:21:04 -0400

    comment to explain why a step and not new test (and retrigger ci)
    Signed-off-by: Rodrigo Silva Ferreira <129895386+rodrigosf672@users.noreply.github.com>

--- test/e2e/tests/autocomplete/autocomplete-notebook-console.test.ts
+++ test/e2e/tests/autocomplete/autocomplete-notebook-console.test.ts
@@ -71,6 +71,7 @@ test.describe('Autocomplete with Notebook Console', {
 		await editors.expectSuggestionListCount(1, { retryTimeout: 30000 });
 
 		// Closing the notebook removes its console session (#12940)
+		// As a step in the end here instead of new test to make faster/optimal in CI
 		await test.step('Closing the notebook removes its console session', async () => {
 			await page.getByRole('tab', { name: 'Untitled-1.ipynb' }).click();
 			await runCommand('workbench.action.revertAndCloseActiveEditor');
