d906e3524257fa9cbd9fcbb25bc23415d1c87f48
Metehan Kaygısız <metehankaygsz@yahoo.com.tr>
2026-06-13 09:00:42 +0300

    Test input-boundary RPC in Quarto session harness

--- src/vs/workbench/services/runtimeSession/test/common/testLanguageRuntimeSession.ts
+++ src/vs/workbench/services/runtimeSession/test/common/testLanguageRuntimeSession.ts
@@ -161,6 +161,10 @@ export class TestLanguageRuntimeSession extends Disposable implements ILanguageR
 		throw new Error('Not implemented.');
 	}
 
+	async callMethod(_method: string, ..._args: unknown[]): Promise<unknown> {
+		throw new Error('Not implemented.');
+	}
+
 	getDynState(): Promise<ILanguageRuntimeSessionState> {
 		return Promise.resolve(this.dynState);
 	}
