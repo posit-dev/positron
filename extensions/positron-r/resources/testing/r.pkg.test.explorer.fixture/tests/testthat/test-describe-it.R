c526ed0e4696edda8777ae8499e3c82ab68dc45a
Jenny Bryan <jenny.f.bryan@gmail.com>
2026-06-12 17:01:39 -0700

    Basic test_that() and describe() / it() usage

---
+++ extensions/positron-r/resources/testing/r.pkg.test.explorer.fixture/tests/testthat/test-describe-it.R
@@ -0,0 +1,14 @@
+describe("simple describe() 1", {
+   it("it number 1-1", {
+     expect_equal(10 * 2, 20)
+   })
+   it("it number 1-2", {
+    expect_length(month.abb, 12)
+   })
+})
+
+describe("simple describe() 2", {
+   it("it number 2-1", {
+     expect_equal(10 * 2, 20)
+   })
+})
---
+++ extensions/positron-r/resources/testing/r.pkg.test.explorer.fixture/tests/testthat/test-test-that.R
@@ -0,0 +1,7 @@
+test_that("test_that number 1", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("test_that number 2", {
+  expect_equal(2 * 2, 4)
+})
