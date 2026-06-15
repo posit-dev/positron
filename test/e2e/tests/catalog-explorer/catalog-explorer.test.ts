ba3d7d8a23c0e81a7d1d0baeefaab82efea6de8b
Rodrigo Silva Ferreira <rodrigo.ferreira@posit.co>
2026-06-15 14:43:11 -0400

    e2e: import expect from _test.setup instead of @playwright/test

--- test/e2e/tests/catalog-explorer/catalog-explorer.test.ts
+++ test/e2e/tests/catalog-explorer/catalog-explorer.test.ts
@@ -3,8 +3,7 @@
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { expect } from '@playwright/test';
-import { test, tags } from '../_test.setup';
+import { test, expect, tags } from '../_test.setup';
 
 test.use({
 	suiteId: __filename
