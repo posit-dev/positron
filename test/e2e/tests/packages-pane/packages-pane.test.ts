00b393b26ce58edd3c53c954c84be497d91f6906
Christopher Mead <chris.mead@posit.co>
2026-06-17 12:24:30 -0600

    E2E test:untag windows for R packages pane test (#14349)
    Just removing a tag due to an issue.

--- test/e2e/tests/packages-pane/packages-pane.test.ts
+++ test/e2e/tests/packages-pane/packages-pane.test.ts
@@ -63,7 +63,10 @@ test.describe('Packages Pane', {
 			});
 	});
 
-	test('R - Install, search, and uninstall package', { tag: [tags.WIN] },
+	test.skip('R - Install, search, and uninstall package', {
+		tag: [tags.WIN],
+		annotation: { type: 'issue', description: 'https://github.com/posit-dev/positron/issues/14346' }
+	},
 		async function ({ app, r: _r }) {
 			const { packages } = app.workbench;
 
