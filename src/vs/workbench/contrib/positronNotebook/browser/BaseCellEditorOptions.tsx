b58a35a58320f4832350a9822f8932a02687e2ab
Dhruvi Sompura <dhruvi.sompura@posit.co>
2026-06-17 14:05:24 -0700

    Remove declaration of lineDecorationsWidth since we are using default value

--- src/vs/workbench/contrib/positronNotebook/browser/BaseCellEditorOptions.tsx
+++ src/vs/workbench/contrib/positronNotebook/browser/BaseCellEditorOptions.tsx
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+ *  Copyright (C) 2024-2026 Posit Software, PBC. All rights reserved.
  *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
@@ -30,7 +30,6 @@ export class BaseCellEditorOptions extends Disposable implements IBaseCellEditor
 		},
 		renderLineHighlightOnlyWhenFocus: true,
 		overviewRulerLanes: 0,
-		lineDecorationsWidth: 10,
 		folding: true,
 		fixedOverflowWidgets: true,
 		minimap: { enabled: false },
