662679563e1f8559301b74776b5d20418a724094
Chris Mead <chris.mead@posit.co>
2026-06-23 12:56:39 -0600

    E2E test: try to add postgres tests to windows

--- test/e2e/tests/connections/connections-postgres.test.ts
+++ test/e2e/tests/connections/connections-postgres.test.ts
@@ -15,7 +15,7 @@ const user = process.env.E2E_POSTGRES_USER || 'testuser';
 const password = process.env.E2E_POSTGRES_PASSWORD || 'testpassword';
 
 test.describe('Postgres DB Connection', {
-	tag: [tags.WEB, tags.CONNECTIONS, tags.WORKBENCH]
+	tag: [tags.WEB, tags.WIN, tags.CONNECTIONS, tags.WORKBENCH]
 }, () => {
 
 	test('Python - Can establish a Postgres connection to a docker container', async function ({ app, hotKeys, python }) {
