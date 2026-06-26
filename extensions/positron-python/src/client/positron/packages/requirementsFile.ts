23d4fe359157b89f4f8cdc0f4bc881434c42cc78
Brice Stacey <bricestacey@gmail.com>
2026-06-26 14:38:56 -0400

    Fix copyright header wording to match repo convention

--- extensions/positron-python/src/client/positron/packages/requirementsFile.ts
+++ extensions/positron-python/src/client/positron/packages/requirementsFile.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (C) 2026 Posit Software, PBC. All rights reserved.
- *  Licensed under the Elastic License 2.0. See LICENSE.txt in the project root for license information.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
  *--------------------------------------------------------------------------------------------*/
 
 /**
