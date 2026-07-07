a5ef2bffb248bb39fc40acee9c9f84f4b78af140
Marie Idleman <marie.idleman@posit.co>
2026-07-07 17:52:18 -0500

    test: scenario C - add unmapped positron* dir (stale + missing) DO NOT MERGE

---
+++ src/vs/workbench/contrib/positronScenarioTest/scenario.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (C) 2024-2026 Posit Software, PBC. All rights reserved.
+ *  Licensed under the Elastic License 2.0. See LICENSE.txt for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// Throwaway file for testing the weekly test-tag-map check (Scenario C:
+// an unmapped positron* dir). DO NOT MERGE.
+export const scenario = 'test-tag-map-missing-dir';
