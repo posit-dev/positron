3c3e39082991b51b1ad9ba16ba678d168f3e2d55
Michael Burtin <michael.burtin@univ-poitiers.fr>
2026-09-11 17:04:33 +0200

    WebBrowser: fix overlay for Positron modal popup and dialog

--- src/vs/workbench/contrib/browserView/electron-browser/overlayManager.ts
+++ src/vs/workbench/contrib/browserView/electron-browser/overlayManager.ts
@@ -30,7 +30,11 @@ const OVERLAY_DEFINITIONS: ReadonlyArray<{ className: string; type: BrowserOverl
 	{ className: 'notifications-center', type: BrowserOverlayType.Notification },
 	{ className: 'notification-toast-container', type: BrowserOverlayType.Notification },
 	// Context view is very generic, so treat the content as unknown
-	{ className: 'context-view', type: BrowserOverlayType.Unknown }
+	{ className: 'context-view', type: BrowserOverlayType.Unknown },
+	// --- Start Positron ---
+	{ className: 'positron-modal-popup', type: BrowserOverlayType.Menu },
+	{ className: 'positron-modal-dialog-container', type: BrowserOverlayType.Dialog }
+	// --- End Positron ---
 ];
 
 const HIT_TEST_EXCLUDED_CLASSES = [
